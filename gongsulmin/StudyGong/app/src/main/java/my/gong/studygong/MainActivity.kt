2d6a96b310c037c1e9ffb7f0deb4d1bf164a9639
GongSulMin <tv302@naver.com>
2019-04-23 01:52:49 +0900

    Android Init

---
+++ gongsulmin/StudyGong/app/src/main/java/my/gong/studygong/MainActivity.kt
@@ -0,0 +1,12 @@
+package my.gong.studygong
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
