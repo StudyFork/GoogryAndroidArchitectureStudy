815dff46db97758c959c1db8e9b76c73d2ee67d1
Hoyuo <psh9907@gmail.com>
2019-04-30 01:11:16 +0900

    모델 추가
    * [Change Type] 기능구현
    * [Issue#] APP-002
    * [Cause & Measure]
      * 모델 추가
         * `Market`, `Ticker`
    * [Impact Range] All
    * [Developer] Sanghyun PARK (@hoyuo)

---
+++ hoyuo/MyApplication/app/src/main/java/me/hoyuo/myapplication/model/upbit/Market.kt
@@ -0,0 +1,7 @@
+package me.hoyuo.myapplication.model.upbit
+
+data class Market(
+    val english_name: String,
+    val korean_name: String,
+    val market: String
+)
---
+++ hoyuo/MyApplication/app/src/main/java/me/hoyuo/myapplication/model/upbit/Ticker.kt
@@ -0,0 +1,30 @@
+package me.hoyuo.myapplication.model.upbit
+
+data class Ticker(
+    val acc_trade_price: Double,
+    val acc_trade_price_24h: Double,
+    val acc_trade_volume: Double,
+    val acc_trade_volume_24h: Double,
+    val change: String,
+    val change_price: Int,
+    val change_rate: Double,
+    val high_price: Int,
+    val highest_52_week_date: String,
+    val highest_52_week_price: Int,
+    val low_price: Int,
+    val lowest_52_week_date: String,
+    val lowest_52_week_price: Int,
+    val market: String,
+    val opening_price: Int,
+    val prev_closing_price: Int,
+    val signed_change_price: Int,
+    val signed_change_rate: Double,
+    val timestamp: Long,
+    val trade_date: String,
+    val trade_date_kst: String,
+    val trade_price: Int,
+    val trade_time: String,
+    val trade_time_kst: String,
+    val trade_timestamp: Long,
+    val trade_volume: Double
+)
