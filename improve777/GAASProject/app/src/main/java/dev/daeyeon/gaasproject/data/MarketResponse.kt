f1e45602d01e45f82bb777dc26b0cb8f34b16def
improve777 <improve777@gmail.com>
2019-04-30 17:01:42 +0900

    data class 추가

---
+++ improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/data/MarketResponse.kt
@@ -0,0 +1,9 @@
+package dev.daesin.gaasdy.data
+
+import com.google.gson.annotations.SerializedName
+
+data class MarketResponse(
+    @SerializedName("market") val market: String,
+    @SerializedName("korean_name") val koreanName: String,
+    @SerializedName("english_name") val englishName: String
+)
