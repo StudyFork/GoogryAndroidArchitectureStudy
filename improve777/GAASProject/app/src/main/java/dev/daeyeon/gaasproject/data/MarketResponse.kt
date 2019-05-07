356b81900176d68a6c73ffb408f2e384bdf3aa7b
Dae Yeon Kim <improve777@gmail.com>
2019-05-07 21:05:57 +0900

    HW-2 data 패키지 내부 클래스 reformat code

--- improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/data/MarketResponse.kt
+++ improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/data/MarketResponse.kt
@@ -3,7 +3,7 @@ package dev.daeyeon.gaasproject.data
 import com.google.gson.annotations.SerializedName
 
 data class MarketResponse(
-    @SerializedName("market") val market: String,
-    @SerializedName("korean_name") val koreanName: String,
-    @SerializedName("english_name") val englishName: String
+        @SerializedName("market") val market: String,
+        @SerializedName("korean_name") val koreanName: String,
+        @SerializedName("english_name") val englishName: String
 )
--- improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/data/source/Ticker.kt
+++ improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/data/source/Ticker.kt
@@ -1,8 +1,8 @@
 package dev.daeyeon.gaasproject.data.source
 
 data class Ticker(
-    val market: String,
-    val tradePrice: String,
-    val signedChangeRate: String,
-    val accTradePrice24h: String
+        val market: String,
+        val tradePrice: String,
+        val signedChangeRate: String,
+        val accTradePrice24h: String
 )
