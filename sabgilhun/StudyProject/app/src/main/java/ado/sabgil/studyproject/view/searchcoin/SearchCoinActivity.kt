129fe067305bd6a7a002084d86b34afb1da289b1
김용훈 <>
2019-05-16 20:11:12 +0900

    HW 2-1 View 구현

--- sabgilhun/StudyProject/app/src/main/java/ado/sabgil/studyproject/view/searchcoin/SearchCoinActivity.kt
+++ sabgilhun/StudyProject/app/src/main/java/ado/sabgil/studyproject/view/searchcoin/SearchCoinActivity.kt
@@ -1,10 +1,12 @@
 package ado.sabgil.studyproject.view.searchcoin
 
 import ado.sabgil.studyproject.R
+import ado.sabgil.studyproject.adapter.TickerAdapter
 import ado.sabgil.studyproject.data.CoinRepositoryImpl
 import ado.sabgil.studyproject.data.model.Ticker
 import ado.sabgil.studyproject.databinding.ActivitySearchCoinBinding
 import ado.sabgil.studyproject.view.base.BaseActivity
+import android.os.Bundle
 
 class SearchCoinActivity :
     BaseActivity<ActivitySearchCoinBinding, SearchCoinContract.Presenter>(R.layout.activity_search_coin),
@@ -12,6 +14,26 @@ class SearchCoinActivity :
 
     override fun createPresenter() = SearchCoinPresenter(this, CoinRepositoryImpl)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.rvTickerList.adapter = TickerAdapter()
+
+        binding.tvSearch.setOnClickListener {
+            presenter.searchCoin(binding.etKeyword.text.toString())
+        }
+    }
+
     override fun updateCoinList(list: List<Ticker>) {
+        binding.it = list
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.subscribeCoinData()
+    }
+
+    override fun onPause() {
+        presenter.unSubscribeCoinData()
+        super.onPause()
     }
 }
