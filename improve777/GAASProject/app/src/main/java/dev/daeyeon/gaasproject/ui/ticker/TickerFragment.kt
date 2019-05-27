5f1598da69674dfda591fc993642f494a6d3b6d5
improve777 <improve777@gmail.com>
2019-05-27 14:17:59 +0900

    HW 3 TickerFragment 내부에 TickerViewModel 클래스 생성 및 구현

--- improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/ui/ticker/TickerFragment.kt
+++ improve777/GAASProject/app/src/main/java/dev/daeyeon/gaasproject/ui/ticker/TickerFragment.kt
@@ -9,21 +9,24 @@ import android.view.inputmethod.EditorInfo
 import dev.daeyeon.gaasproject.R
 import dev.daeyeon.gaasproject.data.Ticker
 import dev.daeyeon.gaasproject.data.response.ResponseCode
+import dev.daeyeon.gaasproject.data.source.UpbitDataSource
 import dev.daeyeon.gaasproject.data.source.UpbitRepository
 import dev.daeyeon.gaasproject.databinding.DialogTickerSearchBinding
 import dev.daeyeon.gaasproject.databinding.FragmentTickerBinding
 import dev.daeyeon.gaasproject.network.NetworkManager
 import org.jetbrains.anko.toast
+import kotlin.properties.Delegates
 
-class TickerFragment : Fragment(), TickerContract.View {
+class TickerFragment : Fragment() {
 
-    override lateinit var presenter: TickerContract.Presenter
     private lateinit var binding: FragmentTickerBinding
     private lateinit var tickerAdapter: TickerAdapter
 
     private val searchDialogBinding by lazy { initDialogTickerSearchBinding() }
     // 검색 다이얼로그
     private var searchDialog: AlertDialog? = null
+
+    private val tickerViewModel by lazy { TickerViewModel(UpbitRepository(NetworkManager.instance)) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,11 +40,6 @@ class TickerFragment : Fragment(), TickerContract.View {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         tickerAdapter = TickerAdapter()
         binding.rvTicker.adapter = tickerAdapter
-
-        presenter = TickerPresenter(
-            view = this,
-            upbitRepository = UpbitRepository(upbitApi = NetworkManager.instance)
-        )
 
         setHasOptionsMenu(true)
         swipeInit()
@@ -83,7 +81,7 @@ class TickerFragment : Fragment(), TickerContract.View {
             .setView(searchDialogBinding.root)
             .setPositiveButton(R.string.all_positive) { dialog, _ ->
                 dialog.dismiss()
-                presenter.loadUpbitTicker(searchDialogBinding.etSearch.text.toString())
+                tickerViewModel.loadUpbitTicker(searchDialogBinding.etSearch.text.toString())
                 searchDialogBinding.etSearch.setText("")
             }
             .setNegativeButton(R.string.all_negative) { dialog, _ ->
@@ -105,19 +103,21 @@ class TickerFragment : Fragment(), TickerContract.View {
             false
         )
 
-        binding.btnTickerSearch.setOnClickListener {
-            searchDialog?.dismiss()
-            presenter.loadUpbitTicker(binding.etSearch.text.toString())
-            binding.etSearch.setText("")
-        }
-
-        binding.etSearch.setOnEditorActionListener { _, actionId, _ ->
-            // 키보드의 검색 키
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                binding.btnTickerSearch.performClick()
-                return@setOnEditorActionListener true
+        binding.run {
+            btnTickerSearch.setOnClickListener {
+                searchDialog?.dismiss()
+                tickerViewModel.loadUpbitTicker(binding.etSearch.text.toString())
+                binding.etSearch.setText("")
             }
-            return@setOnEditorActionListener false
+
+            etSearch.setOnEditorActionListener { _, actionId, _ ->
+                // 키보드의 검색 키
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    binding.btnTickerSearch.performClick()
+                    return@setOnEditorActionListener true
+                }
+                return@setOnEditorActionListener false
+            }
         }
 
         return binding
@@ -130,14 +130,14 @@ class TickerFragment : Fragment(), TickerContract.View {
         AlertDialog.Builder(activity!!)
             .setTitle(R.string.ticker_fragment_base_currency)
             .setSingleChoiceItems(
-                presenter.getCurrencyArray(),
-                presenter.getCurrencyArray().indexOf(presenter.getBaseCurrency())
+                tickerViewModel.getCurrencyArray(),
+                tickerViewModel.getCurrencyArray().indexOf(tickerViewModel.getBaseCurrency())
             ) { _, item ->
-                presenter.setBaseCurrency(presenter.getCurrencyArray()[item])
+                tickerViewModel.setBaseCurrency(tickerViewModel.getCurrencyArray()[item])
             }
             .setPositiveButton(R.string.all_positive) { dialog, _ ->
                 dialog.dismiss()
-                presenter.loadUpbitTicker()
+                tickerViewModel.loadUpbitTicker()
             }
             .setCancelable(false)
             .create()
@@ -146,11 +146,11 @@ class TickerFragment : Fragment(), TickerContract.View {
 
     override fun onResume() {
         super.onResume()
-        presenter.start()
+        tickerViewModel.loadUpbitTicker()
     }
 
     override fun onDestroyView() {
-        presenter.cancelApi()
+        tickerViewModel.cancelApi()
         super.onDestroyView()
     }
 
@@ -165,34 +165,97 @@ class TickerFragment : Fragment(), TickerContract.View {
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light
             )
-            setOnRefreshListener { presenter.loadUpbitTicker() }
-        }
-    }
-
-    override fun showProgress() {
-        binding.srlTicker.isRefreshing = true
-    }
-
-    override fun hideProgress() {
-        binding.srlTicker.isRefreshing = false
-    }
-
-    override fun replaceTickerList(tickerList: List<Ticker>) {
-        tickerAdapter.replaceList(tickerList)
-    }
-
-    override fun toastTickerFailMsg(msg: String) {
-        when (msg) {
-            ResponseCode.CODE_NULL_SUCCESS,
-            ResponseCode.CODE_NULL_FAIL_MSG -> activity!!.toast(R.string.all_network_error)
-            ResponseCode.CODE_EMPTY_SUCCESS -> activity!!.toast(R.string.ticker_fragment_empty)
-            else -> activity!!.toast(msg)
+            setOnRefreshListener { tickerViewModel.loadUpbitTicker() }
         }
     }
 
     companion object {
         fun newInstance(): TickerFragment {
             return TickerFragment()
+        }
+    }
+
+    inner class TickerViewModel(private val upbitRepository: UpbitDataSource) {
+
+        private val ALL_CURRENCY = "전체"
+
+        private var tickerList by Delegates.observable<List<Ticker>>(arrayListOf()) { _, _, newValue ->
+            tickerAdapter.replaceList(newValue)
+        }
+
+        /**
+         * 통신 실패시 메시지
+         */
+        private var failMsg by Delegates.observable("") { _, _, newValue ->
+            when (newValue) {
+                ResponseCode.CODE_NULL_SUCCESS,
+                ResponseCode.CODE_NULL_FAIL_MSG -> activity!!.toast(R.string.all_network_error)
+                ResponseCode.CODE_EMPTY_SUCCESS -> activity!!.toast(R.string.ticker_fragment_empty)
+                else -> activity!!.toast(newValue)
+            }
+        }
+
+        /**
+         * 프로그레스바
+         */
+        private var isShowProgressBar by Delegates.observable(false) { _, oldValue, newValue ->
+            if (oldValue == newValue) {
+                return@observable
+            }
+            binding.srlTicker.isRefreshing = newValue
+        }
+
+        /**
+         * 마켓 어레이
+         */
+        private lateinit var _currencyArray: Array<String>
+
+        /**
+         * 기준 마켓
+         */
+        private lateinit var _baseCurrency: String
+
+        fun loadUpbitTicker(searchTicker: String? = null) {
+            isShowProgressBar = true
+
+            upbitRepository.getTicker(
+                getBaseCurrency(),
+                searchTicker ?: "",
+                success = {
+                    isShowProgressBar = false
+                    if (!::_currencyArray.isInitialized) {
+                        initCurrentArray()
+                    }
+                    tickerList = it
+                },
+                fail = {
+                    isShowProgressBar = false
+                    failMsg = it
+                }
+            )
+        }
+
+        fun getBaseCurrency() = if (!::_baseCurrency.isInitialized) "" else _baseCurrency
+
+        fun setBaseCurrency(baseCurrency: String) {
+            _baseCurrency = if (baseCurrency == ALL_CURRENCY) "" else baseCurrency
+        }
+
+        fun getCurrencyArray() = if (!::_currencyArray.isInitialized) emptyArray() else _currencyArray
+
+        private fun initCurrentArray() {
+            val list = arrayListOf<String>()
+            list.add(ALL_CURRENCY)
+            list.addAll(upbitRepository.markets.split(",")
+                .map { market -> market.substringBefore("-") }
+                .distinct())
+
+            _currencyArray = list.toTypedArray()
+        }
+
+        fun cancelApi() {
+            upbitRepository.cancelMarketCall()
+            upbitRepository.cancelTickerCall()
         }
     }
 }
