f7dbc2a5afb7f44a2cf667edd488cd5ae3a9a960
공공공 <tv302@naver.com>
2019-05-30 01:32:52 +0900

    HW - 3 Activity , Dialog ViewModel 적용

---
+++ gongsulmin/StudyGong/app/src/main/java/my/gong/studygong/ui/CoinListActivity.kt
@@ -0,0 +1,88 @@
+package my.gong.studygong.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import my.gong.studygong.Injection
+import my.gong.studygong.R
+import my.gong.studygong.adapter.CoinAdapter
+import my.gong.studygong.viewmodel.CoinViewModel
+import java.util.*
+
+class CoinListActivity : AppCompatActivity() {
+
+    private var timer: Timer = Timer()
+    private var coinMarket: String = "KRW"
+    private val coinMarketDialog = CoinMarketDialog()
+
+    private val coinViewModel: CoinViewModel by lazy {
+        CoinViewModel(Injection.provideCoinRepository())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadCoin(coinMarket)
+    }
+
+    override fun onStop() {
+        timer.cancel()
+        super.onStop()
+    }
+
+    private fun init() {
+        recyclerview_main_ticker_list.adapter = CoinAdapter()
+
+        linear_coin_markets.setOnClickListener {
+            coinMarketDialog.show(supportFragmentManager, null)
+        }
+
+        btn_search.setOnClickListener {
+            val coinSearchDialog = CoinSearchDialog()
+
+            coinSearchDialog.arguments =
+                Bundle().apply {
+                    putString(COIN_DETAIL, edit_search_ticker.text.toString())
+                }
+
+            coinSearchDialog.show(supportFragmentManager, null)
+        }
+
+        coinViewModel.tickerLoadedListener = {
+            (recyclerview_main_ticker_list.adapter as CoinAdapter).refreshData(it)
+        }
+
+        coinViewModel.errorLoadedListener = { msg ->
+            Toast.makeText(this@CoinListActivity, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loadCoin(coinMarket: String) {
+        timer.cancel()
+        timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                coinViewModel.loadTickerList(coinMarket)
+            }
+        }, 0, REPEAT_INTERVAL_MILLIS)
+    }
+
+    fun setDialogCallBackListener() {
+        coinMarketDialog.dialogCallBackListener = {
+            txt_select_coin_market.text = it
+            coinMarket = it
+            loadCoin(coinMarket)
+        }
+    }
+
+    companion object {
+        const val REPEAT_INTERVAL_MILLIS = 3000L
+        const val COIN_DETAIL = "COIN_DETAIL"
+    }
+}
---
+++ gongsulmin/StudyGong/app/src/main/java/my/gong/studygong/ui/CoinMarketDialog.kt
@@ -0,0 +1,47 @@
+package my.gong.studygong.ui
+
+import android.content.Context
+import android.os.Bundle
+import kotlinx.android.synthetic.main.dialog_coin_market.*
+import my.gong.studygong.Injection
+import my.gong.studygong.R
+import my.gong.studygong.adapter.CoinMarketAdapter
+import my.gong.studygong.base.BaseDialog
+import my.gong.studygong.viewmodel.CoinViewModel
+
+/**
+ *          Coin Market 필터 다이얼로그
+ *
+ */
+class CoinMarketDialog
+    : BaseDialog(R.layout.dialog_coin_market) {
+
+    lateinit var dialogCallBackListener: (String) -> Unit
+
+    private val coinViewModel: CoinViewModel by lazy {
+        CoinViewModel(
+            Injection.provideCoinRepository()
+        )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        recyclerview_coin_market.adapter = CoinMarketAdapter(
+            clickCoinMarketListener = {
+                dialogCallBackListener.invoke(it)
+                dismiss()
+            }
+        )
+        coinViewModel.loadBaseCurrency()
+
+        coinViewModel.baseCurrencyLoadedListener = {
+            (recyclerview_coin_market.adapter as CoinMarketAdapter).refreshData(it)
+        }
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        (activity as CoinListActivity).setDialogCallBackListener()
+    }
+}
