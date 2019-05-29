4add772462cd48b4fd4c371b86d284dfb108e80b
공공공 <tv302@naver.com>
2019-05-30 01:32:05 +0900

    HW - 3 VIewModel 생성

---
+++ gongsulmin/StudyGong/app/src/main/java/my/gong/studygong/viewmodel/CoinViewModel.kt
@@ -0,0 +1,70 @@
+package my.gong.studygong.viewmodel
+
+import my.gong.studygong.data.model.Ticker
+import my.gong.studygong.data.source.upbit.UpbitDataSource
+import kotlin.properties.Delegates
+
+class CoinViewModel(
+    private val upbitRepository: UpbitDataSource
+) {
+
+    var tickerList: List<Ticker> by Delegates.observable(listOf()) { _, _, newValue ->
+        tickerLoadedListener?.invoke(tickerList)
+    }
+
+    var searchTickerResultList: List<Ticker> by Delegates.observable(listOf()) { _, _, newValue ->
+        searchTickerLoadedListener?.invoke(searchTickerResultList)
+    }
+
+    var errorMessage: String by Delegates.observable("") { _, _, newValue ->
+        errorLoadedListener?.invoke(errorMessage)
+    }
+
+    var baseCurrencyList: List<String> by Delegates.observable(listOf()) { _, _, newValue ->
+        baseCurrencyLoadedListener?.invoke(baseCurrencyList)
+    }
+
+    var tickerLoadedListener: ((List<Ticker>) -> Unit)? = null
+    var searchTickerLoadedListener: ((List<Ticker>) -> Unit)? = null
+    var baseCurrencyLoadedListener: ((List<String>) -> Unit)? = null
+    var errorLoadedListener: ((String) -> Unit)? = null
+
+    fun loadTickerList(currency: String) {
+        upbitRepository.getTickers(
+            currency,
+            success = {
+                tickerList = it
+            },
+            fail = {
+                errorMessage = it
+            }
+        )
+    }
+
+    fun loadTickerSearchResult(searchTicker: String) {
+        if (searchTicker.isNotEmpty()) {
+            upbitRepository.getDetailTickers(
+                searchTicker,
+                success = {
+                    searchTickerResultList = it
+                },
+                fail = {
+                    errorMessage = it
+                }
+            )
+        } else {
+            errorMessage = "검색한 코인은 찾을수 없습니다!"
+        }
+    }
+
+    fun loadBaseCurrency() {
+        upbitRepository.getCoinCurrency(
+            success = {
+                baseCurrencyList = it
+            },
+            fail = {
+                errorMessage = it
+            }
+        )
+    }
+}
