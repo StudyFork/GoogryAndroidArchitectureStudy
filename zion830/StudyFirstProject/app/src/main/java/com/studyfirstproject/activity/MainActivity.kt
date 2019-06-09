8bfdb29106a94be75c20bada62b01718a7be5890
yunji <zion830@honglab.org>
2019-06-09 19:42:15 +0900

    HW-1 코인 조회, 표시 구현

---
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/activity/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.studyfirstproject.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log.e
+import android.view.View
+import com.studyfirstproject.R
+import com.studyfirstproject.adapter.CoinRecyclerViewAdapter
+import com.studyfirstproject.model.TickerModel
+import com.studyfirstproject.network.CoinApi
+import com.studyfirstproject.network.RetrofitBuilder
+import com.studyfirstproject.network.retrofitCallback
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var coinAdapter: CoinRecyclerViewAdapter
+    private lateinit var service: CoinApi
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        service = RetrofitBuilder.getInstance()
+        initView()
+    }
+
+    private fun initView() {
+        coinAdapter = CoinRecyclerViewAdapter(arrayListOf())
+        rv_main.adapter = coinAdapter
+
+        getMarketData()
+    }
+
+    private fun getMarketData() {
+        showProgress()
+        service.getAllMarket().enqueue(retrofitCallback { throwable, response ->
+            response?.let {
+                val marketList = response.body()!!
+                    .map { it.market }
+                    .filter { it.substring(0, it.indexOf("-")) == "KRW" }
+
+                val marketStr = marketList.joinToString().replace(" ", "")
+                getCoinData(marketStr)
+            }
+
+            throwable?.let {
+                toast(getString(R.string.network_err))
+                e(localClassName, throwable.message)
+            }
+        })
+    }
+
+    private fun getCoinData(marketStr: String) {
+        service.getTickers(marketStr).enqueue(retrofitCallback { throwable, response ->
+            response?.let {
+                setCoinInfo(response.body() ?: listOf())
+            }
+            throwable?.let {
+                toast(getString(R.string.network_err))
+                e(localClassName, throwable.message)
+            }
+        })
+    }
+
+    private fun setCoinInfo(coinData: List<TickerModel>) {
+        coinAdapter.coinList = ArrayList(coinData)
+        coinAdapter.notifyDataSetChanged()
+        hideProgress()
+    }
+
+    private fun showProgress() {
+        progress_main.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        progress_main.visibility = View.INVISIBLE
+    }
+}
