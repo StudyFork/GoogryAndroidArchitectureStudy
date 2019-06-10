c09fa8491276738243ae7b92f6997934dcea1e9b
machinehuman <44284077+machinehuman@users.noreply.github.com>
2019-06-10 19:37:46 +0900

    HW-1 뷰페이저 추가

---
+++ AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/ui/activity/MainActivity.kt
@@ -0,0 +1,50 @@
+package org.study.kotlin.androidarchitecturestudy.ui.activity
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import org.study.kotlin.androidarchitecturestudy.R
+import org.study.kotlin.androidarchitecturestudy.adapter.viewpageradapter.MainViewPagerAdapter
+import org.study.kotlin.androidarchitecturestudy.ui.fragment.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewpager_main: ViewPager
+    private lateinit var tablayout_main: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+        setupViewPager()
+    }
+
+
+    private fun initViews() {
+        tablayout_main = findViewById(R.id.tablayout_main)
+        viewpager_main = findViewById(R.id.viewpager_main)
+    }
+
+    private fun setupViewPager() {
+
+        val adapter = MainViewPagerAdapter(
+            getSupportFragmentManager()
+        )
+
+        var firstFragmet: MainFragment = MainFragment.createInstance("KRW")
+        var secondFragmet: MainFragment = MainFragment.createInstance("BTC")
+        var thirdFragmet: MainFragment = MainFragment.createInstance("ETH")
+        var fourFragmet: MainFragment = MainFragment.createInstance("USDT")
+
+        adapter.addFragment(firstFragmet, "KRW")
+        adapter.addFragment(secondFragmet, "BTC")
+        adapter.addFragment(thirdFragmet, "ETH")
+        adapter.addFragment(fourFragmet, "USDT")
+
+        viewpager_main!!.adapter = adapter
+        tablayout_main!!.setupWithViewPager(viewpager_main)
+
+    }
+
+
+}
