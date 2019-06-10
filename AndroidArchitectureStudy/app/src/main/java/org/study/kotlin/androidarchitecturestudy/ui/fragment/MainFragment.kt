3653314910905767cd799cc1fa527b83f5e70f1d
machinehuman <44284077+machinehuman@users.noreply.github.com>
2019-06-10 19:37:12 +0900

    HW-1 Ticker Api 사용 후 데이터 리사이클러뷰에 표시

---
+++ AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/ui/fragment/MainFragment.kt
@@ -0,0 +1,107 @@
+package org.study.kotlin.androidarchitecturestudy.ui.fragment
+
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.study.kotlin.androidarchitecturestudy.R
+import org.study.kotlin.androidarchitecturestudy.adapter.recyclerviewadapter.RecyclerviewAdapter
+import org.study.kotlin.androidarchitecturestudy.api.UPbitApi
+import org.study.kotlin.androidarchitecturestudy.api.model.MarketModel
+import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class MainFragment : Fragment() {
+
+    private val restApi: UPbitApi
+    private val baseUrl = "https://api.upbit.com"
+    var listData: ArrayList<TickerModel> = ArrayList()
+    private var recyclerviewAdapter: RecyclerviewAdapter? = null
+    private var recyclerView: RecyclerView? = null
+
+    companion object {
+        fun createInstance(message: String): MainFragment {
+            val fragmentInstance = MainFragment()
+            val bundle = Bundle(1)
+            bundle.putString(EXTRA_MESSAGE, message)
+            fragmentInstance.setArguments(bundle)
+            return fragmentInstance
+        }
+    }
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        restApi = retrofit.create(UPbitApi::class.java)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view: View? = inflater.inflate(R.layout.fragment_main, container, false);
+        recyclerView = view!!.findViewById(R.id.recyclerview_mainfragment)
+        recyclerView!!.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(activity?.applicationContext)
+        recyclerView!!.layoutManager = layoutManager
+        recyclerviewAdapter = RecyclerviewAdapter(arrayListOf())
+        val message = arguments!!.getString(EXTRA_MESSAGE)
+        getMarketList(message)
+        return view
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (isVisibleToUser) {
+            val message = arguments!!.getString(EXTRA_MESSAGE)
+            getMarketList(message)
+        }
+    }
+
+    private fun getMarketList(key: String) {
+        restApi.getMarket().enqueue(object : Callback<ArrayList<MarketModel>> {
+            override fun onFailure(call: Call<ArrayList<MarketModel>>, t: Throwable) {
+                Log.e("TAG onFailure", t.toString())
+            }
+
+            override fun onResponse(call: Call<ArrayList<MarketModel>>, response: Response<ArrayList<MarketModel>>) {
+                response?.let {
+                    val responseList = response.body()!!
+                        .map { it.market }
+                        .filter { it.substringBeforeLast("-") == key }
+                    val coinList = responseList.joinToString()
+                    getTickerList(coinList)
+                }
+            }
+        })
+    }
+
+    private fun getTickerList(markets: String) {
+        restApi.getTicker(markets).enqueue(object : Callback<ArrayList<TickerModel>> {
+            override fun onFailure(call: Call<ArrayList<TickerModel>>, t: Throwable) {
+                Log.e("TAG onFailure", t.toString())
+            }
+
+            override fun onResponse(call: Call<ArrayList<TickerModel>>, response: Response<ArrayList<TickerModel>>) {
+                listData = response?.body()!!
+                recyclerviewAdapter!!.setList(listData)
+                recyclerview_mainfragment.adapter = RecyclerviewAdapter(response?.body()!!)
+            }
+        })
+    }
+}
