c71d29a89f9a42b497ac2faab7d2eb1843a774a4
machinehuman <44284077+machinehuman@users.noreply.github.com>
2019-06-12 21:44:12 +0900

    HW-1 Util  나머지 기능 추가

---
+++ AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/adapter/recyclerviewadapter/MainListAdapter.kt
@@ -0,0 +1,57 @@
+package org.study.kotlin.androidarchitecturestudy.adapter.recyclerviewadapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_ticker.view.*
+import org.study.kotlin.androidarchitecturestudy.R
+import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
+import org.study.kotlin.androidarchitecturestudy.util.FormatUtil
+import org.study.kotlin.androidarchitecturestudy.util.FormatUtil.accTradePriceFormat
+
+class MainListAdapter(var tickerList: ArrayList<TickerModel>) :
+    RecyclerView.Adapter<MainListAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, vewType: Int): ItemViewHolder {
+        val v = LayoutInflater.from(viewGroup?.context).inflate(R.layout.item_ticker, viewGroup, false)
+        return ItemViewHolder(v)
+    }
+
+    override fun getItemCount(): Int = tickerList!!.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        tickerList!![position].let { item ->
+            with(holder.itemView) {
+                textview_item_market.text = item.market.substringAfterLast("-")
+                if (item.market.substringBeforeLast("-") == "BTC" || item.market.substringBeforeLast("-") == "ETH") {
+                    textview_item_trade_price.text = FormatUtil.floatingEightPointFormat(item.trade_price)
+                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
+                    textview_item_acc_trade_price_24h.text =  FormatUtil.floatingThreePointFormat(item.acc_trade_price_24h)
+                } else if(item.market.substringBeforeLast("-") == "KRW") {
+                    textview_item_trade_price.text = FormatUtil.commaDoubleFormat(item.trade_price)
+                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
+                    textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.acc_trade_price_24h,"KRW")
+                } else {
+                    textview_item_trade_price.text = FormatUtil.commaDoubleFormat(item.trade_price)
+                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
+                    textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.acc_trade_price_24h,"USDT")
+                }
+            }
+        }
+
+    }
+
+    fun setList(list: ArrayList<TickerModel>) {
+        if (tickerList != null)
+            tickerList.clear()
+
+        tickerList?.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
+
+
+
---
+++ AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/util/FormatUtil.kt
@@ -0,0 +1,30 @@
+package org.study.kotlin.androidarchitecturestudy.util
+
+import android.icu.text.DecimalFormat
+import org.study.kotlin.androidarchitecturestudy.R
+
+object FormatUtil {
+    val format = DecimalFormat("###,###")
+    fun commaDoubleFormat(number: Double) = format.format(number).toString()
+    fun commaIntFormat(number: Long) = format.format(number).toString()
+    fun floatingEightPointFormat(number: Double) = String.format("%.8f", number)
+    fun floatingThreePointFormat(number: Double) = String.format("%.3f", number)
+    fun percentFormat(number: Double) = String.format("%.2f", number * 100) + "%"
+    fun accTradePriceFormat(number: Double, market: String): String {
+        if (market == "KRW") {
+            var num1 = number / 1000000
+            val num2 = Math.round(num1)
+            return commaIntFormat(num2) + "M"
+        } else {
+            if (number > 999999) {
+                var num1 = number / 1000
+                val num2 = Math.round(num1)
+                return commaIntFormat(num2) + "K"
+            }else{
+                val num1 = Math.round(number)
+                return commaIntFormat(num1)
+            }
+
+        }
+    }
+}
