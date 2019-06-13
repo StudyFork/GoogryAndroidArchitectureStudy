c55693f060bec2bc47d65f8a69c96e53099e0456
sport0102 <sunminlee89@gmail.com>
2019-06-13 22:37:15 +0900

    getUsdtTotalDealPrice 메소드의 리턴방법 변경

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/util/StringUtil.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/util/StringUtil.kt
@@ -34,13 +34,13 @@ fun getKrwTotalDealPrice(totalPrice: Double): String = "${String.format("%,d", (
 
 fun getBtcEthTotalDealPrice(totalPrice: Double): String = String.format("%1$,.3f", totalPrice)
 
-fun getUsdtTotalDealPrice(totalPrice: Double): String {
+fun getUsdtTotalDealPrice(totalPrice: Double): String =
     if (totalPrice > 1_000_000) {
-        return "${String.format("%,d", (totalPrice / 1_000).toInt())} K"
+        "${String.format("%,d", (totalPrice / 1_000).toInt())} K"
     } else {
-        return String.format("%,d", totalPrice.toInt())
+        String.format("%,d", totalPrice.toInt())
     }
-}
+
 
 
 
