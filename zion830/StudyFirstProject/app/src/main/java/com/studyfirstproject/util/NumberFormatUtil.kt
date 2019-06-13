004c665978975e29e1c3975e418b3a7489f43001
yunji <zion830@honglab.org>
2019-06-14 01:02:59 +0900

    HW-1 NumberFormatUtil 가독성 개선

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/util/NumberFormatUtil.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/util/NumberFormatUtil.kt
@@ -6,27 +6,25 @@ object NumberFormatUtil {
     val df = DecimalFormat("###,###")
 
     // 백만 이하 숫자는 생략
-    fun skipUnderMillions(num: Double): String = if (num < 1000000) {
-        insertComma(num)
-    } else {
-        insertComma(num / 1000000) + "M"
-    }
+    fun skipUnderMillions(num: Double): String =
+        if (num < 1_000_000) {
+            insertComma(num)
+        } else {
+            insertComma(num / 1_000_000) + "M"
+        }
 
     // 세자리 단위로 끊어 , 삽입
     fun insertComma(num: Number): String = df.format(num).toString()
 
     // 백분율 변환
     fun getPercent(num: Double): String {
-        var type = ""
-        var percent = num * 100
+        val (type, percent) =
+            when {
+                num < 0 -> "▼" to -num
+                num > 0 -> "▲" to num
+                else -> "" to num
+            }
 
-        if (num < 0) {
-            type = "▼"
-            percent = -percent
-        } else if (num > 0) {
-            type = "▲"
-        }
-
-        return String.format("%s %.2f%%", type, percent)
+        return String.format("%s %.2f%%", type, percent * 100)
     }
 }
