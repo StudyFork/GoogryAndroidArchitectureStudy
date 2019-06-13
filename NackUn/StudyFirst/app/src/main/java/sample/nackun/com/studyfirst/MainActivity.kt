90dc511adde6111922265b465d00f6af1c04b2bd
NackUn <king4521@naver.com>
2019-06-14 00:24:30 +0900

    query 작성

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/MainActivity.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/MainActivity.kt
@@ -64,14 +64,19 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call<ArrayList<Market>>, response: Response<ArrayList<Market>>) {
-                    var markets = ""
-                    response.body()
-                        ?.filter { it.market.startsWith(marketLike) }
-                        ?.map { it-> markets += it.market+","}
-                    markets = markets.substring(0, markets.length - 1)
-                    Log.d("aa12", markets)
+                lateinit var query: String
+                response.body()?.let {
+                    query =
+                        it.filter { item ->
+                            item.market.startsWith(marketLike)
+                        }
+                            .joinToString { marketModel ->
+                                marketModel.market
+                            }
+                }
+                    Log.d("aa12", query)
 
-                    service.requestTicker(markets)
+                    service.requestTicker(query)
                         .enqueue(object : Callback<ArrayList<Ticker>> {
                             override fun onResponse(
                                 call: Call<ArrayList<Ticker>>,
