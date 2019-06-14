96f5a04b7530fbacf2a50d7ce455d553fb71d6da
hyunsung.shin <cordorshs@gmail.com>
2019-06-14 23:04:09 +0900

    HW-1 if 문 when 문 대체

--- Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/api/ServiceDefine.kt
+++ Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/api/ServiceDefine.kt
@@ -3,7 +3,6 @@ package com.nanamare.mac.sample.api
 import com.nanamare.mac.sample.AppApplication
 import com.nanamare.mac.sample.BuildConfig
 import com.nanamare.mac.sample.R
-import com.nanamare.mac.sample.api.upbit.UpBitService
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,15 +14,13 @@ object ServiceDefine {
 
     private val BASE_URL = AppApplication.instance.context().getString(R.string.upbit_base_url)
 
-    val retrofit : Retrofit = Retrofit.Builder()
-        .client(OkHttpClient.Builder()
-            .addInterceptor( HttpLoggingInterceptor().apply {
-                level = if (BuildConfig.DEBUG)
-                    HttpLoggingInterceptor.Level.BASIC
-                else
-                    HttpLoggingInterceptor.Level.NONE
-            } as Interceptor)
-            .build())
+    val retrofit: Retrofit = Retrofit.Builder()
+        .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+            level = when {
+                BuildConfig.DEBUG -> HttpLoggingInterceptor.Level.BASIC
+                else -> HttpLoggingInterceptor.Level.NONE
+            }
+        } as Interceptor).build())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL)
