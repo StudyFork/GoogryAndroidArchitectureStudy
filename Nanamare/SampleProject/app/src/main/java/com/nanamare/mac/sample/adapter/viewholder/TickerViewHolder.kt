6d25ef822e07c0010f972ac3be337952628b5966
hyunsung.shin <cordorshs@gmail.com>
2019-06-14 23:35:07 +0900

    HW-1 아이템 바인딩 로직 수정

--- Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/adapter/viewholder/TickerViewHolder.kt
+++ Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/adapter/viewholder/TickerViewHolder.kt
@@ -8,15 +8,18 @@ class TickerViewHolder(itemView: View) : BaseViewHolder<TickerModel>(itemView) {
 
     override fun bind(data: Any) {
 
-        val data = data as TickerModel
-
         /**
          * Todo market 에 맞춰서 나중에 포맷 바꿔야함
          */
-        itemView.tv_coin_name.text = data.market!!.split('-')[1]
-        itemView.tv_coin_current_price.text = data.tradePrice.toString()
-        itemView.tv_coin_compare_rate.text = data.signedChangeRate.toString()
-        itemView.tv_coin_all_price.text = data.accTradePrice24h.toString()
+
+        (data as TickerModel).run {
+            with(itemView) {
+                tv_coin_name.text = data.market!!.split('-')[1]
+                tv_coin_current_price.text = data.tradePrice.toString()
+                tv_coin_compare_rate.text = data.signedChangeRate.toString()
+                tv_coin_all_price.text = data.accTradePrice24h.toString()
+            }
+        }
 
     }
 
