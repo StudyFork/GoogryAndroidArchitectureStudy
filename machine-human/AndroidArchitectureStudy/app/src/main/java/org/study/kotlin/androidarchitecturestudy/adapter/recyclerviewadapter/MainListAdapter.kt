996578e33640a24adf11f6389610b6153b299e91
ojisu <>
2019-06-15 03:07:46 +0900

    HW-1 data set 기능 ViewHolder 내부에서 구현, 외부 List -> 내부 List로 변경

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/adapter/recyclerviewadapter/MainListAdapter.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/adapter/recyclerviewadapter/MainListAdapter.kt
@@ -10,9 +10,10 @@ import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
 import org.study.kotlin.androidarchitecturestudy.util.FormatUtil
 import org.study.kotlin.androidarchitecturestudy.util.FormatUtil.accTradePriceFormat
 
-class MainListAdapter(var tickerList: ArrayList<TickerModel>) :
+class MainListAdapter :
     RecyclerView.Adapter<MainListAdapter.ItemViewHolder>() {
-
+    var tickerList: ArrayList<TickerModel>? = null
+    var itemViewHolder: ItemViewHolder? = null
     override fun onCreateViewHolder(viewGroup: ViewGroup, vewType: Int): ItemViewHolder {
         val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_ticker, viewGroup, false)
         return ItemViewHolder(v)
@@ -21,36 +22,44 @@ class MainListAdapter(var tickerList: ArrayList<TickerModel>) :
     override fun getItemCount(): Int = tickerList!!.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        tickerList!![position].let { item ->
-            with(holder.itemView) {
-                textview_item_market.text = item.market.substringAfterLast("-")
-                if (item.market.substringBeforeLast("-") == "BTC" || item.market.substringBeforeLast("-") == "ETH") {
-                    textview_item_trade_price.text = FormatUtil.floatingEightPointFormat(item.trade_price)
-                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
-                    textview_item_acc_trade_price_24h.text =
-                        FormatUtil.floatingThreePointFormat(item.acc_trade_price_24h)
-                } else if (item.market.substringBeforeLast("-") == "KRW") {
-                    textview_item_trade_price.text = FormatUtil.commaDoubleFormat(item.trade_price)
-                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
-                    textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.acc_trade_price_24h, "KRW")
-                } else {
-                    textview_item_trade_price.text = FormatUtil.usdtFloatingPointFormat(item.trade_price)
-                    textview_item_change_rate.text = FormatUtil.percentFormat(item.change_rate)
-                    textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.acc_trade_price_24h, "USDT")
-                }
-            }
-        }
+        itemViewHolder?.setData(holder,position,tickerList)
 
     }
 
     fun setList(list: ArrayList<TickerModel>) {
         tickerList?.clear()
-
         tickerList?.addAll(list)
         notifyDataSetChanged()
     }
 
-    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
+//        val textview_item_market = view.textview_item_market
+//        val textview_item_trade_price = view.textview_item_trade_price
+//        val textview_item_change_rate = view.textview_item_change_rate
+//        val textview_item_acc_trade_price_24h = view.textview_item_acc_trade_price_24h
+
+        fun setData(holder: ItemViewHolder, position: Int, tickerList: ArrayList<TickerModel>?){
+            tickerList!![position].let { item ->
+                with(holder.itemView) {
+                    textview_item_market.text = item.market.substringAfterLast("-")
+                    if (item.market.substringBeforeLast("-") == "BTC" || item.market.substringBeforeLast("-") == "ETH") {
+                        textview_item_trade_price.text = FormatUtil.floatingEightPointFormat(item.tradePrice)
+                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
+                        textview_item_acc_trade_price_24h.text =
+                            FormatUtil.floatingThreePointFormat(item.accTradePrice24h)
+                    } else if (item.market.substringBeforeLast("-") == "KRW") {
+                        textview_item_trade_price.text = FormatUtil.commaDoubleFormat(item.tradePrice)
+                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
+                        textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.accTradePrice24h, "KRW")
+                    } else {
+                        textview_item_trade_price.text = FormatUtil.usdtFloatingPointFormat(item.tradePrice)
+                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
+                        textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.accTradePrice24h, "USDT")
+                    }
+                }
+            }
+        }
+    }
 }
 
 
