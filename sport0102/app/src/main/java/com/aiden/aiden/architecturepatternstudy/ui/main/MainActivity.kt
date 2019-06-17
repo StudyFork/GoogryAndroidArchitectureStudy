78c0e8480c6ebfb1822e4a0489f6b433d941fa59
sport0102 <sunminlee89@gmail.com>
2019-06-17 23:27:54 +0900

    탭 레이아웃 탭 온 탭 셀렉트 리스너 추가

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainActivity.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.aiden.aiden.architecturepatternstudy.data.enums.Market
-import com.aiden.aiden.architecturepatternstudy.data.source.UpbitRepository
-import com.aiden.aiden.architecturepatternstudy.data.source.remote.UpbitRemoteDataSource
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_main_tab.view.*
@@ -23,6 +20,20 @@ class MainActivity : AppCompatActivity() {
         }
         main_vp.adapter = MainPagerAdapter(supportFragmentManager)
         main_vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(main_market_tab_tl))
+        main_market_tab_tl.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    main_vp.currentItem = it.position
+                }
+            }
+
+        })
     }
 
     private fun createTabView(tabName: String): View {
