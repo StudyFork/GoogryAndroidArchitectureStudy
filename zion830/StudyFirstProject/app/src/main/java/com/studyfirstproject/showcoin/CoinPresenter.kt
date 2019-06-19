487b7a0660e08c40627f76902d439d023b7907a4
yunji <zion830@honglab.org>
2019-06-20 06:24:59 +0900

    HW-2 View와 Presenter 분리

---
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/CoinPresenter.kt
@@ -0,0 +1,29 @@
+package com.studyfirstproject.showcoin
+
+import com.studyfirstproject.data.CoinDataSource
+import com.studyfirstproject.data.CoinRepository
+import com.studyfirstproject.data.model.TickerModel
+
+class CoinPresenter(private val view: CoinContract.View) :
+    CoinContract.Presenter, CoinDataSource.LoadMarketsCallback, CoinDataSource.LoadTickersCallback {
+    private var repository: CoinDataSource = CoinRepository()
+
+    override fun start() {
+    }
+
+    override fun getMarketData() {
+        repository.getAllMarkets(this)
+    }
+
+    override fun onMarketsLoaded(markets: String) {
+        repository.getCoinData(markets, this)
+    }
+
+    override fun onTickersLoaded(tickers: List<TickerModel>) {
+        view.setCoinInfo(tickers)
+    }
+
+    override fun onDataNotAvailable(msg: String, reason: String?) {
+        view.notifyError(msg, reason)
+    }
+}
---
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/view/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.studyfirstproject.showcoin.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import com.studyfirstproject.R
+import com.studyfirstproject.adapter.CoinRecyclerViewAdapter
+import com.studyfirstproject.data.model.TickerModel
+import com.studyfirstproject.showcoin.CoinContract
+import com.studyfirstproject.showcoin.CoinPresenter
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+
+class MainActivity : AppCompatActivity(), CoinContract.View {
+    override lateinit var presenter: CoinContract.Presenter
+    private lateinit var coinAdapter: CoinRecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        presenter = CoinPresenter(this)
+        initView()
+    }
+
+    override fun initView() {
+        coinAdapter = CoinRecyclerViewAdapter()
+        rv_main.adapter = coinAdapter
+        layout_main_swipe.setOnRefreshListener {
+            getMarketData()
+        }
+
+        getMarketData()
+    }
+
+    fun getMarketData() {
+        showProgress()
+        presenter.getMarketData()
+    }
+
+    override fun setCoinInfo(coinData: List<TickerModel>) {
+        coinAdapter.setCoinList(coinData)
+        hideProgress()
+        hideRefreshIcon()
+    }
+
+    override fun showProgress() {
+        progress_main.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        progress_main.visibility = View.INVISIBLE
+    }
+
+    override fun hideRefreshIcon() {
+        if (layout_main_swipe.isRefreshing)
+            layout_main_swipe.isRefreshing = false
+    }
+
+    override fun notifyError(msg: String, reason: String?) {
+        toast(msg)
+        Log.e(localClassName, reason ?: "No error message")
+    }
+}
