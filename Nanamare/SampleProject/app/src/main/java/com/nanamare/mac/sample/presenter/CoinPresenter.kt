1ebcbba65e7888ed94f1ad6f733ad20c75c41373
hyunsung.shin <cordorshs@gmail.com>
2019-06-19 23:59:55 +0900

    HW-2 Presenter 수정

--- Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/presenter/CoinPresenter.kt
+++ Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/presenter/CoinPresenter.kt
@@ -1,30 +1,20 @@
 package com.nanamare.mac.sample.presenter
 
-import com.nanamare.mac.sample.api.DisposableManager
-import com.nanamare.mac.sample.api.upbit.UpBitServiceManager
 import com.nanamare.mac.sample.contract.CoinContract
-import io.reactivex.android.schedulers.AndroidSchedulers
+import com.nanamare.mac.sample.data.CoinRepository
 
-class CoinPresenter(private val view: CoinContract.CoinView) :
-    CoinContract.CoinPresenter {
-
-    private var disposableManager : DisposableManager = DisposableManager()
+class CoinPresenter(private val view: CoinContract.CoinView) : CoinContract.CoinPresenter {
 
     override fun getCoins(ticketList: MutableList<String>) {
-        disposableManager.add(
-            UpBitServiceManager.getTickerList(ticketList)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    view.showCoins(it.body())
-                }, {
-                    view.hideLoadingDialog()
-                })
-        )
-
+        CoinRepository.getCoins(ticketList, success = {
+            view.showCoins(it)
+        }, failed = {
+            view.hideLoadingDialog()
+        })
     }
 
     override fun networkDispose() {
-        disposableManager.dispose()
+        CoinRepository.close()
     }
 
 }
--- Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/presenter/MarketPresenter.kt
+++ Nanamare/SampleProject/app/src/main/java/com/nanamare/mac/sample/presenter/MarketPresenter.kt
@@ -2,42 +2,36 @@ package com.nanamare.mac.sample.presenter
 
 import android.os.Bundle
 import com.google.gson.Gson
-import com.nanamare.mac.sample.api.DisposableManager
-import com.nanamare.mac.sample.api.upbit.UpBitServiceManager
 import com.nanamare.mac.sample.contract.MarketContract
+import com.nanamare.mac.sample.data.MarketRepository
 import com.nanamare.mac.sample.ui.MainActivity
 import com.nanamare.mac.sample.ui.MarketListFragment
-import io.reactivex.android.schedulers.AndroidSchedulers
 
 class MarketPresenter(private val view: MarketContract.MarketView) : MarketContract.MarketPresenter {
 
-    private var disposableManager: DisposableManager = DisposableManager()
-
     override fun getMarketList() {
         view.showLoadingDialog()
-        disposableManager.add(UpBitServiceManager.getAllMarketList()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                view.hideLoadingDialog()
-                val marketMap: LinkedHashMap<String, List<String>> = LinkedHashMap()
-                val marketList = listOf<String>().toMutableList()
-                it.body()?.map { marketModel ->
-                    marketModel.market!!.split("-")[0].let { market ->
-                        marketList.add(marketModel.market)
-                        marketMap.put(market, marketList)
-                    }
+        MarketRepository.getMarketList(success = {
+            view.hideLoadingDialog()
+            val marketMap: LinkedHashMap<String, List<String>> = LinkedHashMap()
+            val marketList = listOf<String>().toMutableList()
+            it.map { marketModel ->
+                marketModel.market!!.split("-")[0].let { market ->
+                    marketList.add(marketModel.market)
+                    marketMap.put(market, marketList)
                 }
-                val bundle = Bundle().apply {
-                    putString(MainActivity.KET_MARKET_LIST, Gson().toJson(marketMap))
-                }
-                view.goToFragment(MarketListFragment::class.java, bundle)
-            }, {
-                view.hideLoadingDialog()
-            })
-        )
+            }
+            val bundle = Bundle().apply {
+                putString(MainActivity.KET_MARKET_LIST, Gson().toJson(marketMap))
+            }
+            view.goToFragment(MarketListFragment::class.java, bundle)
+        }, failed = {
+            view.hideLoadingDialog()
+        })
+
     }
 
     override fun networkDispose() {
-        disposableManager.dispose()
+        MarketRepository.close()
     }
 }
