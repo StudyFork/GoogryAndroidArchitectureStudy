28e12097ada9ef6843f3a303a27b0ab27430ad3f
NackUn <king4521@naver.com>
2019-06-19 13:14:26 +0900

    network 제거 interface UpbitApi 를 DataSource로 이동

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/data/DataSource.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/data/DataSource.kt
@@ -1,8 +1,20 @@
 package sample.nackun.com.studyfirst.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import sample.nackun.com.studyfirst.vo.Market
 import sample.nackun.com.studyfirst.vo.Ticker
 
 interface DataSource {
+    interface UpbitApi {
+        @GET("market/all")
+        fun requestMarket(): Call<ArrayList<Market>>
+
+        @GET("ticker/")
+        fun requestTicker(@Query("markets") markets: String): Call<ArrayList<Ticker>>
+    }
+
     var tickers: ArrayList<Ticker>
     fun requestMarkets(marketLike: String)
     fun requestTickers(query: String)
