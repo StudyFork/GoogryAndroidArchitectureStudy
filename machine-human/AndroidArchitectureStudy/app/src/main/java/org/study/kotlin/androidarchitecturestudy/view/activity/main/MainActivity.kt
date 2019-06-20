97f47a1cb7172d13913d3fb43fd6cc8bea194635
ojisu <>
2019-06-21 03:38:22 +0900

    HW-2 view 분리

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/view/activity/main/MainActivity.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/view/activity/main/MainActivity.kt
@@ -1,14 +1,13 @@
-package org.study.kotlin.androidarchitecturestudy.ui.activity
+package org.study.kotlin.androidarchitecturestudy.view.activity.main
 
 import android.os.Bundle
 import android.support.design.widget.TabLayout
-import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.util.Log
 import org.study.kotlin.androidarchitecturestudy.R
 import org.study.kotlin.androidarchitecturestudy.adapter.viewpageradapter.MainViewPagerAdapter
-import org.study.kotlin.androidarchitecturestudy.ui.fragment.MainFragment
+import org.study.kotlin.androidarchitecturestudy.view.fragment.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewPagerMain: ViewPager
@@ -17,27 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("TAG MainActivity", "onCreate")
+
         initViews()
         setupViewPager()
     }
 
 
     private fun initViews() {
+        Log.e("TAG MainActivity", "initViews")
+
         tablayoutMain = findViewById(R.id.tablayout_main)
         viewPagerMain = findViewById(R.id.viewpager_main)
-
     }
 
     private fun setupViewPager() {
+        Log.e("TAG MainActivity", "setupViewPager")
 
         val adapter = MainViewPagerAdapter(
             supportFragmentManager
         )
 
-        val firstFragment: MainFragment = MainFragment.createInstance("KRW")
-        val secondFragment: MainFragment = MainFragment.createInstance("BTC")
-        val thirdFragment: MainFragment = MainFragment.createInstance("ETH")
-        val fourFragment: MainFragment = MainFragment.createInstance("USDT")
+        val firstFragment: MainFragment =
+            MainFragment.createInstance("KRW")
+        val secondFragment: MainFragment =
+            MainFragment.createInstance("BTC")
+        val thirdFragment: MainFragment =
+            MainFragment.createInstance("ETH")
+        val fourFragment: MainFragment =
+            MainFragment.createInstance("USDT")
 
         adapter.addFragment(firstFragment, "KRW")
         adapter.addFragment(secondFragment, "BTC")
@@ -46,7 +53,6 @@ class MainActivity : AppCompatActivity() {
 
         viewPagerMain.adapter = adapter
         tablayoutMain.setupWithViewPager(viewPagerMain)
-
     }
 
 
