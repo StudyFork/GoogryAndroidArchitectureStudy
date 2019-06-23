b9d2612584ae39acc14b3332948c587c3fb94e66
sport0102 <sunminlee89@gmail.com>
2019-06-23 18:58:00 +0900

    HW-2 BaseFragment 작성 및 toast 관련 함수 추가

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainFragment.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainFragment.kt
@@ -1,26 +1,22 @@
 package com.aiden.aiden.architecturepatternstudy.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.aiden.aiden.architecturepatternstudy.R
 import com.aiden.aiden.architecturepatternstudy.api.model.TickerModel
+import com.aiden.aiden.architecturepatternstudy.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment(val market: String) : Fragment(), MainContract.View {
+class MainFragment(val market: String) : BaseFragment(R.layout.fragment_main), MainContract.View {
+
 
     override lateinit var presenter: MainContract.Presenter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
-        presenter.loadMarketList(market)
-        return view
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = start()
 
+    override fun start() {
+        presenter.loadMarketList(market)
+    }
 
     override fun showTickerList(tickerList: List<TickerModel>) {
         fragment_ticker_list_rv.adapter = TickerListAdapter().apply {
@@ -30,7 +26,7 @@ class MainFragment(val market: String) : Fragment(), MainContract.View {
     }
 
     override fun showErrorToast() {
-        Toast.makeText(context, getString(R.string.all_error_load_data_fail), Toast.LENGTH_SHORT).show()
+        toastM(getString(R.string.all_error_load_data_fail))
     }
 
 }
