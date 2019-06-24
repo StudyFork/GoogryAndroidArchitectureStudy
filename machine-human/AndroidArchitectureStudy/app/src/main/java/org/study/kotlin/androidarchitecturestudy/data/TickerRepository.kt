984e7f9b3dd423647eb8d070c471000cab69181e
ojisu <>
2019-06-25 01:59:05 +0900

    HW-3 error 타입 throwable 타입으로 받기 & invoke 함수 생성

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/data/TickerRepository.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/data/TickerRepository.kt
@@ -3,53 +3,49 @@ package org.study.kotlin.androidarchitecturestudy.data
 import android.util.Log
 import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
 import org.study.kotlin.androidarchitecturestudy.base.BaseDataSource
+
 /**
-***************************
+ ***************************
 BaseDataSource - structure
 
 i = interface
 f = function
-***************************
+ ***************************
 
 i = BaseDataSource
 
-    i = GetTickerListCallback
+i = GetTickerListCallback
 
-        f = onTickerListLoaded(tickerList: ArrayList<TickerModel>)
-        f = onDataNotAvailable(error: String)
+f = onTickerListLoaded(tickerList: ArrayList<TickerModel>)
+f = onDataNotAvailable(error: String)
 
-    f = requestMarkets(marketName: String, callback: GetTickerListCallback)
+f = requestMarkets(marketName: String, callback: GetTickerListCallback)
 
  */
-class TickerRepository(
+class TickerRepository private constructor(
     val tickerRemoteDataSource: BaseDataSource
 ) : BaseDataSource {
 
-
-    /**
-        requestMarkets() = BaseDataSource의 requestMarkets()
-     */
     override fun requestMarkets(marketName: String, callback: BaseDataSource.GetTickerListCallback) {
-        Log.e("TAG TickerRepository", "requestMarkets")
 
-        /**
-            requestMarkets() = BaseDataSource의 requestMarkets()
-            - 위 override 함수와 동일
-         */
         tickerRemoteDataSource.requestMarkets(marketName, object : BaseDataSource.GetTickerListCallback {
             override fun onTickerListLoaded(tickerList: ArrayList<TickerModel>) {
-                Log.e("TAG TickerRepository", "onTickerListLoaded")
                 callback.onTickerListLoaded(tickerList)
             }
 
-            /**
-                requestMarkets() = BaseDataSource의 onDataNotAvailable()
-             */
-            override fun onDataNotAvailable(error: String) {
-                Log.e("TAG TickerRepository", "onDataNotAvailable")
+            override fun onDataNotAvailable(error: Throwable) {
                 callback.onDataNotAvailable(error)
             }
 
         })
+    }
+
+    companion object {
+        private var instance: TickerRepository? = null
+        operator fun invoke(tickerRemoteDataSource: BaseDataSource): TickerRepository {
+            Log.e("TAG", "repositoryinvoke")
+            return instance ?: TickerRepository(tickerRemoteDataSource = tickerRemoteDataSource)
+                .apply { instance = this }
+        }
     }
 }
