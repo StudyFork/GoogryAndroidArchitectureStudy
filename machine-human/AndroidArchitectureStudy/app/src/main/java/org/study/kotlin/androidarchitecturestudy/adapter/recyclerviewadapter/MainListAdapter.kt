b2509e2d5b5b97b6c2bcbe0033606bee6e641598
ojisu <>
2019-06-25 01:47:59 +0900

    HW-3 enum 클래스 추가(MainListAdapter 42~56줄 사용) & tickerList 변수타입 val로 변경 & findViewId 호출 부분 분리 & setData함수 파라메타 변경 & setList함수 run 이용하여 코드 단축

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/adapter/recyclerviewadapter/MainListAdapter.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/adapter/recyclerviewadapter/MainListAdapter.kt
@@ -1,66 +1,63 @@
 package org.study.kotlin.androidarchitecturestudy.adapter.recyclerviewadapter
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.item_ticker.view.*
 import org.study.kotlin.androidarchitecturestudy.R
 import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
+import org.study.kotlin.androidarchitecturestudy.data.enum.TabTitle
 import org.study.kotlin.androidarchitecturestudy.util.FormatUtil
 import org.study.kotlin.androidarchitecturestudy.util.FormatUtil.accTradePriceFormat
 
 class MainListAdapter : RecyclerView.Adapter<MainListAdapter.ItemViewHolder>() {
-    var tickerList: ArrayList<TickerModel> = arrayListOf()
-
+    private val tickerList: ArrayList<TickerModel> = ArrayList()
     override fun onCreateViewHolder(viewGroup: ViewGroup, vewType: Int): ItemViewHolder {
         val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_ticker, viewGroup, false)
-        Log.e("TAG MAinListAdapter", "onCreateViewHolder")
+
         return ItemViewHolder(v)
     }
 
-    override fun getItemCount(): Int =tickerList.size
-    
+    override fun getItemCount(): Int = tickerList.size
+
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        Log.e("TAG MAinListAdapter", "onBindViewHolder")
-        holder?.setData(holder, position, tickerList)
-
+        holder?.setData(tickerList[position])
     }
 
-    fun setList(list: ArrayList<TickerModel>) {
-        Log.e("TAG MAinListAdapter", "setList")
-        tickerList.clear()
-        tickerList.addAll(list)
+    fun setList(list: List<TickerModel>) {
+        tickerList.run {
+            clear()
+            addAll(list)
+        }
     }
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun setData(holder: ItemViewHolder, position: Int, tickerList: ArrayList<TickerModel>?) {
-            Log.e("TAG ItemViewHolder", "setData")
-            tickerList!![position].let { item ->
-                with(holder.itemView) {
-                    textview_item_market.text = item.market.substringAfterLast("-")
-                    if (item.market.substringBeforeLast("-") == "BTC" || item.market.substringBeforeLast("-") == "ETH") {
-                        textview_item_trade_price.text = FormatUtil.floatingEightPointFormat(item.tradePrice)
-                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
-                        textview_item_acc_trade_price_24h.text =
-                            FormatUtil.floatingThreePointFormat(item.accTradePrice24h)
-                    } else if (item.market.substringBeforeLast("-") == "KRW") {
-                        textview_item_trade_price.text = FormatUtil.commaDoubleFormat(item.tradePrice)
-                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
-                        textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.accTradePrice24h, "KRW")
-                    } else {
-                        textview_item_trade_price.text = FormatUtil.usdtFloatingPointFormat(item.tradePrice)
-                        textview_item_change_rate.text = FormatUtil.percentFormat(item.changeRate)
-                        textview_item_acc_trade_price_24h.text = accTradePriceFormat(item.accTradePrice24h, "USDT")
-                    }
-                }
-            }
+        private val tvItemMarket by lazy { view.textview_item_market }
+        private val tvItemTradePrice by lazy { view.textview_item_trade_price }
+        private val tvItemChangeRate by lazy { view.textview_item_change_rate }
+        private val tvItemAccTradePrice by lazy { view.textview_item_acc_trade_price_24h }
 
+        fun setData(item: TickerModel) {
+            tvItemMarket.text = item.market.substringAfterLast("-")
+            if (item.market.substringBeforeLast("-") == TabTitle.BTC.toString() || item.market.substringBeforeLast("-") == TabTitle.ETH.toString()) {
+                tvItemTradePrice.text = FormatUtil.floatingEightPointFormat(item.tradePrice)
+                tvItemChangeRate.text = FormatUtil.percentFormat(item.changeRate)
+                tvItemAccTradePrice.text = FormatUtil.floatingThreePointFormat(item.accTradePrice24h)
+            } else if (item.market.substringBeforeLast("-") == TabTitle.KRW.toString()) {
+                tvItemTradePrice.text = FormatUtil.commaFormat(item.tradePrice)
+                tvItemChangeRate.text = FormatUtil.percentFormat(item.changeRate)
+                tvItemAccTradePrice.text = accTradePriceFormat(item.accTradePrice24h, TabTitle.KRW.toString())
+            } else {
+                tvItemTradePrice.text = FormatUtil.usdtFloatingPointFormat(item.tradePrice)
+                tvItemChangeRate.text = FormatUtil.percentFormat(item.changeRate)
+                tvItemAccTradePrice.text = accTradePriceFormat(item.accTradePrice24h, TabTitle.USDT.toString())
+            }
         }
     }
 }
+
 
 
 
