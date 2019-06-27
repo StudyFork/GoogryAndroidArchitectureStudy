28a7f349ffdadf48b13daf3e40bcbec57f620e5f
BeokBeok <kekemusa37@nate.com>
2019-06-27 10:20:56 +0900

    HW-3 presenter.start 호출부 변경 (onCreateView -> onViewCreated)
    - 리뷰 URL
    https://github.com/StudyFork/GoogryAndroidArchitectureStudy/pull/63#discussion_r297720928

--- BeokBeok/app/src/main/java/com/architecturestudy/upbitmarket/UpbitFragment.kt
+++ BeokBeok/app/src/main/java/com/architecturestudy/upbitmarket/UpbitFragment.kt
@@ -2,9 +2,7 @@ package com.architecturestudy.upbitmarket
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import com.architecturestudy.R
 import com.architecturestudy.base.BaseFragment
@@ -27,20 +25,12 @@ class UpbitFragment : BaseFragment(
         )
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        presenter.start()
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?
     ) {
         super.onViewCreated(view, savedInstanceState)
+        presenter.start()
         initClickListener()
         initRecyclerView()
     }
