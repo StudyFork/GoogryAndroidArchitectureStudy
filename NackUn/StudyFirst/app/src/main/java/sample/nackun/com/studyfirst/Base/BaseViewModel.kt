60c54c4728689908482bab79657f3de52ed1bb65
NackUn <king4521@naver.com>
2019-07-03 21:24:24 +0900

    HW-3 layout 추가, BaseViewModel, MainViewModel 추가

---
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/Base/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package sample.nackun.com.studyfirst.Base
+
+import android.arch.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel()
---
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package sample.nackun.com.studyfirst.main
+
+import sample.nackun.com.studyfirst.Base.BaseViewModel
+
+class MainViewModel : BaseViewModel()
