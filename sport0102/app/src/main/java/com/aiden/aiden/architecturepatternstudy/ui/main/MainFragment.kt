2d5d7af90ea3ea4d6b816aadbab6dc3731ff750a
sport0102 <sunminlee89@gmail.com>
2019-07-03 16:26:47 +0900

    HW-3 view model 생성

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainFragment.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainFragment.kt
@@ -9,6 +9,14 @@ import com.aiden.aiden.architecturepatternstudy.base.BaseFragment
 import com.aiden.aiden.architecturepatternstudy.data.source.UpbitRepository
 import com.aiden.aiden.architecturepatternstudy.data.source.remote.UpbitRemoteDataSource
 import kotlinx.android.synthetic.main.fragment_main.*
+import android.widget.Toast
+import android.R.attr.password
+import androidx.databinding.Observable.OnPropertyChangedCallback
+import android.R
+import android.app.Activity
+import androidx.databinding.DataBindingUtil
+
+
 
 
 class MainFragment : BaseFragment(com.aiden.aiden.architecturepatternstudy.R.layout.fragment_main), MainContract.View {
@@ -18,7 +26,10 @@ class MainFragment : BaseFragment(com.aiden.aiden.architecturepatternstudy.R.lay
     private val upbitApi by lazy { retrofit.create(UpbitApi::class.java) }
 
     private lateinit var marketName: String
+
     private val error = "error"
+
+    private lateinit var mBinding : DataBindingUtil
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         arguments?.let {
@@ -30,7 +41,6 @@ class MainFragment : BaseFragment(com.aiden.aiden.architecturepatternstudy.R.lay
                 this.marketName = marketName
             }
         }
-        presenter = MainPresenter(UpbitRepository.getInstance(UpbitRemoteDataSource(upbitApi)), this)
         super.onViewCreated(view, savedInstanceState)
     }
 
---
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainViewModel.kt
@@ -0,0 +1,124 @@
+package com.aiden.aiden.architecturepatternstudy.ui.main
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableField
+import com.aiden.aiden.architecturepatternstudy.api.model.MarketResponse
+import com.aiden.aiden.architecturepatternstudy.api.model.TickerResponse
+import com.aiden.aiden.architecturepatternstudy.data.enums.Market
+import com.aiden.aiden.architecturepatternstudy.data.source.UpbitDataSource
+import com.aiden.aiden.architecturepatternstudy.data.source.UpbitRepository
+import com.aiden.aiden.architecturepatternstudy.util.StringUtil
+import java.math.BigDecimal
+
+class MainViewModel(private val upbitRepository: UpbitRepository) : BaseObservable() {
+
+    private var tickerObservable: ObservableField<List<TickerResponse>> = ObservableField()
+
+    private var isDataLoadingError: ObservableField<Boolean> = ObservableField()
+
+    private var marketName = Market.BTC.marketName
+
+    init {
+        loadMarketList(marketName)
+    }
+
+    private fun setMarketName(marketName: String) {
+
+        this.marketName = marketName
+
+    }
+
+
+    private fun loadMarketList(market: String) {
+
+        upbitRepository.getMarketList(object : UpbitDataSource.GetMarketListCallback {
+
+            override fun onMarketListLoaded(marketList: List<MarketResponse>) {
+                val modifiedMarketList =
+                    marketList.filter { item -> item.market.startsWith(market, true) }
+                loadTickerList(modifiedMarketList)
+            }
+
+            override fun onDataNotAvailable() {
+                isDataLoadingError.set(false)
+            }
+
+        })
+
+    }
+
+    private fun loadTickerList(marketList: List<MarketResponse>) {
+
+        upbitRepository.getTickerList(marketList, object : UpbitDataSource.GetTickerListCallback {
+
+            override fun onTickerListLoaded(tickerList: List<TickerResponse>) {
+                tickerObservable.set(tickerList.map(::modifyTicker))
+            }
+
+            override fun onDataNotAvailable() {
+                isDataLoadingError.set(false)
+            }
+
+        })
+
+    }
+
+    private fun modifyTicker(ticker: TickerResponse): TickerResponse {
+
+        with(ticker) {
+
+            // 코인 이름
+            coinName = market.split("-")[1]
+
+            //  현재 가격
+            nowPrice = if (market.startsWith(
+                    Market.KRW.marketName,
+                    true
+                )
+            ) {
+                StringUtil.getKrwCommaPrice(BigDecimal(tradePrice))
+            } else if (market.startsWith(
+                    Market.BTC.marketName,
+                    true
+                ) || market.startsWith(
+                    Market.ETH.marketName,
+                    true
+                )
+            ) {
+                StringUtil.getBtcEthCommaPrice(tradePrice)
+            } else {
+                StringUtil.getUsdtCommaPrice(tradePrice)
+            }
+
+            // 전일대비 퍼센트
+            compareBeforePercentage = StringUtil.getPercent(
+                prevClosingPrice,
+                tradePrice
+            )
+
+            // 거래대금
+            totalDealPrice = if (market.startsWith(
+                    Market.KRW.marketName,
+                    true
+                )
+            ) {
+                StringUtil.getKrwTotalDealPrice(accTradePrice24h)
+            } else if (market.startsWith(
+                    Market.BTC.marketName,
+                    true
+                ) || market.startsWith(
+                    Market.ETH.marketName,
+                    true
+                )
+            ) {
+                StringUtil.getBtcEthTotalDealPrice(accTradePrice24h)
+            } else {
+                StringUtil.getUsdtTotalDealPrice(accTradePrice24h)
+            }
+
+        }
+
+        return ticker
+
+    }
+}
