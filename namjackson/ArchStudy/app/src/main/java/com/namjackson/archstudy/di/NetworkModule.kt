f941769d0a216abc6c9e14ee6347d92fce8b320a
namjackson <skawo32167@naver.com>
2019-07-03 16:14:35 +0900

    HW-6 DI (Koin) 적용 - 멘토리뷰 2.networkModule 수정

--- namjackson/ArchStudy/app/src/main/java/com/namjackson/archstudy/di/NetworkModule.kt
+++ namjackson/ArchStudy/app/src/main/java/com/namjackson/archstudy/di/NetworkModule.kt
@@ -1,8 +1,10 @@
 package com.namjackson.archstudy.di
 
-import com.namjackson.archstudy.network.UpbitService
+import com.namjackson.archstudy.base.UPBIT_API
+import com.namjackson.archstudy.data.source.remote.upbit.UpbitApi
+import com.namjackson.archstudy.network.RetrofitCreator
 import org.koin.dsl.module
 
 val networkModule = module {
-    single { UpbitService.upbitApi }
+    single { RetrofitCreator.createRetrofit(UPBIT_API, UpbitApi::class.java) }
 }
