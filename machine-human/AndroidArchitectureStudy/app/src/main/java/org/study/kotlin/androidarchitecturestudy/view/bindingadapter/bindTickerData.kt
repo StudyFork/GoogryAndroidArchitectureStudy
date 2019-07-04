d8360bd99b3116c5db3a962ff89e546be0d86000
ojisu <dhwltn02@gmail.com>
2019-07-04 19:16:48 +0900

    HW-4 ViewModel, BindingAdapter 추가

---
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/view/bindingadapter/bindTickerData.kt
@@ -0,0 +1,18 @@
+package org.study.kotlin.androidarchitecturestudy.view.bindingadapter
+
+import android.databinding.BindingAdapter
+import android.support.v7.widget.RecyclerView
+import org.study.kotlin.androidarchitecturestudy.adapter.recyclerviewadapter.MainListAdapter
+import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
+
+@BindingAdapter("setTickerList")
+fun bindTickerListRecyclerView(
+    view: RecyclerView,
+    list: List<TickerModel>?
+) {
+    (view.adapter as? MainListAdapter)?.run {
+        notifyDataSetChanged()
+        list?.let { setList(it) }
+    }
+
+}
