07529c86949b9b8f3daca6844a835dad4c9a9787
yunji <zion830@honglab.org>
2019-07-05 03:13:45 +0900

    HW-3 BaseActivity, BaseViewHolder 추가

---
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/base/BaseViewHolder.kt
@@ -0,0 +1,9 @@
+package com.studyfirstproject.base
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+abstract class BaseViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    abstract fun bind(item: T)
+}
