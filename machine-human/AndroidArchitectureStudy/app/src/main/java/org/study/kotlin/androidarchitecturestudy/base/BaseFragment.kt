ebd610d1d277844d9045f3139a89015239ea0072
ojisu <dhwltn02@gmail.com>
2019-07-04 19:17:18 +0900

    HW-3 데이터 바인딩 적용

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseFragment.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseFragment.kt
@@ -1,16 +1,30 @@
 package org.study.kotlin.androidarchitecturestudy.base
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
-abstract class BaseFragment(
+abstract class BaseFragment<B : ViewDataBinding>(
     @LayoutRes
     private val layoutId: Int
-) : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(layoutId, container, false)
+) : Fragment(),BaseView {
+
+    protected lateinit var binding: B
+        private set
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun showErrorToast(errorMessage: Throwable) {
+
+    }
 }
--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/view/activity/main/MainFragment.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/view/activity/main/MainFragment.kt
@@ -1,17 +1,17 @@
 package org.study.kotlin.androidarchitecturestudy.view.activity.main
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.util.Log
 import android.view.View
-import kotlinx.android.synthetic.main.fragment_main.*
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import org.study.kotlin.androidarchitecturestudy.R
 import org.study.kotlin.androidarchitecturestudy.adapter.recyclerviewadapter.MainListAdapter
-import org.study.kotlin.androidarchitecturestudy.api.model.TickerModel
 import org.study.kotlin.androidarchitecturestudy.base.BaseFragment
 import org.study.kotlin.androidarchitecturestudy.data.TickerRepository
 import org.study.kotlin.androidarchitecturestudy.data.source.remote.TickerRemoteDataSource
+import org.study.kotlin.androidarchitecturestudy.databinding.FragmentMainBinding
 
 /**
  ***************************
@@ -31,26 +31,30 @@ i = Presenter : BasePresenter
 
 f = requestDataFromTickerRepository(marketName: String)
  */
-class MainFragment : BaseFragment(R.layout.fragment_main), MainContract.View {
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
-
-    override lateinit var presenter: MainContract.Presenter
     private val mainListAdapter: MainListAdapter = MainListAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         view.recyclerview_mainfragment.run {
             setHasFixedSize(true)
+            val message = arguments!!.getString(EXTRA_MESSAGE)
+
+            binding.viewModel = MainViewModel(TickerRepository(TickerRemoteDataSource()), message)
             adapter = mainListAdapter
         }
-        val message = arguments!!.getString(EXTRA_MESSAGE)
-        MainPresenter(
-            this,
-            TickerRepository(TickerRemoteDataSource())
-            , message
-        )
+
+        binding.viewModel?.observableErrorMessage?.observe(this, Observer {
+            errorMessage -> errorMessage?.let { showErrorToast(it) }
+        })
+
     }
 
+    override fun showErrorToast(errorMessage: Throwable) {
+        super.showErrorToast(errorMessage)
+        Toast.makeText(context,errorMessage.toString(),Toast.LENGTH_SHORT).show()
+    }
 
     companion object {
         fun createInstance(message: String): MainFragment {
@@ -62,28 +66,5 @@ class MainFragment : BaseFragment(R.layout.fragment_main), MainContract.View {
         }
     }
 
-
-    //MainContract.View의 함수
-    override fun setTickers(tickers: List<TickerModel>) {
-        mainListAdapter?.setList(tickers)
-        mainListAdapter?.notifyDataSetChanged()
-    }
-
-    //(기능 추가 예정)
-
-    override fun showProgress() {
-    }
-
-    //(기능 추가 예정)
-    override fun hideProgress() {
-    }
-
-    //(기능 추가 예정)
-    override fun setProgress(success: Boolean) {
-    }
-
-    //(기능 추가 예정)
-    override fun showErrorToast(errorMessage: Throwable) {
-    }
 }
 
