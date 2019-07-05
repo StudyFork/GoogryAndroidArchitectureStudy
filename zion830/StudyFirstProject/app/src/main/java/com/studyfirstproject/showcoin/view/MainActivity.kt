816ec958705f416d5cda9cb555b34c1dc0c231cb
yunji <zion830@honglab.org>
2019-07-06 02:22:46 +0900

    HW-3 사용하지 않는 view 초기화 코드 삭제, viewModel 생성

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/view/MainActivity.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/view/MainActivity.kt
@@ -1,72 +1,26 @@
 package com.studyfirstproject.showcoin.view
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.View
 import com.studyfirstproject.R
-import com.studyfirstproject.adapter.CoinRecyclerViewAdapter
+import com.studyfirstproject.base.BaseActivity
 import com.studyfirstproject.data.CoinRepository
-import com.studyfirstproject.data.model.TickerModel
 import com.studyfirstproject.databinding.ActivityMainBinding
-import com.studyfirstproject.showcoin.CoinContract
-import com.studyfirstproject.showcoin.CoinPresenter
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
+import com.studyfirstproject.showcoin.CoinViewModel
 
 
-class MainActivity : AppCompatActivity(), CoinContract.View {
-    override val presenter: CoinContract.Presenter =
-        CoinPresenter(this, CoinRepository())
+class MainActivity :
+    BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val coinAdapter: CoinRecyclerViewAdapter by lazy {
-        CoinRecyclerViewAdapter()
-    }
+    val viewModel by lazy { CoinViewModel(CoinRepository()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = DataBindingUtil
-            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.activity = this
 
         initView()
     }
 
     private fun initView() {
-        rv_main.adapter = coinAdapter
-        layout_main_swipe.setOnRefreshListener {
-            getMarketData()
-        }
-
-        getMarketData()
-    }
-
-    private fun getMarketData() {
-        presenter.getMarketData()
-    }
-
-    override fun setCoinInfo(coinData: List<TickerModel>) {
-        coinAdapter.setCoinList(coinData)
-    }
-
-    override fun showProgress() {
-        progress_main.visibility = View.VISIBLE
-    }
-
-    override fun hideProgress() {
-        progress_main.visibility = View.INVISIBLE
-    }
-
-    override fun hideRefreshIcon() {
-        if (layout_main_swipe.isRefreshing) {
-            layout_main_swipe.isRefreshing = false
-        }
-    }
-
-    override fun notifyError(msg: String, reason: String?) {
-        toast(msg)
-        Log.e(localClassName, reason ?: "No error message")
+        binding.viewModel = viewModel
+        viewModel.getMarketData()
     }
 }
