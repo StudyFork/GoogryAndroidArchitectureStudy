dfeb2ad7e4f90ec5113dcdf374ed053b59c495b9
yunji <zion830@honglab.org>
2019-07-06 02:21:40 +0900

    HW-3 recyclerview adapter에 databinding 추가

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/adapter/CoinRecyclerViewAdapter.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/adapter/CoinRecyclerViewAdapter.kt
@@ -1,26 +1,30 @@
 package com.studyfirstproject.adapter
 
-import android.graphics.Color
+import android.databinding.DataBindingUtil
+import android.databinding.ObservableArrayList
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.studyfirstproject.R
+import com.studyfirstproject.base.BaseViewHolder
 import com.studyfirstproject.data.model.TickerModel
-import com.studyfirstproject.util.NumberFormatUtil
-import kotlinx.android.synthetic.main.item_coin_info.view.*
-import org.jetbrains.anko.textColor
+import com.studyfirstproject.databinding.ItemCoinInfoBinding
 
-class CoinRecyclerViewAdapter
-    : RecyclerView.Adapter<CoinRecyclerViewAdapter.CoinItemViewHolder>() {
-    private var coinList = mutableListOf<TickerModel>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, type: Int): CoinItemViewHolder {
-        val viewGroup = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.item_coin_info, parent, false)
+class CoinRecyclerViewAdapter(
+    private val layoutResId: Int
+) : RecyclerView.Adapter<CoinRecyclerViewAdapter.CoinItemViewHolder>() {
+    private val coinList = ObservableArrayList<TickerModel>()
 
-        return CoinItemViewHolder(viewGroup)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: ItemCoinInfoBinding =
+            DataBindingUtil.inflate(inflater, viewType, parent, false)
+
+        return CoinItemViewHolder(binding)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return layoutResId
     }
 
     override fun getItemCount(): Int = coinList.size
@@ -37,23 +41,11 @@ class CoinRecyclerViewAdapter
         notifyDataSetChanged()
     }
 
-    inner class CoinItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val tvCoinName by lazy { itemView.tv_coin_name }
-        private val tvCoinTradePrice by lazy { itemView.tv_coin_trade_price }
-        private val tvCoinChangeRate by lazy { itemView.tv_coin_change_rate }
-        private val tvCoinAccTradePrice by lazy { itemView.tv_coin_acc_trade_price }
+    inner class CoinItemViewHolder(private val binding: ItemCoinInfoBinding) :
+        BaseViewHolder<TickerModel>(binding.root) {
 
-        fun bind(data: TickerModel) {
-            tvCoinName.text = data.market.substring(data.market.indexOf("-") + 1)
-            tvCoinTradePrice.text = NumberFormatUtil.insertComma(data.tradePrice.toLong())
-            tvCoinChangeRate.text = NumberFormatUtil.getPercent(data.signedChangeRate)
-            tvCoinAccTradePrice.text = NumberFormatUtil.skipUnderMillions(data.accTradePrice24h)
-
-            tvCoinChangeRate.textColor = when {
-                data.signedChangeRate > 0 -> Color.BLUE
-                data.signedChangeRate < 0 -> Color.RED
-                else -> Color.BLACK
-            }
+        override fun bind(item: TickerModel) {
+            binding.item = item
         }
     }
 }
