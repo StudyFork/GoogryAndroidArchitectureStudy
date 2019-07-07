6a084cc9917ef2c8617f16bb2790280f91685574
NackUn <king4521@naver.com>
2019-07-08 00:00:37 +0900

    HW-5 as?를 이용해 safe type casting

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerRecyclerViewExt.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerRecyclerViewExt.kt
@@ -5,8 +5,8 @@ import android.support.v7.widget.RecyclerView
 
 @BindingAdapter("app:setItems")
 fun setItems(recyclerView: RecyclerView, items: List<Map<String, String>>) {
-    val adapter: TickerAdapter = recyclerView.adapter as TickerAdapter
-    adapter.let {
+    val adapter: TickerAdapter? = recyclerView.adapter as? TickerAdapter
+    adapter?.let {
         it.setItems(items)
         it.notifyDataSetChanged()
     }
