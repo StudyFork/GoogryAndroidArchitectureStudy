32bc0b2c52a93a527b0a91e789e0de57eb8b6661
sport0102 <sunminlee89@gmail.com>
2019-07-09 11:59:35 +0900

    변하지 않는 변수 val로 선언

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainViewModel.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/ui/main/MainViewModel.kt
@@ -12,7 +12,7 @@ import java.math.BigDecimal
 
 class MainViewModel(private val upbitRepository: UpbitRepository) : BaseObservable() {
 
-    var tickerObservable = ObservableField<List<TickerResponse>>()
+    val tickerObservable = ObservableField<List<TickerResponse>>()
 
     var isDataLoadingError = ObservableField<Boolean>()
 
