c020f149580f11cbb04db6360efb64bccce6a91e
yunji <zion830@honglab.org>
2019-07-12 08:30:56 +0900

    HW-3 viewModel에서 View import 삭제

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/CoinViewModel.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/CoinViewModel.kt
@@ -2,24 +2,19 @@ package com.studyfirstproject.showcoin
 
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
-import android.databinding.ObservableInt
 import android.util.Log
-import android.view.View
 import com.studyfirstproject.data.CoinDataSource
 import com.studyfirstproject.data.model.TickerModel
 
 class CoinViewModel(private val repository: CoinDataSource) {
 
-    val progressStatus: ObservableInt = ObservableInt(View.INVISIBLE)
     val isLoading: ObservableBoolean = ObservableBoolean(false)
     val items = ObservableField<List<TickerModel>>(listOf())
 
     fun getMarketData() {
-        progressStatus.set(View.VISIBLE)
         repository.getAllMarkets({
             repository.getCoinData(it,
                 success = { tickers ->
-                    progressStatus.set(View.INVISIBLE)
                     isLoading.set(false)
                     items.set(tickers)
                 }, failed = { msg, reason ->
