6783858966f78fc1f80d7d217415699f5e6e9abe
NackUn <king4521@naver.com>
2019-07-12 11:40:22 +0900

    HW-5 equals -> ==로 변경

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerFragmentExt.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerFragmentExt.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 
 @BindingAdapter("selectedMarketColor")
 fun selectedMarketColor(textView: TextView, selectedMarket: String) {
-    if (textView.text.toString().equals(selectedMarket)) {
+    if (textView.text.toString() == selectedMarket) {
         textView.setTextColor(Color.BLUE)
     } else {
         textView.setTextColor(Color.GRAY)
