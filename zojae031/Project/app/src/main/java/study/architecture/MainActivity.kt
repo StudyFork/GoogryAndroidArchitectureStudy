9917bf2c2858bee811f9fe95ea9a7534e9ec4dd6
Zojae031 <edgar3@naver.com>
2019-07-14 15:19:31 +0900

    HW-01 AndroidX migration

--- zojae031/Project/app/src/main/java/study/architecture/MainActivity.kt
+++ zojae031/Project/app/src/main/java/study/architecture/MainActivity.kt
@@ -1,12 +1,41 @@
 package study.architecture
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabSetting()
+        pagerSetting()
+
+    }
+
+    private fun tabSetting() {
+        with(tabLayout) {
+            tabGravity = com.google.android.material.tabs.TabLayout.GRAVITY_FILL
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    pager.currentItem = tab!!.position
+                }
+            })
+        }
+    }
+
+    private fun pagerSetting() {
+        pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
     }
 }
