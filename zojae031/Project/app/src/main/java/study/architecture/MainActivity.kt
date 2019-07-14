74d5e948abbd47b07ae12eafc474c41b151b2510
Zojae031 <edgar3@naver.com>
2019-07-14 19:09:18 +0900

    HW-01 App name, App Icon 추가

--- zojae031/Project/app/src/main/java/study/architecture/MainActivity.kt
+++ zojae031/Project/app/src/main/java/study/architecture/MainActivity.kt
@@ -44,7 +44,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
+    /**
+     * [FragmentPagerAdapter] vs [FragmentStatePagerAdapter]
+     * @see FragmentPagerAdapter
+     * 등록한 Fragment를 최대한 영구적으로 가지도록 구현되어있음
+     * 적은양의 Fragment를 사용할 때 효과적
+     * @see FragmentStatePagerAdapter
+     * Fragment를 사용하지 않을때 Destroy가 됨
+     * 많은양의 Fragment를 사용할 때 효과적
+     * -> [FragemntPagerAdapter]를 사용한 이유 : 정해진 갯수의 프래그먼트를 사용하며, Destroy가 되는경우 API 콜이 다시 일어나기 때문에 오버헤드를 줄이기 위함
+     */
     inner class MainPageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         override fun getCount(): Int = TAB_NUMBER
 
