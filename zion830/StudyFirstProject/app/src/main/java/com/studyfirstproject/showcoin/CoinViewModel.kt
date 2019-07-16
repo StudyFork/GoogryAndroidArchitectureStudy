2af1e7c20e70ba2c14879bb0900e73c2e9b322a3
yunji <zion830@honglab.org>
2019-07-17 01:02:24 +0900

    HW-4 에러 발생시 화면 UI 변경

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/CoinViewModel.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/showcoin/CoinViewModel.kt
@@ -8,12 +8,16 @@ import com.studyfirstproject.data.model.TickerModel
 
 class CoinViewModel(private val repository: CoinDataSource) {
 
-    private val _isLoading by lazy { MutableLiveData<Boolean>() }
-    private val _errorMsg by lazy { MutableLiveData<String>() }
+    private val _isLoading = MutableLiveData<Boolean>()
+    private val _dataExist = MutableLiveData<Boolean>().apply { value = true }
+    private val _errorMsg = MutableLiveData<String>()
     private val _items = MutableLiveData<List<TickerModel>>()
 
     val isLoading: LiveData<Boolean>
         get() = _isLoading
+
+    val dataExist: LiveData<Boolean>
+        get() = _dataExist
 
     val errorMsg: LiveData<String>
         get() = _errorMsg
@@ -32,6 +36,7 @@ class CoinViewModel(private val repository: CoinDataSource) {
             repository.getCoinData(it,
                 success = { tickers ->
                     _isLoading.value = false
+                    _dataExist.value = true
                     _items.value = tickers
                 }, failed = { msg, reason ->
                     onDataNotAvailable(msg, reason)
@@ -50,5 +55,6 @@ class CoinViewModel(private val repository: CoinDataSource) {
         Log.e(msg, reason ?: "No error message")
         _errorMsg.value = "오류가 발생했습니다. 다시 시도해주세요."
         _isLoading.value = false
+        _dataExist.value = false
     }
 }
