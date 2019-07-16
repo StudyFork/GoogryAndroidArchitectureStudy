5e5ce0ddd2dbd81b67fcf246004afcc26db2040f
ojisu <dhwltn02@gmail.com>
2019-07-17 01:16:21 +0900

    HW-6 ACC ViewModel 제거

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseViewModel.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseViewModel.kt
@@ -2,6 +2,6 @@ package org.study.kotlin.androidarchitecturestudy.base
 
 import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel: ViewModel() {
+abstract class BaseViewModel{
     abstract fun onDataNotAvailable(errorMessage: Throwable)
 }
