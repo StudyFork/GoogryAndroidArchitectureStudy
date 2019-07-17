771de47c0f42ba907ddede1bd179289e853bf0e8
sport0102 <sunminlee89@gmail.com>
2019-07-17 16:22:40 +0900

    검색 기능 대소문자 구분 하지 않게 쿼리 수정

--- sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/domain/dao/TickerDao.kt
+++ sport0102/app/src/main/java/com/aiden/aiden/architecturepatternstudy/domain/dao/TickerDao.kt
@@ -11,7 +11,7 @@ interface TickerDao {
     @Query("SELECT * FROM ticker")
     fun getAll(): List<TickerResponse>
 
-    @Query("SELECT * FROM ticker WHERE market = :market")
+    @Query("SELECT * FROM ticker WHERE market = :market COLLATE NOCASE")
     fun getByMarket(market: String): List<TickerResponse>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
