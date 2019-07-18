85f778232103ace201dd9321553590b18a82a9bc
wswon <dnjsdntjr1@gmail.com>
2019-07-19 00:01:25 +0900

    HW-1 splash activity - 초기데이터 get

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/activity/MainActivity.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/activity/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.architecture.study
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
