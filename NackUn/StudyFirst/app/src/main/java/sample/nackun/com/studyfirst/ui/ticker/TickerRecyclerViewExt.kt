25ed01102a1860024ef331b6f5b9a9e12cfc89bd
NackUn <king4521@naver.com>
2019-07-18 16:25:32 +0900

    HW-6 UNCHECKED_CAST 어노테이션 추가

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerRecyclerViewExt.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerRecyclerViewExt.kt
@@ -4,6 +4,7 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import sample.nackun.com.studyfirst.base.BaseRecyclerView
 
+@Suppress("UNCHECKED_CAST")
 @BindingAdapter("setItems")
 fun RecyclerView.setItems(items: List<Map<String, String>>) {
     (this.adapter as? BaseRecyclerView.BaseAdapter<Any, *>)?.run {
