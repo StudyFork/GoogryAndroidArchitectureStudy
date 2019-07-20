a8d3f2300492aff106e65b5ab5f6f543da49b3ce
wswon <dnjsdntjr1@gmail.com>
2019-07-20 21:01:07 +0900

    수정사항 2 - TabPagerAdapter

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/adapter/TabPagerAdapter.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/adapter/TabPagerAdapter.kt
@@ -7,32 +7,23 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import com.architecture.study.R
 import com.architecture.study.fragment.CoinListFragment
 
-class TabPagerAdapter : FragmentStatePagerAdapter {
-    // Count number of tabs
-    private var tabCount: Int
-    private var mContext: Context
-
-    constructor(fm: FragmentManager, tabCount: Int, context: Context) : super(fm) {
-        this.tabCount = tabCount
-        this.mContext = context
-    }
-
+class TabPagerAdapter(fm: FragmentManager, private val tabCount: Int, private val context: Context) : FragmentStatePagerAdapter(fm) {
 
     /*4개 탭 one fragment로 bundle argument 처리*/
     override fun getItem(position: Int): Fragment? {
         // Returning the current tabs
         when (position) {
             0 -> {
-                return CoinListFragment.newInstance(mContext.getString(R.string.monetary_unit_1))
+                return CoinListFragment.newInstance(context.getString(R.string.monetary_unit_1))
             }
             1 -> {
-                return CoinListFragment.newInstance(mContext.getString(R.string.monetary_unit_2))
+                return CoinListFragment.newInstance(context.getString(R.string.monetary_unit_2))
             }
             2 -> {
-                return CoinListFragment.newInstance(mContext.getString(R.string.monetary_unit_3))
+                return CoinListFragment.newInstance(context.getString(R.string.monetary_unit_3))
             }
             3 -> {
-                return CoinListFragment.newInstance(mContext.getString(R.string.monetary_unit_4))
+                return CoinListFragment.newInstance(context.getString(R.string.monetary_unit_4))
             }
             else -> return null
         }
