6aaebe38d62221913417b7f7797d34729dfca1e9
wswon <dnjsdntjr1@gmail.com>
2019-07-20 20:51:22 +0900

    수정사항 1 - CoinListAdapter

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/adapter/CoinListAdapter.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/adapter/CoinListAdapter.kt
@@ -10,7 +10,7 @@ import com.architecture.study.adapter.viewholder.CoinViewHolder
 import com.architecture.study.model.Ticker
 import java.text.DecimalFormat
 
-class CoinListAdapter(internal var context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class CoinListAdapter(internal var context: Context) : RecyclerView.Adapter<CoinViewHolder>() {
 
     private var mCoinListData: List<Ticker> = arrayListOf()
     private var mCoinItemClickListner: CoinItemRecyclerViewClickListener? = null
@@ -35,7 +35,7 @@ class CoinListAdapter(internal var context: Context) : RecyclerView.Adapter<Recy
     }
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_coin, parent, false)
 
         val height = parent.measuredHeight / 10
@@ -47,53 +47,47 @@ class CoinListAdapter(internal var context: Context) : RecyclerView.Adapter<Recy
 
     override fun getItemCount(): Int = mCoinListData.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
         val pos = holder.adapterPosition
 
-        if (holder is CoinViewHolder) {
-            val coinViewHolder = holder
-            val coinTickerData = mCoinListData[pos]
+        val coinTickerData = mCoinListData[pos]
 
-            coinViewHolder.run {
-                itemView.setOnClickListener {
-                    mCoinItemClickListner?.let {
-                        it.onItemClicked(pos)
-                    }
+        holder.run {
+            itemView.setOnClickListener {
+                mCoinItemClickListner?.let {
+                    it.onItemClicked(pos)
                 }
-
-                tvCoinName.text = coinTickerData.market.replace("$monetaryUnitName-", "")
-                tvNowPrice.text = DecimalFormat("0.########").format(coinTickerData.trade_price)
-                if (coinTickerData.trade_price > coinTickerData.prev_closing_price) {
-                    tvCompareYesterday.text =
-                        DecimalFormat("0.##").format((1 - (coinTickerData.prev_closing_price / coinTickerData.trade_price)) * 10.0) + "%"
-                    tvCompareYesterday.setTextColor(ContextCompat.getColor(context, R.color.colorRed))
-                } else {
-                    tvCompareYesterday.text =
-                        "-" + DecimalFormat("0.##").format((1 - (coinTickerData.trade_price / coinTickerData.prev_closing_price)) * 10.0) + "%"
-                    tvCompareYesterday.setTextColor(ContextCompat.getColor(context, R.color.colorBlue))
-                }
-
-                /* 거래대금 단위 예외처리 */
-                tvTransactionAmount.text = when(monetaryUnitName){
-                    context.getString(R.string.monetary_unit_1)->{
-                        String.format("%,d", (coinTickerData.acc_trade_price_24h / 1000000).toInt()) + "M"
-                    }
-                    context.getString(R.string.monetary_unit_2)->{
-                        String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[0].toInt()) +
-                                "." + DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[1]
-                    }
-                    context.getString(R.string.monetary_unit_3)->{
-                        String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[0].toInt()) +
-                                "." + DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[1]
-                    }
-                    else->{
-                        String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h / 1000).split(".")[0].toInt()) + " k"
-                    }
-                }
-
             }
 
-        }
+            tvCoinName.text = coinTickerData.market.replace("$monetaryUnitName-", "")
+            tvNowPrice.text = DecimalFormat("0.########").format(coinTickerData.trade_price)
+            if (coinTickerData.trade_price > coinTickerData.prev_closing_price) {
+                tvCompareYesterday.text =
+                    DecimalFormat("0.##").format((1 - (coinTickerData.prev_closing_price / coinTickerData.trade_price)) * 10.0) + "%"
+                tvCompareYesterday.setTextColor(ContextCompat.getColor(context, R.color.colorRed))
+            } else {
+                tvCompareYesterday.text =
+                    "-" + DecimalFormat("0.##").format((1 - (coinTickerData.trade_price / coinTickerData.prev_closing_price)) * 10.0) + "%"
+                tvCompareYesterday.setTextColor(ContextCompat.getColor(context, R.color.colorBlue))
+            }
 
+            /* 거래대금 단위 예외처리 */
+            tvTransactionAmount.text = when (monetaryUnitName) {
+                context.getString(R.string.monetary_unit_1) -> {
+                    String.format("%,d", (coinTickerData.acc_trade_price_24h / 1000000).toInt()) + "M"
+                }
+                context.getString(R.string.monetary_unit_2) -> {
+                    String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[0].toInt()) +
+                            "." + DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[1]
+                }
+                context.getString(R.string.monetary_unit_3) -> {
+                    String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[0].toInt()) +
+                            "." + DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h).split(".")[1]
+                }
+                else -> {
+                    String.format("%,d", DecimalFormat("0.###").format(coinTickerData.acc_trade_price_24h / 1000).split(".")[0].toInt()) + " k"
+                }
+            }
+        }
     }
 }
