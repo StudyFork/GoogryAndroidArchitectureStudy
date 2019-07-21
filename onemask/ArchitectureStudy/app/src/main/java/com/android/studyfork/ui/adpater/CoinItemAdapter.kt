7cef523c4123da667b18ebb8cb05e08926f9d3dd
onemask <onemask@naver.com>
2019-07-21 21:23:48 +0900

    Edit > CoinAdapter에서 Viwholder Extension에 관한 리뷰 반영

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/adpater/CoinItemAdapter.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/adpater/CoinItemAdapter.kt
@@ -2,11 +2,14 @@ package com.android.studyfork.ui.adpater
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.studyfork.R
-import com.android.studyfork.ext.inflate
 import com.android.studyfork.network.remote.model.TickerResponse
-import kotlinx.android.synthetic.main.item_coin.view.*
+import com.android.studyfork.utill.filterTrade
+import com.android.studyfork.utill.inflate
+import com.android.studyfork.utill.setTradeAmount
+import com.android.studyfork.utill.setTradeDiff
 
 class CoinItemAdapter
     : RecyclerView.Adapter<CoinItemAdapter.ViewHolder>() {
@@ -30,11 +33,17 @@ class CoinItemAdapter
     }
 
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        private val textTitle: TextView = itemView.findViewById(R.id.text_title)
+        private val textCurrentPrice: TextView = itemView.findViewById(R.id.text_current_price)
+        private val textBeforeDay: TextView = itemView.findViewById(R.id.text_before_day)
+        private val textTotalTrade: TextView = itemView.findViewById(R.id.text_total_trade)
+
         fun bind(ticker: TickerResponse) = with(itemView) {
-            text_title.text = ticker.market.split("-")[1]
-            text_current_price.text = ticker.tradePrice.toString()
-            text_before_day.text = ticker.signedChangeRate.toString()
-            text_total_trade.text = ticker.accTradePrice24h.toString()
+            textTitle.text = ticker.market.split("-")[1]
+            textCurrentPrice.filterTrade(ticker.tradePrice)
+            textBeforeDay.setTradeDiff(ticker.signedChangeRate)
+            textTotalTrade.setTradeAmount(ticker.accTradePrice24h)
         }
     }
 }
