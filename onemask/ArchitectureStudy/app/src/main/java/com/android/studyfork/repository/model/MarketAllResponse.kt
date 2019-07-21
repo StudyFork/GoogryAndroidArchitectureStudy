481c2227fbd58e5b5ff571e9a46bcbdea76e761c
onemask <onemask@naver.com>
2019-07-21 17:41:04 +0900

    Delete 기존 모델 pacakge root 변경

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/repository/model/MarketAllResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.android.studyfork.repository.model
-
-import com.google.gson.annotations.SerializedName
-
-data class MarketAllResponse(
-    @SerializedName("market") val market: String,
-    @SerializedName("korean_name") val koreanName: String,
-    @SerializedName("english_name") val englishName: String
-)
