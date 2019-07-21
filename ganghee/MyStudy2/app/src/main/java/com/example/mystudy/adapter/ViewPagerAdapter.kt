62c23c5a97029f9a99b7fa0ef197e4babdbd4549
ganghee <idiosauri@naver.com>
2019-07-21 20:46:14 +0900

    HW-1 getItem 부분 수정

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/ViewPagerAdapter.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/ViewPagerAdapter.kt
@@ -7,17 +7,6 @@ import com.example.mystudy.fragment.UpbitFragment
 
 class ViewPagerAdapter(fm: FragmentManager, private val num_fragment: Int) : FragmentStatePagerAdapter(fm) {
 
-/*    //Singleton Design Pattern: 기존에 생성되었던 객체를 재사용
-    companion object {
-        private var upbitFragment: UpbitFragment? = null
-    }
-
-    @Synchronized
-    fun getUpbitFragment(): UpbitFragment {
-        if (upbitFragment == null) upbitFragment = UpbitFragment()
-        return upbitFragment!!
-    }*/
-
     override fun getItem(p0: Int): Fragment {
         return when (p0) {
             0 -> UpbitFragment("KRW")
