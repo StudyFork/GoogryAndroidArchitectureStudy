7b6b58755dd59de065388baebba8faf933ab6bed
onemask <onemask@naver.com>
2019-07-21 16:45:42 +0900

    Revert "Edit > model Type SnakeType 적용."
    This reverts commit d0b59477144dffddd3c2bd1bd3aa26fdeeca1bd0.

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/repository/model/MarketAllResponse.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/repository/model/MarketAllResponse.kt
@@ -1,9 +1,7 @@
 package com.android.studyfork.repository.model
 
-import com.google.gson.annotations.SerializedName
-
 data class MarketAllResponse(
-    @SerializedName("market") val market: String,
-    @SerializedName("korean_name") val koreanName: String,
-    @SerializedName("english_name") val englishName: String
+    val market: String,
+    val english_name: String,
+    val korean_name: String
 )
