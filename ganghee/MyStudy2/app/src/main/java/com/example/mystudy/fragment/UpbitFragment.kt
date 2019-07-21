cd87e1b59dfc3df043a0a5a91cb074446a089366
ganghee <idiosauri@naver.com>
2019-07-21 20:34:09 +0900

    HW-1 프래그먼트 4개를 하나로 수정

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/fragment/UpbitFragment.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/fragment/UpbitFragment.kt
@@ -1,14 +1,15 @@
 package com.example.mystudy.fragment
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mystudy.*
+import com.example.mystudy.R
 import com.example.mystudy.adapter.RecyclerViewAdapter
 import com.example.mystudy.network.RetrofitClient
 import com.example.mystudy.network.TickerResponse
@@ -16,22 +17,24 @@ import com.example.mystudy.network.UpbitApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_tether.*
+import kotlinx.android.synthetic.main.fragment_korean.*
 import org.jetbrains.anko.support.v4.ctx
 
-class TetherFragment : Fragment() {
+class UpbitFragment(tabName: String) : Fragment() {
 
     lateinit var recyclerViewAdapter: RecyclerViewAdapter
     internal lateinit var jsonApi: UpbitApi
     private val compositeDisposable = CompositeDisposable()
+    var firstMarket = tabName
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tether, container, false)
+        return inflater.inflate(R.layout.fragment_korean, container, false)
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -41,32 +44,45 @@ class TetherFragment : Fragment() {
         jsonApi = retrofit.create(UpbitApi::class.java)
 
         recyclerViewAdapter = RecyclerViewAdapter(context!!, dataList)
-        rv_tether.adapter = recyclerViewAdapter
-        rv_tether.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rv_korean.adapter = recyclerViewAdapter
+        rv_korean.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
 
-        getTickerResponse()
-
-
+        getMarketResponse()
     }
 
-    private fun getTickerResponse() {
+    private fun getMarketResponse() {
 
         compositeDisposable.add(
-            jsonApi.getTickers("USDT-BTC, USDT-ETH, USDT-LTC, USDT-XRP, USDT-ETC," +
-                    "USDT-DASH, USDT-ZEC, USDT-XMR, USDT-OMG, USDT-XVG")
+            jsonApi.getMarkets()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ marketList ->
-                    displayData(marketList)
+                .map {
+                    it.map {
+                        it.market
+                    }.joinToString(",")
+                }
+                .subscribe({
+                    jsonApi.getTickers(it)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .map {
+                            it.filter{
+                                it.market.split("-")[0] == firstMarket
+                            }
+                        }
+                        .subscribe({
+                            displayData(it)
+                        }, {
+                        })
                 }, {
-                    Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
-                })
+                        Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
+                    })
         )
     }
 
     private fun displayData(posts: List<TickerResponse>?) {
         val adapter = RecyclerViewAdapter(ctx, posts!!)
-        rv_tether.adapter = adapter
+        rv_korean.adapter = adapter
     }
 
     override fun onStop() {
