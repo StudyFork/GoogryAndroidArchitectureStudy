4c85c96bd13ebbe2b94fb94f7a54c417b1fb87fd
onemask <onemask@naver.com>
2019-07-23 00:27:45 +0900

    Edit > model data의 prefix다 Upbit로 통일

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/network/remote/model/MarketResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.android.studyfork.network.remote.model
-
-import com.google.gson.annotations.SerializedName
-
-data class MarketResponse(
-    @SerializedName("market") val market: String,
-    @SerializedName("korean_name") val koreanName: String,
-    @SerializedName("english_name") val englishName: String
-)
