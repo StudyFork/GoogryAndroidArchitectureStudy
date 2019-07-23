15683a3a86fc3cc9867dbf9d48e9c48a6ba95b89
ojisu <dhwltn02@gmail.com>
2019-07-24 01:23:30 +0900

    HW-7 ConvertTickerModel data class 제거

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/api/model/ConvertTickerModel.kt
+++
@@ -1,9 +0,0 @@
-package org.study.kotlin.androidarchitecturestudy.api.model
-
-
-data class ConvertTickerModel(
-    var convertMarket: String? = null,
-    var convertChangeRate: String? = null,
-    var convertTradePrice: String? = null,
-    var convertAccTradePrice24h: String? = null
-)
