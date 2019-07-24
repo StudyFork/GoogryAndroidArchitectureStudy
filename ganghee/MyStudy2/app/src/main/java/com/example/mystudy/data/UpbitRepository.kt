85eed1abb5150e09040207f976e72dc547217bed
ganghee <idiosauri@naver.com>
2019-07-24 17:55:48 +0900

    HW-2 가져온 데이터를 가공한 repository

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/UpbitRepository.kt
@@ -0,0 +1,28 @@
+package com.example.mystudy.data
+
+import com.example.mystudy.data.remote.UpbitDataSource
+import com.example.mystudy.network.UpbitApi
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * 가져온 데이터를 가공하는 클래스
+ **/
+
+class UpbitRepository (
+    private val api: UpbitApi
+){
+    fun getMarket(): Single<String> =
+        UpbitDataSource.getMarketResponse()
+            .map {
+                it.joinToString(",") {
+                    it.market
+                }
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+    fun getTicker(marketList: String) =
+        UpbitDataSource.getTickerResponse(marketList)
+}
