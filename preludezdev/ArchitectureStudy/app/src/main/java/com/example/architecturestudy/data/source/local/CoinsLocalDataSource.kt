35d196ced00d9821fbe9e76a8d64d23c8e770aea
Taehwi <preludez.dev@gmail.com>
2019-07-24 16:22:45 +0900

    HW-2 CoinsLocalDataSource 임시 구현

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/source/local/CoinsLocalDataSource.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/source/local/CoinsLocalDataSource.kt
@@ -1,5 +1,14 @@
 package com.example.architecturestudy.data.source.local
 
-object CoinsLocalDataSource {
+import com.example.architecturestudy.data.source.CoinsDataSource
+
+object CoinsLocalDataSource : CoinsDataSource {
+    override fun getAllMarket(callback: CoinsDataSource.GetAllMarketCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCoinTickers(markets: String, callback: CoinsDataSource.GetCoinTickersCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
