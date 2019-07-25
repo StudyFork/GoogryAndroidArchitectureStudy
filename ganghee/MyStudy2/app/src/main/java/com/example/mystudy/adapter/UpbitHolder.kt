9a3da6b8ba6e643810411c54c27e17866560d1db
ganghee <idiosauri@naver.com>
2019-07-26 04:41:42 +0900

    HW-2(require) ViewHolder클래스를 따로 파일로 만듬 bind메서드 추가

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/UpbitHolder.kt
@@ -0,0 +1,25 @@
+package com.example.mystudy.adapter
+
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mystudy.data.FormatTickers
+import kotlinx.android.synthetic.main.rv_item_list.view.*
+
+class UpbitHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val coinName: TextView = itemView.tv_coin_name
+    val tradePrice: TextView = itemView.tv_trade_price
+    val signedChangeRate: TextView = itemView.tv_signed_change_rate
+    val accTradePrice24th: TextView = itemView.tv_acc_trade_price_24h
+
+    fun bind(context: Context, ticker: FormatTickers) {
+        coinName.text = ticker.toMarket
+        tradePrice.text = ticker.toTradePrice
+        signedChangeRate.text = ticker.toSignedChangeRate
+        signedChangeRate.setTextColor(
+            ContextCompat.getColor(context, ticker.toRateColor))
+        accTradePrice24th.text = ticker.toAccTradePrice24h
+    }
+}
