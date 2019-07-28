a1513903f58f09435c0c08e7ee057a136e56315e
ganghee <idiosauri@naver.com>
2019-07-28 11:05:29 +0900

    HW2(require) repository의 데이터흐름 제거

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/UpbitRepository.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/UpbitRepository.kt
@@ -1,29 +1,24 @@
 package com.example.mystudy.data
 
-import com.example.mystudy.data.remote.UpbitDataSource
+import com.example.mystudy.data.remote.UpbitRemoteDataSource
 import com.example.mystudy.network.UpbitApi
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.internal.schedulers.NewThreadScheduler
 import io.reactivex.schedulers.Schedulers
 
 /**
  * 가져온 데이터를 가공하는 클래스
  **/
 
-class UpbitRepository (
-    private val api: UpbitApi
-){
+class UpbitRepository {
     fun getMarket(): Single<String> =
-        UpbitDataSource.getMarketResponse()
+        UpbitRemoteDataSource.getMarketResponse()
             .map {
                 it.joinToString(",") {
                     it.market
                 }
             }
-            .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.newThread())
+
 
     fun getTicker(marketList: String) =
-        UpbitDataSource.getTickerResponse(marketList)
+        UpbitRemoteDataSource.getTickerResponse(marketList)
 }
