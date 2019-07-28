e331f5062156d91bf89cf896b85a5d1d3b42dab8
leejinseong <dlwls5201@naver.com>
2019-07-28 14:46:33 +0900

    HW-2 database, api 제공자의 이름 변경

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/Injection.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/Injection.kt
@@ -3,16 +3,16 @@ package study.architecture.myarchitecture.data
 import android.content.Context
 import study.architecture.myarchitecture.data.repository.UpbitRepository
 import study.architecture.myarchitecture.data.repository.UpbitRepositoryImpl
-import study.architecture.myarchitecture.data.source.local.LocalDataSource
-import study.architecture.myarchitecture.data.source.remote.RemoteDataSource
+import study.architecture.myarchitecture.data.source.local.LocalDataBase
+import study.architecture.myarchitecture.data.source.remote.RemoteClient
 import study.architecture.myarchitecture.util.NetworkUtil
 
 object Injection {
 
     fun provideFolderRepository(context: Context): UpbitRepository {
-        val localDataSource = LocalDataSource.getInstance(context)
+        val localDataSource = LocalDataBase.getInstance(context)
         return UpbitRepositoryImpl(
-            RemoteDataSource.provideUpbitApi(),
+            RemoteClient.provideUpbitApi(),
             localDataSource.getUpbitDao(),
             NetworkUtil.isOnline(context)
         )
--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/source/local/LocalDataBase.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/source/local/LocalDataBase.kt
@@ -8,21 +8,21 @@ import study.architecture.myarchitecture.data.model.UpbitMarket
 import study.architecture.myarchitecture.data.model.UpbitTicker
 
 @Database(entities = [UpbitMarket::class, UpbitTicker::class], version = 1)
-abstract class LocalDataSource : RoomDatabase() {
+abstract class LocalDataBase : RoomDatabase() {
 
     abstract fun getUpbitDao(): UpbitDao
 
     companion object {
 
-        private var INSTANCE: LocalDataSource? = null
+        private var INSTANCE: LocalDataBase? = null
 
-        fun getInstance(context: Context): LocalDataSource {
+        fun getInstance(context: Context): LocalDataBase {
 
             if (INSTANCE == null) {
-                synchronized(LocalDataSource::class) {
+                synchronized(LocalDataBase::class) {
                     INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
-                        LocalDataSource::class.java,
+                        LocalDataBase::class.java,
                         "upbit.db"
                     )
                         //테스트용 : 빌드 시 마다 기존 데이터베이스 삭제
