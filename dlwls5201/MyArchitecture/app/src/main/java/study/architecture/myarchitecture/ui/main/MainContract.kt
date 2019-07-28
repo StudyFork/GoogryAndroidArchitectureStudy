e5a81d2d22452da26595c53a37586c6b5601e206
leejinseong <dlwls5201@naver.com>
2019-07-28 18:24:29 +0900

    HW-3 MainContract 관련 함수 선언

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainContract.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainContract.kt
@@ -1,13 +1,22 @@
 package study.architecture.myarchitecture.ui.main
 
+import android.os.Bundle
+
 interface MainContract {
 
     interface View {
 
         fun detachView()
+
+        fun setViewPagers(pagers: Array<String>)
+
+        fun setViewPagerTitles(titles: Array<String>)
     }
 
     interface Presenter {
 
+        fun loadData()
+
+        fun sendEventBus(bundle: Bundle)
     }
 }
