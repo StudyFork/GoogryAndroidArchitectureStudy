4b0c4805551100283032c98060dac4968d2734f3
wswon <dnjsdntjr1@gmail.com>
2019-07-28 10:51:45 +0900

    HW-2 CoinRemoteDataSource 추가

---
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/data/source/CoinRemoteDataSource.kt
@@ -0,0 +1,44 @@
+package com.architecture.study.data.source
+
+import com.architecture.study.network.model.MarketResponse
+import com.architecture.study.network.model.TickerResponse
+import com.architecture.study.network.request.UpbitRequest
+import com.architecture.study.network.request.UpbitRequestListener
+
+object CoinRemoteDataSource : CoinDataSource {
+
+    override fun getMarketList(listener: UpbitRequestListener<MarketResponse>) {
+        UpbitRequest().getMarketList(object : UpbitRequestListener<MarketResponse> {
+            override fun onSucess(dataList: List<MarketResponse>) {
+                listener.onSucess(dataList)
+            }
+
+            override fun onEmpty(str: String) {
+                listener.onEmpty(str)
+            }
+
+            override fun onFailure(str: String) {
+                listener.onFailure(str)
+            }
+        })
+
+    }
+
+    override fun getTickerList(marketNames: String, listener: UpbitRequestListener<TickerResponse>) {
+        UpbitRequest().getTickerList(marketNames, object :
+            UpbitRequestListener<TickerResponse> {
+            override fun onSucess(dataList: List<TickerResponse>) {
+                listener.onSucess(dataList)
+            }
+
+            override fun onEmpty(str: String) {
+                listener.onEmpty(str)
+            }
+
+            override fun onFailure(str: String) {
+                listener.onFailure(str)
+            }
+        })
+    }
+
+}
