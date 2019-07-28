2a17259632cd740a1570499995a7ff07027a122b
wswon <dnjsdntjr1@gmail.com>
2019-07-28 13:10:49 +0900

    HW-2 ViewHolder에서 context를 parameter로 받지않도록 변경

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/view/coin/adapter/holder/CoinViewHolder.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/view/coin/adapter/holder/CoinViewHolder.kt
@@ -14,14 +14,14 @@ class CoinViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val tvNowPrice = itemView.findViewById(R.id.now_price_tv) as TextView
     private val tvCompareYesterday = itemView.findViewById(R.id.compare_yesterday_tv) as TextView
     private val tvTransactionAmount = itemView.findViewById(R.id.transaction_amount_tv) as TextView
-    fun bind(context: Context, ticker: Ticker, listener: CoinListAdapter.CoinItemRecyclerViewClickListener) {
+    fun bind(ticker: Ticker, listener: CoinListAdapter.CoinItemRecyclerViewClickListener) {
         itemView.setOnClickListener {
             listener.onItemClicked(adapterPosition)
         }
         tvCoinName.text = ticker.coinName
         tvNowPrice.text = ticker.nowPrice
         tvCompareYesterday.text = ticker.compareYesterday
-        tvCompareYesterday.setTextColor(ContextCompat.getColor(context, ticker.compareYesterdayTextColor))
+        tvCompareYesterday.setTextColor(ContextCompat.getColor(itemView.context, ticker.compareYesterdayTextColor))
         tvTransactionAmount.text = ticker.transactionAmount
     }
 }
