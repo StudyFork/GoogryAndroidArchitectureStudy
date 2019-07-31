6224f4cd50c9e875cc0af6204e800cd9f82262d1
ojisu <dhwltn02@gmail.com>
2019-07-31 22:34:12 +0900

    BaseFragment 수정

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseFragment.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/base/BaseFragment.kt
@@ -7,24 +7,33 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 
-abstract class BaseFragment<B : ViewDataBinding>(
+abstract class BaseFragment<B : ViewDataBinding, VM: ViewModel>(
     @LayoutRes
     private val layoutId: Int
 ) : Fragment(), BaseView {
 
     protected lateinit var binding: B
-        private set
-
+    abstract val viewModel: VM
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
-        binding.lifecycleOwner = this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind {
+            binding.lifecycleOwner = this@BaseFragment
+            binding.setVariable(BR.vm, viewModel)
+        }
     }
 
     override fun showErrorToast(errorMessage: Throwable) {
