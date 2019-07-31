ff68aac5ad633d030c196fddc1d9d6e520cfaac9
NackUn <king4521@naver.com>
2019-07-31 14:36:08 +0900

    HW-8 viewModelModule 변수로 변경

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/ViewModelModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/ViewModelModule.kt
@@ -4,6 +4,6 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import sample.nackun.com.studyfirst.ui.ticker.TickerViewModel
 
-fun getViewModelModule() = module {
+val viewModelModule = module {
     viewModel { TickerViewModel(get()) }
 }
