e4542acf4d0ba43b3250f6eb7a26688495da589e
ojisu <dhwltn02@gmail.com>
2019-08-01 00:35:02 +0900

    TickerRemoteDataSource 모듈 생성시 BaseDataSource 명시

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/DataSourceModule.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/DataSourceModule.kt
@@ -1,10 +1,9 @@
 package org.study.kotlin.androidarchitecturestudy.di
 
 import org.koin.dsl.module
+import org.study.kotlin.androidarchitecturestudy.base.BaseDataSource
 import org.study.kotlin.androidarchitecturestudy.data.source.remote.TickerRemoteDataSource
 
 val tickerRemoteDataSource = module {
-    single{
-        TickerRemoteDataSource()
-    }
+    single{ TickerRemoteDataSource(get()) as BaseDataSource }
 }
