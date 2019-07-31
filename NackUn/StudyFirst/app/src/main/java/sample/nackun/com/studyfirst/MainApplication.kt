b3cc6b986c54709b9183ce6dbe4d65c75ca4eb0b
NackUn <king4521@naver.com>
2019-07-31 14:36:51 +0900

    HW-8 Module들 추가

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/MainApplication.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/MainApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import sample.nackun.com.studyfirst.di.getNetworkModule
+import sample.nackun.com.studyfirst.di.remoteModule
+import sample.nackun.com.studyfirst.di.repositoryModule
+import sample.nackun.com.studyfirst.di.viewModelModule
 
 @Suppress("unused")
 class MainApplication : Application() {
@@ -13,6 +16,9 @@ class MainApplication : Application() {
         startKoin {
             androidContext(this@MainApplication)
             modules(getNetworkModule("https://api.upbit.com/"))
+            repositoryModule
+            remoteModule
+            viewModelModule
         }
     }
 }
