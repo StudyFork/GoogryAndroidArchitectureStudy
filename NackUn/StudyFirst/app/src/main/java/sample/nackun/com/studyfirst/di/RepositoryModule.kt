a8db19998720cdda1fe51934f9b2b57c14e8d510
NackUn <king4521@naver.com>
2019-07-31 14:27:34 +0900

    HW-8 RepositoryModule 변경

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RepositoryModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RepositoryModule.kt
@@ -2,14 +2,7 @@ package sample.nackun.com.studyfirst.di
 
 import org.koin.dsl.module
 import sample.nackun.com.studyfirst.data.Repository
-import sample.nackun.com.studyfirst.data.remote.RemoteDataSource
 
-fun getRepositoryModule() = module {
-    single {
-        Repository(get())
-    }
-
-    single {
-        RemoteDataSource(get())
-    }
+val repositoryModule = module {
+    single { Repository(get()) }
 }
