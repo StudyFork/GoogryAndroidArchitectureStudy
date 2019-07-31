583dc5325d55954a9b9db05ce440d21a61e145b4
NackUn <king4521@naver.com>
2019-07-31 14:30:58 +0900

    HW-8 RemoteModule 변경

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
@@ -1,2 +1,8 @@
 package sample.nackun.com.studyfirst.di
 
+import org.koin.dsl.module
+import sample.nackun.com.studyfirst.data.remote.RemoteDataSource
+
+val RemoteModule = module {
+    single { RemoteDataSource(get()) }
+}
