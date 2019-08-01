992c86c1e98e585a46e8bb59105e3070cb9eb2a0
NackUn <king4521@naver.com>
2019-08-01 18:19:34 +0900

    HW-8 TickerFragment 코드 정리

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerFragment.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/ui/ticker/TickerFragment.kt
@@ -3,7 +3,6 @@ package sample.nackun.com.studyfirst.ui.ticker
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.ticker_fragment.*
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import sample.nackun.com.studyfirst.BR
 import sample.nackun.com.studyfirst.R
@@ -11,13 +10,11 @@ import sample.nackun.com.studyfirst.base.BaseFragment
 import sample.nackun.com.studyfirst.base.BaseRecyclerView
 import sample.nackun.com.studyfirst.databinding.TickerFragmentBinding
 import sample.nackun.com.studyfirst.databinding.TickerItemBinding
-import sample.nackun.com.studyfirst.network.UpbitApi
 
 class TickerFragment : BaseFragment<TickerFragmentBinding>(
     R.layout.ticker_fragment
 ) {
     private val firstMarketName = "KRW"
-    private val upbitApi: UpbitApi by inject()
     private val tickerAdapter =
         object :
             BaseRecyclerView.BaseAdapter<List<Map<String, String>>, TickerItemBinding>(
@@ -25,7 +22,7 @@ class TickerFragment : BaseFragment<TickerFragmentBinding>(
                 BR.tickerItem
             ) {}
 
-    private val vm: TickerViewModel by viewModel<TickerViewModel>()
+    private val vm: TickerViewModel by viewModel()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
