a08f50abdce95bcee484e4e8f2f1ee00cbb08066
leejinseong <dlwls5201@naver.com>
2019-08-04 15:35:14 +0900

    HW-3 Repo에서 처리했던 데이터 변경을 Present로 이동

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/tickerlist/TickerListPresenter.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/tickerlist/TickerListPresenter.kt
@@ -3,6 +3,7 @@ package study.architecture.myarchitecture.ui.tickerlist
 import io.reactivex.disposables.CompositeDisposable
 import study.architecture.myarchitecture.data.repository.UpbitRepository
 import study.architecture.myarchitecture.rxobserver.RxObserverHelper
+import study.architecture.myarchitecture.util.TextUtil
 import timber.log.Timber
 
 class TickerListPresenter(
@@ -44,6 +45,14 @@ class TickerListPresenter(
             .subscribe({
 
                 Timber.d("$it")
+
+                it.map { ticker ->
+                    ticker.setCoinName(TextUtil.getCoinName(ticker.market))
+                    ticker.setLast(TextUtil.getLast(ticker.tradePrice))
+                    ticker.setTradeDiff(TextUtil.getTradeDiff(ticker.signedChangeRate))
+                    ticker.setTradeAmount(TextUtil.getTradeAmount(ticker.accTradePrice24h))
+                }
+
                 view.setTickers(it.toMutableList())
 
             }) {
