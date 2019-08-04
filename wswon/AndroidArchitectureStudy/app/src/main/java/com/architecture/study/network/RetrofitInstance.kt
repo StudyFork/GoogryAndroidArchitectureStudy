1c7cd4e98c28c035a5fbc22ef5960f5e2f5bd952
wswon <dnjsdntjr1@gmail.com>
2019-08-04 23:51:12 +0900

    HW-3 RetrofitInstance파일분리와 인스턴스 callback interface 생성

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/network/RetrofitInstance.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/network/RetrofitInstance.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
     inline fun <reified T> getInstance(url: String) =
         Retrofit.Builder()
             .baseUrl(url)
