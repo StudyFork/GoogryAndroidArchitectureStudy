1a9937ac028e95301c25749cc9273c66ea42247f
wswon <dnjsdntjr1@gmail.com>
2019-08-04 23:47:52 +0900

    HW-3 CoinRepository 구현하는 CoinRespositoryImp

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/data/repository/CoinRepositoryImp.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/data/repository/CoinRepositoryImp.kt
@@ -3,13 +3,15 @@ package com.architecture.study.data.repository
 import com.architecture.study.data.source.CoinRemoteDataSourceImp
 import com.architecture.study.data.source.CoinRemoteDataSourceListener
 import com.architecture.study.network.RetrofitInstance
+import com.architecture.study.network.RetrofitInstanceCallBack
 import com.architecture.study.network.api.UpbitApi
 import com.architecture.study.network.model.MarketResponse
 import com.architecture.study.network.model.TickerResponse
 
 class CoinRepositoryImp : CoinRepository {
-
     private val upbitUrl = "https://api.upbit.com"
+
+    private lateinit var upbitRetrofitInstance: UpbitApi
 
     companion object {
         private var instance: CoinRepositoryImp? = null
@@ -22,13 +24,28 @@ class CoinRepositoryImp : CoinRepository {
             }
     }
 
+
+    // retrofit instance 가져옴
+    override fun setRetrofitInstance(callBack: RetrofitInstanceCallBack) {
+        upbitRetrofitInstance = RetrofitInstance.getInstance<UpbitApi>(upbitUrl)
+
+        if (upbitRetrofitInstance != null) {
+            callBack.onLoadded()
+        } else {
+            callBack.onNotLoadded()
+        }
+
+    }
+
+
+    // 생성시 Retrofit 객체를 주입해 작동하도록 변경
     override fun getMarketList(listener: CoinRemoteDataSourceListener<MarketResponse>) {
-        CoinRemoteDataSourceImp.getInstance(RetrofitInstance.getInstance<UpbitApi>(upbitUrl))
+        CoinRemoteDataSourceImp.getInstance(upbitRetrofitInstance)
             .getMarketList(listener)
     }
 
     override fun getTickerList(marketNames: String, listener: CoinRemoteDataSourceListener<TickerResponse>) {
-        CoinRemoteDataSourceImp.getInstance(RetrofitInstance.getInstance<UpbitApi>(upbitUrl))
+        CoinRemoteDataSourceImp.getInstance(upbitRetrofitInstance)
             .getTickerList(marketNames, listener)
     }
 }
