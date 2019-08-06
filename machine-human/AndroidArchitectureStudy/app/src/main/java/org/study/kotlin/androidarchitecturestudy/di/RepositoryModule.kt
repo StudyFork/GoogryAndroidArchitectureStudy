af7cb56caeb3dddfcc49255e52460b35cc13cb47
ojisu <dhwltn02@gmail.com>
2019-08-07 01:03:22 +0900

    HW-8 TickerRepository 모듈의 인자값을 BaseRepository 인터페이스로 받게 변경 적용

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/RepositoryModule.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/RepositoryModule.kt
@@ -1,8 +1,9 @@
 package org.study.kotlin.androidarchitecturestudy.di
 
 import org.koin.dsl.module
+import org.study.kotlin.androidarchitecturestudy.base.BaseRepository
 import org.study.kotlin.androidarchitecturestudy.data.TickerRepository
 
 val tickerRepositoryModule = module {
-    single { TickerRepository(get()) }
+    single<BaseRepository> { TickerRepository(get()) }
 }
