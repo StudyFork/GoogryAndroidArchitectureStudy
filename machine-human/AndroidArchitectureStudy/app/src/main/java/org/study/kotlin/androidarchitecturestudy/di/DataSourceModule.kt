8dbd4a6703e81c6da880e9a355e310ffcebf60db
ojisu <dhwltn02@gmail.com>
2019-08-07 01:03:55 +0900

    HW-8 TickerRemoteDataSource 모듈 캐스팅 표현 방법 변경

--- machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/DataSourceModule.kt
+++ machine-human/AndroidArchitectureStudy/app/src/main/java/org/study/kotlin/androidarchitecturestudy/di/DataSourceModule.kt
@@ -5,5 +5,5 @@ import org.study.kotlin.androidarchitecturestudy.base.BaseDataSource
 import org.study.kotlin.androidarchitecturestudy.data.source.remote.TickerRemoteDataSource
 
 val tickerRemoteDataSource = module {
-    single{ TickerRemoteDataSource(get()) as BaseDataSource }
+    single<BaseDataSource> { TickerRemoteDataSource(get()) }
 }
