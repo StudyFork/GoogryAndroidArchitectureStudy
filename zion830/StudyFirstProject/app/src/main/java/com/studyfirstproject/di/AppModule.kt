1a6b956133fca04be82f407686927c848bcdebf7
yunji <zion830@honglab.org>
2019-08-06 19:36:50 +0900

    HW-6 ViewModel 초기화 코드 간략하게 수정

--- zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/di/AppModule.kt
+++ zion830/StudyFirstProject/app/src/main/java/com/studyfirstproject/di/AppModule.kt
@@ -1,9 +1,5 @@
 package com.studyfirstproject.di
 
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.studyfirstproject.data.CoinDataSource
 import com.studyfirstproject.data.CoinRepository
 import com.studyfirstproject.showcoin.CoinViewModel
@@ -14,12 +10,5 @@ val appModule = module {
 
     single<CoinDataSource> { CoinRepository(get()) }
 
-    @Suppress("UNCHECKED_CAST")
-    viewModel { (fragmentActivity: FragmentActivity) ->
-        ViewModelProviders.of(fragmentActivity, object : ViewModelProvider.Factory {
-
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-                CoinViewModel(get()) as T
-        })[CoinViewModel::class.java]
-    }
+    viewModel { CoinViewModel(get()) }
 }
