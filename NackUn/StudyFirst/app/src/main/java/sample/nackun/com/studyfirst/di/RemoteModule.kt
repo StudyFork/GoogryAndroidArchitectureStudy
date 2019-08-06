e7cdf9406c1c365ae7861ef2465da5e94ecb49c9
NackUn <king4521@naver.com>
2019-08-06 11:21:51 +0900

    HW-8 remoteModule DataSource interface로 변환

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
@@ -1,8 +1,9 @@
 package sample.nackun.com.studyfirst.di
 
 import org.koin.dsl.module
+import sample.nackun.com.studyfirst.data.DataSource
 import sample.nackun.com.studyfirst.data.remote.RemoteDataSource
 
 val remoteModule = module {
-    single { RemoteDataSource(get()) }
+    single<DataSource> { RemoteDataSource(get()) }
 }
