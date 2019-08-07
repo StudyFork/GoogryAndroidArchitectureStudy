9d90db28baddbade607e8894c9780883d285f311
ganghee <idiosauri@naver.com>
2019-08-07 20:12:15 +0900

    [MainActivity] 패키지 변경

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
@@ -1,29 +1,17 @@
-package com.example.mystudy.ui.activity
+package com.example.mystudy.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mystudy.R
 import com.example.mystudy.adapter.ViewPagerAdapter
-import com.example.mystudy.data.UpbitRepository
-import com.example.mystudy.presenter.UpbitPresenter
-import com.example.mystudy.ui.fragment.UpbitFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var upbitPresenter: UpbitPresenter
-    private val upbitRepository = UpbitRepository()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val upbitFragment = supportFragmentManager.findFragmentById(R.id.fragment_upbit)
-                as UpbitFragment? ?: UpbitFragment.newInstance().also {
-            replaceFragmentInActivity(it, R.id.fragment_upbit)
-        }
-        upbitPresenter = UpbitPresenter(upbitRepository, upbitFragment)
 
         configureMainTab()
     }
