6ac672b4fba8be160a13e9927944d16a2c553d24
ganghee <idiosauri@naver.com>
2019-08-07 20:07:49 +0900

    [UpbitPresenter] View에서 구현된 기능, model 객체를 Presenter에서 구현

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/UpbitPresenter.kt
@@ -0,0 +1,48 @@
+package com.example.mystudy.ui
+
+import android.annotation.SuppressLint
+import com.example.mystudy.data.FormatTickers
+import com.example.mystudy.data.UpbitRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class UpbitPresenter(private val repository: UpbitRepository, private val tickerView: UpbitContract.View) :
+    UpbitContract.Presenter {
+
+    override fun start() {
+        //TODO: start UI
+    }
+
+    init {
+        tickerView.presenter = this
+    }
+
+    private val tickerList by lazy { mutableListOf<FormatTickers>() }
+
+
+    @SuppressLint("CheckResult")
+    override fun getTicker(firstMarket: String) {
+        repository.getMarket()
+            .observeOn(Schedulers.newThread())
+            .subscribe { it ->
+                repository.getTicker(it)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map {
+                        it.filter { TickerResponse ->
+                            TickerResponse.market.split("-")[0] == firstMarket
+
+                        }
+                    }
+                    .subscribe({ list ->
+                        list.map {
+                            tickerList.add(
+                                it.toTicker()
+                            )
+                            tickerView.showSuccessUpbitTickerList(tickerList)
+                        }
+                    }, {
+                        tickerView.showFailedUpbitTickerList()
+                    })
+            }
+    }
+}
