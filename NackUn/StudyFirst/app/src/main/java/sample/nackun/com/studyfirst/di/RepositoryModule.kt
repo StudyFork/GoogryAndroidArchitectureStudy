9a4373eca7f786737a50b5478ad649565cbabb73
NackUn <king4521@naver.com>
2019-08-08 10:43:50 +0900

    HW-8 repositoryModule RepositoryInterface interface로 변환

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RepositoryModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RepositoryModule.kt
@@ -2,7 +2,8 @@ package sample.nackun.com.studyfirst.di
 
 import org.koin.dsl.module
 import sample.nackun.com.studyfirst.data.Repository
+import sample.nackun.com.studyfirst.data.RepositoryInterface
 
 val repositoryModule = module {
-    single { Repository(get()) }
+    single<RepositoryInterface> { Repository(get()) }
 }
