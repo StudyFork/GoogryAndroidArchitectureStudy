468cae076f374c65b954930c2f4e385bc566d34d
soo <onemask@naver.com>
2019-08-10 14:29:50 +0900

    migration androidx

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/MainActivity.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.android.studyfork
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,3 +10,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
