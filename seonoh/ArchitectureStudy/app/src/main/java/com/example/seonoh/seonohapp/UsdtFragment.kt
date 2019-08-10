1d6cddebfe8abd309b954059e9ccd062a13daf0f
Seonoh Eam <>
2019-08-10 18:42:35 +0900

    create USDT Fragment

---
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/UsdtFragment.kt
@@ -0,0 +1,28 @@
+package com.example.seonoh.seonohapp
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UsdtFragment : Fragment() {
+
+    lateinit var mView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mView = inflater.inflate(R.layout.usdt_fragment, container, false)
+        return mView
+    }
+
+    companion object {
+        fun newInstance(): UsdtFragment {
+            return UsdtFragment()
+        }
+    }
+
+}
