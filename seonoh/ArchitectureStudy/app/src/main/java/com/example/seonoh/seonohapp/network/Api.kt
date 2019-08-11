85889af8d13fbfcdd6a11a30fd7ed2d95f30ebba
Seonoh Eam <>
2019-08-11 14:06:02 +0900

    create retroft

---
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/Api.kt
@@ -0,0 +1,9 @@
+package com.example.seonoh.seonohapp.network
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+
+}
---
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/RetrofitCreator.kt
@@ -0,0 +1,46 @@
+package com.example.seonoh.seonohapp.network
+
+import com.example.seonoh.seonohapp.BuildConfig
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.ihsanbal.logging.Level
+import com.ihsanbal.logging.LoggingInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.internal.platform.Platform
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://api.upbit.com/v1/market/all"
+class RetrofitCreator {
+
+    companion object{
+        fun createNet() : Api{
+            val client = OkHttpClient.Builder()
+            client.addInterceptor(
+                LoggingInterceptor.Builder()
+                    .loggable(BuildConfig.DEBUG)
+                    .setLevel(Level.BASIC)
+                    .log(Platform.INFO)
+                    .request("OH_req")
+                    .response("OH_res")
+                    .build()
+            )
+                .addNetworkInterceptor(StethoInterceptor())
+                .build()
+           val okClient = client.build()
+
+
+           val retrofit =  Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .client(okClient)
+                .build()
+
+
+           return retrofit.create(Api::class.java)
+        }
+
+    }
+
+}
