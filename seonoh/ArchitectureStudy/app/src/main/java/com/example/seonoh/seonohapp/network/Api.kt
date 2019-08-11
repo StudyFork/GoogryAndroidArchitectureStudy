949fd0bc7757fb65d0e5f715425b5a1178e4a998
Seonoh Eam <>
2019-08-11 14:25:22 +0900

    현재가 정보 API 추가, 모델 생성

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/Api.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/Api.kt
@@ -1,9 +1,14 @@
 package com.example.seonoh.seonohapp.network
 
+import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
+
+    @GET("/ticker")
+    fun getCurrentPriceInfo(
+        @Query("markets") page: Int): Single<CurrentPriceInfoModel>
 
 }
