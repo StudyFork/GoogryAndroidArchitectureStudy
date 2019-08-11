fd6638c704d8eb835f67cf057d415b4365ee9227
Seonoh Eam <>
2019-08-11 15:05:42 +0900

    add internet permission & 현재가API 적용

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/KrwFrgment.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/KrwFrgment.kt
@@ -2,20 +2,34 @@ package com.example.seonoh.seonohapp
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
+import com.example.seonoh.seonohapp.network.CurrentPriceInfoRequest
 
 
-class KrwFrgment : Fragment() {
+class KrwFrgment : Fragment(),CurrentPriceInfoRequest.ResultListener {
 
     lateinit var mView : View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.krw_fragment, container, false)
+
+        CurrentPriceInfoRequest().send(this,"KRW-BTC,BTC-BCC")
+
         return mView
+    }
+
+    override fun getCurrentInfoSuccess(result: CurrentPriceInfoModel) {
+        Log.e("getCurrentInfoSuccess","getCurrentInfoSuccess !!")
+    }
+
+    override fun getCurrentInfoFailed(code: String) {
+        Log.e("getCurrentInfoFailed","getCurrentInfoFailed !!")
     }
 
     companion object{
@@ -23,6 +37,4 @@ class KrwFrgment : Fragment() {
             return KrwFrgment()
         }
     }
-
-
 }
