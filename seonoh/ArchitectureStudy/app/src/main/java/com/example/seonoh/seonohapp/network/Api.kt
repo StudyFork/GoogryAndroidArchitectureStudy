99eabcb2fd4f68915b6aa27a719f7533d913cf41
Seonoh Eam <>
2019-08-11 16:39:24 +0900

    create market/all API

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/Api.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/network/Api.kt
@@ -1,14 +1,17 @@
 package com.example.seonoh.seonohapp.network
 
 import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
+import com.example.seonoh.seonohapp.model.Market
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
 
-    @GET("/ticker")
+    @GET("/v1/ticker")
     fun getCurrentPriceInfo(
         @Query("markets") markets: String): Single<CurrentPriceInfoModel>
 
+    @GET("/v1/market/all")
+    fun getMarketAll(): Single<Market>
 }
