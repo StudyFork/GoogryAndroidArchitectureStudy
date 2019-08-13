b8b3cb4ed50cef73ed974651216172ee5b51ce81
onemask <onemask@naver.com>
2019-08-13 19:06:58 +0900

    Add ViewPagerAdapter

---
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,42 @@
+package com.android.studyfork.ui.adapter
+
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.android.studyfork.ui.TickerListFragment
+
+/**
+ * created by onemask
+ */
+class ViewpagerAdapter(fragmentManager : FragmentManager) :
+    FragmentPagerAdapter(fragmentManager){
+
+    private var titles: Array<String>? = null
+    private var marketDataSet : Array<String>? = null
+
+    override fun getItem(position: Int): Fragment {
+        val item = marketDataSet?.get(position) ?:""
+        return TickerListFragment.newInstance(item)
+    }
+
+    override fun getCount(): Int  = marketDataSet?.size ?:0
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+    }
+
+    fun setData(items : Array<String>){
+        this.marketDataSet = items
+        notifyDataSetChanged()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titles?.get(position)
+    }
+
+    fun setTitles(items : Array<String>){
+        this.titles = items
+    }
+
+}
