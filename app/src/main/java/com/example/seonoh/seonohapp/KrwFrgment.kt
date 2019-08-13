eb37d7cd893f8d05d1da2b7dbdf745ae8ea54216
seonoh <eso0609@naver.com>
2019-08-13 10:57:49 +0900

    KRW 프레그먼트 생성

---
+++ app/src/main/java/com/example/seonoh/seonohapp/KrwFrgment.kt
@@ -0,0 +1,62 @@
+package com.example.seonoh.seonohapp
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
+import com.example.seonoh.seonohapp.network.CurrentPriceInfoRequest
+import kotlinx.android.synthetic.main.krw_fragment.view.*
+
+
+class KrwFrgment : Fragment(),CurrentPriceInfoRequest.ResultListener{
+
+    lateinit var mView : View
+    lateinit var mData : ArrayList<CurrentPriceInfoModel>
+    lateinit var mLinearLayoutManager: LinearLayoutManager
+    lateinit var mAdapter : CoinAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        mView = inflater.inflate(R.layout.krw_fragment, container, false)
+        Log.e("onCreateView","onCreateView")
+
+        requestData()
+
+        return mView
+    }
+
+    fun initView( data: ArrayList<CurrentPriceInfoModel>){
+        mAdapter = CoinAdapter(data,KRW_TYPE)
+        mLinearLayoutManager = LinearLayoutManager(activity,RecyclerView.VERTICAL,false)
+        mView.krwRecyclerView.apply {
+            layoutManager = mLinearLayoutManager
+            adapter = mAdapter
+        }
+    }
+
+    fun requestData(){
+        CurrentPriceInfoRequest().send(this, MainActivity.krwMarketData)
+    }
+
+    override fun getCurrentInfoSuccess(result: ArrayList<CurrentPriceInfoModel>) {
+        mData = result
+        initView(mData)
+    }
+
+    override fun getCurrentInfoFailed(code: String) {
+
+    }
+
+
+    companion object{
+        fun newInstance(): KrwFrgment {
+            return KrwFrgment()
+        }
+    }
+}
