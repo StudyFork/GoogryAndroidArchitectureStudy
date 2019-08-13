07a8dead1fca53e3c04e5661af1af0c41885301d
seonoh <eso0609@naver.com>
2019-08-13 10:57:04 +0900

    어댑터 생성

---
+++ app/src/main/java/com/example/seonoh/seonohapp/CoinAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.seonoh.seonohapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
+
+class CoinAdapter(data: ArrayList<CurrentPriceInfoModel>,val type : Int) : RecyclerView.Adapter<KrwItemViewHolder>(){
+
+    lateinit var mView : View
+    val mData = data
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KrwItemViewHolder {
+
+        mView = LayoutInflater.from(parent.context).inflate(R.layout.krw_item,parent,false)
+        return KrwItemViewHolder(mView,type)
+    }
+
+    override fun getItemCount(): Int = mData.size
+
+    override fun onBindViewHolder(holder: KrwItemViewHolder, position: Int) {
+        holder.bind(mData,position)
+    }
+}
+
