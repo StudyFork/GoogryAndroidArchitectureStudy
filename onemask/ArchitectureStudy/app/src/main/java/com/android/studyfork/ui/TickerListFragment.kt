b0590d2f6d2a87d8277042efa069a8b680cfd910
onemask <onemask@naver.com>
2019-08-13 19:07:40 +0900

    Add TickerListFragment

---
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/TickerListFragment.kt
@@ -0,0 +1,81 @@
+package com.android.studyfork.ui
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import com.android.studyfork.R
+import com.android.studyfork.network.api.UpbitService
+import com.android.studyfork.repository.UpbitRepository
+import com.android.studyfork.repository.UpbitRepositoryImpl
+import com.android.studyfork.ui.adapter.CoinItemAdapter
+import com.android.studyfork.util.inflate
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_ticker_list.*
+import timber.log.Timber
+
+
+class TickerListFragment : Fragment() {
+
+    private val upbitService by lazy{ UpbitService.getInstance().upbitDataSource }
+
+    private lateinit var upbitRepository: UpbitRepository
+    private lateinit var coinItemAdapter: CoinItemAdapter
+
+    @SuppressLint("CheckResult")
+    private fun getTicker(){
+        val markets = arguments?.getString(KEY_MARKETS) ?:""
+        upbitRepository.getTikcers(markets)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Timber.d("getTicker success")
+                coinItemAdapter.setData(it)
+            },{
+                Timber.e("${it.printStackTrace()}")
+            })
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = container?.inflate(R.layout.fragment_ticker_list)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        upbitRepository = UpbitRepositoryImpl(upbitService)
+        setRecyclerView()
+        getTicker()
+
+    }
+
+    private fun setRecyclerView() {
+        coinItemAdapter = CoinItemAdapter()
+        recyclerview.apply {
+            adapter = coinItemAdapter
+            addItemDecoration(DividerItemDecoration(context,1))
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(false)
+        }
+    }
+
+
+    companion object {
+        const val KEY_MARKETS = "markets"
+        fun newInstance(tickers: String) = TickerListFragment().apply {
+            arguments = Bundle().apply {
+                putString(KEY_MARKETS, tickers)
+            }
+        }
+    }
+
+
+}
+
+
