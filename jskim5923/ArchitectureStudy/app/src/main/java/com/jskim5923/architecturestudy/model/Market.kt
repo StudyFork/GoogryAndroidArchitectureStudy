59db69a72b7c43a70768712a9e805a1396b2ddcb
jskim <kjs6030@naver.com>
2019-08-15 00:04:17 +0900

    coin api 모델 클래스 추가

---
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/model/Market.kt
@@ -0,0 +1,7 @@
+package com.jskim5923.architecturestudy.model
+
+data class Market(
+    val english_name: String,
+    val korean_name: String,
+    val market: String
+)
---
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/model/Ticker.kt
@@ -0,0 +1,30 @@
+package com.jskim5923.architecturestudy.model
+
+data class Ticker(
+    val market: String,
+    val trade_date: String,
+    val trade_time: String,
+    val trade_date_kst: String,
+    val trade_time_kst: String,
+    val trade_timestamp: Long,
+    val opening_price: Double,
+    val high_price: Double,
+    val low_price: Double,
+    val trade_price: Double,
+    val prev_closing_price: Double,
+    val change: String,
+    val change_price: Double,
+    val change_rate: Double,
+    val signed_change_price: Double,
+    val signed_change_rate: Double,
+    val trade_volume: Double,
+    val acc_trade_price: Double,
+    val acc_trade_price_24h: Double,
+    val acc_trade_volume: Double,
+    val acc_trade_volume_24h: Double,
+    val highest_52_week_price: Double,
+    val highest_52_week_date: String,
+    val lowest_52_week_price: Double,
+    val lowest_52_week_date: String,
+    val timestamp: Long
+)
