54cfc5fd72156a5a7d86ab591d3e711a75f80573
syli9526 <syli4800@gmail.com>
2019-08-14 18:30:19 +0900

    add api service

---
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/ApiService.kt
@@ -0,0 +1,17 @@
+package com.example.architecturestudy.util
+
+import com.example.architecturestudy.model.MarketResponse
+import com.example.architecturestudy.model.TickerResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("/v1/market/all")
+    fun getMarketData(): Call<List<MarketResponse>>
+
+    @GET("/v1/ticker?")
+    fun getTickerData(
+        @Query("markets") markets: String
+    ): Call<List<TickerResponse>>
+}
