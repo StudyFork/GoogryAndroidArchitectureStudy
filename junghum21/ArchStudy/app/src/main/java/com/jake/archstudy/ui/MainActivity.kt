012a345e664185e9d937d3a8138db25a15dd5e8f
Jake <junghum21@gmail.com>
2019-08-15 14:43:18 +0900

    Update MainActivity
    데이터바인딩 객체 binding 프로퍼티 추가

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/MainActivity.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package com.jake.archstudy.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.jake.archstudy.R
+import com.jake.archstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
 }
