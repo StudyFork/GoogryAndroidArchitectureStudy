47908ef21461feddf5953aae963247108af03c05
syli9526 <syli4800@gmail.com>
2019-08-15 18:55:45 +0900

    add extend coinAdapter in coinFragment

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/CoinFragment.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/CoinFragment.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.architecturestudy.R
 import com.example.architecturestudy.model.TickerResponse
+import com.example.architecturestudy.ui.adapter.CoinAdapter
 import com.example.architecturestudy.util.UpbitListener
 import com.example.architecturestudy.util.UpbitRequest
+import kotlinx.android.synthetic.main.fragment_list_coin.*
 import java.text.DecimalFormat
 
-class CoinFragment : Fragment() {
+class CoinFragment : Fragment(), CoinAdapter.CoinItemRecyclerViewClickListener{
 
     var currencyList: List<String>? = null
-    lateinit var coinInfoList: List<CoinInfo>
+    lateinit var coinAdapter: CoinAdapter
+
+    override fun onItemClicked(position: Int) {
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return return inflater.inflate(R.layout.fragment_list_coin, container, false)
@@ -27,6 +34,13 @@ class CoinFragment : Fragment() {
         currencyList = arguments?.getStringArrayList(CURRENCY_LIST)
         currencyList?.let {
             getTickerInfo(it.joinToString(","))
+        }
+
+        coinAdapter = CoinAdapter(requireContext(),this)
+
+        rv_coin_list.run {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = coinAdapter
         }
 
     }
@@ -83,6 +97,8 @@ class CoinFragment : Fragment() {
                             Log.d("CoinInfo ::", toString())
                         })
                     }
+
+                    coinAdapter.setData(list)
                 }
 
                 override fun onFailure(str: String) {
