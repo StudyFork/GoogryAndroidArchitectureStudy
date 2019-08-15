79352b9609da39d75c03fcc8214cf105ac785e9e
syli9526 <syli4800@gmail.com>
2019-08-15 20:11:29 +0900

    register recyclerviewAdapter in mainActivity

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MainActivity.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MainActivity.kt
@@ -3,11 +3,66 @@ package com.example.architecturestudy.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.architecturestudy.R
+import com.example.architecturestudy.model.MarketResponse
+import com.example.architecturestudy.ui.adapter.ViewPagerAdapter
+import com.example.architecturestudy.util.UpbitListener
+import com.example.architecturestudy.util.UpbitRequest
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getInfomation()
+    }
+
+    fun getInfomation() {
+
+        UpbitRequest().getMarketInfo(object : UpbitListener<MarketResponse> {
+            override fun onResponse(dataList: List<MarketResponse>) {
+
+                currency_tab_layout.run {
+                    pageList.forEach {
+                        addTab(newTab().setText(getString(it)))
+                    }
+
+                    addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                        override fun onTabReselected(p0: TabLayout.Tab) {
+                            coin_list_viewpager.currentItem = p0.position
+                        }
+
+                        override fun onTabUnselected(p0: TabLayout.Tab) {
+                        }
+
+                        override fun onTabSelected(p0: TabLayout.Tab) {
+
+                        }
+
+                    })
+                }
+
+                coin_list_viewpager.run {
+                    adapter = ViewPagerAdapter(supportFragmentManager, this@MainActivity, pageList, dataList)
+                    addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(currency_tab_layout))
+                }
+            }
+
+            override fun onFailure(str: String) {
+            }
+
+        })
+    }
+
+
+    companion object {
+        val pageList = listOf(
+            R.string.currency_1,
+            R.string.currency_2,
+            R.string.currency_3,
+            R.string.currency_4
+        )
     }
 }
