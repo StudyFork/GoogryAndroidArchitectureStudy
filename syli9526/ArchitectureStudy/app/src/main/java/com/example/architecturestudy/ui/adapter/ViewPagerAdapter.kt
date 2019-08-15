8d5dd75984c69668f2fc48947f5d1ae6036bf5e9
syli9526 <syli4800@gmail.com>
2019-08-15 18:41:24 +0900

    add viewPager

---
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.architecturestudy.ui.adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.architecturestudy.model.MarketResponse
+import com.example.architecturestudy.ui.CoinFragment
+
+class ViewPagerAdapter(
+    fm: FragmentManager,
+    val context: Context,
+    val pageList: List<Int>,
+    val marketList: List<MarketResponse>
+) : FragmentStatePagerAdapter(fm) {
+
+    override fun getCount(): Int = pageList.size
+
+    override fun getItemPosition(`object`: Any): Int = POSITION_NONE
+
+    override fun getItem(position: Int): Fragment? = CoinFragment.newInstance(
+        marketList.filter { it.market.split("-")[0] == context.getString(pageList[position]) }.map { it.market } as ArrayList<String>
+    )
+
+}
