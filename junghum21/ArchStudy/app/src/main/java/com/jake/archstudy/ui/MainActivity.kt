9e083f867f6574c884fbf872f3af3535cfcffe77
Jake <junghum21@gmail.com>
2019-08-15 14:27:05 +0900

    Add MainActivity and xml

---
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jake.archstudy.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jake.archstudy.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
