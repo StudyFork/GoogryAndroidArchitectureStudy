b75714654c1c086492093cb4477b639ce7c8bae0
Jake <junghum21@gmail.com>
2019-08-15 16:12:40 +0900

    Remove nullable MarketResponse

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/network/response/MarketResponse.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/network/response/MarketResponse.kt
@@ -3,9 +3,9 @@ package com.jake.archstudy.network.response
 import com.google.gson.annotations.SerializedName
 
 data class MarketResponse(
-    val market: String?,
+    val market: String,
     @SerializedName("korean_name")
-    val koreanName: String?,
+    val koreanName: String,
     @SerializedName("english_name")
-    val englishName: String?
+    val englishName: String
 )
