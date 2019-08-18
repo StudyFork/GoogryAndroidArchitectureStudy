3a39e00e0e452e8bcded1d57d6bf57027673f692
Jake <junghum21@gmail.com>
2019-08-18 17:05:02 +0900

    Add Coin chart View

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/TickersFragment.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/TickersFragment.kt
@@ -6,7 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.jake.archstudy.data.source.UpbitRepository
 import com.jake.archstudy.databinding.FragmentTickersBinding
+import com.jake.archstudy.network.response.TickerResponse
+import com.jake.archstudy.ui.adapter.TickersAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TickersFragment : Fragment() {
 
@@ -26,6 +34,8 @@ class TickersFragment : Fragment() {
 
     private val marketName by lazy { arguments?.getString(ARGS_MARKET_NAME) }
 
+    private val repository = UpbitRepository()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,7 +47,33 @@ class TickersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tv.text = marketName
+        getTickers()
+    }
+
+    private fun getTickers() {
+        repository.getTicker(marketName ?: "")
+            .enqueue(object : Callback<List<TickerResponse>?> {
+                override fun onFailure(call: Call<List<TickerResponse>?>, t: Throwable) {
+                    t.printStackTrace()
+                }
+
+                override fun onResponse(
+                    call: Call<List<TickerResponse>?>,
+                    response: Response<List<TickerResponse>?>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { setTickers(it) }
+                    }
+                }
+            })
+    }
+
+    private fun setTickers(tickers: List<TickerResponse>) {
+        binding.rvTickers.run {
+            val adapter = adapter as? TickersAdapter ?: TickersAdapter().apply { adapter = this }
+            adapter.set(tickers)
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+        }
     }
 
 }
---
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/adapter/TickersAdapter.kt
@@ -0,0 +1,51 @@
+package com.jake.archstudy.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jake.archstudy.databinding.ItemTickerBinding
+import com.jake.archstudy.ext.setChangeRate
+import com.jake.archstudy.ext.setCoinName
+import com.jake.archstudy.ext.setCurrentPrice
+import com.jake.archstudy.ext.setTradePrice
+import com.jake.archstudy.network.response.TickerResponse
+
+class TickersAdapter : RecyclerView.Adapter<TickersAdapter.ViewHolder>() {
+
+    private val items = mutableListOf<TickerResponse>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            ItemTickerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    fun set(list: List<TickerResponse>) {
+        items.clear()
+        items.addAll(list)
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemTickerBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: TickerResponse) {
+            binding.run {
+                tvName.setCoinName(item.market)
+                tvCurrentPrice.setCurrentPrice(item.tradePrice)
+                tvChangeRate.setChangeRate(item.signedChangeRate)
+                tvAccTradePrice.setTradePrice(item.accTradePrice24h)
+            }
+        }
+
+    }
+}
