4a1a5b9e4e8adff5364e522f39def0b49e11a408
soo <onemask@naver.com>
2019-08-18 18:44:12 +0900

    HW-1 Edit TextView.kt > 모든 마켓 거래대금에 M 붙이는 사항 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/util/TextView.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/util/TextView.kt
@@ -3,6 +3,7 @@ package com.android.studyfork.util
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.android.studyfork.R
+import com.android.studyfork.network.model.TickerResponse
 import java.text.DecimalFormat
 
 /**
@@ -35,7 +36,7 @@ fun TextView.setTradeDiff(signedChangeRate : Double){
     text = "$rate%"
 }
 
-//거래대금
+/*//거래대금
 fun TextView.setTradeAmount(accTradePrice24h : Double) {
 
     val df = DecimalFormat("#,###")
@@ -44,6 +45,86 @@ fun TextView.setTradeAmount(accTradePrice24h : Double) {
         (accTradePrice24h < 1000000 && accTradePrice24h >= 1000) -> df.format(accTradePrice24h / 1000)
         else -> df.format(accTradePrice24h)
     }
-    text="$amount M"
+    text="$amount"
+}*/
+
+//거래대금2
+fun TextView.setTradeAmount(ticker: TickerResponse) {
+
+    var amount = ticker.accTradePrice24h.toLong()
+    when (ticker.market.split("-")[0]) {
+        "KRW" -> {
+            text = String.format(
+                context.getString(
+                    when {
+                        amount < 1_000_000L -> {
+                            R.string.trade_amount_fmt
+                        }
+                        amount < 1_000_000_000_000L -> {
+                            amount /= 1_000_000L
+                            R.string.trade_amount_mega_fmt
+                        }
+                        else -> {
+                            amount /= 1_000_000_000L
+                            R.string.trade_amount_giga_fmt
+                        }
+                    }
+                ), amount
+            )
+        }
+        "BTC", "ETH" -> text =
+            String.format(context.getString(R.string.trade_amount_milli_fmt), ticker.accTradePrice24h)
+        "USDT" -> {
+            text = String.format(
+                context.getString(
+                    when {
+                        amount < 1_000_000L -> {
+                            R.string.trade_amount_fmt
+                        }
+                        amount < 1_000_000_000L -> {
+                            amount /= 1_000L
+                            R.string.trade_amount_kilo_fmt
+                        }
+                        amount < 1_000_000_000_000L -> {
+                            amount /= 1_000_000L
+                            R.string.trade_amount_mega_fmt
+                        }
+                        else -> {
+                            amount /= 1_000_000_000L
+                            R.string.trade_amount_giga_fmt
+                        }
+                    }
+                ), amount
+            )
+        }
+        else -> {
+            val fmtResId = when {
+                amount < 1_000L -> {
+
+                    R.string.trade_amount_milli_fmt
+                }
+                amount < 1_000_000L -> {
+                    R.string.trade_amount_fmt
+                }
+                amount < 1_000_000_000L -> {
+                    amount /= 1_000L
+                    R.string.trade_amount_kilo_fmt
+                }
+                amount < 1_000_000_000_000L -> {
+                    amount /= 1_000_000L
+                    R.string.trade_amount_mega_fmt
+                }
+                else -> {
+                    amount /= 1_000_000_000L
+                    R.string.trade_amount_giga_fmt
+                }
+            }
+            text = if (amount < 1_000L) {
+                String.format(context.getString(fmtResId), ticker.accTradePrice24h)
+            } else {
+                String.format(context.getString(fmtResId), amount)
+            }
+        }
+    }
 }
 
