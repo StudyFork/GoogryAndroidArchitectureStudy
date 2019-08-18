f85e0ab68103002495e3683d3e6d6f00705b6554
syli9526 <syli4800@gmail.com>
2019-08-18 15:23:43 +0900

    HW-1 UpbitRequest 상위 클래스추가

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/UpbitRequest.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/UpbitRequest.kt
@@ -5,23 +5,15 @@ import com.example.architecturestudy.model.TickerResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class UpbitRequest {
 
-    private val url = "https://api.upbit.com"
+class UpbitRequest{
+
+    private val upbitService = UpbitService.getInstance().retrofit
 
     fun getMarketInfo(listenser: UpbitListener<MarketResponse>) {
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val apiService = retrofit.create(ApiService::class.java)
-
-        apiService.getMarketData()?.enqueue(object : Callback<List<MarketResponse>> {
+        upbitService.getMarketData()?.enqueue(object : Callback<List<MarketResponse>> {
             override fun onFailure(call: Call<List<MarketResponse>>, t: Throwable) {
                 t.message?.let {
                     listenser.onFailure(it)
@@ -38,14 +30,7 @@ class UpbitRequest {
 
     fun getTickerInfo(listenser: UpbitListener<TickerResponse>, name: String) {
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val apiService = retrofit.create(ApiService::class.java)
-
-        apiService.getTickerData(name).enqueue(object : Callback<List<TickerResponse>> {
+        upbitService.getTickerData(name).enqueue(object : Callback<List<TickerResponse>> {
 
             override fun onFailure(call: Call<List<TickerResponse>>, t: Throwable) {
                 t.message?.let {
