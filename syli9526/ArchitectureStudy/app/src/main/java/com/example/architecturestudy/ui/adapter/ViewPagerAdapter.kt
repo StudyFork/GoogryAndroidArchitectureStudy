6094d39344af02d7fa8184284115cc07c041b54f
syli9526 <syli4800@gmail.com>
2019-08-18 17:40:46 +0900

    HW-1 개행 추가

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/adapter/ViewPagerAdapter.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/adapter/ViewPagerAdapter.kt
@@ -19,7 +19,8 @@ class ViewPagerAdapter(
     override fun getItemPosition(`object`: Any): Int = POSITION_NONE
 
     override fun getItem(position: Int): Fragment? = CoinFragment.newInstance(
-        marketList.filter { it.market.split("-")[0] == context.getString(pageList[position]) }.map { it.market } as ArrayList<String>
+        marketList.filter { it.market.split("-")[0] == context.getString(pageList[position])}
+            .map { it.market } as ArrayList<String>
     )
 
 }
