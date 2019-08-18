e50e421fcda8b15a863461d75309476c08fea3a9
syli9526 <syli4800@gmail.com>
2019-08-18 15:47:40 +0900

    타입 캐스팅 삭제

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/adapter/ViewHolder.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/adapter/ViewHolder.kt
@@ -8,12 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.architecturestudy.R
 import com.example.architecturestudy.ui.CoinInfo
 
-class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val tvCoinName = itemView.findViewById(R.id.tv_coin_name) as TextView
-    private val tvPresentPrice = itemView.findViewById(R.id.tv_present_price) as TextView
-    private val tvCompareWithYesterday = itemView.findViewById(R.id.tv_compare_yesterday) as TextView
-    private val tvTransactionAmount = itemView.findViewById(R.id.tv_transaction_amount) as TextView
+    private val tvCoinName: TextView = itemView.findViewById(R.id.tv_coin_name)
+    private val tvPresentPrice: TextView = itemView.findViewById(R.id.tv_present_price)
+    private val tvCompareWithYesterday: TextView = itemView.findViewById(R.id.tv_compare_yesterday)
+    private val tvTransactionAmount: TextView = itemView.findViewById(R.id.tv_transaction_amount)
 
     fun bind(context: Context, coinInfo: CoinInfo) {
 
