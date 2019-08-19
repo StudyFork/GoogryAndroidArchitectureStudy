063ba041265290b2a708a6dca43abbd3473eccaf
preludezdev <preludez.dev@gmail.com>
2019-08-19 13:51:36 +0900

    HW-3 convertTickerIntoCoin() 메소드를 data class 내부 메소드로 이동

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/CoinTickerResponse.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/CoinTickerResponse.kt
@@ -1,5 +1,7 @@
 package com.example.architecturestudy.data
 
+import android.graphics.Color
+import com.example.architecturestudy.util.Util
 import com.google.gson.annotations.SerializedName
 
 data class CoinTickerResponse(
@@ -11,4 +13,47 @@ data class CoinTickerResponse(
     val signedChangeRate: Double = 0.0,
     @SerializedName("acc_trade_price_24h")
     val accTradePriceH: Double = 0.0
-)
+) {
+    private val oneThousand = 1_000
+    private val oneHundredThousand = 100_000
+    private val tenMillion = 10_000_000
+
+    fun convertTickerIntoCoin(): Coin {
+        //코인 이름
+        val coinName = market.split("-")[1]
+
+        //현재가
+        val tradePrice = when {
+            tradePrice > oneThousand ->
+                Util.convertBigNumberToStdString(tradePrice.toInt())
+            tradePrice > 2 ->
+                String.format("%.2f", tradePrice)
+            else ->
+                String.format("%.8f", tradePrice)
+        }
+
+        //전일대비
+        val signedChangeRate = String.format("%.2f", signedChangeRate * 100) + "%"
+
+        //전일대비 색깔 지정
+        val coinColor = if (signedChangeRate.startsWith('-')) Color.BLUE else Color.RED
+
+        //거래대금
+        val accTradePriceH = when {
+            accTradePriceH > tenMillion -> {
+                Util.convertBigNumberToStdString((accTradePriceH / 1000000).toInt()) + "M"
+            }
+
+            accTradePriceH > oneHundredThousand ->
+                Util.convertBigNumberToStdString(accTradePriceH.toInt() / 1000) + "k"
+
+            accTradePriceH > oneThousand ->
+                Util.convertBigNumberToStdString(accTradePriceH.toInt())
+
+            else ->
+                String.format("%.3f", accTradePriceH)
+        }
+
+        return Coin(coinName, tradePrice, signedChangeRate, accTradePriceH, coinColor)
+    }
+}
--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MarketPresenter.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MarketPresenter.kt
@@ -1,11 +1,7 @@
 package com.example.architecturestudy.ui
 
-import android.graphics.Color
 import android.util.Log
-import com.example.architecturestudy.data.Coin
-import com.example.architecturestudy.data.CoinTickerResponse
 import com.example.architecturestudy.data.source.CoinsRepository
-import com.example.architecturestudy.util.Util
 
 class MarketPresenter(
     private val marketFragmentView: MarketContract.View,
@@ -19,19 +15,19 @@ class MarketPresenter(
 
         if (keyMarket != null) {
             repository
-                .getAllMarket({ coinMarketResponse ->
+                .getAllMarket({ coinMarketResponses ->
 
-                    if (coinMarketResponse != null) {
+                    if (coinMarketResponses != null) {
 
-                        val targetTickers = coinMarketResponse.filter {
+                        val targetTickers = coinMarketResponses.filter {
                             it.market.split('-')[0] == keyMarket
                         }.joinToString(separator = ",") { it.market }
 
                         repository
-                            .getCoinTickers(targetTickers, { coinTickerResponse ->
+                            .getCoinTickers(targetTickers, { coinTickerResponses ->
                                 //map() 스트림 함수 : 컬렉션 내 인자를 변환하여 반환
-                                if (coinTickerResponse != null) {
-                                    marketFragmentView.showTickerData(coinTickerResponse.map(::convertTickerIntoCoin))
+                                if (coinTickerResponses != null) {
+                                    marketFragmentView.showTickerData(coinTickerResponses.map { it.convertTickerIntoCoin() })
                                     marketFragmentView.hideProgressBar() // 프로그레스바 종료
                                 }
                             }, { onFailCallback(it) })
@@ -43,44 +39,4 @@ class MarketPresenter(
     override fun onFailCallback(errorMsg: String) {
         Log.d("test", errorMsg)
     }
-
-    private fun convertTickerIntoCoin(ticker: CoinTickerResponse): Coin {
-        //코인 이름
-        val market = ticker.market.split("-")[1]
-
-        //현재가
-        val tradePrice = when {
-            ticker.tradePrice > 1_000 ->
-                Util.convertBigNumberToStdString(ticker.tradePrice.toInt())
-            ticker.tradePrice > 2 ->
-                String.format("%.2f", ticker.tradePrice)
-            else ->
-                String.format("%.8f", ticker.tradePrice)
-        }
-
-        //전일대비
-        val signedChangeRate = String.format("%.2f", ticker.signedChangeRate * 100) + "%"
-
-        //전일대비 색깔 지정
-        val coinColor = if (signedChangeRate.startsWith('-')) Color.BLUE else Color.RED
-
-        //거래대금
-        val accTradePriceH = when {
-            ticker.accTradePriceH > 10_000_000 -> {
-                Util.convertBigNumberToStdString((ticker.accTradePriceH / 1000000).toInt()) + "M"
-            }
-
-            ticker.accTradePriceH > 100_000 ->
-                Util.convertBigNumberToStdString(ticker.accTradePriceH.toInt() / 1000) + "k"
-
-            ticker.accTradePriceH > 1_000 ->
-                Util.convertBigNumberToStdString(ticker.accTradePriceH.toInt())
-
-            else ->
-                String.format("%.3f", ticker.accTradePriceH)
-        }
-
-        return Coin(market, tradePrice, signedChangeRate, accTradePriceH, coinColor)
-    }
-
 }
