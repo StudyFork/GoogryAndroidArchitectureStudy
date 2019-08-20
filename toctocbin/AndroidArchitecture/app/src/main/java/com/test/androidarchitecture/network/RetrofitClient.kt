19ad5628f820a4e87e8482613eab63c3c48986ea
toctocbin <haanbin@naver.com>
2019-08-20 15:57:18 +0900

    생성자 private 추가

--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/network/RetrofitClient.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/network/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class RetrofitClient {
+class RetrofitClient private constructor(){
 
     private lateinit var retrofit: Retrofit
 
