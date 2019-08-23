508bcb91bfcbf9d77e150f49a9d2d9380c7fac7a
Seonoh Eam <>
2019-08-23 10:28:13 +0900

    expose어노테이션 제거

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/CurrentPriceInfoModel.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/CurrentPriceInfoModel.kt
@@ -5,76 +5,52 @@ import com.google.gson.annotations.SerializedName
 
 data class CurrentPriceInfoModel(
 
-    @Expose
     @SerializedName("market")
     val market: String,
-    @Expose
     @SerializedName("trade_date")
     val tradeDate: String,
-    @Expose
     @SerializedName("trade_time")
     val tradeTime: String,
-    @Expose
     @SerializedName("trade_date_kst")
     val tradeDateKst: String,
-    @Expose
     @SerializedName("trade_time_kst")
     val tradeTimeKst: String,
-    @Expose
     @SerializedName("trade_timestamp")
     val tradeTimeStamp: Long,
-    @Expose
     @SerializedName("opening_price")
     val openingPrice: Double,
-    @Expose
     @SerializedName("high_price")
     val highPrice: Double,
-    @Expose
     @SerializedName("low_price")
     val lowPrice: Double,
-    @Expose
     @SerializedName("trade_price")
     val tradePrice: Double,
-    @Expose
     @SerializedName("prev_closing_price")
     val prevClosingPrice: Double,
-    @Expose
     @SerializedName("change")
     val change: String,
-    @Expose
     @SerializedName("change_price")
     val changePrice: Double,
-    @Expose
     @SerializedName("change_rate")
     val changeRate: Double,
-    @Expose
     @SerializedName("signed_change_price")
     val signedChangePrice: Double,
-    @Expose
     @SerializedName("signed_change_rate")
     val signedChangeRate: Double,
-    @Expose
     @SerializedName("trade_volume")
     val tradeVolume: Double,
-    @Expose
     @SerializedName("acc_trade_price")
     val accTradePrice: Double,
-    @Expose
     @SerializedName("acc_trade_price_24h")
     val accTradePrice_24h: Double,
-    @Expose
     @SerializedName("highest_52_week_price")
     val highest52WeekPrice: Double,
-    @Expose
     @SerializedName("highest_52_week_date")
     val highest52WeekDate: String,
-    @Expose
     @SerializedName("lowest_52_week_price")
     val lowest52WeekPrice: Double,
-    @Expose
     @SerializedName("lowest_52_week_date")
     val lowest52WeekDate: String,
-    @Expose
     @SerializedName("timestamp")
     val timestamp: Long
 )
