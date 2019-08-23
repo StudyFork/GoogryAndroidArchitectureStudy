7bd245b1700d5b6afffff01c7e13a6ec0fd363f6
onemask <onemask@naver.com>
2019-08-23 20:04:39 +0900

    HW-1 > CoinItemAdapter에서 TickerResponse가 아닌 Ticker로 참조 Data Type 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/adapter/CoinItemAdapter.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/adapter/CoinItemAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.studyfork.R
-import com.android.studyfork.network.model.TickerResponse
+import com.android.studyfork.network.model.Ticker
 import com.android.studyfork.util.filterTrade
 import com.android.studyfork.util.inflate
 import com.android.studyfork.util.setTradeAmount
@@ -16,7 +16,7 @@ import com.android.studyfork.util.setTradeDiff
  */
 class CoinItemAdapter  : RecyclerView.Adapter<CoinItemAdapter.ViewHolder>() {
 
-     private val dataList = mutableListOf<TickerResponse>()
+     private val dataList = mutableListOf<Ticker>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(parent.inflate(R.layout.item_coin))
@@ -26,7 +26,7 @@ class CoinItemAdapter  : RecyclerView.Adapter<CoinItemAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
         holder.bind(dataList[position])
 
-    fun setData(dataSet: List<TickerResponse>) {
+    fun setData(dataSet: List<Ticker>) {
         with(this.dataList) {
             clear()
             addAll(dataSet)
@@ -41,10 +41,10 @@ class CoinItemAdapter  : RecyclerView.Adapter<CoinItemAdapter.ViewHolder>() {
         private val textBeforeDay: TextView = itemView.findViewById(R.id.text_before_day)
         private val textTotalTrade: TextView = itemView.findViewById(R.id.text_total_trade)
 
-        fun bind(ticker: TickerResponse) = with(itemView) {
-            textTitle.text = ticker.market.split("-")[1]
-            textCurrentPrice.filterTrade(ticker.tradePrice)
-            textBeforeDay.setTradeDiff(ticker.signedChangeRate)
+        fun bind(ticker: Ticker) = with(itemView) {
+            textTitle.text = ticker.marketName.split("-")[1]
+            textCurrentPrice.filterTrade(ticker.currentTradePrice)
+            textBeforeDay.setTradeDiff(ticker.beforeSignedChangeRate)
             textTotalTrade.setTradeAmount(ticker)
         }
     }
