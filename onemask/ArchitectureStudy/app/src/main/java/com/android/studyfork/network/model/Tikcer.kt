0c6a944015686d995dbcc20f20d2db2ebfc65ed7
onemask <onemask@naver.com>
2019-08-23 14:09:26 +0900

    HW-1 > Add Ticker.kt(뷰에 필요한 데이터만 담은 별도의 모델 추가)

---
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/network/model/Tikcer.kt
@@ -0,0 +1,11 @@
+package com.android.studyfork.network.model
+
+/**
+ * created by onemask
+ */
+data class Ticker(
+    val marketName : String, //코인명
+    val currentTradePrice : Double, //현재가
+    val beforeSignedChangeRate : Double, //전일대비
+    val totalTrade : Double  //거래대금
+)
