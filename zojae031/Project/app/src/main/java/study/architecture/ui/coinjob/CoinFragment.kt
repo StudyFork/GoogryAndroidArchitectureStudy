3ed090c60a9575e85305e3502e1bfbddfa92db18
Zojae031 <edgar3@naver.com>
2019-08-25 16:30:15 +0900

    HW-05 coingFragment 수정

--- zojae031/Project/app/src/main/java/study/architecture/ui/coinjob/CoinFragment.kt
+++ zojae031/Project/app/src/main/java/study/architecture/ui/coinjob/CoinFragment.kt
@@ -9,13 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_coin.*
-import kotlinx.android.synthetic.main.fragment_coin.view.*
 import study.architecture.R
 import study.architecture.data.local.LocalDataSourceImpl
 import study.architecture.data.remote.RemoteDataSourceImpl
 import study.architecture.data.repository.RepositoryImpl
+import study.architecture.databinding.FragmentCoinBinding
 import study.architecture.presentation.CoinContract
 import study.architecture.presentation.CoinPresenter
 
@@ -40,23 +40,30 @@ class CoinFragment : Fragment(), CoinContract.View {
         }
     }
     private val adapter = CoinDataAdapter()
+    private lateinit var binding: FragmentCoinBinding
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_coin, container, false).apply {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate<FragmentCoinBinding>(
+            LayoutInflater.from(context),
+            R.layout.fragment_coin,
+            container,
+            false
+        ).apply {
             recyclerView.adapter = adapter
             loading.animation = AnimationUtils.loadAnimation(context, R.anim.loading)
         }
+        return binding.root
+    }
 
 
     override fun showProgress() {
-        loading?.visibility = View.VISIBLE
-        loading?.animation?.start()
+        binding.loading?.visibility = View.VISIBLE
+        binding.loading?.animation?.start()
     }
 
     override fun hideProgress() {
-        loading?.visibility = View.INVISIBLE
-        loading?.animation?.cancel()
+        binding.loading?.visibility = View.INVISIBLE
+        binding.loading?.animation?.cancel()
     }
 
     override fun onPause() {
