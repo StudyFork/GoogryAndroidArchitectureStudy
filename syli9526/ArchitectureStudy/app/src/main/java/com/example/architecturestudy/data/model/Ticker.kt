0cdbb816ac0ab5a2f968d632bcc18ff8d7ed979b
syli9526 <syli4800@gmail.com>
2019-08-26 22:17:10 +0900

    HW-2 Ticker 자료형 변경 및 속성 추가

--- syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/model/Ticker.kt
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/data/model/Ticker.kt
@@ -3,7 +3,8 @@ package com.example.architecturestudy.data.model
 data class Ticker(
     val currencyType: String,
     val coinName: String,
-    val presentPrice: Double,
-    val signedChangeRate: Double,
-    val transactionAmount: Double
+    val presentPrice: String,
+    val signedChangeRate: String,
+    val signedChangeRateColor: Int,
+    val transactionAmount: String
 )
