bb69ec33665513c66e7dceb1eda96c06886c44fa
Jake <junghum21@gmail.com>
2019-08-27 10:22:09 +0900

    HW-2 toast ext 개선
    1) 파라미터 개행
    2) Fragment ext 함수는 Activity ext 함수를 호출하도록 변경

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ext/ActivityExt.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ext/ActivityExt.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.widget.Toast
 import androidx.annotation.StringRes
 
-fun Activity.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
+fun Activity.toast(
+    text: String,
+    duration: Int = Toast.LENGTH_SHORT
+) {
     Toast.makeText(this, text, duration).show()
 }
 
-fun Activity.toast(@StringRes stringRes: Int, duration: Int = Toast.LENGTH_SHORT) {
+fun Activity.toast(
+    @StringRes stringRes: Int,
+    duration: Int = Toast.LENGTH_SHORT
+) {
     Toast.makeText(this, stringRes, duration).show()
 }
--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ext/FragmentExt.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ext/FragmentExt.kt
@@ -4,10 +4,16 @@ import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
-fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(activity, text, duration).show()
+fun Fragment.toast(
+    text: String,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    activity?.toast(text, duration)
 }
 
-fun Fragment.toast(@StringRes stringRes: Int, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(activity, stringRes, duration).show()
+fun Fragment.toast(
+    @StringRes stringRes: Int,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    activity?.toast(stringRes, duration)
 }
