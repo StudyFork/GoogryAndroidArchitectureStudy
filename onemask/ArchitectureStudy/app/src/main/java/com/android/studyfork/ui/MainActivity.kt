0ca44c69e5bd5bf20a81420e0983b6b0f6a1edcc
soo <onemask@naver.com>
2019-08-28 01:32:01 +0900

    UpbitService 변수 제거, UpbitRepository : object UpbitRepositoryImpl로 생성

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/MainActivity.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/MainActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.studyfork.R
-import com.android.studyfork.network.api.UpbitService
-import com.android.studyfork.repository.UpbitRepository
 import com.android.studyfork.repository.UpbitRepositoryImpl
 import com.android.studyfork.ui.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,14 +11,11 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private val upbitService by lazy{ UpbitService.getInstance().upbitApi }
     private val viewpagerAdapter by lazy { ViewPagerAdapter(supportFragmentManager) }
-    private lateinit var upbitRepository: UpbitRepository
-
+    private val upbitRepository = UpbitRepositoryImpl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        upbitRepository = UpbitRepositoryImpl(upbitService)
         initViewPager()
         loadData()
     }
