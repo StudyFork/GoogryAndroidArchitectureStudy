7ecd337343b41f9b727827513b6b6c5c743503de
preludezdev <preludez.dev@gmail.com>
2019-08-28 20:36:56 +0900

    HW-5 Util 클래스 수정

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/Util.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/Util.kt
@@ -6,10 +6,10 @@ object Util {
     const val THOUSAND = 1_000
     const val MILLION = 1_000_000
 
+    private val df = DecimalFormat("#,###")
+
     //천 단위로 끊어서 콤마 붙여주는 함수
     fun convertBigNumberToStdString(num: Int): String {
-        val df = DecimalFormat("#,###")
-
         return df.format(num)
     }
 
