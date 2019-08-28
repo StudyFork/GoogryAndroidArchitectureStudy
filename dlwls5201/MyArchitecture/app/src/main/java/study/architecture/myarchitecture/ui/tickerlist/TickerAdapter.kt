181e3e6aeb7af7afd74e2771b4fe9494ecd83719
leejinseong <dlwls5201@naver.com>
2019-08-28 23:27:33 +0900

    HW-5 BaseViewHolder 추가

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/tickerlist/TickerAdapter.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/tickerlist/TickerAdapter.kt
@@ -1,8 +1,12 @@
 package study.architecture.myarchitecture.ui.tickerlist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import study.architecture.myarchitecture.R
 import study.architecture.myarchitecture.ui.model.TickerItem
@@ -64,6 +68,33 @@ class TickerAdapter(
             tvSignedChangeRate.setTradeDiffColor(ticker.signedChangeRate)
             tvSignedChangeRate.text = ticker.tradeDiff
             tvAccTradePrice24h.text = ticker.tradeAmount
+        }
+    }
+
+    abstract class BaseViewHolder<B : ViewDataBinding>(
+        @LayoutRes layoutRes: Int,
+        parent: ViewGroup?,
+        private val bindingVariableId: Int?
+    ) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent?.context)
+            .inflate(layoutRes, parent, false)
+    ) {
+
+        val binding: B = DataBindingUtil.bind(itemView)!!
+
+        fun onBindViewHolder(item: Any?) {
+            try {
+                binding.run {
+                    bindingVariableId?.let {
+                        setVariable(it, item)
+                    }
+                    executePendingBindings()
+                }
+                itemView.visibility = View.VISIBLE
+            } catch (e: Exception) {
+                e.printStackTrace()
+                itemView.visibility = View.GONE
+            }
         }
     }
 
