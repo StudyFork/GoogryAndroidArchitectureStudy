4b1ec05a9b1faaa43ad0c1d1320a85693c77a070
leejinseong <dlwls5201@naver.com>
2019-08-28 22:42:10 +0900

    HW-5 BaseActivity 변경

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/BaseActivity.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/BaseActivity.kt
@@ -1,15 +1,17 @@
 package study.architecture.myarchitecture
 
-import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-@SuppressLint("Registered")
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+    protected lateinit var binding: B
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
 
         setOrientationToPortrait()
     }
