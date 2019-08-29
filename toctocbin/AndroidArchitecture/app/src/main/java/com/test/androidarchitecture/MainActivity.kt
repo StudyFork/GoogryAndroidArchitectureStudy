aea1e7c7f9a1c183809162d1e7e02a57a64aabdd
toctocbin <haanbin@naver.com>
2019-08-29 23:10:49 +0900

    disposables 관리 추가

--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/MainActivity.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.test.androidarchitecture
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
