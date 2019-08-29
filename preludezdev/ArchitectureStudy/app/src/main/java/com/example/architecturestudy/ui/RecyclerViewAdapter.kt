4886eb090c4799be79cd45f955b14186a3d4d018
preludezdev <preludez.dev@gmail.com>
2019-08-29 14:22:00 +0900

    HW-5 BaseViewHolder 클래스 생성하고 관련 코드 수정

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/RecyclerViewAdapter.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/RecyclerViewAdapter.kt
@@ -1,16 +1,14 @@
 package com.example.architecturestudy.ui
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.architecturestudy.R
+import com.example.architecturestudy.base.BaseViewHolder
 import com.example.architecturestudy.data.Coin
 import com.example.architecturestudy.databinding.ItemCoinBinding
 
 class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
     private val coins = mutableListOf<Coin>()
-    private lateinit var binding: ItemCoinBinding
 
     fun clearData() {
         coins.clear()
@@ -24,22 +22,17 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolde
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        binding = DataBindingUtil.inflate(inflater, R.layout.item_coin, parent, false)
-
-        return MyViewHolder(binding)
+        return MyViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bindView(coins[position])
-        binding.executePendingBindings()
     }
 
     override fun getItemCount(): Int = coins.size
 
-    //뷰홀더가 item_coin 레이아웃 바인딩 객체를 들고있게 한다
-    inner class MyViewHolder(private val binding: ItemCoinBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class MyViewHolder(parent: ViewGroup) :
+        BaseViewHolder<ItemCoinBinding>(R.layout.item_coin, parent) {
 
         fun bindView(item: Coin) {
             //각 레이아웃 내 뷰에 텍스트 및 색상 설정
@@ -49,6 +42,8 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolde
                 tvSignedChangedRate.text = item.signedChangeRate
                 tvAccTradePriceH.text = item.accTradePriceH
                 tvSignedChangedRate.setTextColor(item.coinColor)
+
+                executePendingBindings()
             }
         }
     }
