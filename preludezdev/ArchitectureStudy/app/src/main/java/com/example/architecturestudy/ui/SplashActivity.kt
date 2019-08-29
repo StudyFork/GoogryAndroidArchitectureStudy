931d53ebdf0d546b0972837cb140d9631f895b42
preludezdev <preludez.dev@gmail.com>
2019-08-29 12:44:20 +0900

    HW-5 SplashActivity 에도 데이터바인딩 적용

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/SplashActivity.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/SplashActivity.kt
@@ -2,14 +2,14 @@ package com.example.architecturestudy.ui
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.architecturestudy.R
+import com.example.architecturestudy.base.BaseActivity
+import com.example.architecturestudy.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
         // 메인엑티비티로 이동
         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
