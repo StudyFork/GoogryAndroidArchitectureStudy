93cd6122a257567364a1c49c76227eb7a7fa1773
Seonoh Eam <>
2019-09-01 17:49:52 +0900

    HW 3주차 - CoinMainPresenter 생성

---
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/CoinMainPresenter.kt
@@ -0,0 +1,51 @@
+package com.example.seonoh.seonohapp
+
+import android.util.Log
+import com.example.seonoh.seonohapp.contract.CoinMainContract
+import com.example.seonoh.seonohapp.model.Market
+import com.example.seonoh.seonohapp.repository.CoinRepositoryImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
+
+class CoinMainPresenter(
+    private val view: CoinMainContract.View
+) : CoinMainContract.Presenter {
+
+    init {
+        view.presenter = this
+    }
+
+    private val coinRepository = CoinRepositoryImpl()
+
+    override fun loadMarketData() {
+        coinRepository.sendMarket().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                view.setPager(it)
+            }, { e ->
+                if (e is HttpException) {
+                    Log.e("market", "Market Network failed !! ${e.message()}")
+                }
+            })
+    }
+
+    override fun classifyMarketData(marketData: List<Market>): ArrayList<String> {
+        val conMarketNameList = marketData.map {
+            it.market.substringBefore("-")
+        }.distinct()
+
+        val marketDataList = ArrayList<String>()
+
+        conMarketNameList.forEach { title ->
+
+            marketDataList += (marketData.filter {
+                it.market.substringBefore("-") == title
+            }.joinToString(",") {
+                it.market
+            })
+        }
+
+        return marketDataList
+    }
+}
