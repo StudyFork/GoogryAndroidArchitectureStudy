793035a09c8c5940282b18bbd8cf7f2273c7740c
ganghee <idiosauri@naver.com>
2019-09-03 01:51:51 +0900

    [MainActivity] private 키워드 추가

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.mystudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
