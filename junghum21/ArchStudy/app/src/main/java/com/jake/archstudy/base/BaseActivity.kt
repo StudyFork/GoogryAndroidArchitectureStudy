14155be0798f223a6229cf327d5e30fd05f2bc56
Jake <junghum21@gmail.com>
2019-09-03 10:34:59 +0900

    BaseActivity 생성

---
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.jake.archstudy.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes val layoutRes: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+    }
+
+}
