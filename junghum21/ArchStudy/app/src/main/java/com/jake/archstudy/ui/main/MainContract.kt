b6df8d9c5c6afb155d6b41dd173be035b9d9512e
Jake <junghum21@gmail.com>
2019-09-03 10:43:02 +0900

    Base V,P 적용

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/main/MainContract.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/ui/main/MainContract.kt
@@ -1,12 +1,18 @@
 package com.jake.archstudy.ui.main
 
+import com.jake.archstudy.base.BasePresenter
+import com.jake.archstudy.base.BaseView
+import com.jake.archstudy.data.model.Market
+
 interface MainContract {
 
-    interface View {
-        
+    interface View : BaseView<Presenter> {
+
+        fun setViewPager(markets: List<Market>)
+
     }
 
-    interface Presenter {
+    interface Presenter : BasePresenter<View> {
 
     }
 
