281db700bd17f8f6cdc0494d4ad98e7f6ef13d64
Jake <junghum21@gmail.com>
2019-09-03 10:51:03 +0900

    ResourceProvider 생성

---
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/util/ResourceProvider.kt
@@ -0,0 +1,16 @@
+package com.jake.archstudy.util
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+interface ResourceProvider {
+
+    fun getString(@StringRes resId: Int): String
+
+}
+
+class ResourceProviderImpl(private val context: Context) : ResourceProvider {
+
+    override fun getString(resId: Int): String = context.getString(resId)
+
+}
