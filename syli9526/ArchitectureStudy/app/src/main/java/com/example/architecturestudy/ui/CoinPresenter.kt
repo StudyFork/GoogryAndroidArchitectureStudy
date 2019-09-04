2642c268c35cac36b8d080838613e5f21a25daac
syli9526 <syli4800@gmail.com>
2019-09-04 15:44:07 +0900

    HW-3 add CoinPresenter

---
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/CoinPresenter.kt
@@ -0,0 +1,23 @@
+package com.example.architecturestudy.ui
+
+import com.example.architecturestudy.data.model.Ticker
+import com.example.architecturestudy.data.repository.CoinRepositoryImpl
+import com.example.architecturestudy.data.source.UpbitListener
+
+class CoinPresenter(private val view: CoinContract.View) : CoinContract.Presenter {
+
+
+    override fun getTickerList(marketName: String) {
+        CoinRepositoryImpl.getInstance().getTickerInfo(marketName, object : UpbitListener<Ticker> {
+
+            override fun onResponse(dataList: List<Ticker>) {
+                view.setData(dataList)
+            }
+
+            override fun onFailure(str: String) {
+                view.showMessage(str)
+            }
+
+        })
+    }
+}
