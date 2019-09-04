e8d145432491cdaae3d20f5a17f222d15d928eed
ganghee <idiosauri@naver.com>
2019-09-04 23:38:08 +0900

    [UpbitHolder] class에 관한 컨벤션

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/UpbitHolder.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/UpbitHolder.kt
@@ -4,8 +4,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mystudy.data.FormatTickers
 import com.example.mystudy.databinding.RvItemListBinding
 
-class UpbitHolder(private val binding: RvItemListBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class UpbitHolder(
+    private val binding: RvItemListBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
     private var rvBinding: RvItemListBinding = binding
 
     fun bind(ticker: FormatTickers) {
