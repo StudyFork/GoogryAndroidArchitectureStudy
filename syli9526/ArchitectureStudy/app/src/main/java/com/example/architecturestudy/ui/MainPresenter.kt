dd8c07c074f32a5a0fc1c8a644efabb722b99c63
syli9526 <syli4800@gmail.com>
2019-09-04 15:20:43 +0900

    HW-3 add MainPresenter

---
+++ syli9526/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MainPresenter.kt
@@ -0,0 +1,23 @@
+package com.example.architecturestudy.ui
+
+
+import com.example.architecturestudy.data.repository.CoinRepositoryImpl
+import com.example.architecturestudy.data.source.UpbitListener
+import com.example.architecturestudy.network.model.MarketResponse
+
+class MainPresenter(private val view: MainContract.View): MainContract.Presenter {
+
+    override fun getMarketList() {
+
+        CoinRepositoryImpl.getInstance().getMarketInfo(object : UpbitListener<MarketResponse> {
+            override fun onResponse(dataList: List<MarketResponse>) {
+                view.setData(dataList)
+            }
+
+            override fun onFailure(str: String) {
+                view.showMessage("Call Failure : $str")
+            }
+
+        })
+    }
+}
