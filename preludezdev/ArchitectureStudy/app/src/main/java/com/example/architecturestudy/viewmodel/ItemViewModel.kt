a856a59c3b6f61d54a99682fcb50a114a787de62
preludezdev <preludez.dev@gmail.com>
2019-09-10 18:20:48 +0900

    HW-6 ItemViewModel 클래스 추가

---
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/viewmodel/ItemViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.architecturestudy.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.architecturestudy.data.Coin
+
+class ItemViewModel(var coin: Coin) : ViewModel() {
+    
+
+}
