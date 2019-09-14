f37828aa731fbf13edee8130a59aa2aa64c205ba
leejinseong <dlwls5201@naver.com>
2019-09-14 16:59:39 +0900

    HW-6 드로어, 슬라이드 레이아웃 제거

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainActivity.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainActivity.kt
@@ -1,10 +1,6 @@
 package study.architecture.myarchitecture.ui.main
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import androidx.drawerlayout.widget.DrawerLayout
-import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import study.architecture.myarchitecture.R
 import study.architecture.myarchitecture.base.BaseActivity
 import study.architecture.myarchitecture.data.Injection
@@ -25,9 +21,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 binding.mainModel = this
             }
 
-        initToolbar()
-        initDrawer()
-
         mainViewModel.loadData()
 
     }
@@ -35,71 +28,5 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onDestroy() {
         mainViewModel.detachView()
         super.onDestroy()
-    }
-
-    private fun initToolbar() {
-
-        with(binding) {
-            llToolbarTitle.setOnClickListener {
-
-                if (suplRoot.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
-                    suplRoot.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
-                } else {
-                    suplRoot.panelState = SlidingUpPanelLayout.PanelState.EXPANDED
-                }
-            }
-
-            llToolbarTitle.setOnClickListener {
-
-                if (suplRoot.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
-                    suplRoot.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
-                } else {
-                    suplRoot.panelState = SlidingUpPanelLayout.PanelState.EXPANDED
-                }
-            }
-
-            suplRoot.addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
-                override fun onPanelSlide(panel: View?, slideOffset: Float) {
-                    ivToolbarArrow.rotation = slideOffset * 180
-                }
-
-                override fun onPanelStateChanged(
-                    panel: View?,
-                    previousState: SlidingUpPanelLayout.PanelState?,
-                    newState: SlidingUpPanelLayout.PanelState?
-                ) {
-
-                }
-
-            })
-
-            ivToolbarMenu.setOnClickListener {
-                dlRoot.openDrawer(flEndSideInDrawer)
-            }
-        }
-    }
-
-    private fun initDrawer() {
-
-        binding.dlRoot.run {
-            setScrimColor(Color.TRANSPARENT)
-            addDrawerListener(object : DrawerLayout.DrawerListener {
-                override fun onDrawerStateChanged(newState: Int) {
-                }
-
-                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-                    if (drawerView.id == R.id.flEndSideInDrawer) {
-                        binding.llMainInDrawer.translationX =
-                            -binding.llMainInDrawer.width * slideOffset
-                    }
-                }
-
-                override fun onDrawerClosed(drawerView: View) {
-                }
-
-                override fun onDrawerOpened(drawerView: View) {
-                }
-            })
-        }
     }
 }
