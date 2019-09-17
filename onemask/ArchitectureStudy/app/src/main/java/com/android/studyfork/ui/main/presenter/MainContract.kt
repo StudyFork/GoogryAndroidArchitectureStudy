d313925a76dd308ac7f0333cb5edcd5530b7f48e
onemask <onemask@naver.com>
2019-09-17 16:42:43 +0900

    HW-3 > MainContract > setViewPagerData() 받는 파라미터 변경

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/presenter/MainContract.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/presenter/MainContract.kt
@@ -1,13 +1,16 @@
 package com.android.studyfork.ui.main.presenter
 
+import com.android.studyfork.network.model.MarketResponse
+
 
 interface MainContract {
     interface View {
-        fun setViewPagerData(titles: List<String>, items: List<String>)
+        fun setViewPagerData(marketData: Map<String, List<MarketResponse>>)
     }
 
     interface Presenter {
         fun loadData()
+        fun clearDisposable()
     }
 
 }
