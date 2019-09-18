697af336ec3f61020e1024087e05c548036ea492
wswon <dnjsdntjr1@gmail.com>
2019-09-18 22:51:32 +0900

    HW-6 BindingAdapter 추가

---
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/ext/RecyclerViewExt.kt
@@ -0,0 +1,17 @@
+package com.architecture.study.ext
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.architecture.study.data.model.Ticker
+import com.architecture.study.view.coin.adapter.CoinListAdapter
+
+@BindingAdapter(value = ["replaceAll"])
+fun RecyclerView.replaceAll(list: List<Ticker>?) {
+
+    (this.adapter as? CoinListAdapter)?.let {
+        list?.let { tickerList ->
+            it.setData(tickerList)
+        }
+        it.notifyDataSetChanged()
+    }
+}
