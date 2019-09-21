0bbaf6f021fc7da9513f50940471dc53e1709db9
Zojae031 <edgar3@naver.com>
2019-09-21 19:09:33 +0900

    Remove ItemTickerBinding on Static Field

--- zojae031/Project/app/src/main/java/study/architecture/ui/coinjob/CoinDataAdapter.kt
+++ zojae031/Project/app/src/main/java/study/architecture/ui/coinjob/CoinDataAdapter.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import study.architecture.data.entity.ProcessingTicker
-import study.architecture.databinding.ListItemBinding
+import study.architecture.databinding.ItemTickerBinding
 
 /**
  * RecyclerView에 아이템을 뿌려주는 Adpater
@@ -13,22 +13,21 @@ class CoinDataAdapter : RecyclerView.Adapter<CoinDataAdapter.Holder>() {
 
     private val lists: MutableList<ProcessingTicker> = mutableListOf()
 
-    private lateinit var binding: ListItemBinding
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        binding = ListItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
+        return Holder(
+            ItemTickerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         )
-        return Holder(binding)
+
     }
 
     override fun getItemCount(): Int = lists.size
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(position)
-        binding.executePendingBindings()
     }
 
     fun updateLists(list: List<ProcessingTicker>) {
@@ -38,10 +37,14 @@ class CoinDataAdapter : RecyclerView.Adapter<CoinDataAdapter.Holder>() {
     }
 
 
-    inner class Holder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class Holder(private val binding: ItemTickerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(position: Int) {
-            binding.pTicker = lists[position]
+            binding.apply {
+                pTicker = lists[position]
+                executePendingBindings()
+            }
         }
 
     }
