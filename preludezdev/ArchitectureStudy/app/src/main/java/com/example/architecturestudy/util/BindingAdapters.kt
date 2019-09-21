6a68a8545431e559c990aba563c3c1188f4629ca
preludezdev <preludez.dev@gmail.com>
2019-09-21 12:17:56 +0900

    HW-6 BindingAdapter 변경

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
@@ -6,14 +6,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.architecturestudy.data.Coin
 import com.example.architecturestudy.ui.market.RecyclerViewAdapter
 
-object BindingAdapters {
+@BindingAdapter("items")
+fun RecyclerView.replaceAll(items: ObservableField<List<Coin>>) {
+    val adapter = this.adapter
 
-    @JvmStatic
-    @BindingAdapter("items")
-    fun RecyclerView.replaceAll(items: ObservableField<List<Coin>>) {
-        val adapter = this.adapter
-
-        if (adapter is RecyclerViewAdapter)
-            adapter.setData(items.get())
+    if (adapter is RecyclerViewAdapter) {
+        adapter.setData(items.get())
     }
 }
