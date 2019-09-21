7809965e0a53475e7c3dfa6a3fcb1279b6161d25
leejinseong <dlwls5201@naver.com>
2019-09-21 10:51:12 +0900

    HW-6 class convention 적용

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/base/BaseViewHolder.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/base/BaseViewHolder.kt
@@ -5,11 +5,12 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseViewHolder(@LayoutRes layoutRes: Int, parent: ViewGroup?) :
-    RecyclerView.ViewHolder(
-        LayoutInflater.from(parent?.context)
-            .inflate(layoutRes, parent, false)
-    ) {
+abstract class BaseViewHolder(
+    @LayoutRes layoutRes: Int, parent: ViewGroup?
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent?.context)
+        .inflate(layoutRes, parent, false)
+) {
 
     abstract fun onBindView(item: Any?)
 }
