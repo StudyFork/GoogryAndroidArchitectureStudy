dd2b6abf959ff8616cc9727b5ccd0d62c7ad8595
ganghee <idiosauri@naver.com>
2019-09-22 10:38:16 +0900

    [ViewPagerAdapter] FragmentPagerAdapter 상속

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/ViewPagerAdapter.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/adapter/ViewPagerAdapter.kt
@@ -2,11 +2,12 @@ package com.example.mystudy.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.mystudy.ui.UpbitFragment
 
 class ViewPagerAdapter(fm: FragmentManager) :
-    FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     private val marketList = listOf("KRW", "BTC", "ETH", "USDT")
 
