a2a1fafa32ab575740c4a5d5b40f0a2ff32e618a
Zojae031 <edgar3@naver.com>
2019-09-22 17:18:40 +0900

    Main ViewModel Adapater Seperated

--- zojae031/Project/app/src/main/java/study/architecture/ui/mainjob/MainActivity.kt
+++ zojae031/Project/app/src/main/java/study/architecture/ui/mainjob/MainActivity.kt
@@ -18,9 +18,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        with(MainViewModel(MainPageAdapter(supportFragmentManager))) {
-            binding.viewModel = this
-            initView()
+
+        with(binding) {
+            pager.adapter = MainPageAdapter(supportFragmentManager)
+            viewModel = MainViewModel()
         }
 
         supportActionBar?.elevation = 0.0f
--- zojae031/Project/app/src/main/java/study/architecture/ui/mainjob/MainViewModel.kt
+++ zojae031/Project/app/src/main/java/study/architecture/ui/mainjob/MainViewModel.kt
@@ -1,15 +1,7 @@
 package study.architecture.ui.mainjob
 
-import androidx.databinding.ObservableField
 import study.architecture.ui.coinjob.CoinFragment
 
-class MainViewModel(
-    private val adapter: MainPageAdapter
-) {
-    val mainAdapter = ObservableField<MainPageAdapter>()
+class MainViewModel {
     val pageLimit: Int = CoinFragment.FragIndex.values().size
-
-    fun initView() {
-        mainAdapter.set(adapter)
-    }
 }
