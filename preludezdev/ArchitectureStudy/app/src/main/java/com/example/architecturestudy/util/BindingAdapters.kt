4d81f7f64b0e5a4c2049c049612040a9596af8bf
preludezdev <preludez.dev@gmail.com>
2019-09-22 16:10:41 +0900

    HW-7 BindingAdapters 코드 리팩토링

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
@@ -7,10 +7,6 @@ import com.example.architecturestudy.data.Coin
 import com.example.architecturestudy.ui.market.RecyclerViewAdapter
 
 @BindingAdapter("items")
-fun RecyclerView.replaceAll(items: ObservableField<List<Coin>>) {
-    val adapter = this.adapter
+fun RecyclerView.replaceAll(items: ObservableField<List<Coin>>) =
+    (adapter as? RecyclerViewAdapter)?.setData(items.get())
 
-    if (adapter is RecyclerViewAdapter) {
-        adapter.setData(items.get())
-    }
-}
