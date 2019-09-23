f8699feec49b152c4725a566c2ddb1380a5e829d
ganghee <idiosauri@naver.com>
2019-09-23 16:08:09 +0900

    [MainActivity] view에서 adapter 초기화

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/MainActivity.kt
@@ -6,22 +6,19 @@ import androidx.databinding.DataBindingUtil
 import com.example.mystudy.R
 import com.example.mystudy.adapter.ViewPagerAdapter
 import com.example.mystudy.databinding.ActivityMainBinding
-import com.example.mystudy.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewPagerAdapter by lazy { ViewPagerAdapter(supportFragmentManager) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        MainViewModel(ViewPagerAdapter(supportFragmentManager)).run {
-            binding.vm = this
-            initView()
-        }
-        binding.tabLayout.run {
-            setupWithViewPager(binding.viewPager)
+        binding.run {
+            viewPager.adapter = viewPagerAdapter
+            tabLayout.setupWithViewPager(binding.viewPager)
         }
     }
 }
