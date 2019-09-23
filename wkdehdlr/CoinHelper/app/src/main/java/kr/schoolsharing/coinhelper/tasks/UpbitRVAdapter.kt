7d439e8e01d1e555654a61ae577d814aae861625
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-09-23 20:38:12 +0900

    binding멤버제거 & Holder 클래스 변수명 수정

--- wkdehdlr/CoinHelper/app/src/main/java/kr/schoolsharing/coinhelper/tasks/UpbitRVAdapter.kt
+++ wkdehdlr/CoinHelper/app/src/main/java/kr/schoolsharing/coinhelper/tasks/UpbitRVAdapter.kt
@@ -9,23 +9,21 @@ import kr.schoolsharing.coinhelper.model.UpbitItem
 class UpbitRVAdapter : RecyclerView.Adapter<UpbitRVAdapter.Holder>() {
 
     private val itemList: MutableList<UpbitItem> = ArrayList()
-    private lateinit var binding: MainRvItemBinding
 
     override fun getItemCount(): Int = itemList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        binding = MainRvItemBinding.inflate(
+        return Holder(
+            MainRvItemBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
+            )
         )
-
-        return Holder(binding)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(itemList[position])
-        binding.executePendingBindings()
     }
 
     fun setTickerList(tickerList: List<UpbitItem>?) {
@@ -36,11 +34,13 @@ class UpbitRVAdapter : RecyclerView.Adapter<UpbitRVAdapter.Holder>() {
         }
     }
 
-    inner class Holder(itemView: MainRvItemBinding) : RecyclerView.ViewHolder(itemView.root) {
+    inner class Holder(private val binding: MainRvItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(upbitItem: UpbitItem) {
             with(binding) {
                 item = upbitItem
+                executePendingBindings()
             }
         }
     }
