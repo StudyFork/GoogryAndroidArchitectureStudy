46e325a1f35b79ef0116a745d3bd3c6906b3c0ac
preludezdev <preludez.dev@gmail.com>
2019-09-24 10:48:26 +0900

    HW-7 BindingAdapters 의 replaceAll 함수 매개변수 타입 변경

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/util/BindingAdapters.kt
@@ -1,12 +1,17 @@
 package com.example.architecturestudy.util
 
 import androidx.databinding.BindingAdapter
-import androidx.databinding.ObservableField
 import androidx.recyclerview.widget.RecyclerView
 import com.example.architecturestudy.data.Coin
 import com.example.architecturestudy.ui.market.RecyclerViewAdapter
 
 @BindingAdapter("items")
-fun RecyclerView.replaceAll(items: ObservableField<List<Coin>>) =
-    (adapter as? RecyclerViewAdapter)?.setData(items.get())
+fun RecyclerView.replaceAll(items: List<Coin>?) {
+    (adapter as? RecyclerViewAdapter)?.run {
+        if (items != null) {
+            setData(items)
+        }
+    }
+
+}
 
