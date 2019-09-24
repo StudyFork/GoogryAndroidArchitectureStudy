95339adb6623c6849fd248afc4c0676ac65c2bc6
ganghee <idiosauri@naver.com>
2019-09-24 23:21:12 +0900

    [BaseFragment] BaseFragment 추가

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseFragment.kt
@@ -0,0 +1,23 @@
+package com.example.mystudy.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
