749284f18f94923432205f7bac1d90ce77798454
ganghee <idiosauri@naver.com>
2019-09-24 23:21:56 +0900

    [BaseRecyclerViewAdapter] BaseRecyclerViewAdapter 추가

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseRecyclerViewAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.mystudy.base
+
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseRecyclerViewAdapter<ITEM : Any, B : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int,
+    private val bindingVariableId: Int? = null
+) : RecyclerView.Adapter<BaseViewHolder<B>>() {
+
+    private val items = mutableListOf<ITEM>()
+
+    fun replaceAll(items: List<ITEM>?) {
+        items?.let {
+            this.items.run {
+                clear()
+                addAll(it)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        object : BaseViewHolder<B>(
+            layoutRes = layoutRes,
+            parent = parent,
+            bindingVariableId = bindingVariableId
+        ) {}
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<B>, position: Int) {
+        holder.onBindViewHolder(items[position])
+    }
+}
