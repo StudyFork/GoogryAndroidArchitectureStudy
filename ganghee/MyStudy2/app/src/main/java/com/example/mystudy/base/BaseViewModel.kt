46286734badc27f428a5a7e28c726d1a1195b1c3
ganghee <idiosauri@naver.com>
2019-09-24 23:23:20 +0900

    [BaseViewModel] BaseViewModel추가

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.mystudy.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+        super.onCleared()
+    }
+}
