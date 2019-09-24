c86f69f1dcb41edc82a1c3ebf8c4b92ffa5438f8
ganghee <idiosauri@naver.com>
2019-09-24 23:20:23 +0900

    [BaseActivity] BaseActivity 추가

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mystudy.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
