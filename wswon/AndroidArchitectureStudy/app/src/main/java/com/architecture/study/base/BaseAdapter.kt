8562286c5654ce146940009092223f860ad243a8
wswon <dnjsdntjr1@gmail.com>
2019-09-27 12:30:43 +0900

    HW-7 null check 변경

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/base/BaseAdapter.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/base/BaseAdapter.kt
@@ -25,10 +25,10 @@ abstract class BaseAdapter<ITEM : Any, B : ViewDataBinding>(
         holder.bind(items[position])
 
     fun replaceAll(items: List<ITEM>?) {
-        items?.let {
+        if(items != null){
             this.items.run {
                 clear()
-                addAll(it)
+                addAll(items)
             }
             notifyDataSetChanged()
         }
