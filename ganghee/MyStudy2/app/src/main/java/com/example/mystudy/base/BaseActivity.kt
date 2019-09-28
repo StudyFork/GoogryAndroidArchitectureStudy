2fbedcde8c4c648f5dd3dfc586c4ac13f3599de2
ganghee <idiosauri@naver.com>
2019-09-28 11:10:31 +0900

    [BaseActivity] @LayoutRes 추가

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseActivity.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/base/BaseActivity.kt
@@ -1,11 +1,13 @@
 package com.example.mystudy.base
 
 import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<B : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
     protected lateinit var binding: B
 
     override fun onCreate(savedInstanceState: Bundle?) {
