83c69e3ba61883e0778e550bc086815e3165d549
JongsunKim <kjs6030@naver.com>
2019-09-30 00:03:48 +0900

    HW-5 CoinContract.Prenter/View BasePresenter/View 상속하도록 변경

--- jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/coin/CoinContract.kt
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/coin/CoinContract.kt
@@ -1,15 +1,15 @@
 package com.jskim5923.architecturestudy.coin
 
+import com.jskim5923.architecturestudy.base.BasePresenter
+import com.jskim5923.architecturestudy.base.BaseView
 import com.jskim5923.architecturestudy.model.Ticker
 
 interface CoinContract {
-    interface View {
+    interface View : BaseView<Presenter> {
         fun updateRecyclerView(tickerList: List<Ticker>)
     }
 
-    interface Presenter {
+    interface Presenter : BasePresenter<View> {
         fun getTickerList(market: String?)
-
-        fun clearCompositeDisposable()
     }
 }
