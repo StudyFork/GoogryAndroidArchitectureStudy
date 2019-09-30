8d9a293129820a0cc8b22651c227c7cb5f6f233f
toctocbin <haanbin@naver.com>
2019-09-30 10:21:04 +0900

    HW-5 BaseActivity, BaseContract 생성 및 Market 적용

---
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.test.androidarchitecture.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity(
+    @LayoutRes
+    private val layoutRes: Int
+) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutRes)
+        start()
+    }
+
+    abstract fun start()
+
+}
--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/market/MarketActivity.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/market/MarketActivity.kt
@@ -1,35 +1,35 @@
 package com.test.androidarchitecture.ui.market
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.test.androidarchitecture.R
 import com.test.androidarchitecture.adpter.ViewPagerAdapter
+import com.test.androidarchitecture.base.BaseActivity
 import com.test.androidarchitecture.data.MarketTitle
 import kotlinx.android.synthetic.main.activity_market.*
 
-class MarketActivity :
-    AppCompatActivity(),
-    MarketContract.View {
+class MarketActivity : BaseActivity(R.layout.activity_market), MarketContract.View {
 
     private lateinit var viewPagerAdapter: ViewPagerAdapter
-    private val presenter by lazy { MarketPresenter(this) }
+    override val presenter by lazy { MarketPresenter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_market)
+        presenter.getMarketAll()
+    }
+
+    override fun start() {
         main_tabLayout.setupWithViewPager(main_viewPager)
         viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         with(main_viewPager) {
             offscreenPageLimit = 3
             adapter = viewPagerAdapter
         }
-        presenter.getMarketAll()
     }
 
     override fun onDestroy() {
-        presenter.disposablesClear()
         super.onDestroy()
+        presenter.clearDisposable()
     }
 
     override fun setViewpagerData(list: List<MarketTitle>) {
