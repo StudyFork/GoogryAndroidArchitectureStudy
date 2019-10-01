65865abac5d657de60fddbca353ba7ad0459be27
onemask <onemask@naver.com>
2019-10-01 20:32:09 +0900

    HW-4 > getTicker() 로직 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerPresenter.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerPresenter.kt
@@ -1,43 +1,28 @@
 package com.android.studyfork.ui.tickerlist.presenter
 
-import com.android.studyfork.network.model.Ticker
+import com.android.studyfork.network.model.TickerResponse
 import com.android.studyfork.repository.UpbitRepositoryImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.rxkotlin.addTo
 import timber.log.Timber
 
 /**
  * created by onemask
  */
 class TickerPresenter(
-    private val view: TickerContract.View
+    override val view: TickerContract.View
 ) : TickerContract.Presenter {
 
-    private val disposables = CompositeDisposable()
+    override val disposables = CompositeDisposable()
 
     override fun getTicker(market: String) {
 
-        val tickerList: ArrayList<Ticker> = arrayListOf()
-
-        disposables += UpbitRepositoryImpl.getTickers(market)
-            .subscribe({
-                Timber.d("getTicker success")
-                it.forEachIndexed { index, tickerResponse ->
-                    tickerList.add(
-                        index,
-                        Ticker(
-                            tickerResponse.market,
-                            tickerResponse.tradePrice,
-                            tickerResponse.signedChangeRate,
-                            tickerResponse.accTradePrice24h
-                        )
-                    )
-                }
-                view.setData(tickerList)
-            }, {
-                Timber.e(it)
-            })
-
+        UpbitRepositoryImpl.getTickers(market)
+            .map { it.map(TickerResponse::toTicker) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(view::setData, Timber::e)
+            .addTo(disposables)
     }
 
     override fun clearDisposable() {
