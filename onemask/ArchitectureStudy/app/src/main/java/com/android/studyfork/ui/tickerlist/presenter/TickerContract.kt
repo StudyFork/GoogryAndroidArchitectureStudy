5af186fc013066b309c78a10f426de65688841dd
onemask <onemask@naver.com>
2019-10-01 20:31:01 +0900

    HW-4 > TickerContract BaseView,BasePresenter 상속 받도록 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerContract.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerContract.kt
@@ -1,17 +1,18 @@
 package com.android.studyfork.ui.tickerlist.presenter
 
+import com.android.studyfork.base.BasePresenter
+import com.android.studyfork.base.BaseView
 import com.android.studyfork.network.model.Ticker
 
 /**
  * created by onemask
  */
 interface TickerContract {
-    interface View {
+    interface View : BaseView<Presenter>{
         fun setData(ticker: List<Ticker>)
     }
 
-    interface Presenter {
+    interface Presenter : BasePresenter<View>{
         fun getTicker(market: String)
-        fun clearDisposable()
     }
 }
