e6d449d767787df645e9b964cfe6f84942560b72
onemask <onemask@naver.com>
2019-10-02 14:05:16 +0900

    HW-4 >MainActivity BaseActivity 상속받도록 변경

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/MainActivity.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.android.studyfork.ui.main
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.android.studyfork.R
-import com.android.studyfork.network.model.MarketResponse
+import com.android.studyfork.base.BaseActivity
 import com.android.studyfork.ui.main.adapter.ViewPagerAdapter
 import com.android.studyfork.ui.main.presenter.MainContract
 import com.android.studyfork.ui.main.presenter.MainPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainContract.View {
-
+class MainActivity : BaseActivity(R.layout.activity_main), MainContract.View {
 
     private val viewPagerAdapter by lazy {
         ViewPagerAdapter(
@@ -20,38 +17,19 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     override val presenter by lazy { MainPresenter(this) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onStart() {
+        super.onStart()
         initViewPager()
-        loadData()
+        presenter.loadData()
     }
-/*
-
-    override fun setViewPagerData(marketData: Map<String, List<MarketResponse>>) {
-        with(viewPagerAdapter) {
-            setTitles(marketData.keys.toList())
-            setData( marketData.values.map { responseList ->
-                responseList.joinToString { response ->
-                    response.market
-                }
-            })
-        }
-    }
-
- */
 
     override fun setViewPagerData(marketData: Pair<List<String>, List<String>>) {
-        with(viewPagerAdapter){
-
+        with(viewPagerAdapter) {
+            setTitles(marketData.first.toList())
+            setData( marketData.second)
+            }
         }
-    }
 
-
-    override fun onDestroy() {
-        presenter.clearDisposable()
-        super.onDestroy()
-    }
 
     private fun initViewPager() {
         with(pager_content) {
@@ -59,10 +37,6 @@ class MainActivity : AppCompatActivity(), MainContract.View {
             layout_tab.setupWithViewPager(this)
             offscreenPageLimit = 3
         }
-    }
-
-    private fun loadData() {
-        presenter.loadData()
     }
 
 }
