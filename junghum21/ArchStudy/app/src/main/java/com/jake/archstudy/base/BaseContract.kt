f3b2a04fdfb0abe724ceaecc98356a5d6bcb46ac
Jake <junghum21@gmail.com>
2019-10-02 20:37:02 +0900

    BaseView, BasePresenter를 BaseContract 하위로 이동

---
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/base/BaseContract.kt
@@ -0,0 +1,35 @@
+package com.jake.archstudy.base
+
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+interface BaseContract {
+
+
+    interface View<P> {
+
+        val presenter: P
+
+        fun showToast(
+            text: String,
+            duration: Int = Toast.LENGTH_SHORT
+        )
+
+        fun showToast(
+            @StringRes
+            stringResId: Int,
+            duration: Int = Toast.LENGTH_SHORT
+        )
+
+    }
+
+    interface Presenter<V> {
+
+        val view: V
+
+        fun onCreate()
+
+    }
+
+
+}
