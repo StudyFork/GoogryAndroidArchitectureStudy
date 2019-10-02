f71f3b99fbff279af65a0a29d198c2797747bbe6
onemask <onemask@naver.com>
2019-10-02 14:06:06 +0900

    HW-4 > BaseFragment 받도록 변경

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/TickerListFragment.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/TickerListFragment.kt
@@ -2,25 +2,19 @@ package com.android.studyfork.ui
 
 
 import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
+import android.view.View
 import com.android.studyfork.R
+import com.android.studyfork.base.BaseFragment
 import com.android.studyfork.network.model.Ticker
 import com.android.studyfork.ui.adapter.CoinItemAdapter
 import com.android.studyfork.ui.tickerlist.presenter.TickerContract
 import com.android.studyfork.ui.tickerlist.presenter.TickerPresenter
-import com.android.studyfork.util.inflate
 import kotlinx.android.synthetic.main.fragment_ticker_list.*
 
-class TickerListFragment : Fragment(), TickerContract.View {
+class TickerListFragment : BaseFragment(R.layout.fragment_ticker_list), TickerContract.View {
 
     private lateinit var coinItemAdapter: CoinItemAdapter
     override val presenter by lazy { TickerPresenter(this) }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = container?.inflate(R.layout.fragment_ticker_list)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
