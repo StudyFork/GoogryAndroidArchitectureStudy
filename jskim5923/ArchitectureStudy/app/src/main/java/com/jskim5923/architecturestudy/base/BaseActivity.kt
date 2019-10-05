8376cc1ecf91c20d6a96292443354b9e0a3da2fb
jskim <kjs6030@naver.com>
2019-10-05 16:14:49 +0900

    HW-4 BaseActivity 추가 및 상속구조 변경

---
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.jskim5923.architecturestudy.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(), BaseContract.View {
+    @get:LayoutRes
+    abstract val layoutRes: Int
+
+    abstract val presenter: BaseContract.Presenter
+
+    abstract override fun initView()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutRes)
+        initView()
+    }
+
+    override fun onDestroy() {
+        presenter.clearCompositeDisposable()
+        super.onDestroy()
+    }
+}
--- jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/main/MainActivity.kt
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/main/MainActivity.kt
@@ -1,38 +1,30 @@
 package com.jskim5923.architecturestudy.ui
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.jskim5923.architecturestudy.R
 import com.jskim5923.architecturestudy.adapter.ViewPagerAdapter
+import com.jskim5923.architecturestudy.base.BaseActivity
 import com.jskim5923.architecturestudy.main.MainContract
 import com.jskim5923.architecturestudy.main.MainPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : BaseActivity(), MainContract.View {
+    override val layoutRes = R.layout.activity_main
+
     private val viewpagerAdapter by lazy {
         ViewPagerAdapter(supportFragmentManager)
     }
 
-    private val presenter = MainPresenter(this)
+    override val presenter = MainPresenter(this)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+    override fun initView() {
         with(viewPager) {
             tabLayout.setupWithViewPager(this)
             adapter = viewpagerAdapter
         }
-
         presenter.loadMarketList()
     }
 
     override fun updateViewpager(marketList: List<String>) {
         viewpagerAdapter.updateItem(marketList)
-    }
-
-    override fun onDestroy() {
-        presenter.clearCompositeDisposable()
-        super.onDestroy()
     }
 }
