15995aaa39de73d82a2c7c56ee584d926a80a084
soo <onemask@naver.com>
2019-10-05 18:10:13 +0900

    [HW-4] MainContract,TickerContract BaseContract 상속바도록 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/presenter/MainContract.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/presenter/MainContract.kt
@@ -1,15 +1,14 @@
 package com.android.studyfork.ui.main.presenter
 
-import com.android.studyfork.base.BasePresenter
-import com.android.studyfork.base.BaseView
+import com.android.studyfork.base.BaseContract
 
 
 interface MainContract {
-    interface View : BaseView<Presenter>{
+    interface View : BaseContract.View<Presenter> {
         fun setViewPagerData(marketData: Pair<List<String>, List<String>>)
     }
 
-    interface Presenter : BasePresenter<View> {
+    interface Presenter : BaseContract.Presenter {
         fun loadData()
     }
 
--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerContract.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/presenter/TickerContract.kt
@@ -1,18 +1,17 @@
 package com.android.studyfork.ui.tickerlist.presenter
 
-import com.android.studyfork.base.BasePresenter
-import com.android.studyfork.base.BaseView
+import com.android.studyfork.base.BaseContract
 import com.android.studyfork.network.model.Ticker
 
 /**
  * created by onemask
  */
 interface TickerContract {
-    interface View : BaseView<Presenter>{
+    interface View : BaseContract.View<Presenter> {
         fun setData(ticker: List<Ticker>)
     }
 
-    interface Presenter : BasePresenter<View>{
+    interface Presenter : BaseContract.Presenter {
         fun getTicker(market: String)
     }
 }
