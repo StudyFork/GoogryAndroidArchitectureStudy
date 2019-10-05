5e8a08a54cca44719510971274d6db742647cd0c
soo <onemask@naver.com>
2019-10-05 18:03:45 +0900

    [HW-4] BaseActivity 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseActivity.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 
-abstract class BaseActivity(@LayoutRes private val layoutRes: Int) : AppCompatActivity(){
+abstract class BaseActivity<P : BaseContract.Presenter>
+    (@LayoutRes private val layoutRes: Int) : AppCompatActivity(), BaseContract.View<P> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutRes)
         onStart()
+    }
+
+    override fun onDestroy() {
+        presenter.clearDisposable()
+        super.onDestroy()
     }
 }
