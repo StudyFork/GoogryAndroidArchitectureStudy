203cf29239554e9fc38164786a526162baeb8b6c
Seonoh Eam <>
2019-10-05 19:30:12 +0900

    BaseAcitivity 적용 ( 잘못된 분리 코드 되돌림 )

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainActivity.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainActivity.kt
@@ -3,19 +3,59 @@ package com.example.seonoh.seonohapp
 import android.os.Bundle
 import com.example.seonoh.seonohapp.contract.CoinMainContract
 import com.example.seonoh.seonohapp.model.Market
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity(), CoinMainContract.View {
+class MainActivity : BaseActivity(
+    R.layout.activity_main
+), CoinMainContract.View {
 
-    override val presenter: CoinMainContract.Presenter by lazy { CoinMainPresenter(this) }
+    override val presenter = CoinMainPresenter(this)
+    private lateinit var coinMarketNameList: List<String>
+    private lateinit var pagerAdapter: TabPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         initView()
         presenter.loadData()
     }
 
+    private fun initView() {
+        pagerAdapter = TabPagerAdapter(supportFragmentManager)
+        coinViewPager.run {
+            adapter = pagerAdapter
+            addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        }
+
+        tabLayout.run {
+            addTab(tabLayout.newTab().setText("KRW"))
+            addTab(tabLayout.newTab().setText("BTC"))
+            addTab(tabLayout.newTab().setText("ETH"))
+            addTab(tabLayout.newTab().setText("USDT"))
+            addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(coinViewPager))
+        }
+    }
+
     override fun setData(data: List<Market>) = pagerAdapter.setData(refineData(data))
 
+    private fun refineData(marketData: List<Market>): List<String> {
+
+        coinMarketNameList = marketData.map {
+            it.market.substringBefore("-")
+        }.distinct()
+
+        val marketDataList = ArrayList<String>()
+
+        coinMarketNameList.forEach { title ->
+
+            marketDataList += (marketData.filter {
+                it.market.substringBefore("-") == title
+            }.joinToString(",") {
+                it.market
+            })
+        }
+
+        return marketDataList
+    }
 
 }
