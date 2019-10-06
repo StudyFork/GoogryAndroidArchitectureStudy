ab03d325d825c4456dd35ddf15d443d0a129a446
leejinseong <dlwls5201@naver.com>
2019-10-07 01:55:46 +0900

    HW-8 ViewPager의 프래그먼트 재사용 코드 추가

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainAdapter.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainAdapter.kt
@@ -16,24 +16,28 @@ class MainAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     private var fragmentReferences: Hashtable<Int, WeakReference<TickerListFragment>> = Hashtable()
 
+    // Do NOT try to save references to the Fragments in getItem(),
+    // because getItem() is not always called. If the Fragment
+    // was already created then it will be retrieved from the FragmentManger
+    // and not here (i.e. getItem() won't be called again).
     override fun getItem(position: Int): Fragment {
-
         val item = markets?.get(position) ?: ""
+        return TickerListFragment.newInstance(item)
 
-        val fragment = TickerListFragment.newInstance(item)
+    }
 
-        fragmentReferences[position] = WeakReference(fragment)
-
-        return fragment
-
+    // Here we can finally safely save a reference to the created
+    // Fragment, no matter where it came from (either getItem() or
+    // FragmentManger). Simply save the returned Fragment from
+    // super.instantiateItem() into an appropriate reference depending
+    // on the ViewPager position.
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val createdFragment = super.instantiateItem(container, position) as Fragment
+        fragmentReferences[position] = WeakReference(createdFragment as TickerListFragment)
+        return createdFragment
     }
 
     override fun getCount() = markets?.size ?: 0
-
-    //생성된 fragment를 파괴하지 않는다.
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        //super.destroyItem(container, position, `object`)
-    }
 
     override fun getPageTitle(position: Int): CharSequence? {
         return titles?.get(position)
