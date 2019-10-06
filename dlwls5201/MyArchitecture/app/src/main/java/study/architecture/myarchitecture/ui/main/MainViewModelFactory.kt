781011968721c312862b8dcbefbdb59929e8bdaa
leejinseong <dlwls5201@naver.com>
2019-10-06 20:14:30 +0900

    HW-8 MainViewModelFactory 생성

---
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/ui/main/MainViewModelFactory.kt
@@ -0,0 +1,15 @@
+package study.architecture.myarchitecture.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import study.architecture.myarchitecture.data.repository.UpbitRepository
+
+class MainViewModelFactory(private val upbitRepository: UpbitRepository) :
+    ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(upbitRepository) as T
+    }
+
+}
