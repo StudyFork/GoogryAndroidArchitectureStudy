20dbabb3280961f063c7a560f21fc0ad1f817170
ganghee <idiosauri@naver.com>
2019-10-09 01:05:37 +0900

    [UpbitViewModel] 파리미터 제거하고 repository 변수 생성

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/viewmodel/UpbitViewModel.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/viewmodel/UpbitViewModel.kt
@@ -5,17 +5,19 @@ import androidx.databinding.ObservableField
 import com.example.mystudy.base.BaseViewModel
 import com.example.mystudy.data.FormatTickers
 import com.example.mystudy.data.UpbitRepository
+import com.example.mystudy.data.remote.UpbitRemoteDataSource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class UpbitViewModel(
-    private val repository: UpbitRepository
-) : BaseViewModel() {
+class UpbitViewModel: BaseViewModel() {
 
+    private val repository: UpbitRepository by lazy { UpbitRepository.getInstance(UpbitRemoteDataSource) }
     val tickerList = ObservableField<List<FormatTickers>>()
 
     fun getTicker(firstMarket: String?) {
         repository.getMarket()
+            .repeatWhen { it.delay(5, TimeUnit.SECONDS) }
             .observeOn(Schedulers.newThread())
             .subscribe { it ->
                 repository.getTicker(it)
