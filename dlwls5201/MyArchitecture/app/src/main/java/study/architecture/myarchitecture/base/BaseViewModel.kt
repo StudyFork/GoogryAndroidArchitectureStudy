03b2e59b16eaaeaec39f1d8db0c0fb6e7c98ec71
leejinseong <dlwls5201@naver.com>
2019-10-09 13:04:52 +0900

    HW-8 abstract class 로 변경

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/base/BaseViewModel.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/base/BaseViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-open class BaseViewModel : ViewModel() {
+abstract class BaseViewModel : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
 
