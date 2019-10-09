97de87e0692ec8e957f1800e4d79c11d02148ba5
soo <onemask@naver.com>
2019-10-09 18:21:25 +0900

    [HW-5] BaseActivity에 DataBinding 받을 수 있도록 추가

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseActivity.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseActivity.kt
@@ -3,13 +3,17 @@ package com.android.studyfork.base
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<P : BaseContract.Presenter>
+abstract class BaseActivity<B : ViewDataBinding, P : BaseContract.Presenter>
     (@LayoutRes private val layoutRes: Int) : AppCompatActivity(), BaseContract.View<P> {
+
+    protected lateinit var binding: B
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutRes)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
         onStart()
     }
 
