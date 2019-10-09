180a7d442c335d16c0295d6d81e8bba3cef442e4
soo <onemask@naver.com>
2019-10-09 18:23:48 +0900

    [HW-5] BaseFragment에 DataBinding 받을수 있도록 수정

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseFragment.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/base/BaseFragment.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<P : BaseContract.Presenter>(@LayoutRes private val layoutRes: Int) :
+abstract class BaseFragment<B : ViewDataBinding, P : BaseContract.Presenter>(@LayoutRes private val layoutRes: Int) :
     Fragment(), BaseContract.View<P> {
+
+    protected lateinit var binding: B
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(layoutRes, container, false)
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
     }
 
     override fun onDestroy() {
