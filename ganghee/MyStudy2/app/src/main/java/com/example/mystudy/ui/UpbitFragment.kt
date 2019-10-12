4ea482604cbea7cc96c176e47ffb648fdba9a3e0
ganghee <idiosauri@naver.com>
2019-10-12 10:46:20 +0900

    [UpbitFragment] ViewModelProviders 로 변경

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/UpbitFragment.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/ui/UpbitFragment.kt
@@ -3,21 +3,23 @@ package com.example.mystudy.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProviders
 import com.example.mystudy.BR
 import com.example.mystudy.R
 import com.example.mystudy.base.BaseFragment
 import com.example.mystudy.base.BaseRecyclerViewAdapter
 import com.example.mystudy.data.FormatTickers
-import com.example.mystudy.data.UpbitRepository
-import com.example.mystudy.data.remote.UpbitRemoteDataSource
 import com.example.mystudy.databinding.FragmentUpbitBinding
 import com.example.mystudy.databinding.RvItemListBinding
 import com.example.mystudy.viewmodel.UpbitViewModel
 
 class UpbitFragment : BaseFragment<FragmentUpbitBinding>(R.layout.fragment_upbit) {
 
-    private val upbitViewModel by viewModels<UpbitViewModel>()
+    private val upbitViewModel by lazy {
+        ViewModelProviders
+            .of(this)
+            .get(UpbitViewModel::class.java)
+    }
 
     @SuppressLint("CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
