bc43c5de5ca4d271cec8e0770c5bf133bb0a002c
Zojae031 <edgar3@naver.com>
2019-10-12 16:10:06 +0900

    Edit BaseActivity LifeCycle Owner

--- zojae031/Project/app/src/main/java/study/architecture/base/BaseActivity.kt
+++ zojae031/Project/app/src/main/java/study/architecture/base/BaseActivity.kt
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
     protected lateinit var binding: B
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
     }
 }
