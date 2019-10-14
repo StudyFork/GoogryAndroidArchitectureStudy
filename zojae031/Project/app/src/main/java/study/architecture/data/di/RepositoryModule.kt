f47fc4bf6957e1ef5082d8ee1eef19feb1f86de9
Zojae031 <edgar3@naver.com>
2019-10-14 17:16:33 +0900

    Add RepositoryModule

---
+++ zojae031/Project/app/src/main/java/study/architecture/data/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package study.architecture.data.di
+
+import android.content.Context
+import android.net.ConnectivityManager
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+import study.architecture.data.repository.Repository
+import study.architecture.data.repository.RepositoryImpl
+
+val repositoryModule = module {
+    single<Repository> { RepositoryImpl.getInstance(get(), get(), get()) }
+    single {
+        androidApplication().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+    }
+}
