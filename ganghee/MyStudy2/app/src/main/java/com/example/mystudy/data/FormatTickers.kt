949a659a3e700c1b44ed77e0e7f06b977dfe04a1
ganghee <idiosauri@naver.com>
2019-10-14 22:54:00 +0900

    [FormatTickers] 문자형 변형

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/FormatTickers.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/FormatTickers.kt
@@ -1,9 +1,9 @@
 package com.example.mystudy.data
 
 data class FormatTickers(
-    val toMarket: String,
-    val toTradePrice: String,
-    val toSignedChangeRate: String,
-    val toAccTradePrice24h: String,
-    val toRateColor: Int
+    val toMarket: String?,
+    val toTradePrice: Double?,
+    val toSignedChangeRate: Double?,
+    val toAccTradePrice24h: Double?,
+    val toRateColor: Double?
 )
