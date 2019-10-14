acbac0832dddbfb300a1f5788bd5c8e26d1a4ab9
wswon <dnjsdntjr1@gmail.com>
2019-10-14 11:20:10 +0900

    HW-9 Application class koin set

--- wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/App.kt
+++ wswon/AndroidArchitectureStudy/app/src/main/java/com/architecture/study/App.kt
@@ -2,10 +2,14 @@ package com.architecture.study
 
 import android.app.Application
 import android.content.Context
+import com.architecture.study.data.repository.di.repositoryModule
+import com.architecture.study.data.source.remote.di.remoteModule
+import com.architecture.study.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     companion object {
-
         lateinit var instance: App
             private set
     }
@@ -13,6 +17,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        startKoin{
+            androidContext(this@App)
+            modules(listOf(viewModelModule, repositoryModule, remoteModule))
+        }
     }
 
     fun context(): Context = applicationContext
