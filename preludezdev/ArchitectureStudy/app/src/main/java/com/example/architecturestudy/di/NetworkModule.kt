561ecb3c6f859cc39d3776571a36a9d6e907cd26
preludezdev <preludez.dev@gmail.com>
2019-10-15 14:42:27 +0900

    HW-9 networkModule 변경하고 RetrofitHelper 클래스 삭제

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/di/NetworkModule.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/di/NetworkModule.kt
@@ -1,15 +1,16 @@
 package com.example.architecturestudy.di
 
 import com.example.architecturestudy.network.CoinApiService
-import com.example.architecturestudy.network.RetrofitHelper
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+private const val API_URL = "https://api.upbit.com/"
+
 val networkModule = module {
     single {
         Retrofit.Builder()
-            .baseUrl(RetrofitHelper.API_URL)
+            .baseUrl(API_URL)
             .addConverterFactory(get())
             .build()
     }
