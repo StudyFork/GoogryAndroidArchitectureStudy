1bce2964c28f51dc247f7c87b18620926fb5c33c
soo <onemask@naver.com>
2019-10-16 00:56:26 +0900

    [HW-06] MainViewModel 생성

---
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.android.studyfork.ui.main.viewmodel
+
+import androidx.databinding.ObservableField
+import com.android.studyfork.base.BaseViewModel
+import com.android.studyfork.repository.UpbitRepositoryImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+/**
+ * created by onemask
+ */
+class MainViewModel : BaseViewModel() {
+
+    val datas = ObservableField<Pair<List<String>, List<String>>>()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        UpbitRepositoryImpl.run {
+            getMarketAll()
+                .map { response ->
+                    response.toList().map {
+                        it.first
+                    } to response.toList().map { it.second }.map {
+                        it.joinToString {
+                            it.market
+                        }
+                    }
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    datas.set(it)
+                }, { it.printStackTrace() })
+        }
+
+    }
+}
