a867f017b0e4ce0b871cc7e869ba8e9d700607f7
leejinseong <dlwls5201@naver.com>
2019-10-17 01:39:21 +0900

    HW-9 Room Warning 제거

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/source/local/LocalDataBase.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/data/source/local/LocalDataBase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import study.architecture.myarchitecture.data.model.UpbitMarket
 import study.architecture.myarchitecture.data.model.UpbitTicker
 
-@Database(entities = [UpbitMarket::class, UpbitTicker::class], version = 1)
+@Database(entities = [UpbitMarket::class, UpbitTicker::class], version = 1, exportSchema = false)
 abstract class LocalDataBase : RoomDatabase() {
 
     abstract fun getUpbitDao(): UpbitDao
