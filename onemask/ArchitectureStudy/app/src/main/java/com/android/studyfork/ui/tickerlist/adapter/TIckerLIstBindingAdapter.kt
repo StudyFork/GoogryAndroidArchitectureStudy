45ca1864d9089d286867f796f9d8cf6baf966103
soo <onemask@stunitas.com>
2019-10-17 00:01:00 +0900

    [HW-06] fragment_ticker_list.xml 수정

---
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/adapter/TIckerLIstBindingAdapter.kt
@@ -0,0 +1,19 @@
+package com.android.studyfork.ui.tickerlist.adapter
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.android.studyfork.network.model.Ticker
+import com.android.studyfork.ui.adapter.CoinItemAdapter
+
+/**
+ * created by onemask
+ */
+object TIckerLIstBindingAdapter {
+    @BindingAdapter("updateItem")
+    @JvmStatic
+    fun RecyclerView.updateTickerList(list :List<Ticker>?) {
+        list?.let {
+            (adapter as? CoinItemAdapter)?.setData(it)
+        }
+    }
+}
