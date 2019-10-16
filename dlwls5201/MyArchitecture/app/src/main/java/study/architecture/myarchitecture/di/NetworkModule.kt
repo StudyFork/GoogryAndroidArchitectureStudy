7d943b7208567865cc180133dfb6d595b92b5078
leejinseong <dlwls5201@naver.com>
2019-10-17 01:39:05 +0900

    HW-9 Network 모듈 수정

--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/di/NetworkModule.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/di/NetworkModule.kt
@@ -3,12 +3,36 @@ package study.architecture.myarchitecture.di
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import study.architecture.myarchitecture.BuildConfig
 import java.util.concurrent.TimeUnit
+
+const val HOST_URL = "https://api.upbit.com/"
 
 val networkModule = module {
 
     single {
+        Retrofit.Builder()
+            .baseUrl(HOST_URL)
+            .client(get())
+            .addCallAdapterFactory(get())
+            .addConverterFactory(get())
+            .build()
+    }
+
+    single<CallAdapter.Factory> {
+        RxJava2CallAdapterFactory.createAsync()
+    }
+
+    single<Converter.Factory> {
+        GsonConverterFactory.create()
+    }
+
+    single<OkHttpClient> {
         OkHttpClient.Builder().apply {
 
             //TimeOut 시간을 지정합니다.
--- dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/di/RemoteModule.kt
+++ dlwls5201/MyArchitecture/app/src/main/java/study/architecture/myarchitecture/di/RemoteModule.kt
@@ -2,13 +2,9 @@ package study.architecture.myarchitecture.di
 
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import study.architecture.myarchitecture.data.source.remote.UpbitApi
 import study.architecture.myarchitecture.data.source.remote.UpbitRemoteDataSource
 import study.architecture.myarchitecture.data.source.remote.UpbitRemoteLocalDataSourceImpl
-
-const val HOST_URL = "https://api.upbit.com/"
 
 val remoteModule = module {
 
@@ -17,12 +13,6 @@ val remoteModule = module {
     }
 
     single {
-        Retrofit.Builder()
-            .baseUrl(HOST_URL)
-            .client(get())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(UpbitApi::class.java)
+        get<Retrofit>().create(UpbitApi::class.java)
     }
 }
