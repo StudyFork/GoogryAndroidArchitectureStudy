0c7c893a1aa31983e33dad85c801feb76f202928
Seonoh Eam <>
2019-10-17 18:19:08 +0900

    [HW-06] CoinFragment 뷰모델 적용

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/CoinFragment.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/CoinFragment.kt
@@ -1,40 +1,78 @@
 package com.example.seonoh.seonohapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
-import com.example.seonoh.seonohapp.contract.CoinFragmentContract
 import com.example.seonoh.seonohapp.databinding.CoinFragmentBinding
+import com.example.seonoh.seonohapp.model.CoinViewModel
+import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
 import com.example.seonoh.seonohapp.model.UseCoinModel
+import com.example.seonoh.seonohapp.repository.CoinRepositoryImpl
+import com.example.seonoh.seonohapp.util.CalculateUtils
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class CoinFragment : BaseFragment<CoinFragmentContract.Presenter, CoinFragmentBinding>(
+class CoinFragment : BaseFragment<CoinFragmentBinding>(
     R.layout.coin_fragment
-), CoinFragmentContract.View {
+) {
 
-    override val presenter = CoinPresenter(this)
-    private lateinit var fragmentAdapter: CoinAdapter
+    private var fragmentAdapter = CoinAdapter()
     private var marketName: String? = null
+    private val coinRepository = CoinRepositoryImpl()
+    private val viewModel = CoinViewModel()
 
-    override fun setData(data: List<UseCoinModel>) = fragmentAdapter.addCoinData(data)
+    private fun loadData(marketName: String) {
+        compositeDisposable.add(
+            coinRepository.sendCurrentPriceInfo(marketName)
+                .map {
+                    viewModel.coinItem.set(refineData(it))
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({}, {
+                    Log.e("currentPriceInfo", "Fragment Network failed!! ${it.message}")
+                })
+        )
+    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        initView()
+    private fun refineData(result: List<CurrentPriceInfoModel>): List<UseCoinModel> {
+        // 데이터 가공후 모델에 넣음.
+        // signedChangeRate textcolor 처리때문에 viewholder에서 진행
+        val marketType = if (result.isNotEmpty()) {
+            result[0].market.substringBefore("-")
+        } else ""
+
+        return result.map {
+            var rate = CalculateUtils.setTradeDiff(it.signedChangeRate)["rate"].toString()
+            var color = CalculateUtils.setTradeDiff(it.signedChangeRate)["color"].toString().toInt()
+
+            UseCoinModel(
+                CalculateUtils.setMarketName(it.market),
+                CalculateUtils.filterTrade(it.tradePrice),
+                rate,
+                color,
+                it.accTradePrice24h?.let { CalculateUtils.setTradeAmount(marketType, it) ?: "0" }
+            )
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            coinViewModel = viewModel
+            krwRecyclerView.adapter = fragmentAdapter
+        }
         marketName = arguments?.getString(MARKET)
+        Log.e("market", "name : $marketName")
         marketName?.let {
-            presenter.loadData(it)
+            loadData(it)
         } ?: Toast.makeText(
             activity,
             resources.getString(R.string.empty_market_text),
             Toast.LENGTH_LONG
         ).show()
     }
-
-    private fun initView() {
-        fragmentAdapter = CoinAdapter()
-        binding.krwRecyclerView.adapter = fragmentAdapter
-    }
-
-    override fun showToast() {}
 
     companion object {
         private const val MARKET = "market"
@@ -47,4 +85,5 @@ class CoinFragment : BaseFragment<CoinFragmentContract.Presenter, CoinFragmentBi
             return fragment
         }
     }
+
 }
