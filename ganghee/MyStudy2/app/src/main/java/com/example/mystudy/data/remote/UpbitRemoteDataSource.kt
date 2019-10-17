98b055e29e15d1e7e43639741a2e61ea8fbcee31
ganghee <idiosauri@naver.com>
2019-10-17 21:30:49 +0900

    [UpbitRemoteDataSource] UpbitService를 파라미터로 변경하고 Retrofit 빌더패턴을 삭제

--- ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/remote/UpbitRemoteDataSource.kt
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/data/remote/UpbitRemoteDataSource.kt
@@ -3,29 +3,18 @@ package com.example.mystudy.data.remote
 import com.example.mystudy.data.MarketResponse
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * 서버에서 데이터를 가져오는 클래스
  **/
 
-object UpbitRemoteDataSource : UpbitService {
-    private val retrofit: UpbitService = Retrofit.Builder()
-        .baseUrl("https://api.upbit.com/v1/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build()
-        .create(UpbitService::class.java)
+class UpbitRemoteDataSource(private val upbitService: UpbitService) {
 
-    override fun getMarkets(): Single<List<MarketResponse>> =
-        retrofit.getMarkets()
+    fun getMarkets(): Single<List<MarketResponse>> =
+        upbitService.getMarkets()
             .subscribeOn(Schedulers.io())
 
-    override fun getTickers(marketList: String) =
-        retrofit.getTickers(marketList)
+    fun getTickers(marketList: String) =
+        upbitService.getTickers(marketList)
             .subscribeOn(Schedulers.io())
-
-
 }
