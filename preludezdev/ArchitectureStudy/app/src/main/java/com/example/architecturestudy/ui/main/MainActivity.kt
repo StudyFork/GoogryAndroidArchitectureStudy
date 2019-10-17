28b07d68be9b828ffb43a766320df60d9d730507
preludezdev <preludez.dev@gmail.com>
2019-10-17 12:12:43 +0900

    HW-9 initClickEvent 이름 변경 및 관련 함수들 리팩토링 2

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/main/MainActivity.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/main/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.viewpager.widget.ViewPager
 import com.example.architecturestudy.R
 import com.example.architecturestudy.base.BaseActivity
 import com.example.architecturestudy.databinding.ActivityMainBinding
-import com.example.architecturestudy.util.Filter
 import com.example.architecturestudy.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -45,7 +44,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             }
 
             override fun onPageSelected(position: Int) {
-                refreshData(position, mainViewModel.type.value!!, mainViewModel.order.value!!)
+                refreshData(position)
             }
         })
     }
@@ -58,15 +57,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         })
 
         mainViewModel.isReload.observe(this, Observer {
-            refreshData(
-                binding.viewPager.currentItem,
-                mainViewModel.type.value!!,
-                mainViewModel.order.value!!
-            )
+            refreshData(binding.viewPager.currentItem)
         })
     }
 
-    private fun refreshData(position: Int, type: Filter.Type, order: Filter.Order) {
-        vpAdapter.getFragment(position)?.refresh(type, order)
+    private fun refreshData(position: Int) {
+        vpAdapter.getFragment(position)?.refresh(
+            mainViewModel.type.value!!,
+            mainViewModel.order.value!!
+        )
     }
 }
