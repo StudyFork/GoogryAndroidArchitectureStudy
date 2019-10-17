978241a16b4f4dea5e56ba5d73f4d90b03ce5738
ganghee <idiosauri@naver.com>
2019-10-17 21:24:17 +0900

    [CoinHelperApplication] 전역객체 생성

---
+++ ganghee/MyStudy2/app/src/main/java/com/example/mystudy/CoinHelperApplication.kt
@@ -0,0 +1,28 @@
+package com.example.mystudy
+
+import android.app.Application
+import com.example.mystudy.di.dataSourceModule
+import com.example.mystudy.di.getRemoteServiceModules
+import com.example.mystudy.di.repositoryModule
+import com.example.mystudy.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class CoinHelperApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@CoinHelperApplication)
+            modules(
+                listOf(
+                    viewModelModule,
+                    repositoryModule,
+                    dataSourceModule,
+                    getRemoteServiceModules("https://api.upbit.com/v1/")
+                )
+            )
+        }
+    }
+}
