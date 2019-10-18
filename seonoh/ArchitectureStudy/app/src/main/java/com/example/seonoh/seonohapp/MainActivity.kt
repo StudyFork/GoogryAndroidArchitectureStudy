4753f1e99993eda80d0c8483e6ae090517650aca
seonoh <eso0609@naver.com>
2019-10-18 11:08:21 +0900

    [HW-06] MainActivity ViewModel 수정

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainActivity.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainActivity.kt
@@ -1,42 +1,20 @@
 package com.example.seonoh.seonohapp
 
 import android.os.Bundle
-import android.util.Log
 import com.example.seonoh.seonohapp.databinding.ActivityMainBinding
 import com.example.seonoh.seonohapp.model.MainViewModel
-import com.example.seonoh.seonohapp.model.Market
-import com.example.seonoh.seonohapp.repository.CoinRepositoryImpl
 import com.google.android.material.tabs.TabLayout
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
 ) {
 
-    private lateinit var coinMarketNameList: List<String>
     private var pagerAdapter = TabPagerAdapter(supportFragmentManager)
-    private val coinRepository = CoinRepositoryImpl()
-    private val viewModel = MainViewModel()
+    override val viewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
-        loadData()
-    }
-
-    private fun loadData() {
-        compositeDisposable.add(
-            coinRepository.sendMarket()
-                .map {
-                    viewModel.marketInfo.set(refineData(it))
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({}, {
-                    Log.e("currentPriceInfo", "Main Network failed!! ${it.message}")
-                })
-        )
     }
 
     private fun initView() {
@@ -54,26 +32,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
                 addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(coinViewPager))
             }
         }
-    }
-
-    private fun refineData(marketData: List<Market>): List<String> {
-
-        coinMarketNameList = marketData.map {
-            it.market.substringBefore("-")
-        }.distinct()
-
-        val marketDataList = ArrayList<String>()
-
-        coinMarketNameList.forEach { title ->
-
-            marketDataList += (marketData.filter {
-                it.market.substringBefore("-") == title
-            }.joinToString(",") {
-                it.market
-            })
-        }
-
-        return marketDataList
     }
 
 }
--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/MainViewModel.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/MainViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.seonoh.seonohapp.model
 
+import android.util.Log
 import androidx.databinding.ObservableField
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class MainViewModel {
+class MainViewModel : BaseViewModel() {
     var marketInfo = ObservableField<List<String>>()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        compositeDisposable.add(
+            coinRepository.sendMarket()
+                .map {
+                    refineData(it)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    marketInfo.set(it)
+                }, {
+                    Log.e("currentPriceInfo", "Main Network failed!! ${it.message}")
+                })
+        )
+    }
+
+    private fun refineData(marketData: List<Market>): List<String> {
+
+        var coinMarketNameList = marketData.map {
+            it.market.substringBefore("-")
+        }.distinct()
+
+        val marketDataList = ArrayList<String>()
+
+        coinMarketNameList.forEach { title ->
+
+            marketDataList += (marketData.filter {
+                it.market.substringBefore("-") == title
+            }.joinToString(",") {
+                it.market
+            })
+        }
+
+        return marketDataList
+    }
+
+
 }
