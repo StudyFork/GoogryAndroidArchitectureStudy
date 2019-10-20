248249547b406a7c0370412ff34d36d518c21385
soo <onemask@naver.com>
2019-10-20 16:57:52 +0900

    [HW-06]TickerViewModel > addTol(disposable)처리 추가

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/viewmodel/TickerViewModel.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/viewmodel/TickerViewModel.kt
@@ -3,24 +3,26 @@ package com.android.studyfork.ui.tickerlist.viewmodel
 import androidx.databinding.ObservableField
 import com.android.studyfork.base.BaseViewModel
 import com.android.studyfork.network.model.Ticker
-import com.android.studyfork.network.model.TickerResponse
 import com.android.studyfork.repository.UpbitRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
 
 class TickerViewModel : BaseViewModel() {
 
     val tickerList = ObservableField<List<Ticker>>()
 
     fun getTicker(market: String) {
-        UpbitRepositoryImpl.run {
-            getTickers(market)
-                .map {
-                    tickerList.set(it.map(TickerResponse::toTicker))
+        UpbitRepositoryImpl.getTickers(market)
+            .map { tickerList ->
+                tickerList.map { ticker ->
+                    ticker::toTicker.invoke()
                 }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({}, { it.printStackTrace() })
-        }
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                tickerList.set(it)
+            }, { it.printStackTrace() })
+            .addTo(disposable)
     }
-
-
 }
+
