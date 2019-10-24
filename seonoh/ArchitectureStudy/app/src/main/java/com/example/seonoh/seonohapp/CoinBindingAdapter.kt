179c97b81df41e70e859ba4fdd304e6f9435a64a
seonoh <eso0609@naver.com>
2019-10-24 10:01:13 +0900

    object 제거 후 바인딩어댑터 사용

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/CoinBindingAdapter.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/CoinBindingAdapter.kt
@@ -4,12 +4,9 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.seonoh.seonohapp.model.UseCoinModel
 
-object CoinBindingAdapter{
-    @BindingAdapter("addCoinItem")
-    @JvmStatic
-    fun RecyclerView.addCoinData(data : List<UseCoinModel>?){
-        data?.let {
-            (adapter as? CoinAdapter)?.setCoinData(data)
-        }
+@BindingAdapter("addCoinItem")
+fun RecyclerView.addCoinData(data: List<UseCoinModel>?) {
+    data?.let {
+        (adapter as? CoinAdapter)?.setCoinData(data)
     }
 }
--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainBindingAdapter.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/MainBindingAdapter.kt
@@ -3,12 +3,9 @@ package com.example.seonoh.seonohapp
 import androidx.databinding.BindingAdapter
 import androidx.viewpager.widget.ViewPager
 
-object MainBindingAdapter {
-    @BindingAdapter("setMarketNameData")
-    @JvmStatic
-    fun ViewPager.setMarketNameData(data : List<String>?){
-        data?.let {
-            (adapter as? TabPagerAdapter)?.setData(data)
-        }
+@BindingAdapter("setMarketNameData")
+fun ViewPager.setMarketNameData(data: List<String>?) {
+    data?.let {
+        (adapter as? TabPagerAdapter)?.setData(data)
     }
 }
