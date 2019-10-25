9dd6856da826bf5a46f767cd49087a70c76fea31
preludezdev <preludez.dev@gmail.com>
2019-10-25 12:04:34 +0900

    HW-9 RepositoryModule 코드 변경

--- preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/di/RepositoryModule.kt
+++ preludezdev/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/di/RepositoryModule.kt
@@ -1,12 +1,15 @@
 package com.example.architecturestudy.di
 
+import com.example.architecturestudy.data.source.CoinsDataSource
+import com.example.architecturestudy.data.source.CoinsRepository
 import com.example.architecturestudy.data.source.CoinsRepositoryImpl
 import com.example.architecturestudy.data.source.local.CoinsLocalDataSource
 import com.example.architecturestudy.data.source.remote.CoinsRemoteDataSource
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { CoinsRepositoryImpl(get(), get()) }
-    single { CoinsRemoteDataSource(get()) }
-    single { CoinsLocalDataSource() }
+    single { CoinsRepositoryImpl(get(named("Remote")), get(named("Local"))) as CoinsRepository }
+    single(named("Remote")) { CoinsRemoteDataSource(get()) as CoinsDataSource }
+    single(named("Local")) { CoinsLocalDataSource() as CoinsDataSource }
 }
