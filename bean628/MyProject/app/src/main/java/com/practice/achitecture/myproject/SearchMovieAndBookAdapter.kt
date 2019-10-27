fbfbf6bcd99f0d5ef4d8c8c925a3be04fc47b7e5
bean628 <dudwp628@naver.com>
2019-10-27 23:46:30 +0900

    HW-1 Glide 라이브러리 이용해서 이미지 불러오기 적용

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/SearchMovieAndBookAdapter.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/SearchMovieAndBookAdapter.kt
@@ -5,9 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.practice.achitecture.myproject.model.Item
 import com.practice.achitecture.myproject.utils.MyStringUtil
-import kotlinx.android.synthetic.main.item_blog_and_news.view.*
+import kotlinx.android.synthetic.main.item_blog_and_news.view.tv_description
+import kotlinx.android.synthetic.main.item_blog_and_news.view.tv_title
+import kotlinx.android.synthetic.main.item_book_and_movie.view.*
 
 class SearchMovieAndBookAdapter(private val items: ArrayList<Item>) :
     RecyclerView.Adapter<SearchMovieAndBookAdapter.ViewHolder>() {
@@ -39,6 +42,14 @@ class SearchMovieAndBookAdapter(private val items: ArrayList<Item>) :
         private var view: View = v
 
         fun bind(listener: View.OnClickListener, item: Item?) {
+
+            item?.image?.let {
+                Glide
+                    .with(view.context)
+                    .load(it)
+                    .centerCrop()
+                    .into(view.iv_main_image)
+            }
 
             view.tv_title.text = MyStringUtil.removeHtmlTags(item?.title)
             view.tv_description.text =
