59b7ae31745be2c733a209ca7b1d2d8bb9947bb8
Jason <ironelder@gmail.com>
2019-10-29 08:56:17 +0900

    CustomItemView에 타입 추가 및 타입에 따른 이미지 처리

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/CustomItemView.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/CustomItemView.kt
@@ -2,13 +2,28 @@ package com.ironelder.androidarchitecture.view
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.HtmlCompat
 import com.ironelder.androidarchitecture.R
+import com.ironelder.androidarchitecture.data.Item
+import kotlinx.android.synthetic.main.item_custom_item_view.view.*
 
 class CustomItemView : ConstraintLayout {
-    constructor(context: Context?) : super(context)
+    private val mItemType:String
+    constructor(context: Context?, type:String) : super(context){
+        mItemType = type
+        when(mItemType){
+            "blog", "news"-> itemImage.visibility = View.GONE
+        }
+    }
 
     init {
         LayoutInflater.from(context).inflate(R.layout.item_custom_item_view, this, true)
+
+    }
+
+    fun setData(item:Item){
+        itemTitle.text = HtmlCompat.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_COMPACT)
     }
 }
