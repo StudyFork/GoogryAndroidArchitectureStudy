a2f749e5f57f88279891c8f9861d716bf4eeab6a
Junseok Oh <kotohana5706@edcan.kr>
2019-10-29 01:42:43 +0900

    [HW-1 / Utils] 메인 화면별 모드 상수 생성

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/Constants.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/Constants.kt
@@ -4,4 +4,9 @@ package com.buddman1208.architecturestudy.utils
 object Constants {
     const val API_KEY = "yYql_n1XvzhzsN2_42gh"
     const val API_KEY_SERECT = "O5ATnchoCc"
+
+    const val MODE_BLOG = "blog"
+    const val MODE_NEWS = "news"
+    const val MODE_MOVIE = "movie"
+    const val MODE_BOOK = "book"
 }
