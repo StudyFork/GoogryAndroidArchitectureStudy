4b1a4b6fa852ae728d6cbcfea46a5364495a1e5e
Junseok Oh <kotohana5706@edcan.kr>
2019-10-28 23:13:08 +0900

    [HW-1 / MainActivity] 툴바 연결 및 제목 지정

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/MainActivity.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.buddman1208.architecturestudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
     }
+
+    private fun initToolbar() {
+        toolbar.apply {
+            setSupportActionBar(this)
+            setTitleTextColor(ContextCompat.getColor(applicationContext, android.R.color.white))
+            setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+            contentInsetStartWithNavigation = 0
+        }
+        supportActionBar?.title = getToolbarTitle()
+    }
+
+    private fun getToolbarTitle() = "블로그"
 }
