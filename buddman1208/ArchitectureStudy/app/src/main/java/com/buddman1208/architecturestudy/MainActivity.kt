570e5450fd6878bb6543ee27d3b608af7ee80c33
Junseok Oh <kotohana5706@edcan.kr>
2019-10-28 23:17:39 +0900

    [HW-1 / MainActivity] BottomNavigation 선택에 따라 제목 변경되도록 변경

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/MainActivity.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.buddman1208.architecturestudy
 
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initToolbar()
+        initBottomNavigation()
     }
 
     private fun initToolbar() {
@@ -21,8 +23,24 @@ class MainActivity : AppCompatActivity() {
             setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
             contentInsetStartWithNavigation = 0
         }
-        supportActionBar?.title = getToolbarTitle()
+        updateToolbarTitle(bottomTabView.selectedItemId)
     }
 
-    private fun getToolbarTitle() = "블로그"
+    private fun initBottomNavigation() {
+        bottomTabView.setOnNavigationItemSelectedListener {
+            updateToolbarTitle(it.itemId)
+            true
+        }
+    }
+
+    private fun updateToolbarTitle(@IdRes itemId: Int) {
+        supportActionBar?.title = when (itemId) {
+            R.id.menuBlog -> "블로그"
+            R.id.menuBook -> "도서"
+            R.id.menuMovie -> "영화"
+            R.id.menuNews -> "뉴스"
+            else -> ""
+        }
+    }
+
 }
