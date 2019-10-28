248ba60fdfce774e61fb358bfaa775b5e2c8bae7
taeiim <taeiim.dev@gmail.com>
2019-10-29 01:16:17 +0900

    HW-1 fragment 추가, navigation과 연동

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/MainActivity.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/MainActivity.kt
@@ -2,11 +2,41 @@ package com.god.taeiim.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        loadFragment(bottomNavigation.selectedItemId)
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        loadFragment(item.itemId)
+        true
+    }
+
+    private fun loadFragment(itemId: Int) {
+        val tag = itemId.toString()
+        val fragment = supportFragmentManager.findFragmentByTag(tag) ?: when (itemId) {
+            R.id.blogNavigation -> BlogFragment()
+            R.id.newsNavigation -> NewsFragment()
+            R.id.movieNavigation -> MovieFragment()
+            R.id.bookNavigation -> BookFragment()
+            else -> null
+        }
+        fragment?.let { replaceFragment(fragment) }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment, fragment.tag)
+                .commit()
     }
 }
---
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/MovieFragment.kt
@@ -0,0 +1,17 @@
+package com.god.taeiim.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MovieFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, null)
+    }
+}
