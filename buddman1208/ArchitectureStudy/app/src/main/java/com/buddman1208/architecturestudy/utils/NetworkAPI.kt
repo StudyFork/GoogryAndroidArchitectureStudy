6a0ba44a4058c2020e42b098e5a2957bf94af090
Junseok Oh <kotohana5706@edcan.kr>
2019-10-28 20:31:13 +0900

    [HW-1 / Utils] Naver 검색 호출 API 추가

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/NetworkAPI.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/NetworkAPI.kt
@@ -1,3 +1,17 @@
 package com.buddman1208.architecturestudy.utils
 
-interface NetworkAPI
+import io.reactivex.Observable
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface NetworkAPI {
+
+    @GET("/v1/search/{searchType}.json")
+    fun searchByType(
+        @Path("searchType") searchType: String,
+        @Query("query") query: String
+    ): Observable<Response<HashMap<String, String>>>
+
+}
--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/NetworkManager.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/NetworkManager.kt
@@ -1,9 +1,15 @@
 package com.buddman1208.architecturestudy.utils
 
+import io.reactivex.Observable
+import retrofit2.Response
+
 
 object NetworkManager {
 
     private var service = NetworkServiceGenerator.createService(NetworkAPI::class.java)
 
-
+    fun searchByType(
+        searchType: String,
+        query: String
+    ): Observable<Response<HashMap<String, String>>> = service.searchByType(searchType, query)
 }
