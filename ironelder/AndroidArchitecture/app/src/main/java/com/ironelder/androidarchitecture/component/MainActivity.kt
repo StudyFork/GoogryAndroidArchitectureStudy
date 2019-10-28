b25783a1fa0a18693466df83eec2549fa1f97198
Jason <ironelder@gmail.com>
2019-10-28 23:16:50 +0900

    Fragment를 사용하기 위한 ViewPager및 TabLayout추가
    Fragment를 사용하기 위한 ViewPager및 TabLayout추가

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/component/MainActivity.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/component/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.ironelder.androidarchitecture.component
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.ironelder.androidarchitecture.R
+
 
 class MainActivity : BaseActivity() {
     override fun doCreate(savedInstanceState: Bundle?) {
         super.doCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val pageAdapter = PagerAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        pager.adapter = pageAdapter
+
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+        tab.setupWithViewPager(pager)
+
+    }
+}
+
+class PagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm)
+{
+    val PAGE_MAX_CNT = 4
+
+    override fun getCount(): Int {
+        return PAGE_MAX_CNT
+    }
+
+    override fun getItem(position: Int): Fragment? {
+        val fragment = when(position)
+        {
+            1 -> TestFragment()
+            2 -> TestFragment()
+            3 -> TestFragment()
+            else -> BlogFragment()
+        }
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        val title = when(position)
+        {
+            1 -> "one"
+            2 -> "tow"
+            3 -> "three"
+            else -> "Blog"
+        }
+        return title
     }
 }
