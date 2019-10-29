22455b6876d1d86b4651bcdc4097b5bc50a0814d
Hak <spingferry@gmail.com>
2019-10-29 17:10:15 +0900

    HW-1 Blog 프래그먼트 api 요청,응답 임시 테스트

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.kt
@@ -1,13 +1,20 @@
 package com.example.androidstudy.ui.blog
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.androidstudy.R
 import com.example.androidstudy.R.layout
+import com.example.androidstudy.api.RetrofitBuilder
+import com.example.androidstudy.api.data.TotalModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BlogFragment : Fragment() {
     override fun onCreateView(
@@ -26,10 +33,24 @@ class BlogFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        init();
+        init()
     }
 
-    private fun init(){
+    private fun init() {
+        apiFetchData()
+    }
 
+    private fun apiFetchData() {
+        val result = RetrofitBuilder.instance().requestSearchForNaver("blog", "치킨")
+        result.enqueue(object : Callback<TotalModel> {
+            override fun onFailure(call: Call<TotalModel>, t: Throwable) {
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT)
+            }
+
+            override fun onResponse(call: Call<TotalModel>, response: Response<TotalModel>) {
+                var resultList = response.body()
+                Log.d("TEST1234", "RESUTL : ${resultList.toString()}")
+            }
+        })
     }
 }
