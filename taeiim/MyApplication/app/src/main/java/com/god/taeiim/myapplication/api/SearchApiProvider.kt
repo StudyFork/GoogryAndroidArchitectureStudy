c8450d3d273beeedad8e001d7b776be36fff67c0
taeiim <taeiim.god@gmail.com>
2019-10-29 23:08:45 +0900

    HW-1 API 통신을 위한 item, provider 생성

---
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/api/SearchApiProvider.kt
@@ -0,0 +1,45 @@
+package com.god.taeiim.myapplication.api
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+
+fun provideAuthApi(): SearchApi = Retrofit.Builder()
+        .baseUrl("https://openapi.naver.com/v1/")
+        .client(provideOkHttpClient(provideLoggingInterceptor(), AuthInterceptor()))
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(SearchApi::class.java)
+
+
+private fun provideOkHttpClient(
+        interceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor?
+): OkHttpClient = OkHttpClient.Builder()
+        .run {
+            if (null != authInterceptor) {
+                addInterceptor(authInterceptor)
+            }
+            addInterceptor(interceptor)
+            build()
+        }
+
+private fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+internal class AuthInterceptor() : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain)
+            : Response = with(chain) {
+        val newRequest = request().newBuilder().run {
+            addHeader("X-Naver-Client-Id", "4kOUBLrom4188o7GQtsO")
+            addHeader("X-Naver-Client-Secret", "bRjUd3_TJB")
+            build()
+        }
+        proceed(newRequest)
+    }
+}
