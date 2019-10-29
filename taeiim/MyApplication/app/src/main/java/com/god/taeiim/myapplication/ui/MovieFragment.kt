5dff68f2043ce2152d628ffdd7e702b78683969c
taeiim <taeiim.god@gmail.com>
2019-10-30 00:17:40 +0900

    HW-1 영화 데이터 가져온 후 recyclerView 표시

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/ui/MovieFragment.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/ui/MovieFragment.kt
@@ -5,21 +5,83 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.god.taeiim.myapplication.R
 import com.god.taeiim.myapplication.api.SearchApi
 import com.god.taeiim.myapplication.api.model.SearchResult
 import com.god.taeiim.myapplication.api.provideAuthApi
 import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.item_movie.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MovieFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    val api: SearchApi by lazy { provideAuthApi() }
+    internal var searchCall: Call<SearchResult>? = null
+    var searchResultList: ArrayList<SearchResult.Item> = ArrayList()
+    private val adapter = MovieAdapter(searchResultList)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, null)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        with(searchResultRecyclerView) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = this@MovieFragment.adapter
+        }
+        searchBtn.setOnClickListener {
+            val query = searchEditTv.text.toString()
+            searchMovie(query)
+        }
+    }
+
+    private fun searchMovie(query: String) {
+        searchCall = api.searchContents("movie", query)
+        searchCall!!.enqueue(object : Callback<SearchResult> {
+
+            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
+                with(adapter) {
+                    setItems(response.body()!!.items as ArrayList<SearchResult.Item>)
+                    notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+
+            }
+        })
+    }
+
+    private inner class MovieAdapter(var resultList: ArrayList<SearchResult.Item>) : RecyclerView.Adapter<MovieListHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+            return MovieListHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return resultList.size
+        }
+
+        fun setItems(items: ArrayList<SearchResult.Item>) {
+            resultList = items
+        }
+
+        override fun onBindViewHolder(holder: MovieListHolder, position: Int) {
+            resultList[position].let { movie ->
+                with(holder.itemView) {
+                    titleTv.text = movie.title
+                    subTitleTv.text = movie.pubDate
+                    descTv.text = movie.director + movie.actor
+                }
+            }
+        }
+    }
+
+    private inner class MovieListHolder(view: View) : RecyclerView.ViewHolder(view)
 }
