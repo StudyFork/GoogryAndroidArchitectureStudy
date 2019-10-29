39c6d069fdb749a8fa50694c81763d15eaf4a1ee
Jason <ironelder@gmail.com>
2019-10-29 23:21:48 +0900

    CustomItemView 구체화
    CustomItemView 변수명 정리, setData함수 내부 이미지 Glide추가

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/CustomItemView.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/CustomItemView.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.text.HtmlCompat
+import com.bumptech.glide.Glide
 import com.ironelder.androidarchitecture.R
+import com.ironelder.androidarchitecture.common.*
 import com.ironelder.androidarchitecture.data.Item
 import kotlinx.android.synthetic.main.item_custom_item_view.view.*
 
 class CustomItemView : ConstraintLayout {
-    private val mItemType:String
-    constructor(context: Context?, type:String) : super(context){
+    private val mItemType: String
+
+    constructor(context: Context?, type: String) : super(context) {
         mItemType = type
-        when(mItemType){
-            "blog", "news"-> itemImage.visibility = View.GONE
-        }
     }
 
     init {
@@ -23,7 +23,21 @@ class CustomItemView : ConstraintLayout {
 
     }
 
-    fun setData(item:Item){
+    fun setData(item: Item) {
         itemTitle.text = HtmlCompat.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_COMPACT)
+        itemContent.text = HtmlCompat.fromHtml(
+            item.description ?: item.director,
+            HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
+        when (mItemType) {
+            BLOG, NEWS -> {
+                itemImage.visibility = View.GONE
+            }
+            else -> {
+                itemImage.visibility = View.VISIBLE
+                Glide.with(context).load(item.image).centerCrop()
+                    .placeholder(R.drawable.ic_launcher_background).into(itemImage)
+            }
+        }
     }
 }
