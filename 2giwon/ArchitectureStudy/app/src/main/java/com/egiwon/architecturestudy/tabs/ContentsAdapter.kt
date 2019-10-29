3a8c2b80482a42c2b3606e9377b5ca09b1543a9f
middlefinger <2giwon@gmail.com>
2019-10-29 15:02:42 +0900

    HW-1 Retrofit 결과 처리

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsAdapter.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsAdapter.kt
@@ -1,9 +1,14 @@
 package com.egiwon.architecturestudy.tabs
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.egiwon.architecturestudy.R
 import com.egiwon.architecturestudy.data.Content
@@ -20,7 +25,44 @@ class ContentsAdapter : RecyclerView.Adapter<ContentsAdapter.ViewHolder>() {
     override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder) {
 
+            list[position].title?.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    tvTitle.text = Html.fromHtml(it, Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    tvTitle.text = Html.fromHtml(it)
+                }
+            }
+
+            list[position].description?.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    tvDescription.text = Html.fromHtml(it, Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    tvDescription.text = Html.fromHtml(it)
+                }
+            }
+
+            linkUrl = list[position].link?.let { it }
+
+        }
+
+        holder.itemView.setOnClickListener {
+
+            startActivity(
+                holder.itemView.context,
+                Intent(
+                    Intent.ACTION_VIEW, Uri.parse(holder.linkUrl)
+                ),
+                null
+            )
+        }
+    }
+
+    fun setList(items: List<Content.Item>) {
+        items.iterator().forEach {
+            list.add(it)
+        }
     }
 
     inner class ViewHolder(
--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsFragment.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsFragment.kt
@@ -1,9 +1,12 @@
 package com.egiwon.architecturestudy.tabs
 
+import android.os.Bundle
 import com.egiwon.architecturestudy.R
 import com.egiwon.architecturestudy.base.BaseFragment
 import com.egiwon.architecturestudy.data.Content
 import com.egiwon.architecturestudy.data.SearchCallback
+import com.egiwon.architecturestudy.data.SearchService
+import kotlinx.android.synthetic.main.fg_contents.*
 
 
 class ContentsFragment(
@@ -12,7 +15,27 @@ class ContentsFragment(
     R.layout.fg_contents
 ), SearchCallback {
 
-    override fun onSuccess(searchContents: List<Content.Item>) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        btn_search.setOnClickListener {
+            context?.let {
+                SearchService(this).getContentsList(
+                    et_search.text.toString(),
+                    type,
+                    it.getString(R.string.naver_api_client_id),
+                    it.getString(R.string.naver_api_client_secret)
+                )
+            }
+        }
+    }
+
+    override fun onSuccess(searchContents: List<Content.Item>) {
+        val contentsAdapter = ContentsAdapter()
+        contentsAdapter.setList(searchContents)
+
+        rv_contents.adapter = contentsAdapter
+        rv_contents.setHasFixedSize(true)
+        (rv_contents.adapter)?.notifyItemInserted(searchContents.size - 1)
     }
 }
