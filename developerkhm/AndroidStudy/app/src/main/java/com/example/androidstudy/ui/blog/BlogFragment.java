22332130c5d3806dbb0d54def5209062a08e65a8
Hak <spingferry@gmail.com>
2019-10-29 16:20:15 +0900

    HW-1 기존 샘플 Fragment 파일명 및 패키지명 변경 BookFragment 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.java
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.java
@@ -1,4 +1,4 @@
-package com.example.androidstudy.ui.home;
+package com.example.androidstudy.ui.blog;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,30 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.androidstudy.R;
 
-public class HomeFragment extends Fragment {
+public class BlogFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_movie, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView.setText("blog");
+
         return root;
     }
 }
--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/news/NewsFragment.java
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/news/NewsFragment.java
@@ -1,4 +1,4 @@
-package com.example.androidstudy.ui.dashboard;
+package com.example.androidstudy.ui.news;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,30 +6,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.androidstudy.R;
 
-public class DashboardFragment extends Fragment {
+public class NewsFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_news, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView.setText("news");
+
         return root;
     }
 }
