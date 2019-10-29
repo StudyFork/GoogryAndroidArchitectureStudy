99e03acd0fabe7c6acee159e978b2c90742b242e
Jason <ironelder@gmail.com>
2019-10-29 23:17:52 +0900

    탭에서 사용하게될 상수값 추가
    탭에서 사용하게될 상수값 추가

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/common/Contants.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/common/Contants.kt
@@ -2,3 +2,9 @@ package com.ironelder.androidarchitecture.common
 
 const val CLIENT_ID = "HZCMoKj79hgAMoqJfBoJ"
 const val CLIENT_KEY = "M2G9WywvRE"
+
+const val TAB_MAX_CNT = 4
+const val BLOG = "blog"
+const val NEWS = "news"
+const val BOOK = "book"
+const val MOVIE = "movie"
