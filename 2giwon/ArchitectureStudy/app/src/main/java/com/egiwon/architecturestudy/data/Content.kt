aa1391d63efdacf6a2e47b790d9cb37d4060ce79
middlefinger <2giwon@gmail.com>
2019-10-29 14:41:52 +0900

    HW-1 api 결과를 받아올 data클래스 생성

---
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/Content.kt
@@ -0,0 +1,42 @@
+package com.egiwon.architecturestudy.data
+
+import com.google.gson.annotations.SerializedName
+
+class Content(
+    @SerializedName("lastBuildDate") var lastBuildDate: String,
+    @SerializedName("total") var total: Int,
+    @SerializedName("start") var start: Int,
+    @SerializedName("display") var display: Int,
+    @SerializedName("items") var items: List<Item>
+) {
+    data class Item(
+        @SerializedName("image")
+        val image: String?,
+        @SerializedName("subtitle")
+        val subtitle: String,
+        @SerializedName("pubdate")
+        val pubDate: String,
+        @SerializedName("director")
+        val director: String,
+        @SerializedName("actor")
+        val actor: String,
+        @SerializedName("userRating")
+        val userRating: Double,
+
+        @SerializedName("bloggerlink")
+        val bloggerlink: String,
+        @SerializedName("bloggername")
+        val bloggername: String,
+        @SerializedName("description")
+        val description: String?,
+        @SerializedName("link")
+        val link: String?,
+        @SerializedName("postdate")
+        val postdate: String,
+        @SerializedName("title")
+        val title: String?,
+
+        @SerializedName("originallink")
+        val originallink: String
+    )
+}
