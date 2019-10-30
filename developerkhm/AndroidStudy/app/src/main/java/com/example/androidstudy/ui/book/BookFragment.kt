b7df0a8c7d6f41134803cd931a3ebb793b5a2652
Hak <spingferry@gmail.com>
2019-10-30 15:23:58 +0900

    HW-1 movie, book BaseFragment로 상속, 기본 검색 코드 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/book/BookFragment.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/book/BookFragment.kt
@@ -1,15 +1,21 @@
 package com.example.androidstudy.ui.book
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidstudy.R
 import com.example.androidstudy.R.layout
+import com.example.androidstudy.ui.base.BaseFragment
+import com.ironelder.androidarchitecture.view.AdapterBlog
+import kotlinx.android.synthetic.main.layout_search_view.*
 
-class BookFragment : Fragment() {
+class BookFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,9 +23,36 @@ class BookFragment : Fragment() {
     ): View? {
         val root: View =
             inflater.inflate(layout.fragment_book, container, false)
-//        val textView: TextView =
-//            root.findViewById(R.id.text_dashboard)
-//        textView.text = "book"
         return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        setLayout()
+    }
+
+    private fun setLayout() {
+
+        searchButton.setOnClickListener {
+            if (!TextUtils.isEmpty(searchEditText.text.toString())) {
+                apiFetchData(searchEditText.text.toString(), typeArray[3], { response ->
+                    var resultList = response.body()
+                    Log.d("TEST1234", "RESUTL : ${resultList.toString()}")
+                    (resultRecyclerView?.adapter as AdapterBlog).setItemList(resultList?.items)
+                }, {
+
+                })
+            }
+        }
+
+        resultRecyclerView.adapter = AdapterBlog(context, arrayListOf(), "blog")
+        resultRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
     }
 }
--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/movie/MovieFragment.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/movie/MovieFragment.kt
@@ -1,20 +1,56 @@
 package com.example.androidstudy.ui.movie
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidstudy.R
 import com.example.androidstudy.R.layout
+import com.example.androidstudy.ui.base.BaseFragment
+import com.ironelder.androidarchitecture.view.AdapterBlog
+import kotlinx.android.synthetic.main.layout_search_view.*
 
-class MovieFragment : Fragment() {
+class MovieFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        setLayout()
+    }
+
+    private fun setLayout() {
+
+        searchButton.setOnClickListener {
+            if (!TextUtils.isEmpty(searchEditText.text.toString())) {
+                apiFetchData(searchEditText.text.toString(), typeArray[2], { response ->
+                    var resultList = response.body()
+                    Log.d("TEST1234", "RESUTL : ${resultList.toString()}")
+                    (resultRecyclerView?.adapter as AdapterBlog).setItemList(resultList?.items)
+                }, {
+
+                })
+            }
+        }
+
+        resultRecyclerView.adapter = AdapterBlog(context, arrayListOf(), "blog")
+        resultRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
     }
 }
