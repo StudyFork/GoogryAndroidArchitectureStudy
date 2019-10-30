2c393fc0aba4655cc6a45ba8fa46bad9eea715a4
Hak <spingferry@gmail.com>
2019-10-30 17:51:49 +0900

    HW-1 네이버 api client 키 변경

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/common/CommonContant.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/common/CommonContant.kt
@@ -1,4 +1,4 @@
 package com.example.androidstudy.common
 
-const val CLIENT_ID = "HZCMoKj79hgAMoqJfBoJ"
-const val CLIENT_KEY = "M2G9WywvRE"
+const val CLIENT_ID = "MT0R4eO0lor3qcDBQ6bq"
+const val CLIENT_KEY = "KzDc6dNBOD"
