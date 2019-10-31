4866ff3a9d017fd254238aac9a182050ec1c076b
Junseok Oh <kotohana5706@edcan.kr>
2019-10-31 12:23:46 +0900

    [HW-1 / BookItem] 기본값 추가

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/models/BookItem.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/models/BookItem.kt
@@ -1,11 +1,11 @@
 package com.buddman1208.architecturestudy.models
 
 data class BookItem(
-    val title: String,
-    val link: String,
-    val description: String,
-    val image: String,
-    val author: String,
-    val publisher: String,
-    val pubdate: String
+    val title: String = "",
+    val link: String = "",
+    val description: String = "",
+    val image: String = "",
+    val author: String = "",
+    val publisher: String = "",
+    val pubdate: String = ""
 )
