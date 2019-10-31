141fb0adca2834aa8d7b3e986e7562c441bc1092
Junseok Oh <kotohana5706@edcan.kr>
2019-10-31 11:18:38 +0900

    [HW-1 / BookItem] author, publisher, pubdate 필드 추가

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/models/BookItem.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/models/BookItem.kt
@@ -4,5 +4,8 @@ data class BookItem(
     val title: String,
     val link: String,
     val description: String,
-    val image: String
+    val image: String,
+    val author: String,
+    val publisher: String,
+    val pubdate: String
 )
