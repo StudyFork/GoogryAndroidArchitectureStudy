36bda53be8c95ba8b773d0e1d439f347c70a3f26
bean628 <dudwp628@naver.com>
2019-11-02 23:36:23 +0900

    HW-1 class 대신 object GlideWrapper 선언하기

--- bean628/MyProject/app/src/main/java/common/GlideWrapper.kt
+++ bean628/MyProject/app/src/main/java/common/GlideWrapper.kt
@@ -5,16 +5,14 @@ import com.bumptech.glide.Glide
 import com.practice.achitecture.myproject.R
 import kotlinx.android.synthetic.main.item_book_and_movie.view.*
 
-class GlideWrapper {
+object GlideWrapper {
 
-    companion object {
-        fun showImage(imageView: ImageView, url: String) {
-            Glide.with(imageView.context)
-                .load(url)
-                .centerCrop()
-                .placeholder(R.drawable.drawable_gray)
-                .error(R.drawable.ic_empty_image)
-                .into(imageView.iv_main_image)
-        }
+    fun showImage(imageView: ImageView, url: String) {
+        Glide.with(imageView.context)
+            .load(url)
+            .centerCrop()
+            .placeholder(R.drawable.drawable_gray)
+            .error(R.drawable.ic_empty_image)
+            .into(imageView.iv_main_image)
     }
 }
