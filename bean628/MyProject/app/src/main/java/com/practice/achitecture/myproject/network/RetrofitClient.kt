ee4b2c65ccd0b77d6393221e31b8d6ecf19872ba
bean628 <dudwp628@naver.com>
2019-11-02 15:52:41 +0900

    HW-1 okHttpClient 프로퍼티 custom getter 말고 함수로 만들어서 처리

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/network/RetrofitClient.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/network/RetrofitClient.kt
@@ -11,31 +11,30 @@ class RetrofitClient(private val _baseUrl: String) {
     private val _connectionTime: Long = 15
 
     //log를 볼 수 있도록 httpLogginInterceptor를 만듦
-    private val _okHttpClient: OkHttpClient
-        get() {
-            val httpLoggingInterceptor = HttpLoggingInterceptor()
-            val addHeaderInterceptor = Interceptor { chain ->
-                val original = chain.request()
-                val requestBuilder = original.newBuilder()
-                    .addHeader("X-Naver-Client-Id", "pjKtnD_jiM0jPuKVf0ce")
-                    .addHeader("X-Naver-Client-Secret", "pZfWJLt0Hk")
-                val request = requestBuilder.build()
-                chain.proceed(request)
-            }
-            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-            return OkHttpClient().newBuilder()
-                .connectTimeout(_connectionTime, TimeUnit.SECONDS)
-                .addInterceptor(httpLoggingInterceptor)
-                .addInterceptor(addHeaderInterceptor)
-                .build()
+    private fun makeOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        val addHeaderInterceptor = Interceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+                .addHeader("X-Naver-Client-Id", "pjKtnD_jiM0jPuKVf0ce")
+                .addHeader("X-Naver-Client-Secret", "pZfWJLt0Hk")
+            val request = requestBuilder.build()
+            chain.proceed(request)
         }
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient().newBuilder()
+            .connectTimeout(_connectionTime, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(addHeaderInterceptor)
+            .build()
+    }
 
     //retrofit에 각종 값을 setting
     val retrofitInterface: RetrofitService
         get() {
             return Retrofit.Builder()
                 .baseUrl(_baseUrl)
-                .client(_okHttpClient) // httpClient연결을 통해 log 확인
+                .client(makeOkHttpClient()) // httpClient연결을 통해 log 확인
                 .addConverterFactory(GsonConverterFactory.create()) //Gson을 쓸 수 있도록 Factory생성
                 .build().create(RetrofitService::class.java)
         }
