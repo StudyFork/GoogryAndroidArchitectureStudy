615770bb334608892d251477c2e8037c0037d5e8
bean628 <dudwp628@naver.com>
2019-11-02 19:32:40 +0900

    HW-1 개행 수정

--- bean628/MyProject/app/src/main/java/common/Constant.kt
+++ bean628/MyProject/app/src/main/java/common/Constant.kt
@@ -2,7 +2,6 @@ package common
 
 const val NAVER_API_BASE_URL: String = "https://openapi.naver.com/"
 
-
 const val SEARCH_TYPE_MOVIE = 0
 const val SEARCH_TYPE_BOOK = 1
 const val SEARCH_TYPE_BLOG = 2
