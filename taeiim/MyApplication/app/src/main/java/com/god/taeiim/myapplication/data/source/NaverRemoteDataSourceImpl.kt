8c074edf2499706e2da4d25084ac50147fadd175
taeiim <taeiim.dev@gmail.com>
2019-11-04 21:29:43 +0900

    HW-2 DataSource 추가

---
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/data/source/NaverRemoteDataSourceImpl.kt
@@ -0,0 +1,31 @@
+package com.god.taeiim.myapplication.data.source
+
+import com.god.taeiim.myapplication.api.model.SearchResult
+import com.god.taeiim.myapplication.api.provideAuthApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+object NaverRemoteDataSourceImpl : NaverRemoteDataSource {
+    override fun getResultData(
+        searchType: String,
+        query: String,
+        success: (results: SearchResult) -> Unit,
+        fail: (t: Throwable) -> Unit
+    ) {
+        provideAuthApi().searchContents(searchType, query).let {
+            it.enqueue(object : Callback<SearchResult> {
+                override fun onResponse(
+                    call: Call<SearchResult>,
+                    response: Response<SearchResult>
+                ) {
+                    response.body()?.let { result -> success(result) } ?: fail(Throwable())
+                }
+
+                override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                    fail(t)
+                }
+            })
+        }
+    }
+}
