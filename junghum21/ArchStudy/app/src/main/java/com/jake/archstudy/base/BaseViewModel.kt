5a376257618f25ca39d5b9db9d3f9340f4f147bd
jake <jake@umsun.co.kr>
2019-11-05 10:11:41 +0900

    HW-07 BaseViewModel에 ViewModel 상속

--- junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/base/BaseViewModel.kt
+++ junghum21/ArchStudy/app/src/main/java/com/jake/archstudy/base/BaseViewModel.kt
@@ -1,8 +1,9 @@
 package com.jake.archstudy.base
 
 import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel {
+abstract class BaseViewModel : ViewModel() {
 
     val toast = ObservableField<String>()
 
