5da50d221ef8cadd8dc5a3463e3bb214734a1e9b
seonoh <eso0609@naver.com>
2019-11-05 13:57:54 +0900

    [HW-07] LiveData 적용 (ObservableField -> MutableLiveData 변경)

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/MainViewModel.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/model/MainViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.seonoh.seonohapp.model
 
 import android.util.Log
-import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.seonoh.seonohapp.repository.CoinRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(private val repo: CoinRepositoryImpl) : BaseViewModel() {
-    val marketInfo = ObservableField<List<String>>()
+    private val _marketInfo = MutableLiveData<List<String>>()
+
+    val marketInfo: LiveData<List<String>>
+        get() = _marketInfo
 
     init {
         loadData()
@@ -23,7 +27,9 @@ class MainViewModel(private val repo: CoinRepositoryImpl) : BaseViewModel() {
             .map(::refineData)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(marketInfo::set, ::handleError)
+            .subscribe({
+                _marketInfo.value = it
+            }, ::handleError)
             .addCompositeDisposable()
 
     }
