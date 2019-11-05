0b7fb72b17dc462ea2eb3de7fa44c6abbe1a557a
khm <spingferry@gmail.com>
2019-11-05 18:21:47 +0900

    HW-2 NaverData search api fun 구현

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/data/NaverDataRepositoryImpl.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/data/NaverDataRepositoryImpl.kt
@@ -1,10 +1,11 @@
 package com.example.androidstudy.model.data
 
+import com.example.androidstudy.api.RetrofitBuilder
 import com.example.androidstudy.api.data.TotalModel
 import retrofit2.Call
 
 object NaverDataRepositoryImpl : INaverDataRepository{
     override fun callAPiNaverSearch(type: String, query: String): Call<TotalModel> {
-        
+        return RetrofitBuilder.instance().requestSearchForNaver(type, query)
     }
 }
