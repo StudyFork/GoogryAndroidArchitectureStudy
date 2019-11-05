c9bf0fda62fc9a3f902de4e48ffb093ed562dde2
khm <spingferry@gmail.com>
2019-11-05 18:37:36 +0900

    HW-2 repo 함수api 호출 응답 작성

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/repository/INaverDataRepository.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/repository/INaverDataRepository.kt
@@ -1,12 +1,13 @@
 package com.example.androidstudy.model.repository
 
 import com.example.androidstudy.R
+import com.example.androidstudy.api.data.TotalModel
 
 interface INaverDataRepository {
     fun getNaverSearchData(
         type: String,
         query: String,
-        success: () -> Unit,
+        success: (result : TotalModel) -> Unit,
         fail: (msg: String) -> Unit
     )
 }
--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/repository/INaverDataRepositoryImpl.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/model/repository/INaverDataRepositoryImpl.kt
@@ -1,13 +1,29 @@
 package com.example.androidstudy.model.repository
 
+import com.example.androidstudy.api.data.TotalModel
+import com.example.androidstudy.model.data.NaverDataImpl
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 object INaverDataRepositoryImpl : INaverDataRepository{
     override fun getNaverSearchData(
         type: String,
         query: String,
-        success: () -> Unit,
+        success: (result : TotalModel) -> Unit,
         fail: (msg: String) -> Unit
     ) {
-        
+        NaverDataImpl.callAPiNaverSearch(type, query).enqueue(object : Callback<TotalModel> {
+            override fun onFailure(call: Call<TotalModel>, t: Throwable) {
+                t.message?.let{
+                    fail(it)    //message 에서 ?하면 it이 message 인가?? 헷갈림..
+                }
+            }
+            override fun onResponse(call: Call<TotalModel>, response: Response<TotalModel>) {
+                response.body()?.let{
+                    success(it)
+                }
+            }
+        })
     }
-
 }
