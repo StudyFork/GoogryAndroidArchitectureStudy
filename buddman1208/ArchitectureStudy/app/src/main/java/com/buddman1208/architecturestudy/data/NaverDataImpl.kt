e812d1740f5b963bf80139c83f6679402000c9c6
Junseok Oh <kotohana5706@edcan.kr>
2019-11-09 16:51:40 +0900

    [HW-2 / Data] NaverDataImpl <- NaverData를 상속, 내부 구현

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/data/NaverDataImpl.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/data/NaverDataImpl.kt
@@ -1,3 +1,13 @@
 package com.buddman1208.architecturestudy.data
 
-object NaverDataImpl
+import com.buddman1208.architecturestudy.models.CommonResponse
+import com.buddman1208.architecturestudy.utils.NetworkManager
+import io.reactivex.Single
+
+object NaverDataImpl : NaverData {
+
+    override fun searchByType(searchType: String, query: String): Single<CommonResponse> {
+        return NetworkManager.searchByType(searchType, query)
+    }
+
+}
