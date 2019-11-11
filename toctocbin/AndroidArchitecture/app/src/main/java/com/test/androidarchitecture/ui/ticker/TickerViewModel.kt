180bb2eb2c92ec4e4ea966e622856e2f397c1934
toctocbin <haanbin@naver.com>
2019-11-11 12:33:06 +0900

    HW7 TickerViewModel val로 변경

--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/ticker/TickerViewModel.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/ticker/TickerViewModel.kt
@@ -16,8 +16,8 @@ import java.util.*
 class TickerViewModel(private val marketSearch: String) : BaseViewModel() {
 
     private val upbitRepository = UpbitRepository
-    private var _tickerList = MutableLiveData<List<TickerFormat>>()
-    var tickerList : LiveData<List<TickerFormat>> = _tickerList
+    private val _tickerList = MutableLiveData<List<TickerFormat>>()
+    val tickerList : LiveData<List<TickerFormat>> = _tickerList
 
     init {
         getTicker()
