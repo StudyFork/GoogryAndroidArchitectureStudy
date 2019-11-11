c4e246145f167ec123351a515d5334072c12ea47
toctocbin <haanbin@naver.com>
2019-11-11 12:34:11 +0900

    HW7 MarketViewModel val로 변경

--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/market/MarketViewModel.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/ui/market/MarketViewModel.kt
@@ -11,7 +11,7 @@ import io.reactivex.rxkotlin.addTo
 class MarketViewModel : BaseViewModel() {
 
     private val upbitRepository = UpbitRepository
-    private var _marketTitle = MutableLiveData<List<MarketTitle>>()
+    private val _marketTitle = MutableLiveData<List<MarketTitle>>()
     var marketTitle: LiveData<List<MarketTitle>> = _marketTitle
 
     init {
