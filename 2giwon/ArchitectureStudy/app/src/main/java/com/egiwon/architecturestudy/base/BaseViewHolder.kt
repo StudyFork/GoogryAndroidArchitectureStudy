66e7b3d5bacd91e498798c9e60a771c25cf21317
middlefgr <2giwon@gmail.com>
2019-11-12 21:04:46 +0900

    HW-3 BaseViewHolder 추가

---
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BaseViewHolder.kt
@@ -0,0 +1,21 @@
+package com.egiwon.architecturestudy.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.rv_contents_item.view.*
+
+abstract class BaseViewHolder(
+    parent: ViewGroup,
+    @LayoutRes
+    private val resourceId: Int
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(resourceId, parent, false)
+) {
+    val tvTitle: TextView = itemView.tv_title
+    val tvDescription: TextView = itemView.tv_description
+    var linkUrl: String? = null
+
+}
