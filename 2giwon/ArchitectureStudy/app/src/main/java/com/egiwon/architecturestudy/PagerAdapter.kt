0c50d84b3c3a79208c714ef6c3c0ce58432d6057
middlefgr <2giwon@gmail.com>
2019-11-12 21:08:43 +0900

    HW-3 tab 종류별 enum값으로 fragment를 생성하도록 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/PagerAdapter.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/PagerAdapter.kt
@@ -5,18 +5,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.egiwon.architecturestudy.tabs.ContentsFragment
+import com.egiwon.architecturestudy.tabs.Tab
 
-class PagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+class PagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(
+    fm,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+) {
 
     override fun getItem(
         @IntRange(from = 0, to = 3)
         position: Int
     ): Fragment =
         when (position) {
-            0 -> ContentsFragment.newInstance("blog")
-            1 -> ContentsFragment.newInstance("news")
-            2 -> ContentsFragment.newInstance("movie")
-            3 -> ContentsFragment.newInstance("book")
+            0 -> ContentsFragment.newInstance(Tab.BLOG)
+            1 -> ContentsFragment.newInstance(Tab.NEWS)
+            2 -> ContentsFragment.newInstance(Tab.MOVIE)
+            3 -> ContentsFragment.newInstance(Tab.BOOK)
             else -> throw IllegalArgumentException()
         }
 
