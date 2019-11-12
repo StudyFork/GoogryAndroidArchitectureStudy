bdddbe699acb45844b441d89c09a872ced16ec61
middlefgr <2giwon@gmail.com>
2019-11-12 21:25:03 +0900

    HW-3 viewType 값으로 enum과 비교하여 viewHolder를 나누어서 만들도록 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsAdapter.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsAdapter.kt
@@ -2,33 +2,46 @@ package com.egiwon.architecturestudy.tabs
 
 import android.content.Intent
 import android.net.Uri
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.egiwon.architecturestudy.R
+import com.egiwon.architecturestudy.base.BaseViewHolder
 import com.egiwon.architecturestudy.data.Content
 import com.egiwon.architecturestudy.ext.fromHtml
-import kotlinx.android.synthetic.main.rv_contents_item.view.*
+import com.egiwon.architecturestudy.ext.loadAsync
 
 
 class ContentsAdapter(
-    private val type: String
-) : RecyclerView.Adapter<ContentsAdapter.ViewHolder>() {
+    private val tab: Tab
+) : RecyclerView.Adapter<BaseViewHolder>() {
 
     private val list = ArrayList<Content.Item>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
+        when (viewType) {
+            Tab.BLOG.ordinal, Tab.NEWS.ordinal -> TextViewHolder(parent)
+            Tab.MOVIE.ordinal, Tab.BOOK.ordinal -> ImageViewHolder(parent)
+            else -> throw IllegalArgumentException()
+        }.also { viewHolder ->
+            viewHolder.itemView.setOnClickListener {
+                viewHolder.linkUrl?.let { url ->
+                    startActivity(
+                        it.context,
+                        Intent(
+                            Intent.ACTION_VIEW, Uri.parse(url)
+                        ),
+                        null
+                    )
+                }
+            }
+        }
+
 
     override fun getItemCount(): Int = list.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         with(holder) {
 
             tvTitle.text =
@@ -41,20 +54,20 @@ class ContentsAdapter(
 
             linkUrl = list[position].link
 
-            when (type) {
-                "blog", "news" -> holder.imageThumbnail.visibility = View.GONE
-                "movie", "book" -> {
-                    holder.imageThumbnail.visibility = View.VISIBLE
-
-                    Glide.with(holder.itemView.context)
-                        .load(list[position].image)
-                        .apply(RequestOptions.placeholderOf(R.mipmap.ic_launcher))
-                        .into(holder.imageThumbnail)
-                }
-                else -> throw IllegalArgumentException()
+            (holder as? ImageViewHolder)?.run {
+                loadImage(holder, position)
             }
-
         }
+    }
+
+    private fun loadImage(
+        holder: ImageViewHolder,
+        position: Int
+    ) {
+        holder.imageThumbnail.loadAsync(
+            list[position].image,
+            RequestOptions.placeholderOf(R.mipmap.ic_launcher)
+        )
     }
 
     fun setList(items: List<Content.Item>) {
@@ -65,31 +78,5 @@ class ContentsAdapter(
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(
-        parent: ViewGroup
-    ) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-    ) {
-        val tvTitle: TextView = itemView.tv_title
-        val tvDescription: TextView = itemView.tv_description
-        var linkUrl: String? = null
-        val imageThumbnail: ImageView = itemView.iv_thumbnail
-
-        init {
-            itemView.setOnClickListener {
-
-                startActivity(
-                    itemView.context,
-                    Intent(
-                        Intent.ACTION_VIEW, Uri.parse(linkUrl)
-                    ),
-                    null
-                )
-            }
-        }
-    }
-
-    companion object {
-        private const val layoutRes = R.layout.rv_contents_item
-    }
+    override fun getItemViewType(position: Int): Int = tab.ordinal
 }
