54da23f724bfab950bbe758d0f6536de554476e7
Jason <ironelder@gmail.com>
2019-11-16 17:50:22 +0900

    HW-3-MainActivity 잘못된 적용 원복
    HW-3-MainActivity 잘못된 적용 원복

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/mainview/MainActivity.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/mainview/MainActivity.kt
@@ -11,7 +11,7 @@ import com.ironelder.androidarchitecture.view.baseview.BaseContract
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), BaseContract.View {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
