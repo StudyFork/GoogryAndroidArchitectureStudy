7e3ca020098ba93fdb3aa850d0e527eedc6eecfa
middlefgr <2giwon@gmail.com>
2019-11-23 16:39:07 +0900

    HW-4 ContentViewHolder 생성, Image, TextViewHolder가 상속 하도록 수정

---
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentViewHolder.kt
@@ -0,0 +1,47 @@
+package com.egiwon.architecturestudy.tabs
+
+import android.content.Intent
+import android.net.Uri
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import com.egiwon.architecturestudy.base.BaseViewHolder
+import com.egiwon.architecturestudy.data.Content
+import com.egiwon.architecturestudy.ext.fromHtml
+import kotlinx.android.synthetic.main.rv_contents_item.view.*
+
+open class ContentViewHolder(
+    parent: ViewGroup,
+    @LayoutRes
+    private val resourceId: Int
+) : BaseViewHolder<Content.Item>(
+    parent,
+    resourceId
+) {
+    private val tvTitle: TextView = itemView.tv_title
+    private val tvDescription: TextView = itemView.tv_description
+    private var linkUrl: String? = null
+
+    override fun bind(item: Content.Item) {
+        with(item) {
+            tvDescription.text = (actor + description).fromHtml()
+            tvTitle.text = title.fromHtml()
+            linkUrl = link
+        }
+    }
+
+    init {
+        itemView.setOnClickListener {
+            linkUrl?.let { url ->
+                ContextCompat.startActivity(
+                    it.context,
+                    Intent(
+                        Intent.ACTION_VIEW, Uri.parse(url)
+                    ),
+                    null
+                )
+            }
+        }
+    }
+}
