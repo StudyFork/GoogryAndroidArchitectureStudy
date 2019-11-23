12f23afb2eed9d78187a1fb540e77535d6e00858
middlefgr <2giwon@gmail.com>
2019-11-23 16:36:06 +0900

    HW-4 BaseContentViewHolder 제거, BaseViewHolder 생성

---
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BaseViewHolder.kt
@@ -0,0 +1,16 @@
+package com.egiwon.architecturestudy.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder<T>(
+    parent: ViewGroup,
+    @LayoutRes
+    private val resourceId: Int
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(resourceId, parent, false)
+) {
+    abstract fun bind(item: T)
+}
