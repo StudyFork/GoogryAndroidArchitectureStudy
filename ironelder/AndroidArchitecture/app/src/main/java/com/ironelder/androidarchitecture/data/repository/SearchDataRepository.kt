f233e6d42e6d0f5e78575e558f4435ab377294a9
Jason <ironelder@gmail.com>
2019-11-26 21:07:30 +0900

    HW-3.1-Repository에 rx Single추가
    HW-3.1-Repository에 rx Single추가

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/data/repository/SearchDataRepository.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/data/repository/SearchDataRepository.kt
@@ -1,8 +1,10 @@
 package com.ironelder.androidarchitecture.data.repository
 
 import com.ironelder.androidarchitecture.data.TotalModel
+import io.reactivex.Single
 import retrofit2.Call
 
 interface SearchDataRepository {
     fun getDataForSearch(type: String, query: String): Call<TotalModel>
+    fun getDataForSearchWithAdapter(type: String, query: String): Single<TotalModel>
 }
