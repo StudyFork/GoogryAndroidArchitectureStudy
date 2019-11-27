31018c596fc9da797cb4c4ac9a519a8826f2b0f8
이용준 <wooooooak@gmail.com>
2019-11-27 11:22:30 +0900

    안드로이드 프로젝트 생성 & gitignore 수정

---
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package wooooooak.com.studyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
