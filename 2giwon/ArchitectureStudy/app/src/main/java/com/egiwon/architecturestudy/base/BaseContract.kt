c591378919501a461cbd29de44ac3cc6cdadb764
middlefgr <2giwon@gmail.com>
2019-11-29 00:46:03 +0900

    HW-4 Dispose처리를 BasePresenter에서 하도록 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BaseContract.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BaseContract.kt
@@ -1,6 +1,12 @@
 package com.egiwon.architecturestudy.base
 
+import io.reactivex.disposables.CompositeDisposable
+
 interface BaseContract {
-    interface View<T>
-    interface Presenter
+    interface View
+    interface Presenter {
+        fun clearDisposable()
+        fun disposeDisposable()
+        val compositeDisposable: CompositeDisposable
+    }
 }
--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsContract.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentsContract.kt
@@ -1,11 +1,10 @@
 package com.egiwon.architecturestudy.tabs
 
 import com.egiwon.architecturestudy.base.BaseContract
-import com.egiwon.architecturestudy.base.BasePresenter
 import com.egiwon.architecturestudy.data.Content
 
-interface ContentsContract {
-    interface View : BaseContract.View<BasePresenter> {
+interface ContentsContract : BaseContract {
+    interface View : BaseContract.View {
         fun onUpdateUi(resultList: List<Content.Item>)
         fun onFail(throwable: Throwable)
     }
