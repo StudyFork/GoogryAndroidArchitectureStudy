ffbfefd3fda7aa57bb840d0b8c72136bfac0fe4c
middlefgr <2giwon@gmail.com>
2019-11-28 22:18:50 +0900

    HW-3-2 Dispose 처리 추가 및 compositeDisposable 추가

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BasePresenter.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BasePresenter.kt
@@ -1,3 +1,22 @@
 package com.egiwon.architecturestudy.base
 
-abstract class BasePresenter : BaseContract.Presenter
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BasePresenter : BaseContract.Presenter {
+    private val compositeDisposable = CompositeDisposable()
+
+    protected fun clearDisposable() {
+        compositeDisposable.clear()
+    }
+
+    protected fun disposeDisposable() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+    }
+
+    protected fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+}
