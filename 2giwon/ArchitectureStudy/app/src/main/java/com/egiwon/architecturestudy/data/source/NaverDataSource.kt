2acbe0c024bf48b30c5d4d3bf4c80cc3cc79d50e
middlefgr <2giwon@gmail.com>
2019-11-28 22:21:20 +0900

    HW-4 Disposable 넘기도록 수정 및 remote처리 rxJava로 변경

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/NaverDataSource.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/NaverDataSource.kt
@@ -1,13 +1,15 @@
 package com.egiwon.architecturestudy.data.source
 
 import com.egiwon.architecturestudy.data.Content
+import io.reactivex.disposables.Disposable
+
 
 interface NaverDataSource {
 
     fun getContents(
         type: String,
         query: String,
-        onSuccess: (resultList: List<Content.Item>) -> Unit,
-        onFailure: (throwable: Throwable) -> Unit
-    )
+        onSuccess: (List<Content.Item>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ): Disposable
 }
--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/remote/NaverRemoteDataSource.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/remote/NaverRemoteDataSource.kt
@@ -1,57 +1,31 @@
 package com.egiwon.architecturestudy.data.source.remote
 
-import android.util.Log
-import com.egiwon.architecturestudy.BuildConfig
 import com.egiwon.architecturestudy.data.Content
 import com.egiwon.architecturestudy.data.source.NaverDataSource
 import com.egiwon.architecturestudy.data.source.service.RetrofitApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class NaverRemoteDataSource : NaverDataSource {
 
     override fun getContents(
         type: String,
         query: String,
-        onSuccess: (resultList: List<Content.Item>) -> Unit,
-        onFailure: (throwable: Throwable) -> Unit
-    ) {
+        onSuccess: (List<Content.Item>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ): Disposable =
         RetrofitApi.retrofit.getContentsInfo(
             type = type,
             query = query
-        ).enqueue(object : Callback<Content> {
-
-            override fun onFailure(call: Call<Content>, t: Throwable) {
-                t.let {
-                    if (BuildConfig.DEBUG) {
-                        Log.d("RetroFit", "onFailure ${it.message}")
-                    }
-
+        ).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                onSuccess(it.items)
+            }, {
                     onFailure(it)
-                }
-            }
+            })
 
-            override fun onResponse(
-                call: Call<Content>,
-                response: Response<Content>
-            ) {
-                if (response.isSuccessful) {
-                    if (response.body() != null) {
-                        onSuccess(
-                            response.body()!!.items
-                        )
-
-                    } else {
-                        onFailure(Throwable())
-                    }
-
-                } else {
-                    onFailure(Throwable())
-                }
-            }
-        })
-    }
 
     companion object {
         private var instance: NaverRemoteDataSource? = null
