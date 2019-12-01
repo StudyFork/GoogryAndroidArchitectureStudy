46d0a9bc69c40f6ccab77f49134da56982612aba
이용준 <wooooooak@gmail.com>
2019-12-01 12:45:50 +0900

    HW-1 Naver api 요청 인터페이스 정의

---
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/model/request/NaverRequestApi.kt
@@ -0,0 +1,39 @@
+package wooooooak.com.studyapp.model.request
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import wooooooak.com.studyapp.model.response.blog.BlogList
+import wooooooak.com.studyapp.model.response.image.ImageList
+import wooooooak.com.studyapp.model.response.kin.KinList
+import wooooooak.com.studyapp.model.response.movie.MovieList
+
+interface NaverRequestApi {
+
+    @GET("movie.json")
+    suspend fun getMovies(
+        @Query("query") query: String,
+        @Query("display") display: Int?,
+        @Query("start") start: Int?
+    ): MovieList
+
+    @GET("blog.json")
+    suspend fun getBlogs(
+        @Query("query") query: String,
+        @Query("display") display: Int?,
+        @Query("start") start: Int?
+    ): BlogList
+
+    @GET("image.json")
+    suspend fun getImages(
+        @Query("query") query: String,
+        @Query("display") display: Int?,
+        @Query("start") start: Int?
+    ): ImageList
+
+    @GET("kin.json")
+    suspend fun getKins(
+        @Query("query") query: String,
+        @Query("display") display: Int?,
+        @Query("start") start: Int?
+    ): KinList
+}
