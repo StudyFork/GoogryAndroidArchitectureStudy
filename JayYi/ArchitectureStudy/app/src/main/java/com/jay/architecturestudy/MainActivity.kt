1b54c0967643a315eaa98384ae1df5478c511ceb
lee.minjae <lee.minjae@nbt.com>
2019-12-01 22:24:38 +0900

    HW-1 MainActivity UI 변경 (BottomNavigation 추가)

--- JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/MainActivity.kt
+++ JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/MainActivity.kt
@@ -1,24 +1,52 @@
 package com.jay.architecturestudy
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val pagerAdapter = PagerAdapter(supportFragmentManager)
+
+    private val navigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_movie -> {
+                    setPageWithIndex(0)
+                    true
+                }
+                R.id.menu_image -> {
+                    setPageWithIndex(1)
+                    true
+                }
+                R.id.menu_blog -> {
+                    setPageWithIndex(2)
+                    true
+                }
+                R.id.menu_kin -> {
+                    setPageWithIndex(3)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        bottom_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
+        view_pager.adapter = pagerAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +63,22 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setPageWithIndex(index: Int) {
+        view_pager?.currentItem = index
+    }
+
+    inner class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        private val fragments = arrayListOf<Fragment>()
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
     }
 }
