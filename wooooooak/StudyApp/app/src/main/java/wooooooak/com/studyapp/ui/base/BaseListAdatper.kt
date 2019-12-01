61f2a7054c1b3f3b9225521e6d510ec0c179efb5
이용준 <wooooooak@gmail.com>
2019-12-01 17:42:31 +0900

    HW-1 바텀 아이콘 변경

---
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/base/BaseListAdatper.kt
@@ -0,0 +1,18 @@
+//package wooooooak.com.studyapp.ui.base
+//
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.DiffUtil
+//import androidx.recyclerview.widget.ListAdapter
+//import androidx.recyclerview.widget.RecyclerView
+//
+//abstract class BaseListAdatper<T, H : RecyclerView.ViewHolder>(
+//    private val layoutId: Int,
+//    diffCallback: DiffUtil
+//    .ItemCallback<T>
+//) :
+//    ListAdapter<T, H>(diffCallback) {
+////    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): H =
+//
+//    protected abstract fun
+//
+//}
