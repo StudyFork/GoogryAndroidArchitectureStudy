afd89a9888ae542119b89b43673af801425e0f3a
이용준 <wooooooak@gmail.com>
2019-12-01 18:51:57 +0900

    HW-1 MovieFragment 리팩토링

--- wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/movie/MovieFragment.kt
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/movie/MovieFragment.kt
@@ -1,41 +1,12 @@
 package wooooooak.com.studyapp.ui.movie
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.textfield.TextInputEditText
-import kotlinx.android.synthetic.main.fragment_movie.view.*
 import wooooooak.com.studyapp.R
+import wooooooak.com.studyapp.ui.base.BaseSearchFragment
 
-class MovieFragment : Fragment() {
+class MovieFragment(override val layoutId: Int = R.layout.fragment_movie) : BaseSearchFragment() {
 
-    private lateinit var movieListView: RecyclerView
-    private lateinit var searchButton: Button
-    private lateinit var inputTextView: TextInputEditText
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_movie, container, false)
-        movieListView = view.list_view
-        movieListView.adapter = MovieListAdapter(this.requireActivity())
-        searchButton = view.search_button
-        inputTextView = view.edit_text
-
-        searchButton.setOnClickListener {
-            inputTextView.text?.let { text ->
-                if (text.isNotEmpty()) {
-                    (movieListView.adapter as MovieListAdapter).searchByTitle(text.toString())
-                }
-            }
-        }
-
-        return view
+    override fun setListViewAdapter(listView: RecyclerView) {
+        listView.adapter = MovieListAdapter(requireActivity())
     }
-
 }
