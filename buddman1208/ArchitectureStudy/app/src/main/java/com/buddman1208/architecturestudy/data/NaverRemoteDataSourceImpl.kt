5a6e8c1d7987371359b2a0c78ddea87093859ac4
Junseok Oh <kotohana5706@edcan.kr>
2019-12-01 13:25:29 +0900

    [HW-2] NaverData -> NaverRemoveDataSource 이름 변경 (상속 오류 해결)

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/data/NaverRemoteDataSourceImpl.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/data/NaverRemoteDataSourceImpl.kt
@@ -4,7 +4,7 @@ import com.buddman1208.architecturestudy.models.CommonResponse
 import com.buddman1208.architecturestudy.utils.NetworkManager
 import io.reactivex.Single
 
-object NaverRemoteDataSourceImpl : NaverData {
+object NaverRemoteDataSourceImpl : NaverRemoteDataSource {
 
     override fun searchByType(searchType: String, query: String): Single<CommonResponse> {
         return NetworkManager.searchByType(searchType, query)
