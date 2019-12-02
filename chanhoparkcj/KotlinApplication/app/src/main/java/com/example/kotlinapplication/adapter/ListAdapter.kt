4faf8326e44b64e013b574ef1140ac33c3e2cf48
chpark <chpark@sysofting.co.kr>
2019-12-03 00:10:53 +0900

    HW-1 ListAdapter 수정

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/ListAdapter.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/ListAdapter.kt
@@ -1,4 +1,32 @@
 package com.example.kotlinapplication.adapter
 
-class listAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinapplication.R
+import com.example.kotlinapplication.adapter.viewholder.ListViewHolder
+import com.example.kotlinapplication.model.MovieItems
+
+class ListAdapter(val itemListener: ItemListener,val items:List<MovieItems>,val context: Context) :RecyclerView.Adapter<ListViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(LayoutInflater.from(context).inflate(R.layout.movie_list_item,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(items.get(position),itemListener,position)
+    }
+
+    interface ItemListener{
+        fun onItemClick(movieItems: MovieItems)
+    }
+
+
 }
