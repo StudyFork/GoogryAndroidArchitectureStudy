273ca9fcc5667797d7a0c9b127535fdc864a07d1
chpark <chpark@sysofting.co.kr>
2019-12-02 10:22:46 +0900

    HW-1 network 수정

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/network/RetrofitClient.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class SearchRetrofit{
+class RetrofitClient{
     companion object{
        /* val clientId :String = "o4YDMUoQQ3B5C1aJtX2g"
         val clientScret :String = "FrW3bUTwCa"*/
--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/network/RetrofitService.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/network/RetrofitService.kt
@@ -1,4 +1,16 @@
 package com.example.kotlinapplication.network
 
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
 interface RetrofitService {
+    /*val clientId :String = "o4YDMUoQQ3B5C1aJtX2g"
+    val clientScret :String = "FrW3bUTwCa"*/
+//    @Headers({
+//        "X-Naver-Client-Id:o4YDMUoQQ3B5C1aJtX2g",
+//
+//    })
+//    @GET("shop")
+
+
 }
