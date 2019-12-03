3f60321c21ea9a2a9b409526609ea8bad9d3637d
chpark <chpark@sysofting.co.kr>
2019-12-03 22:14:22 +0900

    HW-1 ListNaverAdapter 와 ListViewHolder 생성 및 작성

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/ListNaverAdapter.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/ListNaverAdapter.kt
@@ -8,12 +8,19 @@ import com.example.kotlinapplication.R
 import com.example.kotlinapplication.adapter.viewholder.ListViewHolder
 import com.example.kotlinapplication.model.MovieItems
 
-class ListAdapter(val items:List<MovieItems>,val context: Context) :RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ListNaverAdapter(val items: List<MovieItems>, val context: Context?) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private var mListener : ItemListener?=null
+    private var mListener: ItemListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        return ListViewHolder(LayoutInflater.from(context).inflate(R.layout.movie_list_item,parent,false))
+        return ListViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.movie_list_item,
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
@@ -22,16 +29,16 @@ class ListAdapter(val items:List<MovieItems>,val context: Context) :RecyclerView
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(holder is ListViewHolder){
-            holder.bind(items.get(position),mListener,position)
+        if (holder is ListViewHolder) {
+            holder.bind(items.get(position), mListener)
         }
     }
 
-    fun setListener(listener: ItemListener?){
+    fun setListener(listener: ItemListener?) {
         mListener = listener
     }
 
-    interface ItemListener{
+    interface ItemListener {
         fun onItemClick(movieItems: MovieItems)
     }
 
--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/viewholder/ListViewHolder.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/adapter/viewholder/ListViewHolder.kt
@@ -1,29 +1,58 @@
 package com.example.kotlinapplication.adapter.viewholder
 
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
+import android.text.Html
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.kotlinapplication.R
-import com.example.kotlinapplication.adapter.ListAdapter
+import com.example.kotlinapplication.adapter.ListNaverAdapter
 import com.example.kotlinapplication.model.MovieItems
-import kotlinx.android.synthetic.main.movie_list_item.view.*
+import com.squareup.picasso.Picasso
+import java.net.URL
 
-class ListViewHolder(view: View):RecyclerView.ViewHolder(view) {
-    val imageView:ImageView = view.findViewById(R.id.movie_item_image)
-    val titleView:TextView=view.findViewById(R.id.movie_item_title)
-    val ratingView:RatingBar=view.findViewById(R.id.movie_item_user_rating)
-    val pubDateView:TextView=view.findViewById(R.id.movie_item_pubDate)
-    val directorView:TextView=view.findViewById(R.id.movie_item_director)
-    val actorView:TextView=view.findViewById(R.id.movie_item_actor)
+class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val mView: View = view
+    private val imageView: ImageView = view.findViewById(R.id.movie_item_image)
+    private val titleView: TextView = view.findViewById(R.id.movie_item_title)
+    private val ratingView: RatingBar = view.findViewById(R.id.movie_item_user_rating)
+    private val pubDateView: TextView = view.findViewById(R.id.movie_item_pubDate)
+    private val directorView: TextView = view.findViewById(R.id.movie_item_director)
+    private val actorView: TextView = view.findViewById(R.id.movie_item_actor)
+    private var mItemLisener: ListNaverAdapter.ItemListener? = null
+    private val movieLayout: LinearLayout = view.findViewById(R.id.movie_item_layout)
 
-    fun bind(item:MovieItems,listener:ListAdapter.ItemListener,position:Int){
-        imageView!!.setImageURI(Uri.parse(item!!.image))
-        titleView!!.setText(item!!.title)
-        pubDateView!!.setText(item!!.pubDate)
-        directorView!!.setText(item!!.director)
-        actorView!!.setText(item!!.actor)
+    fun bind(item: MovieItems, listener: ListNaverAdapter.ItemListener?) {
+        mItemLisener = listener
+
+        if (!item.image.equals("")) {
+            Picasso.get()
+                .load(item.image)
+                .into(imageView)
+        }
+        ratingView.rating = item.userRating.toFloat()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            titleView.text = Html.fromHtml(item.title, 0)
+            pubDateView.text = Html.fromHtml(item.pubDate, 0)
+            directorView.text = Html.fromHtml(item.director, 0)
+            actorView.text = Html.fromHtml(item.actor, 0)
+        } else {
+            titleView.text = item.title
+            pubDateView.text = item.pubDate
+            directorView.text = item.director
+            actorView.text = item.actor
+        }
+        movieLayout.setOnClickListener({
+            mItemLisener!!.onItemClick(item)
+        })
+
+
     }
 }
