60781c6dca7b894aa7ae4dffbd9404df1dfa50c8
chpark <chpark@sysofting.co.kr>
2019-12-03 22:15:20 +0900

    HW-1 HomeFragment 수정

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/home/HomeFragment.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinapplication.home
 
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,37 +11,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListAdapter
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.example.kotlinapplication.R
+import com.example.kotlinapplication.adapter.ListNaverAdapter
+import com.example.kotlinapplication.model.MovieItems
 import com.example.kotlinapplication.model.ResponseItems
-import com.example.kotlinapplication.network.RetrofitClient
 import com.example.kotlinapplication.network.RetrofitClient.client
 import com.example.kotlinapplication.network.RetrofitService
-import io.reactivex.Scheduler
-import io.reactivex.Single
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.Schedulers.io
+import java.net.URI
+import java.util.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), ListNaverAdapter.ItemListener {
     private var mHomeSearchBtn: Button? = null
     private var mHomeSearchEdit: EditText? = null
     private var mHomeRecyclerView: RecyclerView? = null
-    private var mHomeTypeBtn: Button? = null
     private var mService: RetrofitService? = null
     private var mResult: ResponseItems? = null
+    private var mAdapter: ListNaverAdapter? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -48,10 +48,7 @@ class HomeFragment : Fragment() {
         mHomeSearchBtn = view.findViewById(R.id.home_search_btn)
         mHomeRecyclerView = view.findViewById(R.id.home_recyclerview)
         mHomeSearchEdit = view.findViewById(R.id.home_search_edit)
-        mHomeTypeBtn = view.findViewById(R.id.home_type_btn)
         mService = client!!.create(RetrofitService::class.java)
-
-
         start()
     }
 
@@ -83,10 +80,25 @@ class HomeFragment : Fragment() {
 
     private fun handleResponse(res: ResponseItems) {
         mResult = res
-
+        Log.e("test", res.items.toString())
+        mAdapter = ListNaverAdapter(res.items, context)
+        mAdapter!!.setListener(this)
+        mHomeRecyclerView!!.layoutManager = LinearLayoutManager(
+            activity, RecyclerView.VERTICAL, false
+        )
+        mHomeRecyclerView!!.adapter = mAdapter
     }
+
 
     private fun HandleError(error: Throwable) {
         Log.d(TAG, "Error ${error.localizedMessage}")
+    }
+
+    override fun onItemClick(movieItems: MovieItems) {
+        Toast.makeText(activity, movieItems.link, Toast.LENGTH_SHORT).show()
+        val uri: Uri = Uri.parse(movieItems.link)
+        val intent: Intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+
     }
 }
