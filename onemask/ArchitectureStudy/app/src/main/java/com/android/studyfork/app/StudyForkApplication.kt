8c83aff11fd786bf727ec2bd9acf7cb7736f31ca
김수연 <onemask@naver.com>
2019-12-05 19:41:01 +0900

    Add DaggerApplication에 Dagger Setting

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/app/StudyForkApplication.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/app/StudyForkApplication.kt
@@ -1,16 +1,40 @@
 package com.android.studyfork.app
 
 import android.app.Application
+import androidx.fragment.app.Fragment
+import com.android.studyfork.dagger.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import timber.log.Timber
+import javax.inject.Inject
 
-class StudyForkApplication : Application(){
+
+class StudyForkApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
+        setupDagger()
         setupTimber()
     }
 
-    private fun setupTimber(){
+    private fun setupDagger() {
+        DaggerAppComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
+    }
+
+    private fun setupTimber() {
         Timber.plant(Timber.DebugTree())
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = activityInjector
+
+
+
 }
