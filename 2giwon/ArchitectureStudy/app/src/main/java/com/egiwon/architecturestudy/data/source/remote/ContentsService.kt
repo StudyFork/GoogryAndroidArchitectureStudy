e38db608991600ccbe6ef9a805e97c227906b0ad
middlefgr <2giwon@gmail.com>
2019-12-06 01:54:25 +0900

    HW-4 버전 값 ContentsService 적용

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/remote/ContentsService.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/remote/ContentsService.kt
@@ -1,17 +1,17 @@
 package com.egiwon.architecturestudy.data.source.remote
 
-import com.egiwon.architecturestudy.data.Content
+import com.egiwon.architecturestudy.data.source.remote.response.ContentResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ContentsService {
-    @GET("search/{type}.json")
+    @GET("v1/search/{type}.json")
     fun getContentsInfo(
         @Path("type")
         type: String,
         @Query("query")
         query: String
-    ): Single<Content>
+    ): Single<ContentResponse>
 }
