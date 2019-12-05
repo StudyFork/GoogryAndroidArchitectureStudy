d09d65b7cf384d7815cee56a11e6ccd8795ce578
soo <onemask@stunitas.com>
2019-12-05 22:10:18 +0900

    [HW-08] TickerViewModelFactory 생성자에 @Inject repository

--- onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/viewmodel/TickerViewModelFactory.kt
+++ onemask/ArchitectureStudy/app/src/main/java/com/android/studyfork/ui/tickerlist/viewmodel/TickerViewModelFactory.kt
@@ -2,9 +2,11 @@ package com.android.studyfork.ui.tickerlist.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.android.studyfork.repository.UpbitRepositoryImpl
+import javax.inject.Inject
 
-class TickerViewModelFactory(private val market : String) : ViewModelProvider.Factory {
+class TickerViewModelFactory @Inject constructor(private val repository: UpbitRepositoryImpl, private val market : String) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return TickerViewModel(market = market) as T
+        return TickerViewModel(repository = repository ,market = market) as T
     }
 }
