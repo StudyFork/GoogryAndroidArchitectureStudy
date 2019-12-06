1cec5acc1cd25f8fa8831b726d79beb6523e553b
dksush0828 <dksush0828@hanmail.net>
2019-12-06 12:02:22 +0900

    HW-1 NetworkUtil 수정

--- dksush/AndroidArchitecture/app/src/main/java/com/example/androidarchitecture/apis/NetworkUtil.kt
+++ dksush/AndroidArchitecture/app/src/main/java/com/example/androidarchitecture/apis/NetworkUtil.kt
@@ -4,82 +4,60 @@ import okhttp3.Dns
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
-import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 import java.net.UnknownHostException
 
-class NetworkUtil {
-
-    companion object {
-
-        private val SAFE_DNS: Dns by lazy {
-            Dns { hostname ->
-                try {
-                    return@Dns Dns.SYSTEM.lookup(hostname)
-                } catch (e: IllegalArgumentException) {
-                    // Hack. See https://github.com/square/okhttp/issues/3345
-                    throw UnknownHostException(e.message)
-                } catch (e: NullPointerException) {
-                    // Hack. See https://github.com/square/okhttp/issues/3345
-                    throw UnknownHostException(e.message)
-                } catch (e: Exception) {
-                    // Hack. See https://github.com/square/okhttp/issues/3345
-                    throw UnknownHostException(e.message)
-                }
+object NetworkUtil {
+    private val SAFE_DNS: Dns by lazy {
+        Dns { hostname ->
+            try {
+                return@Dns Dns.SYSTEM.lookup(hostname)
+            } catch (e: IllegalArgumentException) {
+                // Hack. See https://github.com/square/okhttp/issues/3345
+                throw UnknownHostException(e.message)
+            } catch (e: NullPointerException) {
+                // Hack. See https://github.com/square/okhttp/issues/3345
+                throw UnknownHostException(e.message)
+            } catch (e: Exception) {
+                // Hack. See https://github.com/square/okhttp/issues/3345
+                throw UnknownHostException(e.message)
             }
         }
+    }
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .dns(SAFE_DNS)
+            .addInterceptor(AppInterceptor())
+            .build()
+    }
 
-
-        private val okHttpClient: OkHttpClient by lazy {
-            OkHttpClient.Builder()
-                .dns(SAFE_DNS)
-                .addInterceptor(appInterceptor())
+    fun getRetrofit(baseUrl: String): Retrofit {
+        return try {
+            Retrofit.Builder().client(okHttpClient)
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
+    }
 
-        fun getRetrofit(baseUrl: String, factory: Converter.Factory?): Retrofit {
-            var retrofit: Retrofit
-            try {
-                if (factory == null) {
-                    retrofit = Retrofit.Builder().client(okHttpClient)
-                        .baseUrl(baseUrl)
-                        .build()
-                } else {
-                    retrofit = Retrofit.Builder().client(okHttpClient)
-                        .baseUrl(baseUrl)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                if (factory == null) {
-                    retrofit = Retrofit.Builder()
-                        .baseUrl(baseUrl)
-                        .build()
-                } else {
-                    retrofit = Retrofit.Builder()
-                        .baseUrl(baseUrl)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                }
-            }
-
-            return retrofit
-        }
-
-        // 인터셉터.
-        class appInterceptor : Interceptor {
-            @Throws(IOException::class)
-            override fun intercept(chain: Interceptor.Chain): Response {
-                val mRequest = chain.request()
-                val newmequest = mRequest.newBuilder()
-                    .addHeader("X-Naver-Client-Id", "Cjj07G06ms2sCWlKWezF")
-                    .addHeader("X-Naver-Client-Secret", "4v_hXcCSrz")
-                    .build()
-                return chain.proceed(newmequest)
-            }
+    // 인터셉터.
+    class AppInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val mRequest = chain.request()
+            val newRequest = mRequest.newBuilder()
+                .addHeader("X-Naver-Client-Id", "Cjj07G06ms2sCWlKWezF")
+                .addHeader("X-Naver-Client-Secret", "4v_hXcCSrz")
+                .build()
+            return chain.proceed(newRequest)
         }
     }
 
