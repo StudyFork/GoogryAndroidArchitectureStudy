9cdcd6439ee04e15cd81f94bca45219bc6819cec
jskim <kjs6030@naver.com>
2019-12-08 01:20:52 +0900

    HW-8 Dagger Custom Scope 추가

---
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/di/scope/CoinScope.kt
@@ -0,0 +1,8 @@
+package com.jskim5923.architecturestudy.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class CoinScope
