e23b008ab0c1ec743594434033c7d59e2f825e28
toctocbin <haanbin@naver.com>
2019-12-07 16:49:35 +0900

    HW-8 Application open 제거

--- toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/UpbitApplication.kt
+++ toctocbin/AndroidArchitecture/app/src/main/java/com/test/androidarchitecture/UpbitApplication.kt
@@ -4,7 +4,7 @@ import com.test.androidarchitecture.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
-open class UpbitApplication : DaggerApplication() {
+class UpbitApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
 
         return DaggerAppComponent.factory().create(applicationContext)
