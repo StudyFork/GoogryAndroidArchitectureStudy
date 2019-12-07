cd962198509d8c18a61e0a31d771de3ef155ceb2
bean628 <dudwp628@naver.com>
2019-12-08 00:46:03 +0900

    HW-4 확장함수에 만들어놓은 토스트 보여주는 함수로 변경

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/base/BaseActivity.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/base/BaseActivity.kt
@@ -1,9 +1,9 @@
 package com.practice.achitecture.myproject.base
 
 import android.view.View
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import com.practice.achitecture.myproject.makeToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 abstract class BaseActivity<P : BaseContract.Presenter>(@LayoutRes contentLayoutId: Int) :
@@ -12,11 +12,11 @@ abstract class BaseActivity<P : BaseContract.Presenter>(@LayoutRes contentLayout
     abstract val presenter: P
 
     override fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        this.makeToast(message)
     }
 
     override fun showToast(stringResId: Int) {
-        showToast(this.getString(stringResId))
+        this.makeToast(stringResId)
     }
 
     override fun showLoading() {
