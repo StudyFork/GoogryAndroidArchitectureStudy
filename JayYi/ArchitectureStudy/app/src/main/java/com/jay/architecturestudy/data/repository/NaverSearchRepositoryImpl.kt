3d93fe5c0b1e21ec50a6d7b91e194ca53e165e5d
JayYi <stkment@gmail.com>
2019-12-07 15:03:34 +0900

    HW-1 NaverSearchRepositoryImpl interface 수정

--- JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/data/repository/NaverSearchRepositoryImpl.kt
+++ JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/data/repository/NaverSearchRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package com.jay.architecturestudy.data.repository
 
+import com.jay.architecturestudy.data.model.Blog
+import com.jay.architecturestudy.data.model.Image
+import com.jay.architecturestudy.data.model.Kin
 import com.jay.architecturestudy.data.model.Movie
 import com.jay.architecturestudy.data.source.remote.NaverSearchRemoteDataSource
 import com.jay.architecturestudy.data.source.remote.NaverSearchRemoteDataSourceImpl
 
 class NaverSearchRepositoryImpl: NaverSearchRepository {
-
     override val naverSearchRemoteDataSource: NaverSearchRemoteDataSource by lazy {
         NaverSearchRemoteDataSourceImpl()
     }
@@ -14,15 +16,15 @@ class NaverSearchRepositoryImpl: NaverSearchRepository {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getImage(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getImage(keyword: String, success: (Image) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getBlog(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getBlog(keyword: String, success: (Blog) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getKin(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getKin(keyword: String, success: (Kin) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
