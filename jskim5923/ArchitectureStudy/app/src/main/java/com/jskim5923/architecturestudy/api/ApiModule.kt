f442b40d1a8e96a7c6f76e10c5c9d0c71f3b7a4b
jskim <kjs6030@naver.com>
2019-12-08 01:15:23 +0900

    HW-8 Api module 추가

---
+++ jskim5923/ArchitectureStudy/app/src/main/java/com/jskim5923/architecturestudy/api/ApiModule.kt
@@ -0,0 +1,55 @@
+package com.jskim5923.architecturestudy.api
+
+import com.jskim5923.architecturestudy.BuildConfig
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class ApiModule {
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideRxJava2CallAdapterFactory(): RxJava2CallAdapterFactory =
+        RxJava2CallAdapterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+    @Singleton
+    @Provides
+    fun provideCoinApiService(
+        gsonConverterFactory: GsonConverterFactory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory,
+        okHttpClient: OkHttpClient
+    ): CoinApi =
+        Retrofit.Builder()
+            .baseUrl("https://api.upbit.com/")
+            .addConverterFactory(gsonConverterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
+            .client(okHttpClient)
+            .build()
+            .create(CoinApi::class.java)
+}
