febcfcc0bf999e1d5519ea8a49bc98a4207c2f58
JayYi <stkment@gmail.com>
2019-12-07 15:03:13 +0900

    HW-2 NaverSearchRemoteDataSource interface 수정

--- JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/data/source/remote/NaverSearchRemoteDataSourceImpl.kt
+++ JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/data/source/remote/NaverSearchRemoteDataSourceImpl.kt
@@ -1,5 +1,8 @@
 package com.jay.architecturestudy.data.source.remote
 
+import com.jay.architecturestudy.data.model.Blog
+import com.jay.architecturestudy.data.model.Image
+import com.jay.architecturestudy.data.model.Kin
 import com.jay.architecturestudy.data.model.Movie
 
 class NaverSearchRemoteDataSourceImpl : NaverSearchRemoteDataSource {
@@ -7,15 +10,15 @@ class NaverSearchRemoteDataSourceImpl : NaverSearchRemoteDataSource {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getImage(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getImage(keyword: String, success: (Image) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getBlog(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getBlog(keyword: String, success: (Blog) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getKin(keyword: String, success: (Movie) -> Unit, fail: (Throwable) -> Unit) {
+    override fun getKin(keyword: String, success: (Kin) -> Unit, fail: (Throwable) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
