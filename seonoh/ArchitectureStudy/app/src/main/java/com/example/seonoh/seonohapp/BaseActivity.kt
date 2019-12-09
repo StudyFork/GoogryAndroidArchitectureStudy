de1fffc7493a04a5a9861e6aaf8f7b42b4d01ee5
Seonoh Eam <>
2019-12-09 09:52:17 +0900

    [HW-08] BaseActivity dagger 적용

--- seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/BaseActivity.kt
+++ seonoh/ArchitectureStudy/app/src/main/java/com/example/seonoh/seonohapp/BaseActivity.kt
@@ -6,7 +6,11 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
 import com.example.seonoh.seonohapp.model.BaseViewModel
+import dagger.android.AndroidInjection
+import javax.inject.Inject
+import dagger.android.support.DaggerAppCompatActivity
 
 abstract class BaseActivity<B : ViewDataBinding>(
     @LayoutRes
@@ -18,11 +22,15 @@ abstract class BaseActivity<B : ViewDataBinding>(
     abstract val viewModel: BaseViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutRes)
         binding.lifecycleOwner = this
         toast = Toast.makeText(this, resources.getString(R.string.back_text), Toast.LENGTH_LONG)
     }
+
+//    @Inject
+//    lateinit var viewModelFactory : ViewModelProvider.Factory
 
     private fun showToast() {
         if (toast.view.isShown) {
