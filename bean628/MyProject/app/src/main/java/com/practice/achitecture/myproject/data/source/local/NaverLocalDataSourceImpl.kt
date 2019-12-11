fbed6ef905e29ffca702bc17884a626b428a103a
bean628 <dudwp628@naver.com>
2019-12-11 23:15:27 +0900

    HW-4 동작 없는 함수에 이유 작성.

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/data/source/local/NaverLocalDataSourceImpl.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/data/source/local/NaverLocalDataSourceImpl.kt
@@ -9,7 +9,8 @@ class NaverLocalDataSourceImpl : NaverDataSource {
         word: String,
         callBack: NaverDataSource.GettingResultOfSearchingCallBack
     ) {
-
+        // TODO : 4.1 추가 과제 검색했던 결과 히스토리 (검색할때마다 저장해서 히스토리를 남김)
+        //         할 때 Room 사용과 함께 쓸 계획입니다.
     }
 
 
