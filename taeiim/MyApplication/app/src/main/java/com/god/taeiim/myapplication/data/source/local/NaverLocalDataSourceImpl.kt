8f112f2dddfca87e037177b1cd6954dce4e16d4b
taeiim <taeiim.god@gmail.com>
2019-12-12 20:55:21 +0900

    HW-4 NaverLocalDataSourceImpl 추가

---
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/data/source/local/NaverLocalDataSourceImpl.kt
@@ -0,0 +1,30 @@
+package com.god.taeiim.myapplication.data.source.local
+
+import com.god.taeiim.myapplication.data.SearchHistory
+import com.god.taeiim.myapplication.data.source.NaverDataSource
+
+class NaverLocalDataSourceImpl private constructor(private val searchHistoryDao: SearchHistoryDao) :
+    NaverDataSource.LocalDataSource {
+
+    override fun getLastSearchResultData(searchType: String): SearchHistory =
+        searchHistoryDao.getLastSearchResult(searchType)
+
+
+    override fun saveSearchResult(searchHistory: SearchHistory) =
+        searchHistoryDao.saveSearchHistory(searchHistory)
+
+    companion object {
+        private var INSTANCE: NaverLocalDataSourceImpl? = null
+
+        @JvmStatic
+        fun getInstance(searchHistoryDao: SearchHistoryDao): NaverLocalDataSourceImpl {
+            if (INSTANCE == null) {
+                synchronized(NaverLocalDataSourceImpl::javaClass) {
+                    INSTANCE = NaverLocalDataSourceImpl(searchHistoryDao)
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+
+}
