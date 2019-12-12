e03a71e650ca8562696bc428e1b9e581f5298316
taeiim <taeiim.god@gmail.com>
2019-12-12 20:54:54 +0900

    HW-4 NaverDataSource Local 관련 추가, Remote/Local interface 분리

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/data/source/NaverDataSource.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/data/source/NaverDataSource.kt
@@ -1,12 +1,27 @@
 package com.god.taeiim.myapplication.data.source
 
 import com.god.taeiim.myapplication.api.model.SearchResult
+import com.god.taeiim.myapplication.data.SearchHistory
 
 interface NaverDataSource {
-    fun getResultData(
-        searchType: String,
-        query: String,
-        success: (results: SearchResult) -> Unit,
-        fail: (t: Throwable) -> Unit
-    )
+
+    interface RemoteDataSource {
+
+        fun getResultData(
+            searchType: String,
+            query: String,
+            success: (results: SearchResult) -> Unit,
+            fail: (t: Throwable) -> Unit
+        )
+
+    }
+
+    interface LocalDataSource {
+
+        fun getLastSearchResultData(searchType: String): SearchHistory
+
+        fun saveSearchResult(searchHistory: SearchHistory)
+
+    }
+
 }
