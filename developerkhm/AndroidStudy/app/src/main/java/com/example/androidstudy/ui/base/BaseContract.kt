e32382e43737defe9b17a8d74dac26cd6aa45a36
developerkim <spingferry@gmail.com>
2019-12-16 21:45:38 +0900

    HW-3 BaseFragment Contract interfase 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseContract.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseContract.kt
@@ -1,4 +1,21 @@
 package com.example.androidstudy.ui.base
 
+import com.example.androidstudy.api.data.Item
+
 interface BaseContract {
+
+    interface View {
+        fun onDataChanged(result: ArrayList<Item>)
+        fun showErrorMessage(msg: String?)
+        fun showNoSearchData()
+        fun showLoading()
+        fun hideLoading()
+    }
+
+    interface Presenter {
+        fun search(
+            type: String,
+            query: String?
+        )
+    }
 }
