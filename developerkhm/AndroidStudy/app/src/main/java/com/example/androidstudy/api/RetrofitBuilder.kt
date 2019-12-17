7183bb9dfc71e6fbd2191d173ec9e9b968abf4f7
developerkhm <spingferry@gmail.com>
2019-12-17 11:08:59 +0900

    HW-3 Rxjava2 AdapterFactory 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/api/RetrofitBuilder.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/api/RetrofitBuilder.kt
@@ -2,6 +2,7 @@ package com.example.androidstudy.api
 
 import com.example.androidstudy.api.service.ApiNaverService
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 val BASE_URL = "https://openapi.naver.com/"
@@ -10,6 +11,7 @@ object RetrofitBuilder {
     private var retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
 
     fun instance(): ApiNaverService = retrofit.create(ApiNaverService::class.java)
