5fe551759ea205d51325f770153e3c6dd3ca947b
handnew04 <handnew04@gmail.com>
2019-12-19 00:52:55 +0900

    사전과제-2 패키지명 파일 잘 못 올라간 것들 교체

--- handnew04/app/src/main/java/com/example/handnew04/MainActivity.kt
+++ handnew04/app/src/main/java/com/example/handnew04/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.handnew04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- handnew04/app/src/test/java/com/example/handnew04/ExampleUnitTest.kt
+++ handnew04/app/src/test/java/com/example/handnew04/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.handnew04
 
 import org.junit.Test
 
