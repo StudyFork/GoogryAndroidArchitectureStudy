896e1a68e54521750f67596f0540b2214bec14f9
doubleclutch30 <cometruth30@gmail.com>
2019-12-18 13:49:35 +0900

    생성자로 데이터 넘겨주는 부분 수정

---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/blog/BlogAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.architecturestudy.ui.blog
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.architecturestudy.R
+import com.example.architecturestudy.model.BlogItems
+import com.example.architecturestudy.ui.startWebView
+import kotlinx.android.synthetic.main.item_blog.view.*
+
+
+class BlogAdapter : RecyclerView.Adapter<BlogAdapter.BlogHolder>(){
+
+    private val blogItem : MutableList<BlogItems> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_blog, parent, false)
+        return BlogHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return blogItem.size
+
+    }
+
+    override fun onBindViewHolder(holder: BlogHolder, position: Int) {
+        holder.bind(blogItem[position])
+    }
+
+    fun upDate(blogList : List<BlogItems>) {
+        this.blogItem.clear()
+        this.blogItem.addAll(blogList)
+        notifyDataSetChanged()
+    }
+
+
+    class BlogHolder(view : View) : RecyclerView.ViewHolder(view)  {
+
+        lateinit var item : BlogItems
+
+        init {
+            itemView.setOnClickListener { view ->
+                view.startWebView(item.link)
+            }
+        }
+
+        fun bind(item : BlogItems) {
+            this.item = item
+
+            with(itemView) {
+                blog_title.text = HtmlCompat.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                blog_contents.text = HtmlCompat.fromHtml(item.description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                blog_owner.text = item.bloggername
+                blog_date.text = item.postdate
+            }
+        }
+
+    }
+
+}
---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/image/ImageAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.architecturestudy.ui.image
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.architecturestudy.R
+import com.example.architecturestudy.model.BlogItems
+import com.example.architecturestudy.model.ImageData
+import com.example.architecturestudy.model.ImageItems
+import com.example.architecturestudy.ui.startWebView
+import kotlinx.android.synthetic.main.item_image.view.*
+import java.lang.Exception
+
+class ImageAdapter() : RecyclerView.Adapter<ImageAdapter.ImageHolder>() {
+
+    private val imageItem : MutableList<ImageItems> = mutableListOf()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return imageItem.size
+    }
+
+    override fun onBindViewHolder(holder: ImageHolder, position: Int) {
+        holder.bind(imageItem[position])
+    }
+
+    fun upDate(imageList : List<ImageItems>) {
+        this.imageItem.clear()
+        this.imageItem.addAll(imageList)
+        notifyDataSetChanged()
+    }
+
+    class ImageHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+        lateinit var item: ImageItems
+
+        init {
+            itemView.setOnClickListener { view ->
+                view.startWebView(item.link)
+            }
+        }
+
+        fun bind(item : ImageItems) {
+            this.item = item
+
+            with(itemView) {
+                try {
+                    Glide.with(this).load(item.thumbnail).into(img_image)
+                } catch (e : Exception) {
+                    error(message = e.toString())
+                }
+            }
+        }
+
+    }
+}
