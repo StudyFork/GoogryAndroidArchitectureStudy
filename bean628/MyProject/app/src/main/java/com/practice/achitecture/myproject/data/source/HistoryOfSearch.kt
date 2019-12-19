402f0a1cb1d46a436c3c5b05ac49447e1d6de178
bean628 <dudwp628@naver.com>
2019-12-19 22:43:35 +0900

    HW-4 Dao, Database, Entity 파일 만듬

---
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/data/source/HistoryOfSearch.kt
@@ -0,0 +1,14 @@
+package com.practice.achitecture.myproject.data.source
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.practice.achitecture.myproject.model.SearchedItem
+
+@Entity(tableName = "historyOfSearch")
+data class HistoryOfSearch constructor(
+    @PrimaryKey @ColumnInfo(name = "id") val id: Long,
+    @ColumnInfo(name = "category") val category: String,
+    @ColumnInfo(name = "query") val query: String,
+    @ColumnInfo(name = "searchedItemList") val searchedItemList: List<SearchedItem>
+)
---
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/data/source/local/NaverDatabase.kt
@@ -0,0 +1,30 @@
+package com.practice.achitecture.myproject.data.source.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.practice.achitecture.myproject.data.source.HistoryOfSearch
+
+
+@Database(version = 1, entities = [HistoryOfSearch::class])
+abstract class NaverDatabase : RoomDatabase() {
+
+    abstract fun naverDao(): NaverDao
+
+    companion object {
+        private var INSATNCE: NaverDatabase? = null
+        private val lock = Any()
+        fun getInstance(context: Context): NaverDatabase {
+            synchronized(lock) {
+                if (INSATNCE == null) {
+                    INSATNCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        NaverDatabase::class.java, "HistoryOfSearch.db"
+                    ).build()
+                }
+            }
+            return INSATNCE!!
+        }
+    }
+}
