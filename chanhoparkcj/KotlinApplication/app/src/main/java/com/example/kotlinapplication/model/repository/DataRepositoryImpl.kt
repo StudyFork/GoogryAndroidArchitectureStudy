811bb27cd7c559153801a767be8fe846afe8985a
chpark <chpark@sysofting.co.kr>
2019-12-20 09:04:33 +0900

    HW-2 private val 로 선언 및 변경

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/model/repository/DataRepositoryImpl.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/model/repository/DataRepositoryImpl.kt
@@ -1,21 +1,26 @@
 package com.example.kotlinapplication.model.repository
 
-class DataRepositoryImpl : DataRepository {
-    var remote: RemoteRepositoryImpl=RemoteRepositoryImpl()
+class DataRepositoryImpl(listener:DataRepository.response) : DataRepository.call {
+    private val remote: RemoteDataSourceImpl=RemoteDataSourceImpl(listener)
 
-    override fun getMovieResources(query: String) {
+    override fun callMovieResources(query: String) {
         remote.getMovieCall(query)
     }
 
-    override fun getImageResources(query: String) {
+    override fun callImageResources(query: String) {
         remote.getImageCall(query)
     }
 
-    override fun getBlogResources(query: String) {
+    override fun callBlogResources(query: String) {
         remote.getBlogCall(query)
     }
 
-    override fun getKinResources(query: String) {
+    override fun callKinResources(query: String) {
         remote.getKinCall(query)
     }
+
+
+
+
+
 }
