14fc8878bb00b3816be92a3c3b8b581b0ec97cbc
middlefgr <2giwon@gmail.com>
2019-12-21 23:58:38 +0900

    HW-4.2 doAfterSuccess 제거 flatmap으로 대체

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/NaverDataRepositoryImpl.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/NaverDataRepositoryImpl.kt
@@ -10,13 +10,18 @@ class NaverDataRepositoryImpl(
 ) : NaverDataRepository {
 
     private fun loadRemoteContents(type: String, query: String): Single<ContentResponse> =
-        naverRemoteDataSource.getContents(type, query).doAfterSuccess { response ->
-            naverLocalDataSource.saveContents(
-                type,
-                query,
-                response
-            )
-        }
+        naverRemoteDataSource.getContents(type, query)
+            .flatMap { response ->
+                if (response.contentItems.isNotEmpty()) {
+                    naverLocalDataSource.saveContents(
+                        type,
+                        query,
+                        response
+                    ).toSingleDefault(response)
+                } else {
+                    Single.error(Throwable())
+                }
+            }
 
     override fun getContents(type: String, query: String): Single<ContentResponse> =
         loadRemoteContents(type, query)
