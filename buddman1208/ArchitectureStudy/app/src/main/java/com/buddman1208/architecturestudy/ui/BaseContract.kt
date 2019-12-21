6ee82694187e3181d447c54e243448e0352f943e
Junseok Oh <kotohana5706@edcan.kr>
2019-12-21 18:16:29 +0900

    [HW-3] BaseContract : View, Presenter 추상화 함수 구현

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/ui/BaseContract.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/ui/BaseContract.kt
@@ -1,9 +1,24 @@
 package com.buddman1208.architecturestudy.ui
 
+import com.buddman1208.architecturestudy.models.CommonResponse
+
 interface BaseContract {
 
-    interface View
+    interface View {
 
-    interface Presenter
+        fun updateData(data: CommonResponse)
+        fun showNoResult()
+        fun showError(msg : String = "")
+        fun showLoading()
+        fun hideLoading()
+
+    }
+
+    interface Presenter {
+        fun searchByQuery(
+            query : String,
+            type: String
+        )
+    }
 
 }
