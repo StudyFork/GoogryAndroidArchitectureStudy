08a7d9bb163d460a2bba668b918ab5e035eda653
Junseok Oh <kotohana5706@edcan.kr>
2019-12-21 18:22:47 +0900

    [HW-3] BasePresenter : searchByQuery() 구현

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/ui/BasePresenter.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/ui/BasePresenter.kt
@@ -1,9 +1,34 @@
 package com.buddman1208.architecturestudy.ui
 
+import com.buddman1208.architecturestudy.repo.NaverDataRepositoryImpl
+import com.buddman1208.architecturestudy.utils.subscribeOnIO
+import io.reactivex.android.schedulers.AndroidSchedulers
+
 class BasePresenter(val view : BaseContract.View) : BaseContract.Presenter {
 
     override fun searchByQuery(query: String, type: String) {
+        val query = query.trim()
+        if (query.isNotBlank()) {
+            NaverDataRepositoryImpl
+                .searchByTypeFromNaver(
+                    searchType = type,
+                    query = query
+                )
+                .subscribeOnIO()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        // TODO : onSuccess
+                    },
+                    {
+                        // TODO : onFailure
+                    }
+                )
 
+
+        } else {
+            // TODO : Must show blank query error
+        }
     }
 
 }
