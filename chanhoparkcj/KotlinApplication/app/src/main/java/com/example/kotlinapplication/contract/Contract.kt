eb765eb7aaabcf06ca56df1d50b758a3f0e58951
chan33344 <chan33344@gmail.com>
2019-12-22 00:35:46 +0900

    HW-3 Contract , Presenter 생성

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Contract.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Contract.kt
@@ -1,4 +1,19 @@
 package com.example.kotlinapplication.contract
 
-interface contract {
+import com.example.kotlinapplication.model.BlogItems
+import com.example.kotlinapplication.model.ImageItems
+import com.example.kotlinapplication.model.KinItems
+import com.example.kotlinapplication.model.MovieItems
+
+interface Contract {
+    interface Presenter{
+        fun loadData(type: String?, query: String)
+    }
+    interface View{
+        fun resultMovie(movieItems:List<MovieItems>)
+        fun resultImage(imageItems:List<ImageItems>)
+        fun resultBlog(blogItems:List<BlogItems>)
+        fun resultKin(kinItems:List<KinItems>)
+        fun resultError(message:String)
+    }
 }
--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Presenter.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Presenter.kt
@@ -1,4 +1,47 @@
 package com.example.kotlinapplication.contract
 
-class Presenter {
+import com.example.kotlinapplication.model.repository.DataRepositoryImpl
+import io.reactivex.functions.Consumer
+
+class Presenter(listener: Contract.View) : Contract.Presenter {
+    private var view: Contract.View = listener
+    private var dataRepositoryImpl: DataRepositoryImpl = DataRepositoryImpl()
+
+    override fun loadData(type: String?, query: String) {
+        when (type) {
+            "영화" -> dataRepositoryImpl.callMovieResources(query).subscribe(
+                Consumer { list ->
+                    view.resultMovie(list.items)
+                },
+                Consumer { errorMessage ->
+                    view.resultError("error 에러" + errorMessage)
+                })
+
+            "이미지" -> dataRepositoryImpl.callImageResources(query).subscribe(
+                Consumer { list ->
+                    view.resultImage(list.items)
+                },
+                Consumer { errorMessage ->
+                    view.resultError("error 에러" + errorMessage)
+                })
+            "블로그" -> dataRepositoryImpl.callBlogResources(query).subscribe(
+                Consumer { list ->
+                    view.resultBlog(list.items)
+                },
+                Consumer { errorMessage ->
+                    view.resultError("error 에러" + errorMessage)
+                })
+            "지식인" -> dataRepositoryImpl.callKinResources(query).subscribe(
+                Consumer { list ->
+                    view.resultKin(list.items)
+                },
+                Consumer { errorMessage ->
+                    view.resultError("error 에러" + errorMessage)
+                })
+            else -> {
+                view.resultError("에러")
+            }
+        }
+    }
+
 }
