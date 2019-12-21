7f49a66178ad8219cb857c3fe76afb7312e2a29e
chan33344 <chan33344@gmail.com>
2019-12-22 00:37:11 +0900

    HW-3 fragmentpage에 contract의 view, presenter 적용

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/fragment/FragmentPage.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/fragment/FragmentPage.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,25 +17,28 @@ import com.example.kotlinapplication.adapter.ListBlogAdapter
 import com.example.kotlinapplication.adapter.ListImageAdapter
 import com.example.kotlinapplication.adapter.ListKinAdapter
 import com.example.kotlinapplication.adapter.ListMovieAdapter
+import com.example.kotlinapplication.contract.Contract
+import com.example.kotlinapplication.contract.Presenter
 import com.example.kotlinapplication.model.BlogItems
 import com.example.kotlinapplication.model.ImageItems
 import com.example.kotlinapplication.model.KinItems
 import com.example.kotlinapplication.model.MovieItems
-import com.example.kotlinapplication.model.repository.DataRepositoryImpl
-import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_page.*
 
 
 class FragmentPage : Fragment(), ListMovieAdapter.ItemListener, ListImageAdapter.ItemListener,
-    ListBlogAdapter.ItemListener, ListKinAdapter.ItemListener {
+    ListBlogAdapter.ItemListener, ListKinAdapter.ItemListener, Contract.View {
+
     private lateinit var movieAdapter: ListMovieAdapter
     private lateinit var blogAdapter: ListBlogAdapter
     private lateinit var imageAdapter: ListImageAdapter
     private lateinit var kinAdapter: ListKinAdapter
-    private lateinit var dataRepositoryImpl: DataRepositoryImpl
+
     private var type: String? = null
+    private lateinit var presenter: Presenter
 
     companion object {
+
         fun newInstance(message: String): FragmentPage {
             val f = FragmentPage()
             val bdl = Bundle(1)
@@ -54,15 +56,15 @@ class FragmentPage : Fragment(), ListMovieAdapter.ItemListener, ListImageAdapter
         return inflater.inflate(R.layout.fragment_page, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         start()
         setUpBuuttonClickListener()
     }
 
+
     private fun start() {
-        dataRepositoryImpl = DataRepositoryImpl()
+        presenter = Presenter(this)
         type = arguments?.getString(EXTRA_MESSAGE)
         home_search_btn.text = type + " 검색"
         when (type) {
@@ -107,30 +109,10 @@ class FragmentPage : Fragment(), ListMovieAdapter.ItemListener, ListImageAdapter
                     "검색어 :" + home_search_edit.text.toString(),
                     Toast.LENGTH_SHORT
                 ).show()
-                loadMovieData(type, home_search_edit.text.toString())
+                presenter.loadData(type, home_search_edit.text.toString())
             }
         }
 
-    }
-
-    private fun loadMovieData(type: String?, query: String) {
-        when (type) {
-            "영화" -> dataRepositoryImpl.callMovieResources(query).subscribe(Consumer {
-                movieAdapter?.addAllItems(it.items)
-            })
-            "이미지" -> dataRepositoryImpl.callImageResources(query).subscribe(Consumer {
-                imageAdapter?.addAllItems(it.items)
-            })
-            "블로그" -> dataRepositoryImpl.callBlogResources(query).subscribe(Consumer {
-                blogAdapter?.addAllItems(it.items)
-            })
-            "지식인" -> dataRepositoryImpl.callKinResources(query).subscribe(Consumer {
-                kinAdapter?.addAllItems(it.items)
-            })
-            else -> {
-                Log.d("Error", "error")
-            }
-        }
     }
 
     override fun onMovieItemClick(movieItems: MovieItems) {
@@ -138,7 +120,6 @@ class FragmentPage : Fragment(), ListMovieAdapter.ItemListener, ListImageAdapter
         val uri: Uri = Uri.parse(movieItems.link)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
-
     }
 
     override fun onImageItemClick(imageItems: ImageItems) {
@@ -160,5 +141,25 @@ class FragmentPage : Fragment(), ListMovieAdapter.ItemListener, ListImageAdapter
         val uri: Uri = Uri.parse(kinItems.link)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
+    }
+
+    override fun resultMovie(movieItems: List<MovieItems>) {
+        movieAdapter?.addAllItems(movieItems)
+    }
+
+    override fun resultImage(imageItems: List<ImageItems>) {
+        imageAdapter?.addAllItems(imageItems)
+    }
+
+    override fun resultBlog(blogItems: List<BlogItems>) {
+        blogAdapter?.addAllItems(blogItems)
+    }
+
+    override fun resultKin(kinItems: List<KinItems>) {
+        kinAdapter?.addAllItems(kinItems)
+    }
+
+    override fun resultError(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
     }
 }
