6b8c344f4f45572785e78694c431a7c3f231058d
Junseok Oh <kotohana5706@edcan.kr>
2019-12-21 19:27:51 +0900

    [HW-3] Extensions : Disposable을 CompositeDisposable에 추가하는 Extension 추가

--- buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/Extensions.kt
+++ buddman1208/ArchitectureStudy/app/src/main/java/com/buddman1208/architecturestudy/utils/Extensions.kt
@@ -1,7 +1,12 @@
 package com.buddman1208.architecturestudy.utils
 
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
 fun String.removeHtmlBoldTags(): String {
     return this
         .replace("<b>", "")
         .replace("</b>", "")
 }
+
+fun Disposable.addToCompositeDisposable(target : CompositeDisposable) = target.add(this)
