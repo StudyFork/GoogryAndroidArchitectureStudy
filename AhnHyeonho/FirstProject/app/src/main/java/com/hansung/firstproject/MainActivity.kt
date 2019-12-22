c315f5b6f74c7c8c467a68b9b7b9b4e36c70882e
ahndding <ahndding@gmail.com>
2019-12-22 22:38:26 +0900

    HW-1 main activity 라인정리

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/MainActivity.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hansung.firstproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val clientId = "wqaH4p5lX5tCWVMOGv0y" // naver 검색API 사용을 위한 Client ID
+    private val clientSecret = "cnOKERR71G" //naver 검색API 사용을 위한 Client Secret
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
