30733bb4d3dabca5fe1e890c020590d2ffc38738
이용준 <wooooooak@gmail.com>
2019-12-22 13:00:33 +0900

    HW-3 프레그먼트가 Contract의 View를 구현하도록 수정

--- wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/image/ImageFragment.kt
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/image/ImageFragment.kt
@@ -1,14 +1,34 @@
 package wooooooak.com.studyapp.ui.image
 
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import wooooooak.com.studyapp.R
-import wooooooak.com.studyapp.ui.base.BaseSearchFragment
+import wooooooak.com.studyapp.common.ext.startWebView
+import wooooooak.com.studyapp.data.model.repository.NaverApiRepositoryImpl
+import wooooooak.com.studyapp.data.model.response.image.Image
+import wooooooak.com.studyapp.ui.base.BaseSearchListAdapter
+import wooooooak.com.studyapp.ui.base.ItemSearchFragment
 
+class ImageFragment : ItemSearchFragment<Image>(R.layout.fragment_image) {
 
-class ImageFragment : BaseSearchFragment(R.layout.fragment_image) {
+    override val adapter = ImageListAdapter(object : BaseSearchListAdapter.ItemListener<Image> {
+        override fun loadMoreItems(list: List<Image>, index: Int) {
+            lifecycleScope.launch {
+                presenter.fetchMoreItems(list, index)
+            }
+        }
 
-    override fun setListViewAdapter(listView: RecyclerView) {
-        listView.adapter = ImageListAdapter(requireActivity())
+        override fun renderWebView(url: String) {
+            requireContext().startWebView(url)
+        }
+    })
+
+    private val presenter by lazy { ImagePresenter(this, NaverApiRepositoryImpl) }
+
+    override fun initItemsByTitle(title: String) {
+        lifecycleScope.launch {
+            presenter.fetchItemsWithNewTitle(title)
+        }
     }
 
 }
--- wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/kin/KinFragment.kt
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/kin/KinFragment.kt
@@ -1,13 +1,35 @@
 package wooooooak.com.studyapp.ui.kin
 
 
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import wooooooak.com.studyapp.R
-import wooooooak.com.studyapp.ui.base.BaseSearchFragment
+import wooooooak.com.studyapp.common.ext.startWebView
+import wooooooak.com.studyapp.data.model.repository.NaverApiRepositoryImpl
+import wooooooak.com.studyapp.data.model.response.kin.Kin
+import wooooooak.com.studyapp.ui.base.BaseSearchListAdapter
+import wooooooak.com.studyapp.ui.base.ItemSearchFragment
 
-class KinFragment : BaseSearchFragment(R.layout.fragment_kin) {
+class KinFragment : ItemSearchFragment<Kin>(R.layout.fragment_kin) {
 
-    override fun setListViewAdapter(listView: RecyclerView) {
-        listView.adapter = KinListAdapter(requireActivity())
+    override val adapter = KinListAdapter(object : BaseSearchListAdapter.ItemListener<Kin> {
+        override fun loadMoreItems(list: List<Kin>, index: Int) {
+            lifecycleScope.launch {
+                presenter.fetchMoreItems(list, index)
+            }
+        }
+
+        override fun renderWebView(url: String) {
+            requireContext().startWebView(url)
+        }
+    })
+
+    private val presenter by lazy { KinPresenter(this, NaverApiRepositoryImpl) }
+
+    override fun initItemsByTitle(title: String) {
+        lifecycleScope.launch {
+            presenter.fetchItemsWithNewTitle(title)
+        }
     }
+
 }
