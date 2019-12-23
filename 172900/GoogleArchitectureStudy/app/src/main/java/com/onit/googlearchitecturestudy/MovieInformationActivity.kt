dd1fc222683fed125b919f6b98eee653c384c691
172900 <dlsldlrpdho123@gmail.com>
2019-12-23 23:53:45 +0900

    HW-1 영화의 URL을 이용하여 영화정보 페이지를 보여주기 위한 Activity생성

---
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MovieInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.onit.googlearchitecturestudy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_information)
+    }
+}
