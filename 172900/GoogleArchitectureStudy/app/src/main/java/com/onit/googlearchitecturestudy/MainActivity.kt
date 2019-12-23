2a21ddf25c4f1c4935e160a8fc142141bfe4ac88
172900 <dlsldlrpdho123@gmail.com>
2019-12-23 22:53:21 +0900

    HW-1 recyclerView어댑터 생성과 layoutManager생성 후 recyclerView에 설정

--- 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
@@ -2,11 +2,34 @@ package com.onit.googlearchitecturestudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        init()
     }
+
+    private fun init() {
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        with(resultMovieListRecyclerView) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ResultMovieListRecyclerAdapter(
+                ArrayList(),
+                object : ResultMovieListRecyclerAdapter.MovieViewHolder.ClickListener {
+                    override fun clickViewHolder(position: Int) {
+
+                    }
+                })
+        }
+    }
+
 }
