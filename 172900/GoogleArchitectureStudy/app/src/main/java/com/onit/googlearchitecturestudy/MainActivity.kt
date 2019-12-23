2f6905fdc0b71150620008dddf6cee51ddcc1e16
172900 <dlsldlrpdho123@gmail.com>
2019-12-23 23:22:11 +0900

    HW-1 검색 버튼 동작 구현

--- 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
@@ -3,7 +3,14 @@ package com.onit.googlearchitecturestudy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        searchButton.setOnClickListener {
+            clickSearchButton()
+        }
         setRecyclerView()
     }
 
@@ -32,4 +42,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun clickSearchButton() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val movieList =
+                withContext(Dispatchers.IO) { searchMovieList(searchFieldEditText.text.toString()) }
+        }
+    }
+
+    private suspend fun searchMovieList(searchWord: String): List<Movie> {
+        return Retrofit.Builder().apply {
+            baseUrl(Config.NAVER_API_BASE_URL)
+            addConverterFactory(GsonConverterFactory.create())
+            addCallAdapterFactory(CoroutineCallAdapterFactory())
+        }.build().create(NaverAPIService::class.java).getMovieList(
+            Config.NAVER_API_CLIENT_ID,
+            Config.NAVER_API_CLIENT_SECRET,
+            searchWord
+        ).await().movies
+    }
 }
