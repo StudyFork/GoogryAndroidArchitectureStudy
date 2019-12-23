e9615a11573b2f49743aa4b9389a4f3b0c1439e0
swppppp <sw931110@naver.com>
2019-12-24 02:25:29 +0900

    - 평점 xml컴포넌트 변경(ratingBar) - adapter 작성중

--- swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
+++ swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
@@ -2,11 +2,36 @@ package com.siwon.prj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    inner class MovieListAdapter(val list: ArrayList<Movie>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val itemView = LayoutInflater.from(baseContext).inflate(R.layout.list_item, parent, false)
+            return movieHolder(itemView)
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        }
+
+        inner class movieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var image = itemView
+
+            fun bind()
+        }
+
     }
 }
