a5184a958529e1c0046ca9bfc835439ca302dbc8
172900 <dlsldlrpdho123@gmail.com>
2019-12-23 22:46:42 +0900

    HW-1 네이버 API responseData를 받을 Movies데이터 클래스 추가 및 SerializedName어노테이션 설정

--- 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/Movie.kt
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/Movie.kt
@@ -1,11 +1,17 @@
 package com.onit.googlearchitecturestudy
 
+import com.google.gson.annotations.SerializedName
+
+data class Movies(
+    @SerializedName("items") val movies: List<Movie>
+)
+
 data class Movie(
-    val posterImage: String,
-    val title: String,
-    val userRating: Float,
-    val releaseDate: String,
-    val director: String,
-    val actor: String,
-    val link: String
+    @SerializedName("image") val posterImage: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("userRating") val userRating: Float,
+    @SerializedName("pubDate") val releaseDate: String,
+    @SerializedName("director") val director: String,
+    @SerializedName("actor") val actor: String,
+    @SerializedName("link") val link: String
 )
