f4e899ecb837a521d97ff574cabe75266f947d19
172900 <dlsldlrpdho123@gmail.com>
2019-12-23 20:11:10 +0900

    HW-1 Movie 데이터 클래스 추가

---
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/Movie.kt
@@ -0,0 +1,11 @@
+package com.onit.googlearchitecturestudy
+
+data class Movie(
+    val posterImage: String,
+    val title: String,
+    val userRating: Double,
+    val releaseDate: String,
+    val director: String,
+    val actor: String,
+    val link: String
+)
