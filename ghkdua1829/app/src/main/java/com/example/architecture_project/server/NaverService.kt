bf56e14b224a88639f05c4116d725324bcd6bdf8
ghkdua1829 <ghkdua059@gmail.com>
2019-12-23 16:17:14 +0900

    HW-1 코드 정리, link 데이터 추가

--- ghkdua1829/app/src/main/java/com/example/architecture_project/server/NaverService.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/server/NaverService.kt
@@ -4,7 +4,6 @@ import com.example.architecture_project.data.NaverApi
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NaverService {
