56db3951a52aa46e08671d0c12e2579bc932d420
이용준 <wooooooak@gmail.com>
2019-12-24 17:44:57 +0900

    HW-3 불필요한 Interface 제거

--- wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/base/ItemContract.kt
+++ wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/base/ItemContract.kt
@@ -1,7 +1,17 @@
 package wooooooak.com.studyapp.ui.base
 
 interface ItemContract {
-    interface View<T> : ItemView<T>
+    interface View<T> {
+        fun renderItems(items: List<T>)
 
-    interface Presenter<T> : ItemPresenter<T>
+        fun renderWebView(url: String)
+
+        fun renderErrorToast(message: String?)
+    }
+
+    interface Presenter<T> {
+        suspend fun fetchItemsWithNewTitle(title: String)
+
+        suspend fun fetchMoreItems(originList: List<T>, index: Int = 0)
+    }
 }
--- wooooooak/StudyApp/app/src/main/java/wooooooak/com/studyapp/ui/base/ItemView.kt
+++
@@ -1,9 +0,0 @@
-package wooooooak.com.studyapp.ui.base
-
-interface ItemView<T> {
-    fun renderItems(items: List<T>)
-
-    fun renderWebView(url: String)
-
-    fun renderErrorToast(message: String?)
-}
