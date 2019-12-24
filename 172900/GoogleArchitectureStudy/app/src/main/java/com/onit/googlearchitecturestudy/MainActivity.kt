a6549057bf313cea85ca51946d4130d39284cdd0
172900 <dlsldlrpdho123@gmail.com>
2019-12-24 14:28:19 +0900

    HW-1 response형태의 반환값을 처리핤 수 있도록 관련 코드 수정

--- 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.onit.googlearchitecturestudy
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
@@ -10,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -56,13 +59,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun clickSearchButton() {
         CoroutineScope(Dispatchers.Main).launch {
-            val movieList =
+            val response =
                 withContext(Dispatchers.IO) { searchMovieList(searchFieldEditText.text.toString()) }
-            resultMovieListRecyclerAdapter.setMovieList(movieList)
+
+            if (response.isSuccessful) {
+                val movieList = response.body()?.movies
+                if (movieList == null) {
+                    Toast.makeText(applicationContext, "검색결과가 없습니다.", Toast.LENGTH_LONG).show()
+                    return@launch
+                }
+                resultMovieListRecyclerAdapter.setMovieList(movieList)
+            } else {
+                Log.e("MainActivity", "네이버 API요청에 실패했습니다. responseCode = ${response.code()}")
+                Toast.makeText(applicationContext, "네트워크가 불안정합니다.", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    private suspend fun searchMovieList(searchWord: String): List<Movie> {
+    private suspend fun searchMovieList(searchWord: String): Response<Movies> {
         return Retrofit.Builder().apply {
             baseUrl(Config.NAVER_API_BASE_URL)
             addConverterFactory(GsonConverterFactory.create())
@@ -71,6 +85,6 @@ class MainActivity : AppCompatActivity() {
             Config.NAVER_API_CLIENT_ID,
             Config.NAVER_API_CLIENT_SECRET,
             searchWord
-        ).await().movies
+        ).await()
     }
 }
