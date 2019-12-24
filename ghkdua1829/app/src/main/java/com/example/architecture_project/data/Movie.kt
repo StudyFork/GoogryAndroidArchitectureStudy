eba475cfe787c649469f011c20b3ddb10b1d95d6
ghkdua1829 <ghkdua059@gmail.com>
2019-12-24 19:45:16 +0900

    불필요한 serializedName 삭제

--- ghkdua1829/app/src/main/java/com/example/architecture_project/data/Movie.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/data/Movie.kt
@@ -3,18 +3,12 @@ package com.example.architecture_project.data
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
-    @SerializedName("image")
     val image: String,
-    @SerializedName("title")
     val title: String,
     @SerializedName("userRating")
     val rating: Float,
-    @SerializedName("pubDate")
     val pubDate:String,
-    @SerializedName("director")
     val director: String,
-    @SerializedName("actor")
     val actor: String,
-    @SerializedName("link")
     val link:String
 )
