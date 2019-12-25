ea5b5f71a63b2dde3bd46be7142640366ba0d821
swppppp <sw931110@naver.com>
2019-12-26 02:36:41 +0900

    -검색결과 리사이클러뷰와 연결

--- swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
+++ swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.RatingBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,42 +24,52 @@ class MainActivity : AppCompatActivity() {
     private val CLIENT_ID = "KjLtxBCCy8ZTWORQ7uas"
     private val CLIENT_SECRET = "PHBD6IlPgd"
     private val BASE_URL = "https://openapi.naver.com"
+    lateinit var movies: ArrayList<Movie>
+    lateinit var retrofit: Retrofit
+    lateinit var apiService: MovieSearchService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        var apiService = retrofit.create(MovieSearchService::class.java)
+        apiServiceSet()
 
         // 검색버튼
         search_btn.setOnClickListener {
             if (edit_text_input.text.isNullOrBlank()) {
-                var toast = Toast.makeText(baseContext, "검색어를 입력해 주세요", Toast.LENGTH_SHORT)
+                var toast = Toast.makeText(this, "검색어를 입력해 주세요", Toast.LENGTH_SHORT)
                 toast.show()
             }else{
-                var toast = Toast.makeText(baseContext, "입력한 겁색어는 ${edit_text_input.text.toString()}", Toast.LENGTH_SHORT)
+                var toast = Toast.makeText(this, "입력한 겁색어는 ${edit_text_input.text.toString()}", Toast.LENGTH_SHORT)
                 toast.show()
 
                 apiService.search(CLIENT_ID, CLIENT_SECRET, edit_text_input.text.toString()).enqueue(object : Callback<Movies>{
                     override fun onFailure(call: Call<Movies>, t: Throwable) {
                         Log.i("로그로그", "####실패메시지: ${t.message}")
-                        var toast = Toast.makeText(baseContext, "검색 실패", Toast.LENGTH_SHORT)
+                        var toast = Toast.makeText(this@MainActivity, "검색에 실패하였습니다.", Toast.LENGTH_SHORT)
                         toast.show()
                     }
 
                     override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
                         Log.i("로그로그", "####응답: ${response.body().toString()}\n ${response.message()}")
+                        movies = response.body()!!.movies
+                        val adapter = MovieListAdapter(movies)
+                        list.layoutManager = LinearLayoutManager(this@MainActivity)
+                        list.adapter = adapter
                     }
                 })
             }
         }
+
     }
 
-
+    fun apiServiceSet() {
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        apiService = retrofit.create(MovieSearchService::class.java)
+    }
 
     // 영화리스트 어댑터
     inner class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.movieHolder> {
@@ -66,18 +77,20 @@ class MainActivity : AppCompatActivity() {
 
         constructor(items: ArrayList<Movie>) {
             this.items = items
+            notifyDataSetChanged()
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): movieHolder
-            = movieHolder(LayoutInflater.from(baseContext).inflate(R.layout.list_item, parent, false))
+            = movieHolder(LayoutInflater.from(this@MainActivity).inflate(R.layout.list_item, parent, false))
 
         override fun getItemCount(): Int = items.size
 
         override fun onBindViewHolder(holder: movieHolder, position: Int) {
             holder.bind(items[position])
             holder.itemView.setOnClickListener {
-                val toast = Toast.makeText(baseContext, "웹뷰로 이동", Toast.LENGTH_SHORT)
+                val toast = Toast.makeText(this@MainActivity, "웹뷰로 이동", Toast.LENGTH_SHORT)
                 toast.show()
+                
             }
         }
 
