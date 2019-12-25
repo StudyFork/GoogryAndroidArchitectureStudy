b9181d6203c3150cc5696130a4d909c084c93109
middlefgr <2giwon@gmail.com>
2019-12-26 01:14:24 +0900

    HW-5 BaseRecyclerview 추가

---
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/base/BaseRecyclerView.kt
@@ -0,0 +1,52 @@
+package com.egiwon.architecturestudy.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseRecyclerView {
+
+    abstract class BaseViewHolder<B : ViewDataBinding>(
+        parent: ViewGroup,
+        @LayoutRes resourceId: Int
+    ) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(resourceId, parent, false)
+    ) {
+        protected val binding: B = DataBindingUtil.bind(itemView)!!
+
+        open fun onBindViewHolder(item: Any?) = Unit
+    }
+
+    abstract class Adapter<A : Any, B : ViewDataBinding>(
+        @LayoutRes private val layoutResId: Int
+    ) : RecyclerView.Adapter<BaseViewHolder<B>>() {
+
+        private val items = mutableListOf<A>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<B> =
+            object : BaseViewHolder<B>(
+                parent,
+                layoutResId
+            ) {}
+
+        override fun getItemCount(): Int = items.size
+
+        override fun onBindViewHolder(holder: BaseViewHolder<B>, position: Int) =
+            holder.onBindViewHolder(items[position])
+
+        fun replaceAll(items: List<A>?) {
+            this.items.run {
+                clear()
+                items?.let {
+                    addAll(it)
+                }
+            }
+
+            notifyDataSetChanged()
+        }
+    }
+}
