4b36520dcad33242ed00d3fc9a08dd993cc4119a
middlefgr <2giwon@gmail.com>
2019-12-26 01:36:36 +0900

    HW-5 ContentsAdapter, ContentViewHolder 데이터 바인딩 추가, 이미지 Visible 처리를 onCreateViewHolder에서 처리하도록 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentViewHolder.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/tabs/ContentViewHolder.kt
@@ -2,29 +2,28 @@ package com.egiwon.architecturestudy.tabs
 
 import android.content.Intent
 import android.net.Uri
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.core.content.ContextCompat
 import com.egiwon.architecturestudy.R
-import com.egiwon.architecturestudy.base.BaseViewHolder
+import com.egiwon.architecturestudy.base.BaseRecyclerView
 import com.egiwon.architecturestudy.data.source.remote.response.ContentItem
+import com.egiwon.architecturestudy.databinding.RvContentsItemBinding
 import com.egiwon.architecturestudy.ext.fromHtml
-import kotlinx.android.synthetic.main.rv_contents_item.view.*
+import com.egiwon.architecturestudy.ext.loadUrl
 
-open class ContentViewHolder(
+class ContentViewHolder(
     parent: ViewGroup,
-    @LayoutRes
-    private val resourceId: Int = R.layout.rv_text_contents_item
-) : BaseViewHolder<ContentItem>(
+    @LayoutRes private val layoutRes: Int
+) : BaseRecyclerView.BaseViewHolder<RvContentsItemBinding>(
     parent,
-    resourceId
+    layoutRes
 ) {
-    private val tvTitle: TextView = itemView.tv_title
-    private val tvDescription: TextView = itemView.tv_description
     private var linkUrl: String? = null
 
     init {
+
         itemView.setOnClickListener {
             linkUrl?.let { url ->
                 ContextCompat.startActivity(
@@ -38,12 +37,21 @@ open class ContentViewHolder(
         }
     }
 
-    override fun bind(item: ContentItem) {
-        with(item) {
-            tvDescription.text = (actor + description).fromHtml()
-            tvTitle.text = title.fromHtml()
-            linkUrl = link
+    override fun onBindViewHolder(item: Any?) {
+        (item as? ContentItem)?.let {
+            with(item) {
+                binding.tvDescription.text = (actor + description).fromHtml()
+                binding.tvTitle.text = title.fromHtml()
+                binding.ivThumbnail.loadUrl(image) {
+                    placeholder(R.mipmap.ic_launcher)
+                }
+                linkUrl = link
+            }
         }
+    }
+
+    fun thumbnailVisible(visible: Boolean) {
+        binding.ivThumbnail.visibility = if (visible) View.VISIBLE else View.GONE
     }
 
 }
