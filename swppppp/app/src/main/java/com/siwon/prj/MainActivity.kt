9a32c35a1f0a220c69867b9da01f2a33b2785057
swppppp <sw931110@naver.com>
2019-12-26 02:05:57 +0900

    - api 서비스 생성

--- swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
+++ swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
@@ -2,6 +2,7 @@ package com.siwon.prj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +11,55 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val CLIENT_ID = "KjLtxBCCy8ZTWORQ7uas"
+    private val CLIENT_SECRET = "PHBD6IlPgd"
+    private val BASE_URL = "https://openapi.naver.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var apiService = retrofit.create(MovieSearchService::class.java)
+
+        // 검색버튼
+        search_btn.setOnClickListener {
+            if (edit_text_input.text.isNullOrBlank()) {
+                var toast = Toast.makeText(baseContext, "검색어를 입력해 주세요", Toast.LENGTH_SHORT)
+                toast.show()
+            }else{
+                var toast = Toast.makeText(baseContext, "입력한 겁색어는 ${edit_text_input.text.toString()}", Toast.LENGTH_SHORT)
+                toast.show()
+
+                apiService.search(CLIENT_ID, CLIENT_SECRET, edit_text_input.text.toString()).enqueue(object : Callback<Movies>{
+                    override fun onFailure(call: Call<Movies>, t: Throwable) {
+                        Log.i("로그로그", "####실패메시지: ${t.message}")
+                        var toast = Toast.makeText(baseContext, "검색 실패", Toast.LENGTH_SHORT)
+                        toast.show()
+                    }
+
+                    override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
+                        Log.i("로그로그", "####응답: ${response.body().toString()}\n ${response.message()}")
+                    }
+                })
+            }
+        }
     }
-    
+
+
+
     // 영화리스트 어댑터
     inner class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.movieHolder> {
         val items : ArrayList<Movie>
--- swppppp/app/src/main/java/com/siwon/prj/MovieSearchService.kt
+++ swppppp/app/src/main/java/com/siwon/prj/MovieSearchService.kt
@@ -1,5 +1,6 @@
 package com.siwon.prj
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -10,5 +11,5 @@ interface MovieSearchService {
         @Header("X-Naver-Client-Id") Client_Id: String,
         @Header("X-Naver-Client-Secret") Client_Secret: String,
         @Query("query") input: String
-    ): ArrayList<Movie>
+    ): Call<Movies>
 }
