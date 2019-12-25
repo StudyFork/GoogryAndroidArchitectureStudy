85436928b399a6fbd0247796e8838ca72b3e4925
swppppp <sw931110@naver.com>
2019-12-25 18:16:30 +0900

    - 어댑터 생성 - 글라이드 사용을 위한 디펜던시 추가

--- swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
+++ swppppp/app/src/main/java/com/siwon/prj/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,24 +17,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    
+    // 영화리스트 어댑터
+    inner class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.movieHolder> {
+        val items : ArrayList<Movie>
 
-    inner class MovieListAdapter(val list: ArrayList<Movie>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            val itemView = LayoutInflater.from(baseContext).inflate(R.layout.list_item, parent, false)
-            return movieHolder(itemView)
+        constructor(items: ArrayList<Movie>) {
+            this.items = items
         }
 
-        override fun getItemCount(): Int {
-            return list.size
-        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): movieHolder
+            = movieHolder(LayoutInflater.from(baseContext).inflate(R.layout.list_item, parent, false))
 
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        override fun getItemCount(): Int = items.size
+
+        override fun onBindViewHolder(holder: movieHolder, position: Int) {
+            holder.bind(items[position])
+            holder.itemView.setOnClickListener {
+                val toast = Toast.makeText(baseContext, "웹뷰로 이동", Toast.LENGTH_SHORT)
+                toast.show()
+            }
         }
 
         inner class movieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var image = itemView
 
-            fun bind()
+            fun bind(item: Movie){
+                Glide.with(itemView).load(item.image).into(itemView.findViewById(R.id.img_view))
+                itemView.findViewById<TextView>(R.id.movie_name).text = item.title
+                itemView.findViewById<RatingBar>(R.id.score).rating = item.userRating.toFloat()/2f
+                itemView.findViewById<TextView>(R.id.pub_date).text = item.pubDate
+                itemView.findViewById<TextView>(R.id.director).text = item.director
+                itemView.findViewById<TextView>(R.id.actor).text = item.actor
+            }
         }
 
     }
