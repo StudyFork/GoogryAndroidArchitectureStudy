9d13b066b1fce7eb57ee8ee717b9b4809671ced3
taeiim <taeiim.god@gmail.com>
2019-12-26 16:54:09 +0900

    HW-5 BaseActivity에 dataBinding 적용

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseActivity.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseActivity.kt
@@ -1,7 +1,20 @@
 package com.god.taeiim.myapplication.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+) : AppCompatActivity(layoutResId) {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+    }
 
 }
