48ae8ec944c81b41a4f22cbeab9f3da80d65abbb
taeiim <taeiim.god@gmail.com>
2019-12-26 16:52:51 +0900

    HW-5 BaseFragment에 dataBinding 적용

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseFragment.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseFragment.kt
@@ -1,7 +1,27 @@
 package com.god.taeiim.myapplication.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+) : Fragment(layoutResId) {
+
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
 
 }
