4d40978793521cbea39e219be27cb9da50acf670
ghkdua1829 <ghkdua059@gmail.com>
2019-12-27 04:32:31 +0900

    HW-1 val 선언

--- ghkdua1829/app/src/main/java/com/example/architecture_project/feature/movie/MovieAdapter.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/feature/movie/MovieAdapter.kt
@@ -14,7 +14,7 @@ import com.example.architecture_project.data.Movie
 
 class MovieAdapter(private val clickListener: MovieViewHolder.ItemClickListener) :
     RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
-    private var movieData: ArrayList<Movie> = ArrayList()
+    private val movieData: ArrayList<Movie> = ArrayList()
 
     fun setMovieItemList(newMovieData: ArrayList<Movie>) {
         with(movieData) {
