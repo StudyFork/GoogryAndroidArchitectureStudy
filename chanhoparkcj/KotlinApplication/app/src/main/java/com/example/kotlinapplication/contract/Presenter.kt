1b60c8bc70e6208e4426645611e65c47e1f1bb93
chan33344 <chan33344@gmail.com>
2019-12-26 11:04:25 +0900

    HW-3 Consumer 제거

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Presenter.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/contract/Presenter.kt
@@ -1,7 +1,6 @@
 package com.example.kotlinapplication.contract
 
 import com.example.kotlinapplication.model.repository.DataRepositoryImpl
-import io.reactivex.functions.Consumer
 
 class Presenter(listener: Contract.View) : Contract.Presenter {
     private var view: Contract.View = listener
@@ -10,34 +9,17 @@ class Presenter(listener: Contract.View) : Contract.Presenter {
     override fun loadData(type: String?, query: String) {
         when (type) {
             "영화" -> dataRepositoryImpl.callMovieResources(query).subscribe(
-                Consumer { datas ->
-                    view.resultMovie(datas.items)
-                },
-                Consumer { errorMessage ->
-                    view.resultError("error 에러" + errorMessage)
-                })
-
+                { datas -> view.resultMovie(datas.items) },
+                { errorMessage -> view.resultError("error 에러" + errorMessage) })
             "이미지" -> dataRepositoryImpl.callImageResources(query).subscribe(
-                Consumer { datas ->
-                    view.resultImage(datas.items)
-                },
-                Consumer { errorMessage ->
-                    view.resultError("error 에러" + errorMessage)
-                })
+                { datas -> view.resultImage(datas.items) },
+                { errorMessage -> view.resultError("error 에러" + errorMessage) })
             "블로그" -> dataRepositoryImpl.callBlogResources(query).subscribe(
-                Consumer { datas ->
-                    view.resultBlog(datas.items)
-                },
-                Consumer { errorMessage ->
-                    view.resultError("error 에러" + errorMessage)
-                })
+                { datas -> view.resultBlog(datas.items) },
+                { errorMessage -> view.resultError("error 에러" + errorMessage) })
             "지식인" -> dataRepositoryImpl.callKinResources(query).subscribe(
-                Consumer { datas ->
-                    view.resultKin(datas.items)
-                },
-                Consumer { errorMessage ->
-                    view.resultError("error 에러" + errorMessage)
-                })
+                { datas -> view.resultKin(datas.items) },
+                { errorMessage -> view.resultError("error 에러" + errorMessage) })
             else -> {
                 view.resultError("에러")
             }
