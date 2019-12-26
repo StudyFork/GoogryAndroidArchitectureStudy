34c778ad9305d330be9612150d989f66eab9cde8
ghkdua1829 <ghkdua059@gmail.com>
2019-12-27 04:29:20 +0900

    HW-1 import 적용

--- ghkdua1829/app/src/main/java/com/example/architecture_project/feature/movie/MovieAdapter.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/feature/movie/MovieAdapter.kt
@@ -9,8 +9,8 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.architecture_project.R
 import com.example.architecture_project.data.Movie
-
 
 class MovieAdapter(private val clickListener: MovieViewHolder.ItemClickListener) :
     RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
@@ -30,7 +30,7 @@ class MovieAdapter(private val clickListener: MovieViewHolder.ItemClickListener)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(com.example.architecture_project.R.layout.item_movie, parent, false)
+            .inflate(R.layout.item_movie, parent, false)
         return MovieViewHolder(view, clickListener)
     }
 
@@ -47,17 +47,17 @@ class MovieAdapter(private val clickListener: MovieViewHolder.ItemClickListener)
         RecyclerView.ViewHolder(view) {
 
         private val image: ImageView =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_iv_movie_image)
+            view.findViewById(R.id.rv_item_iv_movie_image)
         private val title: TextView =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_tv_movie_title)
+            view.findViewById(R.id.rv_item_tv_movie_title)
         private val userRating: RatingBar =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_rb_movie_rating)
+            view.findViewById(R.id.rv_item_rb_movie_rating)
         private val pubDate: TextView =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_tv_movie_pubDate)
+            view.findViewById(R.id.rv_item_tv_movie_pubDate)
         private val director: TextView =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_tv_movie_director)
+            view.findViewById(R.id.rv_item_tv_movie_director)
         private val actor: TextView =
-            view.findViewById(com.example.architecture_project.R.id.rv_item_tv_movie_actor)
+            view.findViewById(R.id.rv_item_tv_movie_actor)
         private lateinit var link: String
 
         init {
