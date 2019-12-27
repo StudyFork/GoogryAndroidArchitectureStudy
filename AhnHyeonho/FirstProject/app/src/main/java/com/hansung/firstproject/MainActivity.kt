0391e90e04b77a9ea54ab3715971073595a5c374
ahndding <ahndding@gmail.com>
2019-12-28 05:04:51 +0900

    HW-1 retrofit 적용3: 기존 Okhttp3를 통한 방식을 retrofit으로 전환 및 일부 메소드 정리

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/MainActivity.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hansung.firstproject
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -10,88 +9,92 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.GsonBuilder
+import com.hansung.firstproject.adapter.RecyclerViewAdapter
+import com.hansung.firstproject.data.MovieResponseModel
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.*
-import java.io.IOException
-import java.net.URL
-import java.net.URLEncoder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val clientId = getString(R.string.client_id) // naver 검색API 사용을 위한 Client ID
-    private val clientSecret = getString(R.string.client_secret) //naver 검색API 사용을 위한 Client Secret
+    private lateinit var clientId: String // naver 검색API 사용을 위한 Client ID
+    private lateinit var clientSecret: String //naver 검색API 사용을 위한 Client Secret
+    private val searchResult: SearchResult = SearchResult()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("ahn", "MainActivity onCreate...")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_search.setOnClickListener {
 
-            // 입력값이 없을 때 처리
-            if (editText_search.text.isEmpty()) {
+        clientId = getString(R.string.client_id)
+        clientSecret = getString(R.string.client_secret)
+
+        // recyclerView 초기화
+        initRecyclerView()
+
+        btn_search.setOnClickListener {
+            // 입력값이 없을 때
+            if (et_search.text.isEmpty()) {
+                Log.d("ahn", "검색어 누락")
                 Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             } else {
-                //레이아웃매니저 설정
-                recycler_view_movies.layoutManager =
-                    LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-                recycler_view_movies.setHasFixedSize(true)
-                // movie 항목별 구분선 추가
-                recycler_view_movies.addItemDecoration(
-                    DividerItemDecoration(
-                        this,
-                        LinearLayoutManager.VERTICAL
-                    )
-                )
-
-                fetchJson(editText_search.text.toString())
+                // 입력값이 있을 때
+                doSearch(et_search.text.toString())
 
                 //키보드 제거
-                val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(editText_search.windowToken, 0)
+                removeKeyboard()
             }
         }
     }
 
-    //OkHttp3로 요청
-    private fun fetchJson(vararg p0: String) {
-
-        val text = URLEncoder.encode(p0[0], "UTF-8")
-        val url =
-            URL("https://openapi.naver.com/v1/search/movie.json?query=${text}&display=10&start=1&genre=")
-
-        val request = Request.Builder()
-            .url(url)
-            .addHeader("X-Naver-Client-Id", clientId)
-            .addHeader("X-Naver-Client-Secret", clientSecret)
-            .method("GET", null)
-            .build()
-
-
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call?, response: Response?) {
-                val body = response?.body()?.string()
-                println("Success to execute request : $body")
-
-                //Gson을 Kotlin에서 사용 가능한 object로 만든다.
-                val gson = GsonBuilder().create()
-                val movies = gson.fromJson(body, Movies::class.java)
-
-                //어답터를 연결
-                runOnUiThread {
-                    recycler_view_movies.adapter = RecyclerViewAdapter(movies)
-                }
-            }
-
-            override fun onFailure(call: Call?, e: IOException?) {
-                Looper.prepare()
-                Toast.makeText(applicationContext, "인터넷 연결을 확인하세요", Toast.LENGTH_SHORT).show()
-                e?.printStackTrace()
-                Looper.loop()
-            }
-        })
+    // recyclerView 초기화 메소드
+    private fun initRecyclerView() {
+        recycler_view_movies.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recycler_view_movies.setHasFixedSize(true)
+        // movie 항목별 구분선 추가
+        recycler_view_movies.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
     }
+
+    // 검색 메소드
+    private fun doSearch(keyword: String) {
+        searchResult.getResult(clientId, clientSecret, keyword, 100)
+            .enqueue(object : Callback<MovieResponseModel> {
+                override fun onFailure(call: Call<MovieResponseModel>, t: Throwable) {
+                    //To change body of created functions use File | Settings | File Templates.
+                    Toast.makeText(applicationContext, "인터넷 연결을 확인하세요", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(
+                    call: Call<MovieResponseModel>,
+                    response: Response<MovieResponseModel>
+                ) {
+                    //To change body of created functions use File | Settings | File Templates.
+                    if (response.isSuccessful) {
+                        //올바르게 통신이 왔으면
+                        recycler_view_movies.adapter = RecyclerViewAdapter(response.body()!!.items)
+                    } else {
+                        Toast.makeText(applicationContext, "인터넷 연결을 확인하세요", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }
+            })
+    }
+
+    //키보드 제거 메소드
+    private fun removeKeyboard() =
+        (this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+            et_search.windowToken,
+            0
+        )
 }
