090e985494d183eaa9d5ff5d5c8bce7114d0e2e0
chpark <chpark@sysofting.co.kr>
2019-12-27 09:19:06 +0900

    HW-3 BaseFragment 클래스명 변경

--- chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/util/BaseFragment.kt
+++ chanhoparkcj/KotlinApplication/app/src/main/java/com/example/kotlinapplication/util/BaseFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.kotlinapplication.R
 
-open class baseFragment :Fragment() {
+open class BaseFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,10 +17,12 @@ open class baseFragment :Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_page, container, false)
     }
-    fun toast(message:String){
+
+    fun toast(message: String) {
         android.widget.Toast.makeText(activity, message, android.widget.Toast.LENGTH_SHORT).show()
     }
-    fun webLink(message:String){
+
+    fun webLink(message: String) {
         val uri: Uri = Uri.parse(message)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
