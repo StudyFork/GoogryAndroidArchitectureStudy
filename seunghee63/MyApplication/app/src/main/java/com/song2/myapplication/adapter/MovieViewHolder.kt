fcabf461b84f6fd2f59a80b2b843cc3262a48cbc
seunghee63 <0603yang@naver.com>
2019-12-29 08:03:24 +0900

    HW-1 ViewHolder에서 클릭리스너 기능 초기화(미완)

--- seunghee63/MyApplication/app/src/main/java/com/song2/myapplication/adapter/MovieViewHolder.kt
+++ seunghee63/MyApplication/app/src/main/java/com/song2/myapplication/adapter/MovieViewHolder.kt
@@ -13,8 +13,10 @@ import com.bumptech.glide.Glide
 import com.song2.myapplication.R
 import com.song2.myapplication.data.MovieData
 
+
+
 @Suppress("DEPRECATION")
-class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
 
     private val image: ImageView = view.findViewById(R.id.iv_rv_movie_item_image)
     private val title: TextView = view.findViewById(R.id.tv_rv_movie_item_title)
@@ -23,22 +25,34 @@ class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val pubDate: TextView = view.findViewById(R.id.tv_rv_movie_item_pub_date)
     private val actor: TextView = view.findViewById(R.id.tv_rv_movie_item_actor)
 
+    override fun onClick(view: View?) {
+        val pos = adapterPosition
+        Log.e("position",pos.toString())
+
+//        val url = movieData[pos].link
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+//        view!!.context.startActivity(intent)
+    }
+
+    //바인딩 될 때마다 호출 .. 리스너가 반복호출 될 필요는 없음
     fun onBind(movieData: MovieData) {
         title.text = Html.fromHtml(movieData.title)
         pubDate.text = movieData.pubDate
         actor.text = movieData.actor
         director.text = movieData.director
-        rating.rating = (movieData.userRating!!/2).toFloat()
+        rating.rating = (movieData.userRating!! / 2).toFloat()
 
         Glide.with(itemView.context).load(movieData.image) to image
 
+        onClick(itemView)
+/*
         itemView.setOnClickListener {
-            Log.e("geg",movieData.image)
+            Log.e("geg", movieData.image)
             val context = it.context
             val url = movieData.link
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             context.startActivity(intent)
         }
-
+*/
     }
 }
