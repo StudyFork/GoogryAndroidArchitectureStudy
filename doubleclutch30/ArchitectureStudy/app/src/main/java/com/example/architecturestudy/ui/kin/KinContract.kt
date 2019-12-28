8980c9e43705ff4c5ab26d4e1716249121c20a2a
doubleclutch30 <cometruth30@gmail.com>
2019-12-28 14:09:11 +0900

    HW-3 KinContract, KinPresenter 생성

---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/kin/KinContract.kt
@@ -0,0 +1,12 @@
+package com.example.architecturestudy.ui.kin
+
+interface KinContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/kin/KinPresenter.kt
@@ -0,0 +1,5 @@
+package com.example.architecturestudy.ui.kin
+
+class KinPresenter {
+
+}
