a4844494f2f0f009a70cd36e00318fc5e86caab2
doubleclutch30 <cometruth30@gmail.com>
2019-12-28 15:31:41 +0900

    HW-3 BlogContract, BlogPresenter 생성

---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/blog/BlgoContract.kt
@@ -0,0 +1,12 @@
+package com.example.architecturestudy.ui.blog
+
+interface BlogContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/blog/BlogPresenter.kt
@@ -0,0 +1,5 @@
+package com.example.architecturestudy.ui.blog
+
+class BlogPresenter {
+
+}
