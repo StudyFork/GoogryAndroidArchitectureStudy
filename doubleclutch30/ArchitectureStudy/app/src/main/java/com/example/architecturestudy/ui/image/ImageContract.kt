b22fcb78caa536d5dc96c0457304bac7a7d0f110
doubleclutch30 <cometruth30@gmail.com>
2019-12-28 15:14:59 +0900

    HW-3 ImageContract, ImagePresenter 생성

---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/image/ImageContract.kt
@@ -0,0 +1,12 @@
+package com.example.architecturestudy.ui.image
+
+interface ImageContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ doubleclutch30/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/image/ImagePresenter.kt
@@ -0,0 +1,5 @@
+package com.example.architecturestudy.ui.image
+
+class ImagePresenter {
+
+}
