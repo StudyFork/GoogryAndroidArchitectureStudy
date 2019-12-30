64ea1894f2f3126445a39cc76c9c884387dbf809
ahndding <ahndding@gmail.com>
2019-12-31 01:38:20 +0900

    Adapter 수정: 제네릭으로 변환

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/data/MovieModel.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/data/MovieModel.kt
@@ -2,11 +2,11 @@ package com.hansung.firstproject.data
 
 // 검색된 결과 중 한 movie에 대한 data class
 data class MovieModel(
-    val title: String?,
-    val link: String?,
-    val image: String?,
-    val pubDate: String?,
-    val director: String?,
-    val actor: String?,
-    val userRating: String?
+    val title: String,
+    val link: String,
+    val image: String,
+    val pubDate: String,
+    val director: String,
+    val actor: String,
+    val userRating: String
 )
