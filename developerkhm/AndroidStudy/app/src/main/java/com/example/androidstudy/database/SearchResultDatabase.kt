930d6a2aae664c42bc6d31be5180610b572857d1
김학명 <spingferry@gmail.com>
2019-12-30 15:06:47 +0900

    HW-4 room 사용databse 파일 추가

---
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/database/SearchResultDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.androidstudy.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.androidstudy.model.data.SearchResultEntity
+
+@Database(entities = [(SearchResultEntity::class)], version = 1)
+abstract class SearchResultDatabase : RoomDatabase() {
+
+    abstract fun SearchResultDao(): SearchResultDao
+
+    companion object {
+        private var INSTANCE: SearchResultDatabase? = null
+
+        fun getInstance(context: Context): SearchResultDatabase? {
+            if (INSTANCE == null) {
+                synchronized(SearchResultDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        SearchResultDatabase::class.java, "searchData.db"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+
+}
