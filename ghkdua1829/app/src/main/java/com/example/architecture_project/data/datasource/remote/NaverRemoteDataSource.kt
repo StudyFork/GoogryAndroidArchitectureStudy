63cdc105999b339f377623f32c7073a07a7c5555
ghkdua1829 <ghkdua059@gmail.com>
2019-12-31 13:37:40 +0900

    HW-2 RemoteDataSource 적용(데이터 받는 부분 분리)

--- ghkdua1829/app/src/main/java/com/example/architecture_project/data/datasource/remote/NaverRemoteDataSource.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/data/datasource/remote/NaverRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.example.architecture_project.data.datasource.remote
 
+import com.example.architecture_project.data.model.NaverApi
+
 interface NaverRemoteDataSource {
+    fun getMovieData(title: String, onResponse: (NaverApi) -> Unit, onFailure: (Throwable) -> Unit)
 }
