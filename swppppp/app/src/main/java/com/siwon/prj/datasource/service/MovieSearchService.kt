b2fafd3b478cb00bea0f4eb46e8fe6ca2cf128ec
swppppp <sw931110@naver.com>
2020-01-02 00:56:33 +0900

    - api서비스 및 서비스 생성 분리

---
+++ swppppp/app/src/main/java/com/siwon/prj/datasource/service/MovieSearchService.kt
@@ -0,0 +1,30 @@
+package com.siwon.prj.datasource.service
+
+import com.siwon.prj.model.ApiInfo
+import com.siwon.prj.model.Movies
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface MovieSearchService {
+    @GET("/v1/search/movie.json")
+    fun search(
+        @Header("X-Naver-Client-Id") Client_Id: String,
+        @Header("X-Naver-Client-Secret") Client_Secret: String,
+        @Query("query") input: String
+    ): Call<Movies>
+}
+
+object MovieSearchServiceImpl{
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(ApiInfo.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val service: MovieSearchService = retrofit.create(
+        MovieSearchService::class.java)
+}
