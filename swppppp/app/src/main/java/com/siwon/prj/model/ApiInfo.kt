3110340237cf024b7c1e394efd0f9f01736f68c7
swppppp <sw931110@naver.com>
2020-01-02 00:55:16 +0900

    - api정보 object로 분리

---
+++ swppppp/app/src/main/java/com/siwon/prj/model/ApiInfo.kt
@@ -0,0 +1,7 @@
+package com.siwon.prj.model
+
+object ApiInfo {
+    const val CLIENT_ID = "KjLtxBCCy8ZTWORQ7uas"
+    const val CLIENT_SECRET = "PHBD6IlPgd"
+    const val BASE_URL = "https://openapi.naver.com"
+}
