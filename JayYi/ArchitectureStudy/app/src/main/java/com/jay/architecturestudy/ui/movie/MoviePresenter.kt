ffc60e69321dd26ed5e561a397599a4303620504
JayYi <stkment@gmail.com>
2020-01-01 22:37:40 +0900

    HW-4 Movie 검색 후 결과 값 저장

--- JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/ui/movie/MoviePresenter.kt
+++ JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/ui/movie/MoviePresenter.kt
@@ -1,8 +1,12 @@
 package com.jay.architecturestudy.ui.movie
 
+import com.jay.architecturestudy.data.database.entity.MovieEntity
 import com.jay.architecturestudy.data.repository.NaverSearchRepositoryImpl
 import com.jay.architecturestudy.ui.BaseSearchPresenter
 import com.jay.architecturestudy.util.addTo
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MoviePresenter(
     override val view: MovieContract.View,
@@ -13,8 +17,32 @@ class MoviePresenter(
         repository.getMovie(
             keyword = keyword
         )
-            .subscribe({ responseMovie ->
-                val movies = responseMovie.movies
+            .map {
+                // 기존 결과 삭제
+                updateMovieResult { repository.clearMovieResult() }
+                it.movies.isNotEmpty().then {
+                    val movieList = arrayListOf<MovieEntity>()
+                    it.movies.mapTo(movieList) { movie ->
+                        MovieEntity(
+                            title = movie.title,
+                            link =  movie.link,
+                            image = movie.image,
+                            subtitle = movie.subtitle,
+                            director = movie.director,
+                            actor = movie.actor,
+                            pubDate = movie.pubDate,
+                            userRating = movie.userRating
+                        )
+                    }
+                    updateMovieResult {
+                        // 최신 결과 저장
+                        repository.saveMovieResult(movieList)
+                    }
+                }
+                repository.saveMovieKeyword(keyword)
+                it.movies
+            }
+            .subscribe({ movies ->
                 if (movies.isEmpty()) {
                     view.hideResultListView()
                     view.showEmptyResultView()
@@ -27,5 +55,12 @@ class MoviePresenter(
                 handleError(e)
             })
             .addTo(disposables)
+    }
+
+    private fun updateMovieResult(func: () -> Unit) {
+        Completable.fromCallable(func)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
     }
 }
