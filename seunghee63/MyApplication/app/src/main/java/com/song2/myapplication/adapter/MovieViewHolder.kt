3074b06271cbd848387f9d56dfd959e65e9abadc
seunghee63 <0603yang@naver.com>
2020-01-02 18:26:00 +0900

    HW-1 click 리스너를 viewHolder에서 초기화

--- seunghee63/MyApplication/app/src/main/java/com/song2/myapplication/adapter/MovieViewHolder.kt
+++ seunghee63/MyApplication/app/src/main/java/com/song2/myapplication/adapter/MovieViewHolder.kt
@@ -18,6 +18,8 @@ import com.song2.myapplication.data.MovieData
 @Suppress("DEPRECATION")
 class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
 
+    lateinit var tempData : MovieData
+
     private val image: ImageView = view.findViewById(R.id.iv_rv_movie_item_image)
     private val title: TextView = view.findViewById(R.id.tv_rv_movie_item_title)
     private val rating: RatingBar = view.findViewById(R.id.rb_rv_movie_item_rating)
@@ -25,22 +27,19 @@ class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickL
     private val pubDate: TextView = view.findViewById(R.id.tv_rv_movie_item_pub_date)
     private val actor: TextView = view.findViewById(R.id.tv_rv_movie_item_actor)
 
-    init{
-        //itemView.
-
-    }
 
     override fun onClick(view: View?) {
         val pos = adapterPosition
         Log.e("position",pos.toString())
 
-//        val url = movieData[pos].link
-//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-//        view!!.context.startActivity(intent)
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(tempData.link))
+        this.itemView.context.startActivity(intent)
     }
 
     //바인딩 될 때마다 호출 .. 리스너가 반복호출 될 필요는 없음
     fun onBind(movieData: MovieData) {
+        tempData = movieData
+
         title.text = Html.fromHtml(movieData.title)
         pubDate.text = movieData.pubDate
         actor.text = movieData.actor
@@ -49,15 +48,8 @@ class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickL
 
         Glide.with(itemView.context).load(movieData.image).into(image)
 
-        onClick(itemView)
-/*
         itemView.setOnClickListener {
-            Log.e("geg", movieData.image)
-            val context = it.context
-            val url = movieData.link
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            context.startActivity(intent)
+            onClick(itemView)
         }
-*/
     }
 }
