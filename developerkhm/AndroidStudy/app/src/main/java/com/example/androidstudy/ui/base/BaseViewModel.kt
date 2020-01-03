ee5bf33c7941455cc109dd37de88b45c1abe730b
김학명 <spingferry@gmail.com>
2020-01-03 12:49:54 +0900

    HW-6 ViewModel 검색어 저장 쿼리, 검색결과 리스트 ObserableField 변수 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.androidstudy.ui.base
 
-class BaseViewModel {
+import androidx.databinding.ObservableField
+import com.example.androidstudy.model.data.Item
+import java.util.*
 
+class BaseViewModel {
+    var query : ObservableField<String> = ObservableField()
+    var searchResult : ObservableField<ArrayList<Item>> = ObservableField()
 }
