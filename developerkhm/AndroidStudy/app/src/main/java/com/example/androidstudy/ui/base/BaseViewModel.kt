0458234549080fd25e8109a1b4f7e6b447e8c0e8
김학명 <spingferry@gmail.com>
2020-01-03 17:07:55 +0900

    HW-6 ViewModel로 비즈니스 로직 추가 search, 기타

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
@@ -4,19 +4,69 @@ import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.databinding.ObservableField
+import com.example.androidstudy.database.SearchResultDatabase
 import com.example.androidstudy.model.data.Item
+import com.example.androidstudy.model.data.SearchResultEntity
+import com.example.androidstudy.model.data.TotalModel
+import com.example.androidstudy.model.repository.NaverDataRepositoryImpl
+import com.google.gson.Gson
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import java.util.*
 
 class BaseViewModel {
+
+    lateinit var type : String
     var query : ObservableField<String> = ObservableField()
     var searchResult : ObservableField<ArrayList<Item>> = ObservableField()
 
+    var onLoading : ObservableField<Boolean> = ObservableField()
+    var noDataError : ObservableField<String> = ObservableField()
 
+    private fun search(query: String?, type: String) {
+        NaverDataRepositoryImpl.getNaverSearchData(
+            type,
+            query!!)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                onLoading.set(true)
+            }
+            .doAfterTerminate {
+                onLoading.set(false)
+            }
+            .doAfterSuccess {
+//                insertSeachResult(
+//                    view.loadLocalDatabase(),
+//                    SearchResultEntity(null, type, query, Gson().toJson(it?.items))
+//                )
+            }
+            .subscribe(::onSuccess, ::onError)
+//            .addDisposable()
+    }
+
+    private fun onSuccess(result: TotalModel) {
+        if (result.items.isNullOrEmpty()) {
+            noDataError.set("데이터가 없습니다.")
+        } else {
+            noDataError.set("")
+//            view.onDataChanged(result.items)
+        }
+    }
+
+    private fun onError(t: Throwable) {
+        println(t.message)
+//        view.showErrorMessage(t.message)
+    }
+
+    fun insertSeachResult(searchResultDatabase: SearchResultDatabase?, searchResult: SearchResultEntity) {
+        NaverDataRepositoryImpl.setLocalSearchData(searchResultDatabase, searchResult)
+    }
 
     fun onEditorActionClicked(view: TextView, actionId: Int?, event: KeyEvent?) : Boolean {
         when(actionId) {
             EditorInfo.IME_ACTION_SEARCH -> {
-
+                search(view.text.toString(), this.type)
             }
         }
         return false
--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/blog/BlogFragment.kt
@@ -6,9 +6,13 @@ import android.view.inputmethod.EditorInfo
 import com.example.androidstudy.R
 import com.example.androidstudy.databinding.FragmentBlogBinding
 import com.example.androidstudy.ui.base.BaseFragment
+import com.example.androidstudy.ui.base.BaseViewModel
 import com.ironelder.androidarchitecture.view.AdapterSearch
+import java.util.*
 
 class BlogFragment : BaseFragment<FragmentBlogBinding>(R.layout.fragment_blog) {
+
+    private lateinit var viewModel : BaseViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -16,6 +20,31 @@ class BlogFragment : BaseFragment<FragmentBlogBinding>(R.layout.fragment_blog) {
     }
 
     private fun init() {
+
+        //TODO ViewModel이 여러개 뷰를 가질 수 있지만 화면당 하나의 뷰모델이 맞는건가 싶어서?
+        //검색에 사용할, type를 주 생성자로 해줘야 하는지? 이렇게 하는게 좋은지... 모르...???
+
+        viewModel = BaseViewModel()
+        viewModel.type = typeArray[0]
+        binding.vm = viewModel
+
+
+
+        viewModel.onLoading.addOnPropertyChangedCallback(object : androidx.databinding.Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(p0: androidx.databinding.Observable?, p1: Int) {
+
+                viewModel.onLoading.get().let {
+                    if(it!!){
+                        //TODO 로딩중~~
+                    }else{
+                        //TODO 로딩 아님~~~
+                    }
+                }
+           }
+        })
+
+        viewModel.noDataError.addOnPropertyChangedCallback()
+
         setLayout()
     }
 
@@ -35,4 +64,8 @@ class BlogFragment : BaseFragment<FragmentBlogBinding>(R.layout.fragment_blog) {
         binding.searchLayout.resultRecyclerView.adapter =
             AdapterSearch(context, arrayListOf(), "blog")
     }
+}
+
+private fun Observable.addOnPropertyChangedCallback(function: () -> Unit) {
+
 }
