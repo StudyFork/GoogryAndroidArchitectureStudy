a407e017ee7f6fb9ab67aa2f44f38777c549bee1
김학명 <spingferry@gmail.com>
2020-01-03 13:20:10 +0900

    HW-6 에딧텍스트 검색확인을 위한 editorAction BindingMethods 추가

--- developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
+++ developerkhm/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/base/BaseViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.androidstudy.ui.base
 
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.databinding.ObservableField
 import com.example.androidstudy.model.data.Item
 import java.util.*
@@ -7,4 +10,16 @@ import java.util.*
 class BaseViewModel {
     var query : ObservableField<String> = ObservableField()
     var searchResult : ObservableField<ArrayList<Item>> = ObservableField()
+
+
+
+    fun onEditorActionClicked(view: TextView, actionId: Int?, event: KeyEvent?) : Boolean {
+        when(actionId) {
+            EditorInfo.IME_ACTION_SEARCH -> {
+
+            }
+        }
+        return false
+    }
+
 }
