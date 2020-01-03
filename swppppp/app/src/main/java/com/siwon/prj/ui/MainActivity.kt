4ecbbc5aac4a11ea7260cc1aa0577415ea0cb44f
sw931110 <sw931110@naver.com>
2020-01-03 10:13:00 +0900

    - 주석제거

--- swppppp/app/src/main/java/com/siwon/prj/ui/MainActivity.kt
+++ swppppp/app/src/main/java/com/siwon/prj/ui/MainActivity.kt
@@ -12,11 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    private val CLIENT_ID = "KjLtxBCCy8ZTWORQ7uas"
-//    private val CLIENT_SECRET = "PHBD6IlPgd"
-//    private val BASE_URL = "https://openapi.naver.com"
-//    lateinit var retrofit: Retrofit
-//    lateinit var apiService: MovieSearchService
     lateinit var repository: MovieSearchRepository
     lateinit var mAdapter: MovieAdapter
     var toast: Toast? = null
@@ -26,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         toast = Toast.makeText(this, "", Toast.LENGTH_SHORT)
-//        apiServiceSet()
         mAdapter = MovieAdapter { link: String -> itemClick(link) }
         repository = MovieSearchRepositoryImpl()
 
@@ -48,21 +42,6 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 getMovies(edit_text_input.text.toString())
-//                apiService.search(CLIENT_ID, CLIENT_SECRET, edit_text_input.text.toString()).enqueue(object : Callback<Movies>{
-//                    override fun onFailure(call: Call<Movies>, t: Throwable) {
-//                        Log.i("로그로그", "####실패메시지: ${t.message}")
-//                        toast?.let {
-//                            it.setText("검색에 실패하였습니다.")
-//                            it.show()
-//                        }
-//                    }
-//
-//                    override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
-//                        Log.i("로그로그", "####응답: ${response.body().toString()}\n ${response.message()}")
-//                        mAdapter.setItems(response.body()!!.movies)
-//                        movieListRv.adapter = mAdapter
-//                    }
-//                })
             }
         }
     }
@@ -77,13 +56,6 @@ class MainActivity : AppCompatActivity() {
                 Log.e("에러", "에러메시지: ${it.message}")
             })
     }
-//    fun apiServiceSet() {
-//        retrofit = Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//        apiService = retrofit.create(MovieSearchService::class.java)
-//    }
 
     fun itemClick(link: String){
         val detailWebview = Intent(this@MainActivity, DetailWebview::class.java)
