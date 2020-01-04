b958f47ba4f1d5321422359b4eb1deb3a025758d
ahndding <ahndding@gmail.com>
2020-01-04 21:36:12 +0900

    HW-2 제네릭 뷰홀더 형태로 변경

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/MovieHolder.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/MovieHolder.kt
@@ -4,47 +4,50 @@ import android.content.Intent
 import android.net.Uri
 import android.text.Html
 import android.view.View
-import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.hansung.firstproject.MovieInformationActivity
 import com.hansung.firstproject.data.MovieModel
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-
-//MovieView 재사용하는 Holder
 @Suppress("DEPRECATION")
-class MovieHolder(private val movieView: View) : RecyclerView.ViewHolder(movieView) {
+open class MovieHolder(layoutId: Int, parents: ViewGroup) :
+    BaseViewHolder<View>(layoutId, parents) {
+
     private lateinit var webPage: Uri
 
     init {
         itemView.setOnClickListener {
             val webIntent = Intent(
-                movieView.context,
+                it.context,
                 MovieInformationActivity::class.java
             ).apply {
                 putExtra("WEB_PAGE", webPage.toString())
             }
-            movieView.context.startActivity(webIntent)
+            it.context.startActivity(webIntent)
         }
     }
 
-    fun bindItems(data: MovieModel) {
-        //image 바인딩
-        with(itemView) {
-            // userRating(별점)의 경우 단순 문자열로 전달됨. 이를 실수형으로 변환한 뒤 10점만점을 별 5개 기준으로 변경하기 위해 2로 나누고 이를 기반으로 ratingBar(별점)의 값을 변경한다.
-            grade_movieitem.rating = data.userRating.toFloat() / 2
-            releasedate_movieitem.text = data.pubDate
-            // 검색어와 일치하는 부분은 태그로 감싸져 오는 제목 String에서 HTML Tag를 제거하는 method
-            title_movieitem.text = Html.fromHtml(data.title, Html.FROM_HTML_MODE_LEGACY)
-            director_movieitem.text = data.director
-            actor_movieitem.text = data.actor
+    override fun <T> bindItems(item: T) {
+        if (item is MovieModel) {
+            //image 바인딩
+            with(itemView) {
+                // userRating(별점)의 경우 단순 문자열로 전달됨. 이를 실수형으로 변환한 뒤 10점만점을 별 5개 기준으로 변경하기 위해 2로 나누고 이를 기반으로 ratingBar(별점)의 값을 변경한다.
+                grade_movieitem.rating = item.userRating.toFloat() / 2
+                releasedate_movieitem.text = item.pubDate
+                // 검색어와 일치하는 부분은 태그로 감싸져 오는 제목 String에서 HTML Tag를 제거하는 method
+                title_movieitem.text = Html.fromHtml(item.title, Html.FROM_HTML_MODE_LEGACY)
+                director_movieitem.text = item.director
+                actor_movieitem.text = item.actor
 
-            Glide.with(context).load(data.image).apply(RequestOptions().override(300, 450))
-                .apply(RequestOptions.centerCropTransform()).into(posterimage_movieitem)
+                Glide.with(context).load(item.image).apply(RequestOptions().override(300, 450))
+                    .apply(RequestOptions.centerCropTransform()).into(posterimage_movieitem)
 
-            //클릭시 웹사이트 연결을 위한 uri 바인딩
-            webPage = Uri.parse(data.link)
+                //클릭시 웹사이트 연결을 위한 uri 바인딩
+                webPage = Uri.parse(item.link)
+            }
         }
+
     }
 }
