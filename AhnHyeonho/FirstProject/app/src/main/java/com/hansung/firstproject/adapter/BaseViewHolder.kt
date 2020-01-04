e938f0d06f98637dc937ff9cce4d67a0b11aa7c0
ahndding <ahndding@gmail.com>
2020-01-04 21:35:41 +0900

    HW-2 제네릭 뷰홀더 적용을 위한 base클래스 생성

---
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/BaseViewHolder.kt
@@ -0,0 +1,13 @@
+package com.hansung.firstproject.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder<T>(@LayoutRes layoutId: Int, parents: ViewGroup) :
+    RecyclerView.ViewHolder(
+        LayoutInflater.from(parents.context).inflate(layoutId, parents, false)
+    ) {
+    abstract fun <T> bindItems(item: T)
+}
