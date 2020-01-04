a8a5fd5b303ec9e9c114530bbf08e1ec29d680ac
SuminGo <jennifer045020@gmail.com>
2020-01-03 23:45:30 -0800

    Create RecyclerView

--- class08/L91519/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ class08/L91519/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,3 +10,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
---
+++ class08/L91519/My Application/app/src/main/java/com/example/myapplication/MovieRecyclerViewAdpater.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieRecyclerViewAdpater : RecyclerView.Adapter<MovieRecyclerViewAdpater.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieRecyclerViewAdpater.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MovieRecyclerViewAdpater.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
+
+    }
+}
