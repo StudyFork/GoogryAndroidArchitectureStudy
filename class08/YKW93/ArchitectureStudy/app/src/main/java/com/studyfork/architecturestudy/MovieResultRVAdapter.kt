72162dad0fe26d2a02693a3ccd30cedab4aabebf
YKW93 <rhkddn1657@gmail.com>
2020-01-05 17:44:40 +0900

    HW-1 html text 적용 및 rating 값 수정

--- class08/YKW93/ArchitectureStudy/app/src/main/java/com/studyfork/architecturestudy/MovieResultRVAdapter.kt
+++ class08/YKW93/ArchitectureStudy/app/src/main/java/com/studyfork/architecturestudy/MovieResultRVAdapter.kt
@@ -1,5 +1,8 @@
 package com.studyfork.architecturestudy
 
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,11 +36,19 @@ class MovieResultRVAdapter : RecyclerView.Adapter<MovieResultRVAdapter.MovieResu
 
         fun bind(item: MovieResponse.Item) {
             Glide.with(itemView.context).load(item.image).into(itemView.iv_movie_image)
-            itemView.tv_movie_name.text = item.title
-            itemView.rb_movie_rating.rating = item.userRating
+            itemView.tv_movie_name.text = item.title.getHtmlText()
+            itemView.rb_movie_rating.rating = item.userRating / 2f
             itemView.tv_movie_publish_date.text = item.pubDate
             itemView.tv_movie_director.text = item.director
             itemView.tv_movie_actor.text = item.actor
+        }
+    }
+
+    private fun String.getHtmlText(): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(this)
         }
     }
 }
