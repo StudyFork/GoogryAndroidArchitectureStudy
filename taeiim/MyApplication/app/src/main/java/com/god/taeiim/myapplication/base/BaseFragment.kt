9712294364801f65d8c4da7b43d0680b43e8f1a6
taeiim <taeiim.dev@gmail.com>
2020-01-05 23:11:34 +0900

    HW-5 BaseFragment에서 view inflate 중복 제거

--- taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseFragment.kt
+++ taeiim/MyApplication/app/src/main/java/com/god/taeiim/myapplication/base/BaseFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 
 abstract class BaseFragment<B : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int
-) : Fragment(layoutResId) {
+) : Fragment() {
 
     protected lateinit var binding: B
 
