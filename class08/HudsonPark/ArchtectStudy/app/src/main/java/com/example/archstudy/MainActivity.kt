5dfd728089fbc0979ed3a9bdcaab5e0086b1cc06
Hudson Park <logishudson0218@gmail.com>
2020-01-05 13:46:18 +0900

    Refectoring MainActivity Class And Init initEvent() func

--- class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MainActivity.kt
+++ class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MainActivity.kt
@@ -18,13 +18,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initView() // 뷰 초기화
+        initEvent() // 이벤트 처리
+    }
 
+    private fun initEvent() {
         btnSearch.setOnClickListener {
             // 검색 버튼 클릭 시
-
             var query = edtQuery.text.toString()
-            if (query.isEmpty()){
-                Toast.makeText(this,"검색어를 다시 입력해주세요.",Toast.LENGTH_SHORT).show()
+            if (query.isEmpty()) {
+                Toast.makeText(this, "검색어를 다시 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
                 // 네이버 영화 정보 API 요청
             }
