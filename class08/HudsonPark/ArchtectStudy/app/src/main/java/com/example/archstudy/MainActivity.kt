7fbd1a87a0e2f817aadccc819b3a1b2eb41e3d87
Hudson Park <logishudson0218@gmail.com>
2020-01-05 12:45:58 +0900

    Edit Views In activity_main

--- class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MainActivity.kt
+++ class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.archstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtQuery: EditText
+    private lateinit var btnSearch: Button
+    private lateinit var rvMovieList : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView() // 뷰 초기화
+
+        btnSearch.setOnClickListener {
+            // 검색 버튼 클릭 시
+
+            var query = edtQuery.text.toString()
+            if (query.isEmpty()){
+                Toast.makeText(this,"검색어를 다시 입력해주세요.",Toast.LENGTH_SHORT).show()
+            } else {
+                // 네이버 영화 정보 API 요청
+            }
+        }
+    }
+
+    private fun initView() {
+        edtQuery = findViewById(R.id.edtQuery)
+        btnSearch = findViewById(R.id.btnSearch)
+        rvMovieList = findViewById(R.id.rvMovieList)
     }
 }
