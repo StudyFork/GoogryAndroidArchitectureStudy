2cc393c1e260979165039395f0ed7269ab52dd45
handnew <handnew04@gmail.com>
2020-01-07 01:49:23 +0900

    HW-3 searchMovie 함수 분리

--- handnew04/app/src/main/java/com/example/handnew04/ui/main/MainActivity.kt
+++ handnew04/app/src/main/java/com/example/handnew04/ui/main/MainActivity.kt
@@ -2,20 +2,19 @@ package com.example.handnew04.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.handnew04.ui.movie.MovieDetailActivity
-import com.example.handnew04.adapter.MovieRecyclerAdapter
 import com.example.handnew04.R
-import com.example.handnew04.data.MovieRepository
+import com.example.handnew04.adapter.MovieRecyclerAdapter
+import com.example.handnew04.data.NaverMovieResponse
 import com.example.handnew04.data.items
+import com.example.handnew04.ui.movie.MovieDetailActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity(val presenter: MainContract.Presenter) : AppCompatActivity(), MainContract.View {
     lateinit var recyclerAdapter: MovieRecyclerAdapter
-    lateinit var movieRepository: MovieRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +26,12 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
 
     private fun initailize() {
-        movieRepository = MovieRepository()
-
         recyclerAdapter = MovieRecyclerAdapter()
+
         recyclerAdapter.setItemClickListener(object :
             MovieRecyclerAdapter.ItemClickListener {
             override fun onClick(view: View, position: Int) {
-                val nextIntent = Intent(this@MainActivity, MovieDetailActivity::class.java)
-                nextIntent.putExtra(
-                    getString(R.string.movieLink),
-                    recyclerAdapter.getMovieLink(position)
-                )
-                startActivity(nextIntent)
+
             }
         })
 
@@ -48,34 +41,34 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     private fun setEventHandler() {
         btn_searchButton.setOnClickListener {
             val inputText = et_searchBar.text.toString()
-            if (inputText.isNotEmpty()) searchMovie(inputText)
+            presenter.serchMovie(inputText)
         }
     }
 
-
-    private fun searchMovie(inputText: String) {
-        movieRepository.getMovieData(inputText
-            , success = { recyclerAdapter.setItemList(it.items as ArrayList<items>) },
-            fail = { Log.e("NaverMovieApi Fail ", it.message) })
-    }
-
     override fun showEmptyResult() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
     }
 
     override fun showInputLengthZero() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this@MainActivity, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
     }
 
     override fun showNotConnectedNetwork() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun showSuccessSearchMovie() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun showSuccessSearchMovie(data: NaverMovieResponse) {
+        recyclerAdapter.setItemList(data.items as ArrayList<items>)
     }
 
-    override fun showMovieDetailActivity() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun showMovieDetailActivity(position : Int) {
+        val nextIntent = Intent(this@MainActivity, MovieDetailActivity::class.java)
+        nextIntent.putExtra(
+            getString(R.string.movieLink),
+            recyclerAdapter.getMovieLink(position)
+        )
+        startActivity(nextIntent)
     }
+
+    override fun getContext() = this@MainActivity
 }
