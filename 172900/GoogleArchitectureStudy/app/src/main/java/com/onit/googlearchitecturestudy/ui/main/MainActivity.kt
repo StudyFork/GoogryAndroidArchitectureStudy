6737789f79c431fd76f5fa37e2c48dac73ea9efd
172900 <dlsldlrpdho123@gmail.com>
2020-01-07 17:31:17 +0900

    HW-3 MainContract interface에 맞게 MainActivity리팩토링

--- 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/ui/main/MainActivity.kt
+++ 172900/GoogleArchitectureStudy/app/src/main/java/com/onit/googlearchitecturestudy/ui/main/MainActivity.kt
@@ -3,38 +3,56 @@ package com.onit.googlearchitecturestudy.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.onit.googlearchitecturestudy.Movie
 import com.onit.googlearchitecturestudy.R
-import com.onit.googlearchitecturestudy.data.repository.MovieRepository
-import com.onit.googlearchitecturestudy.data.repository.MovieRepositoryImpl
 import com.onit.googlearchitecturestudy.ui.movieInformation.MovieInformationActivity
 import com.onit.googlearchitecturestudy.ui.movieInformation.MovieInformationActivity.Companion.MOVIE_URL
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
 
-    private val movieRepositroy: MovieRepository = MovieRepositoryImpl()
     private lateinit var resultMovieListRecyclerAdapter: ResultMovieListRecyclerAdapter
+    private lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
+        presenter = MainPresenter(this)
         init()
+    }
+
+    override fun hideKeyBoard() {
+        val view = this.currentFocus
+        view?.let { v ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(v.windowToken, 0)
+        }
+    }
+
+    override fun showLoadingProgressBar() {
+        loadingProgressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideLoadingProgressBar() {
+        loadingProgressBar.visibility = View.GONE
+    }
+
+    override fun showToastMessage(message: String, option: Int) {
+        Toast.makeText(applicationContext, message, option).show()
+    }
+
+    override fun setMovieList(movieList: List<Movie>) {
+        resultMovieListRecyclerAdapter.setMovieList(movieList)
     }
 
     private fun init() {
         searchButton.setOnClickListener {
-            clickSearchButton()
+            presenter.searchMovies(searchFieldEditText.text.toString())
         }
         setRecyclerView()
     }
@@ -54,39 +72,5 @@ class MainActivity : AppCompatActivity() {
             putExtra(MOVIE_URL, resultMovieListRecyclerAdapter.getMovieURL(position))
         }
         startActivity(intent)
-    }
-
-    private fun clickSearchButton() {
-        hideKeyboard()
-
-        CoroutineScope(Dispatchers.Main).launch {
-            loadingProgressBar.visibility = View.VISIBLE
-
-            val searchKeyword = searchFieldEditText.text.toString()
-            val response =
-                withContext(Dispatchers.IO) { movieRepositroy.getMovieList(searchKeyword) }
-
-            if (response.isSuccessful) {
-                val movieList = response.body()?.movies
-                if (movieList == null) {
-                    Toast.makeText(applicationContext, "검색결과가 없습니다.", Toast.LENGTH_LONG).show()
-                } else {
-                    resultMovieListRecyclerAdapter.setMovieList(movieList)
-                }
-            } else {
-                Log.e("MainActivity", "네이버 API요청에 실패했습니다. responseCode = ${response.code()}")
-                Toast.makeText(applicationContext, "네트워크가 불안정합니다.", Toast.LENGTH_LONG).show()
-            }
-
-            loadingProgressBar.visibility = View.GONE
-        }
-    }
-
-    private fun hideKeyboard() {
-        val view = this.currentFocus
-        view?.let { v ->
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            imm?.hideSoftInputFromWindow(v.windowToken, 0)
-        }
     }
 }
