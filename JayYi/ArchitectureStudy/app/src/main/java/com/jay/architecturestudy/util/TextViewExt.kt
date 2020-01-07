418e047fb6085e07e3f49e8bd0f67dc42b46d801
JayYi <stkment@gmail.com>
2020-01-08 02:00:08 +0900

    HW-5 binding adapter value 변경

--- JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/util/TextViewExt.kt
+++ JayYi/ArchitectureStudy/app/src/main/java/com/jay/architecturestudy/util/TextViewExt.kt
@@ -4,7 +4,7 @@ import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("android:text")
-fun TextView.setText(title: String) {
+@BindingAdapter("android:textFromHtml")
+fun TextView.setTextFromHtml(title: String) {
     text = HtmlCompat.fromHtml(title, HtmlCompat.FROM_HTML_MODE_COMPACT)
 }
