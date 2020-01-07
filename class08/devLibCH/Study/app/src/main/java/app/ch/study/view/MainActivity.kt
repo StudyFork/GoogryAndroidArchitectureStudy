092ac45d07dc0573061d0e9d1e26ae8ea55d86ee
ach <poohanh1@gmail.com>
2020-01-08 01:49:10 +0900

    view 수정

--- class08/devLibCH/Study/app/src/main/java/app/ch/study/view/MainActivity.kt
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/view/MainActivity.kt
@@ -1,24 +1,59 @@
 package app.ch.study.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import app.ch.study.R
 import app.ch.study.core.BaseActivity
+import app.ch.study.data.db.entitiy.MovieModel
 import app.ch.study.databinding.ActivityMainBinding
+import app.ch.study.viewmodel.MovieViewModel
+import app.ch.study.viewmodel.MovieViewModelFactory
+import org.koin.android.ext.android.inject
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
+    private val movieViewModelFactory: MovieViewModelFactory by inject()
+
+    private var adapter: MovieAdapter? = null
+
     override fun getLayoutResId(): Int = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        binding.view = this
         binding.lifecycleOwner = this
 
-        initLayout()
+        initEvent()
     }
 
-    private fun initLayout() {
+    private fun initEvent() {
+        val movieViewModel = ViewModelProviders.of(this, movieViewModelFactory).get(
+            MovieViewModel::class.java)
 
+        binding.viewModel = movieViewModel
+
+        movieViewModel.error.observe(this, Observer {
+            error -> Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+        })
+
+        movieViewModel.movieList.observe(this, Observer {
+            list ->
+
+            if(adapter == null) {
+                adapter = MovieAdapter(this, list as ArrayList<MovieModel>)
+                binding.rvMovie?.adapter = adapter
+            } else {
+                adapter?.replaceAll(list as ArrayList<MovieModel>)
+                adapter?.notifyDataSetChanged()
+            }
+        })
+    }
+
+    fun searchMovie() {
+        val name = binding.etSearch.text.toString()
+        binding.viewModel?.searchMovie(name)
     }
 }
---
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/view/MovieAdapter.kt
@@ -0,0 +1,54 @@
+package app.ch.study.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import app.ch.study.BR
+import app.ch.study.R
+import app.ch.study.data.db.entitiy.MovieModel
+import app.ch.study.databinding.ListItemMovieBinding
+import com.bumptech.glide.Glide
+
+class MovieAdapter(private val context: Context, private val list: ArrayList<MovieModel>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    fun replaceAll(items: ArrayList<MovieModel>) {
+        items.let {
+            this.list.run {
+                clear()
+                addAll(it)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MovieViewHolder(parent.context, R.layout.list_item_movie, parent)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) =
+        (holder as MovieViewHolder).onBindViewHolder(list[position])
+
+    inner class MovieViewHolder(
+        context: Context, view: Int, parent: ViewGroup
+    ) : RecyclerView.ViewHolder(
+        LayoutInflater.from(context).inflate(view, parent, false)
+    ) {
+        private val binding: ListItemMovieBinding? = DataBindingUtil.bind(itemView)
+
+        fun onBindViewHolder(item: MovieModel) {
+            binding?.setVariable(BR.item, item)
+
+            binding?.ivPoster?.let {
+                Glide
+                    .with(context)
+                    .load(item.image)
+                    .centerCrop()
+                    .into(it)
+            }
+        }
+    }
+
+}
