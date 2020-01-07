20cdf3ce1508b168e7766be8b0eff01d0a195db4
Jason <ironelder@gmail.com>
2020-01-07 17:34:59 +0900

    HW-7-AAC ViewModel적용을 위한 Base에 ViewModel상속
    HW-7-AAC ViewModel적용을 위한 Base에 ViewModel상속

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/baseview/BaseViewModel.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/view/baseview/BaseViewModel.kt
@@ -1,9 +1,10 @@
 package com.ironelder.androidarchitecture.view.baseview
 
+import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-abstract class BaseViewModel {
+abstract class BaseViewModel : ViewModel(){
 
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
@@ -12,6 +13,11 @@ abstract class BaseViewModel {
     }
 
     open fun clearDisposable() {
+        compositeDisposable.clear()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
         compositeDisposable.clear()
     }
 }
