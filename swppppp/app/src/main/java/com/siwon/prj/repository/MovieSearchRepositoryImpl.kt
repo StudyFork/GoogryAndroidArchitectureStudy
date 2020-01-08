495e37348e6747f9f18b321d829ab1011b7ff07c
sw931110 <sw931110@naver.com>
2020-01-08 17:07:22 +0900

    - 메인프레젠트 작업중

--- swppppp/app/src/main/java/com/siwon/prj/repository/MovieSearchRepositoryImpl.kt
+++ swppppp/app/src/main/java/com/siwon/prj/repository/MovieSearchRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.siwon.prj.repository
 
 import com.siwon.prj.datasource.RemoteMovieSearchDataSource
-import com.siwon.prj.datasource.RemoteMovieSearchDataSourceImpl
 import com.siwon.prj.model.Movie
 
 class MovieSearchRepositoryImpl(val dataSorceRemote: RemoteMovieSearchDataSource): MovieSearchRepository {
