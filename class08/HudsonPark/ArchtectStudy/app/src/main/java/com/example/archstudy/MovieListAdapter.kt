7d457483ecde37e78da01fcd28581c602dd14296
Hudson Park <logishudson0218@gmail.com>
2020-01-09 15:43:35 +0900

    HW-1 Correct Code Style Using Glide

--- class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MovieListAdapter.kt
+++ class08/HudsonPark/ArchtectStudy/app/src/main/java/com/example/archstudy/MovieListAdapter.kt
@@ -49,6 +49,7 @@ class MovieListAdapter(private var movieList: List<Item>, @Nullable private var 
                 // 아이템 클릭 시 호출될 커스텀 리스너 정의
                 layoutItem.setOnClickListener {
                     listener.onItemClick(link)
+
                 }
             }
 
@@ -69,11 +70,13 @@ class MovieListAdapter(private var movieList: List<Item>, @Nullable private var 
         private fun loadImage(image: String) {
 
             if (image.trim().isNotEmpty()) {
+
                 Glide.with(itemView.context)
                     .load(image)
                     .override(600, 1000)
                     .into(ivThumbnail)
             } else {
+
                 Glide.with(itemView.context)
                     .load(R.drawable.no_image)
                     .override(600, 1000)
