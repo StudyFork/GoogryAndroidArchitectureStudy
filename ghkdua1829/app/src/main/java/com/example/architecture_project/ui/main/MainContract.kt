601703c9887aea4b5636587b4ab97899af4bb740
ghkdua1829 <ghkdua059@gmail.com>
2020-01-10 00:26:52 +0900

    HW-3 함수 이름 변경

--- ghkdua1829/app/src/main/java/com/example/architecture_project/ui/main/MainContract.kt
+++ ghkdua1829/app/src/main/java/com/example/architecture_project/ui/main/MainContract.kt
@@ -4,13 +4,15 @@ import com.example.architecture_project.data.model.NaverApi
 
 interface MainContract {
     interface View {
-        fun noShowResult()
+        fun showNoResult()
         fun notAvailableKeyword()
         fun showResult(data: NaverApi)
-        fun noKeyword()
+        fun showNotExistKeyword()
+        fun showDataNum(num:Int)
     }
 
     interface Presenter {
         fun getMovieData(keyword: String)
+        fun getMovieDataNum(data:NaverApi)
     }
 }
