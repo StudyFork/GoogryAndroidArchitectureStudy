a6f035f3e7534766cef9e99e85e349e265537011
mino <k3000js@naver.com>
2020-01-11 16:36:19 +0900

    HW-2 Remote 추가

--- class08/Minoyam/app/src/main/java/com/cnm/homework/data/source/remote/NaverQueryRemoteDataSource.kt
+++ class08/Minoyam/app/src/main/java/com/cnm/homework/data/source/remote/NaverQueryRemoteDataSource.kt
@@ -1,4 +1,9 @@
 package com.cnm.homework.data.source.remote
 
+import com.cnm.homework.data.model.NaverResponse
+import io.reactivex.Single
+
 interface NaverQueryRemoteDataSource {
+    fun getNaverMovie(query: String): Single<NaverResponse>
+
 }
--- class08/Minoyam/app/src/main/java/com/cnm/homework/data/source/remote/NaverQueryRemoteDataSourceImpl.kt
+++ class08/Minoyam/app/src/main/java/com/cnm/homework/data/source/remote/NaverQueryRemoteDataSourceImpl.kt
@@ -1,2 +1,12 @@
 package com.cnm.homework.data.source.remote
 
+import com.cnm.homework.data.model.NaverResponse
+import com.cnm.homework.data.source.remote.network.NetworkHelper
+import io.reactivex.Single
+
+class NaverQueryRemoteDataSourceImpl : NaverQueryRemoteDataSource
+{
+    override fun getNaverMovie(query: String): Single<NaverResponse> {
+        return NetworkHelper.naverApi.getNaverMovie(query)
+    }
+}
