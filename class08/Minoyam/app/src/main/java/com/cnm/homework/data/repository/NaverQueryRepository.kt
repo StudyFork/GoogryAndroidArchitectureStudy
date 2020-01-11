724135bacdb9dfe31ff577d62083d791b8c22bb5
mino <k3000js@naver.com>
2020-01-11 16:36:50 +0900

    HW-2 Repository 추가

--- class08/Minoyam/app/src/main/java/com/cnm/homework/data/repository/NaverQueryRepository.kt
+++ class08/Minoyam/app/src/main/java/com/cnm/homework/data/repository/NaverQueryRepository.kt
@@ -1,4 +1,8 @@
 package com.cnm.homework.data.repository
 
+import com.cnm.homework.data.model.NaverResponse
+import io.reactivex.Single
+
 interface NaverQueryRepository {
+    fun getNaverMovie(query: String): Single<NaverResponse>
 }
--- class08/Minoyam/app/src/main/java/com/cnm/homework/data/repository/NaverQueryRepositoryImpl.kt
+++ class08/Minoyam/app/src/main/java/com/cnm/homework/data/repository/NaverQueryRepositoryImpl.kt
@@ -1,2 +1,12 @@
 package com.cnm.homework.data.repository
 
+import com.cnm.homework.data.model.NaverResponse
+import com.cnm.homework.data.source.remote.NaverQueryRemoteDataSource
+import io.reactivex.Single
+
+class NaverQueryRepositoryImpl(private val remoteDataSource: NaverQueryRemoteDataSource) :
+    NaverQueryRepository {
+    override fun getNaverMovie(query: String): Single<NaverResponse> {
+        return remoteDataSource.getNaverMovie(query)
+    }
+}
