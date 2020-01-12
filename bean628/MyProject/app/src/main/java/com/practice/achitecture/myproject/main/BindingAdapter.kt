827cdff2eb059806b6ee1840199ab36b115bb1b3
bean628 <dudwp628@naver.com>
2020-01-13 00:15:42 +0900

    HW-8 null 에러 수정

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/main/BindingAdapter.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/main/BindingAdapter.kt
@@ -11,16 +11,17 @@ object BindingAdapter {
 
     @JvmStatic
     @BindingAdapter("bind:setImageUrl")
-    fun showImage(view: ImageView, url: String) {
-        if (url.isNotEmpty()) {
+    fun showImage(view: ImageView, url: String?) {
+        if (!url.isNullOrEmpty()) {
             GlideWrapper.showImage(view, url)
         }
     }
 
     @JvmStatic
     @BindingAdapter("bind:setItemList", "bind:setSearchType")
-    fun setItemList(view: RecyclerView, itemList: List<SearchedItem>, searchType: SearchType) {
-        (view.adapter as SearchNaverAdapter).searchType = searchType
-        (view.adapter as SearchNaverAdapter).notifyDataSetChanged(itemList)
+    fun setItemList(view: RecyclerView, itemList: List<SearchedItem>?, searchType: SearchType?) {
+        if (!itemList.isNullOrEmpty() && !searchType?.value.isNullOrEmpty())
+            (view.adapter as SearchNaverAdapter?)?.searchType = searchType!!
+        (view.adapter as SearchNaverAdapter?)?.notifyDataSetChanged(itemList!!)
     }
 }
