885371bec1c54b0051d30b412d12cb8a09eef7d3
bean628 <dudwp628@naver.com>
2020-01-14 07:13:33 +0900

    HW-8 ViewModelModule 하나로 합침

--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/MyApp.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/MyApp.kt
@@ -1,9 +1,8 @@
 package com.practice.achitecture.myproject
 
 import android.app.Application
-import com.practice.achitecture.myproject.di.historyViewModelModule
-import com.practice.achitecture.myproject.di.mainViewModelModule
 import com.practice.achitecture.myproject.di.naverRepositoryModule
+import com.practice.achitecture.myproject.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.logger.AndroidLogger
 import org.koin.core.context.startKoin
@@ -17,7 +16,7 @@ class MyApp : Application() {
         startKoin {
             logger(if (BuildConfig.DEBUG) AndroidLogger() else EmptyLogger())
             androidContext(this@MyApp)
-            modules(listOf(naverRepositoryModule, mainViewModelModule, historyViewModelModule))
+            modules(listOf(naverRepositoryModule, viewModelModule))
         }
     }
 }
--- bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/di/ViewModelModule.kt
+++ bean628/MyProject/app/src/main/java/com/practice/achitecture/myproject/di/ViewModelModule.kt
@@ -5,10 +5,7 @@ import com.practice.achitecture.myproject.main.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val mainViewModelModule = module {
+val viewModelModule = module {
     viewModel { MainViewModel(get()) }
-}
-
-val historyViewModelModule = module {
     viewModel { HistoryViewModel(get()) }
 }
