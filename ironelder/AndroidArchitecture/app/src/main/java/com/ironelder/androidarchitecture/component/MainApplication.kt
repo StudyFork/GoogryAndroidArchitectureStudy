feb9e12961d3e3edcc3c25a6775c0ad6ed09a234
Jason <ironelder@gmail.com>
2020-01-14 18:30:08 +0900

    HW-8-Koin 초기화를 위한 Application에 koin초기화 추가
    HW-8-Koin 초기화를 위한 Application에 koin초기화 추가

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/component/MainApplication.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/component/MainApplication.kt
@@ -2,14 +2,43 @@ package com.ironelder.androidarchitecture.component
 
 import android.app.Application
 import com.ironelder.androidarchitecture.data.database.SearchResultDatabase
+import com.ironelder.androidarchitecture.data.repository.SearchDataRepositoryImpl
+import com.ironelder.androidarchitecture.data.source.local.LocalSearchDataSourceImpl
+import com.ironelder.androidarchitecture.data.source.remote.RemoteSearchDataSourceImpl
+import com.ironelder.androidarchitecture.view.mainview.MainViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class MainApplication : Application() {
+
+    val veiwModelModule: Module = module {
+        single { MainViewModel(get()) }
+    }
+
+    val repositoryModule: Module = module {
+        single { SearchDataRepositoryImpl(get()) }
+    }
+
+    val dataSourceModule: Module = module {
+        single { SearchResultDatabase.getInstance(androidApplication()) }
+        single { LocalSearchDataSourceImpl(get()) }
+        single { RemoteSearchDataSourceImpl }
+    }
+
+
     override fun onCreate() {
         super.onCreate()
         initialized()
     }
 
     private fun initialized() {
-        SearchResultDatabase.getInstance(applicationContext)
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(listOf(dataSourceModule, repositoryModule, veiwModelModule))
+        }
     }
+
 }
