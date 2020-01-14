af69cd023e0ab66cb7e5689fd776826755de81c5
Jason <ironelder@gmail.com>
2020-01-14 18:27:40 +0900

    HW-8-interface의 함수에서 객체를 넘겨받지 않도록 수정
    HW-8-interface의 함수에서 객체를 넘겨받지 않도록 수정

--- ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/data/source/local/LocalSearchDataSource.kt
+++ ironelder/AndroidArchitecture/app/src/main/java/com/ironelder/androidarchitecture/data/source/local/LocalSearchDataSource.kt
@@ -1,15 +1,14 @@
 package com.ironelder.androidarchitecture.data.source.local
 
 import com.ironelder.androidarchitecture.data.SearchResult
-import com.ironelder.androidarchitecture.data.dao.SearchResultDao
 import io.reactivex.Single
 
 interface LocalSearchDataSource {
 
-    fun getLocalSearchData(searchResultDao: SearchResultDao, type: String): Single<SearchResult>?
+    fun getLocalSearchData(type: String): Single<SearchResult>?
 
     fun setLocalSearchData(
-        searchResultDao: SearchResultDao, searchResult: SearchResult
+        searchResult: SearchResult
     )
 
 }
