267c897058264f04002c61c21eb208b3c0f536c0
ahndding <ahndding@gmail.com>
2020-01-14 23:06:18 +0900

    HW-4 base adapter 추가에 의한 코드 변경

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/BaseViewHolder.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/BaseViewHolder.kt
@@ -1,13 +1,8 @@
 package com.hansung.firstproject.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseViewHolder<T>(@LayoutRes layoutId: Int, parents: ViewGroup) :
-    RecyclerView.ViewHolder(
-        LayoutInflater.from(parents.context).inflate(layoutId, parents, false)
-    ) {
+abstract class BaseViewHolder<T>(view: View) : RecyclerView.ViewHolder(view) {
     abstract fun bindItems(item: T)
 }
--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/MovieHolder.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/adapter/MovieHolder.kt
@@ -2,18 +2,14 @@ package com.hansung.firstproject.adapter
 
 import android.net.Uri
 import android.os.Build
-import android.text.Html
-import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.hansung.firstproject.ui.movieinformation.MovieInformationActivity
 import com.hansung.firstproject.data.MovieModel
-import kotlinx.android.synthetic.main.movie_item.view.*
+import com.hansung.firstproject.databinding.MovieItemBinding
+import com.hansung.firstproject.ui.movieinformation.MovieInformationActivity
 
 @Suppress("DEPRECATION")
-open class MovieHolder(layoutId: Int, parents: ViewGroup) :
-    BaseViewHolder<MovieModel>(layoutId, parents) {
+open class MovieHolder(private val binding: MovieItemBinding) :
+    BaseViewHolder<MovieModel>(binding.root) {
 
     private lateinit var webPage: Uri
 
@@ -27,25 +23,15 @@ open class MovieHolder(layoutId: Int, parents: ViewGroup) :
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun bindItems(item: MovieModel) {
-        //image 바인딩
-        with(itemView) {
-            // userRating(별점)의 경우 단순 문자열로 전달됨. 이를 실수형으로 변환한 뒤 10점만점을 별 5개 기준으로 변경하기 위해 2로 나누고 이를 기반으로 ratingBar(별점)의 값을 변경한다.
-            grade_movieitem.rating = item.userRating.toFloat() / 2
-            releasedate_movieitem.text = item.pubDate
-            // 검색어와 일치하는 부분은 태그로 감싸져 오는 제목 String에서 HTML Tag를 제거하는 method
-            title_movieitem.text = Html.fromHtml(item.title, Html.FROM_HTML_MODE_LEGACY)
-            director_movieitem.text = item.director
-            actor_movieitem.text = item.actor
 
-            Glide.with(context).load(item.image).apply(RequestOptions().override(300, 450))
-                .apply(RequestOptions.centerCropTransform()).into(posterimage_movieitem)
+        //클릭시 웹사이트 연결을 위한 uri 바인딩
+        webPage = Uri.parse(item.link)
 
-            // binding.item = item // 이 형식으로 진행 되게 끔...
-
-
-            //클릭시 웹사이트 연결을 위한 uri 바인딩
-            webPage = Uri.parse(item.link)
+        /*이런식으로 넣어주는게 맞는지...?*/
+        binding.apply {
+            this.item = item
+        }.run {
+            executePendingBindings()
         }
-
     }
 }
