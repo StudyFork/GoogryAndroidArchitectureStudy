bbf22efd10a6a7762da571fcc4fc7f7d741cfe0e
2giwon <2giwon@gmail.com>
2020-01-15 12:56:31 +0900

    HW-8 database koin 적용

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/di/DataSourceModule.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/di/DataSourceModule.kt
@@ -1,18 +1,29 @@
 package com.egiwon.architecturestudy.di
 
+import android.content.Context
+import androidx.room.Room
 import com.egiwon.architecturestudy.data.NaverDataRepository
 import com.egiwon.architecturestudy.data.NaverDataRepositoryImpl
 import com.egiwon.architecturestudy.data.source.NaverDataSource
+import com.egiwon.architecturestudy.data.source.local.ContentDao
 import com.egiwon.architecturestudy.data.source.local.ContentDataBase
 import com.egiwon.architecturestudy.data.source.local.NaverLocalDataSource
 import com.egiwon.architecturestudy.data.source.remote.NaverRemoteDataSource
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataSourceModule = module {
-    single { ContentDataBase.getInstance(context = androidContext()).contentDao() }
+    single { createDatabase(get()) }
+    single { createContentDao(get()) }
     single<NaverDataSource.Local> { NaverLocalDataSource(get()) }
     single<NaverDataSource.Remote> { NaverRemoteDataSource(get(named("remote"))) }
     single<NaverDataRepository>(named("repo")) { NaverDataRepositoryImpl(get(), get()) }
 }
+
+private fun createDatabase(context: Context): ContentDataBase = Room.databaseBuilder(
+    context.applicationContext,
+    ContentDataBase::class.java, ContentDataBase.DB_NAME
+).build()
+
+private fun createContentDao(contentDataBase: ContentDataBase): ContentDao =
+    contentDataBase.contentDao()
