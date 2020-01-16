8b0a821d729d958ddd5725ac46dc8bd7850c7f3d
handnew <handnew04@gmail.com>
2020-01-17 00:17:43 +0900

    HW-4 MovieDetailActivity 데이터 바인딩으로 변경

--- handnew04/app/src/main/java/com/example/handnew04/ui/movie/MovieDetailActivity.kt
+++ handnew04/app/src/main/java/com/example/handnew04/ui/movie/MovieDetailActivity.kt
@@ -3,17 +3,19 @@ package com.example.handnew04.ui.movie
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.handnew04.R
+import com.example.handnew04.databinding.ActivityMovieDetailBinding
 
 class MovieDetailActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityMovieDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_detail)
+        binding =
+            DataBindingUtil.setContentView(this@MovieDetailActivity, R.layout.activity_movie_detail)
         supportActionBar?.hide()
         initWebView()
     }
@@ -22,22 +24,20 @@ class MovieDetailActivity : AppCompatActivity() {
 
     @SuppressLint("ObsoleteSdkInt", "SetJavaScriptEnabled")
     private fun initWebView() {
-        val movieWebView = findViewById<View>(R.id.wb_movieDetail) as WebView
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WebView.setWebContentsDebuggingEnabled(true)
         }
 
-        movieWebView.webViewClient = object : WebViewClient() {
+        binding.wbMovieDetail.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
                 return true
             }
         }
 
-        val settings = movieWebView.settings
+        val settings = binding.wbMovieDetail.settings
         settings.javaScriptEnabled = true
 
-        movieWebView.loadUrl(getMovieLink())
+        binding.wbMovieDetail.loadUrl(getMovieLink())
     }
 }
