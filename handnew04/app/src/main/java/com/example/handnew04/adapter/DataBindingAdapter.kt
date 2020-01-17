5157c584e8cb65df15910941318fdc6c0344c863
handnew <handnew04@gmail.com>
2020-01-18 03:14:21 +0900

    HW-4 BindingAdapter setText 내에 기존 setText 내용 추가

--- handnew04/app/src/main/java/com/example/handnew04/adapter/DataBindingAdapter.kt
+++ handnew04/app/src/main/java/com/example/handnew04/adapter/DataBindingAdapter.kt
@@ -2,6 +2,7 @@ package com.example.handnew04.adapter
 
 import android.os.Build
 import android.text.Html
+import android.text.Spanned
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
@@ -19,13 +20,45 @@ fun bindRatingString(ratingBar: RatingBar, rating: String) {
 }
 
 @BindingAdapter("android:text")
-fun setText(textView: TextView, text: String) {
-    val htmlText: String
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        htmlText = Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY).toString()
+fun setText(textView: TextView, text: CharSequence?) {
+    val oldText: CharSequence = textView.text
+    if (text === oldText || text == null && oldText.isEmpty()) {
+        return
+    }
+    if (text is Spanned) {
+        if (text == oldText) {
+            return
+        }
+    } else if (!haveContentsChanged(text, oldText)) {
+        return
+    }
+
+    val htmlText: String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(text as String, Html.FROM_HTML_MODE_LEGACY).toString()
     } else {
-        htmlText = Html.fromHtml(text).toString()
+        Html.fromHtml(text as String).toString()
     }
 
     textView.setText(htmlText)
+}
+
+fun haveContentsChanged(
+    str1: CharSequence?,
+    str2: CharSequence?
+): Boolean {
+    if (str1 == null != (str2 == null)) {
+        return true
+    } else if (str1 == null) {
+        return false
+    }
+    val length = str1.length
+    if (length != str2!!.length) {
+        return true
+    }
+    for (i in 0 until length) {
+        if (str1[i] != str2[i]) {
+            return true
+        }
+    }
+    return false
 }
