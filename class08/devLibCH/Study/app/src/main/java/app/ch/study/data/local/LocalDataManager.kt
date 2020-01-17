5048d2e9195d61335d44b674cd72ea8ad0849edb
CH-Ahn <changhwan@midashnt.com>
2020-01-17 10:01:29 +0900

    companion object 위치변경

--- class08/devLibCH/Study/app/src/main/java/app/ch/study/data/local/LocalDataManager.kt
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/data/local/LocalDataManager.kt
@@ -8,18 +8,6 @@ open class LocalDataManager(context: Context) {
     private val prefs: SharedPreferences =
         context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
 
-    companion object {
-        private var staticSharedPrefer: LocalDataManager? = null
-
-        fun getInstance(context: Context): LocalDataManager? {
-            if (staticSharedPrefer == null)
-                staticSharedPrefer =
-                    LocalDataManager(context)
-
-            return staticSharedPrefer
-        }
-    }
-
     fun saveMovieList(list: String) {
         val editor = prefs.edit()
         editor.putString("movieList", list)
@@ -35,5 +23,17 @@ open class LocalDataManager(context: Context) {
     }
 
     fun getQuery(): String = prefs.getString("query", "") ?: ""
+
+    companion object {
+        private var staticSharedPrefer: LocalDataManager? = null
+
+        fun getInstance(context: Context): LocalDataManager? {
+            if (staticSharedPrefer == null)
+                staticSharedPrefer =
+                    LocalDataManager(context)
+
+            return staticSharedPrefer
+        }
+    }
 
 }
