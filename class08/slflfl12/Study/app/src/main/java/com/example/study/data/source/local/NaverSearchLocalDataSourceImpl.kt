cae53a6c5d2a1c3e4c64a597692636ad3936d464
SSH <slflfl12@naver.com>
2020-01-23 11:03:26 +0900

    HW-3 LocalDataSource 추가

---
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/NaverSearchLocalDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.example.study.data.source.local
+
+import com.example.study.data.source.local.model.SearchResult
+
+class NaverSearchLocalDataSourceImpl : NaverSearchLocalDataSource {
+
+    override fun addSearchResult(searchResult: SearchResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getRecentSearchResult(): SearchResult {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/model/SearchResult.kt
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/model/SearchResult.kt
@@ -1,2 +1,6 @@
 package com.example.study.data.source.local.model
 
+data class SearchResult(
+
+    val id: Long
+)
