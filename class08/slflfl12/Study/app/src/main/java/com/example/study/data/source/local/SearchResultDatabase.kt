7aca4b3a04fe31835c7a2b4cb4cd483ad5d57f0d
SSH <slflfl12@naver.com>
2020-01-23 17:27:25 +0900

    HW-3 SearchResultDatabase 작성

--- class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/SearchResultDatabase.kt
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/SearchResultDatabase.kt
@@ -1,9 +1,28 @@
 package com.example.study.data.source.local
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.study.data.source.local.model.SearchResult
 
+@Database(version = 1, entities = [SearchResult::class])
 abstract class SearchResultDatabase : RoomDatabase() {
 
+    abstract fun searchResultDao(): SearchResultDao
 
 
+    companion object {
+
+        private var instance: SearchResultDatabase? = null
+
+        fun getInstance(context: Context): SearchResultDatabase {
+
+            instance ?: synchronized(SearchResultDatabase::class.java) {
+                instance ?: Room.databaseBuilder(context, SearchResultDatabase::class.java, "SearchResult.db").build()
+            }
+
+            return instance!!
+        }
+    }
 }
--- class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/model/SearchResult.kt
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/data/source/local/model/SearchResult.kt
@@ -1,5 +1,8 @@
 package com.example.study.data.source.local.model
 
+import androidx.room.Entity
+
+@Entity
 data class SearchResult(
 
     val id: Long
