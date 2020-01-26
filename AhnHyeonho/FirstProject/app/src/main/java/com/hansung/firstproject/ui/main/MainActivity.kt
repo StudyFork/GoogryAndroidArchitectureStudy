1175e97069aa9b8a0cde2d9f605f708b0046edbc
ahndding <ahndding@gmail.com>
2020-01-26 22:31:23 +0900

    HW-5 main activity 수정1

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainActivity.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainActivity.kt
@@ -9,20 +9,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hansung.firstproject.R
 import com.hansung.firstproject.adapter.MovieItemAdapter
 import com.hansung.firstproject.data.ErrorStringResource
-import com.hansung.firstproject.data.MovieResponseModel
 import com.hansung.firstproject.data.repository.NaverRepository
 import com.hansung.firstproject.data.source.remote.NaverRemoteDataSourceImpl
 import com.hansung.firstproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+//class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity() {
 
-    private val presenter by lazy {
+    /*private val presenter by lazy {
         MainPresenter(
             this,
             NaverRepository.getInstance(
@@ -34,10 +35,23 @@ class MainActivity : AppCompatActivity(), MainContract.View {
                 )
             )
         )
-    }
+    }*/
 
     private val movieItemAdapter: MovieItemAdapter = MovieItemAdapter()
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by lazy {
+        MainViewModel(
+            NaverRepository.getInstance(
+                NaverRemoteDataSourceImpl.getInstance(
+                    Pair<String, String>(
+                        getString(R.string.client_id),
+                        getString(R.string.client_secret)
+                    )
+                )
+            )
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +62,15 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         // keyboard function
         setKeyboardSearchFunc()
 
-        with(binding) {
+        initObserveCallback()
+
+
+        /*with(binding) {
             btnSearch.setOnClickListener {
-                presenter.doSearch(etSearch.text.toString())
+                //presenter.doSearch(etSearch.text.toString())
+                viewModel!!.doSearch(etSearch.text.toString())
             }
-        }
+        }*/
     }
 
     // recyclerView 초기화 메소드
@@ -70,39 +88,45 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         }
     }
 
+    fun btnSearchClick(){
+        viewModel!!.doSearch(binding.etSearch.text.toString())
+    }
+
+
     //키보드 제거 메소드
-    override fun removeKeyboard() =
+    fun removeKeyboard() =
         (this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
             binding.etSearch.windowToken,
             0
         )
 
-    override fun showErrorKeywordEmpty() {
+    fun showErrorKeywordEmpty() {
         Toast.makeText(this, getString(R.string.empty_keyword_message), Toast.LENGTH_SHORT).show()
     }
 
-    override fun showErrorByErrorMessage(errorMessage: String) {
+    fun showErrorByErrorMessage(errorMessage: String) {
         Toast.makeText(this, ErrorStringResource.valueOf(errorMessage).resId, Toast.LENGTH_SHORT)
             .show()
     }
 
-    override fun showErrorEmptyList() {
+    fun showErrorEmptyList() {
         Toast.makeText(this, getString(R.string.empty_list_message), Toast.LENGTH_SHORT).show()
     }
 
-    override fun addItemToAdapter(response: MovieResponseModel) {
+    /*override fun addItemToAdapter(response: MovieResponseModel) {
         movieItemAdapter.run {
             addItems(response.items)
             notifyDataSetChanged()
         }
-    }
+    }*/
 
     private fun setKeyboardSearchFunc() {
         binding.etSearch.setOnEditorActionListener(object : TextView.OnEditorActionListener {
             override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
                 when (actionId) {
                     EditorInfo.IME_ACTION_SEARCH -> {
-                        presenter.doSearch(binding.etSearch.text.toString())
+                        //presenter.doSearch(binding.etSearch.text.toString())
+                        viewModel.doSearch(binding.etSearch.text.toString())
                     }
                     else ->
                         return false
@@ -110,5 +134,40 @@ class MainActivity : AppCompatActivity(), MainContract.View {
                 return true
             }
         })
+    }
+
+
+    private fun initObserveCallback() {
+        with(viewModel) {
+            showError.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    showErrorByErrorMessage(showError.toString())
+                }
+            })
+
+            isEmptyResult.addOnPropertyChangedCallback(object :
+                Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    showErrorEmptyList()
+                    isEmptyResult.set(false)
+                }
+            })
+
+            showKeywordEmptyError.addOnPropertyChangedCallback(object :
+                Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    showErrorKeywordEmpty()
+                    showKeywordEmptyError.set(false)
+                }
+            })
+
+            hideKeyboard.addOnPropertyChangedCallback(object :
+                Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    removeKeyboard()
+                    hideKeyboard.set(false)
+                }
+            })
+        }
     }
 }
