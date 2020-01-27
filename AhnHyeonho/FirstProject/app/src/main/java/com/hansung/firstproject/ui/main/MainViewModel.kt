9f1c16d9b3585543d0e1edf671c1336f4ae6d3c5
ahndding <ahndding@gmail.com>
2020-01-27 12:52:20 +0900

    HW-5 main view model 수정

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModel.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModel.kt
@@ -1,10 +1,51 @@
 package com.hansung.firstproject.ui.main
 
-class MainViewModel() {
+import android.util.Log
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableField
+import androidx.databinding.ObservableList
+import com.hansung.firstproject.data.MovieModel
+import com.hansung.firstproject.data.repository.NaverRepository
+
+class MainViewModel(private val repository: NaverRepository) {
+
+    //var movieData = ObservableField<ArrayList<MovieModel>>()
+    val movieData: ObservableArrayList<MovieModel> = ObservableArrayList()
+    var showError = ObservableField<String>()
+    var isEmptyResult = ObservableField<Boolean>()
+    var showKeywordEmptyError = ObservableField<Boolean>()
+    var hideKeyboard = ObservableField<Boolean>()
+
+    fun doSearch(keyword: String) {
+        if (keyword.isEmpty()) {
+            showKeywordEmptyError.set(true)
+        } else {
+            // 검색 메소드
+            repository.getMoviesData(
+                keyword,
+                success = {
+                    //view.addItemToAdapter(it)
+                    movieData.clear()
+                    movieData.addAll(it.items)
+                    movieData.forEach{ a ->
+                        Log.d("ahn", a.title)
+                    }
 
 
-
-    fun doSearch(){
-
+                    //movieData.set(it.items)
+                    //Log.d("ahn", movieData.get()?.size.toString())
+                },
+                fail = {
+                    //view.showErrorByErrorMessage(it.message!!)
+                    showError.set(it.message!!)
+                },
+                isEmptyList = {
+                    //view.showErrorEmptyList()
+                    isEmptyResult.set(true)
+                }
+            )
+            //view.removeKeyboard()
+            hideKeyboard.set(true)
+        }
     }
 }
