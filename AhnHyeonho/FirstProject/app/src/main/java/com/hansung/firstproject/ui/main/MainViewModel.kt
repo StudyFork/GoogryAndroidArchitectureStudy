599c44d5c389f3d0ac61143b7ec0ee47d792b92a
ahndding <ahndding@gmail.com>
2020-01-27 13:12:31 +0900

    HW-5 main view model 주석 정리

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModel.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModel.kt
@@ -1,15 +1,12 @@
 package com.hansung.firstproject.ui.main
 
-import android.util.Log
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableField
-import androidx.databinding.ObservableList
 import com.hansung.firstproject.data.MovieModel
 import com.hansung.firstproject.data.repository.NaverRepository
 
 class MainViewModel(private val repository: NaverRepository) {
 
-    //var movieData = ObservableField<ArrayList<MovieModel>>()
     val movieData: ObservableArrayList<MovieModel> = ObservableArrayList()
     var showError = ObservableField<String>()
     var isEmptyResult = ObservableField<Boolean>()
@@ -24,27 +21,16 @@ class MainViewModel(private val repository: NaverRepository) {
             repository.getMoviesData(
                 keyword,
                 success = {
-                    //view.addItemToAdapter(it)
                     movieData.clear()
                     movieData.addAll(it.items)
-                    movieData.forEach{ a ->
-                        Log.d("ahn", a.title)
-                    }
-
-
-                    //movieData.set(it.items)
-                    //Log.d("ahn", movieData.get()?.size.toString())
                 },
                 fail = {
-                    //view.showErrorByErrorMessage(it.message!!)
                     showError.set(it.message!!)
                 },
                 isEmptyList = {
-                    //view.showErrorEmptyList()
                     isEmptyResult.set(true)
                 }
             )
-            //view.removeKeyboard()
             hideKeyboard.set(true)
         }
     }
