30921d6105e33bfe3c83df04bd9704dd53f4c58c
mino <k3000js@naver.com>
2020-02-02 19:02:30 +0900

    HW-4 BindingAdapter 추가

--- class08/Minoyam/app/src/main/java/com/cnm/homework/adapter/BindingAdapter.kt
+++ class08/Minoyam/app/src/main/java/com/cnm/homework/adapter/BindingAdapter.kt
@@ -1,2 +1,16 @@
 package com.cnm.homework.adapter
 
+import android.widget.ImageView
+import android.widget.RatingBar
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("bind:bindImage")
+fun bindImage(imageView: ImageView, imageUri: String) {
+    Glide.with(imageView.context).load(imageUri).into(imageView)
+}
+
+@BindingAdapter("bind:bindRating")
+fun bindRating(ratingBar: RatingBar, rating: Float) {
+    ratingBar.rating = rating / 2
+}
