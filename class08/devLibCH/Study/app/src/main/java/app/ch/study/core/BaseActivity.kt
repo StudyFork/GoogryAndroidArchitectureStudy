00112bc6a3de6f35f2b3d8f651a831e3f3cb8c00
ach <poohanh1@gmail.com>
2020-02-06 01:41:47 +0900

    baseActivity dataBinding을 위한 설정

--- class08/devLibCH/Study/app/src/main/java/app/ch/study/core/BaseActivity.kt
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/core/BaseActivity.kt
@@ -1,15 +1,30 @@
 package app.ch.study.core
 
+import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<P : BaseContract.Presenter>(@LayoutRes resId: Int) :
+abstract class BaseActivity<T : ViewDataBinding, P : BaseContract.Presenter>(@LayoutRes resId: Int) :
     AppCompatActivity(resId), BaseContract.View {
 
     abstract val presenter: P
     abstract val pbLoading: FrameLayout?
+
+    protected lateinit var binding: T
+        private set
+
+    @LayoutRes
+    abstract fun getLayoutResId(): Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, getLayoutResId())
+    }
 
     override fun showLoading() {
         pbLoading?.visibility = View.VISIBLE
