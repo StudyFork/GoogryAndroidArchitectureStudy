9c52f8ac806a680058e5d04f778fdef1cb3fb554
SSH <slflfl12@naver.com>
2020-02-06 14:33:30 +0900

    HW-4 add BindingAdapters

---
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/util/binding/BindingAdapters.kt
@@ -0,0 +1,30 @@
+package com.example.study.util.binding
+
+import android.os.Build
+import android.text.Html
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("loadUrl")
+fun ImageView.loadUrl(url:String) {
+    Glide.with(context)
+        .load(url)
+        .into(this)
+}
+
+@BindingAdapter("bindRating")
+fun bindRating(ratingBar: RatingBar, rating: String) {
+    ratingBar.rating = rating.toFloat() / 2
+}
+
+@BindingAdapter("setTitle")
+fun TextView.setTitle(movieTitle: String) {
+    if (Build.VERSION.SDK_INT >= 24) {
+        Html.fromHtml(movieTitle, Build.VERSION.SDK_INT).toString() // for 24 api and more
+    } else {
+        Html.fromHtml(movieTitle).toString() // or for older api
+    }
+}
