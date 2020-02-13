2176fe8ecd44099f503ca841a3e88e1a36c6b2f8
handnew <handnew04@gmail.com>
2020-02-14 00:38:51 +0900

    HW-7 networkManagerModule 의 Context androidApplication() 으로 변경

--- handnew04/app/src/main/java/com/example/handnew04/di/NetworkManagerModule.kt
+++ handnew04/app/src/main/java/com/example/handnew04/di/NetworkManagerModule.kt
@@ -1,9 +1,8 @@
 package com.example.handnew04.di
 
-import android.app.Application
 import com.example.handnew04.network.NetworkManager
-import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val networkManagerModule =
-    module { single<NetworkManager> { NetworkManager(androidContext() as Application) } }
+    module { single { NetworkManager(androidApplication()) } }
