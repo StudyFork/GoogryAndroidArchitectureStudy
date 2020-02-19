5b613401ab754ad0a4b3ce5bd2c99dfb93629099
ahndding <ahndding@gmail.com>
2020-02-19 15:52:42 +0900

    HW-8 module화로 인한 전체 코드 수정

--- AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModelFactory.kt
+++ AhnHyeonho/FirstProject/app/src/main/java/com/hansung/firstproject/ui/main/MainViewModelFactory.kt
@@ -1,11 +1,12 @@
+/*
 package com.hansung.firstproject.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.hansung.firstproject.data.repository.NaverRepository
+import com.hansung.repository.NaverRepository
 
 class MainViewModelFactory(private val repository: NaverRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return MainViewModel(repository) as T
     }
-}
+}*/
