bc6b07c3fe4e6c1ae767af046b354ec05aa568fa
tjdwlsl1021 <tjdwlsl1021@gmail.com>
2020-02-29 15:19:17 +0900

    Create androidx project including gitignore

---
+++ 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// 2020-02-29 hsj 프로젝트 생성
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
