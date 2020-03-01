c9df2624080799001a9f904e7ab1abe609c4d856
sooakim <alice2863@naver.com>
2020-03-01 20:44:39 +0900

    HW-1 initView in SAMainActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SAMainActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SAMainActivity.kt
@@ -2,12 +2,26 @@ package io.github.sooakim.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.recyclerview.widget.RecyclerView
 import io.github.sooakim.R
 
 class SAMainActivity : AppCompatActivity() {
+    private lateinit var mSearchEdit: AppCompatEditText
+    private lateinit var mSearchButton: AppCompatButton
+    private lateinit var mSearchResultRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        mSearchEdit = findViewById(R.id.et_search)
+        mSearchButton = findViewById(R.id.btn_search)
+        mSearchResultRecyclerView = findViewById(R.id.rv_search_result)
     }
 }
