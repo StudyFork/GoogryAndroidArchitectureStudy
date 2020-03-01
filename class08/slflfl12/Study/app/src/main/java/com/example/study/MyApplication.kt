a3e3019a09040c6c33f65a61ad0ea363582b8163
seunghwan <slflfl12@naver.com>
2020-03-01 23:23:10 +0900

    HW-7 Application 생성, 패키징

---
+++ class08/slflfl12/Study/app/src/main/java/com/example/study/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.study
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
