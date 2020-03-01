a06f2be401107ea47346acf40cf072777ad7a4e2
sooakim <alice2863@naver.com>
2020-03-01 20:46:51 +0900

    HW-1 initRecyclerView in SAMainActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SAMainActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SAMainActivity.kt
@@ -12,16 +12,23 @@ class SAMainActivity : AppCompatActivity() {
     private lateinit var mSearchButton: AppCompatButton
     private lateinit var mSearchResultRecyclerView: RecyclerView
 
+    private val mSearchResultAdapter: SAMainSearchResultAdapter = SAMainSearchResultAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initView()
+        initRecyclerView()
     }
 
     private fun initView() {
         mSearchEdit = findViewById(R.id.et_search)
         mSearchButton = findViewById(R.id.btn_search)
         mSearchResultRecyclerView = findViewById(R.id.rv_search_result)
+    }
+
+    private fun initRecyclerView() {
+        mSearchResultRecyclerView.adapter = mSearchResultAdapter
     }
 }
