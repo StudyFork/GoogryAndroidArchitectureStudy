c552021468bec395930d145b930e30f7bf72a01d
sooakim <alice2863@naver.com>
2020-03-01 18:28:05 +0900

    HW-1 rename MainActivity to SAMainActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/SAMainActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/SAMainActivity.kt
@@ -1,9 +1,9 @@
 package io.github.sooakim
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SAMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
