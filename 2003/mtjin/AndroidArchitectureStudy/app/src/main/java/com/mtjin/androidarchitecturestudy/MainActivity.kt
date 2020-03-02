1e6a9391c00af0c669ee85bbaf647dc7266ea309
seungeon <tmddjs210@naver.com>
2020-03-02 18:25:29 +0900

    HW-1 MainActivity 구현

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/MainActivity.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/MainActivity.kt
@@ -1,12 +1,77 @@
 package com.mtjin.androidarchitecturestudy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), MovieAdapter.ItemClickListener {
+
+    private lateinit var etInput: EditText
+    private lateinit var btnSearch: Button
+    private lateinit var rvMovies: RecyclerView
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initListener()
+    }
+
+    fun initView() {
+        etInput = findViewById(R.id.et_input)
+        btnSearch = findViewById(R.id.btn_search)
+        rvMovies = findViewById(R.id.rv_movies)
+        movieAdapter = MovieAdapter()
+        rvMovies.adapter = movieAdapter
+    }
+
+    private fun initListener() {
+        //어댑터 아이템 클릭리스너
+        movieAdapter.setItemClickListener(this)
+        //검색버튼
+        btnSearch.setOnClickListener {
+            var query = etInput.text.toString().trim()
+            if (query.isEmpty()) {
+                onToastMessage("검색어를 입력해주세요.")
+            } else {
+                onToastMessage("잠시만 기다려주세요.")
+                movieAdapter.clear()
+                val apiInterface = ApiClient.getApiClient().create(ApiInterface::class.java)
+                apiInterface.getSearchMovie(
+                    getString(R.string.client_id),
+                    getString(R.string.client_secret),
+                    query
+                ).subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        // handle success
+                        movieAdapter.setItems(it.movies)
+                        movieAdapter.notifyDataSetChanged()
+                    }, {
+                        // handle fail
+                        onToastMessage("불러오는데 실패 했습니다.")
+                    })
+
+            }
+        }
+    }
+
+    override fun onItemClick(movie: Movie) {
+        var intent = Intent(Intent.ACTION_VIEW , Uri.parse(movie.link))
+        startActivity(intent)
+    }
+
+    fun onToastMessage(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
