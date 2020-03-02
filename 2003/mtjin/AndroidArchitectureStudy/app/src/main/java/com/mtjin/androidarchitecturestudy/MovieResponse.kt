9f24caeb65c3a3b576585cc67b5ef3ce445738cd
seungeon <tmddjs210@naver.com>
2020-03-02 20:04:49 +0900

    HW-1 data class @Expose 어노테이션 추가

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/MovieResponse.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/MovieResponse.kt
@@ -1,16 +1,21 @@
 package com.mtjin.androidarchitecturestudy
 
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
     @SerializedName("display")
+    @Expose
     val display: Int,
     @SerializedName("items")
+    @Expose
     val movies: List<Movie>,
     @SerializedName("lastBuildDate")
+    @Expose
     val lastBuildDate: String,
     @SerializedName("start")
+    @Expose
     val start: Int,
     @SerializedName("total")
     val total: Int
