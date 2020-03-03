69dca92cf891d87606b9dc013b588abfee9686a2
kangraemin <ee4787@naver.com>
2020-03-03 23:50:47 +0900

    HW-1 Add search function in main

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/MainActivity.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/MainActivity.kt
@@ -1,12 +1,80 @@
 package com.example.kangraemin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kangraemin.Response.ResponseMovieSearch
+import com.example.kangraemin.adapter.SearchResultAdapter
+import com.example.kangraemin.model.KangBaseActivity
+import com.example.kangraemin.model.MovieSearchInterface
+import com.example.kangraemin.util.NetworkUtil
+import com.example.kangraemin.util.RetrofitClient
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : KangBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = ArrayList<ResponseMovieSearch>()
+        val adapter = SearchResultAdapter(this, data = data)
+
+        rv_search_result.layoutManager = LinearLayoutManager(this)
+        rv_search_result.adapter = adapter
+
+        val service = RetrofitClient()
+            .getClient("https://openapi.naver.com")
+            .create(MovieSearchInterface::class.java)
+
+        val whenSearchTextChanged = et_search.textChanges()
+            .map { enteredText ->
+                enteredText.isNotEmpty()
+            }
+            .subscribe { isText ->
+                if (isText) {
+                    btn_search.alpha = 1f
+                    btn_search.isEnabled = true
+                } else {
+                    btn_search.alpha = 0.3f
+                    btn_search.isEnabled = false
+                }
+            }
+        compositeDisposable.add(whenSearchTextChanged)
+
+        val whenSearchButtonClicked = btn_search.clicks()
+            .map {
+                when (NetworkUtil().getConnectivityStatus(context = this)) {
+                    NetworkUtil.NetworkStatus.NOT_CONNECTED -> false
+                    else -> true
+                }
+            }
+            .subscribe { connectedToInternet ->
+                if (connectedToInternet) {
+                    rv_search_result.visibility = View.VISIBLE
+                    tv_network_error.visibility = View.GONE
+                    service.getSearchItems(
+                        display = "10",
+                        start = "1",
+                        query = et_search.text.toString()
+                    )
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ responseMovieSearch ->
+                            adapter.data = responseMovieSearch.items
+                            adapter.notifyDataSetChanged()
+                        }, { it.printStackTrace() })
+                } else {
+                    rv_search_result.visibility = View.GONE
+                    tv_network_error.visibility = View.VISIBLE
+                }
+            }
+        compositeDisposable.add(whenSearchButtonClicked)
+
+
     }
 }
