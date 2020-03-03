cf35b195a1123c970b21453895f4eea1d2b38ce3
kangraemin <ee4787@naver.com>
2020-03-03 23:46:54 +0900

    HW-1 Add utils for retrofit client

---
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/util/RetrofitClient.kt
@@ -0,0 +1,27 @@
+package com.example.kangraemin.util
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitClient {
+    fun getClient(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        builder.addInterceptor(interceptor)
+        return builder.build()
+    }
+}
