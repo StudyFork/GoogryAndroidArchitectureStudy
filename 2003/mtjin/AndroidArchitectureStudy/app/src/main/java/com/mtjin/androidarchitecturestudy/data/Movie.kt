d78e16fdca133b502f6772ed85fec3895dc89ce7
seungeon <tmddjs210@naver.com>
2020-03-03 14:13:46 +0900

    HW-1 @EXPOSE 제거

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/Movie.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/Movie.kt
@@ -6,27 +6,19 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie(
     @SerializedName("actor")
-    @Expose
     val actor: String,
     @SerializedName("director")
-    @Expose
     val director: String,
     @SerializedName("image")
-    @Expose
     val image: String,
     @SerializedName("link")
-    @Expose
     val link: String,
     @SerializedName("pubDate")
-    @Expose
     val pubDate: String,
     @SerializedName("subtitle")
-    @Expose
     val subtitle: String,
     @SerializedName("title")
-    @Expose
     val title: String,
     @SerializedName("userRating")
-    @Expose
     val userRating: String
 )
--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/MovieResponse.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/MovieResponse.kt
@@ -7,16 +7,12 @@ import com.mtjin.androidarchitecturestudy.data.Movie
 
 data class MovieResponse(
     @SerializedName("display")
-    @Expose
     val display: Int,
     @SerializedName("items")
-    @Expose
     val movies: List<Movie>,
     @SerializedName("lastBuildDate")
-    @Expose
     val lastBuildDate: String,
     @SerializedName("start")
-    @Expose
     val start: Int,
     @SerializedName("total")
     val total: Int
