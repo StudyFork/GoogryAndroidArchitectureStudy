ed47ccf40b8cca9a9efcec3b05478ac7ada8a40b
ahndding <ahndding@gmail.com>
2020-03-04 14:51:38 +0900

    HW-8 NaverRepositoryImpl 분리로 인한 interface화 작업

--- AhnHyeonho/FirstProject/repository/src/main/java/com/hansung/repository/NaverRepository.kt
+++ AhnHyeonho/FirstProject/repository/src/main/java/com/hansung/repository/NaverRepository.kt
@@ -1,17 +1,12 @@
 package com.hansung.repository
 
 import com.hansung.firstproject.data.MovieResponseModel
-import com.hansung.remote.NaverRemoteDataSource
 
-class NaverRepository(
-    private val dataSource: NaverRemoteDataSource
-) {
+interface NaverRepository {
     fun getMoviesData(
         title: String,
         success: (MovieResponseModel) -> Unit,
         fail: (Throwable) -> Unit,
         isEmptyList: () -> Unit
-    ) {
-        dataSource.getMoviesData(title, success, fail, isEmptyList)
-    }
+    )
 }
