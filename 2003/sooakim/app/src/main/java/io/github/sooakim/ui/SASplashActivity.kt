bee1b0f9d697233eeb18edaabf13eac3d8599635
sooakim <alice2863@naver.com>
2020-03-05 01:30:28 +0900

    HW-1 route by login result in SASplashActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SASplashActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SASplashActivity.kt
@@ -3,15 +3,33 @@ package io.github.sooakim.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.sooakim.ui.application.SAApplication
 import io.github.sooakim.ui.login.SALoginActivity
+import io.github.sooakim.ui.movie.SAMovieSearchActivity
 
 class SASplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        routeMovieSearch()
+
+        route()
+    }
+
+    private fun route() {
+        val isAuthRequired = (applicationContext as? SAApplication)?.preferencesHelper
+            ?.isAuthRequired
+            ?: return
+        if (isAuthRequired) {
+            routeLogin()
+        } else {
+            routeMovieSearch()
+        }
+    }
+
+    private fun routeLogin() {
+        startActivity(Intent(applicationContext, SALoginActivity::class.java))
     }
 
     private fun routeMovieSearch() {
-        startActivity(Intent(applicationContext, SALoginActivity::class.java))
+        startActivity(Intent(applicationContext, SAMovieSearchActivity::class.java))
     }
 }
