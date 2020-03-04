83b7682ab337c5a4fcb96a8a1b149a9cc6dfd336
sooakim <alice2863@naver.com>
2020-03-05 01:41:52 +0900

    HW-1 add netWorkService instance in SAApplication

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/application/SAApplication.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/application/SAApplication.kt
@@ -1,14 +1,18 @@
 package io.github.sooakim.ui.application
 
 import android.app.Application
+import io.github.sooakim.network.SANetworkService
+import io.github.sooakim.network.SANetworkServiceImpl
 import io.github.sooakim.pref.SAPreferencesHelper
 import io.github.sooakim.pref.SAPreferencesHelperImpl
 
 class SAApplication : Application() {
     lateinit var preferencesHelper: SAPreferencesHelper
+    lateinit var networkService: SANetworkService
 
     override fun onCreate() {
         super.onCreate()
         preferencesHelper = SAPreferencesHelperImpl(applicationContext)
+        networkService = SANetworkServiceImpl()
     }
 }
