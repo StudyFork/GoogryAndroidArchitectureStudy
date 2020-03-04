62aee938daa909ab8209adfcbfe1024e3ff59da2
sooakim <alice2863@naver.com>
2020-03-04 23:26:37 +0900

    HW-1 create SALoginActivity

---
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/login/SALoginActivity.kt
@@ -0,0 +1,12 @@
+package io.github.sooakim.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.sooakim.R
+
+class SALoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
