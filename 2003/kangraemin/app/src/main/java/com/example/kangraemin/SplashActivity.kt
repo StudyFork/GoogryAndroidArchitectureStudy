5a330dd48019b255f3cf260b7a41ffab73f8d72a
kangraemin <ee4787@naver.com>
2020-03-04 23:36:40 +0900

    HW-1 Add splash function

---
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.kangraemin
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.kangraemin.model.KangBaseActivity
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : KangBaseActivity() {
+
+    var splashTime = 3
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splashTimer = Observable
+            .interval(1, TimeUnit.SECONDS)
+            .takeWhile { splashTime > 0 }
+            .doOnComplete {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .subscribe {
+                splashTime--
+            }
+        compositeDisposable.add(splashTimer)
+    }
+}
