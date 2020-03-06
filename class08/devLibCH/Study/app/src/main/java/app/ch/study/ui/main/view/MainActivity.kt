01c89bc3717db9f88d289ef60af077b6155213ff
ach <poohanh1@gmail.com>
2020-03-06 22:39:45 +0900

    activity koin 적용

--- class08/devLibCH/Study/app/src/main/java/app/ch/study/ui/main/view/MainActivity.kt
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/ui/main/view/MainActivity.kt
@@ -1,34 +1,21 @@
 package app.ch.study.ui.main.view
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.FrameLayout
-import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import app.ch.study.R
 import app.ch.study.core.BaseActivity
 import app.ch.study.data.common.EXTRA_URL
-import app.ch.study.data.common.PREF_NAME
-import app.ch.study.data.local.LocalDataManager
 import app.ch.study.databinding.ActivityMainBinding
 import app.ch.study.ui.detail.DetailActivity
 import app.ch.study.ui.main.viewmodel.MainViewModel
 import app.ch.study.util.handleError
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
 
-    override val vm: MainViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                val prefs = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-                val localDataManager = LocalDataManager.getInstance(prefs)
-                return MainViewModel(localDataManager) as T
-            }
-        }
-    }
+    override val vm: MainViewModel by viewModel()
 
     override val pbLoading: FrameLayout by lazy {
         binding.pbLoading
