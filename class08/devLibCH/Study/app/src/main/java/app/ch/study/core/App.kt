b9f8522b8cd0329a362526876e9af35a3bd64302
ach <poohanh1@gmail.com>
2020-03-06 22:45:59 +0900

    app에 koin 생성

--- class08/devLibCH/Study/app/src/main/java/app/ch/study/core/App.kt
+++ class08/devLibCH/Study/app/src/main/java/app/ch/study/core/App.kt
@@ -2,11 +2,26 @@ package app.ch.study.core
 
 import android.app.Application
 import android.content.Context
+import app.ch.study.di.apiModule
+import app.ch.study.di.networkModule
+import app.ch.study.di.repositoryModelModule
+import app.ch.study.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(
+                networkModule,
+                apiModule,
+                repositoryModelModule,
+                viewModelModule))
+        }
     }
 
     companion object {
