e07d0418b9a69892cf7ae0ba6d297a9db468f6dd
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-03-07 06:23:40 +0900

    BaseUrl은 domain 부분만 작성 나머지를 api쪽에 작성

--- 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/network/SearchMovieServerURL.kt
+++ 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/network/SearchMovieServerURL.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.network
 
+/**
+ * baseURL에는 domain 부분만 사용
+ * */
 object SearchMovieServerURL {
     const val URL_BASE = "https://openapi.naver.com/"
     const val URL_MOVIE = "v1/search/movie.json"
