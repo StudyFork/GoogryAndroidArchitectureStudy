041a0f7189113192a1fb3e2b696ca0033742ea4c
kangraemin <ee4787@naver.com>
2020-03-07 17:27:20 +0900

    Remov construct for context, data in adapter

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/adapter/SearchResultAdapter.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/adapter/SearchResultAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.kangraemin.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +10,10 @@ import com.example.kangraemin.Response.ResponseMovieSearch
 import com.example.kangraemin.util.Utils
 import kotlinx.android.synthetic.main.adapter_search_result.view.*
 
-class SearchResultAdapter(val context: Context, var data: List<ResponseMovieSearch>) :
+class SearchResultAdapter :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    val utils = Utils()
+    var data = ArrayList<ResponseMovieSearch>()
 
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
@@ -36,9 +35,10 @@ class SearchResultAdapter(val context: Context, var data: List<ResponseMovieSear
         val result = data[position]
         holder.itemView.tv_castings.text = result.actor
         holder.itemView.tv_director.text = result.director
-        holder.itemView.tv_title.text = utils.fromHtml(result.title)
+        holder.itemView.tv_title.text = Utils.fromHtml(result.title)
         holder.itemView.tv_date.text = result.pubDate
-        Glide.with(context).load(result.image).centerCrop().into(holder.itemView.img_thumbnail)
+        Glide.with(holder.itemView.img_thumbnail).load(result.image).centerCrop()
+            .into(holder.itemView.img_thumbnail)
         holder.itemView.rtb_rating.rating = result.userRating.toFloat() / 2
     }
 }
