ef53f883234a1bacb88713213b872778b3066234
kangraemin <ee4787@naver.com>
2020-03-07 16:53:49 +0900

    change utils class to utils object

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/util/Utils.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/util/Utils.kt
@@ -4,7 +4,7 @@ import android.os.Build
 import android.text.Html
 import android.text.Spanned
 
-class Utils {
+object Utils {
     fun fromHtml(input: String): Spanned {
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
             return Html.fromHtml(input)
