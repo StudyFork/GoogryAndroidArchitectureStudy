99c7109b159ad813003a4d9017de40b8826fbdd5
kangraemin <ee4787@naver.com>
2020-03-07 17:41:34 +0900

    Change observable interval to completable timer

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/SplashActivity.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/SplashActivity.kt
@@ -3,28 +3,23 @@ package com.example.kangraemin
 import android.content.Intent
 import android.os.Bundle
 import com.example.kangraemin.base.KangBaseActivity
-import io.reactivex.Observable
+import io.reactivex.Completable
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : KangBaseActivity() {
-
-    var splashTime = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val splashTimer = Observable
-            .interval(1, TimeUnit.SECONDS)
-            .takeWhile { splashTime > 0 }
+        val splashTimer = Completable
+            .timer(3, TimeUnit.SECONDS)
             .doOnComplete {
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
             }
-            .subscribe {
-                splashTime--
-            }
+            .subscribe()
         compositeDisposable.add(splashTimer)
     }
 }
