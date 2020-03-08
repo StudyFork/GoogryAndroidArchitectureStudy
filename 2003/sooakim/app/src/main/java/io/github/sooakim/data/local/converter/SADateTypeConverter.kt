548dc32aaafd5993f70476ecd439396396fb5aaf
sooakim <alice2863@naver.com>
2020-03-08 20:06:53 +0900

    HW-2 create new TypeConverter named SADateTypeConverter

---
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/data/local/converter/SADateTypeConverter.kt
@@ -0,0 +1,18 @@
+package io.github.sooakim.data.local.converter
+
+import androidx.room.TypeConverter
+import java.util.*
+
+object SADateTypeConverter {
+    @TypeConverter
+    @JvmStatic
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun toDate(time: Long): Date {
+        return Date(time)
+    }
+}
