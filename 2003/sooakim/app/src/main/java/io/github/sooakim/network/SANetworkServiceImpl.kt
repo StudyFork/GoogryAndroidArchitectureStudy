75c9457735891dadc926b195dc432d85d3f803a1
sooakim <alice2863@naver.com>
2020-03-08 23:00:47 +0900

    HW-2 add loggingInterceptor in SANetworkServiceImpl

--- 2003/sooakim/app/src/main/java/io/github/sooakim/network/SANetworkServiceImpl.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/network/SANetworkServiceImpl.kt
@@ -10,6 +10,7 @@ import io.github.sooakim.network.api.SANaverMovieApi
 import io.github.sooakim.network.interceptor.SANaverAuthInterceptor
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,9 +18,15 @@ class SANetworkServiceImpl : SANetworkService {
     private val naverAuthInterceptor: SANaverAuthInterceptor by lazy {
         SANaverAuthInterceptor()
     }
+    private val loggingInterceptor: HttpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
     private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient.Builder()
             .addInterceptor(naverAuthInterceptor)
+            .addInterceptor(loggingInterceptor)
             .build()
     }
     private val gson: Gson by lazy {
