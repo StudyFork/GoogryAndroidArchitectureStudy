09b63367c741ff34afd065aa68ec98fdf089a056
seungeon <tmddjs210@naver.com>
2020-03-09 22:36:48 +0900

    HW-2 Local - MovieDatabase 데이터베이스 생성

---
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/data/source/local/MovieDatabase.kt
@@ -0,0 +1,34 @@
+package com.mtjin.androidarchitecturestudy.data.source.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mtjin.androidarchitecturestudy.data.Movie
+
+@Database(entities = [Movie::class], version = 1, exportSchema = false)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getInstance(context: Context): MovieDatabase {
+            synchronized(this) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            MovieDatabase::class.java, "Movies"
+                        )
+                        .build()
+                }
+                return INSTANCE!!
+            }
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+}
