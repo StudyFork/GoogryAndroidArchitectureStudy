4ce061817d0754e3201bed15e6f587bbf79000c8
kangraemin <ee4787@naver.com>
2020-03-10 23:54:23 +0900

    HW-2 change search movie interface name

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/remote/datadao/MovieInterface.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/remote/datadao/MovieInterface.kt
@@ -1,13 +1,12 @@
-package com.example.kangraemin.model
+package com.example.kangraemin.model.remote.datadao
 
-import com.example.kangraemin.response.ItemsMovieSearch
+import com.example.kangraemin.model.remote.datamodel.Movies
 import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
-interface MovieSearchInterface {
+interface MovieInterface {
     @GET("/v1/search/movie.json")
     fun getSearchItems(
         @Header("X-Naver-Client-Id") clientId: String? = "5wa1hBD6a5XVIVLBnAyF",
@@ -15,5 +14,5 @@ interface MovieSearchInterface {
         @Query("display") display: String,
         @Query("start") start: String,
         @Query("query") query: String
-    ): Flowable<ItemsMovieSearch>
+    ): Flowable<Movies>
 }
