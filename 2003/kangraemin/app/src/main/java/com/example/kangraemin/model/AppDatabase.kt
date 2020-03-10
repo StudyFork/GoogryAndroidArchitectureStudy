80e19a8a44f170d5cb40915b94963597d6a6663e
kangraemin <ee4787@naver.com>
2020-03-10 23:51:36 +0900

    HW-2 Add app database

---
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.kangraemin.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kangraemin.model.local.datadao.MovieDao
+import com.example.kangraemin.model.local.datadao.UserDao
+import com.example.kangraemin.model.local.datamodel.Movie
+import com.example.kangraemin.model.local.datamodel.User
+
+@Database(entities = [User::class, Movie::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        private var db: AppDatabase? = null
+        private val lock = Any()
+
+        @JvmStatic
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(lock) {
+                if (db == null) {
+                    db = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "local"
+                    ).build()
+                }
+                return db!!
+            }
+        }
+    }
+}
