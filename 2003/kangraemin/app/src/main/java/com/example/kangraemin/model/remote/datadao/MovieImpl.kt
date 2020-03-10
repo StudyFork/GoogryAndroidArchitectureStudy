388d1d7957610a18df2a16d96271d52f8af1530c
kangraemin <ee4787@naver.com>
2020-03-10 23:53:46 +0900

    HW-2 Add impl class

---
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/remote/datadao/MovieImpl.kt
@@ -0,0 +1,24 @@
+package com.example.kangraemin.model.remote.datadao
+
+import com.example.kangraemin.model.remote.datamodel.Movies
+import com.example.kangraemin.util.RetrofitClient
+import io.reactivex.Flowable
+
+class MovieImpl : MovieInterface {
+    override fun getSearchItems(
+        clientId: String?,
+        clientSecret: String?,
+        display: String,
+        start: String,
+        query: String
+    ): Flowable<Movies> {
+        val service = RetrofitClient()
+            .getClient("https://openapi.naver.com")
+            .create(MovieInterface::class.java)
+        return service.getSearchItems(
+            display = display,
+            start = start,
+            query = query
+        )
+    }
+}
