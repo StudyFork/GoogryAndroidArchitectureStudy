0127e658b02116f9cc0c4315961aebbde5d1d33d
byiryu <byiryu@gmail.com>
2020-03-11 11:59:46 +0900

    HW-2 BaseActivity add hideLoading, showLoading

--- 2003/byiryu/app/src/main/java/com/byiryu/study/ui/BaseActivity.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/BaseActivity.kt
@@ -1,12 +1,13 @@
 package com.byiryu.study.ui
 
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
-
 abstract class BaseActivity : AppCompatActivity() {
 
+    protected abstract val progressBar: View
 
     fun showMsg(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -14,6 +15,14 @@ abstract class BaseActivity : AppCompatActivity() {
 
     fun showMsg(@StringRes res: Int) {
         showMsg(getString(res))
+    }
+
+    fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun hideLoading() {
+        progressBar.visibility = View.INVISIBLE
     }
 
 }
