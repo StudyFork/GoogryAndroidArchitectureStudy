499ec52b473d58a1d86673f40fc6ebb7feacfb2b
byiryu <byiryu@gmail.com>
2020-03-11 12:31:14 +0900

    HW-2 Model package name change

---
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/Apis.kt
@@ -0,0 +1,11 @@
+package com.byiryu.study.model
+
+import com.byiryu.study.model.entity.MovieResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Apis {
+    @GET("v1/search/movie.json")
+    fun getSearchMovie(@Query("query") query: String): Single<MovieResponse>
+}
---
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/entity/MovieResponse.kt
@@ -0,0 +1,28 @@
+package com.byiryu.study.model.entity
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+
+    @SerializedName("total")
+    @Expose
+    val total: Int,
+
+    @SerializedName("start")
+    @Expose
+    val start: Int,
+
+    @SerializedName("display")
+    @Expose
+    val display: Int,
+
+    @SerializedName("lastBuildDate")
+    @Expose
+    val lastBuildDate: String,
+
+    @SerializedName("items")
+    @Expose
+    val items: List<MovieItem>
+
+)
