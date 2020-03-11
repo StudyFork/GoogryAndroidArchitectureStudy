9c75b63cf2c87369ded0057b7e5be38fbf4e6691
kangraemin <ee4787@naver.com>
2020-03-12 00:15:16 +0900

    HW-2 Replace tag to String to set column name

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/UserModel.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/UserModel.kt
@@ -3,11 +3,10 @@ package com.example.kangraemin.model.local.datamodel
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.kangraemin.LoginActivity
 
 @Entity
 data class User(
-    @ColumnInfo(name = LoginActivity.TAG_AUTO_LOGIN) val autoLogin: Boolean,
+    @ColumnInfo(name = "auto_login") val autoLogin: Boolean,
     @ColumnInfo(name = "user_name") val userName: String
 ) {
     @PrimaryKey(autoGenerate = true)
