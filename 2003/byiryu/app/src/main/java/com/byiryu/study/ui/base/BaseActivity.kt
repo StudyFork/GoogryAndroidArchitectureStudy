25144b899e8d612d062e33ceb026ae1c2c6477f9
byiryu <byiryu@gmail.com>
2020-03-12 17:47:17 +0900

    HW-2 BaseActivity 로직 변경 및 function 추가

--- 2003/byiryu/app/src/main/java/com/byiryu/study/ui/base/BaseActivity.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/base/BaseActivity.kt
@@ -1,13 +1,15 @@
-package com.byiryu.study.ui
+package com.byiryu.study.ui.base
 
+import android.content.Intent
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
+
 abstract class BaseActivity : AppCompatActivity() {
 
-    protected abstract val progressBar: View
+    protected open val progressBar: View? = null
 
     fun showMsg(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -18,11 +20,15 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     fun showLoading() {
-        progressBar.visibility = View.VISIBLE
+        progressBar?.visibility = View.VISIBLE
     }
 
     fun hideLoading() {
-        progressBar.visibility = View.INVISIBLE
+        progressBar?.visibility = View.INVISIBLE
+    }
+
+    fun goActivity(clazz: Class<*>) {
+        startActivity(Intent(this, clazz))
     }
 
 }
