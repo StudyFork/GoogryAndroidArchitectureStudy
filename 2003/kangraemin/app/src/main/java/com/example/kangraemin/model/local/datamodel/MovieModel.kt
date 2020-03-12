c5f29ba15102b4daa48db13cf994dcd78312f45c
kangraemin <ee4787@naver.com>
2020-03-12 23:59:54 +0900

    HW-2 Relocate primary key into constructor

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/MovieModel.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/MovieModel.kt
@@ -6,13 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Movie(
+    @PrimaryKey(autoGenerate = true) val idx: Int = 0,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "image") val image: String,
     @ColumnInfo(name = "director") val director: String,
     @ColumnInfo(name = "actor") val actor: String,
     @ColumnInfo(name = "user_rating") val userRating: String,
     @ColumnInfo(name = "pub_data") val pubDate: String
-) {
-    @PrimaryKey(autoGenerate = true)
-    val idx: Int = 0
-}
+)
--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/UserModel.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/model/local/datamodel/UserModel.kt
@@ -6,9 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class User(
+    @PrimaryKey(autoGenerate = true) val idx: Int = 0,
     @ColumnInfo(name = "auto_login") val autoLogin: Boolean,
     @ColumnInfo(name = "user_name") val userName: String
-) {
-    @PrimaryKey(autoGenerate = true)
-    val idx: Int = 0
-}
+)
