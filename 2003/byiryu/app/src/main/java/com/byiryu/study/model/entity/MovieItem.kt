d517fc11124011b6c38802499aa82bbb5f616548
byiryu <byiryu@gmail.com>
2020-03-12 12:33:52 +0900

    HW-2 entity MovieItem -> @Primarykey 추가

--- 2003/byiryu/app/src/main/java/com/byiryu/study/model/entity/MovieItem.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/entity/MovieItem.kt
@@ -1,6 +1,7 @@
 package com.byiryu.study.model.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -9,25 +10,35 @@ data class MovieItem(
     @SerializedName("actor")
     @Expose
     val actor: String,
+
     @SerializedName("director")
     @Expose
     val director: String,
+
     @SerializedName("image")
     @Expose
     val image: String,
+
     @SerializedName("link")
     @Expose
     val link: String,
+
     @SerializedName("pubDate")
     @Expose
     val pubDate: String,
+
     @SerializedName("subtitle")
     @Expose
     val subtitle: String,
+
+    @PrimaryKey(autoGenerate = false)
     @SerializedName("title")
     @Expose
     val title: String,
+
     @SerializedName("userRating")
     @Expose
     val userRating: Float
+
+
 )
