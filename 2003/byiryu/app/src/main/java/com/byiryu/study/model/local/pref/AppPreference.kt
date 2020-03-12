f5ca2e759b78b8dbf3579312da3cdc97116ae514
byiryu <byiryu@gmail.com>
2020-03-12 17:26:17 +0900

    HW-2 AppPreference change function name

--- 2003/byiryu/app/src/main/java/com/byiryu/study/model/local/pref/AppPreference.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/local/pref/AppPreference.kt
@@ -2,22 +2,23 @@ package com.byiryu.study.model.local.pref
 
 import android.content.Context
 
-class AppPreference constructor(context: Context){
+class AppPreference constructor(context: Context) {
 
-    companion object{
+    companion object {
         const val APP_PREF_NAME = "1234-1234-1234-1234"
 
         const val PREF_QUERY = "aaaa-bbbb-cccc-dddd"
     }
 
-    private val pref = context.applicationContext.getSharedPreferences(APP_PREF_NAME, Context.MODE_PRIVATE)
+    private val pref =
+        context.applicationContext.getSharedPreferences(APP_PREF_NAME, Context.MODE_PRIVATE)
 
-    fun getQuery() : String{
+    fun getPrevQuery(): String {
         return pref.getString(PREF_QUERY, "")!!
     }
 
-    fun setQuery(query : String){
-        pref.edit().putString(PREF_QUERY,query ).apply()
+    fun setPrevQuery(query: String) {
+        pref.edit().putString(PREF_QUERY, query).apply()
     }
 
 
