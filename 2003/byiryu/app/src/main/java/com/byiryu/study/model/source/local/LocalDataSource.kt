226228c1e64d09cc3efda4aabe56a244c53f0e0f
byiryu <byiryu@gmail.com>
2020-03-12 12:36:09 +0900

    HW-2 create LocalDataSource

---
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/source/local/LocalDataSource.kt
@@ -0,0 +1,21 @@
+package com.byiryu.study.model.source.local
+
+import com.byiryu.study.model.entity.MovieItem
+import io.reactivex.Completable
+import io.reactivex.Single
+
+class LocalDataSource constructor(private val movieDao: MovieDao) {
+
+    fun getAll(): Single<List<MovieItem>> {
+        return movieDao.getAll()
+    }
+
+    fun saveMovies(movies: List<MovieItem>): Completable {
+        return movieDao.deleteAll()
+            .doOnComplete {
+                movieDao.insertAll(movies)
+            }
+
+
+    }
+}
