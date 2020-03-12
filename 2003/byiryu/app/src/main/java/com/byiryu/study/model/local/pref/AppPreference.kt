77b2d660550e3ae94e79c8f1b3fe0595f4b40090
byiryu <byiryu@gmail.com>
2020-03-12 17:47:55 +0900

    HW-2 AppPreference add function -> AutoLogin

--- 2003/byiryu/app/src/main/java/com/byiryu/study/model/local/pref/AppPreference.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/model/local/pref/AppPreference.kt
@@ -8,6 +8,8 @@ class AppPreference constructor(context: Context) {
         const val APP_PREF_NAME = "1234-1234-1234-1234"
 
         const val PREF_QUERY = "aaaa-bbbb-cccc-dddd"
+
+        const val PREF_AUTO_LOGIN = "A1B2-C3D4-E5F6-G7H8"
     }
 
     private val pref =
@@ -19,6 +21,15 @@ class AppPreference constructor(context: Context) {
 
     fun setPrevQuery(query: String) {
         pref.edit().putString(PREF_QUERY, query).apply()
+    }
+
+    fun setAutoLogin() {
+        pref.edit().putBoolean(PREF_AUTO_LOGIN, true).apply()
+    }
+
+    fun getAutoLogin(): Boolean {
+        return pref.getBoolean(PREF_AUTO_LOGIN, false)
+
     }
 
 
