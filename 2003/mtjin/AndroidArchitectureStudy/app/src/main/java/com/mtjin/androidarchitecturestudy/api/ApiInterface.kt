891120da9084d34af7d377567da4f10534600d55
seungeon <tmddjs210@naver.com>
2020-03-13 16:31:35 +0900

    HW-2 무한스크롤구현 - ApiInterface start 파라미터 추가

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/api/ApiInterface.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/api/ApiInterface.kt
@@ -6,8 +6,13 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
+    /*
+    *  검색하고, 스크롤 더불러오기 함수 나눌려다가 합쳤습니다.
+    * */
     @GET("v1/search/movie.json")
     fun getSearchMovie(
-        @Query("query") query: String
+        @Query("query") query: String,
+        @Query("start") start: Int = 1,
+        @Query("display") display: Int = 15
     ): Call<MovieResponse>
 }
