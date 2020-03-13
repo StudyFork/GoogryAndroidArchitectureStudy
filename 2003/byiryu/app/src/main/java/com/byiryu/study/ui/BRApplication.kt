0cc08449e9c0519cf8f3ee15495bdafec72aab59
byiryu <byiryu@gmail.com>
2020-03-13 13:45:52 +0900

    HW-2 Application 추가

---
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/BRApplication.kt
@@ -0,0 +1,32 @@
+package com.byiryu.study.ui
+
+import android.app.Application
+import com.byiryu.study.model.Repository
+import com.byiryu.study.model.local.LocalDataBase
+import com.byiryu.study.model.local.LocalDataSource
+import com.byiryu.study.model.local.pref.AppPreference
+import com.byiryu.study.model.remote.RemoteDataSource
+
+class BRApplication : Application(){
+
+    lateinit var repository: Repository
+
+    override fun onCreate() {
+        super.onCreate()
+        init()
+    }
+
+    private fun init() {
+
+        val pref = AppPreference(this)
+        val db = LocalDataBase.getInstance(this)
+
+        val remoteDataSource = RemoteDataSource()
+        val localDataSource = LocalDataSource(db.movieDao())
+
+
+        repository = Repository(remoteDataSource,localDataSource, pref)
+
+    }
+
+}
