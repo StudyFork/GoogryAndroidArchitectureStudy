cbbc491734df857370d9311489a7f1ae4511eb3e
jackjackE <tmddjs210@naver.com>
2020-03-15 20:12:20 +0900

    HW-3 AppInterceptor 클래스 파일 삭제 ApiClient 에 합침

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/api/ApiClient.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/api/ApiClient.kt
@@ -1,8 +1,11 @@
 package com.mtjin.androidarchitecturestudy.api
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 object ApiClient {
     private const val BASE_URL = "https://openapi.naver.com/"
@@ -21,4 +24,17 @@ object ApiClient {
             addInterceptor(interceptor)
             build()
         }
+
+    class AppInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain)
+                : Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("X-Naver-Client-Id", "33chRuAiqlSn5hn8tIme")
+                .addHeader("X-Naver-Client-Secret", "fyfwt9PCUN")
+                .build()
+
+            proceed(newRequest)
+        }
+    }
 }
