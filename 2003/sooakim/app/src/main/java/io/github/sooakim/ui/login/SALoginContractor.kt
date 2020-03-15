5bb203087b86c6a0882f5e5a84f9ace43b8bdc8e
sooakim <alice2863@naver.com>
2020-03-15 22:59:44 +0900

    HW-3 create new Contractor named SALoginContractor

---
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/login/SALoginContractor.kt
@@ -0,0 +1,18 @@
+package io.github.sooakim.ui.login
+
+import io.github.sooakim.ui.base.SABasePresenter
+import io.github.sooakim.ui.base.SABaseView
+
+interface SALoginContractor {
+    interface View : SABaseView {
+        fun clearErrors()
+        fun showIdError()
+        fun showPasswordError()
+
+        fun showMovieSearch()
+    }
+
+    interface Presenter : SABasePresenter {
+        fun doLogin(id: String, password: String)
+    }
+}
