a11e53d69d3a1d373f4b10a1c6b2cbcab57b7092
jackjackE <tmddjs210@naver.com>
2020-03-15 22:00:47 +0900

    HW-3 MovieSearch - Activity 구현

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/search/MovieSearchActivity.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/search/MovieSearchActivity.kt
@@ -3,7 +3,6 @@ package com.mtjin.androidarchitecturestudy.ui.search
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -14,11 +13,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mtjin.androidarchitecturestudy.R
 import com.mtjin.androidarchitecturestudy.utils.MyApplication
-import retrofit2.HttpException
 
 
-class MovieSearchActivity : AppCompatActivity() {
+class MovieSearchActivity : AppCompatActivity(), MovieSearchContract.View {
 
+    private lateinit var presenter: MovieSearchContract.Presenter
     private lateinit var etInput: EditText
     private lateinit var btnSearch: Button
     private lateinit var rvMovies: RecyclerView
@@ -43,6 +42,7 @@ class MovieSearchActivity : AppCompatActivity() {
         btnSearch = findViewById(R.id.btn_search)
         rvMovies = findViewById(R.id.rv_movies)
         pbLoading = findViewById(R.id.pb_loading)
+        presenter = MovieSearchPresenter(this, scrollListener, myApplication, movieAdapter)
     }
 
     private fun initAdapter() {
@@ -52,7 +52,7 @@ class MovieSearchActivity : AppCompatActivity() {
         rvMovies.layoutManager = linearLayoutManager
         scrollListener = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
             override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
-                requestPagingMovie(query, totalItemsCount + 1)
+                presenter.requestPagingMovie(query, totalItemsCount + 1)
             }
         }
         rvMovies.addOnScrollListener(scrollListener)
@@ -69,75 +69,20 @@ class MovieSearchActivity : AppCompatActivity() {
         //검색버튼
         btnSearch.setOnClickListener {
             query = etInput.text.toString().trim()
-            if (query.isEmpty()) {
-                onToastMessage("검색어를 입력해주세요.")
-            } else {
-                onToastMessage("잠시만 기다려주세요.")
-                requestMovie(query)
-            }
+            presenter.requestMovie(query)
         }
     }
 
-    private fun requestMovie(query: String) {
-        showLoading()
-        scrollListener.resetState()
-        myApplication.movieRepository.getSearchMovies(query,
-            success = {
-                if (it.isEmpty()) {
-                    onToastMessage("해당 영화는 존재하지 않습니다.")
-                } else {
-                    movieAdapter.clear()
-                    movieAdapter.setItems(it)
-                    onToastMessage("영화를 불러왔습니다.")
-                }
-                hideLoading()
-            },
-            fail = {
-                Log.d(TAG, it.toString())
-                when (it) {
-                    is HttpException -> onToastMessage("네트워크에 문제가 있습니다.")
-                    else -> onToastMessage(it.message.toString())
-                }
-                hideLoading()
-            })
+    override fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
-    fun requestPagingMovie(query: String, offset: Int) {
-        showLoading()
-        myApplication.movieRepository.getPagingMovies(query, offset,
-            success = {
-                if (it.isEmpty()) {
-                    onToastMessage("마지막 페이지")
-                } else {
-                    movieAdapter.setItems(it)
-                    onToastMessage("영화를 불러왔습니다.")
-                }
-                hideLoading()
-            },
-            fail = {
-                Log.d(TAG, it.toString())
-                when (it) {
-                    is HttpException -> onToastMessage("네트워크에 문제가 있습니다.")
-                    else -> onToastMessage(it.message.toString())
-                }
-                hideLoading()
-            })
-    }
-
-    private fun onToastMessage(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun showLoading() {
+    override fun showLoading() {
         pbLoading.visibility = View.VISIBLE
     }
 
-    private fun hideLoading() {
+    override fun hideLoading() {
         pbLoading.visibility = View.GONE
-    }
-
-    companion object {
-        const val TAG = "MovieSearchActivity"
     }
 
 }
