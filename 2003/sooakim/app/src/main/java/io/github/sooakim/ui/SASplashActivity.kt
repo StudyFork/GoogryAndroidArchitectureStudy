f1d125d8e226dca891023383c0198087d5a99b5b
sooakim <alice2863@naver.com>
2020-03-15 22:59:00 +0900

    HW-3 implement SASplashContractor.View to SASplashActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SASplashActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/SASplashActivity.kt
@@ -1,31 +1,22 @@
 package io.github.sooakim.ui
 
 import android.content.Intent
-import android.os.Bundle
 import io.github.sooakim.ui.base.SAActivity
 import io.github.sooakim.ui.login.SALoginActivity
 import io.github.sooakim.ui.movie.SAMovieSearchActivity
 
-class SASplashActivity : SAActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class SASplashActivity : SAActivity<SASplashPresenter>(), SASplashContractor.View {
+    override val presenter: SASplashPresenter
+        get() = SASplashPresenter(
+            authRepository = requireApplication().authRepository,
+            view = this
+        )
 
-        route()
-    }
-
-    private fun route() {
-        if (requireApplication().authRepository.isAuthRequired) {
-            routeLogin()
-        } else {
-            routeMovieSearch()
-        }
-    }
-
-    private fun routeLogin() {
+    override fun showLogin() {
         startActivity(Intent(this, SALoginActivity::class.java))
     }
 
-    private fun routeMovieSearch() {
+    override fun showMain() {
         startActivity(Intent(this, SAMovieSearchActivity::class.java))
     }
 }
