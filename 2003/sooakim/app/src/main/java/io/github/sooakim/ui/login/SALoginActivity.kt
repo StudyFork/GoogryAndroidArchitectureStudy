192f07cba8379aac5c19f339dc4e1b71dd1b091d
sooakim <alice2863@naver.com>
2020-03-15 23:00:44 +0900

    HW-3 implement SAMovieSearchContractor.View to SALoginActivity

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/login/SALoginActivity.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/login/SALoginActivity.kt
@@ -6,27 +6,26 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.textfield.TextInputEditText
-import com.jakewharton.retrofit2.adapter.rxjava2.HttpException
 import com.jakewharton.rxbinding3.view.clicks
-import com.jakewharton.rxbinding3.widget.textChanges
 import io.github.sooakim.R
 import io.github.sooakim.ui.base.SAActivity
 import io.github.sooakim.ui.movie.SAMovieSearchActivity
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.BiFunction
 import io.reactivex.rxkotlin.addTo
-import java.util.concurrent.TimeUnit
 
-class SALoginActivity : SAActivity() {
+class SALoginActivity : SAActivity<SALoginPresenter>(), SALoginContractor.View {
     private lateinit var idInputEditText: TextInputEditText
     private lateinit var passwordInputEditText: TextInputEditText
     private lateinit var loginButton: AppCompatButton
     private lateinit var loadingProgressBar: ProgressBar
 
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    override val presenter: SALoginPresenter
+        get() = SALoginPresenter(
+            authRepository = requireApplication().authRepository,
+            view = this
+        )
 
     override val commonProgressView: View?
         get() = loadingProgressBar
@@ -52,63 +51,30 @@ class SALoginActivity : SAActivity() {
     }
 
     private fun bindRx() {
-        val idChanges = idInputEditText.textChanges()
-            .map(CharSequence::toString)
-            .toFlowable(BackpressureStrategy.DROP)
-        val pwChanges = passwordInputEditText.textChanges()
-            .map(CharSequence::toString)
-            .toFlowable(BackpressureStrategy.DROP)
-        val request = Flowable.combineLatest(
-            idChanges,
-            pwChanges,
-            BiFunction<String, String, Pair<String, String>> { id, password ->
-                id to password
-            }
-        )
-
         loginButton.clicks()
-            .toFlowable(BackpressureStrategy.DROP)
-            .throttleFirst(1, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
-            .doOnNext { clearError() }
-            .flatMap { request }
-            .doOnNext { showLoading() }
-            .switchMap { (id, password) ->
-                requireApplication().authRepository.login(id, password)
+            .subscribe {
+                presenter.doLogin(
+                    id = idInputEditText.text.toString(),
+                    password = passwordInputEditText.text.toString()
+                )
             }
-            .doOnNext { hideLoading() }
-            .doOnError { hideLoading() }
-            .doOnError(this::showError)
-            .retry { error -> (error is HttpException) }
-            .subscribe { routeMovieSearch() }
             .addTo(compositeDisposable)
     }
 
-    private fun clearError() {
+    override fun clearErrors() {
         idInputEditText.error = null
         passwordInputEditText.error = null
     }
 
-    private fun showError(throwable: Throwable) {
-        when (throwable) {
-            is HttpException -> when (throwable.code()) {
-                404 -> {
-                    idInputEditText.error = getString(R.string.activity_login_iet_id_error)
-                }
-                409 -> {
-                    passwordInputEditText.error =
-                        getString(R.string.activity_login_iet_password_error)
-                }
-                else -> {
-                    clearError()
-                }
-            }
-            else -> {
-                clearError()
-            }
-        }
+    override fun showIdError() {
+        idInputEditText.error = getString(R.string.activity_login_iet_id_error)
     }
 
-    private fun routeMovieSearch() {
+    override fun showPasswordError() {
+        passwordInputEditText.error = getString(R.string.activity_login_iet_password_error)
+    }
+
+    override fun showMovieSearch() {
         startActivity(Intent(this, SAMovieSearchActivity::class.java))
         finish()
     }
