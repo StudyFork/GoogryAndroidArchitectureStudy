f88b568282a581486421d1636ada12c7626d5488
kangraemin <ee4787@naver.com>
2020-03-15 20:24:47 +0900

    HW-3 Add login contract

---
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/contract/LoginContract.kt
@@ -0,0 +1,28 @@
+package com.example.kangraemin.contract
+
+import com.example.kangraemin.base.KangBasePresenter
+import com.example.kangraemin.model.AuthRepository
+
+interface LoginContract {
+    interface View {
+        fun showEmptyIdError()
+        fun hideEmptyIdError()
+        fun showPasswordEmptyError()
+        fun hidePasswordEmptyError()
+        fun showFailedLoginError()
+        fun hideFailedLoginError()
+        fun enableLoginButton()
+        fun disableLoginButton()
+        fun startMainActivity()
+    }
+
+    interface Presenter : KangBasePresenter {
+        fun checkIdIsEmpty(id: String, hasFocus: Boolean)
+        fun checkPasswordIsEmpty(password: String, hasFocus: Boolean)
+        fun checkLoginInfoHasEntered(id: String, password: String): Boolean
+        fun checkAutoLoginStatus(authRepository: AuthRepository)
+        fun activateButton(allValueEntered: Boolean)
+        fun addAutoLoginStatus(authRepository: AuthRepository)
+        fun login(id: String, password: String)
+    }
+}
