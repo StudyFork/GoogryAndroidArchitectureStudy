567b21c272a8556cf92ffbdd4e51229be08444c5
jackjackE <tmddjs210@naver.com>
2020-03-17 00:19:59 +0900

    HW-3 Login - 로그인 검사 로직 변경

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/login/LoginPresenter.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/login/LoginPresenter.kt
@@ -3,12 +3,12 @@ package com.mtjin.androidarchitecturestudy.ui.login
 class LoginPresenter(private val view: LoginContract.View) : LoginContract.Presenter {
 
     override fun login(id: String, pw: String) {
-        if (id != USER_ID || pw != USER_PW) {
-            view.showLoginErrorToast()
-        } else if (id.isEmpty()) {
+        if (id.isEmpty()) {
             view.showIdEmptyError()
         } else if (pw.isEmpty()) {
             view.showPwEmptyError()
+        } else if (id != USER_ID || pw != USER_PW) {
+            view.showLoginErrorToast()
         } else {
             view.goMovieSearchActivity()
             view.saveAutoLoginSharedPref()
