48bb5cf7a4e30e249850532b1eb8c94428103c00
byiryu <byiryu@gmail.com>
2020-03-17 15:17:24 +0900

    HW-3 create BaseContract

---
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/base/BaseContract.kt
@@ -0,0 +1,29 @@
+package com.byiryu.study.ui.base
+
+import android.content.Intent
+import androidx.annotation.StringRes
+
+interface BaseContract{
+
+    interface View{
+        fun showMsg(@StringRes res : Int)
+
+        fun showMsg(msg : String)
+
+        fun showLoading()
+
+        fun hideLoading()
+
+        fun goActivity(clazz: Class<*>)
+
+        fun goActivity(intent : Intent)
+
+        fun goActivity()
+    }
+
+    interface Presenter<T : View>{
+        fun onAttach(view: T)
+
+        fun onDetach()
+    }
+}
