df6b4a7d585f6da4a67f5a5d43e201b1df68e1bd
sooakim <alice2863@naver.com>
2020-03-22 23:51:03 +0900

    HW-4 apply dataBinding to recyclerView

--- 2003/sooakim/app/src/main/java/io/github/sooakim/ui/base/SAViewHolder.kt
+++ 2003/sooakim/app/src/main/java/io/github/sooakim/ui/base/SAViewHolder.kt
@@ -1,6 +1,24 @@
 package io.github.sooakim.ui.base
 
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import io.github.sooakim.BR
+import io.github.sooakim.ui.model.SAPresentation
 
-open class SAViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+open class SAViewHolder<VDB : ViewDataBinding, Model : SAPresentation>(
+    itemView: View,
+    protected val brId: Int = BR.item
+) : RecyclerView.ViewHolder(itemView), SAViewHolderLifecycle<Model> {
+    protected val viewDataBinding: VDB = DataBindingUtil.bind<VDB>(itemView)!!
+
+    override fun onBind(item: Model) {
+        viewDataBinding.setVariable(brId, item)
+        viewDataBinding.executePendingBindings()
+    }
+
+    override fun onRecycle() {
+
+    }
+}
