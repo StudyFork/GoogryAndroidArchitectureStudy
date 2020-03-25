88f50312fe4a448e6388fcdebb2f266c2b178841
jackjackE <tmddjs210@naver.com>
2020-03-26 00:01:02 +0900

    HW-4 bindingAdapter - bind prefix 제거

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/utils/DataBindingAdapters.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/utils/DataBindingAdapters.kt
@@ -10,7 +10,7 @@ import com.bumptech.glide.Glide
 import com.mtjin.androidarchitecturestudy.R
 
 
-@BindingAdapter("bind:htmlText")
+@BindingAdapter("htmlText")
 fun setHtmlText(textView: TextView, html: String) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         textView.text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
@@ -19,14 +19,14 @@ fun setHtmlText(textView: TextView, html: String) {
     }
 }
 
-@BindingAdapter("bind:urlImage")
+@BindingAdapter("urlImage")
 fun setUrlImage(imageView: ImageView, url: String) {
     Glide.with(imageView.context).load(url)
         .placeholder(R.drawable.ic_default)
         .into(imageView)
 }
 
-@BindingAdapter("bind:movieRating")
+@BindingAdapter("movieRating")
 fun setMovieRating(ratingBar: RatingBar, score: String) {
     ratingBar.rating = (score.toFloatOrNull() ?: 0f) / 2
 }
