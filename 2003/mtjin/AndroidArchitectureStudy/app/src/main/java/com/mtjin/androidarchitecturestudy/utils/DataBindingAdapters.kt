7d8e2917cb38ed7687f7a16628a83ae15c855d46
jackjackE <tmddjs210@naver.com>
2020-03-30 22:14:15 +0900

    HW-5 DataBindingAdapter 수정

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/utils/DataBindingAdapters.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/utils/DataBindingAdapters.kt
@@ -5,33 +5,38 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mtjin.androidarchitecturestudy.R
+import com.mtjin.androidarchitecturestudy.data.search.Movie
+import com.mtjin.androidarchitecturestudy.ui.search.MovieAdapter
 
 
 @BindingAdapter("htmlText")
-fun setHtmlText(textView: TextView, html: String) {
-    HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
+fun TextView.setHtmlText(html: String) {
+    text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
 }
 
 @BindingAdapter("urlImage")
-fun setUrlImage(imageView: ImageView, url: String) {
-    Glide.with(imageView).load(url)
+fun ImageView.setUrlImage(url: String) {
+    Glide.with(context).load(url)
         .placeholder(R.drawable.ic_default)
-        .into(imageView)
+        .into(this)
 }
 
 @BindingAdapter("movieRating")
-fun setMovieRating(ratingBar: RatingBar, score: String) {
-    ratingBar.rating = (score.toFloatOrNull() ?: 0f) / 2
-}
-
-/*
-확장함수 사용 필기용입니다.
-@BindingAdapter("app:movieRating")
 fun RatingBar.setMovieRating(score: String) {
     rating = (score.toFloatOrNull() ?: 0f) / 2
 }
-*/
+
+@BindingAdapter("setItems", "loadMore")
+fun RecyclerView.setAdapterItems(items: List<Movie>?, loadMore: Boolean) {
+    with((adapter as MovieAdapter)) {
+        if (!loadMore) {
+            this.clear()
+        }
+        items?.let { this.addItems(it) }
+    }
+}
 
 
