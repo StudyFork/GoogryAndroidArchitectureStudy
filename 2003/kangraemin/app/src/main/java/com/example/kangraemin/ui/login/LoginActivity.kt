ba36e8face37c3e98ea13282d5b10dc897b4f2f2
kangraemin <ee4787@naver.com>
2020-04-01 05:02:19 +0900

    HW-5 Use view model in login ui

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/ui/login/LoginActivity.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/ui/login/LoginActivity.kt
@@ -3,22 +3,23 @@ package com.example.kangraemin.ui.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import com.example.kangraemin.R
 import com.example.kangraemin.base.KangBaseActivity
 import com.example.kangraemin.databinding.ActivityLoginBinding
 import com.example.kangraemin.model.AppDatabase
 import com.example.kangraemin.model.AuthRepository
 import com.example.kangraemin.model.local.datadao.AuthLocalDataSourceImpl
+import com.example.kangraemin.ui.login.LoginViewModel.AddAuthResponse.ADD_AUTH_ERROR
+import com.example.kangraemin.ui.login.LoginViewModel.AddAuthResponse.ADD_AUTH_SUCCESS
+import com.example.kangraemin.ui.login.LoginViewModel.LogInResponse.LOGIN_ERROR
+import com.example.kangraemin.ui.login.LoginViewModel.LogInResponse.LOGIN_SUCCESS
 import com.example.kangraemin.ui.main.MainActivity
-import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.view.focusChanges
-import com.jakewharton.rxbinding3.widget.textChanges
-import io.reactivex.Observable
-import io.reactivex.functions.BiFunction
 
-class LoginActivity : KangBaseActivity(), LoginContract.View {
+class LoginActivity : KangBaseActivity() {
 
-    private lateinit var presenter: LoginContract.Presenter
+    private lateinit var loginViewModel: LoginViewModel
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -27,8 +28,7 @@ class LoginActivity : KangBaseActivity(), LoginContract.View {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        presenter = LoginPresenter(
-            this,
+        loginViewModel = LoginViewModel(
             authRepository = AuthRepository(
                 authLocalDataSource = AuthLocalDataSourceImpl(
                     AppDatabase.getInstance(context = this).authDao()
@@ -36,93 +36,74 @@ class LoginActivity : KangBaseActivity(), LoginContract.View {
             )
         )
 
-        val whenTextChanged = Observable
-            .combineLatest(
-                binding.etId.textChanges(),
-                binding.etPw.textChanges(),
-                BiFunction<CharSequence, CharSequence, Pair<String, String>> { id, pw ->
-                    id.toString() to pw.toString()
-                }
-            )
-            .subscribe {
-                presenter.checkLoginInfoHasEntered(it.first, it.second)
-            }
-        compositeDisposable.add(whenTextChanged)
+        binding.vm = loginViewModel
 
         val whenIdFocusChange = binding.etId.focusChanges()
             .skip(1)
             .subscribe { hasFocus ->
-                presenter.checkIdIsEmpty(id = binding.etId.text.toString(), hasFocus = hasFocus)
+                loginViewModel.checkIdIsEmpty(hasFocus = hasFocus)
             }
         compositeDisposable.add(whenIdFocusChange)
 
         val whenPwFocusChange = binding.etPw.focusChanges()
             .skip(1)
             .subscribe { hasFocus ->
-                presenter.checkPasswordIsEmpty(
-                    password = binding.etPw.text.toString(),
-                    hasFocus = hasFocus
-                )
+                loginViewModel.checkPasswordIsEmpty(hasFocus = hasFocus)
             }
         compositeDisposable.add(whenPwFocusChange)
 
-        val whenLoginButtonClicked = binding.btnLogin.clicks()
-            .subscribe {
-                presenter.login(
-                    id = binding.etId.text.toString(),
-                    password = binding.etPw.text.toString(),
-                    isAutoLogin = binding.chbAutoLogin.isChecked
-                )
+        loginViewModel.loginResponse.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                when (loginViewModel.loginResponse.get()) {
+                    LOGIN_SUCCESS -> {
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    LOGIN_ERROR -> {
+                        binding.layoutPw.error = getString(R.string.login_fail)
+                    }
+                }
             }
-        compositeDisposable.add(whenLoginButtonClicked)
-    }
+        })
 
-    override fun showEmptyIdError() {
-        binding.layoutId.error = getString(R.string.login_error_id_empty)
-    }
+        loginViewModel.idIsEmpty.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                if (loginViewModel.idIsEmpty.get()) {
+                    binding.layoutId.error = getString(R.string.login_error_id_empty)
+                } else {
+                    binding.layoutId.error = null
+                }
+            }
+        })
 
-    override fun hideEmptyIdError() {
-        binding.layoutId.error = null
-    }
+        loginViewModel.pwIsEmpty.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                if (loginViewModel.pwIsEmpty.get()) {
+                    binding.layoutPw.error = getString(R.string.login_error_pw_empty)
+                } else {
+                    binding.layoutPw.error = null
+                }
+            }
+        })
 
-    override fun showPasswordEmptyError() {
-        binding.layoutPw.error = getString(R.string.login_error_pw_empty)
-    }
-
-    override fun hidePasswordEmptyError() {
-        binding.layoutPw.error = null
-    }
-
-    override fun showFailedLoginError() {
-        binding.layoutPw.error = getString(R.string.login_fail)
-    }
-
-    override fun hideFailedLoginError() {
-        binding.layoutPw.error = null
-    }
-
-    override fun showAddAuthError() {
-        toast(getString(R.string.login_error_add_auth_toast_message))
-    }
-
-    override fun enableLoginButton() {
-        binding.btnLogin.isEnabled = true
-        binding.btnLogin.alpha = 1f
-    }
-
-    override fun disableLoginButton() {
-        binding.btnLogin.isEnabled = false
-        binding.btnLogin.alpha = 0.5f
-    }
-
-    override fun startMain() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    override fun onDestroy() {
-        presenter.onViewDestroy()
-        super.onDestroy()
+        loginViewModel.addAuthResponse.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                when (loginViewModel.addAuthResponse.get()) {
+                    ADD_AUTH_SUCCESS -> {
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    ADD_AUTH_ERROR -> {
+                        toast(getString(R.string.login_error_add_auth_toast_message))
+                    }
+                }
+            }
+        })
     }
 }
