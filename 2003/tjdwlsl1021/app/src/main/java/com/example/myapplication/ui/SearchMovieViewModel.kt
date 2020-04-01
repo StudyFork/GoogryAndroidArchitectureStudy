5f80a6f8895ff5f956c70eacd2b242f8ad667b14
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-04-02 01:12:53 +0900

    ViewModel 작업

--- 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/ui/SearchMovieViewModel.kt
+++ 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/ui/SearchMovieViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.myapplication.ui
 
-class SearchMovieViewModel {
+import androidx.databinding.ObservableField
+import com.example.myapplication.R
+import com.example.myapplication.data.local.MovieEntity
+import com.example.myapplication.data.repository.MovieRepository
+
+class SearchMovieViewModel(private val movieRepository: MovieRepository) {
+    var query = ObservableField<String>()
+    var movieList = ObservableField<ArrayList<MovieEntity>>()
+    var emptyMsg = ObservableField<Pair<Boolean, Any>>()
+    var failMsg = ObservableField<Pair<Boolean, Any>>()
+
+    fun searchMovie() {
+        if (query.get().isNullOrEmpty()) {
+            emptyMsg.set(Pair(false, R.string.activity_toast_empty_movie_title))
+        } else {
+            movieRepository.getMovieList(
+                query.get().toString(),
+                success = {
+                    movieList.set(it as ArrayList<MovieEntity>)
+                },
+                failed = {
+                    failMsg.set(Pair(false, it.toString()))
+                })
+        }
+    }
 }
