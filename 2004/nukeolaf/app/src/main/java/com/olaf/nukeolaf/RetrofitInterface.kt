64ae5d2f27a8caf86ef26b90130475ee1ec10c6e
nukeolaf <yhjin04170@gmail.com>
2020-04-05 22:52:07 +0900

    RetrofitInterface 에 영화 검색하는 GET 함수 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitInterface.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitInterface.kt
@@ -1,5 +1,16 @@
 package com.olaf.nukeolaf
 
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.QueryMap
+
 interface RetrofitInterface {
-    
+
+    @GET("movie.json")
+    fun searchMovie(
+        @Header("X-Naver-Client-Id") clientId: String,
+        @Header("X-Naver-Client-Secret") clientSecret: String,
+        @QueryMap map: Map<String, String>
+    )
+
 }
