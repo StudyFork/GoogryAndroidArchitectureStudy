74821db014d70c463cb351bb406f29889ddc4a02
nukeolaf <yhjin04170@gmail.com>
2020-04-05 23:13:02 +0900

    retrofit 인터페이스를 MainActivity 전역변수로 선언

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
@@ -7,6 +7,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var retrofitClient: RetrofitInterface
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
