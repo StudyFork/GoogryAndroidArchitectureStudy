ef609304dd79bb263643b871885e103947c25e98
nukeolaf <yhjin04170@gmail.com>
2020-04-05 23:15:11 +0900

    inputString 의 공백확인 메서드 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.olaf.nukeolaf
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,5 +25,10 @@ class MainActivity : AppCompatActivity() {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
         })
+    }
+
+    // inputString 공백 확인 메서드
+    private fun isEmptyText(text: String): Boolean {
+        return TextUtils.isEmpty(text)
     }
 }
