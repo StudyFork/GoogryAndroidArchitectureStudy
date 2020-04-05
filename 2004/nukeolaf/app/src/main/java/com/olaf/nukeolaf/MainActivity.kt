73c5ec1146f5c8ac2f02ab52b02730100d04e52f
nukeolaf <yhjin04170@gmail.com>
2020-04-05 23:01:04 +0900

    searchView 에 검색 이벤트 리스너 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.olaf.nukeolaf
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
