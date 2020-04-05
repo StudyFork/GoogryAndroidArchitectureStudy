3ab9dceab568034e0117635f09a698173a6f88e9
eunjin-hong <ejhong.4991@gmail.com>
2020-04-06 01:14:36 +0900

    HW1 :sparkles: show movie information on item clicked.

--- 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MovieAdapter.kt
+++ 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MovieAdapter.kt
@@ -1,10 +1,13 @@
 package com.eunice.eunicehong.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -45,12 +48,18 @@ class MovieAdapter(private val items: ArrayList<Movie>) :
                 rating.text = movie.userRating
 
                 Glide.with(poster.context).load(movie.imageUrl).into(poster)
+
+                card.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(movie.link))
+                    card.context.startActivity(intent)
+                }
             }
         }
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val card = itemView.findViewById<CardView>(R.id.movie_card)
         val title = itemView.findViewById<TextView>(R.id.movie_title)
         val poster = itemView.findViewById<ImageView>(R.id.movie_poster)
         val subtitle = itemView.findViewById<TextView>(R.id.movie_subtitle)
