c80749d0a8df4c6faaef4c389723c48862fc987d
nukeolaf <yhjin04170@gmail.com>
2020-04-06 00:02:54 +0900

    Retrofit Interface searchMovie() 의 리턴 Call<Movie> 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitInterface.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitInterface.kt
@@ -1,5 +1,6 @@
 package com.olaf.nukeolaf
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.QueryMap
@@ -11,6 +12,6 @@ interface RetrofitInterface {
         @Header("X-Naver-Client-Id") clientId: String,
         @Header("X-Naver-Client-Secret") clientSecret: String,
         @QueryMap map: Map<String, Any>
-    )
+    ): Call<Movie>
 
 }
