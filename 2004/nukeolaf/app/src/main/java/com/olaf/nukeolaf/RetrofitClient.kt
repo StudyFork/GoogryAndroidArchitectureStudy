625c402cc07eb451daa47dd300f3922ed97229c6
nukeolaf <yhjin04170@gmail.com>
2020-04-05 23:10:09 +0900

    retrofit client object 클래스 추가

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitClient.kt
@@ -0,0 +1,21 @@
+package com.olaf.nukeolaf
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    val client: RetrofitInterface
+    private const val BASE_URL = "https://openapi.naver.com/v1/search/"
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        client = retrofit.create(
+            RetrofitInterface::class.java
+        )
+    }
+}
