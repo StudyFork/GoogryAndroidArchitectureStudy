021ce08d5c18898608c4b05d9d571c1a742056cd
nukeolaf <yhjin04170@gmail.com>
2020-04-06 03:17:06 +0900

    MovieDetailActivity 에서 intent 로 넘어온 MovieItem 보여주는 코드 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieDetailActivity.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieDetailActivity.kt
@@ -2,11 +2,32 @@ package com.olaf.nukeolaf
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        var movieItem = intent.getSerializableExtra("movieItem") as MovieItem
+
+        val title = "${movieItem.title} (${movieItem.pubDate})"
+        val director = "감독 : ${movieItem.director}"
+        val actor = "출연진 : ${movieItem.actor}"
+
+        movie_title.text = title
+        movie_subtitle.text = movieItem.subtitle
+        movie_rating.numStars = 5
+        movie_rating.rating = movieItem.userRating / 2
+        movie_rating_num.text = movieItem.userRating.toString()
+        movie_director.text = director
+        movie_actor.text = actor
+        movie_link.text = movieItem.link
+
+        Glide.with(this)
+            .load(movieItem.image)
+            .into(movie_image)
     }
 }
