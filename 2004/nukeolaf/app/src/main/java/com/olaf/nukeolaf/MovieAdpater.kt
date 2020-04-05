eaf7ce8bdaecc96c0d5a0bbd83db11d62ff16d2c
nukeolaf <yhjin04170@gmail.com>
2020-04-05 23:38:17 +0900

    영화 목록 출력 리사이클러뷰 어댑터 클래스 작성

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdpater.kt
@@ -0,0 +1,27 @@
+package com.olaf.nukeolaf
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdpater() : RecyclerView.Adapter<MovieAdpater.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdpater.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MovieAdpater.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind() {
+            
+        }
+
+    }
+}
