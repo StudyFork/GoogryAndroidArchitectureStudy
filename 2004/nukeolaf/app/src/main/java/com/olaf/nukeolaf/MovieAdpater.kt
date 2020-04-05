4348f40aa878f8b654c6c319b3a414108c557d8f
nukeolaf <yhjin04170@gmail.com>
2020-04-06 00:43:15 +0900

    MovieAdapter 파라미터로 영화 목록 리스트 받을 수 있게 함

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdpater.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdpater.kt
@@ -4,7 +4,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class MovieAdpater() : RecyclerView.Adapter<MovieAdpater.ViewHolder>() {
+class MovieAdpater(
+    private var movies: ArrayList<MovieItem>
+) : RecyclerView.Adapter<MovieAdpater.ViewHolder>() {
+    
+    fun setMovies(list: ArrayList<MovieItem>) {
+        this.movies = list
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdpater.ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -20,7 +27,7 @@ class MovieAdpater() : RecyclerView.Adapter<MovieAdpater.ViewHolder>() {
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind() {
-            
+
         }
 
     }
