4e48d3d0989b3581fb5c240856cbb4ed9e9da7d9
nukeolaf <yhjin04170@gmail.com>
2020-04-06 02:17:08 +0900

    영화 상세내용 보여줄 액티비티 추가

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.olaf.nukeolaf
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
