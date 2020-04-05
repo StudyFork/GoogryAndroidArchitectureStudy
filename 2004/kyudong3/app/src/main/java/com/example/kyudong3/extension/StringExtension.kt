57bbca2d1bcbe94dffba3da76c524f9e49aa5f64
kyudong3 <kyudong3@jasoseol.com>
2020-04-06 02:08:43 +0900

    HW-1 add htmlToString extension

---
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/extension/StringExtension.kt
@@ -0,0 +1,12 @@
+package com.example.kyudong3.extension
+
+import android.os.Build
+import android.text.Html
+
+fun String.htmlToString(): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+    } else {
+        Html.fromHtml(this).toString()
+    }
+}
