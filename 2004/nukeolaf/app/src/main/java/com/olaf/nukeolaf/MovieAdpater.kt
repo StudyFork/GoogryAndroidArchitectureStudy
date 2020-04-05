bc40605165ea5381147a49c9d9f6f6fd12a5fc8a
nukeolaf <yhjin04170@gmail.com>
2020-04-06 00:45:15 +0900

    MovieAdapter onCreateViewHolder 작성

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdpater.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdpater.kt
@@ -1,5 +1,6 @@
 package com.olaf.nukeolaf
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,13 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 class MovieAdpater(
     private var movies: ArrayList<MovieItem>
 ) : RecyclerView.Adapter<MovieAdpater.ViewHolder>() {
-    
+
     fun setMovies(list: ArrayList<MovieItem>) {
         this.movies = list
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdpater.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inflatedView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movies_rv, parent)
+        return (ViewHolder(inflatedView))
     }
 
     override fun getItemCount(): Int {
