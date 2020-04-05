ee0c7976098df03fb640a209f70becd42412e8cf
nukeolaf <yhjin04170@gmail.com>
2020-04-06 00:35:20 +0900

    Movie data class 에서 안 쓰이는 변수 제거

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/Movie.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/Movie.kt
@@ -1,9 +1,6 @@
 package com.olaf.nukeolaf
 
 data class Movie(
-    val title: String,
-    val link: String,
-    val description: String,
     val lastBuildDate: String,
     val total: String,
     val start: String,
