055052294e97ed66948c543508869c553102559d
nukeolaf <yhjin04170@gmail.com>
2020-04-07 23:34:19 +0900

    retrofitClient 에 okHttpClient 추가함

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitClient.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.olaf.nukeolaf
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,8 +11,14 @@ object RetrofitClient {
     private const val BASE_URL = "https://openapi.naver.com/"
 
     init {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
