1ea9d19c290c935848d2265ad6bd70fc7260060c
nukeolaf <yhjin04170@gmail.com>
2020-04-08 00:08:27 +0900

    htmlToString() 함수를 ViewHolder 클래스 안으로 옮기고 private 하게 만들어줌

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdapter.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/MovieAdapter.kt
@@ -58,13 +58,13 @@ class MovieAdapter(
                 .into(view.movie_image)
         }
 
-    }
-
-    fun String.htmlToString(): String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
-        } else {
-            Html.fromHtml(this).toString()
+        private fun String.htmlToString(): String {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+            } else {
+                Html.fromHtml(this).toString()
+            }
         }
+
     }
 }
