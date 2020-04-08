04507b59018b8b43d3f2f92ca4844d43f9295755
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-04-08 12:22:09 +0900

    liveData로 변경 및 AAC View Model로 변경

--- 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/ui/SearchMovieViewModel.kt
+++ 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/ui/SearchMovieViewModel.kt
@@ -1,26 +1,29 @@
 package com.example.myapplication.ui
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.myapplication.R
 import com.example.myapplication.data.local.MovieEntity
 import com.example.myapplication.data.repository.MovieRepository
 
-class SearchMovieViewModel(private val movieRepository: MovieRepository) {
-    val query = ObservableField<String>()
-    val movieList = ObservableField<List<MovieEntity>>()
-    val failMsg = ObservableField<Pair<Boolean, Any>>()
+class SearchMovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+
+    val query = MutableLiveData<String>()
+    val movieList = MutableLiveData<List<MovieEntity>>()
+    val failMsg = MutableLiveData<Pair<Boolean, Any>>()
 
     fun searchMovie() {
-        if (query.get().isNullOrEmpty()) {
-            failMsg.set(Pair(false, R.string.activity_toast_empty_movie_title))
+
+        if (query.value.isNullOrEmpty()) {
+            failMsg.value = (Pair(false, R.string.activity_toast_empty_movie_title))
         } else {
             movieRepository.getMovieList(
-                query.get().toString(),
+                query.value.toString(),
                 success = {
-                    movieList.set(it)
+                    movieList.value = it
                 },
                 failed = {
-                    failMsg.set(Pair(false, it.toString()))
+                    failMsg.value = (Pair(false, it.toString()))
                 })
         }
     }
