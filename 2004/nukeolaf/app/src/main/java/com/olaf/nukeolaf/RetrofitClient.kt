7f4fabb1062a8f0299354979eef4b77c9418c022
nukeolaf <yhjin04170@gmail.com>
2020-04-09 05:33:31 +0900

    Retrofit client 에 HeaderInterceptor 추가

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitClient.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/RetrofitClient.kt
@@ -1,5 +1,8 @@
 package com.olaf.nukeolaf
 
+import com.olaf.nukeolaf.BuildConfig.Naver_Client_Id
+import com.olaf.nukeolaf.BuildConfig.Naver_Client_Secret
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,8 +13,16 @@ object RetrofitClient {
     val client: RetrofitInterface
     private const val BASE_URL = "https://openapi.naver.com/"
 
+
     init {
         val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor { chain: Interceptor.Chain ->
+                val original = chain.request()
+                chain.proceed(original.newBuilder().apply {
+                    addHeader("X-Naver-Client-Id", Naver_Client_Id)
+                    addHeader("X-Naver-Client-Secret", Naver_Client_Secret)
+                }.build())
+            }
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             }).build()
