1c2e00209556af3361da0bfc6a0070822c38ed28
TaeseongYun <yts0646@gmail.com>
2020-04-08 11:08:25 +0900

    modify: import package imageview

--- 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/util/widget/CustomImageWithGlide.kt
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/util/widget/CustomImageWithGlide.kt
@@ -5,12 +5,13 @@ import android.util.AttributeSet
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.tsdev.tsandroid.R
+import androidx.appcompat.widget.AppCompatImageView
 
 class CustomImageWithGlide @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defAttrs: Int = 0
-) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defAttrs) {
+) : AppCompatImageView(context, attrs, defAttrs) {
     fun loadMovieImage(url: String) {
         Glide.with(this)
             .load(url)
