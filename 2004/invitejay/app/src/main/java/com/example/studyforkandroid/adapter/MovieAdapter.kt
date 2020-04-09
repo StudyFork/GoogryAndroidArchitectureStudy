f1501ce01e3aaab074baa176d7fd59246a7b8c60
invitejay <sksskdi122345@gmail.com>
2020-04-09 17:24:31 +0900

    fix recyclerview adapter onclicklistener position

--- 2004/invitejay/app/src/main/java/com/example/studyforkandroid/adapter/MovieAdapter.kt
+++ 2004/invitejay/app/src/main/java/com/example/studyforkandroid/adapter/MovieAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.studyforkandroid.adapter
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -21,13 +19,16 @@ class MovieAdapter(val context: Context) : RecyclerView.Adapter<MovieAdapter.Mov
     private var movieList: List<Item> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.MovieVh {
-        return MovieVh(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_movie,
-                parent,
-                false
-            )
-        )
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        val movieVh = MovieVh(view)
+        view.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(movieList[movieVh.adapterPosition].link)
+            parent.context.startActivity(intent)
+        }
+
+        return movieVh
     }
 
     override fun getItemCount(): Int {
@@ -60,6 +61,5 @@ class MovieAdapter(val context: Context) : RecyclerView.Adapter<MovieAdapter.Mov
         val title: TextView = itemView.movieTitle
         val actor: TextView = itemView.movieActor
         val rating: TextView = itemView.movieRating
-        val layout: LinearLayout = itemView.movieLayout
     }
 }
