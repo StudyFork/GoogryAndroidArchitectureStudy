20d4feca84ab3cf8cd7f3b6dfcd98c665527cfee
kyudong3 <kyudong3@jasoseol.com>
2020-04-10 22:48:13 +0900

    HW-1 modify mainActivity class

--- 2004/kyudong3/app/src/main/java/com/example/kyudong3/MainActivity.kt
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/MainActivity.kt
@@ -5,7 +5,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kyudong3.adapter.SearchMovieRvAdapter
-import com.example.kyudong3.model.MovieList
+import com.example.kyudong3.model.MovieReceiver
 import com.example.kyudong3.network.NaverApiServiceImpl
 import com.example.kyudong3.util.RecyclerViewItemDivider
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,7 +16,7 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private val movieRvAdapter: SearchMovieRvAdapter by lazy {
-        SearchMovieRvAdapter(this)
+        SearchMovieRvAdapter()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +25,10 @@ class MainActivity : AppCompatActivity() {
 
         setMovieRecyclerView()
 
-        searchETxt.setOnEditorActionListener { v, actionId, event ->
+        searchETxt.setOnEditorActionListener { _, actionId, _ ->
             when (actionId) {
                 EditorInfo.IME_ACTION_UNSPECIFIED, EditorInfo.IME_ACTION_GO -> {
-                    if (searchETxt.text.trim().isEmpty()) {
-                        Toast.makeText(applicationContext, "검색어를 1자 이상 입력해주세요!", Toast.LENGTH_SHORT)
-                            .show()
-                    } else {
-                        fetchSearchMovieApi(searchETxt.text.trim().toString())
-                    }
+                    searchMovie()
                     true
                 }
                 else -> {
@@ -43,11 +38,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         searchBtn.setOnClickListener {
-            if (searchETxt.text.trim().isEmpty()) {
-                Toast.makeText(applicationContext, "검색어를 1자 이상 입력해주세요!", Toast.LENGTH_SHORT).show()
-            } else {
-                fetchSearchMovieApi(searchETxt.text.trim().toString())
-            }
+            searchMovie()
         }
     }
 
@@ -58,12 +49,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun searchMovie() {
+        if (searchETxt.text.trim().isEmpty()) {
+            Toast.makeText(applicationContext, "검색어를 1자 이상 입력해주세요!", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            fetchSearchMovieApi(searchETxt.text.trim().toString())
+        }
+    }
+
     private fun fetchSearchMovieApi(searchQuery: String) {
         val naverApiServiceImpl: NaverApiServiceImpl = NaverApiServiceImpl()
         val naverApiServiceCall = naverApiServiceImpl.getSearchMovie(searchQuery)
 
-        naverApiServiceCall.enqueue(object : Callback<MovieList> {
-            override fun onFailure(call: Call<MovieList>, t: Throwable) {
+        naverApiServiceCall.enqueue(object : Callback<MovieReceiver> {
+            override fun onFailure(call: Call<MovieReceiver>, t: Throwable) {
                 Toast.makeText(
                     applicationContext,
                     "네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.",
@@ -71,11 +71,11 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
 
-            override fun onResponse(call: Call<MovieList>, response: Response<MovieList>) {
-                if (response.body() != null) {
-                    val movieList = response.body()
+            override fun onResponse(call: Call<MovieReceiver>, response: Response<MovieReceiver>) {
+                val movieReceiver = response.body()
+                if (response.isSuccessful && movieReceiver != null) {
 
-                    movieList?.run {
+                    movieReceiver.run {
                         if (items.isEmpty()) {
                             Toast.makeText(applicationContext, "검색결과가 없습니다", Toast.LENGTH_SHORT)
                                 .show()
