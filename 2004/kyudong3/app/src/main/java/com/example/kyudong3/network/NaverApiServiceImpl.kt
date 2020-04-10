f99caab1c60143bf355928ef58149ec3a0ccad56
kyudong3 <kyudong3@jasoseol.com>
2020-04-11 00:42:12 +0900

    HW-1 change build config constants UpperCase

--- 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/NaverApiServiceImpl.kt
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/NaverApiServiceImpl.kt
@@ -5,8 +5,8 @@ import com.example.kyudong3.BuildConfig
 class NaverApiServiceImpl {
     private val naverService: NaverApiService
 
-    private val clientId: String = BuildConfig.clientId
-    private val clientSecret: String = BuildConfig.clientSecret
+    private val clientId: String = BuildConfig.CLIENT_ID
+    private val clientSecret: String = BuildConfig.CLIENT_SECRET
 
     init {
         naverService = RetrofitClient.getNaverService()
