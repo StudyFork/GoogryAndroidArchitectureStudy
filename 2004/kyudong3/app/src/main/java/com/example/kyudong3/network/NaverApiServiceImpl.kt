4242e14defdc98e45e438f4a46ffe66077bd498e
kyudong3 <kyudong3@jasoseol.com>
2020-04-11 00:47:05 +0900

    HW-1 declare and assign naverService in oneline

--- 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/NaverApiServiceImpl.kt
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/NaverApiServiceImpl.kt
@@ -3,14 +3,10 @@ package com.example.kyudong3.network
 import com.example.kyudong3.BuildConfig
 
 class NaverApiServiceImpl {
-    private val naverService: NaverApiService
+    private val naverService: NaverApiService = RetrofitClient.getNaverService()
 
     private val clientId: String = BuildConfig.CLIENT_ID
     private val clientSecret: String = BuildConfig.CLIENT_SECRET
-
-    init {
-        naverService = RetrofitClient.getNaverService()
-    }
 
     fun getSearchMovie(searchQuery: String) =
         naverService.getSearchMovie(clientId, clientSecret, searchQuery)
