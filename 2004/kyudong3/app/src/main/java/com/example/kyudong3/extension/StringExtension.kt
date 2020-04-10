5893c366a3fea0911a6089df0b22d446c83c4a9e
kyudong3 <kyudong3@jasoseol.com>
2020-04-10 21:37:22 +0900

    HW-1 modify Html String to HtmlCompat

--- 2004/kyudong3/app/src/main/java/com/example/kyudong3/extension/StringExtension.kt
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/extension/StringExtension.kt
@@ -2,11 +2,8 @@ package com.example.kyudong3.extension
 
 import android.os.Build
 import android.text.Html
+import androidx.core.text.HtmlCompat
 
 fun String.htmlToString(): String {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
-    } else {
-        Html.fromHtml(this).toString()
-    }
+    return HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
 }
