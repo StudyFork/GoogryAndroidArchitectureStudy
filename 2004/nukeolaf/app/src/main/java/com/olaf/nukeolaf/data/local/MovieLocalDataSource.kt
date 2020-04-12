770b2ca5919577fe5ee07214dc3057e3de87b75e
nukeolaf <yhjin04170@gmail.com>
2020-04-12 20:50:53 +0900

    HW-2 MovieLocalDataSource 인터페이스 작성

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/data/local/MovieLocalDataSource.kt
@@ -0,0 +1,9 @@
+package com.olaf.nukeolaf.data.local
+
+import com.olaf.nukeolaf.data.model.MovieResponse
+
+interface MovieLocalDataSource {
+
+    fun saveMovies(movies: MovieResponse)
+    fun getMovies(): MovieResponse
+}
