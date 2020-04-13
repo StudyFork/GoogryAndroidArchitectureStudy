fcb326a291bbb355d745c8c47a2dc35bbbf400af
jackjackE <tmddjs210@naver.com>
2020-04-13 15:58:31 +0900

    HW-7  MovieSearch - Koin 적용

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/search/MovieSearchActivity.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/search/MovieSearchActivity.kt
@@ -3,40 +3,28 @@ package com.mtjin.androidarchitecturestudy.ui.search
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.mtjin.androidarchitecturestudy.R
 import com.mtjin.androidarchitecturestudy.base.BaseActivity
 import com.mtjin.androidarchitecturestudy.databinding.ActivityMovieSearchBinding
-import com.mtjin.androidarchitecturestudy.utils.MyApplication
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MovieSearchActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMovieSearchBinding
     private lateinit var movieAdapter: MovieAdapter
-    private lateinit var myApplication: MyApplication
-    private val viewModel: MovieSearchViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return MovieSearchViewModel(myApplication.movieRepository) as T
-            }
-        }
-    }
+    private val viewModel: MovieSearchViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_search)
-        inject()
+        initBinding()
         initViewModelCallback()
         initAdapter()
     }
 
-    private fun inject() {
-        myApplication = application as MyApplication
+    private fun initBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_search)
         binding.vm = viewModel
         binding.lifecycleOwner = this
