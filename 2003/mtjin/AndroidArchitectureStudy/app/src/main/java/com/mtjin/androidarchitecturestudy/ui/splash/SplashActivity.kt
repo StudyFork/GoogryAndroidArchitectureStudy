f524fd156ddd88cac7444ac3d757833ac8ec1d44
jackjackE <tmddjs210@naver.com>
2020-04-13 15:59:11 +0900

    HW-7  Splash - Koin 적용

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/splash/SplashActivity.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/ui/splash/SplashActivity.kt
@@ -2,43 +2,20 @@ package com.mtjin.androidarchitecturestudy.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.mtjin.androidarchitecturestudy.R
 import com.mtjin.androidarchitecturestudy.base.BaseActivity
-import com.mtjin.androidarchitecturestudy.data.login.source.LoginRepository
-import com.mtjin.androidarchitecturestudy.data.login.source.LoginRepositoryImpl
-import com.mtjin.androidarchitecturestudy.data.login.source.local.LoginLocalDataSource
-import com.mtjin.androidarchitecturestudy.data.login.source.local.LoginLocalDataSourceImpl
 import com.mtjin.androidarchitecturestudy.ui.login.LoginActivity
 import com.mtjin.androidarchitecturestudy.ui.search.MovieSearchActivity
-import com.mtjin.androidarchitecturestudy.utils.PreferenceManager
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : BaseActivity() {
-    private lateinit var preferenceManager: PreferenceManager
-    private lateinit var loginLocalDataSource: LoginLocalDataSource
-    lateinit var loginRepository: LoginRepository
-    private val viewModel: SplashViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return SplashViewModel(loginRepository) as T
-            }
-        }
-    }
+    private val viewModel: SplashViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        inject()
         initViewModelCallback()
         viewModel.doSplash()
-    }
-
-    private fun inject() {
-        preferenceManager = PreferenceManager(this)
-        loginLocalDataSource = LoginLocalDataSourceImpl(preferenceManager)
-        loginRepository = LoginRepositoryImpl(loginLocalDataSource)
     }
 
     private fun initViewModelCallback() {
