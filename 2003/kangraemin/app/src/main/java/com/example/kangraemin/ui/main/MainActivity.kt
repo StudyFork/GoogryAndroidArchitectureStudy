9d3f2315c7819f9d31d9020028f7bc97e95e8f93
kangraemin <ee4787@naver.com>
2020-04-15 00:25:36 +0900

    HW-7 Apply DI in ui

--- 2003/kangraemin/app/src/main/java/com/example/kangraemin/ui/main/MainActivity.kt
+++ 2003/kangraemin/app/src/main/java/com/example/kangraemin/ui/main/MainActivity.kt
@@ -2,24 +2,14 @@ package com.example.kangraemin.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.example.kangraemin.R
 import com.example.kangraemin.adapter.SearchResultAdapter
 import com.example.kangraemin.base.KangBaseActivity
 import com.example.kangraemin.databinding.ActivityMainBinding
-import com.example.kangraemin.model.AppDatabase
-import com.example.kangraemin.model.AuthRepository
-import com.example.kangraemin.model.MovieSearchRepository
-import com.example.kangraemin.model.local.datadao.AuthLocalDataSourceImpl
-import com.example.kangraemin.model.local.datadao.LocalMovieDataSourceImpl
-import com.example.kangraemin.model.remote.datadao.MovieRemoteDataSourceImpl
 import com.example.kangraemin.ui.login.LoginActivity
-import com.example.kangraemin.util.NetworkUtil
-import com.example.kangraemin.util.RetrofitClient
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : KangBaseActivity() {
 
@@ -27,28 +17,7 @@ class MainActivity : KangBaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val mainViewModel: MainViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return MainViewModel(
-                    movieSearchRepository = MovieSearchRepository(
-                        localMovieDataSource = LocalMovieDataSourceImpl(
-                            movieDao = AppDatabase.getInstance(
-                                context = this@MainActivity
-                            ).movieDao()
-                        ),
-                        remoteMovieDataSource = MovieRemoteDataSourceImpl(RetrofitClient.getMovieApi())
-                    ),
-                    authRepository = AuthRepository(
-                        authLocalDataSource = AuthLocalDataSourceImpl(
-                            AppDatabase.getInstance(context = this@MainActivity).authDao()
-                        )
-                    ),
-                    networkUtil = NetworkUtil(context = this@MainActivity)
-                ) as T
-            }
-        }
-    }
+    private val mainViewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
