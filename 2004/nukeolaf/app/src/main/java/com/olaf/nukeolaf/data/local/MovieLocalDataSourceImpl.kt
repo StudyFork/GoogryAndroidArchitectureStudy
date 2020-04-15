f99190fe32c1ccf268412d2586096594fe8556f9
nukeolaf <yhjin04170@gmail.com>
2020-04-15 15:12:42 +0900

    HW-2 SharedPreference 이름을 values/strings.xml 에 저장하도록 수정

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/data/local/MovieLocalDataSourceImpl.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/data/local/MovieLocalDataSourceImpl.kt
@@ -2,21 +2,23 @@ package com.olaf.nukeolaf.data.local
 
 import android.content.Context
 import com.google.gson.GsonBuilder
+import com.olaf.nukeolaf.R
 import com.olaf.nukeolaf.data.model.MovieResponse
 
 class MovieLocalDataSourceImpl(context: Context) : MovieLocalDataSource {
 
+    private val name = context.resources.getString(R.string.MOVIE_CACHE_DATA)
     private val sharedPreferences =
-        context.getSharedPreferences("movies", Context.MODE_PRIVATE)
+        context.getSharedPreferences(name, Context.MODE_PRIVATE)
     private val editor = sharedPreferences.edit()
 
     override fun saveMovies(movies: MovieResponse) {
-        editor.putString("movies", GsonBuilder().create().toJson(movies))
+        editor.putString(name, GsonBuilder().create().toJson(movies))
         editor.commit()
     }
 
     override fun getMovies(): MovieResponse? {
-        val movies = sharedPreferences.getString("movies", null)
+        val movies = sharedPreferences.getString(name, null)
         return if (movies != null) {
             GsonBuilder().create().fromJson(movies, MovieResponse::class.java)
         } else {
