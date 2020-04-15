9bb92accb909e0655dc52064e7a48837f6dabe5c
byiryu <nome106@naver.com>
2020-04-15 17:33:10 +0900

    HW-7 koin Module 적용

--- 2003/byiryu/app/src/main/java/com/byiryu/study/ui/BRApplication.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/BRApplication.kt
@@ -1,35 +1,29 @@
 package com.byiryu.study.ui
 
 import android.app.Application
+import com.byiryu.study.di.localModule
+import com.byiryu.study.di.remoteModule
+import com.byiryu.study.di.repositoryModule
+import com.byiryu.study.di.viewModelModule
 import com.byiryu.study.model.Repository
-import com.byiryu.study.model.local.LocalDataBase
-import com.byiryu.study.model.local.LocalDataSource
-import com.byiryu.study.model.local.pref.AppPreference
-import com.byiryu.study.model.remote.RemoteDataSource
-import com.byiryu.study.ui.mvvm.base.BRViewModelFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class BRApplication : Application() {
 
-    lateinit var viewModelFactory: BRViewModelFactory
     lateinit var repository: Repository
 
     override fun onCreate() {
         super.onCreate()
-        init()
+
+        startKoin {
+            androidContext(this@BRApplication)
+            modules(
+                localModule,
+                remoteModule,
+                repositoryModule,
+                viewModelModule
+            )
+        }
     }
-
-    private fun init() {
-
-        val pref = AppPreference(this)
-        val db = LocalDataBase.getInstance(this)
-
-        val remoteDataSource = RemoteDataSource()
-        val localDataSource = LocalDataSource(db.movieDao(), pref)
-
-
-        repository = Repository(remoteDataSource, localDataSource)
-        viewModelFactory = BRViewModelFactory(repository)
-
-    }
-
 }
