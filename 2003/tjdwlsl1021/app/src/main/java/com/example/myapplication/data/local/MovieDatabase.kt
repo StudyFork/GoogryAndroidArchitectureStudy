3ac464e712eb7847abcf1aec1998f81d0bd51cb3
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-04-16 05:49:59 +0900

    주석소스 제거

--- 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/data/local/MovieDatabase.kt
+++ 2003/tjdwlsl1021/app/src/main/java/com/example/myapplication/data/local/MovieDatabase.kt
@@ -6,27 +6,6 @@ import androidx.room.RoomDatabase
 @Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
 abstract class MovieDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
-
-//    companion object {
-//
-//        private var instance: MovieDatabase? = null
-//        private const val DATABASE_NAME = "movie-database"
-//
-//        @Synchronized
-//        fun getDatabase(context: Context): MovieDatabase {
-//            synchronized(this) {
-//                if (instance == null) {
-//                    instance = Room.databaseBuilder(
-//                        context.applicationContext,
-//                        MovieDatabase::class.java, DATABASE_NAME
-//                    )
-//                        .allowMainThreadQueries()
-//                        .build()
-//                }
-//                return instance!!
-//            }
-//        }
-//    }
 }
 
 
