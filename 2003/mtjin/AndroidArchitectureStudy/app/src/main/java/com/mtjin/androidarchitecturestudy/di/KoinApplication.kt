e05c1da871b65a42b715ac068eb92c2b445543bf
jackjackE <tmddjs210@naver.com>
2020-04-15 12:32:42 +0900

    HW-7 KoinApplication - Logger 추가

--- 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/di/KoinApplication.kt
+++ 2003/mtjin/AndroidArchitectureStudy/app/src/main/java/com/mtjin/androidarchitecturestudy/di/KoinApplication.kt
@@ -6,12 +6,14 @@ import com.mtjin.androidarchitecturestudy.module.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class KoinApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             if (BuildConfig.DEBUG) androidLogger()
+            else androidLogger(Level.NONE)
             androidContext(this@KoinApplication)
             modules(
                 repositoryModule,
