12abcc2d8985ce7c7abe81558c6ff2803935fd46
byiryu <nome106@naver.com>
2020-04-15 17:33:53 +0900

    HW-7 Activity viewModel -> koin 적용

--- 2003/byiryu/app/src/main/java/com/byiryu/study/ui/mvvm/login/views/LoginActivity.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/ui/mvvm/login/views/LoginActivity.kt
@@ -1,23 +1,20 @@
 package com.byiryu.study.ui.mvvm.login.views
 
 import android.os.Bundle
-import androidx.databinding.Observable
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.byiryu.study.databinding.ActivityLoginBinding
-
 import com.byiryu.study.ui.mvvm.base.views.BaseActivity
 import com.byiryu.study.ui.mvvm.login.viewmodel.LoginViewModel
 import com.byiryu.study.ui.mvvm.main.views.MainActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : BaseActivity(){
 
-
+    private val viewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val viewModel = ViewModelProvider(this, getBRApplication().viewModelFactory)[LoginViewModel::class.java]
         val binding = ActivityLoginBinding.inflate(layoutInflater).apply {
             lifecycleOwner = this@LoginActivity
             vm = viewModel
