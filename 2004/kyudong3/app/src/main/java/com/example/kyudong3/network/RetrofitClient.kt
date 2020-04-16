829e21da4503c2a74182e77612d35d406b33e055
Kyudong3 <kyudong3@gmail.com>
2020-04-17 01:55:15 +0900

    HW-2 apply retrofit interceptors

--- 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/RetrofitClient.kt
+++ 2004/kyudong3/app/src/main/java/com/example/kyudong3/network/RetrofitClient.kt
@@ -1,28 +1,45 @@
 package com.example.kyudong3.network
 
-import com.ihsanbal.logging.Level
-import com.ihsanbal.logging.LoggingInterceptor
+import com.example.kyudong3.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.internal.platform.Platform
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://openapi.naver.com/"
 
-    val okHttpClient: OkHttpClient by lazy {
+    private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(
-                LoggingInterceptor.Builder().apply {
-                    setLevel(Level.BASIC)
-                    log(Platform.INFO)
-                    request("Request")
-                    response("Response")
-                }.build()
-            ).build()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(naverClientHeaderInterceptor)
+            .build()
     }
 
-    val retrofit: Retrofit by lazy {
+    private val httpLoggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
+        .apply {
+            level =
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+        }
+
+    private val naverClientHeaderInterceptor = Interceptor {
+        val chainRequest = it.request()
+        val request = chainRequest
+            .newBuilder()
+            .apply {
+                addHeader("X-Naver-Client-Id", BuildConfig.CLIENT_ID)
+                addHeader("X-Naver-Client-Secret", BuildConfig.CLIENT_SECRET)
+            }.build()
+
+        return@Interceptor it.proceed(request)
+    }
+
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
