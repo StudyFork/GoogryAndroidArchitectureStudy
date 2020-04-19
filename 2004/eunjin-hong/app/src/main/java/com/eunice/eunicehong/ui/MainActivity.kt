7ffde6a4b6ea9b13e064f05fecd26e2773cb41d1
eunjin-hong <ejhong.4991@gmail.com>
2020-04-19 12:18:16 +0900

    HW-2 :lipstick: add clear history button

--- 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MainActivity.kt
+++ 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.eunice.eunicehong.ui
 
+import android.app.AlertDialog
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
@@ -7,13 +8,16 @@ import android.os.Bundle
 import android.provider.SearchRecentSuggestions
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.eunice.eunicehong.R
 import com.eunice.eunicehong.data.repository.MovieRepository
 import com.eunice.eunicehong.provider.SuggestionProvider
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,6 +61,42 @@ class MainActivity : AppCompatActivity() {
             isQueryRefinementEnabled = true
         }
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.remove_history -> {
+            showRemoveHistoryConfirmDialog()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showRemoveHistoryConfirmDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.app_name)
+            .setMessage(getString(R.string.delete_history_confirmation))
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setPositiveButton(
+                android.R.string.yes
+            ) { _, _ ->
+                clearHistory()
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.complete_delete_history),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setNegativeButton(android.R.string.no, null).show()
+    }
+
+    private fun clearHistory() {
+        SearchRecentSuggestions(
+            this,
+            SuggestionProvider.AUTHORITY,
+            SuggestionProvider.MODE
+        ).clearHistory()
     }
 
     private fun searchMovieList(query: String) {
