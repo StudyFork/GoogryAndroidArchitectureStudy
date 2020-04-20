880bc60e5f6786335d7ea9dd5f50b30a081fd100
nukeolaf <yhjin04170@gmail.com>
2020-04-21 00:23:03 +0900

    HW-3 MainActivity 에 Contract.View 구현

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MainActivity.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MainActivity.kt
@@ -7,18 +7,14 @@ import androidx.appcompat.widget.SearchView
 import com.olaf.nukeolaf.R
 import com.olaf.nukeolaf.data.local.MovieLocalDataSourceImpl
 import com.olaf.nukeolaf.data.model.MovieItem
-import com.olaf.nukeolaf.data.model.MovieResponse
 import com.olaf.nukeolaf.data.remote.MovieRemoteDataSourceImpl
-import com.olaf.nukeolaf.data.repository.MovieRepository
 import com.olaf.nukeolaf.data.repository.MovieRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
 
     private val movieAdapter = MovieAdapter()
-    private lateinit var movieLocalDataSource: MovieLocalDataSourceImpl
-    private lateinit var movieRemoteDataSource: MovieRemoteDataSourceImpl
-    private lateinit var movieRepository: MovieRepositoryImpl
+    private lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,19 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         movie_rv.adapter = movieAdapter
 
-        movieLocalDataSource = MovieLocalDataSourceImpl(applicationContext)
-        movieRemoteDataSource = MovieRemoteDataSourceImpl()
-        movieRepository = MovieRepositoryImpl(movieLocalDataSource, movieRemoteDataSource)
-
-        loadMovies()
+        presenter = MainPresenter(
+            MovieRepositoryImpl(
+                MovieLocalDataSourceImpl(applicationContext),
+                MovieRemoteDataSourceImpl()
+            ), this
+        ).apply { loadMovies() }
 
         search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null) {
-                    searchMovie(query)
-                } else {
-                    makeToast("검색어를 입력해주세요")
-                }
+                presenter.searchMovie(query)
                 return false
             }
 
@@ -48,37 +41,24 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun searchMovie(query: String) {
-        movieRepository.searchMovies(
-            query,
-            object : MovieRepository.LoadMoviesCallback {
-                override fun onMoviesLoaded(movieResponse: MovieResponse) {
-                    if (movieResponse.items.isNotEmpty()) {
-                        showMovies(movieResponse.items)
-                    } else {
-                        makeToast("${query}에 대한 검색결과가 없습니다")
-                    }
-                }
-
-                override fun onResponseError(message: String) {
-                    makeToast("서버 에러 : 서버에 문제가 있습니다")
-                }
-
-                override fun onFailure(t: Throwable) {
-                    makeToast("네트워크 에러 : 인터넷 연결을 확인해 주세요")
-                }
-            })
-    }
-
-    private fun loadMovies() {
-        val movieList = movieRepository.getMovies()
-        if (movieList != null && movieList.items.isNotEmpty()) {
-            showMovies(movieList.items)
-        }
-    }
-
-    private fun showMovies(movies: List<MovieItem>) {
+    override fun showMovies(movies: List<MovieItem>) {
         movieAdapter.setMovies(movies)
+    }
+
+    override fun showEmptySearchWord() {
+        makeToast("검색어를 입력해주세요")
+    }
+
+    override fun showNoResultForSearchWord(query: String) {
+        makeToast("${query}에 대한 검색결과가 없습니다")
+    }
+
+    override fun showServerError() {
+        makeToast("서버 에러 : 서버에 문제가 있습니다")
+    }
+
+    override fun showNetworkError() {
+        makeToast("네트워크 에러 : 인터넷 연결을 확인해 주세요")
     }
 
     private fun makeToast(message: String) {
