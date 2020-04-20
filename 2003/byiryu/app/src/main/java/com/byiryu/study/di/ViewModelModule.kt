0afd6ca7b5934ddf5a3e6f7133dc181675055c32
byiryu <nome106@naver.com>
2020-04-20 22:26:15 +0900

    HW-8 view 모듈 : di 설정

--- 2003/byiryu/app/src/main/java/com/byiryu/study/di/ViewModelModule.kt
+++ 2003/byiryu/app/src/main/java/com/byiryu/study/di/ViewModelModule.kt
@@ -2,7 +2,6 @@ package com.byiryu.study.di
 
 import com.byiryu.study.ui.mvvm.login.viewmodel.LoginViewModel
 import com.byiryu.study.ui.mvvm.main.viewmodel.MainViewModel
-import org.koin.androidx.experimental.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
