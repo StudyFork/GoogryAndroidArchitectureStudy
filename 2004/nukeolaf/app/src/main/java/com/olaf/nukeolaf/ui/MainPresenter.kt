e4cded226163323edf4b4f8799cd587f47b993f5
nukeolaf <yhjin04170@gmail.com>
2020-04-21 00:14:48 +0900

    HW-3 MainPresenter 작성

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MainPresenter.kt
@@ -0,0 +1,45 @@
+package com.olaf.nukeolaf.ui
+
+import com.olaf.nukeolaf.data.model.MovieResponse
+import com.olaf.nukeolaf.data.repository.MovieRepository
+import com.olaf.nukeolaf.data.repository.MovieRepositoryImpl
+
+class MainPresenter(
+    private val movieRepository: MovieRepositoryImpl,
+    private val view: MainContract.View
+) : MainContract.Presenter {
+
+
+    override fun loadMovies() {
+        val movieList = movieRepository.getMovies()
+        if (movieList != null && movieList.items.isNotEmpty()) {
+            view.showMovies(movieList.items)
+        }
+    }
+
+    override fun searchMovie(query: String?) {
+        if (query == null) {
+            view.showEmptySearchWord()
+            return
+        }
+        movieRepository.searchMovies(
+            query,
+            object : MovieRepository.LoadMoviesCallback {
+                override fun onMoviesLoaded(movieResponse: MovieResponse) {
+                    if (movieResponse.items.isNotEmpty()) {
+                        view.showMovies(movieResponse.items)
+                    } else {
+                        view.showNoResultForSearchWord(query)
+                    }
+                }
+
+                override fun onResponseError(message: String) {
+                    view.showServerError()
+                }
+
+                override fun onFailure(t: Throwable) {
+                    view.showServerError()
+                }
+            })
+    }
+}
