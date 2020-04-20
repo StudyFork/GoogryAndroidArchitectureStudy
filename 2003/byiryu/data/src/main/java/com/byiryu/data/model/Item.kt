b9ef6e3fdb9645c4deb96311f36335d46c4ebfa5
byiryu <nome106@naver.com>
2020-04-20 22:19:20 +0900

    HW-8 data 모듈 : di 세팅 및 model 생성 ( Item, Response 공통으로 설계 )

---
+++ 2003/byiryu/data/src/main/java/com/byiryu/data/model/Item.kt
@@ -0,0 +1,14 @@
+package com.byiryu.data.model
+
+data class Item(
+    val actor: String,
+    val director: String,
+    val image: String,
+    val link: String,
+    val pubDate: String,
+    val subtitle: String,
+    val title: String,
+    val userRating: Float
+
+
+)
