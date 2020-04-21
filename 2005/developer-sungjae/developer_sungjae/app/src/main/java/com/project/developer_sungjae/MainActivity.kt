a39bfca13932c5af9a231afcb2cc77f38422ac47
developer-sungjae <dunkey2615@naver.com>
2020-04-21 20:55:27 +0900

    1. developer-sungjae 이름폴더 하위 androidX 프로젝트 생성   - 프로젝트 이름 developer_sungjae
    2. gitignore.io에서 gitignore 파일 생성 후 복사하여 붙여넣음

---
+++ 2005/developer-sungjae/developer_sungjae/app/src/main/java/com/project/developer_sungjae/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.developer_sungjae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
