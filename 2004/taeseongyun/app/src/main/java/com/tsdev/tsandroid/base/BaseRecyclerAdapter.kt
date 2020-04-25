3d1c9be3997c52957e4c35bdf243e2a276904bb9
TaeseongYun <yts0646@gmail.com>
2020-04-25 21:51:01 +0900

    HW-4 fix: modify RecyclerView dataBinding

--- 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/base/BaseRecyclerAdapter.kt
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/base/BaseRecyclerAdapter.kt
@@ -1,15 +1,17 @@
 package com.tsdev.tsandroid.base
 
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseRecyclerAdapter<ITEM> : RecyclerView.Adapter<BaseRecyclerViewHolder<ITEM>>(),
+abstract class BaseRecyclerAdapter<BINDING : ViewDataBinding, ITEM> :
+    RecyclerView.Adapter<BaseRecyclerViewHolder<BINDING, ITEM>>(),
     RecyclerViewModel<ITEM> {
 
     internal val itemList = mutableListOf<ITEM>()
 
     override fun getItemCount(): Int = itemList.size
 
-    override fun onBindViewHolder(holder: BaseRecyclerViewHolder<ITEM>, position: Int) {
+    override fun onBindViewHolder(holder: BaseRecyclerViewHolder<BINDING, ITEM>, position: Int) {
         holder.onBindViewHolder(itemList[position])
     }
 
--- 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/ui/viewholder/MovieRecyclerViewViewHolder.kt
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/ui/viewholder/MovieRecyclerViewViewHolder.kt
@@ -6,16 +6,15 @@ import android.widget.Toast
 import com.tsdev.tsandroid.data.Item
 import com.tsdev.tsandroid.R
 import com.tsdev.tsandroid.base.BaseRecyclerViewHolder
+import com.tsdev.tsandroid.databinding.MovieRecyclerItemBinding
 import com.tsdev.tsandroid.ext.showToast
-import com.tsdev.tsandroid.util.htmlConvert
-import kotlinx.android.synthetic.main.movie_recycler_item.view.*
 
 class MovieRecyclerViewViewHolder(onClickListenerEvent: OnClickDelegate, parent: ViewGroup) :
-    BaseRecyclerViewHolder<Item>(
+    BaseRecyclerViewHolder<MovieRecyclerItemBinding, Item>(
         LayoutInflater.from(parent.context).inflate(R.layout.movie_recycler_item, parent, false)
     ) {
     init {
-        itemView.setOnClickListener {
+        recyclerDataBinding.root.setOnClickListener {
             adapterPosition.takeIf { adapterPosition > -1 }?.let {
                 onClickListenerEvent.onClickEventListener(it)
             } ?: parent.context.showToast(Toast.LENGTH_LONG)
@@ -27,13 +26,6 @@ class MovieRecyclerViewViewHolder(onClickListenerEvent: OnClickDelegate, parent:
     }
 
     override fun onBindViewHolder(item: Item) {
-        itemView.movie_actor.text = item.actor
-
-        itemView.movie_img.loadMovieImage(item.image)
-
-        itemView.movie_director.text = item.director
-        itemView.movie_sub_title.text = item.subtitle
-        itemView.movie_rating.text = item.userRating
-        itemView.movie_name.text = item.title
+        recyclerDataBinding.movieItem = item
     }
 }
