6b6fba9341bdecab999f5bdf4ef186b1efd05231
jackjackE <tmddjs210@naver.com>
2020-04-25 13:40:50 +0900

    HW-8 data - MovieResponse 제거

--- 2003/mtjin/AndroidArchitectureStudy/data/src/main/java/com/mtjin/data/model/search/MovieResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.mtjin.data.model.search
-
-data class MovieResponse(
-    val display: Int,
-    val movies: List<Movie>,
-    val lastBuildDate: String,
-    val start: Int,
-    val total: Int
-)
