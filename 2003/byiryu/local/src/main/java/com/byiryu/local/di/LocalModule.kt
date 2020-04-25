a41890084ff3e378af42d5e0b10fb899e4a5fea5
byiryu <nome106@naver.com>
2020-04-25 17:26:48 +0900

    HW-8 LocalDataBase koin 설정 변경

--- 2003/byiryu/local/src/main/java/com/byiryu/local/di/LocalModule.kt
+++ 2003/byiryu/local/src/main/java/com/byiryu/local/di/LocalModule.kt
@@ -1,5 +1,6 @@
 package com.byiryu.local.di
 
+import androidx.room.Room
 import com.byiryu.data.source.local.LocalDataSource
 import com.byiryu.local.model.data.AppPreference
 import com.byiryu.local.model.data.LocalDataBase
@@ -11,8 +12,10 @@ val localModule = module {
         AppPreference(get())
     }
 
+    single { Room.databaseBuilder(get(), LocalDataBase::class.java, "movie.db").build() }
+
     single {
-        LocalDataBase.getInstance(get())
+        get<LocalDataBase>().movieDao()
     }
 
     single<LocalDataSource> {
--- 2003/byiryu/local/src/main/java/com/byiryu/local/model/data/LocalDataBase.kt
+++ 2003/byiryu/local/src/main/java/com/byiryu/local/model/data/LocalDataBase.kt
@@ -1,35 +1,10 @@
 package com.byiryu.local.model.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.byiryu.local.model.MovieItem
 
 @Database(entities = [MovieItem::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
-
-    companion object {
-        private var ins: LocalDataBase? = null
-
-        fun getInstance(context: Context): LocalDataBase {
-            synchronized(LocalDataBase::class) {
-
-                if (ins == null) {
-                    ins = Room.databaseBuilder(
-                        (context.applicationContext),
-                        LocalDataBase::class.java, "movie.db"
-                    )
-                        .fallbackToDestructiveMigration()
-                        .build()
-                }
-            }
-            return ins!!
-        }
-
-        fun destoryIns() {
-            ins = null
-        }
-    }
 }
