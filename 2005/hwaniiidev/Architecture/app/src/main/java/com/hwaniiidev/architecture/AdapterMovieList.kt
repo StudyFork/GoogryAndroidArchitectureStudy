83d4ec27026ea461c5230d5b2ba1bf346d72164b
hwaniiidev <hwaniiidev@gmail.com>
2020-04-26 23:17:09 +0900

    HW-1 NAVER_API : Create AdapterMovieList class

---
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/AdapterMovieList.kt
@@ -0,0 +1,50 @@
+package com.hwaniiidev.architecture
+
+import android.content.Context
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hwaniiidev.architecture.Model.Item
+
+class AdapterMovieList(val context: Context, val movieList: List<Item>?) :
+    RecyclerView.Adapter<AdapterMovieList.MovieHolder>() {
+    inner class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgMovie = itemView.findViewById<ImageView>(R.id.item_movie_list_image)
+        val txtTile = itemView.findViewById<TextView>(R.id.item_movie_list_title)
+        val txtSubTile = itemView.findViewById<TextView>(R.id.item_movie_list_subtitle)
+        val txtPubDate = itemView.findViewById<TextView>(R.id.item_movie_list_pub_date)
+        val txtDirector = itemView.findViewById<TextView>(R.id.item_movie_list_director)
+        val txtActors = itemView.findViewById<TextView>(R.id.item_movie_list_actors)
+        val txtRating = itemView.findViewById<TextView>(R.id.item_movie_list_rating)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie_list, parent, false)
+        return MovieHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return movieList!!.size
+    }
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        Glide.with(context)
+            .load(movieList!!.get(position).image)
+            .into(holder.imgMovie)
+        holder.txtTile.text = Html.fromHtml(movieList!!.get(position).title)
+        holder.txtSubTile.text = movieList!!.get(position).subtitle
+        holder.txtPubDate.text = movieList!!.get(position).pubDate
+        holder.txtDirector.text = movieList!!.get(position).director
+        holder.txtActors.text = movieList!!.get(position).actor
+        holder.txtRating.text = movieList!!.get(position).userRating
+
+
+    }
+
+
+}
