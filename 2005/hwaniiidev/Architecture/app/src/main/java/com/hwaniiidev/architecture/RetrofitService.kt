e0549c1e4993eb5e88f421b567eef8b82f2f8446
hwaniiidev <hwaniiidev@gmail.com>
2020-04-26 23:21:31 +0900

    HW-1 NAVER_API : Add Header, Query : display

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/RetrofitService.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/RetrofitService.kt
@@ -6,12 +6,11 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface RetrofitService {
-
-
     @GET("v1/search/movie.json")
     fun getMovieSearchData(
-        @Header("X-Naver-Client-Id") clientId : String,
-        @Header("X-Naver-Client-Secret") clientPw : String,
-        @Query("query") query: String
+        @Header("X-Naver-Client-Id") clientId: String,
+        @Header("X-Naver-Client-Secret") clientPw: String,
+        @Query("query") query: String,
+        @Query("display") display: Int
     ): retrofit2.Call<ResponseMovieSearchData>
 }
