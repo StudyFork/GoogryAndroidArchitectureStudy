fbebdbf2d630800c39dc2942a24c8ec2e295b75b
hwaniiidev <hwaniiidev@gmail.com>
2020-04-26 22:05:39 +0900

    HW-1 NAVER_API : Create APICall class

---
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/APICall.kt
@@ -0,0 +1,18 @@
+package com.hwaniiidev.architecture
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class APICall {
+    companion object {
+        val serverUrl: String = "https://openapi.naver.com/"
+
+        fun setUp(): RetrofitService {
+            return Retrofit.Builder()
+                .baseUrl(serverUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RetrofitService::class.java)
+        }
+    }
+}
--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/RetrofitService.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/RetrofitService.kt
@@ -2,12 +2,16 @@ package com.hwaniiidev.architecture
 
 import com.hwaniiidev.architecture.Model.ResponseMovieSearchData
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface RetrofitService {
 
+
     @GET("v1/search/movie.json")
     fun getMovieSearchData(
+        @Header("X-Naver-Client-Id") clientId : String,
+        @Header("X-Naver-Client-Secret") clientPw : String,
         @Query("query") query: String
     ): retrofit2.Call<ResponseMovieSearchData>
 }
