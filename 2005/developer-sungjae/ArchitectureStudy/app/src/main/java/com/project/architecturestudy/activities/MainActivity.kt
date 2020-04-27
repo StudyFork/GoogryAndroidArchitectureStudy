2ae6ee370e105ec7a182e1f531c9f5414b3256fe
developer-sungjae <dunkey2615@naver.com>
2020-04-27 23:14:19 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : add btn_search clicklistener : add keyword search method

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
@@ -3,12 +3,24 @@ package com.project.architecturestudy.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.project.architecturestudy.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_search.setOnClickListener {
+            if (et_search.text.toString().isEmpty()) return@setOnClickListener
+
+
+            fetchJson(et_search.toString())
+        }
+
+    }
+    private fun fetchJson(keyWord: String) {
 
     }
 }
