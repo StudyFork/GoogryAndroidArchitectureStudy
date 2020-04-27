5caf4c8bc153aa5e9a4f473650a857f3d51ca594
kim chul <chuuuul@naver.com>
2020-04-27 23:54:15 +0900

    pubData를 pubDate로 이름 변경 Int형 변수에서 String형 변수로 변경

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/Activity/MovieVO.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/Activity/MovieVO.kt
@@ -1,11 +1,9 @@
 package com.example.architecture.activity.Activity
 
-import java.util.*
-
 data class MovieVO(
         val image : String?,
         val title : String,
-        val pubData : Int,
+        val pubDate: String,
         val link : String,
         val userRating : Int
 )
