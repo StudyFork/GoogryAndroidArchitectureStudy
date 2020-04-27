12a3184b08d2a2fecead5c318a173294a55d59bd
sangjinKIM93 <larger93r@gmail.com>
2020-04-27 21:12:10 +0900

    MovieListAdapter 생성

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/adapter/MovieListAdapter.kt
@@ -0,0 +1,22 @@
+package com.sangjin.newproject.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sangjin.newproject.R
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+class MovieListAdapter (private val movieList: Array<Movie>) : RecyclerView.Adapter<MovieListViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {
+        val item = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+
+        return MovieListViewHolder(item)
+    }
+
+    override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) {
+        holder.containerView.movieTitleTV.text = movieList.get(position).title
+    }
+
+    override fun getItemCount() = movieList.size
+
+}
