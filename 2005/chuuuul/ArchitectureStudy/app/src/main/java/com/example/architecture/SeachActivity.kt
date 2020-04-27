5f7de86d1604a75cf54c65b0dde65d7b72c3f22d
kim chul <chuuuul@naver.com>
2020-04-27 21:36:39 +0900

    SearchActivity 생성

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/SeachActivity.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/SeachActivity.kt
@@ -3,10 +3,10 @@ package com.example.architecture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SeachActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
     }
 }
