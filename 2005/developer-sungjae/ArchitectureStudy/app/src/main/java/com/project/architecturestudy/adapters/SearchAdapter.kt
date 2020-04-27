ae02c465eedfee594f44c2d2d310e818243ff66b
developer-sungjae <dunkey2615@naver.com>
2020-04-28 00:05:25 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 :write SearchAdapter

---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/adapters/SearchAdapter.kt
@@ -0,0 +1,28 @@
+package com.project.architecturestudy.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.project.architecturestudy.R
+import com.project.architecturestudy.models.MovieData
+
+class SearchAdapter(var searchData: ArrayList<MovieData>) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return SearchingResultHolder(LayoutInflater.from(parent.context).inflate(R.layout.movie_list, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+    inner class SearchingResultHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var view = itemView
+    }
+}
