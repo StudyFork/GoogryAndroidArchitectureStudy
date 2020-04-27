3417c3c129dd8362afebb18a426054e029a89a9d
developer-sungjae <dunkey2615@naver.com>
2020-04-27 22:52:18 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : add NAVER API KEYs in Constants class

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/components/Constants.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/components/Constants.kt
@@ -3,5 +3,6 @@ package com.project.architecturestudy.components
 object Constants {
 
     const val NAVER_SEARCH_BASE_URL = "https://openapi.naver.com/v1/search/"
-
+    const val NAVER_CLIENT_ID = "cb6vgtmAXB0EgAX3Bbjv"
+    const val NAVER_CLIENT_SECRET = "sP5Byv5r87"
 }
