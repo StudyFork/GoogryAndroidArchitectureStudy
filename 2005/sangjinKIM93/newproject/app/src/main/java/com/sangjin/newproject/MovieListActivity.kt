211944a9a3c7b0fea6a57e71993246d05c03045c
sangjinKIM93 <larger93r@gmail.com>
2020-04-27 21:37:41 +0900

    MovieListActivity에서 recyclerView 초기화

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
@@ -1,12 +1,28 @@
 package com.sangjin.newproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sangjin.newproject.adapter.Movie
+import com.sangjin.newproject.adapter.MovieListAdapter
+import kotlinx.android.synthetic.main.activity_movie_list.*
 
 class MovieListActivity : AppCompatActivity() {
+
+    private var movieList = arrayOf<Movie>()
+    private val movieListAdapter: MovieListAdapter by lazy {
+        MovieListAdapter(movieList)
+    }
+    private val linearLayoutManager = LinearLayoutManager(this)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
+
+        //리사이클러뷰 초기화
+        movieListView.layoutManager = linearLayoutManager
+        movieListView.adapter = movieListAdapter
+
     }
 }
