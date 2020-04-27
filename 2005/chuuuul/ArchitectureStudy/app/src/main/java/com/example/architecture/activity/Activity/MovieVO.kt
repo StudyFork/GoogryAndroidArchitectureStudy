086f0b6b9308b68159b32cd452853c81bfd2cdf8
kim chul <chuuuul@naver.com>
2020-04-27 23:24:40 +0900

    영화 아이템 VO 생성

---
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/Activity/MovieVO.kt
@@ -0,0 +1,11 @@
+package com.example.architecture.activity.Activity
+
+import java.util.*
+
+data class MovieVO(
+        val image : String?,
+        val title : String,
+        val pubData : Int,
+        val link : String,
+        val userRating : Int
+)
