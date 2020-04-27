4b35677dcc8d35995fc3700d98eea3f95ea60641
sangjinKIM93 <larger93r@gmail.com>
2020-04-27 21:03:33 +0900

    Movie data class 생성

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/adapter/Movie.kt
@@ -0,0 +1,12 @@
+package com.sangjin.newproject.adapter
+
+data class Movie(
+    var title: String = "",
+    var link: String = "",
+    var image: String = "",
+    var subtitle: String = "",
+    var pubDate: String = "",
+    var director: String = "",
+    var actor: String = "",
+    var userRating: String = ""
+)
