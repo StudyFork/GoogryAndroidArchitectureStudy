1ef47a9957fc87dfe22bc7233b3707022a2acbde
kim chul <chuuuul@naver.com>
2020-04-28 08:13:45 +0900

    Movie 리사이클러뷰 적용

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/activity/SearchActivity.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/activity/SearchActivity.kt
@@ -11,6 +11,23 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setRecyclerview()
+    }
+
+    private fun setRecyclerview() {
+        val gridLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+
+        // * Test
+        val movies = arrayListOf(
+                MovieVO("https://search.pstatic.net/common?type=o&size=120x171&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181019_236%2F1539926790655oHv5z_JPEG%2Fmovie_image.jpg"
+                        , "해리포터", "2001", "https://", 5.0f),
+                MovieVO("https://movie-phinf.pstatic.net/20170112_293/1484183486221oM8s1_JPEG/movie_image.jpg"
+                        , "반지의 제왕", "2001", "https://", 4.0f)
+        )
+
+        recyclerview_search_movieList.adapter = SearchListAdapter(movies)
+        recyclerview_search_movieList.layoutManager = gridLayoutManager
     }
 
 
--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/activity/SearchListAdapter.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/activity/SearchListAdapter.kt
@@ -1,17 +1,26 @@
 package com.example.architecture.activity.activity
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.architecture.R
 import kotlinx.android.synthetic.main.item_movie.view.*
-import java.util.*
 
-class SearchListAdapter(private val movies: ArrayList<MovieVO>) : RecyclerView.Adapter<SearchViewHolder>() {
+class SearchListAdapter(private val movies: ArrayList<MovieVO>) :
+        RecyclerView.Adapter<SearchViewHolder>() {
+
+    private lateinit var context: Context
+
+    private val NO_IMAGE_URL = "https://ssl.pstatic.net/static/movie/2012/06/dft_img133x190.png"
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        context = parent.context
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
 
         return SearchViewHolder(view)
     }
@@ -25,11 +34,17 @@ class SearchListAdapter(private val movies: ArrayList<MovieVO>) : RecyclerView.A
         holder.itemView.textView_movie_pubDate.text = movies[position].pubDate
 
         holder.itemView.ratingBar_movie_rating.rating = movies[position].userRating
+
+        setMovieImage(holder.itemView.imageView_movie_Image, movies[position].image)
+    }
+
+    private fun setMovieImage(imageView: ImageView, url: String?) {
+        if (url != null) {
+            Glide.with(context).load(url).fitCenter().error(R.drawable.image_loaderror).into(imageView)
+        } else
+            Glide.with(context).load(NO_IMAGE_URL).fitCenter().error(R.drawable.image_loaderror).into(imageView)
     }
 
 }
 
-class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-
-}
+class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view)
