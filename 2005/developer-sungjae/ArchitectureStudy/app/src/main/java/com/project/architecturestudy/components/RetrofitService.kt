0f99218cb17fe058e0d3e3bfba23e60b6b82b0fd
developer-sungjae <dunkey2615@naver.com>
2020-04-27 22:35:20 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : create RetrofitService class for singleton

---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/components/RetrofitService.kt
@@ -0,0 +1,35 @@
+package com.project.architecturestudy.components
+
+import com.project.architecturestudy.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface RetrofitService {
+    companion object ServiceFactory {
+        fun create(): RetrofitService {
+            val builder = OkHttpClient.Builder()
+            if (BuildConfig.DEBUG) {
+                val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                builder.addInterceptor(interceptor)
+            }
+            // 헤더에 UserAgent
+            builder.addInterceptor {
+                val request = it.request().newBuilder().addHeader("User-Agent", "").build()
+                it.proceed(request)
+            }
+
+            val client = builder.build()
+
+            val services = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Constants.serverUrl)
+                .client(client)
+                .build()
+            return services.create(RetrofitService::class.java)
+        }
+    }
+}
