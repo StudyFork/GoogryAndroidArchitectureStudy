3be837ea504e0b6d393c64e983ce5d2bca450a73
developer-sungjae <dunkey2615@naver.com>
2020-04-27 22:30:32 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : create packages and move MainActivity to package named 'activities'

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.project.architecturestudy
+package com.project.architecturestudy.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.architecturestudy.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
