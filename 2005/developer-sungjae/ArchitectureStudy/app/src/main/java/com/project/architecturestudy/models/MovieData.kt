49d1c61427fbf73e96fe83f83042cb768e431836
developer-sungjae <dunkey2615@naver.com>
2020-04-27 23:41:52 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : change MovieData

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/models/MovieData.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/models/MovieData.kt
@@ -1,12 +1,30 @@
 package com.project.architecturestudy.models
 
-data class MovieData(
-    val title: String? = null,
-    val link: String? = null,
-    val image: String? = null,
-    val subtitle: String? = null,
-    val pubDate: String? = null,
-    val director: String? = null,
-    val actor: String? = null,
-    val usrRating: String? = null
-)
+import com.google.gson.annotations.SerializedName
+
+class MovieData(
+    @SerializedName("lastBuildDate") var lastBuildDate: String,
+    @SerializedName("total") var total: Int,
+    @SerializedName("start") var start: Int,
+    @SerializedName("display") var display: Int,
+    @SerializedName("items") var items: Array<Items>
+) {
+    data class Items(
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("link")
+        val link: String,
+        @SerializedName("image")
+        val image: String,
+        @SerializedName("subtitle")
+        val subtitle: String,
+        @SerializedName("pubdate")
+        val pubDate: String,
+        @SerializedName("director")
+        val director: String,
+        @SerializedName("actor")
+        val actor: String,
+        @SerializedName("userRating")
+        val userRating: Double
+    )
+}
