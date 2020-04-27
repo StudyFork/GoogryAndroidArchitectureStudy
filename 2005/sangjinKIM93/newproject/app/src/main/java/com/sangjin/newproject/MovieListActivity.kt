cedfedda13efa40b0731f5d16116905475e266c4
sangjinKIM93 <larger93r@gmail.com>
2020-04-27 17:15:53 +0900

    MainActivity.class , activity_main 이름 수정
    각각 MovieListActivity.class, activity_movie_list로 수정

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
@@ -3,10 +3,10 @@ package com.sangjin.newproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_list)
     }
 }
