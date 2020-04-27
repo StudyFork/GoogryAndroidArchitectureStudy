972dbc95c5255d7b662d7bd74ab788ace53595dc
developer-sungjae <dunkey2615@naver.com>
2020-04-28 01:05:21 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 :create binditems :move data bind code into viewholder

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/adapters/SearchAdapter.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/adapters/SearchAdapter.kt
@@ -1,5 +1,7 @@
 package com.project.architecturestudy.adapters
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,35 +19,30 @@ class SearchAdapter(var searchData: ArrayList<MovieData.Items>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is SearchingResultHolder -> {
-                when (searchData[position].image.isEmpty()) {
-                    true -> holder.ivMovie.setImageResource(R.drawable.ic_launcher_foreground)
-                    false ->
-                        Picasso.get().load(searchData[position].image)
-                            .placeholder(R.drawable.ic_launcher_foreground)
-                            .centerCrop()
-                            .fit()
-                            .into(holder.ivMovie)
-                }
+            is SearchingResultHolder -> holder.bindItem(searchData[position])
 
-                holder.title.text = searchData[position].title
-                holder.subTitle.text = searchData[position].subtitle
-                holder.pubDate.text = searchData[position].pubDate
-                holder.actors.text = searchData[position].actor
-            }
+
         }
     }
 
-
     override fun getItemCount(): Int = searchData.count()
 
+}
 
-    inner class SearchingResultHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var view = itemView
-        var ivMovie = itemView.iv_movie
-        var title = itemView.tv_title
-        var subTitle = itemView.tv_subTitle
-        var pubDate = itemView.tv_pubDate
-        var actors = itemView.tv_actors
+class SearchingResultHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bindItem(searchData: MovieData.Items) {
+        when (searchData.image.isEmpty()) {
+            true -> itemView.iv_movie.setImageResource(R.drawable.ic_launcher_foreground)
+            false -> Picasso.get().load(searchData.image).centerCrop().into(itemView.iv_movie)
+        }
+        itemView.tv_title.text = searchData.title
+        itemView.tv_subTitle.text = searchData.subtitle
+        itemView.tv_pubDate.text = searchData.pubDate
+        itemView.tv_actors.text = searchData.actor
+        //클릭시 웹사이트 연결
+        itemView.setOnClickListener {
+            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(searchData.link))
+            itemView.context.startActivity(webIntent)
+        }
     }
 }
