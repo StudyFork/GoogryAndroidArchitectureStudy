b15c413a3ab53145393910357a3a8352e4fab24f
kim chul <chuuuul@naver.com>
2020-04-28 13:52:58 +0900

    검색 기능 구현 ( Retrofit2 )

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/SearchActivity.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/SearchActivity.kt
@@ -1,34 +1,103 @@
 package com.example.architecture.activity.search
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.architecture.R
+import com.example.architecture.retrofit.MovieSearchService
 import kotlinx.android.synthetic.main.activity_search.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), Callback<MovieResponseVO> {
+
+    private val MOVIE_SEARCH_API_URL = "https://openapi.naver.com"
+    private var movies = ArrayList<MovieVO>()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(MOVIE_SEARCH_API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         setRecyclerview()
+        setClickEvent()
     }
 
-    private fun setRecyclerview() {
-        val gridLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+    // * 검색
 
-        // * Test
-        val movies = arrayListOf(
-                MovieVO("https://search.pstatic.net/common?type=o&size=120x171&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181019_236%2F1539926790655oHv5z_JPEG%2Fmovie_image.jpg"
-                        , "해리포터", "2001", "https://", 5.0f),
-                MovieVO("https://movie-phinf.pstatic.net/20170112_293/1484183486221oM8s1_JPEG/movie_image.jpg"
-                        , "반지의 제왕", "2001", "https://", 4.0f)
-        )
+    private fun setClickEvent() {
+        button_search_searchButton.setOnClickListener {
+            searchMovie()
+        }
+    }
+
+    private fun searchMovie() {
+        val keyWord = editText_search_searchName.text.toString()
+
+        if (isValidKeyword(keyWord)) {
+            val service: MovieSearchService = retrofit.create(MovieSearchService::class.java)
+            val call = service.requestSearchMovie(keyWord)
+            call.enqueue(this)
+        }
+    }
+
+    private fun isValidKeyword(keyword: String): Boolean {
+        var isValid = true
+        var errorString = ""
+
+        if (keyword == "") {
+            isValid = false
+            errorString = "검색어를 입력하세요"
+        }
+
+        if (!isValid)
+            Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show()
+
+        return isValid
+    }
+
+    // * RecyclerView
+
+    private fun setRecyclerview() {
+
+        val gridLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
 
         recyclerview_search_movieList.adapter = MovieListAdapter(movies)
         recyclerview_search_movieList.layoutManager = gridLayoutManager
     }
 
+    // * Retrofit Response
 
+    override fun onResponse(call: Call<MovieResponseVO>, response: Response<MovieResponseVO>) {
+
+        val result = response.body()
+
+        if (result != null) {
+
+            if (result.total != 0) {
+                movies.clear()
+                movies.addAll(response.body()?.movies!!)
+                recyclerview_search_movieList.adapter?.notifyDataSetChanged()
+            } else {
+                movies.clear()
+                recyclerview_search_movieList.adapter?.notifyDataSetChanged()
+                Toast.makeText(this, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onFailure(call: Call<MovieResponseVO>, t: Throwable) {
+        Log.d("chul", "$t")
+    }
 }
