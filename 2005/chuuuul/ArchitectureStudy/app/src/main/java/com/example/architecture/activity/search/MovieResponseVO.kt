66bf307e061e616ed7be290eb46168b09da53322
kim chul <chuuuul@naver.com>
2020-04-28 12:34:47 +0900

    SerializedName 추가

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/MovieResponseVO.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/MovieResponseVO.kt
@@ -1,8 +1,11 @@
 package com.example.architecture.activity.search
 
+import com.google.gson.annotations.SerializedName
+
 
 data class MovieResponseVO(
     val display: Int,
+    @SerializedName("items")
     val movies: List<MovieVO>,
     val lastBuildDate: String,
     val start: Int,
