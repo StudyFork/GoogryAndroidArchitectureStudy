9486443b60b6c75b3a3565dff4d9b1a1636f3fc3
sangjinKIM93 <larger93r@gmail.com>
2020-04-28 21:12:01 +0900

    webView 생성 및 Link 받아서 출력
    *URL_CLEARTEXT 오류로 인해서 매니페스트에 설정 추가

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieDetailActiviry.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieDetailActiviry.kt
@@ -2,11 +2,29 @@ package com.sangjin.newproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MovieDetailActiviry : AppCompatActivity() {
+
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail_activiry)
+
+        val siteUrl = intent.extras?.get("Link") as String
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(siteUrl)
+
+
     }
 }
