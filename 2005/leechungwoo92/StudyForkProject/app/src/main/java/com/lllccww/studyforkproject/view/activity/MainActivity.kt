398080967aa0e0a3e59386ac3ec29d44676433e4
LeeChungWoo92 <skcjddn@gmail.com>
2020-04-28 22:05:27 +0900

    MainActivity 패키지 변경 및 작업

---
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/view/activity/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.lllccww.studyforkproject.view.activity
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lllccww.studyforkproject.R
+import com.lllccww.studyforkproject.SearchRetrofit
+import com.lllccww.studyforkproject.model.Movie
+import com.lllccww.studyforkproject.model.MovieItem
+import com.lllccww.studyforkproject.view.adapter.MovieListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private var start = 1
+    private var total = 0
+    private var display = 0
+    private var movieList = ArrayList<MovieItem>()
+    lateinit var movieListAdapter: MovieListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        rv_movie_list.layoutManager = LinearLayoutManager(this)
+
+/*        rv_movie_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                val itemTotalCount = recyclerView.adapter!!.itemCount
+                if (lastVisibleItemPosition + 1 == itemTotalCount) { //스크롤 마지막(바닥)
+                    Log.d("fail : ", "---------------바닥------")
+                    if (start < total) {
+                        requestSearchMovie(start + 1)
+                    } else {
+                        Toast.makeText(this@MainActivity, "마지막 페이지입니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+        })*/
+
+
+        btn_search.setOnClickListener {
+
+            closeKeyboard()
+            requestSearchMovie(start)
+        }
+    }
+
+
+    //영화정보 요청
+    private fun requestSearchMovie(pageStart: Int) {
+        SearchRetrofit.getService()
+            .listMovie(keyword = edt_search_keyword.text.toString(), start = pageStart)
+            .enqueue(object : Callback<Movie> {
+                override fun onFailure(call: Call<Movie>, t: Throwable) {
+                    Log.d("fail : ", t.message)
+                }
+
+                override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                    if (response.isSuccessful) {
+                        movieList.clear()
+
+
+                        val movieData = response.body()
+                        Log.d("datadata : ", movieData!!.display)
+                        if (movieData!!.display == "0") {
+                            Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        Log.d("movie : ", movieData.toString())
+                        total = Integer.parseInt(movieData!!.total)
+                        start = Integer.parseInt(movieData!!.start)
+                        display = Integer.parseInt(movieData!!.display)
+
+
+                        for (i in 0 until display) {
+                            movieList!!.add(movieData.items[i])
+                            Log.d("movieList : ", movieList.toString())
+                        }
+
+
+                        movieListAdapter = MovieListAdapter(this@MainActivity, movieList!!)
+                        rv_movie_list.adapter = movieListAdapter
+                        movieListAdapter.notifyDataSetChanged()
+
+                    }
+                }
+
+            })
+    }
+
+
+    //키보드 숨기기
+    fun closeKeyboard() {
+        var view = this.currentFocus
+
+        if (view != null) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+
+}
