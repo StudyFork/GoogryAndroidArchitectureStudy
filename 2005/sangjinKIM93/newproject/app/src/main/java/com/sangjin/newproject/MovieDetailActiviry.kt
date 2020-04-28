81cd0d8ed8b053a9212c4a2ea9ed269c68d69240
sangjinKIM93 <larger93r@gmail.com>
2020-04-28 16:11:30 +0900

    add MovieDetailActivity & activity_movie_detail.xml

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieDetailActiviry.kt
@@ -0,0 +1,12 @@
+package com.sangjin.newproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailActiviry : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail_activiry)
+    }
+}
