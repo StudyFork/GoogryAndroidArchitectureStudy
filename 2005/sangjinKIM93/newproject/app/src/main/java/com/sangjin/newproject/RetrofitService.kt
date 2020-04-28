699d3b917d62c27b194d28e965fd661f4d9c6594
sangjinKIM93 <larger93r@gmail.com>
2020-04-28 13:36:58 +0900

    retrofit & retrofitService 객체 생성

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/RetrofitService.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/RetrofitService.kt
@@ -2,6 +2,8 @@ package com.sangjin.newproject
 
 import com.sangjin.newproject.adapter.Movie
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -17,4 +19,17 @@ interface RetrofitService {
         @Query("query") keyword: String
     ): Call<Movie>
 
+}
+
+//retrofit 객체 생성
+private val retrofit = Retrofit.Builder()
+    .baseUrl("https://openapi.naver.com/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+//retrofitService 객체 생성
+object MovieApi{
+    val retrofitService : RetrofitService by lazy {
+        retrofit.create(RetrofitService::class.java)
+    }
 }
