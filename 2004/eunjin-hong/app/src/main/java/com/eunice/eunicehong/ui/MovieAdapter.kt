2e52069e90517b3a25a9022e9270b85e5e56aebd
eunjin-hong <ejhong.4991@gmail.com>
2020-04-28 23:42:53 +0900

    HW-4 :art: use data binding on movie items

--- 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MovieAdapter.kt
+++ 2004/eunjin-hong/app/src/main/java/com/eunice/eunicehong/ui/MovieAdapter.kt
@@ -2,26 +2,52 @@ package com.eunice.eunicehong.ui
 
 import android.text.Spanned
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.core.text.HtmlCompat
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.eunice.eunicehong.R
 import com.eunice.eunicehong.data.model.Movie
+import com.eunice.eunicehong.databinding.ItemMovieBinding
+
+@BindingAdapter("app:imageSrc")
+fun loadImage(imageView: ImageView, url: String) {
+    Glide.with(imageView.context)
+        .load(url)
+        .into(imageView)
+}
+
+@BindingAdapter("app:formattedText")
+fun formattedText(textView: TextView, text: String) {
+    val formatted = text.split("|")
+        .map { it.trim() }
+        .filter { it != "" }
+        .joinToString(", ")
+
+    textView.text = formatted.parseHTML()
+}
+
+private fun String.parseHTML(): Spanned =
+    HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY)
 
 class MovieAdapter(private val presenter: MoviePresenter) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     private val movieList = mutableListOf<Movie>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false),
-        presenter
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = DataBindingUtil.inflate<ItemMovieBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_movie,
+            parent,
+            false
+        )
+        return ViewHolder(binding, presenter)
+    }
 
     override fun getItemCount(): Int = movieList.size
 
@@ -41,51 +67,22 @@ class MovieAdapter(private val presenter: MoviePresenter) :
         addAllMovies(movies)
     }
 
-    class ViewHolder(itemView: View, private val presenter: MoviePresenter) :
-        RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val binding: ItemMovieBinding, private val presenter: MoviePresenter) :
+        RecyclerView.ViewHolder(binding.root) {
 
         private lateinit var movie: Movie
-
-        private val card = itemView.findViewById<CardView>(R.id.movie_card).apply {
-            setOnClickListener {
-                presenter.showDetail(movie.link)
-            }
-        }
-        private val title = itemView.findViewById<TextView>(R.id.movie_title)
-        private val poster = itemView.findViewById<ImageView>(R.id.movie_poster)
-        private val subtitle = itemView.findViewById<TextView>(R.id.movie_subtitle)
-        private val actors = itemView.findViewById<TextView>(R.id.movie_actors)
-        private val directors = itemView.findViewById<TextView>(R.id.movie_directors)
-        private val pubDate = itemView.findViewById<TextView>(R.id.movie_date_text)
-        private val rating = itemView.findViewById<TextView>(R.id.movie_user_rating_value)
 
         fun bind(item: Movie) {
             movie = item
 
-            title.text = movie.title.parseHTML()
-
-            subtitle.text = movie.subtitle.parseHTML()
-
-            actors.text = movie.actors.formatStaffList().parseHTML()
-
-            directors.text = movie.directors.formatStaffList().parseHTML()
-
-            pubDate.text = movie.pubDate
-            rating.text =
-                rating.context.getString(R.string.movie_item_user_rating_key, movie.userRating)
-
-            Glide.with(poster.context).load(movie.imageUrl).into(poster)
+            binding.movie = movie
+            binding.viewHolder = this@ViewHolder
 
         }
 
-        private fun String.parseHTML(): Spanned =
-            HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        fun showDetailWebPage() {
+            presenter.showDetail(movie.link)
+        }
 
-
-        private fun String.formatStaffList(): String = this.split("|")
-            .map { it.trim() }
-            .filter { it != "" }
-            .joinToString(", ")
     }
-
 }
