ede3f99a72e2f4ddc1382e86d8503f50f20b11a8
kim chul <chuuuul@naver.com>
2020-04-28 15:52:35 +0900

    image변수의 Nullable 제거
     - API Document에 '이미지가 있는경우에만 나타난다'를 이미지가 없을 때 Null 이라는 의미로 잘못 이해함

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/vo/MovieVO.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/vo/MovieVO.kt
@@ -1,9 +1,9 @@
 package com.example.architecture.vo
 
 data class MovieVO(
-        val image : String?,
-        val title : String,
-        val pubDate: String,
-        val link : String,
-        val userRating: Float
+    val image: String,
+    val title : String,
+    val pubDate: String,
+    val link : String,
+    val userRating: Float
 )
