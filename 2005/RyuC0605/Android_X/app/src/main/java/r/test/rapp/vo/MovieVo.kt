d0083017ac11ec3235c17307ac7d4a3e1127346b
Ryu <bbs0605@naver.com>
2020-04-29 23:56:16 +0900

    Vo 클래스 추가

---
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/vo/MovieVo.kt
@@ -0,0 +1,12 @@
+package r.test.rapp.vo
+
+data class MovieVo(
+    val actor: String,
+    val director: String,
+    val image: String,
+    val link: String,
+    val pubDate: String,
+    val subtitle: String,
+    val title: String,
+    val userRating: String
+)
