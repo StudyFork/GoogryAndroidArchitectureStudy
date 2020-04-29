acf453cb9ba5886def940a32b1169fa2e0498512
invitejay <sksskdi122345@gmail.com>
2020-04-29 20:46:37 +0900

    HW-4 fix adapter

--- 2004/invitejay/app/src/main/java/com/example/studyforkandroid/adapter/MovieAdapter.kt
+++ 2004/invitejay/app/src/main/java/com/example/studyforkandroid/adapter/MovieAdapter.kt
@@ -3,31 +3,32 @@ package com.example.studyforkandroid.adapter
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.studyforkandroid.R
 import com.example.studyforkandroid.data.Movie
-import com.example.studyforkandroid.utils.htmlToString
-import kotlinx.android.synthetic.main.item_movie.view.*
+import com.example.studyforkandroid.databinding.ItemMovieBinding
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieVh>() {
 
     private val movieList = mutableListOf<Movie>()
+    lateinit var binding: ItemMovieBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieVh {
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
-        val movieVh = MovieVh(view)
-        view.setOnClickListener {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_movie,
+            parent,
+            false
+        )
+        val movieVh = MovieVh(binding)
+        binding.root.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(movieList[movieVh.adapterPosition].link)
             parent.context.startActivity(intent)
         }
-
         return movieVh
     }
 
@@ -35,8 +36,10 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieVh>() {
         return movieList.size
     }
 
-    fun addAll(movieList: List<Movie>) {
+    fun replaceAll(movieList: List<Movie>) {
+        clear()
         this.movieList.addAll(movieList)
+        notifyDataSetChanged()
     }
 
     fun clear() {
@@ -44,21 +47,13 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieVh>() {
     }
 
     override fun onBindViewHolder(holder: MovieVh, position: Int) {
-        movieList[position].let { movie ->
-            with(holder) {
-                title.text = movie.title.htmlToString()
-                rating.text = movie.userRating.htmlToString()
-                actor.text = movie.actor.htmlToString().replace("|", " ")
-            }
-        }
-
-        Glide.with(holder.img.context).load(movieList[position].image).into(holder.img)
+        holder.bind(movieList[position])
     }
 
-    class MovieVh(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val img: ImageView = itemView.movieImg
-        val title: TextView = itemView.movieTitle
-        val actor: TextView = itemView.movieActor
-        val rating: TextView = itemView.movieRating
+    inner class MovieVh(binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Movie) {
+            binding.movie = item
+            binding.executePendingBindings()
+        }
     }
 }
