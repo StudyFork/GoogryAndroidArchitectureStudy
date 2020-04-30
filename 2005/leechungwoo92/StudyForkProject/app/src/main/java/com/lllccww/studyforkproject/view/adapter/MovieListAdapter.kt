e834c97f6509e54b30772e885d54b82f3633cda0
LeeChungWoo92 <skcjddn@gmail.com>
2020-04-30 18:56:08 +0900

    HW-1
    MovieListAdapter 구조 변경

--- 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/view/adapter/MovieListAdapter.kt
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/view/adapter/MovieListAdapter.kt
@@ -1,8 +1,9 @@
 package com.lllccww.studyforkproject.view.adapter
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,8 +13,8 @@ import com.lllccww.studyforkproject.R
 import com.lllccww.studyforkproject.model.MovieItem
 import kotlinx.android.synthetic.main.item_movie_list.view.*
 
-class MovieListAdapter(private var activity: Activity, private val list: ArrayList<MovieItem>) :
-    RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
+class MovieListAdapter(val context: Context) : RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
+    private val list: ArrayList<MovieItem> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -28,7 +29,7 @@ class MovieListAdapter(private var activity: Activity, private val list: ArrayLi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
 
-        Glide.with(activity).load(item.image).error(R.drawable.ic_no_img).into(holder.ivMovieImage)
+        Glide.with(context).load(item.image).error(R.drawable.ic_no_img).into(holder.ivMovieImage)
 
         holder.tvTitle.text = android.text.Html.fromHtml(item.title).toString()
         holder.tvDirector.text = item.director
@@ -36,11 +37,22 @@ class MovieListAdapter(private var activity: Activity, private val list: ArrayLi
         holder.llParentView.setOnClickListener {
 
             var intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.link))
-            activity.startActivity(intent)
+            context.startActivity(intent)
 
         }
 
 
+    }
+
+    fun setItems(items: ArrayList<MovieItem>) {
+        list.addAll(items)
+        Log.d("movieData : ", list.size.toString())
+        notifyDataSetChanged()
+    }
+
+    fun clear() {
+        list.clear()
+        notifyDataSetChanged()
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
