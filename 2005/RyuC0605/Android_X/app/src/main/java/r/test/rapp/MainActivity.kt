617829ed74b009270c034d4372ffc92337a954b0
Ryu <bbs0605@naver.com>
2020-04-30 09:01:56 +0900

    레이아웃 작업 및 android 3.6 대응 코드 추가

--- 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/MainActivity.kt
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/MainActivity.kt
@@ -2,11 +2,17 @@ package r.test.rapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lv_contents.emptyView = txt_empty;
     }
+
+    fun onClick(view: View) {}
 }
