b8a6f1dc07753a2b1f0550a4bfeb47d894b12aff
Sungjae-Bae <dunkey2615@naver.com>
2020-04-30 16:26:17 +0900

    HW-1 Naver API를 이용한 영화 검색 리스트 구현 : delete SerializedName

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/models/MovieData.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/models/MovieData.kt
@@ -1,30 +1,20 @@
 package com.project.architecturestudy.models
 
-import com.google.gson.annotations.SerializedName
-
 class MovieData(
-    @SerializedName("lastBuildDate") val lastBuildDate: String,
-    @SerializedName("total") val total: Int,
-    @SerializedName("start") val start: Int,
-    @SerializedName("display") val display: Int,
-    @SerializedName("items") val items: Array<Items>
+    val lastBuildDate: String,
+    val total: Int,
+    val start: Int,
+    val display: Int,
+    val items: Array<Items>
 ) {
     data class Items(
-        @SerializedName("title")
         val title: String,
-        @SerializedName("link")
         val link: String,
-        @SerializedName("image")
         val image: String,
-        @SerializedName("subtitle")
         val subtitle: String,
-        @SerializedName("pubdate")
         val pubDate: String,
-        @SerializedName("director")
         val director: String,
-        @SerializedName("actor")
         val actor: String,
-        @SerializedName("userRating")
         val userRating: Double
     )
 }
