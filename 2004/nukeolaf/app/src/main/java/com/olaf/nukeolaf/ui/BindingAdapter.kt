6163d47374f16409d4654922bbf3ffe862e684ee
nukeolaf <yhjin04170@gmail.com>
2020-05-04 00:04:09 +0900

    HW-4 binding 된 이미지 url 을 ImageView 에 보여주기 위한 BindingAdapter 추가

---
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/BindingAdapter.kt
@@ -0,0 +1,14 @@
+package com.olaf.nukeolaf.ui
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.olaf.nukeolaf.R
+
+@BindingAdapter("movieImage")
+fun loadMoiveImage(view: ImageView, imageUrl: String) {
+    Glide.with(view.context)
+        .load(imageUrl)
+        .error(R.drawable.ic_launcher_foreground)
+        .into(view)
+}
