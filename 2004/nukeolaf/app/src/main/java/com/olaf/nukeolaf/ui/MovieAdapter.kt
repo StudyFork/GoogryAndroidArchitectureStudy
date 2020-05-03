20590149f80dce7c5db29e3a48a28d0c541e848c
nukeolaf <yhjin04170@gmail.com>
2020-05-04 00:08:16 +0900

    HW-4 MovieAdapter 의 onCreateViewHolder() 에서 MovieItemViewHolder 초기화시 생성자에 binding 넣도록 수정

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MovieAdapter.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MovieAdapter.kt
@@ -3,8 +3,8 @@ package com.olaf.nukeolaf.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.olaf.nukeolaf.R
 import com.olaf.nukeolaf.data.model.MovieItem
+import com.olaf.nukeolaf.databinding.ItemMoviesRvBinding
 
 class MovieAdapter : RecyclerView.Adapter<MovieItemViewHolder>() {
 
@@ -19,10 +19,9 @@ class MovieAdapter : RecyclerView.Adapter<MovieItemViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieItemViewHolder {
-        val inflatedView = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.item_movies_rv, parent, false)
-        return MovieItemViewHolder(inflatedView)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMoviesRvBinding.inflate(inflater)
+        return MovieItemViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
