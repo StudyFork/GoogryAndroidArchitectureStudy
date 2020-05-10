d5a30865fcc6b47637c3631c8a83884b93587577
developer-sungjae <dunkey2615@naver.com>
2020-05-10 18:47:03 +0900

    create interface named `NaverMovieRepository`, `NaverMovieLocalDataSource`,`NaverMovieRemoteDataSource`

---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
@@ -0,0 +1,4 @@
+package com.project.architecturestudy.data.repository
+
+interface NaverMovieRepository {
+}
---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/NaverMovieLocalDataSource.kt
@@ -0,0 +1,4 @@
+package com.project.architecturestudy.data.source.local
+
+interface NaverMovieLocalDataSource {
+}
