b8249733a4907ce18aae9cfba7e5d79cc6144ae2
developer-sungjae <dunkey2615@naver.com>
2020-05-11 22:13:23 +0900

    add Success, Failure callback

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSource.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.project.architecturestudy.data.source.remote
 
+import com.project.architecturestudy.data.model.Movie
+
 interface NaverMovieRemoteDataSource {
 
-    fun getMovieList(query: String)
+    fun getMovieList(query: String, Success: (Movie.Items) -> Unit, Failure: (Throwable) -> Unit)
 }
