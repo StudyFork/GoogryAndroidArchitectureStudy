802abd8b292a42828a90f359af6cc43b391d7521
kim chul <chuuuul@naver.com>
2020-05-11 23:37:29 +0900

    HW-2 기능 : Const 변수 추가
    - val -> const val
    - Movie Shared Name 추가

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/util/ConstValue.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/util/ConstValue.kt
@@ -2,8 +2,10 @@ package com.example.architecture.util
 
 class ConstValue {
     companion object{
-        val MOVIE_SEARCH_API_URL = "https://openapi.naver.com"
-        val NO_IMAGE_URL = "https://ssl.pstatic.net/static/movie/2012/06/dft_img133x190.png"
+        const val MOVIE_SEARCH_API_URL = "https://openapi.naver.com"
+        const val NO_IMAGE_URL = "https://ssl.pstatic.net/static/movie/2012/06/dft_img133x190.png"
+
+        const val MOVIE_SHARED_PREFERENCE_NAME = "MoviesSharedPreferenceName"
     }
 
 }
