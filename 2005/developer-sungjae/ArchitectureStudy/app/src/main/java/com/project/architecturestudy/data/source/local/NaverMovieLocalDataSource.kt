12893ce6abe2086b5a2ccea8f745dd6049209a5e
developer-sungjae <dunkey2615@naver.com>
2020-05-11 23:26:11 +0900

    change callback param to Arraylist

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/NaverMovieLocalDataSource.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/NaverMovieLocalDataSource.kt
@@ -5,7 +5,7 @@ import com.project.architecturestudy.data.model.Movie
 
 interface NaverMovieLocalDataSource {
 
-    fun getMovieList(Success: (Movie.Items) -> Unit, Failure: (Throwable) -> Unit)
+    fun getMovieList(Success: (ArrayList<Movie.Items>) -> Unit, Failure: (Throwable) -> Unit)
 
     fun saveMovieList(movieItem: ArrayList<Movie.Items>, context: Context)
 }
