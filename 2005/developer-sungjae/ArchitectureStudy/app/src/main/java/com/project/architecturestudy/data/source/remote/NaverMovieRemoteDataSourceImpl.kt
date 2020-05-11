0e724b7e915cabd9d020a0f7deac28f7c4e9c255
developer-sungjae <dunkey2615@naver.com>
2020-05-11 22:46:24 +0900

    add retrofit remote communication business logic

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
@@ -1,7 +1,27 @@
 package com.project.architecturestudy.data.source.remote
 
+import android.annotation.SuppressLint
+import com.project.architecturestudy.components.RetrofitService
+import com.project.architecturestudy.data.model.Movie
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class NaverMovieRemoteDataSourceImpl : NaverMovieRemoteDataSource {
-    override fun getMovieList(query: String) {
+    @SuppressLint("CheckResult")
+    override fun getMovieList(
+        keyWord: String,
+        Success: (ArrayList<Movie.Items>) -> Unit,
+        Failure: (Throwable) -> Unit
+    ) {
+        val service = RetrofitService.create()
+        service.getMovies(keyWord)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                Success.invoke(it.items)
+            }, { error ->
+                Failure.invoke(error)
+            })
     }
 
 }
