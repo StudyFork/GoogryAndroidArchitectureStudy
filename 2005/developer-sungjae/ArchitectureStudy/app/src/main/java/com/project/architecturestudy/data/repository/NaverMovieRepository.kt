90353f2918554f1208593ce7aa1b60383f2ef730
developer-sungjae <dunkey2615@naver.com>
2020-05-11 22:25:25 +0900

    reformat parameter adapter

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
@@ -1,6 +1,8 @@
 package com.project.architecturestudy.data.repository
 
+import com.project.architecturestudy.adapters.SearchAdapter
+
 interface NaverMovieRepository {
 
-    fun getMovieList(query: String)
+    fun getMovieList(keyWord: String, adapter: SearchAdapter?)
 }
