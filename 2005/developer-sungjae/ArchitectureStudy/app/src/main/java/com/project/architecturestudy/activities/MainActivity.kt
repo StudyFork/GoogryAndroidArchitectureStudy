04d3cf964066f1b623f1a2610c5deb383d05f113
developer-sungjae <dunkey2615@naver.com>
2020-05-11 22:24:36 +0900

    delete business logic about retrofit & add naverMovieRepositoryImpl method

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.project.architecturestudy.activities
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.project.architecturestudy.R
 import com.project.architecturestudy.adapters.SearchAdapter
-import com.project.architecturestudy.components.RetrofitService
 import com.project.architecturestudy.data.repository.NaverMovieRepositoryImpl
 import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSourceImpl
 import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSourceImpl
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -34,23 +29,9 @@ class MainActivity : AppCompatActivity() {
             if (et_search.text.toString().isEmpty()) {
                 return@setOnClickListener
             }
-
-            doSearch(et_search.text.toString())
+            naverMovieRepositoryImpl.getMovieList(et_search.text.toString(), adapter)
         }
         setRecyclerView()
-    }
-
-    @SuppressLint("CheckResult")
-    fun doSearch(keyWord: String) {
-        val service = RetrofitService.create()
-        service.getMovies(keyWord)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe({ movie ->
-                adapter?.resetData(movie.items)
-            }, { error ->
-                Log.d("error", error.toString())
-            })
     }
 
     private fun setRecyclerView() {
