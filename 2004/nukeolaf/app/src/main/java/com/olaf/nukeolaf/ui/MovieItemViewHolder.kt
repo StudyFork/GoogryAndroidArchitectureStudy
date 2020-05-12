1b7fc6b2cf8c14476d7262b4ebc982177288ee1f
nukeolaf <yhjin04170@gmail.com>
2020-05-13 06:39:50 +0900

    HW-4 Adapter 코드 수정

--- 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MovieItemViewHolder.kt
+++ 2004/nukeolaf/app/src/main/java/com/olaf/nukeolaf/ui/MovieItemViewHolder.kt
@@ -1,20 +1,11 @@
 package com.olaf.nukeolaf.ui
 
-import android.content.Intent
-import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.olaf.nukeolaf.data.model.MovieItem
 import com.olaf.nukeolaf.databinding.ItemMoviesRvBinding
 
 class MovieItemViewHolder(private val binding: ItemMoviesRvBinding) :
     RecyclerView.ViewHolder(binding.root) {
-
-    init {
-        itemView.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(binding.movieItem.link))
-            it.context.startActivity(intent)
-        }
-    }
 
     fun bind(item: MovieItem) {
         with(binding) {
