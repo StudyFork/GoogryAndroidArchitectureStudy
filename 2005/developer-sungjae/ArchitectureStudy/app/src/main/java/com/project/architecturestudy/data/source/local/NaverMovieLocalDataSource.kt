9d050a83e6549e2f73c0d63b3d5bb7c613c5692f
developer-sungjae <dunkey2615@naver.com>
2020-05-13 00:59:17 +0900

    rewrite all context params be nullable & change other params just name

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/NaverMovieLocalDataSource.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/NaverMovieLocalDataSource.kt
@@ -2,14 +2,15 @@ package com.project.architecturestudy.data.source.local
 
 import android.content.Context
 import com.project.architecturestudy.data.model.Movie
+import com.project.architecturestudy.data.source.local.room.MovieLocal
 
 interface NaverMovieLocalDataSource {
 
     fun getMovieList(
-        context: Context,
-        Success: (ArrayList<Movie.Items>) -> Unit,
+        context: Context?,
+        Success: (ArrayList<MovieLocal>) -> Unit,
         Failure: (Throwable) -> Unit
     )
 
-    fun saveMovieList(movieItem: ArrayList<Movie.Items>, context: Context)
+    fun saveMovieList(data: ArrayList<Movie.Items>, context: Context?)
 }
