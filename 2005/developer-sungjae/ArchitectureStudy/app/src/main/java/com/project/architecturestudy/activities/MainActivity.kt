3b1aab5dca4a1741f25d56fac0133183752c01ff
developer-sungjae <dunkey2615@naver.com>
2020-05-13 01:03:48 +0900

    change logic sequence (when use play app cashing data can be seen)

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/activities/MainActivity.kt
@@ -8,6 +8,7 @@ import com.project.architecturestudy.R
 import com.project.architecturestudy.adapters.SearchAdapter
 import com.project.architecturestudy.data.repository.NaverMovieRepositoryImpl
 import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSourceImpl
+import com.project.architecturestudy.data.source.local.room.MovieRoomDataBase
 import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSourceImpl
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
@@ -25,25 +26,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setRecyclerView()
 
         btn_search.setOnClickListener {
             if (et_search.text.toString().isEmpty()) {
+                toast(getString(R.string.please_write))
                 return@setOnClickListener
             }
-            naverMovieRepositoryImpl.getMovieList(this, et_search.text.toString(), adapter,
+            naverMovieRepositoryImpl.getMovieList(et_search.text.toString(),
                 SuccessMsg = { toast(getString(R.string.get_data_success)) },
                 FailureMsg = { toast(getString(R.string.get_data_failure)) })
         }
-        setRecyclerView()
     }
 
     private fun setRecyclerView() {
         adapter = SearchAdapter()
         listview_movie.adapter = adapter
 
+        naverMovieRepositoryImpl.getCashingMovieList(this, adapter)
+
         adapter?.onClick = { item ->
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.link))
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        MovieRoomDataBase.destoryInstance()
     }
 }
