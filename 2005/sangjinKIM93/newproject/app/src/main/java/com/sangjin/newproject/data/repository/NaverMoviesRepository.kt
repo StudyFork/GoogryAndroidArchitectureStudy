623b4c1dee4a0e592794126647d113ffa842da25
sangjinKIM93 <larger93r@gmail.com>
2020-05-12 21:06:28 +0900

    NaverMovieRepository에 localDataSource 추가
    이와 더불어 필요한 객체인 roomDB도 생성자에 추가

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/data/repository/NaverMoviesRepository.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/data/repository/NaverMoviesRepository.kt
@@ -1,11 +1,13 @@
 package com.sangjin.newproject.data.repository
 
 import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.data.source.local.RoomDB
 
 interface NaverMoviesRepository {
 
     fun getNaverMovies(
         query: String,
+        roomDB: RoomDB,
         onSuccess: ((movies: List<Movie>) -> Unit),
         onFailure: ((t: Throwable) -> Unit)
     )
--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/data/repository/NaverMoviesRepositoryImpl.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/data/repository/NaverMoviesRepositoryImpl.kt
@@ -1,17 +1,27 @@
 package com.sangjin.newproject.data.repository
 
 import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.data.source.local.LocalDataSource
+import com.sangjin.newproject.data.source.local.RoomDB
 import com.sangjin.newproject.data.source.remote.RemoteDataSourceImpl
 
 class NaverMoviesRepositoryImpl(
-    private val remoteDataSourceImpl: RemoteDataSourceImpl
+    private val remoteDataSourceImpl: RemoteDataSourceImpl,
+    private val localDataSource: LocalDataSource
 ) : NaverMoviesRepository {
 
     override fun getNaverMovies(
         query: String,
+        roomDB: RoomDB,
         onSuccess: (movies: List<Movie>) -> Unit,
         onFailure: (t: Throwable) -> Unit
     ) {
+
+        localDataSource.getMovieData(query, roomDB,
+        onSuccess = { movies ->
+            onSuccess(movies)
+        })
+
         remoteDataSourceImpl.getMovieData(query,
             onSuccess = { movies ->
                 onSuccess(movies)
