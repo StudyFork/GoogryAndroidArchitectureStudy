a0930176280ff4c6bb39577f23a8e980afed820e
developer-sungjae <dunkey2615@naver.com>
2020-05-13 00:53:56 +0900

    change entities class name to MovieLocal

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/room/AppDataBase.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/local/room/AppDataBase.kt
@@ -3,7 +3,7 @@ package com.project.architecturestudy.data.source.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [MovieItem::class], version = 1)
+@Database(entities = [MovieLocal::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieItemDAO(): MovieItemDAO
 }
