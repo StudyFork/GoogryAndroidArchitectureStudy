68dbd399668b716f18722d2557377ea34e999127
sangjinKIM93 <larger93r@gmail.com>
2020-05-13 10:24:38 +0900

    Activity 수정
    기존 레트로핏에서 직접 결과를 받아 처리하던 것에서
    Repository에서 결과를 받아 처리하는 것으로 변경하였습니다.

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListActivity.kt
@@ -9,23 +9,27 @@ import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.sangjin.newproject.adapter.Movie
 import com.sangjin.newproject.adapter.MovieListAdapter
-import com.sangjin.newproject.adapter.ResponseData
+import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.data.repository.NaverMoviesRepositoryImpl
+import com.sangjin.newproject.data.source.local.LocalDataSourceImpl
+import com.sangjin.newproject.data.source.local.RoomDB
+import com.sangjin.newproject.data.source.remote.RemoteDataSourceImpl
 import kotlinx.android.synthetic.main.activity_movie_list.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MovieListActivity : AppCompatActivity() {
 
     private var movieList = ArrayList<Movie>()
     private lateinit var movieListAdapter: MovieListAdapter
+    private val naverMoviesRepositoryImpl by lazy {
+        NaverMoviesRepositoryImpl(
+            RemoteDataSourceImpl(),
+            LocalDataSourceImpl()
+        )
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,12 +39,12 @@ class MovieListActivity : AppCompatActivity() {
         setRecyclerView()
 
         movieNameET.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_SEARCH){
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
 
                 onClick(v)
 
                 true
-            }else{
+            } else {
                 false
             }
         }
@@ -52,7 +56,7 @@ class MovieListActivity : AppCompatActivity() {
     /**
      * 키패드 보여주기
      */
-    private fun showKeyPad(){
+    private fun showKeyPad() {
         movieNameET.requestFocus()
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
     }
@@ -61,8 +65,9 @@ class MovieListActivity : AppCompatActivity() {
     /**
      * 키패드 숨기기
      */
-    private fun hideKeyPad(v: View){
-        val imm : InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    private fun hideKeyPad(v: View) {
+        val imm: InputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
 
@@ -74,9 +79,9 @@ class MovieListActivity : AppCompatActivity() {
 
         val keyWord = movieNameET.text.toString().trim()
 
-        if(TextUtils.isEmpty(keyWord)){
+        if (TextUtils.isEmpty(keyWord)) {
             Toast.makeText(this, R.string.no_keyword, Toast.LENGTH_LONG).show()
-        }else{
+        } else {
             getMovieList(keyWord)
             hideKeyPad(view)
         }
@@ -86,38 +91,36 @@ class MovieListActivity : AppCompatActivity() {
     /**
      * 검색 결과 받아서 출력
      */
-    private fun getMovieList(keyWord: String){
+    private fun getMovieList(keyWord: String) {
 
-        MovieApi.retrofitService.requestMovieList(keyword = keyWord)
-            .enqueue(object: Callback<ResponseData>{
-                override fun onResponse(call: Call<ResponseData>, response: Response<ResponseData>) {
-                    val result = response.body()?.items
-
-                    if (result.isNullOrEmpty()) {
-                        movieList.clear()
-                        movieListAdapter.addList(movieList)
-                        Toast.makeText(this@MovieListActivity, R.string.no_movie_list, Toast.LENGTH_SHORT).show()
-                    }
-                    else{
-                        movieList.clear()
-                        movieList.addAll(result)
-                        movieListAdapter.addList(movieList)
-                    }
+        naverMoviesRepositoryImpl.getNaverMovies(
+            keyWord,
+            onSuccess = { movies ->
+                if (movies.isNullOrEmpty()) {
+                    movieList.clear()
+                    movieListAdapter.addList(movieList)
+                    Toast.makeText(this@MovieListActivity, R.string.no_movie_list, Toast.LENGTH_SHORT).show()
+                } else {
+                    movieList.clear()
+                    movieList.addAll(movies)
+                    movieListAdapter.addList(movieList)
                 }
-
-                override fun onFailure(call: Call<ResponseData>, t: Throwable) {
-                }
-            })
+            },
+            onFailure = { t ->
+                Toast.makeText(this@MovieListActivity, t.toString(), Toast.LENGTH_SHORT).show()
+            },
+            roomDB = RoomDB.getInstance(applicationContext)
+        )
     }
 
 
     /**
      * 리사이클러뷰 셋팅
      */
-    private fun setRecyclerView(){
+    private fun setRecyclerView() {
 
         //각 항목 클릭시 이벤트 처리
-        val onItemClickListener: ((Int) -> Unit) = {position ->
+        val onItemClickListener: ((Int) -> Unit) = { position ->
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(movieList.get(position).link))
             startActivity(intent)
         }
