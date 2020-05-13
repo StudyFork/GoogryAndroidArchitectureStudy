2a1ad52083bdfca667e81cdcd1001e3220a3a20d
hwaniiidev <hwaniiidev@gmail.com>
2020-05-13 22:56:32 +0900

    feat: Remote용 interface, 구현체 구현

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/source/remote/NaverMovieRemoteDataSource.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/source/remote/NaverMovieRemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.hwaniiidev.architecture.data.source.remote
 
+import com.hwaniiidev.architecture.model.Item
+import com.hwaniiidev.architecture.model.ResponseMovieSearchData
+
 interface NaverMovieRemoteDataSource {
+    fun getRemoteMovies(
+        query: String,
+        //onSuccess: (movies: ArrayList<Item>) -> Unit,
+        onSuccess: (response: ResponseMovieSearchData) -> Unit,
+        onError: (errorMessage: String) -> Unit,
+        onFailure: (t: Throwable) -> Unit
+    )
+
 }
--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
@@ -1,4 +1,47 @@
 package com.hwaniiidev.architecture.data.source.remote
 
-class NaverMovieRemoteDataSourceImpl {
+import com.hwaniiidev.architecture.ApiCall
+import com.hwaniiidev.architecture.model.ResponseMovieSearchData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class NaverMovieRemoteDataSourceImpl : NaverMovieRemoteDataSource {
+    private val api = ApiCall.api
+
+    companion object {
+        const private val clientId = "KXY8b7w9cuaFFHkDSGwS"
+        const private val clientPw = "HdI9WbTqtt"
+        const private val displayValue = 30
+    }
+
+    override fun getRemoteMovies(
+        query: String,
+        //onSuccess: (movies: ArrayList<Item>) -> Unit,
+        onSuccess: (response: ResponseMovieSearchData) -> Unit,
+        onError: (errorMessage: String) -> Unit,
+        onFailure: (t: Throwable) -> Unit
+    ) {
+        api.getMovieSearchData(clientId, clientPw, query, displayValue)
+            .enqueue(object : Callback<ResponseMovieSearchData> {
+                override fun onResponse(
+                    call: Call<ResponseMovieSearchData>,
+                    response: Response<ResponseMovieSearchData>
+                ) {
+                    if (response.isSuccessful) {
+                        val result = response.body()
+                        if (result != null) {
+                            onSuccess(result)
+                            //onSuccess(result.items)
+                        }
+                    } else {
+                        onError(response.errorBody().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseMovieSearchData>, t: Throwable) {
+                    onFailure(t)
+                }
+            })
+    }
 }
