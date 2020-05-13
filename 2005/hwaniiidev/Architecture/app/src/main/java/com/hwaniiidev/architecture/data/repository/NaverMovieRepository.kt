e06a2c29f4143b83151d18f9beb48c6b5bb05c80
hwaniiidev <hwaniiidev@gmail.com>
2020-05-13 23:00:06 +0900

    feat: Repository 용 interface, 구현체 구현

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepository.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepository.kt
@@ -1,4 +1,14 @@
 package com.hwaniiidev.architecture.data.repository
 
+import com.hwaniiidev.architecture.model.Item
+import com.hwaniiidev.architecture.model.ResponseMovieSearchData
+
 interface NaverMovieRepository {
+    fun getRemoteMovies(
+        query: String,
+        //onSuccess : (movies : ArrayList<Item>) -> Unit,
+        onSuccess: (response: ResponseMovieSearchData) -> Unit,
+        onError: (errorMessage: String) -> Unit,
+        onFailure: (t: Throwable) -> Unit
+    )
 }
--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepositoryImpl.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepositoryImpl.kt
@@ -1,4 +1,23 @@
 package com.hwaniiidev.architecture.data.repository
 
-class NaverMovieRepositoryImpl {
+import com.hwaniiidev.architecture.data.source.remote.NaverMovieRemoteDataSourceImpl
+import com.hwaniiidev.architecture.model.ResponseMovieSearchData
+
+class NaverMovieRepositoryImpl : NaverMovieRepository {
+    private val naverMovieRemoteSource = NaverMovieRemoteDataSourceImpl()
+
+    override fun getRemoteMovies(
+        query: String,
+        //onSuccess: (movies: ArrayList<Item>) -> Unit,
+        onSuccess: (response: ResponseMovieSearchData) -> Unit,
+        onError: (errorMessage: String) -> Unit,
+        onFailure: (t: Throwable) -> Unit
+    ) {
+        naverMovieRemoteSource.getRemoteMovies(
+            query,
+            onSuccess,
+            onError,
+            onFailure
+        )
+    }
 }
