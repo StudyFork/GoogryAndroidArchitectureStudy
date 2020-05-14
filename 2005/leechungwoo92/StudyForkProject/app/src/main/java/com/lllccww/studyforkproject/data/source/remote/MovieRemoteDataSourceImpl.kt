24927843cefb432686224e437f465dc73baf6655
LeeChungWoo92 <skcjddn@gmail.com>
2020-05-14 14:53:42 +0900

    HW-2 DataSourceImpl 추가

---
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/data/source/remote/MovieRemoteDataSourceImpl.kt
@@ -0,0 +1,35 @@
+package com.lllccww.studyforkproject.data.source.remote
+
+import com.lllccww.studyforkproject.SearchRetrofit
+import com.lllccww.studyforkproject.data.model.Movie
+import com.lllccww.studyforkproject.data.model.MovieItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MovieRemoteDataSourceImpl : MovieRemoteDataSource {
+
+    override fun getMovieList(
+        query: String,
+        onSuccess: (List<MovieItem>) -> Unit,
+        onFailure: (e: Throwable) -> Unit
+    ) {
+
+        //영화정보 요청
+        SearchRetrofit.getService().listMovie(keyword = query)
+            .enqueue(object : Callback<Movie> {
+                override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                    val movieItems = response.body()?.items
+                    onSuccess(movieItems!!)
+
+                }
+
+                override fun onFailure(call: Call<Movie>, t: Throwable) {
+                    onFailure(t)
+                }
+
+            })
+    }
+}
+
+
