1923c0ff516831d169b25dafb73293d24bbe9e54
developer-sungjae <dunkey2615@naver.com>
2020-05-16 01:50:13 +0900

    make callbackParams to detach adapter from Model

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
@@ -1,7 +1,9 @@
 package com.project.architecturestudy.data.repository
 
 import android.content.Context
+import com.project.architecturestudy.data.model.Movie
 import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSource
+import com.project.architecturestudy.data.source.local.room.MovieLocal
 import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSource
 
 interface NaverMovieRepository {
@@ -11,12 +13,6 @@ interface NaverMovieRepository {
     var naverMovieRemoteDataSource: NaverMovieRemoteDataSource
 
     fun setRepository(context: Context, naverMovieLocalDataSource: NaverMovieLocalDataSource, naverMovieRemoteDataSource: NaverMovieRemoteDataSource)
-
-    fun getMovieList(
-        keyWord: String,
-        SuccessMsg: () -> Unit,
-        FailureMsg: () -> Unit
-    )
-
-    fun getCashedMovieList()
+    fun getMovieList(keyWord: String, Success: (ArrayList<Movie.Items>) -> Unit, Failure: (Throwable) -> Unit)
+    fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (Throwable) -> Unit)
 }
--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepositoryImpl.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.project.architecturestudy.data.repository
 
 import android.content.Context
-import android.util.Log
 import com.project.architecturestudy.adapters.SearchAdapter
+import com.project.architecturestudy.data.model.Movie
 import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSource
+import com.project.architecturestudy.data.source.local.room.MovieLocal
 import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSource
 
 object NaverMovieRepositoryImpl : NaverMovieRepository {
@@ -24,33 +25,24 @@ object NaverMovieRepositoryImpl : NaverMovieRepository {
         this.naverMovieRemoteDataSource = naverMovieRemoteDataSource
     }
 
-    override fun getCashedMovieList() {
-        naverMovieLocalDataSource.getMovieList(context,
-            Success = { items ->
-                adapter.setLocalMovieData(items)
-                Log.d("bsjbsj", "RoomDataBase LoadData Success")
-            },
-            Failure = {
-                Log.d("bsjbsj", "RoomDataBase LoadData Failure : $it")
-            })
+    override fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (Throwable) -> Unit) {
+        naverMovieLocalDataSource.getMovieList(context, Success, Failure)
     }
 
     override fun getMovieList(
         keyWord: String,
-        SuccessMsg: () -> Unit,
-        FailureMsg: () -> Unit
+        Success: (ArrayList<Movie.Items>) -> Unit,
+        Failure: (Throwable) -> Unit
     ) {
-        naverMovieRemoteDataSource.getMovieList(keyWord,
-            Success = { items ->
-                adapter.setRemoteMovieData(items)
-                naverMovieLocalDataSource.saveMovieList(items, context)
-                SuccessMsg.invoke()
-            },
-            Failure = {
-                Log.d("bsjbsj", "Failure : $it")
-                FailureMsg.invoke()
-            }
-        )
+        naverMovieRemoteDataSource.getMovieList(keyWord, Success, Failure)
     }
-
+//    Success = { items ->
+//        adapter.setRemoteMovieData(items)
+//        naverMovieLocalDataSource.saveMovieList(items, context)
+//        Success.invoke()
+//    },
+//    Failure = {
+//        Log.d("bsjbsj", "Failure : $it")
+//        Failure.invoke()
+//    }
 }
