9a6f1df6a14b3df672c03e38894d9b3fc77b1f53
developer-sungjae <dunkey2615@naver.com>
2020-05-18 20:43:21 +0900

    edit impl

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
@@ -2,17 +2,17 @@ package com.project.architecturestudy.data.repository
 
 import android.content.Context
 import com.project.architecturestudy.data.model.Movie
-import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSource
+import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSourceImpl
 import com.project.architecturestudy.data.source.local.room.MovieLocal
-import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSource
+import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSourceImpl
 
 interface NaverMovieRepository {
 
     var context: Context
-    var naverMovieLocalDataSource: NaverMovieLocalDataSource
-    var naverMovieRemoteDataSource: NaverMovieRemoteDataSource
+    var naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl
+    var naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl
 
-    fun setRepository(context: Context, naverMovieLocalDataSource: NaverMovieLocalDataSource, naverMovieRemoteDataSource: NaverMovieRemoteDataSource)
+    fun setRepository(context: Context, naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl, naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl)
     fun getMovieList(keyWord: String, Success: (ArrayList<Movie.Items>) -> Unit, Failure: (Throwable) -> Unit)
     fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (Throwable) -> Unit)
 }
--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepositoryImpl.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepositoryImpl.kt
@@ -2,24 +2,24 @@ package com.project.architecturestudy.data.repository
 
 import android.content.Context
 import com.project.architecturestudy.data.model.Movie
-import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSource
+import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSourceImpl
 import com.project.architecturestudy.data.source.local.room.MovieLocal
-import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSource
+import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSourceImpl
 
 object NaverMovieRepositoryImpl : NaverMovieRepository {
 
     override lateinit var context: Context
-    override lateinit var naverMovieLocalDataSource: NaverMovieLocalDataSource
-    override lateinit var naverMovieRemoteDataSource: NaverMovieRemoteDataSource
-
+    override lateinit var naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl
+    override lateinit var naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl
     override fun setRepository(
         context: Context,
-        naverMovieLocalDataSource: NaverMovieLocalDataSource,
-        naverMovieRemoteDataSource: NaverMovieRemoteDataSource
+        naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl,
+        naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl
     ) {
         this.context = context
         this.naverMovieLocalDataSource = naverMovieLocalDataSource
         this.naverMovieRemoteDataSource = naverMovieRemoteDataSource
+
     }
 
     override fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (Throwable) -> Unit) {
