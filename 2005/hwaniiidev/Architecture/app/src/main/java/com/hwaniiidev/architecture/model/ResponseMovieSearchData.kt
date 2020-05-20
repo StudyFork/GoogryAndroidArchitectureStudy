bb9bee9c3e5dff5b35aaac5081942d38b2c391cf
hwaniiidev <hwaniiidev@gmail.com>
2020-05-20 09:15:09 +0900

    fix: cache데이터에 검색한 키워드도 포함시키기 위해 data class 수정

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/model/ResponseMovieSearchData.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/model/ResponseMovieSearchData.kt
@@ -1,6 +1,7 @@
 package com.hwaniiidev.architecture.model
 
 data class ResponseMovieSearchData(
+    var query : String,
     val display: Int,
     val items: ArrayList<Item>,
     val lastBuildDate: String,
