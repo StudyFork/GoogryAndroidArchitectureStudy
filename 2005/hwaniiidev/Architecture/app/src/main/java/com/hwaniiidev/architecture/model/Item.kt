0468c02f79524216844b1f975bf412f925e32b54
hwaniiidev <hwaniiidev@gmail.com>
2020-05-20 09:16:41 +0900

    fix: room을 이용하여 local에 영화조회한 데이터 저장을 위해 data class수정

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/model/Item.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/model/Item.kt
@@ -6,13 +6,14 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "movies")
 data class Item(
-    @PrimaryKey(autoGenerate = true) val id : Int,
-    @ColumnInfo(name = "actor")val actor: String,
-    @ColumnInfo(name = "director")val director: String,
-    @ColumnInfo(name = "image")val image: String,
-    @ColumnInfo(name = "link")val link: String,
-    @ColumnInfo(name = "pub_date")val pubDate: String,
-    @ColumnInfo(name = "subtitle")val subtitle: String,
-    @ColumnInfo(name = "title")val title: String,
-    @ColumnInfo(name = "user_rating")val userRating: String
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "query") var query: String,
+    @ColumnInfo(name = "actor") val actor: String,
+    @ColumnInfo(name = "director") val director: String,
+    @ColumnInfo(name = "image") val image: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "pub_date") val pubDate: String,
+    @ColumnInfo(name = "subtitle") val subtitle: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "user_rating") val userRating: String
 )
