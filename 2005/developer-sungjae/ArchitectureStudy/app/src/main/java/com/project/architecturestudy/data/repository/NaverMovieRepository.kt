e38f8cb0907b9be7205228de549ee7f32fbf1e8e
developer-sungjae <dunkey2615@naver.com>
2020-05-22 23:15:13 +0900

    delete abstract properties & setRepository method

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/repository/NaverMovieRepository.kt
@@ -1,30 +1,17 @@
 package com.project.architecturestudy.data.repository
 
-import android.content.Context
 import com.project.architecturestudy.data.model.Movie
-import com.project.architecturestudy.data.source.local.NaverMovieLocalDataSourceImpl
 import com.project.architecturestudy.data.source.local.room.MovieLocal
-import com.project.architecturestudy.data.source.remote.NaverMovieRemoteDataSourceImpl
 
 interface NaverMovieRepository {
-
-    var context: Context
-    var naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl
-    var naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl
-
-    fun setRepository(
-        context: Context,
-        naverMovieLocalDataSource: NaverMovieLocalDataSourceImpl,
-        naverMovieRemoteDataSource: NaverMovieRemoteDataSourceImpl
-    )
 
     fun getMovieList(
         keyWord: String,
         Success: (ArrayList<Movie.Items>) -> Unit,
-        Failure: (t : Throwable) -> Unit
+        Failure: (t: Throwable) -> Unit
     )
 
-    fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (t : Throwable) -> Unit)
+    fun getCashedMovieList(Success: (ArrayList<MovieLocal>) -> Unit, Failure: (t: Throwable) -> Unit)
     fun saveMovieListToLocal(items: ArrayList<Movie.Items>)
     fun dispose()
 
