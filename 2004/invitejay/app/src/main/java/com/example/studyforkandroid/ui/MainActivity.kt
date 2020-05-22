d9aa67ac33d4b0b96ba2ec202f324bf08f8ee15d
invitejay <sksskdi122345@gmail.com>
2020-05-22 14:04:08 +0900

    HW-6 add AAC ViewModel

--- 2004/invitejay/app/src/main/java/com/example/studyforkandroid/ui/MainActivity.kt
+++ 2004/invitejay/app/src/main/java/com/example/studyforkandroid/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.studyforkandroid.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.studyforkandroid.R
 import com.example.studyforkandroid.adapter.MovieAdapter
 import com.example.studyforkandroid.data.source.MovieRepositoryImpl
@@ -12,11 +14,15 @@ import com.example.studyforkandroid.viewmodel.MovieViewModel
 class MainActivity : AppCompatActivity() {
 
     private val rvAdapter = MovieAdapter()
-    private val viewModel = MovieViewModel(MovieRepositoryImpl)
     lateinit var binding: ActivityMainBinding
-
+    lateinit var viewModel: MovieViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return MovieViewModel(MovieRepositoryImpl) as T
+            }
+        }).get(MovieViewModel::class.java)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.apply {
             vm = viewModel
