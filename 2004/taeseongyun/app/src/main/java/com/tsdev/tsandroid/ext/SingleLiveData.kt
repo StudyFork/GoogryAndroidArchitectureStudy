d0cd1bd39df61e5b189ceb2e612e7e238189648e
TaeseongYun <yts0646@gmail.com>
2020-05-23 01:23:15 +0900

    HW-6 remove: conflicts file

---
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/ext/SingleLiveData.kt
@@ -0,0 +1,52 @@
+package com.tsdev.tsandroid.ext
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+typealias SingleEventLiveData<VALUE> = LiveData<Event<VALUE>>
+typealias SingleEventMutableLiveData<VALUE> = MutableLiveData<Event<VALUE>>
+
+open class Event<out VALUE>(private val content: VALUE) {
+    var _isPending = false
+        private set
+
+    fun getValueSingleData(): VALUE? {
+        return if (_isPending) {
+            null
+        }
+        else {
+            content
+        }
+    }
+
+    fun peekContent(): VALUE = content
+}
+
+class ObserverEvent<VALUE>(private val onCheckedData: (VALUE) -> Unit) : Observer<Event<VALUE>> {
+    override fun onChanged(t: Event<VALUE>?) {
+        t?.getValueSingleData()?.let(onCheckedData)
+    }
+}
+
+inline fun <T> SingleEventLiveData<T>.observer(
+    lifecycleOwner: LifecycleOwner,
+    crossinline observer: (T) -> Unit
+) {
+    observe(lifecycleOwner, ObserverEvent { observer(it) })
+}
+
+class SingleMutableLiveData<VALUE> : SingleEventMutableLiveData<VALUE>() {
+    var event: VALUE?
+        set(value) {
+            value?.let { setValue(Event(value)) }
+        }
+        get() = value?.peekContent()
+
+    fun postEvent(value: VALUE?) {
+        value?.let {
+            postValue(Event(it))
+        }
+    }
+}
