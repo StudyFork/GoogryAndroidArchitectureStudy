5158a00b1db98aed310207a41f35e4a9e469e7b5
LeeChungWoo92 <skcjddn@gmail.com>
2020-05-23 15:27:29 +0900

    HW-2 NaverMovieRepositoryImpl 수정

--- 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/data/repository/NaverMovieRepositoryImpl.kt
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/data/repository/NaverMovieRepositoryImpl.kt
@@ -1,16 +1,17 @@
 package com.lllccww.studyforkproject.data.repository
 
 import com.lllccww.studyforkproject.data.model.MovieItem
+import com.lllccww.studyforkproject.data.source.remote.MovieRemoteDataSource
 import com.lllccww.studyforkproject.data.source.remote.MovieRemoteDataSourceImpl
 
-class NaverMovieRepositoryImpl(private val movieRepositoryImpl: MovieRemoteDataSourceImpl) : NaverMovieRepository {
+class NaverMovieRepositoryImpl(private val movieRemoteDataSource: MovieRemoteDataSource) : NaverMovieRepository {
 
     override fun getSearchMovie(
         keyWord: String,
         success: (List<MovieItem>) -> Unit,
         failure: (Throwable) -> Unit
     ) {
-        movieRepositoryImpl.getMovieList(
+        movieRemoteDataSource.getMovieList(
             keyWord,
             success,
             failure
