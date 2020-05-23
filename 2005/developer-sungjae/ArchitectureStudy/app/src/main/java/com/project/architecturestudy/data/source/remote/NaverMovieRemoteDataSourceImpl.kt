0662a71ea2002eaa6cc8571e8f526d2cee08b1c6
developer-sungjae <dunkey2615@naver.com>
2020-05-23 12:53:03 +0900

    change to Service

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/data/source/remote/NaverMovieRemoteDataSourceImpl.kt
@@ -1,13 +1,15 @@
 package com.project.architecturestudy.data.source.remote
 
-import com.project.architecturestudy.components.RetrofitService
+import com.project.architecturestudy.components.Service
 import com.project.architecturestudy.data.model.NaverApiData
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class NaverMovieRemoteDataSourceImpl : NaverMovieRemoteDataSource {
 
-    override val service = RetrofitService.create()
+    override val service = Service.create()
     override val disposable = CompositeDisposable()
 
     override fun getMovieList(
@@ -15,6 +17,10 @@ class NaverMovieRemoteDataSourceImpl : NaverMovieRemoteDataSource {
         Success: (Single<NaverApiData>) -> Unit,
         Failure: (t: Throwable) -> Unit
     ) {
-        Success.invoke(service.getMovies(keyWord))
+        Success.invoke(
+            service.getMovies(keyWord)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+        )
     }
 }
