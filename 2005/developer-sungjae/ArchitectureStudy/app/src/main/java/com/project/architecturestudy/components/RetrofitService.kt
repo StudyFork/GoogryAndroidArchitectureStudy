91bfd93ea1f549774d5607d6c6b0dd4b45dbaa2c
developer-sungjae <dunkey2615@naver.com>
2020-05-23 12:53:30 +0900

    change data class

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/components/RetrofitService.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/components/RetrofitService.kt
@@ -9,7 +9,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-object ServiceFactory {
+object Service {
 
     fun create(): NaverApiService {
 
