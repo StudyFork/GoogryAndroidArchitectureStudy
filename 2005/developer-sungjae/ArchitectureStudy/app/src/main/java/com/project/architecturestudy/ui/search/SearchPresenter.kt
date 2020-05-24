c13f31e3164e1cb4a7eb262bd3e8457608be31ca
developer-sungjae <dunkey2615@naver.com>
2020-05-24 19:23:54 +0900

    split and add it presenter about searching logic(business logic) in Presenter from Activity

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchPresenter.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchPresenter.kt
@@ -1,18 +1,56 @@
 package com.project.architecturestudy.ui.search
 
+import android.util.Log
 import com.project.architecturestudy.data.repository.NaverMovieRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class SearchPresenter(
     private val view: SearchContract.View,
     private val naverMovieRepository: NaverMovieRepository
 ) : SearchContract.Presenter {
 
+    override val adapter = SearchAdapter()
+
     override fun validateSearchWord(searchWord: String) {
+        when (searchWord.isEmpty()) {
+
+            true -> view.showSearchWordIsEmpty()
+            false -> getMovieListFromRemote(searchWord)
+        }
     }
+
 
     override fun getMovieListFromLocal() {
+        naverMovieRepository.getCashedMovieList(
+            onSuccess = {
+                adapter.setLocalMovieData(it)
+            },
+            onFailure = {
+                Log.d("bsjbsj", "Throwable:$it")
+            })
     }
 
-    override fun getMovieListFromRemote() {
+    override fun getMovieListFromRemote(searchWord: String) {
+        naverMovieRepository.getMovieList(searchWord,
+            onGetRemoteData = { single ->
+                single.observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+
+                            adapter.setRemoteMovieData(it.items)
+                            view.showRemoteDataSuccess()
+                        }, { t ->
+
+                            view.showRemoteDataFailure()
+                            Log.d("bsjbsj", t.toString())
+                        })
+
+            })
+    }
+
+    override fun remoteDispose() {
+        naverMovieRepository.dispose()
     }
 }
