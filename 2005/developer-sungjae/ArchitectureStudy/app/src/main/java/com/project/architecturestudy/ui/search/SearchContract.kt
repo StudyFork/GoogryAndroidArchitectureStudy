73ba59fc0901a1a71f820a52732c0c935a3fb808
developer-sungjae <dunkey2615@naver.com>
2020-05-24 18:30:35 +0900

    declare SearchPresenter, SearchContract simply (+change tools package simply xml)

---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchContract.kt
@@ -0,0 +1,12 @@
+package com.project.architecturestudy.ui.search
+
+interface SearchContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchPresenter.kt
@@ -0,0 +1,6 @@
+package com.project.architecturestudy.ui.search
+
+class SearchPresenter {
+
+
+}
