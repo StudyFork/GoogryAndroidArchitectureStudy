78d85fdeddfc2622c0e6c347f6eacb320075cda8
developer-sungjae <dunkey2615@naver.com>
2020-05-24 19:01:44 +0900

    define simply SearchContract

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchContract.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchContract.kt
@@ -4,9 +4,17 @@ interface SearchContract {
 
     interface View {
 
+        fun showSearchWordIsEmpty(emptyMsg: String)
+        fun showLocalDataSuccess(successMsg: String)
+        fun showLocalDataFailure(failureMsg: String)
+        fun showRemoteDataSuccess(successMsg: String)
+        fun showRemoteDataFailure(failureMsg: String)
     }
 
     interface Presenter {
 
+        fun validateSearchWord(searchWord: String)
+        fun getMovieListFromLocal()
+        fun getMovieListFromRemote()
     }
 }
