173b1e13256ac49fec4cb1ef36e2f67e37bbd4e8
hwaniiidev <hwaniiidev@gmail.com>
2020-05-25 22:54:03 +0900

    feat: contract로 view 와 presenter연동

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainActivity.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainActivity.kt
@@ -11,13 +11,14 @@ import com.hwaniiidev.architecture.data.repository.NaverMovieRepositoryImpl
 import com.hwaniiidev.architecture.model.Item
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , MainContract.View{
 
     private val TAG = MainActivity::class.java.simpleName
 
     lateinit var adapterMovieList: AdapterMovieList
 
     private val naverMovieRepositoryImpl = NaverMovieRepositoryImpl(this)
+    private val mainPresenter = MainPresenter(this,naverMovieRepositoryImpl)
 
     lateinit private var imm: InputMethodManager
 
@@ -31,8 +32,9 @@ class MainActivity : AppCompatActivity() {
 
         btn_search.setOnClickListener {
             val searchValue = edit_search_title.text.toString()
+            mainPresenter.searchMovies(searchValue)
 
-            if (edit_search_title.text.isEmpty()) {       //검색어 입력하지 않았을 때
+            /*if (edit_search_title.text.isEmpty()) {       //검색어 입력하지 않았을 때
                 Log.d(TAG, "없음")
                 toast("검색어를 입력해주세요.")
             } else {
@@ -48,13 +50,13 @@ class MainActivity : AppCompatActivity() {
                             updateList(response.items)
                             text_plz_search.visibility = View.GONE
 
-                            /*//Local DB에 저장
+                            //Local DB에 저장
                             if (!this.isFinishing && !this.isDestroyed){
                                 naverMovieRepositoryImpl.cachingMovies(
                                     query = response.query,
                                     movies = response.items
                                 )
-                            }*/
+                            }
 
                         }
                     },
@@ -67,7 +69,7 @@ class MainActivity : AppCompatActivity() {
                         Log.d(TAG, t.toString())
                     }
                 )
-            }
+            }*/
         }
     }
 
@@ -76,9 +78,9 @@ class MainActivity : AppCompatActivity() {
         recyclerview_search_list.adapter = adapterMovieList
     }
 
-    private fun updateList(items: List<Item>) {
+    /*private fun updateList(items: List<Item>) {
         adapterMovieList.addItem(items)
-    }
+    }*/
 
     private fun toast(message: String) {
         Toast.makeText(
@@ -88,7 +90,30 @@ class MainActivity : AppCompatActivity() {
         ).show()
     }
 
-    private fun hideKeyboard() {
+    override fun showQueryIsEmpty() {
+        toast("검색어를 입력해주세요.")
+        Log.d(TAG, "없음")
+    }
+
+    override fun hideKeyBoard() {
         imm.hideSoftInputFromWindow(edit_search_title.windowToken, 0)
+    }
+
+    override fun showResponseIsNone() {
+        text_plz_search.text = "검색결과가 없습니다.\n다른 검색어을 입력해주세요."
+        text_plz_search.visibility = View.VISIBLE
+    }
+
+    override fun showResponseError() {
+        toast("다시 시도해주세요.")
+    }
+
+    override fun showNetworkFailure() {
+        toast("네트워크에 연결할 수 없습니다. 네트워크 연결을 확인해주세요.")
+    }
+
+    override fun showMoviesList(items: List<Item>) {
+        adapterMovieList.addItem(items)
+        text_plz_search.visibility = View.GONE
     }
 }
