ba6f54e576b7902dbb1ab2cde574f0678c8716f5
sangjinKIM93 <larger93r@gmail.com>
2020-05-26 13:36:16 +0900

    MovieListContract 생성

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListContract.kt
@@ -0,0 +1,21 @@
+package com.sangjin.newproject
+
+import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.data.model.NaverMovieResponse
+import io.reactivex.Single
+
+interface MovieListContract{
+    interface View {
+        fun noKeyword()
+        fun showMovieList(movies: List<Movie>)
+        fun noResult()
+        fun onError(t: Throwable)
+    }
+
+    interface Presenter {
+        fun loadCache()
+        fun searchMovie(keyword: String)
+        fun checkMovieResult(movies: List<Movie>)
+        fun clearDisposable()
+    }
+}
