3572064edda00b650a6e9c82eca86eadf6b1336f
hwaniiidev <hwaniiidev@gmail.com>
2020-05-27 08:13:12 +0900

    feat: repository inteface에 local에서 영화리스트 조회하는 기능 추가

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepository.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/data/repository/NaverMovieRepository.kt
@@ -5,16 +5,44 @@ import com.hwaniiidev.architecture.model.Item
 import com.hwaniiidev.architecture.model.ResponseMovieSearchData
 
 interface NaverMovieRepository {
+
+    /**
+     * 영화 검색.
+     * 로컬에서 먼저 검색 후 로컬에 결과가 있으면 로컬 데이터를 바로 출력
+     * 없으면 서버에서 검색
+     */
+    fun searchMovies(
+        query: String,
+        onSuccess: (response: ResponseMovieSearchData) -> Unit,
+        onError: (errorMessage: String) -> Unit,
+        onFailure: (t: Throwable) -> Unit,
+        onCached: (movies: List<Item>?) -> Unit
+    )
+
+    /**
+     * 서버에서 영화검색
+     * 검색 후 로컬 DB에 중복되지 않게 저장
+     */
     fun getRemoteMovies(
         query: String,
-        //onSuccess : (movies : ArrayList<Item>) -> Unit,
         onSuccess: (response: ResponseMovieSearchData) -> Unit,
         onError: (errorMessage: String) -> Unit,
         onFailure: (t: Throwable) -> Unit
     )
 
+    /**
+     * 로컬 DB에 서버에서 검색한 결과 저장
+     */
     fun cachingMovies(
         query: String,
         movies: ArrayList<Item>
+    )
+
+    /**
+     * 로컬 DB에서 검색 결과 조회
+     */
+    fun getCachedMovies(
+        query: String,
+        onCached: (movies: List<Item>?) -> Unit
     )
 }
