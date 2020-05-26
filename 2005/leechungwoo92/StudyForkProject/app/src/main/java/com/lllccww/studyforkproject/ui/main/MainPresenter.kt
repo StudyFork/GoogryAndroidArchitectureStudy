198c5813f33ca58c2cabb7114974b3abcb94c6f4
LeeChungWoo92 <skcjddn@gmail.com>
2020-05-26 17:40:13 +0900

    HW-3 MainPresenter 수정

--- 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainPresenter.kt
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainPresenter.kt
@@ -1,4 +1,33 @@
 package com.lllccww.studyforkproject.ui.main
 
-class MainPresenter {
+import com.lllccww.studyforkproject.data.repository.NaverMovieRepositoryImpl
+
+class MainPresenter(
+    private val naverMovieRepository: NaverMovieRepositoryImpl,
+    private val view: MainContract.View
+) : MainContract.Presenter {
+
+    override fun getSearchMovie(query: String) {
+
+        if (query.isNullOrEmpty()) {
+            view.showMovieEmptySearchQuery()
+            return
+
+        }
+
+        naverMovieRepository.getSearchMovie(
+            query,
+            success = { movieItem ->
+                if (movieItem.isNullOrEmpty()) {
+                    view.showMovieNoResult()
+                } else {
+                    view.showMovieList(movieItem)
+                }
+            },
+            failure = {
+                view.showFailGetData(it.message.toString())
+            }
+        )
+
+    }
 }
