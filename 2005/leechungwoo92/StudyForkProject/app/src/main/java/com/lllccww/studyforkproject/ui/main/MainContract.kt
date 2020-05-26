e569e32133ff3a02b0589ea193ef8c957612592e
LeeChungWoo92 <skcjddn@gmail.com>
2020-05-26 16:08:51 +0900

    HW-3 Maincontract 내부에 View,Presenter interface 추가

--- 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainContract.kt
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainContract.kt
@@ -1,4 +1,21 @@
 package com.lllccww.studyforkproject.ui.main
 
+import android.content.Context
+import android.widget.Toast
+import com.lllccww.studyforkproject.data.model.MovieItem
+
 interface MainContract {
+
+    interface View {
+        fun showLoading()
+        fun hideLoading()
+        fun closeKeyboard()
+        fun showToastMessage(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    interface Presenter {
+        fun getSearchMovie(movieData: List<MovieItem>)
+    }
 }
