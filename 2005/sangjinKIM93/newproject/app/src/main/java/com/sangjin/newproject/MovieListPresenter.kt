2a55dd870f3643c158bff3ee92dd28988673736d
sangjinKIM93 <larger93r@gmail.com>
2020-05-26 13:41:19 +0900

    MovieListPresenter 생성

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MovieListPresenter.kt
@@ -0,0 +1,65 @@
+package com.sangjin.newproject
+
+import android.text.TextUtils
+import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.data.repository.NaverMoviesRepository
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MovieListPresenter(
+    private val view: MovieListContract.View,
+    private val repository: NaverMoviesRepository
+) : MovieListContract.Presenter {
+
+    private val disposables = CompositeDisposable()
+
+
+    override fun loadCache() {
+        repository.loadCachedMovies()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                view.showMovieList(it)
+            },
+                {
+
+                }).let {
+                disposables.add(it)
+            }
+    }
+
+
+    override fun searchMovie(keyWord: String) {
+        if (TextUtils.isEmpty(keyWord)) {
+            view.noKeyword()
+        } else {
+            repository.getNaverMovies(keyWord)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    checkMovieResult(it.items)
+                },
+                    {
+                        view.onError(it)
+                    }).let {
+                    disposables.add(it)
+                }
+        }
+    }
+
+
+    override fun checkMovieResult(movies: List<Movie>) {
+        if (movies.isNullOrEmpty()) {
+            view.noResult()
+        } else {
+            view.showMovieList(movies)
+        }
+    }
+
+
+    override fun clearDisposable() {
+        disposables.clear()
+    }
+}
