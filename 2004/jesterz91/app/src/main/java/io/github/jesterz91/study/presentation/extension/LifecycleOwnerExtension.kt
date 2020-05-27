cccce09f1ff08e2de45370c18fe49ff1f48f2702
jesterz91 <jesterz91@naver.com>
2020-05-27 14:15:52 +0900

    HW-6 AAC ViewModel and LiveData

---
+++ 2004/jesterz91/app/src/main/java/io/github/jesterz91/study/presentation/extension/LifecycleOwnerExtension.kt
@@ -0,0 +1,9 @@
+package io.github.jesterz91.study.presentation.extension
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, onChanged: (T) -> Unit) {
+    liveData.observe(this, Observer(onChanged::invoke))
+}
