5f10d77369f765cf643274dac0853da827a24406
kim chul <chuuuul@naver.com>
2020-05-28 22:53:26 +0900

    HW-3 리팩토링 : Contract에 선언된 메서드의 return 타입 제거

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/adapter/MovieAdapter.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/adapter/MovieAdapter.kt
@@ -4,42 +4,84 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.architecture.R
+import com.example.architecture.data.model.MovieModel
+import com.example.architecture.util.ConstValue.Companion.NO_IMAGE_URL
+import kotlinx.android.synthetic.main.item_movie.view.*
 
-class MovieAdapter : RecyclerView.Adapter<MovieHolder>(), MovieAdapterContract.View {
+class MovieAdapter :  RecyclerView.Adapter<MovieViewHolder>() {
 
-    val movieAdapterPresenter = MovieAdapterPresenter(this)
+    private val movieList = mutableListOf<MovieModel>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
-        val viewHolder = MovieHolder(view)
+        val viewHolder = MovieViewHolder(view)
 
         view.setOnClickListener {
             movieAdapterPresenter.openWebPage(it.context, viewHolder.layoutPosition)
+            openWebPage(it.context, movieList[viewHolder.layoutPosition].link)
         }
 
         return viewHolder
     }
 
-    override fun showMovieList() {
-        notifyDataSetChanged()
-    }
-
-    override fun showMovieWebPage(context: Context, link: String) {
+    private fun openWebPage(context: Context, link: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
         context.startActivity(intent)
     }
 
     override fun getItemCount(): Int {
-        return movieAdapterPresenter.getMovieCount()
+        return movieList.count()
     }
 
-    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
-        movieAdapterPresenter.setMovieElement(holder, position)
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+
+        val movie = movieList[position]
+        holder.onBind( movie )
+    }
+
+    fun addNewItems(movieList: List<MovieModel>) {
+        if (this.movieList.isNotEmpty()) {
+            this.movieList.clear()
+        }
+
+        this.movieList.addAll(movieList)
+        notifyDataSetChanged()
     }
 }
 
+class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view)
+{
+    fun onBind(movie : MovieModel)
+    {
+        itemView.tv_movie_title.text = removeMarkupTag(movie.title)
+        itemView.tv_movie_pubDate.text = movie.pubDate
 
+        itemView.ratingBar_movie_rating.rating = movie.userRating
+
+        setMovieImage(itemView.img_movie_Image, movie.image)
+
+    }
+
+    private fun removeMarkupTag(html: String): String {
+        return html.replace("<b>", "").replace("</b>", "")
+    }
+
+    private fun setMovieImage(imageView: ImageView, imageUrl: String) {
+        val url = if (imageUrl.isBlank()) {
+            NO_IMAGE_URL
+        } else {
+            imageUrl
+        }
+        Glide.with(imageView.context)
+            .load(url).placeholder(R.drawable.ic_loading_black_24dp)
+            .error(R.drawable.image_loaderror).centerCrop().into(imageView)
+    }
+
+}
