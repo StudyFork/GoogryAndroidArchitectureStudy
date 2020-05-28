9fd58ddada43b6ca71441bccb3da0f8ff83c64ea
LeeChungWoo92 <skcjddn@gmail.com>
2020-05-28 16:30:37 +0900

    HW-3 MainContract View에서 BaseContract.View 상속

--- 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainContract.kt
+++ 2005/leechungwoo92/StudyForkProject/app/src/main/java/com/lllccww/studyforkproject/ui/main/MainContract.kt
@@ -1,16 +1,17 @@
 package com.lllccww.studyforkproject.ui.main
 
 import com.lllccww.studyforkproject.data.model.MovieItem
+import com.lllccww.studyforkproject.ui.common.BaseContract
 
 interface MainContract {
 
-    interface View {
+    interface View : BaseContract.View {
         fun showMovieList(items: List<MovieItem>)
         fun showFailGetData(msg: String)
         fun showMovieNoResult()
         fun showMovieEmptySearchQuery()
         fun hideKeyboard()
-        
+
     }
 
     interface Presenter {
