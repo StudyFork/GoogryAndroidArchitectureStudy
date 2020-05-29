dff4c56de7a47eac9a8bde57aecb8911bfb3c094
jesterz91 <jesterz91@naver.com>
2020-05-29 22:28:28 +0900

    HW-6 apply Mentor Feedback

--- 2004/jesterz91/app/src/main/java/io/github/jesterz91/study/presentation/ui/MovieSearchAdapter.kt
+++ 2004/jesterz91/app/src/main/java/io/github/jesterz91/study/presentation/ui/MovieSearchAdapter.kt
@@ -7,6 +7,10 @@ import io.github.jesterz91.study.presentation.common.BaseAdapter
 
 class MovieSearchAdapter : BaseAdapter<Movie, MovieSearchViewHolder>(DIFF_CALLBACK) {
 
+    override fun createViewHolder(layoutInflater: LayoutInflater): MovieSearchViewHolder {
+        return MovieSearchViewHolder(layoutInflater)
+    }
+
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Movie>() {
             override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
@@ -17,9 +21,5 @@ class MovieSearchAdapter : BaseAdapter<Movie, MovieSearchViewHolder>(DIFF_CALLBA
                 return oldItem == newItem
             }
         }
-    }
-
-    override fun createViewHolder(layoutInflater: LayoutInflater): MovieSearchViewHolder {
-        return MovieSearchViewHolder(layoutInflater)
     }
 }
