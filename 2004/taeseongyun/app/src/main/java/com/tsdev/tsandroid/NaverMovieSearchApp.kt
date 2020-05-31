0301b8a83bb0ce7fd2a605079f305253e0ea922e
TaeseongYun <yts0646@gmail.com>
2020-05-31 15:04:42 +0900

    Feat: di package, Application Class

---
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/NaverMovieSearchApp.kt
@@ -0,0 +1,10 @@
+package com.tsdev.tsandroid
+
+import android.app.Application
+
+class NaverMovieSearchApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ 2004/taeseongyun/app/src/main/java/com/tsdev/tsandroid/di/NetworkModule.kt
@@ -0,0 +1,12 @@
+package com.tsdev.tsandroid.di
+
+import org.koin.dsl.module
+
+/***
+ * single :
+ * factory :
+ * viewModel :
+ ***/
+val networkModule = module {
+    single {  }
+}
