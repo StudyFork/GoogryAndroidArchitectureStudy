f670eb7640d84df3f552cff47610052c63c62fbf
Ryu <bbs0605@naver.com>
2020-06-01 22:52:10 +0900

    검색결과에 DataBinding 적용

--- 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/main/MovieAdapter.kt
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/main/MovieAdapter.kt
@@ -4,19 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import kotlinx.android.synthetic.main.row_content.view.*
 import r.test.rapp.R
 import r.test.rapp.data.model.Item
-import r.test.rapp.networks.ImageLoader
+import r.test.rapp.databinding.RowContentBinding
 
 /**
  * 리스트 뷰의 커스텀 어댑터.
  */
 class MovieAdapter : BaseAdapter() {
     private val movieList: ArrayList<Item> = ArrayList()
-    private val imgLoader: ImageLoader = ImageLoader()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val inflater: LayoutInflater = LayoutInflater.from(parent?.context)
@@ -30,10 +26,9 @@ class MovieAdapter : BaseAdapter() {
 
         val item = movieList[position]
 
-        holder.txtTitle.text = item.getHtmlTitle()
-        imgLoader.load(item.image, holder.ivThumbnail)
+        holder.binding.movie = item
 
-        return rowView
+        return holder.binding.root
     }
 
     fun getMovieList(): ArrayList<Item> {
@@ -41,9 +36,7 @@ class MovieAdapter : BaseAdapter() {
     }
 
     private class ViewHolder(view: View) {
-        val txtTitle: TextView = view.txt_title
-        val ivThumbnail: ImageView = view.iv_thumbnail
-
+        val binding: RowContentBinding = RowContentBinding.bind(view)
     }
 
     override fun getItem(position: Int): Item {
