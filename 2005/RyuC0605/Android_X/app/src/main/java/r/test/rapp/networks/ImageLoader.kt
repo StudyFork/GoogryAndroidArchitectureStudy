7369b37f93b5030a951545b737f1237fe30b14dc
Ryu <bbs0605@naver.com>
2020-06-01 22:02:15 +0900

    검색 결과 목록에 DataBinding 적용

--- 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/networks/ImageLoader.kt
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/networks/ImageLoader.kt
@@ -1,15 +1,21 @@
 package r.test.rapp.networks
 
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import r.test.rapp.R
 
+
 class ImageLoader {
-    fun load(url: String, iv: ImageView) {
-        Glide.with(iv.context)
-            .load(url)
-            .placeholder(R.drawable.no_image)
-            .centerCrop()
-            .into(iv)
+    companion object {
+        @BindingAdapter("thumbnail")
+        @JvmStatic
+        fun load(iv: ImageView, url: String?) {
+            Glide.with(iv)
+                .load(url)
+                .placeholder(R.drawable.no_image)
+                .centerCrop()
+                .into(iv)
+        }
     }
 }
