4893e1d83543c866c6f30100c8c338a32f78bacb
sangjinKIM93 <larger93r@gmail.com>
2020-06-02 15:21:26 +0900

    bindAdapter htmltoSpanned() 매개변수 변경
    기존 : item:Movie -> 변경 : title:String

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/utils/BindingUtils.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/utils/BindingUtils.kt
@@ -7,15 +7,14 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.sangjin.newproject.R
-import com.sangjin.newproject.data.model.Movie
 
 @BindingAdapter("htmlToSpanned")
-fun TextView.htmlToSpanned(item: Movie?) {
-    item?.let {
+fun TextView.htmlToSpanned(title: String?) {
+    title?.let {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            text = Html.fromHtml(it.title, Html.FROM_HTML_MODE_LEGACY)
+            text = Html.fromHtml(it, Html.FROM_HTML_MODE_LEGACY)
         } else {
-            text = Html.fromHtml(it.title)
+            text = Html.fromHtml(it)
         }
     }
 }
