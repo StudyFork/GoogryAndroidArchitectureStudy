a482843375a49c65e41ce83463697b1be2389609
sangjinKIM93 <larger93r@gmail.com>
2020-06-02 14:17:05 +0900

    recyclerview에 기본 data-binding 적용
    *이미지 처리와 spanned 처리는 추후 binding-adpater를 활용하여 적용할 예정

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/adapter/MovieListViewHolder.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/adapter/MovieListViewHolder.kt
@@ -2,7 +2,22 @@ package com.sangjin.newproject.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sangjin.newproject.R
+import com.sangjin.newproject.data.model.Movie
+import com.sangjin.newproject.databinding.ItemMovieBinding
 import kotlinx.android.extensions.LayoutContainer
 
-class MovieListViewHolder(override val containerView: View) :
-    RecyclerView.ViewHolder(containerView), LayoutContainer
+class MovieListViewHolder(private val binding: ItemMovieBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Movie) {
+        binding.movie = item
+
+        Glide.with(binding.movieImageIV.context)
+            .load(item.image)
+            .placeholder(R.drawable.img_default)
+            .centerCrop()
+            .into(binding.movieImageIV)
+    }
+}
