84a1d266b5eb51b317e1fd60db3449205b6621dc
chuuuul <chuuuul@naver.com>
2020-06-08 06:18:58 +0900

    HW-5 기능 : 검색 하는 도중에 Progress Bar 표시

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/SearchViewModel.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/activity/search/SearchViewModel.kt
@@ -2,6 +2,7 @@ package com.example.architecture.activity.search
 
 import android.util.Log
 import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import com.example.architecture.data.model.MovieModel
 import com.example.architecture.data.repository.NaverRepositoryImpl
@@ -9,7 +10,7 @@ import com.example.architecture.data.repository.NaverRepositoryImpl
 class SearchViewModel(private val naverRepository: NaverRepositoryImpl) {
 
     val keyword = ObservableField<String>()
-
+    val isLoading = ObservableBoolean(false)
     val movieList = ObservableArrayList<MovieModel>()
     val showMessageEmptyKeyword = ObservableField<Unit>()
     val showMessageEmptyResult = ObservableField<Unit>()
@@ -17,6 +18,7 @@ class SearchViewModel(private val naverRepository: NaverRepositoryImpl) {
     fun searchMovie() {
         keyword.get()?.let { keyword ->
             if (isValidKeyword(keyword)) {
+                playProgressBar()
                 naverRepository.getMovieList(keyword, this::onSuccess, this::onFailure)
             }
         }
@@ -39,10 +41,20 @@ class SearchViewModel(private val naverRepository: NaverRepositoryImpl) {
         } else {
             showMessageEmptyResult.notifyChange()
         }
+        stopProgressBar()
     }
 
     private fun onFailure(t: Throwable) {
         Log.d("chul", "OnFailure : $t")
+        stopProgressBar()
+    }
+
+    private fun playProgressBar() {
+        isLoading.set(true)
+    }
+
+    private fun stopProgressBar() {
+        isLoading.set(false)
     }
 
     private fun clearCacheData(keyword: String) {
