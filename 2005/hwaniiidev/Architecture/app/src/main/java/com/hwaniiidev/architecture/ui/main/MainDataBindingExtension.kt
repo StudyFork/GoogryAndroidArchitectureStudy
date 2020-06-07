7ee7d20edc2a92bd25e62c34a2ef9ac7031a610b
hwaniiidev <hwaniiidev@loplat.com>
2020-06-07 11:28:11 +0900

    fix: databinding활용하여 layout.xml 간소화

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainDataBindingExtension.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainDataBindingExtension.kt
@@ -19,9 +19,33 @@ fun loadImage(view: ImageView, url: String) {
 @BindingAdapter("htmlTitle")
 fun loadHtml(view: TextView, title: String) {
     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-        view.text = Html.fromHtml(title,Html.FROM_HTML_MODE_LEGACY)
+        view.text = "제목 : ${Html.fromHtml(title,Html.FROM_HTML_MODE_LEGACY)}"
     }else{
-        view.text = Html.fromHtml(title)
+        view.text ="제목 : ${ Html.fromHtml(title)}"
     }
+}
 
+@BindingAdapter("loadSubTitle")
+fun loadSubTtitle(view : TextView, subtitle: String){
+    view.text = "서브제목 : ${subtitle}"
+}
+
+@BindingAdapter("loadPubDate")
+fun loadPubDate(view : TextView, pubDate: String){
+    view.text = "개봉년도 : ${pubDate}"
+}
+
+@BindingAdapter("loadDirector")
+fun loadDirector(view : TextView, director: String){
+    view.text = "감독 : ${director}"
+}
+
+@BindingAdapter("loadActor")
+fun loadActor(view : TextView, actor: String){
+    view.text = "개봉년도 : ${actor}"
+}
+
+@BindingAdapter("loadUserRating")
+fun loadUserRating(view : TextView, userRating: String){
+    view.text = "평점 : ${userRating}"
 }
