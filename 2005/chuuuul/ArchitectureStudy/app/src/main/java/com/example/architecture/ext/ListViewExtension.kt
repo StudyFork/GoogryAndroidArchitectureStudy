6eeacf780220d236aec84dd50b21f93ab35dc83a
chuuuul <chuuuul@naver.com>
2020-06-08 02:31:47 +0900

    HW-5 리팩토링 : ListView의 setItems을 BindingAdpater로 구현

---
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/ext/ListViewExtension.kt
@@ -0,0 +1,17 @@
+package com.example.architecture.ext
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.architecture.activity.search.adapter.MovieAdapter
+import com.example.architecture.data.model.MovieModel
+
+
+@BindingAdapter("android:items")
+fun RecyclerView.setItems(items: MutableList<MovieModel>) {
+
+    if (this.adapter is MovieAdapter) {
+        val adapter = this.adapter as MovieAdapter
+        adapter.addNewItems(items)
+    }
+
+}
