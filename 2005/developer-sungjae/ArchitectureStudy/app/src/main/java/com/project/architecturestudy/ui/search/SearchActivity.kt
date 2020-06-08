61f460a8d105d6747dfd30cb4c78c188ff7239fd
developer-sungjae <dunkey2615@naver.com>
2020-06-08 23:59:37 +0900

    Change Activity to MVP -> MVVM
    :delete presenter add binding.vm

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchActivity.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchActivity.kt
@@ -3,12 +3,12 @@ package com.project.architecturestudy.ui.search
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import com.project.architecturestudy.R
 import com.project.architecturestudy.data.model.MovieItem
 import com.project.architecturestudy.data.repository.NaverMovieRepositoryImpl
@@ -18,36 +18,47 @@ import com.project.architecturestudy.databinding.ActivitySearchBinding
 import kotlinx.android.synthetic.main.activity_search.*
 import org.jetbrains.anko.toast
 
-class SearchActivity : AppCompatActivity(), SearchContract.View {
+class SearchActivity : AppCompatActivity() {
 
     private val adapter: SearchAdapter = SearchAdapter()
-    private val presenter by lazy {
+    private val handler by lazy {
+        Handler(Looper.getMainLooper())
+    }
+
+    private val vm by lazy {
         val naverMovieLocalDataSource = NaverMovieLocalDataSourceImpl(this)
         val naverMovieRemoteDataSource = NaverMovieRemoteDataSourceImpl()
-
-        SearchPresenter(
-            this,
-            NaverMovieRepositoryImpl(naverMovieLocalDataSource, naverMovieRemoteDataSource)
-        )
+        val repository = NaverMovieRepositoryImpl(naverMovieLocalDataSource, naverMovieRemoteDataSource)
+        SearchViewModel(repository)
     }
     private lateinit var binding: ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
+        binding.vm = vm
 
-        binding.activity = this@SearchActivity
         setRecyclerView()
         onClickAdapterItem()
-        presenter.getMovieListFromLocal()
+        vm.getMovieListFromLocal()
 
         et_search.doAfterTextChanged {
-            presenter.invokeTextChanged()
+            vm.invokeTextChanged()
         }
-    }
-
-    fun onClickSearchBtn(searchWord: String) {
-        presenter.getMovieListFromRemote(this@SearchActivity, searchWord)
+        vm.showToast.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                val msg = vm.showToast.get().toString()
+                /*for toast*/
+                handler.postDelayed({ toast(msg) }, 0)
+            }
+        })
+//
+//        vm.showMovieData.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+//            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+//                val data = vm.showMovieData.get() as List<MovieItem>
+//                adapter.addMovieData(data)
+//            }
+//        })
     }
 
     private fun onClickAdapterItem() {
@@ -61,37 +72,8 @@ class SearchActivity : AppCompatActivity(), SearchContract.View {
         listview_movie.adapter = adapter
     }
 
-    override fun showMovieData(item: List<MovieItem>) {
-        adapter.setLocalMovieData(item)
-    }
-
-    override fun showSearchKeyWord(result: String?, visibility: Int) {
-        tv_result_text.visibility = visibility
-        tv_result_text.text = result
-    }
-
-    override fun showSearchWordIsEmptyMsg() {
-        toast(getString(R.string.please_write))
-    }
-
-    override fun showLocalDataSuccessMsg() {
-        toast(getString(R.string.get_local_data_success))
-    }
-
-    override fun showLocalDataFailureMsg() {
-        toast(getString(R.string.get_local_data_failure))
-    }
-
-    override fun showRemoteDataSuccessMsg() {
-        toast(getString(R.string.get_data_success))
-    }
-
-    override fun showRemoteDataFailureMsg() {
-        toast(getString(R.string.get_data_failure))
-    }
-
     override fun onDestroy() {
-        presenter.remoteDispose()
+        vm.remoteDispose()
         super.onDestroy()
     }
 }
---
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchViewModel.kt
@@ -0,0 +1,71 @@
+package com.project.architecturestudy.ui.search
+
+import android.util.Log
+import android.view.View
+import androidx.databinding.ObservableField
+import com.project.architecturestudy.R
+import com.project.architecturestudy.components.Constants.customTAG
+import com.project.architecturestudy.components.Strings
+import com.project.architecturestudy.data.model.MovieItem
+import com.project.architecturestudy.data.repository.NaverMovieRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class SearchViewModel(private val repository: NaverMovieRepository) {
+
+    val searchWord = ObservableField<String>()
+    val movieData = ObservableField<List<MovieItem>>()
+    val showToast = ObservableField<String>()
+    val tvResultVisible = ObservableField<Int>()
+
+    init {
+        tvResultVisible.set(View.GONE)
+    }
+
+    fun getMovieListFromLocal() {
+        repository.getCashedMovieList(
+            onSuccess = {
+                Log.d(customTAG, "getLocalData:$it")
+                movieData.set(it.toList())
+                showToast.set(Strings.get(R.string.get_local_data_success))
+            },
+            onFailure = {
+                Log.d(customTAG, "Throwable:$it")
+                showToast.set(Strings.get(R.string.get_local_data_failure))
+            })
+    }
+
+    fun getMovieListFromRemote(searchWord: String) {
+        if (searchWord.isEmpty()) {
+            showToast.set(Strings.get(R.string.please_write))
+            return
+        }
+        this.searchWord.set(searchWord)
+
+        tvResultVisible.set(View.VISIBLE)
+
+        repository.getMovieList(searchWord,
+            onGetRemoteData = { single ->
+                single.observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            Log.d(customTAG, "getRemoteData:$it")
+                            movieData.set(it.items)
+                            showToast.set(Strings.get(R.string.get_data_success))
+                        }, { t ->
+                            showToast.set(Strings.get(R.string.get_data_failure))
+                            Log.d(customTAG, t.toString())
+                        })
+            })
+
+    }
+
+    fun remoteDispose() {
+        repository.dispose()
+    }
+
+    fun invokeTextChanged() {
+        tvResultVisible.set(View.GONE)
+    }
+}
