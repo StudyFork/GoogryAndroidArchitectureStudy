69247c5ef432796d1334765659bb9e264e599300
hwaniiidev <hwaniiidev@gmail.com>
2020-06-09 08:13:00 +0900

    feat: View에Prensenter역할 제외 및 viewModel 추가 하여 데이터 결합

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainActivity.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainActivity.kt
@@ -6,20 +6,21 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import com.hwaniiidev.architecture.R
 import com.hwaniiidev.architecture.data.repository.NaverMovieRepositoryImpl
 import com.hwaniiidev.architecture.databinding.ActivityMainBinding
 import com.hwaniiidev.architecture.model.Item
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity(){
 
     private val TAG = MainActivity::class.java.simpleName
 
-    lateinit var adapterMovieList: AdapterMovieList
+    //lateinit var adapterMovieList: AdapterMovieList
 
     private val naverMovieRepositoryImpl = NaverMovieRepositoryImpl(this)
-    private val mainPresenter = MainPresenter(this, naverMovieRepositoryImpl)
+    //private val mainPresenter = MainPresenter(this, naverMovieRepositoryImpl)
 
     lateinit private var imm: InputMethodManager
     lateinit var binding: ActivityMainBinding
@@ -27,27 +28,27 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val viewModel = MainViewModel(naverMovieRepositoryImpl)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainActivity = this
+        binding.viewModel = viewModel
 
-        initView()
+        //initView()
 
         imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
-        /*btn_search.setOnClickListener {
-            val searchValue = edit_search_title.text.toString()
-            mainPresenter.searchMovies(searchValue)
-        }*/
+
     }
 
-    fun searchMovies(searchValue : String){
+    /*fun searchMovies(searchValue : String){
         mainPresenter.searchMovies(searchValue)
-    }
+    }*/
 
-    private fun initView() {
+    /*private fun initView() {
         adapterMovieList = AdapterMovieList()
         recyclerview_search_list.adapter = adapterMovieList
-    }
+    }*/
 
     private fun toast(message: String) {
         Toast.makeText(
@@ -57,32 +58,33 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         ).show()
     }
 
-    override fun showQueryIsEmpty() {
+    fun showQueryIsEmpty() {
         toast("검색어를 입력해주세요.")
         Log.d(TAG, "없음")
     }
 
-    override fun hideKeyBoard() {
+    fun hideKeyBoard() {
         imm.hideSoftInputFromWindow(edit_search_title.windowToken, 0)
     }
 
-    override fun showResponseIsNone() {
+    fun showResponseIsNone() {
         text_plz_search.text = "검색결과가 없습니다.\n다른 검색어을 입력해주세요."
         binding.movieList = null
     }
 
-    override fun showResponseError() {
+    fun showResponseError() {
         toast("다시 시도해주세요.")
     }
 
-    override fun showNetworkFailure() {
+    fun showNetworkFailure() {
         toast("네트워크에 연결할 수 없습니다. 네트워크 연결을 확인해주세요.")
     }
 
-    override fun showMoviesList(items: List<Item>) {
+    /*fun showMoviesList(items: List<Item>) {
         runOnUiThread {
             adapterMovieList.addItem(items)
             binding.movieList = items
         }
-    }
+    }*/
+
 }
