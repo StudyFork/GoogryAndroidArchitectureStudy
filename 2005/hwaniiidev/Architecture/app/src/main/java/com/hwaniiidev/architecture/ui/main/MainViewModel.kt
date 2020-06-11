861c0a195b7c36e4ab36a048d0b7cf955af5f220
hwaniiidev <hwaniiidev@gmail.com>
2020-06-11 22:13:57 +0900

    feat: null예외처리

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainViewModel.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/MainViewModel.kt
@@ -17,47 +17,48 @@ class MainViewModel(
         error.set(INIT)
     }
 
-    fun searchMovies(searchValue: String) {
+    fun searchMovies(searchValue: String?) {
         if (searchValue.isNullOrBlank()) {
             //TODO : 에러처리 "검색어 입력"
             error.set(ERROR_QUERY_IS_NONE)
-        }
-        naverMovieRepositoryImpl.searchMovies(
-            query = searchValue,
-            onSuccess = { response ->
+        }else{
+            naverMovieRepositoryImpl.searchMovies(
+                query = searchValue,
+                onSuccess = { response ->
 
-                if (response.total == 0) {
-                    //TODO : 에러처리 showResponseIsNone
-                    movies.set(null)
-                    error.set(ERROR_RESPONSE_IS_NONE)
-                } else {
-                    //TODO : 영화리스트 추가
-                    movies.set(response.items)
-                    error.set(NONE_ERROR)
-                    hideKeyboard.notifyChange()
-                }
-            },
-            onError = { errorMessage ->
-                //TODO : 에러처리 showResponseError
-                error.set(ERROR_RESPONSE_ERROR)
-                Log.d(TAG, errorMessage)
-            },
-            onFailure = { t ->
-                //TODO : 에러처리 showNetworkFailure
-                error.set(ERROR_NETWORK_FAILURE)
-                Log.d(TAG, t.toString())
-            },
-            onCached = { movies ->
-                if (!movies.isNullOrEmpty()) {
-                    //TODO : 영화리스트 추가
-                    this.movies.set(movies)
-                    error.set(NONE_ERROR)
-                    hideKeyboard.notifyChange()
-                    movies.forEach {
-                        Log.d(TAG, it.title)
+                    if (response.total == 0) {
+                        //TODO : 에러처리 showResponseIsNone
+                        movies.set(null)
+                        error.set(ERROR_RESPONSE_IS_NONE)
+                    } else {
+                        //TODO : 영화리스트 추가
+                        movies.set(response.items)
+                        error.set(NONE_ERROR)
+                        hideKeyboard.notifyChange()
                     }
-                }
-            })
+                },
+                onError = { errorMessage ->
+                    //TODO : 에러처리 showResponseError
+                    error.set(ERROR_RESPONSE_ERROR)
+                    Log.d(TAG, errorMessage)
+                },
+                onFailure = { t ->
+                    //TODO : 에러처리 showNetworkFailure
+                    error.set(ERROR_NETWORK_FAILURE)
+                    Log.d(TAG, t.toString())
+                },
+                onCached = { movies ->
+                    if (!movies.isNullOrEmpty()) {
+                        //TODO : 영화리스트 추가
+                        this.movies.set(movies)
+                        error.set(NONE_ERROR)
+                        hideKeyboard.notifyChange()
+                        movies.forEach {
+                            Log.d(TAG, it.title)
+                        }
+                    }
+                })
+        }
     }
 
     companion object {
