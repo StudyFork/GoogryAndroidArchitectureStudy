2b3ada1a06fc9bc47aa6df7df132c3facb1d2cf4
hwaniiidev <hwaniiidev@gmail.com>
2020-06-14 08:21:42 +0900

    fix: 오타 수정

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/SearchError.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/ui/main/SearchError.kt
@@ -3,7 +3,7 @@ package com.hwaniiidev.architecture.ui.main
 import com.hwaniiidev.architecture.R
 
 enum class SearchError(val errorMessage:String){
-    INIT("하는 검색어를 입력해주세요."),
+    INIT("원하는 검색어를 입력해주세요."),
     NONE_ERROR(""),
     RESPONSE_IS_NONE("검색결과가 없습니다.\n다른 검색어을 입력해주세요."),
     RESPONSE_ERROR("다시 시도해주세요."),
