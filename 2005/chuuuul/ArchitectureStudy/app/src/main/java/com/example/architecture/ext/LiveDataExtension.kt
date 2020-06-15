a93f2ce2a0095a7fd9335233d0ff328f379f2d9b
chuuuul <chuuuul@naver.com>
2020-06-16 06:34:47 +0900

    HW-6 기능 : LiveData의 Debounce 구현

---
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/ext/LiveDataExtension.kt
@@ -0,0 +1,26 @@
+package com.example.architecture.ext
+
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+
+fun <T : Any> MutableLiveData<T>.createDefault(defaultValue: T): MutableLiveData<T> {
+    this.value = defaultValue
+    return this
+}
+
+fun <T> LiveData<T>.debounce(duration: Long = 1000L) = MediatorLiveData<T>().also { mld ->
+    val source = this
+    val handler = Handler(Looper.getMainLooper())
+
+    val runnable = Runnable {
+        mld.value = source.value
+    }
+
+    mld.addSource(source) {
+        handler.removeCallbacks(runnable)
+        handler.postDelayed(runnable, duration)
+    }
+}
--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/ext/MutableLiveDataExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.example.architecture.ext
-
-import androidx.lifecycle.MutableLiveData
-
-fun <T : Any> MutableLiveData<T>.createDefault(defaultValue: T): MutableLiveData<T> {
-    this.value = defaultValue
-    return this
-}
