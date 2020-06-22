ba7327d5f931a085ade45d1b9cb0869d04a16d51
sangjinKIM93 <larger611@gmail.com>
2020-06-22 11:49:09 +0900

    appModule 추가

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/di/appModule.kt
@@ -0,0 +1,24 @@
+package com.sangjin.newproject.di
+
+import com.sangjin.newproject.MovieListViewModel
+import com.sangjin.newproject.data.repository.NaverMoviesRepository
+import com.sangjin.newproject.data.repository.NaverMoviesRepositoryImpl
+import com.sangjin.newproject.data.source.local.LocalDataSource
+import com.sangjin.newproject.data.source.local.LocalDataSourceImpl
+import com.sangjin.newproject.data.source.remote.RemoteDataSource
+import com.sangjin.newproject.data.source.remote.RemoteDataSourceImpl
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single<RemoteDataSource> { RemoteDataSourceImpl() }
+
+    single<LocalDataSource> { LocalDataSourceImpl(androidApplication()) }
+
+    single<NaverMoviesRepository> { NaverMoviesRepositoryImpl(get(), get()) }
+
+    viewModel { MovieListViewModel(get()) }
+
+}
