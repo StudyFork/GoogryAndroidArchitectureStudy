b623b16253329d08d7d1b2750d907fa2a18e34da
sangjinKIM93 <larger611@gmail.com>
2020-06-22 11:48:23 +0900

    MyApplication 추가

---
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.sangjin.newproject
+
+import android.app.Application
+import com.sangjin.newproject.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(appModule)
+        }
+    }
+}
