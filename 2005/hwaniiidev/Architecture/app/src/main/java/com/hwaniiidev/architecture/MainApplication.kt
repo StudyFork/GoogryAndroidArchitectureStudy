ee6a03426e0df880f58527d0950541f61100c710
hwaniiidev <hwaniiidev@gmail.com>
2020-06-24 08:48:47 +0900

    feat: MainApplication 생성

---
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.hwaniiidev.architecture
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MainApplication)
+        }
+    }
+}
