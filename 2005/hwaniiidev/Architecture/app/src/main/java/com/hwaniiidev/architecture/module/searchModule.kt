fc19b8da2833838f4c473517627c3067763abf60
hwaniiidev <hwaniiidev@gmail.com>
2020-06-24 22:41:40 +0900

    feat: searchModule 에 viewModel 추가

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/module/searchModule.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/module/searchModule.kt
@@ -6,11 +6,14 @@ import com.hwaniiidev.architecture.data.source.local.NaverMovieLocalDataSource
 import com.hwaniiidev.architecture.data.source.local.NaverMovieLocalDataSourceImpl
 import com.hwaniiidev.architecture.data.source.remote.NaverMovieRemoteDataSource
 import com.hwaniiidev.architecture.data.source.remote.NaverMovieRemoteDataSourceImpl
+import com.hwaniiidev.architecture.ui.main.MainViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val searchModule = module {
     single<NaverMovieRepository>{NaverMovieRepositoryImpl(get(),get())}
     single<NaverMovieLocalDataSource>{NaverMovieLocalDataSourceImpl(androidContext())}
     single<NaverMovieRemoteDataSource>{NaverMovieRemoteDataSourceImpl()}
+    viewModel{ MainViewModel(get()) }
 }
