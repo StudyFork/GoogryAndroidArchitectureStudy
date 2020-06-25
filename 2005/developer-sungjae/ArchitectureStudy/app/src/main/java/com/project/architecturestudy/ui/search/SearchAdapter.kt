7006bc002c7232b2fb36ee404a5abba4162d0db2
developer-sungjae <dunkey2615@naver.com>
2020-06-26 00:03:53 +0900

    Delete onCreateViewHolder

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchAdapter.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchAdapter.kt
@@ -1,21 +1,12 @@
 package com.project.architecturestudy.ui.search
 
-import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
 import com.project.architecturestudy.base.BaseRecyclerViewAdapter
-import com.project.architecturestudy.base.BaseViewHolder
-import com.project.architecturestudy.data.model.MovieItem
 
 class SearchAdapter<B : ViewDataBinding, ITEM : Any>(
     @LayoutRes private val itemLayoutRes: Int
-) : BaseRecyclerViewAdapter<B, ITEM>(itemLayoutRes) {
+) : BaseRecyclerViewAdapter<B, ITEM>(itemLayoutRes)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<B> {
-        return object : BaseViewHolder<B>(
-            itemLayoutRes,
-            parent = parent
-        ) {}
-    }
-}
+
 
