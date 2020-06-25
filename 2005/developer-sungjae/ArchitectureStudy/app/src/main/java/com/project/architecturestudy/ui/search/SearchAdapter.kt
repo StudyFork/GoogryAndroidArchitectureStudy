9ae53ae2bf9b24e05793e0706b70e61fc10b591a
developer-sungjae <dunkey2615@naver.com>
2020-06-26 00:00:06 +0900

    Delete itemView.setOnClickListener

--- 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchAdapter.kt
+++ 2005/developer-sungjae/ArchitectureStudy/app/src/main/java/com/project/architecturestudy/ui/search/SearchAdapter.kt
@@ -11,19 +11,11 @@ class SearchAdapter<B : ViewDataBinding, ITEM : Any>(
     @LayoutRes private val itemLayoutRes: Int
 ) : BaseRecyclerViewAdapter<B, ITEM>(itemLayoutRes) {
 
-    private val movieList: MutableList<MovieItem> = mutableListOf()
-    lateinit var onClick: ((MovieItem) -> Unit)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<B> {
         return object : BaseViewHolder<B>(
             itemLayoutRes,
             parent = parent
-        ) {}.apply {
-            itemView.setOnClickListener {
-                val item = movieList[adapterPosition]
-                onClick.invoke(item)
-            }
-        }
+        ) {}
     }
 }
 
