6de0b2210b0bede4f7c6e7e43a3758c0ebd0221d
hwaniiidev <hwaniiidev@gmail.com>
2020-06-25 22:10:42 +0900

    feat: Retrofit api 모듈에 추가

--- 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/module/searchModule.kt
+++ 2005/hwaniiidev/Architecture/app/src/main/java/com/hwaniiidev/architecture/module/searchModule.kt
@@ -1,5 +1,7 @@
 package com.hwaniiidev.architecture.module
 
+import com.hwaniiidev.architecture.ApiCall
+import com.hwaniiidev.architecture.RetrofitService
 import com.hwaniiidev.architecture.data.repository.NaverMovieRepository
 import com.hwaniiidev.architecture.data.repository.NaverMovieRepositoryImpl
 import com.hwaniiidev.architecture.data.source.local.NaverMovieLocalDataSource
@@ -10,10 +12,19 @@ import com.hwaniiidev.architecture.ui.main.MainViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private val serverUrl: String = "https://openapi.naver.com/"
 
 val searchModule = module {
     single<NaverMovieRepository>{NaverMovieRepositoryImpl(get(),get())}
     single<NaverMovieLocalDataSource>{NaverMovieLocalDataSourceImpl(androidContext())}
-    single<NaverMovieRemoteDataSource>{NaverMovieRemoteDataSourceImpl()}
+    single<NaverMovieRemoteDataSource>{NaverMovieRemoteDataSourceImpl(get())}
+    single<RetrofitService>{Retrofit.Builder()
+        .baseUrl(serverUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(RetrofitService::class.java)}
     viewModel{ MainViewModel(get()) }
 }
