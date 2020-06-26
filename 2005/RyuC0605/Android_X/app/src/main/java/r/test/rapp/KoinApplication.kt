21138cf8bea02d65fd5bc5d08bdd5c6ec911cd5f
Ryu <bbs0605@naver.com>
2020-06-26 13:20:26 +0900

    KoinApplication 생성 및 매니패스트 수정

---
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/KoinApplication.kt
@@ -0,0 +1,21 @@
+package r.test.rapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class KoinApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // start Koin!
+        startKoin {
+            androidLogger()
+            // declare used Android context
+            androidContext(this@KoinApplication)
+            // declare modules
+            modules(appModule)
+        }
+    }
+}
