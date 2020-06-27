771f38e30d288377e3a9f268e8a46f6a307f9eb0
ysj1224 <y_sj1224@naver.com>
2020-06-27 23:21:04 +0900

    HW-1 네이버api데이터 생성

---
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/NaverApiData.kt
@@ -0,0 +1,36 @@
+package com.example.study
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NaverApiData(
+    @SerializedName("display")
+    val display: Int,
+    @SerializedName("items")
+    val items: List<Item>,
+    @SerializedName("lastBuildDate")
+    val lastBuildDate: String,
+    @SerializedName("start")
+    val start: Int,
+    @SerializedName("total")
+    val total: Int
+) {
+    data class Item(
+        @SerializedName("actor")
+        val actor: String,
+        @SerializedName("director")
+        val director: String,
+        @SerializedName("image")
+        val image: String,
+        @SerializedName("link")
+        val link: String,
+        @SerializedName("pubDate")
+        val pubDate: String,
+        @SerializedName("subtitle")
+        val subtitle: String,
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("userRating")
+        val userRating: String
+    )
+}
