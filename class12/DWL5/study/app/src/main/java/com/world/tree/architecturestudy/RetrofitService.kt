81347b128d10f8911cb22cb7ad83e422190eb544
DWL5 <ssop6403@gmail.com>
2020-06-28 18:35:19 +0900

    Modify Headers to Header

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/RetrofitService.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/RetrofitService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
@@ -12,10 +13,10 @@ const val CLIENT_SECRET = "TTXAMv3AtQ"
 const val BASE_URL = "https://openapi.naver.com/"
 interface RetrofitService {
 
-    @Headers("X-Naver-Client-Id: {$CLIENT_ID}",
-        "X-Naver-Client-Secret: {$CLIENT_SECRET}")
     @GET("v1/search/movie.json")
     fun requestSearchMovie(
+        @Header("X-Naver-Client-Id") id : String = CLIENT_ID,
+        @Header("X-Naver-Client-Secret") pw : String = CLIENT_SECRET,
         @Query("query") query:String,
         @Query("display") display:Int = 10,
         @Query("start") start: Int = 1
