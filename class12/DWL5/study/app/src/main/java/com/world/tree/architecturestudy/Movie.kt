cda22a9d22e3d2685611cde4257bf1210da8dcc9
DWL5 <ssop6403@gmail.com>
2020-06-28 15:58:10 +0900

    Add Movie data class

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/Movie.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/Movie.kt
@@ -1,2 +1,20 @@
 package com.world.tree.architecturestudy
 
+class Movie (
+    var lastBuildDate : String,
+    var total : Int,
+    var start : Int,
+    var display : Int,
+    var items : List<Items>
+) {
+    data class Items(
+        val title:String,
+        val link : String,
+        val image : String,
+        val subtitle : String,
+        val pubDate : String,
+        val director : String,
+        val actor : String,
+        val userRating: Double
+    )
+}
