d343695487689afdffe6e030b68d7236a8f101c4
sangjinKIM93 <larger611@gmail.com>
2020-06-28 09:14:42 +0900

    androidLogger() -> androidLogger(Level.Debug)

--- 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MyApplication.kt
+++ 2005/sangjinKIM93/newproject/app/src/main/java/com/sangjin/newproject/MyApplication.kt
@@ -5,6 +5,7 @@ import com.sangjin.newproject.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApplication : Application() {
 
@@ -12,7 +13,7 @@ class MyApplication : Application() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.DEBUG)
             androidContext(this@MyApplication)
             modules(appModule)
         }
