d557b65a953c0ac935879cb5d41bd4844440819d
Ryu <bbs0605@naver.com>
2020-06-28 21:52:39 +0900

    KoinApplication 수정

--- 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/KoinApplication.kt
+++ 2005/RyuC0605/Android_X/app/src/main/java/r/test/rapp/KoinApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import r.test.data.dataModule
 
 class KoinApplication : Application() {
 
@@ -15,7 +16,7 @@ class KoinApplication : Application() {
             // declare used Android context
             androidContext(this@KoinApplication)
             // declare modules
-            modules(appModule)
+            modules(dataModule)
         }
     }
 }
