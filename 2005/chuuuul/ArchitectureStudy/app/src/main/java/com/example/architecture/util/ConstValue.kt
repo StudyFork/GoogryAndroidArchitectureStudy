c789c52ef68df909a2eea4adfc4a3854d61fd752
chuuuul <chuuuul@naver.com>
2020-06-29 02:07:28 +0900

    HW-8 리팩토링 : ConstValue 정리

--- 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/util/ConstValue.kt
+++ 2005/chuuuul/ArchitectureStudy/app/src/main/java/com/example/architecture/util/ConstValue.kt
@@ -1,7 +1,7 @@
 package com.example.architecture.util
 
 internal object ConstValue {
-    const val MOVIE_SEARCH_API_URL = "https://openapi.naver.com"
+
     const val NO_IMAGE_URL = "https://ssl.pstatic.net/static/movie/2012/06/dft_img133x190.png"
     const val AUTO_SEARCH_TIME = 3000L
     const val SEARCH_TIME_THROTTLE = 1500L
--- 2005/chuuuul/ArchitectureStudy/data/src/main/java/com/chul/data/util/ConstValue.kt
+++ 2005/chuuuul/ArchitectureStudy/data/src/main/java/com/chul/data/util/ConstValue.kt
@@ -2,4 +2,5 @@ package com.chul.data.util
 
 internal object ConstValue {
     const val MOVIE_SHARED_PREFERENCE_NAME = "MoviesSharedPreferenceName"
+    const val MOVIE_SEARCH_API_URL = "https://openapi.naver.com"
 }
