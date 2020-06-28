4afe7b25a8c6629784a1218be4c4c2b9ed1814d0
DWL5 <ssop6403@gmail.com>
2020-06-28 16:28:51 +0900

    Add Movie Recylcerview Adapter

---
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/MovieAdapter.kt
@@ -0,0 +1,34 @@
+package com.world.tree.architecturestudy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter(data : ArrayList<Movie.Item>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+    var movies : ArrayList<Movie.Item> = data
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val v : View = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.txtTitle).text = movies[position].title
+        holder.itemView.findViewById<TextView>(R.id.txtDirector).text = movies[position].director
+        holder.itemView.findViewById<TextView>(R.id.txtActor).text = movies[position].actor
+    }
+
+    fun setDataList(data: ArrayList<Movie.Item>) {
+        this.movies = data
+        notifyDataSetChanged()
+    }
+}
