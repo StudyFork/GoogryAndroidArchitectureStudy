f55d72a3afa8086bafcd20eb271f060fc1f3250b
ysj1224 <y_sj1224@naver.com>
2020-06-29 21:48:07 +0900

    HW-1 Api인터페이스 구현

---
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/NaverApiInterface.kt
@@ -0,0 +1,17 @@
+package com.example.study
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface NaverApiInterface {
+
+    @GET("/v1/search/movie.json")
+    fun getSearch(
+        @Header("X-Naver-Client-Id") clientId: String,
+        @Header("X-Naver-Client-Secret") clientPw: String,
+        @Query("query") query: String
+    ): Call<NaverApiData>
+}
+
