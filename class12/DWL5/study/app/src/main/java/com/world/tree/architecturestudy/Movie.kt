170e53d721e96690dd70c783163f0658397147d2
DWL5 <ssop6403@gmail.com>
2020-06-30 10:36:46 +0900

    - Modify Code Range

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/Movie.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/Movie.kt
@@ -1,20 +1,20 @@
 package com.world.tree.architecturestudy
 
-class Movie (
-    var lastBuildDate : String,
-    var total : Int,
-    var start : Int,
-    var display : Int,
-    var items : List<Item>
+class Movie(
+    var lastBuildDate: String,
+    var total: Int,
+    var start: Int,
+    var display: Int,
+    var items: List<Item>
 ) {
     data class Item(
-        val title:String,
-        val link : String,
-        val image : String,
-        val subtitle : String,
-        val pubDate : String,
-        val director : String,
-        val actor : String,
+        val title: String,
+        val link: String,
+        val image: String,
+        val subtitle: String,
+        val pubDate: String,
+        val director: String,
+        val actor: String,
         val userRating: Double
     )
 }
