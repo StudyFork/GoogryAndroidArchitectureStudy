68323ad40d4dc77de9490d9ed5ee126b8d6accf9
songmilee <lsm941010@gmail.com>
2020-07-04 08:05:06 +0900

    class to object

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/network/RetrofitHelper.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/network/RetrofitHelper.kt
@@ -6,33 +6,33 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitHelper {
-    companion object {
-        private const val BASE_URL = "https://openapi.naver.com/v1/search/"
-        private var retrofitService: Retrofit? = null
+object RetrofitHelper {
 
-        fun getService(context: Context): MovieService {
-            if (retrofitService == null) {
-                val okHttpClient = OkHttpClient.Builder().addInterceptor { chain ->
-                    val newRequest = chain.request().newBuilder()
-                        .addHeader("X-Naver-Client-Id", context.getString(R.string.naver_client_id))
-                        .addHeader(
-                            "X-Naver-Client-Secret",
-                            context.getString(R.string.naver_client_secret)
-                        )
-                        .build()
+    private const val BASE_URL = "https://openapi.naver.com/v1/search/"
+    private var retrofitService: Retrofit? = null
 
-                    chain.proceed(newRequest)
-                }.build()
-
-                retrofitService = Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .client(okHttpClient)
-                    .addConverterFactory(GsonConverterFactory.create())
+    fun getService(context: Context): MovieService {
+        if (retrofitService == null) {
+            val okHttpClient = OkHttpClient.Builder().addInterceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("X-Naver-Client-Id", context.getString(R.string.naver_client_id))
+                    .addHeader(
+                        "X-Naver-Client-Secret",
+                        context.getString(R.string.naver_client_secret)
+                    )
                     .build()
-            }
 
-            return retrofitService!!.create(MovieService::class.java)
+                chain.proceed(newRequest)
+            }.build()
+
+            retrofitService = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         }
+
+        return retrofitService!!.create(MovieService::class.java)
     }
+
 }
