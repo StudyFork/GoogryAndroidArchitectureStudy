e7c2a6b1d907c8fc3e1c4d98376ce4a5eee1b8be
YANGSEUNGJIN <tmdwls1224@naver.com>
2020-07-03 13:59:56 +0900

    item = apiItem -> item.addAll(apiItem) 변경v2 (리포맷)

--- class12/ysj1224/Study/app/src/main/java/com/example/study/MainActivity.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.study
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 const val clientId = "hDyUQTbovi0BszAf5h87"
 const val clientSecret = "3FsmSYBKbJ"
@@ -37,10 +32,10 @@ class MainActivity : AppCompatActivity() {
 
         btn_check.setOnClickListener {
             if (etv_search.text.isEmpty()) {
-                Log.d("search","${etv_search.text}")
+                Log.d("search", "${etv_search.text}")
                 Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                Log.d("search","${etv_search.text}")
+                Log.d("search", "${etv_search.text}")
                 doSearch()
             }
 
@@ -55,10 +50,11 @@ class MainActivity : AppCompatActivity() {
         ).enqueue(object : Callback<NaverApiData> {
             override fun onFailure(call: Call<NaverApiData>, t: Throwable) {
             }
+
             override fun onResponse(call: Call<NaverApiData>, response: Response<NaverApiData>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
-                       viewAdapter.setItem(it.items)
+                        viewAdapter.setItem(it.items)
                     }
                 } else {
                     Toast.makeText(this@MainActivity, "네트워크를 확인해주세요.", Toast.LENGTH_SHORT).show()
