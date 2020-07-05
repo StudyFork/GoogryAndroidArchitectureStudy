4c4d4e9e0a274f6854fd6764da2f251964d4813b
ysj1224 <y_sj1224@naver.com>
2020-07-05 22:33:56 +0900

    HW-2 메인 UI RepositoryImpl 사용

--- class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.study.view
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.study.data.model.NaverApiData
-import com.example.study.data.api.NaverRetrofit
 import com.example.study.R
 import com.example.study.RecyclerAdapter
+import com.example.study.data.repository.MovieListRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 const val clientId = "hDyUQTbovi0BszAf5h87"
 const val clientSecret = "3FsmSYBKbJ"
@@ -21,6 +16,7 @@ const val baseUrl = "https://openapi.naver.com"
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerAdapter
+    private val moviList = MovieListRepositoryImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,40 +28,16 @@ class MainActivity : AppCompatActivity() {
             adapter = viewAdapter
         }
 
-
-
         btn_check.setOnClickListener {
             if (etv_search.text.isEmpty()) {
-                Log.d("search", "${etv_search.text}")
                 Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                Log.d("search", "${etv_search.text}")
-                doSearch()
+                moviList.doSearch(
+                    etv_search.text.toString(),
+                    response = { viewAdapter.setItem(it) },
+                    fail = { Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show() })
             }
-
         }
-    }
-
-    private fun doSearch() {
-        NaverRetrofit.SERVICE.getSearch(
-            clientId = clientId,
-            clientPw = clientSecret,
-            query = etv_search.text.toString()
-        ).enqueue(object : Callback<NaverApiData> {
-            override fun onFailure(call: Call<NaverApiData>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "네트워크를 확인해주세요.", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onResponse(call: Call<NaverApiData>, response: Response<NaverApiData>) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        viewAdapter.setItem(it.items)
-                    }
-                } else {
-                    Toast.makeText(this@MainActivity, "네트워크를 확인해주세요.", Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
     }
 }
 
