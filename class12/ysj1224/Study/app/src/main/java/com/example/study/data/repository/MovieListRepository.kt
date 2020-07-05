a981aa221f4a9092a0964fb542442248e804c502
ysj1224 <y_sj1224@naver.com>
2020-07-05 22:32:59 +0900

    HW-2 Repository 생성 및 함수선언

--- class12/ysj1224/Study/app/src/main/java/com/example/study/data/repository/MovieListRepository.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/data/repository/MovieListRepository.kt
@@ -3,5 +3,9 @@ package com.example.study.data.repository
 import com.example.study.data.model.NaverApiData
 
 interface MovieListRepository {
-    fun doSearch(query: String): List<NaverApiData.Item>
+    fun doSearch(
+        query: String,
+        response: (List<NaverApiData.Item>) -> Unit,
+        fail: (Throwable) -> Unit
+    )
 }
