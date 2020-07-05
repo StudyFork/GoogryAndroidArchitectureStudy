27632a26c15711bbce57bd5e59c3698d12b6dd3c
DWL5 <ssop6403@gmail.com>
2020-07-05 19:29:15 +0900

    #HW2 - Modify Repository Constuctor

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/repository/NaverRepositoryImpl.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/repository/NaverRepositoryImpl.kt
@@ -2,13 +2,13 @@ package com.world.tree.architecturestudy.repository
 
 import com.world.tree.architecturestudy.model.Movie
 
-class NaverRepositoryImpl(val repository: NaverRepository) : NaverRepository {
+class NaverRepositoryImpl(val remoteDataSource: NaverRepositoryImpl) : NaverRepository {
     override fun getMovies(
         q: String,
         success: (List<Movie.Item>) -> Unit,
         error: (Throwable) -> Unit
     ) {
-        repository.getMovies(q, success = success, error = error)
+        remoteDataSource.getMovies(q, success = success, error = error)
     }
 
 }
