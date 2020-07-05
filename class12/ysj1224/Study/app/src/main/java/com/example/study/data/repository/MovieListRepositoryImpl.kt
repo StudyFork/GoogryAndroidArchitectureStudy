1c3dc2fa563a5773be7fe0150ef3957c088fd9e8
ysj1224 <y_sj1224@naver.com>
2020-07-05 22:33:23 +0900

    HW-2 RepositoryImpl 생성 및 구현

--- class12/ysj1224/Study/app/src/main/java/com/example/study/data/repository/MovieListRepositoryImpl.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/data/repository/MovieListRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.example.study.data.repository
 
-import android.widget.Toast
 import com.example.study.data.api.NaverRetrofit
 import com.example.study.data.model.NaverApiData
 import com.example.study.view.clientId
@@ -9,28 +8,28 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MovieListRepositoryImpl:MovieListRepository {
-    override fun doSearch(query: String): List<NaverApiData.Item> {
-            NaverRetrofit.SERVICE.getSearch(
-                clientId = clientId,
-                clientPw = clientSecret,
-                query = query
-            ).enqueue(object : Callback<NaverApiData> {
-                override fun onFailure(call: Call<NaverApiData>, t: Throwable) {
+class MovieListRepositoryImpl : MovieListRepository {
+    override fun doSearch(
+        query: String,
+        response: (List<NaverApiData.Item>) -> Unit,
+        fail: (Throwable) -> Unit
+    ) {
+        NaverRetrofit.SERVICE.getSearch(
+            clientId = clientId,
+            clientPw = clientSecret,
+            query = query
+        ).enqueue(object : Callback<NaverApiData> {
+            override fun onFailure(call: Call<NaverApiData>, t: Throwable) {
+                fail(t)
+            }
 
-                }
-
-                override fun onResponse(call: Call<NaverApiData>, response: Response<NaverApiData>){
-                    if (response.isSuccessful) {
-
-                        response.body()?.let {
-
-                        }
-                    } else {
-//                        Toast.makeText(this@MainActivity, "네트워크를 확인해주세요.", Toast.LENGTH_SHORT).show()
+            override fun onResponse(call: Call<NaverApiData>, response: Response<NaverApiData>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        response(it.items)
                     }
                 }
-            })
-
-        }
+            }
+        })
+    }
 }
