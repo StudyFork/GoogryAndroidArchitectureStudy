c284f1ed03eaa2d00f03c0a630aa858585f6940b
DWL5 <ssop6403@gmail.com>
2020-07-07 23:07:27 +0900

    Modify Repository Contructor Parmeter to interface

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/repository/remote/NaverRepositoryImpl.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/repository/remote/NaverRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.world.tree.architecturestudy.repository.remote
 
 import com.world.tree.architecturestudy.model.Movie
+import com.world.tree.architecturestudy.source.remote.NaverRemoteDataSource
 import com.world.tree.architecturestudy.source.remote.NaverRemoteDataSourceImpl
 
-class NaverRepositoryImpl(private val remoteDataSource: NaverRemoteDataSourceImpl) :
+class NaverRepositoryImpl(private val remoteDataSource: NaverRemoteDataSource) :
     NaverRepository {
     override fun getMovies(
         q: String,
