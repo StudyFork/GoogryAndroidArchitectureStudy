4a8ec76b70ed3f72818dd3cb7db5231c2b85a27a
ysj1224 <y_sj1224@naver.com>
2020-07-12 21:33:12 +0900

    HW-3 MainActivity.kt 수정 및 프리젠터 적용

--- class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.study.MovieContract
+import com.example.study.MoviePresenter
 import com.example.study.R
 import com.example.study.RecyclerAdapter
+import com.example.study.data.model.NaverApiData
 import com.example.study.data.repository.MovieListRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,10 +16,13 @@ const val clientId = "hDyUQTbovi0BszAf5h87"
 const val clientSecret = "3FsmSYBKbJ"
 const val baseUrl = "https://openapi.naver.com"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MovieContract.View {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerAdapter
-    private val movieListRepositoryImpl = MovieListRepositoryImpl()
+    private val moviePresenter: MovieContract.Presenter by lazy {
+        MoviePresenter(this, MovieListRepositoryImpl())
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,16 +35,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_check.setOnClickListener {
-            if (etv_search.text.isEmpty()) {
-                Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
-            } else {
-                movieListRepositoryImpl.doSearch(
-                    etv_search.text.toString(),
-                    response = { viewAdapter.setItem(it) },
-                    fail = { Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show() })
-
-            }
+            moviePresenter.requestMovieList(etv_search.text.toString())
         }
+    }
+
+    override fun showMovieList(naverApiData: List<NaverApiData.Item>) {
+        viewAdapter.setItem(naverApiData)
+    }
+
+    override fun showErrorResponse(t: Throwable) {
+        Toast.makeText(this@MainActivity, "$t", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showQueryEmpty() {
+        Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
     }
 }
 
