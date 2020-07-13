af0c05fd6bbce3d3d448d9d10836fb68e7c531e5
hoyounglee <pgshoot13@gmail.com>
2020-07-13 11:23:20 +0900

    HW-3 Modify setContentView, Add MainContract

--- class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/ui/MainActivity.kt
+++ class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/ui/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.hyper.hyapplication.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hyper.hyapplication.MovieAdapter
 import com.hyper.hyapplication.R
-import com.hyper.hyapplication.repository.NaverRepositoryImpl
-import com.hyper.hyapplication.source.remote.NaverRemoteDataSourceImpl
+import com.hyper.hyapplication.model.ResultGetSearchMovie
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.View {
 
     private lateinit var viewAdapter: MovieAdapter
     private val moviList = NaverRepositoryImpl(NaverRemoteDataSourceImpl())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         viewAdapter = MovieAdapter()
         recyclerView.apply {
