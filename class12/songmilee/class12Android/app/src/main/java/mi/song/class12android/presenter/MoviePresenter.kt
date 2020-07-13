36c3f06bab500786236e8219b306dc93f5b91689
songmilee <lsm941010@gmail.com>
2020-07-13 14:27:08 +0900

    HW-3-MVP : presenter 변수 private으로 변경

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/presenter/MoviePresenter.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/presenter/MoviePresenter.kt
@@ -5,10 +5,9 @@ import mi.song.class12android.R
 import mi.song.class12android.data.repository.SearchMovieRepository
 import mi.song.class12android.data.repository.SearchMovieRepositoryImpl
 
-class MoviePresenter(val context: Context, val view: MovieInterface.View) :
+class MoviePresenter(private val context: Context, private val view: MovieInterface.View) :
     MovieInterface.Presenter {
     private val searchMovieRepository: SearchMovieRepository = SearchMovieRepositoryImpl(context)
-
 
     override fun requestMovieData(query: String) {
         if (query.isEmpty()) return
