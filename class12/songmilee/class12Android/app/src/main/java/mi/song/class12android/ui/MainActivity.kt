3ec2117beaa8f6eb4582e44c1c6629b50e16dde6
songmilee <lsm941010@gmail.com>
2020-07-20 13:14:41 +0900

    HW-4-binding : queryMovie 변수 제거

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/ui/MainActivity.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/ui/MainActivity.kt
@@ -1,20 +1,17 @@
 package mi.song.class12android.ui
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import mi.song.class12android.R
 import mi.song.class12android.data.model.MovieInfo
 import mi.song.class12android.databinding.ActivityMainBinding
-
 import mi.song.class12android.presenter.MovieInterface
 import mi.song.class12android.presenter.MoviePresenter
 
 class MainActivity : AppCompatActivity(), MovieInterface.View {
     private lateinit var movieAdapter: MovieAdapter
-    private lateinit var queryMovie: View.OnClickListener
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var presenter: MovieInterface.Presenter
