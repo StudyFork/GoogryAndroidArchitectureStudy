3a7d92458c262a420811c5163edf206f1a2a9b67
unman <manjee.study@gmail.com>
2020-07-23 00:20:39 +0900

    HW-4 MovieAdapter dataBinding

--- class12/manjee-study/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MovieAdapter.kt
+++ class12/manjee-study/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MovieAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.architecturestudy.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.architecturestudy.R
 import com.example.architecturestudy.data.model.MovieData
-import kotlinx.android.synthetic.main.item_movie.view.*
+import com.example.architecturestudy.databinding.ItemMovieBinding
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
@@ -15,7 +14,12 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         return MovieViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_movie,
+                parent,
+                false
+            )
         )
     }
 
@@ -33,17 +37,12 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
         notifyDataSetChanged()
     }
 
-    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val thumbnailImageView = itemView.thumbnailImageView
-        private val titleTextView = itemView.titleTextView
-        private val actorTextView = itemView.actorTextView
+    inner class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bindData(movieData: MovieData) {
-            movieData.image.let {
-                Glide.with(itemView.context).load(movieData.image).into(thumbnailImageView)
-            }
-            titleTextView.text = movieData.title.replace("<b>", "").replace("</b>", "")
-            actorTextView.text = movieData.actor
+            binding.movieData = movieData
+            binding.executePendingBindings()
         }
     }
 }
