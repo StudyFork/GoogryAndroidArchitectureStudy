d1ab61b327e45416cd6a48483b1af6989740e4ec
songmilee <lsm941010@gmail.com>
2020-07-27 10:49:46 +0900

    HW-5-mvvm : glide 사진 요청 시 이미지 사이즈 조정

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/util/BindingAdapterUtil.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/util/BindingAdapterUtil.kt
@@ -4,12 +4,14 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("bind:imgUrl")
 fun loadImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
         Glide.with(imgView)
             .load(imgUrl)
+            .apply(RequestOptions().override(80, 80))
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .into(imgView)
     }
