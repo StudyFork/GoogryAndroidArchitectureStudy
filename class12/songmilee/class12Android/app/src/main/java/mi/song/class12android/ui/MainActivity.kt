369466e04c7ffd64338ae789386651ce5b771623
songmilee <lsm941010@gmail.com>
2020-07-27 09:59:30 +0900

    HW-5-mvvm : presenter 변수 제거 및 ObservableField 관찰 추가

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/ui/MainActivity.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/ui/MainActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import mi.song.class12android.R
 import mi.song.class12android.data.model.MovieInfo
 import mi.song.class12android.databinding.ActivityMainBinding
-import mi.song.class12android.presenter.MovieInterface
-import mi.song.class12android.presenter.MoviePresenter
 import mi.song.class12android.viewmodel.MovieViewModel
 
-class MainActivity : AppCompatActivity(), MovieInterface.View {
+class MainActivity : AppCompatActivity() {
     private lateinit var movieAdapter: MovieAdapter
     private lateinit var binding: ActivityMainBinding
-
-    private lateinit var presenter: MovieInterface.Presenter
     private lateinit var viewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,32 +23,43 @@ class MainActivity : AppCompatActivity(), MovieInterface.View {
     }
 
     private fun init() {
-        presenter = MoviePresenter(baseContext, this)
         viewModel = MovieViewModel(this@MainActivity)
 
         initUi()
     }
 
     private fun initUi() {
-        binding.mainView = this@MainActivity
         binding.mainVm = viewModel
 
         movieAdapter = MovieAdapter()
         binding.listMovie.adapter = movieAdapter
+
+        viewModel.message.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                showMessage(viewModel.message.get())
+            }
+        })
+
+        viewModel.successResult.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                updateMovieList(viewModel.successResult.get())
+            }
+        })
     }
 
-    override fun showMessage(msg: String) {
-        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun updateMovieList(list: List<MovieInfo>) {
-        movieAdapter.clearMovieList()
-        movieAdapter.addMovieInfo(list)
-    }
-
-    override fun queryMovie() {
-        binding.edtQuery.text?.toString()?.let { query ->
-            presenter.requestMovieData(query)
+    fun showMessage(msg: String?) {
+        msg?.let {
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun updateMovieList(list: List<MovieInfo>?) {
+        list?.let {
+            movieAdapter.clearMovieList()
+            movieAdapter.addMovieInfo(list)
+        }
+    }
+
 }
