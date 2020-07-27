94a4fc2c8475e3216fa6db4970e7b83d024e9241
songmilee <lsm941010@gmail.com>
2020-07-27 10:48:03 +0900

    HW-5-mvvm : ssl ca 추가

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/data/source/remote/RetrofitHelper.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/data/source/remote/RetrofitHelper.kt
@@ -5,6 +5,12 @@ import mi.song.class12android.R
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object RetrofitHelper {
 
@@ -13,7 +19,7 @@ object RetrofitHelper {
 
     fun getService(context: Context): MovieService {
         if (retrofitService == null) {
-            val okHttpClient = OkHttpClient.Builder().addInterceptor { chain ->
+            val okHttpClient = initSsl(OkHttpClient.Builder().addInterceptor { chain ->
                 val newRequest = chain.request().newBuilder()
                     .addHeader("X-Naver-Client-Id", context.getString(R.string.naver_client_id))
                     .addHeader(
@@ -21,9 +27,8 @@ object RetrofitHelper {
                         context.getString(R.string.naver_client_secret)
                     )
                     .build()
-
                 chain.proceed(newRequest)
-            }.build()
+            }).build()
 
             retrofitService = Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -33,6 +38,36 @@ object RetrofitHelper {
         }
 
         return retrofitService!!.create(MovieService::class.java)
+    }
+
+    private fun initSsl(builder: OkHttpClient.Builder): OkHttpClient.Builder {
+        try {
+            val trustAllcerts = arrayOf<TrustManager>(object: X509TrustManager{
+                override fun checkClientTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?
+                ) {         }
+
+                override fun checkServerTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?
+                ) {         }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            })
+
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllcerts, SecureRandom())
+
+            builder.sslSocketFactory(sslContext.socketFactory, trustAllcerts[0] as X509TrustManager)
+
+            return builder
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return builder
+        }
     }
 
 }
