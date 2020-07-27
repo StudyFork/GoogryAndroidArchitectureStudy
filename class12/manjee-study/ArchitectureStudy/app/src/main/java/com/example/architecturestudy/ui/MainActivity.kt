7b500cd9f5787a97a7702dcb3014f0878f612543
unman <manjee.study@gmail.com>
2020-07-28 00:20:44 +0900

    HW-5 presenter 삭제 및 사용 소스 제거

--- class12/manjee-study/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MainActivity.kt
+++ class12/manjee-study/ArchitectureStudy/app/src/main/java/com/example/architecturestudy/ui/MainActivity.kt
@@ -7,21 +7,21 @@ import androidx.databinding.DataBindingUtil
 import com.example.architecturestudy.R
 import com.example.architecturestudy.data.model.MovieData
 import com.example.architecturestudy.databinding.ActivityMainBinding
-import com.example.architecturestudy.presenter.SearchMovieConstract
-import com.example.architecturestudy.presenter.SearchMoviePresenter
+import com.example.architecturestudy.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), SearchMovieConstract.View {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var movieAdapter: MovieAdapter
-    private val searchMoviePresenter = SearchMoviePresenter(this)
+
+    private var mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
-        binding.searchMoviePresenter = searchMoviePresenter
+//        binding.searchMoviePresenter = searchMoviePresenter
 
         init()
     }
@@ -32,11 +32,11 @@ class MainActivity : AppCompatActivity(), SearchMovieConstract.View {
         movieRecyclerView.adapter = movieAdapter
     }
 
-    override fun showMovieList(movieList: List<MovieData>) {
-        movieAdapter.setData(movieList)
-    }
-
-    override fun showSearchFailToast(throwable: Throwable) {
-        Toast.makeText(applicationContext, throwable.message, Toast.LENGTH_SHORT).show()
-    }
+//    override fun showMovieList(movieList: List<MovieData>) {
+//        movieAdapter.setData(movieList)
+//    }
+//
+//    override fun showSearchFailToast(throwable: Throwable) {
+//        Toast.makeText(applicationContext, throwable.message, Toast.LENGTH_SHORT).show()
+//    }
 }
