2c2fa870323ac3d4707a75674a29d4fd41ad3cea
songmilee <lsm941010@gmail.com>
2020-07-27 09:50:03 +0900

    HW-5-mvvm : Observable field 추가

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/viewmodel/MovieViewModel.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/viewmodel/MovieViewModel.kt
@@ -1,19 +1,25 @@
 package mi.song.class12android.viewmodel
 
 import android.content.Context
+import androidx.databinding.ObservableField
+import mi.song.class12android.data.model.MovieInfo
 import mi.song.class12android.data.repository.SearchMovieRepository
 import mi.song.class12android.data.repository.SearchMovieRepositoryImpl
 
-class MovieViewModel(private val context: Context) {
+class MovieViewModel(context: Context) {
     private val searchMovieRepository: SearchMovieRepository = SearchMovieRepositoryImpl(context)
+
+    val message = ObservableField<String>()
+    val successResult = ObservableField<List<MovieInfo>>()
 
     fun requestMovieData(query: String) {
         if (query.isEmpty()) return
         searchMovieRepository.getRemoteMovieData(query,
             success = {
-
+                successResult.set(it)
             }, fail = {
-
+                it.printStackTrace()
+                message.set(it.message)
             })
     }
 }
