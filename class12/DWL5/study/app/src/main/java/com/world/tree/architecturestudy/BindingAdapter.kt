ff3871331fb77720c9e1b68ed5a613442cad202d
DWL <DWL>
2020-07-28 16:06:09 +0900

    HW5 - ADD bindData function for Movie Recyclerview

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/BindingAdapter.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/BindingAdapter.kt
@@ -2,11 +2,20 @@ package com.world.tree.architecturestudy
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.world.tree.architecturestudy.model.Movie
+import com.world.tree.architecturestudy.view.MovieAdapter
 
 @BindingAdapter("loadImage")
 fun loadImage(imageView: ImageView, url:String) {
     Glide.with(imageView).load(url)
         .centerCrop()
         .into(imageView)
+}
+
+@BindingAdapter("bindData")
+fun bindData(recyclerView: RecyclerView, data : ArrayList<Movie.Item>) {
+    val adapter = recyclerView.adapter as MovieAdapter
+    adapter.addData(data)
 }
