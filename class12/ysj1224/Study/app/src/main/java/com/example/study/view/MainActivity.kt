77ffa8ad6a9cf6d605b4177c5d31b34252817772
YANGSEUNGJIN <tmdwls1224@naver.com>
2020-07-28 16:33:15 +0900

    HW5 - viewmodel 추가

--- class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/view/MainActivity.kt
@@ -4,55 +4,68 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
 import com.example.study.R
 import com.example.study.RecyclerAdapter
 import com.example.study.data.model.NaverApiData
-import com.example.study.data.repository.MovieListRepositoryImpl
 import com.example.study.databinding.ActivityMainBinding
-import com.example.study.presenter.MovieContract
-import com.example.study.presenter.MoviePresenter
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.study.viewmodel.MovieViewModel
 
 const val clientId = "hDyUQTbovi0BszAf5h87"
 const val clientSecret = "3FsmSYBKbJ"
 const val baseUrl = "https://openapi.naver.com"
 
-class MainActivity : AppCompatActivity(), MovieContract.View {
+class MainActivity : AppCompatActivity() {
     private lateinit var viewAdapter: RecyclerAdapter
     private lateinit var binding: ActivityMainBinding
-    private val moviePresenter: MovieContract.Presenter by lazy {
-        MoviePresenter(
-            this,
-            MovieListRepositoryImpl()
-        )
-    }
+    private val viewModel = MovieViewModel()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewAdapter = RecyclerAdapter()
+        init()
+
         binding.run {
             rvMain.apply {
                 setHasFixedSize(true)
                 adapter = viewAdapter
             }
             btnCheck.setOnClickListener {
-                moviePresenter.requestMovieList(binding.etvSearch.text.toString())
+                viewModel.requestMovieList(binding.etvSearch.text.toString())
             }
         }
+
     }
 
-    override fun showMovieList(naverApiData: List<NaverApiData.Item>) {
+    fun showMovieList(naverApiData: List<NaverApiData.Item>) {
         viewAdapter.setItem(naverApiData)
     }
 
-    override fun showErrorResponse(t: Throwable) {
+    fun showErrorResponse(t: Throwable) {
         Toast.makeText(this@MainActivity, "$t", Toast.LENGTH_SHORT).show()
     }
 
-    override fun showQueryEmpty() {
-        Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
+    private fun init() {
+        with(viewModel) {
+            movieList.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    movieList.get()?.let { showMovieList(it) }
+                }
+            })
+
+            fail.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    fail.get()?.let { showErrorResponse(it) }
+                }
+            })
+            noQuery.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                    Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 
 }
