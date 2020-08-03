73647b6baeb501c39f3a8a133fe7380a950b2b93
DWL <DWL>
2020-08-03 20:38:52 +0900

    HW5 - Modify TwoWayBinding ViewModel에서 직접 사용

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModel.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModel.kt
@@ -13,20 +13,23 @@ class MainViewModel(private val repository: NaverRepository) {
     val title = ObservableField<String>()
     val toastMsg = ObservableField<String>()
 
-    fun searchMovie(q: String) {
-        if (q.isBlank()) {
-            toastMsg.set("검색어를 입력 해 주세요")
-            toastMsg.set("")
-        }
-
-        repository.getMovies(q, success = {
-            movieList.clear()
-            movieList.addAll(it)
-        }, error = {
-            it.message?.let { message ->
-                toastMsg.set(message)
+    fun searchMovie() {
+        title.get()?.let {
+            if (it.isBlank()) {
+                toastMsg.set("검색어를 입력 해 주세요")
                 toastMsg.set("")
             }
-        })
+
+            repository.getMovies(it, success = {
+                movieList.clear()
+                movieList.addAll(it)
+            }, error = {
+                it.message?.let { message ->
+                    toastMsg.set(message)
+                    toastMsg.set("")
+                }
+            })
+        }
+
     }
 }
