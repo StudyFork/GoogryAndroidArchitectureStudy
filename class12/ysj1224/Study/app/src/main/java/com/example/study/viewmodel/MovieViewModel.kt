13a9bba11eb0f3171adefbcd1c6d2dd576b378ea
YANGSEUNGJIN <tmdwls1224@naver.com>
2020-08-04 17:31:06 +0900

    HW6 - livedata 적용

--- class12/ysj1224/Study/app/src/main/java/com/example/study/viewmodel/MovieViewModel.kt
+++ class12/ysj1224/Study/app/src/main/java/com/example/study/viewmodel/MovieViewModel.kt
@@ -1,35 +1,40 @@
 package com.example.study.viewmodel
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.study.data.model.NaverApiData
 import com.example.study.data.repository.MovieListRepositoryImpl
 
 class MovieViewModel : ViewModel() {
     private val repository = MovieListRepositoryImpl()
-    val movieList = ObservableField<List<NaverApiData.Item>>()
-    val fail = ObservableField<Throwable>()
-    val noQuery = ObservableField(0)
-    val searchString = ObservableField<String>()
+
+    private val _movieList = MutableLiveData<List<NaverApiData.Item>>()
+    val movieList: LiveData<List<NaverApiData.Item>> get() = _movieList
+    private val _fail = MutableLiveData<Throwable>()
+    val fail: LiveData<Throwable> get() = _fail
+    private val _noQuery = MutableLiveData<Boolean>(false)
+    val noQuery: LiveData<Boolean> get() = _noQuery
+
+    val searchString = MutableLiveData<String>()
 
 
     fun requestMovieList() {
-        if (searchString.get().toString().isNullOrEmpty()) {
+        if (searchString.value.isNullOrEmpty()) {
             showQueryEmpty()
         } else {
-            searchString.get()?.let { doSearch(it) }
+            doSearch(searchString.value.toString())
         }
     }
 
     private fun showQueryEmpty() {
-        noQuery.notifyChange()
+        _noQuery.value = !(_noQuery.value)!!
     }
 
     private fun doSearch(query: String) {
         repository.doSearch(
             query = query,
-            response = { movieList.set(it) },
-            fail = { fail.set(it) })
-
+            response = { _movieList.value = it },
+            fail = { _fail.value = it })
     }
 }
