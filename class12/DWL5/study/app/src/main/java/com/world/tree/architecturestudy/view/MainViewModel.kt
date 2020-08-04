df4fa2df0b4f947289a419107021e5e3727650da
DWL <DWL>
2020-08-04 13:21:45 +0900

    HW6 - Modify Observable to LiveData

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModel.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModel.kt
@@ -2,6 +2,8 @@ package com.world.tree.architecturestudy.view
 
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.world.tree.architecturestudy.model.Movie
 import com.world.tree.architecturestudy.model.repository.remote.NaverRepository
 import java.util.*
@@ -9,24 +11,29 @@ import kotlin.collections.ArrayList
 
 class MainViewModel(private val repository: NaverRepository) {
 
-    val movieList = ObservableArrayList<Movie.Item>()
-    val title = ObservableField<String>()
-    val toastMsg = ObservableField<String>()
+    private val _movieList = MutableLiveData<List<Movie.Item>>().also {
+        it.value = ArrayList()
+    }
+    val movieList = _movieList
+    private val _title = MutableLiveData<String>()
+    val title = _title
+    private val _toastMsg = MutableLiveData<String>()
+    val toastMsg = _toastMsg
+
+
+
 
     fun searchMovie() {
-        title.get()?.let {
+        title.value?.let { it ->
             if (it.isBlank()) {
-                toastMsg.set("검색어를 입력 해 주세요")
-                toastMsg.set("")
+                toastMsg.value = "검색어를 입력 해 주세요"
             }
 
             repository.getMovies(it, success = {
-                movieList.clear()
-                movieList.addAll(it)
+                movieList.value = it
             }, error = {
                 it.message?.let { message ->
-                    toastMsg.set(message)
-                    toastMsg.set("")
+                    toastMsg.value = message
                 }
             })
         }
