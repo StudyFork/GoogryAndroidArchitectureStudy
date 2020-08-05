88a68f3d77afb1c29eb634d678b48a1ca30ecfc6
cswoong <hisunwoong@gmail.com>
2020-08-05 13:25:23 +0900

    HW-6 : Applying LiveData

--- class12/dlqkfzlr/study/app/src/main/java/m/woong/architecturestudy/ui/main/MainActivity.kt
+++ class12/dlqkfzlr/study/app/src/main/java/m/woong/architecturestudy/ui/main/MainActivity.kt
@@ -1,15 +1,15 @@
 package m.woong.architecturestudy.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.Observable
+import androidx.lifecycle.Observer
 import m.woong.architecturestudy.AppContainer
 import m.woong.architecturestudy.MovieViewModel
 import m.woong.architecturestudy.MyApplication
 import m.woong.architecturestudy.R
-import m.woong.architecturestudy.data.source.remote.model.MovieResponse
 import m.woong.architecturestudy.databinding.ActivityMainBinding
 import m.woong.architecturestudy.ui.adapter.MovieAdapter
 
@@ -28,37 +28,23 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         movieViewModel = MovieViewModel(appContainer.movieRepository)
         activityMainBinding.viewModel = movieViewModel
         activityMainBinding.movieRv.adapter = viewAdapter
+        activityMainBinding.lifecycleOwner = this
 
-        with(movieViewModel) {
-            this.queryData.addOnPropertyChangedCallback(object :
-                Observable.OnPropertyChangedCallback() {
-                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                    if (queryData.get() != null) {
-                        Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            })
-
-            this.successResponse.addOnPropertyChangedCallback(object :
-                Observable.OnPropertyChangedCallback() {
-                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                    if (successResponse.get() != null) {
-                        val list = successResponse.get() as List<MovieResponse.Item>
-                        viewAdapter.resetData(list)
-                    }
-                }
-            })
-
-            this.toastMsg.addOnPropertyChangedCallback(object :
-                Observable.OnPropertyChangedCallback() {
-                override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                    Toast.makeText(this@MainActivity, "검색결과를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT)
-                        .show()
-
-                }
-            })
+        val queryObserver = Observer<String> {
+            Log.v("LiveData", "입력:$it")
+            if (it.isNullOrEmpty()) {
+                Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
         }
 
+        movieViewModel.queryData.observe(this, queryObserver)
+
+        movieViewModel.successResponse.observe(this,
+            Observer {
+                Log.v("MVVM", it.toString())
+                viewAdapter.resetData(it)
+            }
+        )
 
     }
 
