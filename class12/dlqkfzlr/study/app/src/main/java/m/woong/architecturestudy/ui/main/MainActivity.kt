1a598ba1b807e70ceacaa439dff0153981b80376
cswoong <hisunwoong@gmail.com>
2020-08-08 22:28:29 +0900

    HW-7: modify observe method for empty-string searching situation

--- class12/dlqkfzlr/study/app/src/main/java/m/woong/architecturestudy/ui/main/MainActivity.kt
+++ class12/dlqkfzlr/study/app/src/main/java/m/woong/architecturestudy/ui/main/MainActivity.kt
@@ -6,38 +6,36 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import m.woong.architecturestudy.AppContainer
 import m.woong.architecturestudy.MovieViewModel
-import m.woong.architecturestudy.MyApplication
 import m.woong.architecturestudy.R
 import m.woong.architecturestudy.databinding.ActivityMainBinding
 import m.woong.architecturestudy.ui.adapter.MovieAdapter
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var viewAdapter: MovieAdapter
-    private lateinit var appContainer: AppContainer
     private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var movieViewModel: MovieViewModel
+    val movieViewModel: MovieViewModel by viewModel()       //viewModel은 Koin, viewModels는 viewModel ktx임
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        appContainer = (application as MyApplication).appContainer
+
         viewAdapter = MovieAdapter()
         activityMainBinding =
             DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
-        movieViewModel = MovieViewModel(appContainer.movieRepository)
         activityMainBinding.viewModel = movieViewModel
         activityMainBinding.movieRv.adapter = viewAdapter
         activityMainBinding.lifecycleOwner = this
 
-        val queryObserver = Observer<String> {
-            Log.v("LiveData", "입력:$it")
-            if (it.isNullOrEmpty()) {
-                Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+        movieViewModel.queryNullorEmpty.observe(this,
+            Observer<Boolean> {
+                Log.v("LiveData", "입력없음:$it")
+                if (it) {
+                    Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    movieViewModel.queryNullorEmpty.value = false
+                }
             }
-        }
-
-        movieViewModel.queryData.observe(this, queryObserver)
+        )
 
         movieViewModel.successResponse.observe(this,
             Observer {
