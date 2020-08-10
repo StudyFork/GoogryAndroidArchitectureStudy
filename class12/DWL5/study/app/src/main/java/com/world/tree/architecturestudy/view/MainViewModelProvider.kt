ff240ca75a09c6bee14b9ec21783d8f659229a03
DWL5 <ssop6403@gmail.com>
2020-08-11 00:57:00 +0900

    HW6 Modify MainViewModelProvider

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModelProvider.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/view/MainViewModelProvider.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.world.tree.architecturestudy.model.repository.remote.NaverRepository
 import java.lang.IllegalArgumentException
 
-class MainViewModelProvider(private val repository: Any) : ViewModelProvider.Factory {
+class MainViewModelProvider(private val repository: NaverRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(repository as NaverRepository) as T
