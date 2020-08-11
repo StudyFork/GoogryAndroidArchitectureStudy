00bbda079391739502feb64ba3dd0d197aea71fb
DWL <DWL>
2020-08-11 15:56:47 +0900

    HW7 - Add HiltAndroidApp in Appliction

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/CommonApplication.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/CommonApplication.kt
@@ -1,7 +1,10 @@
 package com.world.tree.architecturestudy
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+
+@HiltAndroidApp
 class CommonApplication:Application() {
     val movieContainer = MovieContainer()
 }
