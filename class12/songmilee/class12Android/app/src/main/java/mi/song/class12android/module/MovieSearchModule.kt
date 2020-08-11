845926c39bf2d9128001b95b59389cb7353233cb
songmilee <lsm941010@gmail.com>
2020-08-11 21:32:20 +0900

    HW-7 : viewModel 주입을 위해 추가

--- class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/module/MovieSearchModule.kt
+++ class12/songmilee/class12Android/app/src/main/java/mi/song/class12android/module/MovieSearchModule.kt
@@ -2,9 +2,11 @@ package mi.song.class12android.module
 
 import mi.song.class12android.data.repository.SearchMovieRepository
 import mi.song.class12android.data.repository.SearchMovieRepositoryImpl
+import mi.song.class12android.viewmodel.MovieViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val movieSearchModule = module {
     single<SearchMovieRepository> { SearchMovieRepositoryImpl(get()) }
-    single<SearchMovieRepositoryImpl> { SearchMovieRepositoryImpl(get()) }
+    viewModel { MovieViewModel(get()) }
 }
