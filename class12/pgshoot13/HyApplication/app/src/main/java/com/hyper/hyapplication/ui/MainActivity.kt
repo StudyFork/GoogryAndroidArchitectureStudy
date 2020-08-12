1f2e171a0acca5ffc0d227d1c271ba2f1e738dd7
hoyounglee <pgshoot13@gmail.com>
2020-08-12 15:07:25 +0900

    HW-7 inject mainViewModel

--- class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/ui/MainActivity.kt
+++ class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/ui/MainActivity.kt
@@ -5,21 +5,21 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.hyper.hyapplication.MovieAdapter
 import com.hyper.hyapplication.R
 import com.hyper.hyapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var viewAdapter: MovieAdapter
-    private lateinit var mainViewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+
+    private val mainViewModel: MainViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         binding = DataBindingUtil.setContentView<ActivityMainBinding>(
             this@MainActivity, R.layout.activity_main
