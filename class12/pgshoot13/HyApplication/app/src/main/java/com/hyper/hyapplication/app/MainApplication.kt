274cf0484377b47cd17244ff27a98aaa1c719c5c
hoyounglee <pgshoot13@gmail.com>
2020-08-20 15:52:02 +0900

    HW-7 Add NetworkModule

--- class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/app/MainApplication.kt
+++ class12/pgshoot13/HyApplication/app/src/main/java/com/hyper/hyapplication/app/MainApplication.kt
@@ -1,21 +1,23 @@
-package com.hyper.hyapplication
+package com.hyper.hyapplication.app
 
 import android.app.Application
+import com.hyper.hyapplication.BuildConfig
 import com.hyper.hyapplication.module.appModule
-
+import com.hyper.hyapplication.module.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            if (BuildConfig.DEBUG) {
+                androidLogger()
+            }
             androidContext(this@MainApplication)
-            modules(appModule)
+            modules(appModule, networkModule)
         }
     }
 }
