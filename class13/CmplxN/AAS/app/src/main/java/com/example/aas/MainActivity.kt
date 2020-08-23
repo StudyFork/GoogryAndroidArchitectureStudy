79a628f84d2942ed801aa5d3bec7c6cf988c79b8
CmplxN <zayne@naver.com>
2020-08-23 15:31:54 +0900

    HW-1 : MainActivity UI setup

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/MainActivity.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.aas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+	private val compositeDisposable = CompositeDisposable()
+	private val movieAdapter = MovieAdapter()
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		rcv_movie.adapter = movieAdapter
+
+		RxTextView.textChanges(et_movie_name)
+			.subscribe { btn_request.isEnabled = it.isNotBlank() }
+			.addTo(compositeDisposable)
+
+		btn_request.setOnClickListener {
+			val keyword = et_movie_name.text.also { et_movie_name.setText("") }
+
+			et_movie_name.clearFocus()
+			hideKeyboard(this, et_movie_name)
+
+			RetrofitManager.naverMoviesApi.getMovies(keyword.toString())
+				.timeout(5000L, TimeUnit.MILLISECONDS)
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(
+					{
+						Toast.makeText(this, "Search Completed", Toast.LENGTH_SHORT).show()
+						movieAdapter.setList(it.movies)
+					},
+					{
+						Toast.makeText(this, "Network Error", Toast.LENGTH_LONG).show()
+						it.printStackTrace()
+					}
+				)
+				.addTo(compositeDisposable)
+		}
+	}
+
+	override fun onDestroy() {
+		compositeDisposable.clear()
+		super.onDestroy()
+	}
 }
--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/Util.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/Util.kt
@@ -1,12 +1,20 @@
 package com.example.aas
 
+import android.content.Context
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 fun interpretHtml(str: String): Spanned {
 	return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
 		Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY)
 	else
 		Html.fromHtml(str)
+}
+
+fun hideKeyboard(context: Context, view: View) {
+	val inputMethod = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+	inputMethod.hideSoftInputFromWindow(view.windowToken, 0)
 }
