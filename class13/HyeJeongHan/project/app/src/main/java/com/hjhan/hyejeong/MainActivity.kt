a8a242002df22d068716cac03d20201184f44ae9
한혜정 <assg934@gmail.com>
2020-08-23 14:14:28 +0900

    HW-1 xml 수정

--- class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/MainActivity.kt
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hjhan.hyejeong
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
