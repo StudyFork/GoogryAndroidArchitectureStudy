1f537dd3b8510b80b366e84d342d34d09736a648
junggeun <junggeunlee89@gmail.com>
2020-08-24 10:08:58 +0900

    Movie Data Model 추가

---
+++ class13/camai/ArchitectureStudy/app/src/main/java/com/camai/archtecherstudy/model/MovieResponseModel.kt
@@ -0,0 +1,36 @@
+package com.camai.archtecherstudy.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponseModel(
+    @SerializedName("lastBuildDate")
+    var lastBuildDate : String,
+    @SerializedName("total")
+    var total : Int,
+    @SerializedName("start")
+    var start : Int,
+    @SerializedName("display")
+    var display : Int,
+    @SerializedName("items")
+    var items : ArrayList<Items>
+)
+
+data class Items(
+    @SerializedName("title")
+    val title : String,
+    @SerializedName("link")
+    val link : String,
+    @SerializedName("image")
+    val image : String,
+    @SerializedName("subtitle")
+    val subtitle : String,
+    @SerializedName("pubdate")
+    val pubDate : String,
+    @SerializedName("director")
+    val director : String,
+    @SerializedName("actor")
+    val actor : String,
+    @SerializedName("userRating")
+    val userRating : Float
+)
+
