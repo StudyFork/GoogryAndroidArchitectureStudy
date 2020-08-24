62612043ff9379270a8d74246438667803178c6d
sangminkim <apfhdznzl@gmail.com>
2020-08-24 09:01:46 +0900

    create retrofit Builder as a singleton 수정

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/retrofit/RetrofitClient.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/retrofit/RetrofitClient.kt
@@ -9,22 +9,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://openapi.naver.com/v1/search"
 
-class RetrofitClient {
-    private var mRetrofit: Retrofit? = null
+object RetrofitClient {
     private val intercepter =
         HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     private val builder = OkHttpClient.Builder()
         .addInterceptor(intercepter)
     private val mOkHttpClient = builder.build()
 
-    fun <T> createService(service: Class<T>?): T {
-        if (mRetrofit == null) {
-            mRetrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(mOkHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-        return mRetrofit!!.create(service)
-    }
+    private val retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(mOkHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    fun getService(): MovieService = retrofit.create(MovieService::class.java)
 }
