4bae593b17c91e83561de3d129b40d529b969b5d
HwangJunHong <ghkdwnsghd1@naver.com>
2020-08-26 01:34:29 +0900

    Name Change loadGlideImageView -> loadImageView

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ext/ImageViewExt.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ext/ImageViewExt.kt
@@ -5,7 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.hong.architecturestudy.R
 
-fun ImageView.loadGlideImageView(imageUrl: String) {
+fun ImageView.loadImageView(imageUrl: String) {
     Glide.with(context)
         .load(imageUrl)
         .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher_background).centerCrop())
