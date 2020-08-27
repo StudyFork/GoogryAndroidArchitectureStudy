826c5a6bc41922e718f8447ea1c6086943e23798
HwangJunHong <ghkdwnsghd1@naver.com>
2020-08-27 21:51:55 +0900

    add { }

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ext/FuncExt.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ext/FuncExt.kt
@@ -8,14 +8,16 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun String.toHtml(): Spanned {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
-    else
+    } else {
         Html.fromHtml(this)
+    }
 }
 
 fun hideKeyboard(context: Context, view: View) {
-    val inputMethod = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    val inputMethod =
+        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethod.hideSoftInputFromWindow(view.windowToken, 0)
 }
 
