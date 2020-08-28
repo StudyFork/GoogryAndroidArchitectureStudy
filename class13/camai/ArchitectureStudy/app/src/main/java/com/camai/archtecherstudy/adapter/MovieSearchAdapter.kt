4148d8d7bb4af67ecc34dd4e10efa75f9f59f09f
junggeun <junggeunlee89@gmail.com>
2020-08-28 09:28:35 +0900

    HW-1 ViewHolder view 개체 변수 private 선언 추가

--- class13/camai/ArchitectureStudy/app/src/main/java/com/camai/archtecherstudy/adapter/MovieSearchAdapter.kt
+++ class13/camai/ArchitectureStudy/app/src/main/java/com/camai/archtecherstudy/adapter/MovieSearchAdapter.kt
@@ -44,12 +44,12 @@ class MovieSearchAdapter(
 
     class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
 
-        val poster = itemView?.findViewById<ImageView>(R.id.img_poster)
-        val title = itemView?.findViewById<TextView>(R.id.txt_title)
-        val user_rating = itemView?.findViewById<RatingBar>(R.id.rb_user_rating)
-        val pub_data = itemView?.findViewById<TextView>(R.id.txt_pub_data)
-        val director = itemView?.findViewById<TextView>(R.id.txt_director)
-        val actor = itemView?.findViewById<TextView>(R.id.txt_actor)
+        private val poster = itemView?.findViewById<ImageView>(R.id.img_poster)
+        private val title = itemView?.findViewById<TextView>(R.id.txt_title)
+        private val user_rating = itemView?.findViewById<RatingBar>(R.id.rb_user_rating)
+        private val pub_data = itemView?.findViewById<TextView>(R.id.txt_pub_data)
+        private val director = itemView?.findViewById<TextView>(R.id.txt_director)
+        private val actor = itemView?.findViewById<TextView>(R.id.txt_actor)
 
         fun bind(itemInfo: Items, context: Context) {
 
