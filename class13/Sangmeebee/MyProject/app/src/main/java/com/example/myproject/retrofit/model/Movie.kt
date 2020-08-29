65774d1466c5cd87f59e638e5d866d14d757a870
sangminkim <apfhdznzl@gmail.com>
2020-08-30 00:08:04 +0900

    HW-1 Items model data class 별도로 생성

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/retrofit/model/Movie.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/retrofit/model/Movie.kt
@@ -15,23 +15,3 @@ data class Movie(
     @SerializedName("items")
     val items: List<Items>
 )
-
-data class Items(
-
-    @SerializedName("title")
-    val title: String,
-    @SerializedName("link")
-    val link: String,
-    @SerializedName("image")
-    val image: String,
-    @SerializedName("subtitle")
-    val subtitle: String,
-    @SerializedName("pubDate")
-    val pubDate: Int,
-    @SerializedName("director")
-    val director: String,
-    @SerializedName("actor")
-    val actor: String,
-    @SerializedName("userRating")
-    val userRating: Double
-)
