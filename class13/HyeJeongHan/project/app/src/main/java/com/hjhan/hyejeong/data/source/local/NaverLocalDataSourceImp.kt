b8e12c62cccbf1f1c0591b3d3df7978e734997bc
한혜정 <assg934@gmail.com>
2020-09-02 20:04:46 +0900

    HW-2 NaverLocalDataSourceImp, NaverRemoteDataSourceImp 추가

---
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/data/source/local/NaverLocalDataSourceImp.kt
@@ -0,0 +1,13 @@
+package com.hjhan.hyejeong.data.source.local
+
+class NaverLocalDataSourceImp: NaverLocalDataSource {
+
+    override fun saveQuery(query: String) {
+
+    }
+
+    override fun getQueryList(): List<String> {
+
+        return emptyList()
+    }
+}
---
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/data/source/remote/NaverRemoteDataSourceImp.kt
@@ -0,0 +1,8 @@
+package com.hjhan.hyejeong.data.source.remote
+
+class NaverRemoteDataSourceImp: NaverRemoteDataSource {
+
+    override fun getSearchMovies(query: String) {
+
+    }
+}
