662fd9aa8ee0551a733d662b4c20cadff9ab2d95
CmplxN <zayne@naver.com>
2020-09-03 22:21:18 +0900

    HW-2 : added glide extension function

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/utils/Util.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/utils/Util.kt
@@ -6,6 +6,8 @@ import android.text.Html
 import android.text.Spanned
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 fun String.toHtml(): Spanned {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
@@ -17,4 +19,11 @@ fun String.toHtml(): Spanned {
 fun hideKeyboard(context: Context, view: View) {
     val inputMethod = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethod.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun ImageView.glideCenterCrop(uri: String) {
+    Glide.with(this)
+        .load(uri)
+        .centerCrop()
+        .into(this)
 }
