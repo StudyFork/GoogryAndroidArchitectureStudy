7d520d1660a2003597c74e2c29574ef49bd753e0
junggeun <junggeunlee89@gmail.com>
2020-09-14 22:18:00 +0900

    HW-3 rencent dialog contract && presenter add

---
+++ class13/camai/ArchitectureStudy/app/src/main/java/com/camai/archtecherstudy/ui/rencentdialog/RecentMoviePresenter.kt
@@ -0,0 +1,17 @@
+package com.camai.archtecherstudy.ui.rencentdialog
+
+import com.camai.archtecherstudy.data.repository.MovieRepositoryImpl
+
+class RecentMoviePresenter(
+    private val viewDialog: RecentMovieContract.View,
+    private val MovieRepositoryImpl: MovieRepositoryImpl
+): RecentMovieContract.Presenter{
+    
+    override fun setRecentData() {
+
+    }
+
+    override fun closeDialog() {
+
+    }
+}
