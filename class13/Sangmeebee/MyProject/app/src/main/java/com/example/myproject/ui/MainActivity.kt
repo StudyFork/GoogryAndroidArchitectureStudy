d635c534319ead81ebb1b77d45cb720aa21f80d5
sangminkim <apfhdznzl@gmail.com>
2020-09-16 04:29:38 +0900

    HW-3 Modified MainActivity

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/ui/MainActivity.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myproject.ui
 import android.os.Bundle
 import android.text.Editable
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myproject.R
@@ -12,10 +11,12 @@ import com.example.myproject.data.repository.NaverRepository
 import com.example.myproject.data.repository.NaverRepositoryImpl
 import com.example.myproject.data.source.local.NaverLocalDataSourceImpl
 import com.example.myproject.data.source.remote.NaverRemoteDataSourceImpl
+import com.example.myproject.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), OnListItemSelectedInterface {
+class MainActivity : BaseActivity<MainContract.Presenter>(R.layout.activity_main),
+    MainContract.View, OnListItemSelectedInterface {
 
     private var movies: ArrayList<Items> = ArrayList()
     private val movieAdapter = MovieAdapter(this, movies)
@@ -26,6 +27,10 @@ class MainActivity : AppCompatActivity(), OnListItemSelectedInterface {
         NaverRepositoryImpl(naverLocalDataSourceImpl, naverRemoteDataSourceImpl)
     }
 
+    override val presenter by lazy {
+        MainPresenter(this, repositoryDataSourceImpl)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,27 +38,10 @@ class MainActivity : AppCompatActivity(), OnListItemSelectedInterface {
         setRecyclerView()
 
         btn_search.setOnClickListener {
-            val title = edit_title.text.toString()
-            if (title.isEmpty()) {
-                Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
-            } else {
-                recyclerview.layoutManager?.scrollToPosition(0)
-                repositoryDataSourceImpl.getMovieList(title, {
-                    if (it.isEmpty()) {
-                        movieAdapter.clearItems()
-                        Toast.makeText(
-                            applicationContext,
-                            "$title 를 찾을 수 없습니다",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        movieAdapter.clearAndAddItems(it)
-                    }
 
-                }, {
-                    Toast.makeText(this, "error : $it", Toast.LENGTH_LONG).show()
-                })
-            }
+            val title = edit_title.text.toString()
+            presenter.searchMovieList(title)
+
         }
 
         btn_history.setOnClickListener {
@@ -73,5 +61,26 @@ class MainActivity : AppCompatActivity(), OnListItemSelectedInterface {
 
     override fun onItemSelected(title: String) {
         edit_title.text = Editable.Factory.getInstance().newEditable(title)
+    }
+
+    override fun showQueryEmpty() {
+        Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun initScroll() {
+        recyclerview.layoutManager?.scrollToPosition(0)
+    }
+
+    override fun showResultEmpty() {
+        movieAdapter.clearItems()
+        Toast.makeText(
+            applicationContext,
+            "$title 를 찾을 수 없습니다",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun showResult(items: ArrayList<Items>) {
+        movieAdapter.clearAndAddItems(items)
     }
 }
