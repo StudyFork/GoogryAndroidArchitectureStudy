c0e398117269687e7e0e6e3a6a303bd1ffff60af
sangminkim <apfhdznzl@gmail.com>
2020-09-16 14:23:42 +0900

    HW-2 remove unnecessary code

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/ui/main/MainActivity.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/ui/main/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : BaseActivity<MainContract.Presenter>(R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         setRecyclerView()
 
