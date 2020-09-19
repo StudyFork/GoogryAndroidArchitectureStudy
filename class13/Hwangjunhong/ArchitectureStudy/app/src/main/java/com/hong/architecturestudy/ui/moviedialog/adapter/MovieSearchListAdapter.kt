6fde2adf47d4cfadd7048a2e7a356e9184d64a8b
HwangJunHong <ghkdwnsghd1@naver.com>
2020-09-20 05:34:09 +0900

    HW-4 : REFACTOR MovieDialogFragment

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/adapter/MovieSearchListAdapter.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/adapter/MovieSearchListAdapter.kt
@@ -1,7 +1,10 @@
 package com.hong.architecturestudy.ui.moviedialog.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.hong.architecturestudy.R
 import com.hong.architecturestudy.data.source.local.entity.MovieInfo
 import com.hong.architecturestudy.ui.main.GetMovieTitle
 import com.hong.architecturestudy.ui.moviedialog.holder.MovieSearchListViewHolder
@@ -11,11 +14,16 @@ class MovieSearchListAdapter(val onClick: GetMovieTitle) :
     private val movieItems = mutableListOf<MovieInfo>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        MovieSearchListViewHolder(parent).apply {
+        MovieSearchListViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context), R.layout.item_movie_search_list, parent, false
+            )
+        ).apply {
             itemView.setOnClickListener {
                 onClick(movieItems[absoluteAdapterPosition].movieTitle)
             }
         }
+
 
     override fun getItemCount() = movieItems.size
 
