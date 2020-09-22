32d147b0b319d307cb9b9ebbfa6344afa5cf9ad4
ddochi9 <rangskim@naver.com>
2020-09-22 13:57:27 +0900

    최근5개 검색어 저장 & item클릭 시 해당검색어로 검색 기능 추가

--- class13/ddochi93/Dkarch/app/src/main/java/com/example/dkarch/domain/globalconsts/Consts.kt
+++ class13/ddochi93/Dkarch/app/src/main/java/com/example/dkarch/domain/globalconsts/Consts.kt
@@ -5,5 +5,6 @@ class Consts {
         const val BASE_URL = "https://openapi.naver.com/"
         const val PREF_MY = "PREF_MY"
         const val RECENT_QUERY_LIST = "RECENT_QUERY_LIST"
+        const val FRAGMENT_QUERY_LIST = "FRAGMENT_QUERY_LIST"
     }
 }
---
+++ class13/ddochi93/Dkarch/app/src/main/java/com/example/dkarch/presentation/QueryHistoryAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.dkarch.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dkarch.R
+import kotlinx.android.synthetic.main.item_query_history.view.*
+
+class QueryHistoryAdapter(
+    private val queryHistoryList: List<String>,
+    private val queryItemClickedListener: (String) -> Unit
+) :
+    RecyclerView.Adapter<QueryHistoryAdapter.QueryViewHolder>() {
+
+    class QueryViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_query_history, parent, false)
+    ) {
+        private val queryTextView: TextView = itemView.query_text_view
+
+        fun bind(query: String, queryItemClickedListener: (String) -> Unit) {
+            queryTextView.text = query
+            queryTextView.setOnClickListener {
+                queryItemClickedListener(query)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QueryViewHolder {
+        return QueryViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: QueryViewHolder, position: Int) {
+        holder.bind(queryHistoryList[position], queryItemClickedListener)
+    }
+
+    override fun getItemCount(): Int {
+        return queryHistoryList.size
+    }
+
+}
