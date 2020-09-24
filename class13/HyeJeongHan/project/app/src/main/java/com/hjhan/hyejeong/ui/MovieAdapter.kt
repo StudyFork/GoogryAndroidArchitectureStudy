3dc5cb1eae230ba8923458ab671fc59cc5239dbe
한혜정 <assg934@gmail.com>
2020-09-24 23:47:18 +0900

    HW-4 refactoring adapter, xml

--- class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MovieAdapter.kt
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MovieAdapter.kt
@@ -1,15 +1,14 @@
 package com.hjhan.hyejeong.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.hjhan.hyejeong.R
 import com.hjhan.hyejeong.data.model.Item
+import com.hjhan.hyejeong.databinding.ItemMovieBinding
 
 class MovieAdapter :
     RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
@@ -17,8 +16,10 @@ class MovieAdapter :
     private val list = mutableListOf<Item>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_movie, parent,
+        val view = DataBindingUtil.inflate<ItemMovieBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_movie,
+            parent,
             false
         )
 
@@ -44,24 +45,19 @@ class MovieAdapter :
         notifyDataSetChanged()
     }
 
-    class MovieViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
-
-        var title = itemView.findViewById(R.id.title_text_view) as TextView
-        var subTitle = itemView.findViewById(R.id.sub_title_text_view) as TextView
-        var director = itemView.findViewById(R.id.director_text_view) as TextView
-        var image = itemView.findViewById(R.id.movie_image_image_view) as ImageView
+    class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(data: Item) {
 
-            title.text = data.title
-            subTitle.text = data.subtitle
-            director.text = data.director
+            binding.titleTextView.text = data.title
+            binding.subTitleTextView.text = data.subtitle
+            binding.directorTextView.text = data.director
 
-            Glide.with(image.context)
+            Glide.with(binding.movieImageImageView.context)
                 .load(data.image)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(image)
+                .into(binding.movieImageImageView)
 
 
         }
