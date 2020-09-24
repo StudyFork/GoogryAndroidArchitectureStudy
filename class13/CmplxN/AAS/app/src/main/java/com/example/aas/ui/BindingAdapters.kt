eb40d0d48d0789606c6e128a2fdc9c9e0dd29af6
CmplxN <zayne@naver.com>
2020-09-24 10:06:45 +0900

    HW-4 : used HtmlCompat

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/ui/BindingAdapters.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/ui/BindingAdapters.kt
@@ -1,10 +1,9 @@
 package com.example.aas.ui
 
-import android.os.Build
-import android.text.Html
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.jakewharton.rxbinding2.view.RxView
@@ -12,10 +11,7 @@ import java.util.concurrent.TimeUnit
 
 @BindingAdapter("htmlText")
 fun TextView.setHtmlTest(text: String) {
-    this.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-        Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
-    else
-        Html.fromHtml(text)
+    this.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_COMPACT)
 }
 
 @BindingAdapter("loadImageFromUri")
