43c4c5f937fb08b39b9a01b99069a858e493b7cd
CmplxN <zayne@naver.com>
2020-09-30 16:29:47 +0900

    HW-5 : Movie implements Parcelable

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/model/Movie.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/model/Movie.kt
@@ -1,12 +1,52 @@
 package com.example.aas.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Movie(
-	val actor: String,
-	val director: String,
-	val image: String,
-	val link: String,
-	val pubDate: String,
-	val subtitle: String,
-	val title: String,
-	val userRating: String
-)
+    val actor: String,
+    val director: String,
+    val image: String,
+    val link: String,
+    val pubDate: String,
+    val subtitle: String,
+    val title: String,
+    val userRating: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(actor)
+        parcel.writeString(director)
+        parcel.writeString(image)
+        parcel.writeString(link)
+        parcel.writeString(pubDate)
+        parcel.writeString(subtitle)
+        parcel.writeString(title)
+        parcel.writeString(userRating)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Movie> {
+        override fun createFromParcel(parcel: Parcel): Movie {
+            return Movie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Movie?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
