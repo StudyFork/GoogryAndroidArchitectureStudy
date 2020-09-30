b35350434d0fb564c540ad2141308ecdd2973f78
CmplxN <zayne@naver.com>
2020-09-30 23:06:03 +0900

    HW-5 : use Parcelize

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/model/Movie.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/model/Movie.kt
@@ -1,8 +1,9 @@
 package com.example.aas.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Movie(
     val actor: String,
     val director: String,
@@ -12,41 +13,4 @@ data class Movie(
     val subtitle: String,
     val title: String,
     val userRating: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(actor)
-        parcel.writeString(director)
-        parcel.writeString(image)
-        parcel.writeString(link)
-        parcel.writeString(pubDate)
-        parcel.writeString(subtitle)
-        parcel.writeString(title)
-        parcel.writeString(userRating)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
