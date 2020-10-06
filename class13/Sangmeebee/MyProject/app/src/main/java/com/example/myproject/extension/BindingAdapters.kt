351cc6cb2c6fb4524f397fda7d52fae54aab2db3
sangminkim <apfhdznzl@gmail.com>
2020-10-06 23:24:50 +0900

    HW-5 UPDATE BindingAdapters

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/extension/BindingAdapters.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/extension/BindingAdapters.kt
@@ -2,26 +2,27 @@ package com.example.myproject.extension
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import android.widget.RatingBar
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.myproject.data.model.Items
+import com.example.myproject.ui.MainActivity
+import com.example.myproject.ui.MovieAdapter
+import com.example.myproject.ui.TitleAdapter
+import com.example.myproject.viewmodel.MainViewModel
 
 @BindingAdapter("imageUrl")
 fun ImageView.setLoadUrl(imageUrl: String) {
     Glide.with(this)
         .load(imageUrl)
         .diskCacheStrategy(DiskCacheStrategy.ALL)
-        .into(this)}
-
-@BindingAdapter("rating")
-fun RatingBar.setRating(rating: Double){
-    val fRating = rating.toFloat()
-    setRating(fRating/2)
+        .into(this)
 }
 
 @BindingAdapter("htmlText")
@@ -29,3 +30,12 @@ fun TextView.setHtmlText(text: String) {
     this.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_COMPACT)
 }
 
+//open WebView
+@BindingAdapter("webUrl")
+fun View.openWebView(url: String) {
+    this.setOnClickListener {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        context.startActivity(intent)
+    }
+}
--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/ui/OnListItemSelectedInterface.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myproject.ui
-
-interface OnListItemSelectedInterface {
-    fun onItemSelected(title: String)
-}
