d03cd9c6d9155ef1a9858528fb6dd121c50a87bf
HwangJunHong <ghkdwnsghd1@naver.com>
2020-10-11 15:53:13 +0900

    HW-6 : UPDATE livadata set value

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/MovieListDialogViewModel.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/MovieListDialogViewModel.kt
@@ -5,14 +5,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hong.architecturestudy.data.repository.RepositoryDataSource
 import com.hong.architecturestudy.data.source.local.entity.MovieInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MovieListDialogViewModel(private val repository: RepositoryDataSource) : ViewModel() {
 
     private val _movieInfo = MutableLiveData<List<MovieInfo>>()
     val movieInfo: LiveData<List<MovieInfo>> get() = _movieInfo
 
+    private var loadRecentJob: Job? = null
+
     fun loadRecentSearchMovieList() {
         val list = repository.loadRecentSearchQuery()
-        _movieInfo.postValue(list)
+
+        loadRecentJob = CoroutineScope(Dispatchers.Main).launch {
+            _movieInfo.value = list
+        }
+    }
+
+    override fun onCleared() {
+        loadRecentJob?.cancel()
+        super.onCleared()
+
     }
 }
