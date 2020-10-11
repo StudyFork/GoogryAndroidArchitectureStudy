e306c2922601865c2073a6bfecc2329e7bb9b86d
CmplxN <zayne@naver.com>
2020-10-11 19:19:15 +0900

    HW-6 : movieList to be private

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/ui/main/MovieAdapter.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/ui/main/MovieAdapter.kt
@@ -9,7 +9,7 @@ import com.example.aas.data.model.Movie
 
 class MovieAdapter(private val callback: MovieSelectionListener) :
     RecyclerView.Adapter<MovieViewHolder>() {
-    val movieList = arrayListOf<Movie>()
+    private val movieList = arrayListOf<Movie>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         MovieViewHolder(
