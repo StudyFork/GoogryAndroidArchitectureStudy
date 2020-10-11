2b971355853a54c2c12129f98ef5cab3c06501aa
HwangJunHong <ghkdwnsghd1@naver.com>
2020-10-11 14:23:17 +0900

    HW-6 : UPDATE MovieListDialogViewModel LivaData

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/MovieListDialogViewModel.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/moviedialog/MovieListDialogViewModel.kt
@@ -1,5 +1,6 @@
 package com.hong.architecturestudy.ui.moviedialog
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hong.architecturestudy.data.repository.RepositoryDataSource
@@ -7,10 +8,11 @@ import com.hong.architecturestudy.data.source.local.entity.MovieInfo
 
 class MovieListDialogViewModel(private val repository: RepositoryDataSource) : ViewModel() {
 
-    val movieInfo = MutableLiveData<List<MovieInfo>>()
+    private val _movieInfo = MutableLiveData<List<MovieInfo>>()
+    val movieInfo: LiveData<List<MovieInfo>> get() = _movieInfo
 
     fun loadRecentSearchMovieList() {
         val list = repository.loadRecentSearchQuery()
-        movieInfo.postValue(list)
+        _movieInfo.postValue(list)
     }
 }
