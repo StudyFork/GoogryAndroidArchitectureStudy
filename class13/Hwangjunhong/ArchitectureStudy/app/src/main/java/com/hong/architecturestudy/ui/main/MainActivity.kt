55376f29a9d84f8977c6411ab5433c15db65688d
HwangJunHong <ghkdwnsghd1@naver.com>
2020-10-14 06:57:58 +0900

    HW-6 : DELETE binding activity

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/main/MainActivity.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/ui/main/MainActivity.kt
@@ -13,10 +13,17 @@ import com.hong.architecturestudy.ext.toast
 import com.hong.architecturestudy.ui.base.BaseActivity
 import com.hong.architecturestudy.ui.main.adapter.MovieAdapter
 import com.hong.architecturestudy.ui.moviedialog.MovieListDialogFragment
+import com.hong.architecturestudy.utils.log
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
 
-    val vm: MainViewModel by viewModels {
+    private val movieAdapter = MovieAdapter()
+
+    override val vm: MainViewModel by viewModels {
         object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
@@ -24,25 +31,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             }
         }
     }
+
     private val fragment = MovieListDialogFragment.getInstance()
+    lateinit var onQueryCallBack: (String) -> Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setBinding()
         showToastMessage()
         setRecyclerView()
-        dialogDismiss()
+        showSelectedQueryResult()
+        showRecentSearchList()
 
     }
 
-    private fun setBinding() {
-        binding.vm = vm
-        binding.activity = this
+    private fun showSelectedQueryResult() {
+        onQueryCallBack = { query ->
+            log("MainActivity showSelectedQueryResult$query")
+            binding.query = query
+            btn_search.callOnClick()
+        }
     }
 
     private fun setRecyclerView() {
-        val movieAdapter = MovieAdapter()
         binding.rvMoviesList.adapter = movieAdapter
         binding.rvMoviesList.setHasFixedSize(true)
 
@@ -51,21 +62,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         }
     }
 
-    fun showRecentSearchList() {
-        fragment.show(supportFragmentManager, "fragmentDialog")
-
-    }
-
-    private fun dialogDismiss() {
-        vm.isVisible observe {
-            if (it) {
-                fragment.dismiss()
-                vm.isVisible.value = false
-            }
+    private fun showRecentSearchList() {
+        bind {
+            RxView.clicks(btnSearchList)
+                .throttleFirst(500L, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    fragment.show(supportFragmentManager, "fragmentDialog")
+                }
         }
     }
 
     private fun showToastMessage() = vm.msg observe { message ->
+
         when (message) {
             Message.NETWORK_ERROR -> toast(R.string.message_network_error)
             Message.SUCCESS -> toast(R.string.message_success)
