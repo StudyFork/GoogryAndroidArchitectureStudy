72495ff5f0a0bae04f1d368fd3f547e729fe4292
한혜정 <assg934@gmail.com>
2020-10-13 22:14:56 +0900

    HW-6 modify MainActivity, MainViewModel

--- class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MainActivity.kt
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.hjhan.hyejeong.ui
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.databinding.Observable
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.hjhan.hyejeong.R
 import com.hjhan.hyejeong.data.repository.NaverRepositoryImpl
 import com.hjhan.hyejeong.data.source.local.NaverLocalDataSourceImpl
@@ -13,33 +16,33 @@ import com.hjhan.hyejeong.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val viewModel by lazy {
-        val remoteDataSourceImpl = NaverRemoteDataSourceImpl()
-        val localDataSourceImpl = NaverLocalDataSourceImpl()
-        MainViewModel(NaverRepositoryImpl(remoteDataSourceImpl, localDataSourceImpl))
+    private val viewModel: MainViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val remoteDataSourceImpl = NaverRemoteDataSourceImpl()
+                val localDataSourceImpl = NaverLocalDataSourceImpl()
+                return MainViewModel(NaverRepositoryImpl(remoteDataSourceImpl, localDataSourceImpl)) as T
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.activity = this
         binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         setViewModelEvent()
     }
 
     private fun setViewModelEvent() {
-        viewModel.onFailedEvent.addOnPropertyChangedCallback(object :
-            Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                Toast.makeText(this@MainActivity, "네트워크 통신 실패.", Toast.LENGTH_SHORT).show()
-            }
+        viewModel.onFailedEvent.observe(this, Observer {
+            Toast.makeText(this@MainActivity, "네트워크 통신 실패.", Toast.LENGTH_SHORT).show()
         })
 
-        viewModel.onEmptyQuery.addOnPropertyChangedCallback(object :
-            Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                Toast.makeText(this@MainActivity, "제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
-            }
+        viewModel.onEmptyQuery.observe(this, Observer {
+            Toast.makeText(this@MainActivity, "제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
         })
     }
 
