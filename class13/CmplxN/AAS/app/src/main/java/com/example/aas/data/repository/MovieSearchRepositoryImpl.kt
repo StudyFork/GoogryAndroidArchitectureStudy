3e466f5da8324ea74e3dbcf44a634d991a027e9f
CmplxN <zayne@naver.com>
2020-10-18 00:12:19 +0900

    HW-7 : apply Hilt to Repository

--- class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/repository/MovieSearchRepositoryImpl.kt
+++ class13/CmplxN/AAS/app/src/main/java/com/example/aas/data/repository/MovieSearchRepositoryImpl.kt
@@ -2,14 +2,19 @@ package com.example.aas.data.repository
 
 import com.example.aas.data.model.ApiResult
 import com.example.aas.data.source.local.LocalDataSource
-import com.example.aas.data.source.local.LocalDataSourceImpl
 import com.example.aas.data.source.remote.RemoteDataSource
-import com.example.aas.data.source.remote.RemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import io.reactivex.Single
+import javax.inject.Inject
+import javax.inject.Singleton
 
-object MovieSearchRepositoryImpl : MovieSearchRepository {
-    private val localDataSource: LocalDataSource = LocalDataSourceImpl()
-    private val remoteDataSource: RemoteDataSource = RemoteDataSourceImpl()
+class MovieSearchRepositoryImpl @Inject constructor(
+    private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource
+) : MovieSearchRepository {
 
     override fun getMovies(query: String): Single<ApiResult> {
         return remoteDataSource.getMovies(query)
@@ -21,4 +26,12 @@ object MovieSearchRepositoryImpl : MovieSearchRepository {
     override fun onDestroy() {
         localDataSource.onDestroy()
     }
+}
+
+@Module
+@InstallIn(ApplicationComponent::class)
+abstract class MovieSearchRepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMovieSearchRepository(MovieSearchRepositoryImpl: MovieSearchRepositoryImpl): MovieSearchRepository
 }
