d6d1b4b30748cb9043b21734e9128a7942619d2e
HwangJunHong <ghkdwnsghd1@naver.com>
2020-10-18 02:26:59 +0900

    HW-7 : ADD HiltAndroidApp application

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/MyApplication.kt
+++ class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/MyApplication.kt
@@ -2,7 +2,9 @@ package com.hong.architecturestudy
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication : Application() {
 
     init {
