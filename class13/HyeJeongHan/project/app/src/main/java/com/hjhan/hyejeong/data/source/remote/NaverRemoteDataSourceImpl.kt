f6c1a385bfba1ddb7afd5f27a57c7b8a5a1c155d
한혜정 <assg934@gmail.com>
2020-10-19 21:51:26 +0900

    HW-7 modify DataSourceImpl

--- class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/data/source/remote/NaverRemoteDataSourceImpl.kt
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/data/source/remote/NaverRemoteDataSourceImpl.kt
@@ -3,18 +3,26 @@ package com.hjhan.hyejeong.data.source.remote
 import com.hjhan.hyejeong.data.model.MovieData
 import com.hjhan.hyejeong.data.network.NaverApi
 import com.hjhan.hyejeong.data.network.ServiceClient
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class NaverRemoteDataSourceImpl : NaverRemoteDataSource {
+class NaverRemoteDataSourceImpl @Inject constructor(
+    private val serviceClient: ServiceClient
+) : NaverRemoteDataSource {
 
     override fun getSearchMovies(
         query: String,
         success: (MovieData) -> Unit,
         failed: (String) -> Unit
     ) {
-        val api = ServiceClient.createService(NaverApi::class.java)
+        val api = serviceClient.createService(NaverApi::class.java)
         api.getMovies(query, 10, 100).enqueue(object : Callback<MovieData> {
             override fun onResponse(call: Call<MovieData>, response: Response<MovieData>) {
 
@@ -33,4 +41,14 @@ class NaverRemoteDataSourceImpl : NaverRemoteDataSource {
             }
         })
     }
+}
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
+abstract class RemoteDataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteDataSource(remoteDataSourceImpl: NaverRemoteDataSourceImpl): NaverRemoteDataSource
 }
