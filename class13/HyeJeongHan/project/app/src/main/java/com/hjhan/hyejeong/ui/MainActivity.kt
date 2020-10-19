0a16bf1e20e9e1d3a1ea9ee8be668f83c5184edd
한혜정 <assg934@gmail.com>
2020-10-19 21:53:21 +0900

    HW-7 modify MainActivity and add hilt annotation

--- class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MainActivity.kt
+++ class13/HyeJeongHan/project/app/src/main/java/com/hjhan/hyejeong/ui/MainActivity.kt
@@ -4,28 +4,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.hjhan.hyejeong.R
-import com.hjhan.hyejeong.data.repository.NaverRepositoryImpl
-import com.hjhan.hyejeong.data.source.local.NaverLocalDataSourceImpl
-import com.hjhan.hyejeong.data.source.remote.NaverRemoteDataSourceImpl
 import com.hjhan.hyejeong.databinding.ActivityMainBinding
 import com.hjhan.hyejeong.ui.QueryHistoryDialog.Companion.HISTORY_DIALOG_TAG
 import com.hjhan.hyejeong.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val viewModel: MainViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            @Suppress("UNCHECKED_CAST")
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                val remoteDataSourceImpl = NaverRemoteDataSourceImpl()
-                val localDataSourceImpl = NaverLocalDataSourceImpl()
-                return MainViewModel(NaverRepositoryImpl(remoteDataSourceImpl, localDataSourceImpl)) as T
-            }
-        }
-    }
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
