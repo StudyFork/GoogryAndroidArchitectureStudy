771fdb1279c3ea37bebbf28a28b11d777f52bf25
Hwangjunhong <33698885+Hwangjunhong@users.noreply.github.com>
2020-10-26 03:28:34 +0900

    Revert "Test"

--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/constants/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.hong.architecturestudy.constants
-
-object Constants {
-    const val BASE_URL = "https://openapi.naver.com/"
-    const val DEFAULT_TIME_OUT: Long = 15
-}
--- class13/Hwangjunhong/ArchitectureStudy/app/src/main/java/com/hong/architecturestudy/utils/Log.kt
+++
@@ -1,8 +0,0 @@
-package com.hong.architecturestudy.utils
-
-import android.util.Log
-
-fun log(message: String) {
-    Log.d("hjh", message)
-}
-
