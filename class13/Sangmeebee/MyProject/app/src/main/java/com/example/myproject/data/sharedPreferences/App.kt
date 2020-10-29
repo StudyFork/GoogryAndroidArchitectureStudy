8e88cb6b104407331b3e7a789f578b0c1adfc975
sangminkim <apfhdznzl@gmail.com>
2020-10-30 02:12:53 +0900

    HW-7 ADD hilt annotation in Application class

--- class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/data/sharedPreferences/App.kt
+++ class13/Sangmeebee/MyProject/app/src/main/java/com/example/myproject/data/sharedPreferences/App.kt
@@ -1,7 +1,9 @@
 package com.example.myproject.data.sharedPreferences
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
