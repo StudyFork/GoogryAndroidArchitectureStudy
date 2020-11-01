934b0442a49af81b4ba09f7ba3157b34873e3ad5
simuelunbo <simuelunbo@gmail.com>
2020-11-01 22:38:55 +0900

    HW-1 : Retrofit 인터페이스 추가

---
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/api/NaverMovieInterface.kt
@@ -0,0 +1,51 @@
+package com.example.androidarchitecturestudy.api
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NaverMovieInterface {
+    @GET("/v1/search/movie.json")
+    fun searchMovies(
+        @Query("query") query: String,
+    ): Call<MovieResponse>
+
+    companion object{
+        private const val BASE_URL = "https://openapi.naver.com/"
+        private const val CLIENT_ID = "zoFgl2SLHGQXO2WoadKj"
+        private const val CLIENT_SECRET = "Tx2eCHuLZ6"
+
+        fun create(): NaverMovieInterface {
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader(
+                        "X-Naver-Client-Id",
+                        CLIENT_ID
+                    )
+                    .addHeader(
+                        "X-Naver-Client-Secret",
+                        CLIENT_SECRET
+                    )
+                    .build()
+                return@Interceptor it.proceed(request)
+
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(NaverMovieInterface::class.java)
+        }
+
+    }
+}
