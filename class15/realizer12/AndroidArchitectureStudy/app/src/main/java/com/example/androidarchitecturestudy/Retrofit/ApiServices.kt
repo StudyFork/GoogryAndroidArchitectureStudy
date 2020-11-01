7e6d1c13487384fd792336ad247d798c2de861d7
leedonghun <dlwldlwldus123@gmail.com>
2020-11-01 19:43:33 +0900

    HW-1 data class 구성

--- class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/ApiServices.kt
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/ApiServices.kt
@@ -1,6 +1,6 @@
 package com.example.androidarchitecturestudy.Retrofit
 
-import okhttp3.ResponseBody
+import com.example.androidarchitecturestudy.data.GetMovieInfo
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -9,9 +9,11 @@ import retrofit2.http.Query
 
 interface ApiServices {
 
-    @Headers("X-Naver-Client-Id: {67znsYa_iJOrjYNeChNL}", "X-Naver-Client-Secret: {V2h_LIRLgC}" )
+    @Headers("X-Naver-Client-Id: {67znsYa_iJOrjYNeChNL}", "X-Naver-Client-Secret: {V2h_LIRLgC}")
     @GET("movie.json")
-    fun getMovieSearchResult(@Query("query") movieName:String,
-                             @Query("display")resultCount:Int): Call<ResponseBody>
+    fun getMovieSearchResult(
+        @Query("query") movieName: String,
+        @Query("display") resultCount: Int
+    ): Call<GetMovieInfo>
 
 }
---
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/data/GetMovieInfo.kt
@@ -0,0 +1,24 @@
+package com.example.androidarchitecturestudy.data
+
+import com.google.gson.annotations.SerializedName
+
+class GetMovieInfo {
+
+    //검색 쿼리에 따른 네이버 영화 리스트
+    data class MovieList(
+        @SerializedName("items")
+        val items: List<MovieData>
+    )
+
+    //각 영화별 구성된 데이터
+    data class MovieData(
+        @SerializedName("image")
+        val image: String,
+        @SerializedName("link")
+        val link: String,
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("userRating")
+        val userRating: String
+    )
+}
