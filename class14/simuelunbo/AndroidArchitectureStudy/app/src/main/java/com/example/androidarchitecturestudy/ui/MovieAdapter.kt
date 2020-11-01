6d7553a7a022f5f87b6f30ab6907aa09fe6badf9
simuelunbo <simuelunbo@gmail.com>
2020-11-01 22:43:21 +0900

    HW-1 :  RecyclerView Adapter, Holder 추가

---
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MovieAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.androidarchitecturestudy.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidarchitecturestudy.R
+import com.example.androidarchitecturestudy.model.Movie
+
+class MovieAdapter : RecyclerView.Adapter<MovieHolder>() {
+    private var movies = ArrayList<Movie>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return MovieHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+       holder.bind(movies[position])
+    }
+
+    override fun getItemCount() = movies.count()
+
+    fun setMovieList(movieList: ArrayList<Movie>){
+        this.movies.clear()
+        this.movies.addAll(movieList)
+        notifyDataSetChanged()
+    }
+}
