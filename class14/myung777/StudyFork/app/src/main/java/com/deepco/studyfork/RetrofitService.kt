781a2fc7ff420926f1caa0dd1c7a1159b709cad7
myung777 <ae3994@naver.com>
2020-11-02 00:14:41 +0900

    레트로핏 설정

---
+++ class14/myung777/StudyFork/app/src/main/java/com/deepco/studyfork/RetrofitService.kt
@@ -0,0 +1,52 @@
+package com.deepco.studyfork
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+
+    @GET("v1/search/movie.json")
+    fun getSearchNews(
+        @Query("query") query: String,
+        @Query("display") display: Int? = null,
+        @Query("start") start: Int? = null
+    ): Call<MovieData>
+
+    companion object {
+        private const val BASE_URL_NAVER_API = "https://openapi.naver.com/"
+        private const val CLIENT_ID = "hNSL7xP8VekVAD6qbXn4"
+        private const val CLIENT_SECRET = "PAzBOvOWow"
+
+        fun create(): RetrofitService {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("X-Naver-Client-Id", CLIENT_ID)
+                    .addHeader("X-Naver-Client-Secret", CLIENT_SECRET)
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL_NAVER_API)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RetrofitService::class.java)
+        }
+    }
+}
