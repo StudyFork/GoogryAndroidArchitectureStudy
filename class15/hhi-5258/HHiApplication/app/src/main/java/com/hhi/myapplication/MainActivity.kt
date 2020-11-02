53ce4b5470d29abc353ec10c5807a00a9c806316
hhi <hhanhi5258@gmail.com>
2020-11-02 18:13:56 +0900

    HW-1 mainActivity recyclerview, button click 설정

--- class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/MainActivity.kt
+++ class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.hhi.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val recyclerAdapter = RecyclerAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        main_recyclerview.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        main_recyclerview.setHasFixedSize(false)
+        main_recyclerview.adapter = recyclerAdapter
+        main_btn_search.setOnClickListener {
+            if (main_edit_search.text.isEmpty()) {
+                return@setOnClickListener
+            } else {
+
+            }
+        }
     }
 }
