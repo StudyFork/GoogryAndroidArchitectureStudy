320a71a11897c39403330ff03a0c9c6ed5f221be
leedonghun <dlwldlwldus123@gmail.com>
2020-11-04 01:18:06 +0900

    HW-1 code내 typo 및 warning 수정

--- class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/ServerIp.kt
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/ServerIp.kt
@@ -3,6 +3,6 @@ package com.example.androidarchitecturestudy.Retrofit
 interface ServerIp {
     companion object{
         //네이버 영화 api 서버 url
-        val naverMovieApiUrl:String="https://openapi.naver.com/v1/search/"
+        const val naverMovieApiUrl:String="https://openapi.naver.com/v1/search/"
     }
 }
