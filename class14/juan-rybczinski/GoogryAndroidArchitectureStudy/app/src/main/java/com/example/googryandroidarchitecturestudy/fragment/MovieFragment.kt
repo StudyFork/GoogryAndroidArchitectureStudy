b8e169182c8d82cff101a572d588426eec093585
juan-rybczinski <jhreplay.lee@gmail.com>
2020-11-03 14:40:52 +0900

    HW-1 Add MovieFragment

---
+++ class14/juan-rybczinski/GoogryAndroidArchitectureStudy/app/src/main/java/com/example/googryandroidarchitecturestudy/fragment/MovieFragment.kt
@@ -0,0 +1,21 @@
+package com.example.googryandroidarchitecturestudy.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.googryandroidarchitecturestudy.R
+
+class MovieFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+}
