d46bc5d395ccdbd8bc439df751ed2aa6e9a3830d
leedonghun <dlwldlwldus123@gmail.com>
2020-11-04 01:03:00 +0900

    HW-1 RetrofitClient, ServerIp -> 불필요한 줄바꿈 수정

--- class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/RetrofitClient.kt
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/Retrofit/RetrofitClient.kt
@@ -4,13 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient(url:String) {
-
-
     private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(url)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-
     var apiService:ApiServices=retrofit.create(ApiServices::class.java)
-
 }
