94d98110657d4b6c74a4114c43e01c59b8ba865b
simuelunbo <simuelunbo@gmail.com>
2020-11-04 12:42:52 +0900

    HW-1: 감독 이름 필요없는 단어 필터 추가

--- class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MovieHolder.kt
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MovieHolder.kt
@@ -6,15 +6,20 @@ import com.bumptech.glide.Glide
 import com.example.androidarchitecturestudy.model.Movie
 import kotlinx.android.synthetic.main.item_movie.view.*
 
-class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    fun bind(movie: Movie){
+class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(movie: Movie) {
         itemView.tv_movie_title.text = removeUselessTitle(movie.title)
         itemView.tv_pub_date.text = movie.pubDate
         itemView.tv_director_name.text = movie.director
         itemView.tv_movie_rate.text = movie.userRating
         Glide.with(itemView).load(movie.image).into(itemView.iv_movie)
     }
-    private fun removeUselessTitle(title: String?): String?{
+
+    private fun removeUselessTitle(title: String?): String? {
         return title?.replace("<b>", "")?.replace("</b>", "")
+    }
+
+    private fun removeUselessDirector(director: String?): String? {
+        return director?.replace("|", "")
     }
 }
