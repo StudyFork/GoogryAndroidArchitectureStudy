b03994fe0144515dc1414362f832a7be35674984
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-11-05 22:49:40 +0900

    HW-1 WebRequestManager는 객체화시켜서 사용하지 않기 때문에 object로 변경.

--- class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/api/WebRequestManager.kt
+++ class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/api/WebRequestManager.kt
@@ -1,30 +1,26 @@
 package kr.dktsudgg.androidarchitecturestudy.api
 
-import android.util.Log
 import kr.dktsudgg.androidarchitecturestudy.api.naver.NaverMovieApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.IllegalArgumentException
 import kotlin.reflect.KClass
 
-class WebRequestManager {
+object WebRequestManager {
 
-    companion object {
-        val NAVER_OPEN_API_URL = "https://openapi.naver.com/"
+    val NAVER_OPEN_API_URL = "https://openapi.naver.com/"
 
-        fun <T : Any> getInstance(serviceInterface: KClass<T>): T? {
-            val retrofit = Retrofit.Builder();
+    fun <T : Any> getInstance(serviceInterface: KClass<T>): T? {
+        val retrofit = Retrofit.Builder()
 
-            when (serviceInterface) {
-                NaverMovieApi::class -> {   // 네이버 영화 API통신 객체 생성
-                    val retrofitBuilder = retrofit.baseUrl(NAVER_OPEN_API_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                    return retrofitBuilder.create(serviceInterface.java)
-                }
-                else -> {   // 정의되지 않은 API로 요청 시..
-                    throw IllegalArgumentException("잘못된 API 객체 요청..")
-                }
+        when (serviceInterface) {
+            NaverMovieApi::class -> {   // 네이버 영화 API통신 객체 생성
+                val retrofitBuilder = retrofit.baseUrl(NAVER_OPEN_API_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                return retrofitBuilder.create(serviceInterface.java)
+            }
+            else -> {   // 정의되지 않은 API로 요청 시..
+                throw IllegalArgumentException("잘못된 API 객체 요청..")
             }
         }
     }
