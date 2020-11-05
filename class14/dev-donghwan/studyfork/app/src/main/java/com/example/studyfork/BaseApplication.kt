026e220ab1b3a3964ac621bcde080b117fc1f589
김동환 <donghwan@gimdonghwan-ui-MacBookPro.local>
2020-11-05 10:33:04 +0900

    HW-1 Add BaseApplication Setting

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/BaseApplication.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/BaseApplication.kt
@@ -1,4 +1,13 @@
 package com.example.studyfork
 
-class BaseApplication {
+import android.app.Application
+import io.reactivex.plugins.RxJavaPlugins
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        RxJavaPlugins.setErrorHandler {
+            it.printStackTrace()
+        }
+    }
 }
