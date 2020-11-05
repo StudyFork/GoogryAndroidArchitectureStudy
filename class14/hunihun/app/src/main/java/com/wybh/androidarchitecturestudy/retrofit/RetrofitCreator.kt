f2be3c03e04b702bbf33b1ca7bac35e2f2d65836
sanghun <box6687@naver.com>
2020-11-06 01:23:27 +0900

    HW-1 - RetrofitCreator class, object 로 변경

--- class14/hunihun/app/src/main/java/com/wybh/androidarchitecturestudy/retrofit/RetrofitCreator.kt
+++ class14/hunihun/app/src/main/java/com/wybh/androidarchitecturestudy/retrofit/RetrofitCreator.kt
@@ -5,28 +5,25 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitCreator {
+object RetrofitCreator {
+    private const val API_BASE_URL = "https://openapi.naver.com/"
 
-    companion object {
-        private const val API_BASE_URL = "https://openapi.naver.com/"
+    private fun defaultRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createOkHttpClient())
+            .build()
+    }
 
-        private fun defaultRetrofit(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl(API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(createOkHttpClient())
-                .build()
-        }
+    fun <T> create(service: Class<T>): T {
+        return defaultRetrofit().create(service)
+    }
 
-        fun <T> create(service: Class<T>): T {
-            return defaultRetrofit().create(service)
-        }
-
-        private fun createOkHttpClient(): OkHttpClient {
-            return OkHttpClient.Builder()
-                .addInterceptor(RetrofitInterceptor())
-                .build()
-        }
+    private fun createOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(RetrofitInterceptor())
+            .build()
     }
 }
