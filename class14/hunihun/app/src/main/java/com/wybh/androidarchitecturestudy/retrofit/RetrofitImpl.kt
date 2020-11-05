61829a9f3a69a740f5999a7bd820474e438c3bf3
sanghun <box6687@naver.com>
2020-11-06 01:24:39 +0900

    HW-1 - rx Observable >> Single 로변경

--- class14/hunihun/app/src/main/java/com/wybh/androidarchitecturestudy/retrofit/RetrofitImpl.kt
+++ class14/hunihun/app/src/main/java/com/wybh/androidarchitecturestudy/retrofit/RetrofitImpl.kt
@@ -1,12 +1,12 @@
 package com.wybh.androidarchitecturestudy.retrofit
 
 import com.wybh.androidarchitecturestudy.model.ResponseCinemaData
-import io.reactivex.Observable
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RetrofitImpl {
     
     @GET("v1/search/movie.json")
-    fun getCinemaData(@Query("query") query: String?): Observable<ResponseCinemaData>
+    fun getCinemaData(@Query("query") query: String?): Single<ResponseCinemaData>
 }
