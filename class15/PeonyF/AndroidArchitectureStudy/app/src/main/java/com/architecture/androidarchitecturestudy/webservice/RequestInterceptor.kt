f8c45d647c03e7f9d7caf172edecc841ed97f43a
MJ Choi <1621139@gmail.com>
2020-11-05 18:17:35 +0900

    HW-1 UPDATE hiding api keys in local.properties

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/webservice/RequestInterceptor.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/webservice/RequestInterceptor.kt
@@ -1,6 +1,7 @@
 package com.architecture.androidarchitecturestudy.webservice
 
-import com.architecture.androidarchitecturestudy.util.Config
+
+import com.architecture.androidarchitecturestudy.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -8,8 +9,8 @@ internal class RequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response =
         chain.proceed(
             chain.request().newBuilder()
-                .addHeader("X-Naver-Client-Id", Config.CLIENT_ID)
-                .addHeader("X-Naver-Client-Secret", Config.CLIENT_SECRET)
+                .addHeader("X-Naver-Client-Id", BuildConfig.CLIENT_ID)
+                .addHeader("X-Naver-Client-Secret", BuildConfig.CLIENT_SECRET)
                 .build()
         )
 }
