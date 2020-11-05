6e16bc13d71e9089b57b1b2243bd0bab77baf055
Jihoi-Kang <jihoi.kang@gmail.com>
2020-11-05 16:52:47 +0900

    HW-1 Add suspend movie service

--- class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/ui/MovieActivity.kt
+++ class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/ui/MovieActivity.kt
@@ -10,18 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.jay.aas.R
-import com.jay.aas.api.MovieResponse
 import com.jay.aas.api.MovieService
 import com.jay.aas.api.RetrofitHelper
 import com.jay.aas.databinding.ActivityMovieBinding
-import com.jay.aas.model.Movie
 import com.jay.aas.util.toast
 import kotlinx.coroutines.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.resume
 
 class MovieActivity : AppCompatActivity(), CoroutineScope {
 
@@ -77,42 +71,25 @@ class MovieActivity : AppCompatActivity(), CoroutineScope {
 
         launch {
             inputMethodManager.hideSoftInputFromWindow(binding.evSearch.windowToken, 0)
-            val movies = search(query)
-            when {
-                movies == null -> toast(getString(R.string.msg_search_failed))
-                movies.isEmpty() -> {
+            try {
+                val response = movieService.searchMovies(query)
+                val movies = response.items
+
+                if (movies.isEmpty()) {
                     binding.tvNoResult.isVisible = true
                     binding.rvMovie.isGone = true
-                }
-                else -> {
+                } else {
                     binding.tvNoResult.isGone = true
                     binding.rvMovie.isVisible = true
 
                     movieAdapter.setMovies(movies)
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                toast(getString(R.string.msg_search_failed))
+                cancel()
             }
         }
     }
-
-    private suspend fun search(query: String) =
-        suspendCancellableCoroutine<List<Movie>?> { continuation ->
-            movieService.searchMovies(query).enqueue(object : Callback<MovieResponse> {
-                override fun onResponse(
-                    call: Call<MovieResponse>,
-                    response: Response<MovieResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        continuation.resume(response.body()?.items ?: emptyList())
-                    } else {
-                        continuation.resume(null)
-                    }
-                }
-
-                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                    t.printStackTrace()
-                    continuation.resume(null)
-                }
-            })
-        }
 
 }
