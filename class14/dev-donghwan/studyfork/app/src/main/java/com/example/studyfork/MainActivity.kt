37cafea7c41868299e2a4a981415135f6c9768ef
김동환 <donghwan@gimdonghwan-ui-MacBookPro.local>
2020-11-10 00:37:42 +0900

    HW2 - Modify Main Activity

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/MainActivity.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/MainActivity.kt
@@ -2,17 +2,15 @@ package com.example.studyfork
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.studyfork.model.Network
-import com.example.studyfork.model.toDomain
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.rxkotlin.addTo
+import com.example.studyfork.model.RemoteDataSourceImpl
+import com.example.studyfork.model.RepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val repository by lazy {
+        RepositoryImpl(RemoteDataSourceImpl())
+    }
     private val recyclerAdapter by lazy { MovieRecyclerAdapter() }
-    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         btn_search.setOnClickListener {
             edt_query.text?.run {
-                Network.naverApi.searchMovie(this.toString())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { response ->
-                        recyclerAdapter.itemChange(response.toDomain())
-                    }.addTo(compositeDisposable)
+                repository.searchMovie(this.toString(),
+                    {
+                        it.items.run {
+                            recyclerAdapter.itemChange(this)
+                        }
+                    }, {
+                        it.printStackTrace()
+                    })
             }
         }
-    }
-
-    override fun onDestroy() {
-        compositeDisposable.clear()
-        super.onDestroy()
     }
 }
