6d26ea29f1d1f0e3e8aaf6a22cc653834b9550de
leedonghun <dlwldlwldus123@gmail.com>
2020-11-11 01:39:39 +0900

    HW-2 mainacitivy에 기존  retorifit 통신 코드 없애고   Repository 적용

--- class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/MainActivity.kt
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.androidarchitecturestudy.Retrofit.RetrofitClient
-import com.example.androidarchitecturestudy.Retrofit.ServerIp
+import com.example.androidarchitecturestudy.retrofit.RetrofitClient
+import com.example.androidarchitecturestudy.retrofit.ServerIp
 import com.example.androidarchitecturestudy.adapter.MovieListRecyclerViewAdapter
 import com.example.androidarchitecturestudy.data.GetMovieInfo
+import com.example.androidarchitecturestudy.data.repository.MovieRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     // 리사이클러뷰 어뎁터
     private lateinit var recyclerViewAdapter: MovieListRecyclerViewAdapter
+
+
+    private val movieRepositoryImp = MovieRepositoryImpl()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,62 +52,39 @@ class MainActivity : AppCompatActivity() {
     // 영화 검색 실행
     private fun initEventListeners() {
 
-
         // 검색 버튼 눌릴때
         btn_main_search_movie.setOnClickListener {
-
-            // 영화 검색 실행
             getMovieData(edit_main_search_movie.text.toString())
         }
 
-
         // edittext search action 눌릴때
         edit_main_search_movie.setOnEditorActionListener { _, actionId, _ ->
-
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-
-                //영화 검색 실행
                 getMovieData(edit_main_search_movie.text.toString())
+                true
+            }else{
+                false
             }
-            true
         }
 
     }// searchMovie
 
 
+
+
+
     // 서버에  영화 데이터 받아옴
     // TODO: 2020/11/01 추후  코루틴 또는 Rx로 변경하기
     private fun getMovieData(searchQuery: String) {
-        RetrofitClient(ServerIp.naverMovieApiUrl).apiService.getMovieSearchResult(searchQuery)
-            .enqueue(object : Callback<GetMovieInfo.MovieList> {
+        //영화 검색 실행
+        movieRepositoryImp.getMovieSearchResult(searchQuery,{
+            it.movieList?.let { it->
+                updateRecyclerView(it)
+            }
 
-                override fun onResponse(
-                    call: Call<GetMovieInfo.MovieList>,
-                    response: Response<GetMovieInfo.MovieList>
-                ) {
-
-                    // 응답 성공
-                    if (response.isSuccessful) {
-                        val movieList = response.body()?.movieList
-
-                        if (movieList != null) {
-                            updateRecyclerView(movieList = movieList)
-                        }
-
-                    } else {// 응답 실패
-                        Log.v("check_log", response.code().toString())
-                        Log.v("check_log", response.errorBody()?.string().toString())
-                    }
-
-                }
-
-
-                override fun onFailure(call: Call<GetMovieInfo.MovieList>, t: Throwable) {
-                    Log.e("check_log", t.message.toString())
-
-                }
-
-            })
+        },{
+            Log.v("check_log", it.message.toString())
+        })
     }// getMovieData 끝
 
 }
