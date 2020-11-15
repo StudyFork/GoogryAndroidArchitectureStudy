02b78b0cd87e1cdf5a783fde6c91567c82c9f9da
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-15 16:12:39 +0900

    MainActivity MainContract의 View 상속 후 UI처리 함수 오버라이딩

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/MainActivity.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/MainActivity.kt
@@ -3,59 +3,58 @@ package com.example.hw2_project
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hw2_project.data.MovieList
 import com.example.hw2_project.data.repository.MovieRepositoryImpl
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.View {
 
     private lateinit var editTextMovieName : EditText
     private lateinit var buttonSearch : Button
     private lateinit var recyclerView : RecyclerView
 
-    //어댑터 선언
     private val adapter = RecyclerViewAdapter()
 
-    //레퍼지토리 선언
     private val movieRepositoryImp = MovieRepositoryImpl()
+
+    private lateinit var presenter: Presenter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         buttonSearch = findViewById<Button>(R.id.button_search)
         editTextMovieName = findViewById<EditText>(R.id.edittext_movie_name)
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
 
-        //리사이클러뷰 동일한 크기의 아이템 사용
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = adapter
 
         presenter = Presenter(this, movieRepositoryImp)
-        buttonSearch.setOnClickListener {
 
-            //사용자가 EditTextView에 아무값도 넣지 않았을 때
-            if (editTextMovieName.text.isEmpty()) {
-                Toast.makeText(this, "영화 제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            movieRepositoryImp.getMovieList(
-                editTextMovieName.text.toString(),
-                success = {
-                    runOnUiThread{
-                        adapter.movieListChange(it.items)
-                    }
-                },
-                fail = { Log.e("getMovieList","Error in movieRepositoryImp")}
-            )
-
-            //키보드 내리기
-            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(editTextMovieName.windowToken, 0)
+        buttonSearch.findViewById<Button>(R.id.button_search).setOnClickListener {
+            presenter.requestMovieListToRepo(editTextMovieName.text.toString())
         }
+    }
+
+    override fun showErrorEmptyQuery() {
+        Toast.makeText(this, "영화제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showMovieList(movieList: MovieList) {
+        //키보드 내리기
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(editTextMovieName.windowToken, 0)
+
+        runOnUiThread{
+            adapter.movieListChange(movieList.items)
+        }
+    }
+
+    override fun showErrorRespondMsg(t: Throwable) {
+        Toast.makeText(this, "에러 메시지 : $t", Toast.LENGTH_LONG).show()
     }
 
 }
