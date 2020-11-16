e09a96feff17ca1d39566637130d5d60a996098e
leedonghun <dlwldlwldus123@gmail.com>
2020-11-17 00:21:17 +0900

    HW-3 presenter 폴더에  MovieContract 인터페이스 추가

---
+++ class15/realizer12/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/presenter/MovieContract.kt
@@ -0,0 +1,18 @@
+package com.example.androidarchitecturestudy.presenter
+
+import com.example.androidarchitecturestudy.data.GetMovieInfo
+
+interface MovieContract {
+    //뷰에서 할일
+    interface View {
+        fun showMovieResultEmpty()
+        fun showSearchQueryEmpty()
+        fun hideKeyBoard()
+        fun updateRecyclerView(movieList: List<GetMovieInfo.MovieData>)
+    }
+
+    //프레젠터에서 할일
+    interface Presenter {
+        fun getMovieData(searchQuery: String)
+    }
+}
