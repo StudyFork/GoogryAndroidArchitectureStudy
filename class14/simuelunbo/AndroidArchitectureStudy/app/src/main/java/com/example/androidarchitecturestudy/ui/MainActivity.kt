936589e5f924851a9f704d9ef431bf2f2b634f76
simuelunbo <simuelunbo@gmail.com>
2020-11-16 17:48:46 +0900

    HW-3: MainActivity View 처리

--- class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MainActivity.kt
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidarchitecturestudy.ui
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -9,15 +11,25 @@ import androidx.core.view.isVisible
 import com.example.androidarchitecturestudy.R
 import com.example.androidarchitecturestudy.data.local.NaverLocalDataSourceImpl
 import com.example.androidarchitecturestudy.data.model.Movie
+import com.example.androidarchitecturestudy.data.model.MovieData
 import com.example.androidarchitecturestudy.data.remote.NaverRemoteDataSourceImpl
 import com.example.androidarchitecturestudy.data.repository.NaverRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    private var movieAdapter: MovieAdapter = MovieAdapter()
+class MainActivity : AppCompatActivity(), MainContract.View {
     private val remoteMovieDataImpl = NaverRemoteDataSourceImpl()
     private val localMovieDataImpl = NaverLocalDataSourceImpl()
     private val repositoryMovieImpl = NaverRepositoryImpl(remoteMovieDataImpl, localMovieDataImpl)
+
+    private val mainPresenter by lazy {
+        MainPresenter(this, repositoryMovieImpl)
+    }
+
+    private val movieAdapter: MovieAdapter by lazy {
+        MovieAdapter { link ->
+            mainPresenter.openMovieLink(link)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,38 +38,47 @@ class MainActivity : AppCompatActivity() {
 
         btn_search.setOnClickListener {
             val searchText = et_search.text.toString()
-            if (searchText.isEmpty()) {
-                Toast.makeText(this, "검색을 원하시는 영화 제목을 입력해 주세요", Toast.LENGTH_SHORT).show()
-            } else {
-                hideKeyboard(this)
-                progressBar.isVisible = true
-                requestMovieInfo(searchText)
-            }
+            mainPresenter.requestMovieInfo(searchText)
         }
     }
 
-    private fun hideKeyboard(context: Context) {
+    private fun initRecyclerView() {
+        mainPresenter.requestLocalMovieData()
+        rcv_result.adapter = movieAdapter
+    }
+
+    override fun hideKeyboard(context: Context) {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(et_search.windowToken, 0)
     }
 
-    private fun initRecyclerView() {
-        repositoryMovieImpl.getMovieData()?.let { movieAdapter.setMovieList(it) }
-        rcv_result.adapter = movieAdapter
+    override fun showLoadingBar() {
+        progressBar.isVisible = true
     }
 
-    private fun requestMovieInfo(query: String) {
-        repositoryMovieImpl.getSearchMovieList(
-            query = query,
-            success = {
-                movieAdapter.setMovieList(it.items as ArrayList<Movie>)
-                repositoryMovieImpl.saveMovieData(it.items)
-                progressBar.isVisible = false
-            },
-            failed = {
-                Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
-                progressBar.isVisible = false
-            })
-
+    override fun hideLoadingBar() {
+        progressBar.isVisible = false
     }
+
+    override fun showQueryEmpty() {
+        Toast.makeText(this, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showResultEmpty(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showResult(movieData: MovieData) {
+        movieAdapter.setMovieList(movieData.items as ArrayList<Movie>)
+        mainPresenter.saveMovieData(movieData.items)
+    }
+
+    override fun getMovieData(movie: List<Movie>) {
+        movieAdapter.setMovieList(movie)
+    }
+
+    override fun openMovieLink(link: String) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
+    }
+
 }
