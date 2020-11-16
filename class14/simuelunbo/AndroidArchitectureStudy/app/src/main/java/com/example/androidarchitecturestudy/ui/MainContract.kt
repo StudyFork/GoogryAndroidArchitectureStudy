bc60c8fa96f22fc6304d988acf92d272af34c65f
simuelunbo <simuelunbo@gmail.com>
2020-11-16 17:43:58 +0900

    HW-3: Contract 추가

---
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/MainContract.kt
@@ -0,0 +1,25 @@
+package com.example.androidarchitecturestudy.ui
+
+import android.content.Context
+import com.example.androidarchitecturestudy.data.model.Movie
+import com.example.androidarchitecturestudy.data.model.MovieData
+
+interface MainContract {
+    interface View {
+        fun hideKeyboard(context: Context)
+        fun showLoadingBar()
+        fun hideLoadingBar()
+        fun showQueryEmpty()
+        fun showResultEmpty(error: String)
+        fun showResult(movieData: MovieData)
+        fun getMovieData(movie: List<Movie>)
+        fun openMovieLink(link: String)
+    }
+
+    interface Presenter {
+        fun saveMovieData(movie: List<Movie>)
+        fun requestMovieInfo(title: String)
+        fun requestLocalMovieData()
+        fun openMovieLink(link: String)
+    }
+}
