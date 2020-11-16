60477b04d2d56fa8887fc3b5759a07ab54617566
MJ Choi <1621139@gmail.com>
2020-11-16 23:49:24 +0900

    HW-3 ADD main presenter

---
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/MainPresenter.kt
@@ -0,0 +1,23 @@
+package com.architecture.androidarchitecturestudy.ui
+
+import android.util.Log
+import com.architecture.androidarchitecturestudy.data.model.Movie
+import com.architecture.androidarchitecturestudy.data.repository.MovieRepositoryImpl
+
+class MainPresenter(
+    private val view: MainContract.View, private val movieRepository: MovieRepositoryImpl
+) : MainContract.Presenter {
+    override fun findMovie(keyword: String) {
+        if (keyword.isEmpty()) {
+            view.queryNone()
+        } else {
+            movieRepository.getMovieData(
+                keyword,
+                30,
+                onEmptyList = { view.resultNone() },
+                onSuccess = { view.updateMovieRecycler(it.items as ArrayList<Movie>) },
+                onFailure = { Log.e("Api is fail", it.toString()) })
+        }
+        view.removeKeyboard()
+    }
+}
