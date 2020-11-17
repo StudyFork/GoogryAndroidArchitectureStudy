8077d74572138a43ffd8538ec9641bb7903fc88e
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-17 22:32:41 +0900

    API 통신 구현 부분 소스 이동( -> NaverMovieApi.kt)

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/remote/MovieRemoteDataSourceImpl.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/remote/MovieRemoteDataSourceImpl.kt
@@ -2,10 +2,8 @@ package com.example.hw2_project.data.remote
 
 import com.example.hw2_project.data.api.NaverMovieApi
 import com.example.hw2_project.data.api.NaverMovieData
-import okhttp3.*
-import java.io.IOException
-import java.net.URL
-import java.net.URLEncoder
+import retrofit2.Call
+import retrofit2.Response
 
 class MovieRemoteDataSourceImpl : MovieRemoteDataSource {
     companion object{
@@ -14,41 +12,29 @@ class MovieRemoteDataSourceImpl : MovieRemoteDataSource {
         private const val CLIENT_SECRET = "v8aD8p_Ri0"
     }
 
+    private val naverApi = NaverMovieApi.create()
+
     override fun getMovieFromRemote(
         query: String,
         success: (NaverMovieData.NaverMovieResponse) -> Unit,
         fail: (Throwable) -> Unit
     ) {
-        //OkHttp로 요청
-        val text = URLEncoder.encode(query, "UTF-8")
+        naverApi.searchMovieInApi(query).enqueue(object : retrofit2.Callback<NaverMovieData.NaverMovieResponse>{
+            override fun onResponse(
+                call: Call<NaverMovieData.NaverMovieResponse>,
+                response: Response<NaverMovieData.NaverMovieResponse>
+            ) {
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        success(it)
+                    }
+                }
+            }
 
             override fun onFailure(call: Call<NaverMovieData.NaverMovieResponse>, t: Throwable) {
                 fail(t)
-        val request = Request.Builder()
-            .url(url)
-            .addHeader("X-Naver-Client-Id", CLIENT_ID)
-            .addHeader("X-Naver-Client-Secret", CLIENT_SECRET)
-            .method("GET", null)
-            .build()
-
-        //요청을 위한 Client 객체 생성
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                fail(e)
             }
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body()?.string()
 
-                //Gson 라이브러리 사용
-                val gson = GsonBuilder().create()
-                val moviefeed = gson.fromJson<MovieList>(body, MovieList::class.java)
-
-                if(response.isSuccessful){
-                    success(moviefeed)
-                }
-            }
         })
-
     }
 }
