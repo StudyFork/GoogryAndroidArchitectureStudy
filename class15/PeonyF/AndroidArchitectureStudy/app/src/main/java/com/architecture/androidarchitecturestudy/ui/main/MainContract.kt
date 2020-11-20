58dbb11696900ec7e942e6ee634a4b9874348ec5
MJ Choi <1621139@gmail.com>
2020-11-21 01:58:53 +0900

    HW-3 UPDATE change interface implement to BaseContract

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/main/MainContract.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/main/MainContract.kt
@@ -1,9 +1,10 @@
 package com.architecture.androidarchitecturestudy.ui.main
 
 import com.architecture.androidarchitecturestudy.data.model.Movie
+import com.architecture.androidarchitecturestudy.ui.base.BaseContract
 
 interface MainContract {
-    interface View {
+    interface View : BaseContract.View {
         fun removeKeyboard()
         fun updateMovieRecycler(items: List<Movie>)
         fun failMovieGet(msg: String)
@@ -11,7 +12,7 @@ interface MainContract {
         fun noResult()
     }
 
-    interface Presenter {
+    interface Presenter : BaseContract.Presenter {
         fun findMovie(query: String)
     }
 }
