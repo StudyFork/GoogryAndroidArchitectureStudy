bd946a52f165e0ac45f7d2abca4e063161ad6424
hhi <hhanhi5258@gmail.com>
2020-11-22 00:21:42 +0900

    HW-4 DataBinding 환경설정

--- class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/MainActivity.kt
+++ class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/MainActivity.kt
@@ -7,18 +7,21 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import com.hhi.myapplication.base.BaseActivity
 import com.hhi.myapplication.data.model.MovieData
 import com.hhi.myapplication.data.repository.NaverRepositoryDataSourceImpl
+import com.hhi.myapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainContract.Presenter>(), MainContract.View {
+    private lateinit var binding: ActivityMainBinding
     private val recyclerAdapter = RecyclerAdapter()
     private val mainPresenter = MainPresenter(this, NaverRepositoryDataSourceImpl())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setUpUI()
         setUpListener()
