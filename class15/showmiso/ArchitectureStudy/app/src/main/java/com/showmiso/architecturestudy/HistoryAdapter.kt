de3078f55a0583f3ed19d8e44805aa9d59beff77
Miso Park <showmiso@gmail.com>
2020-11-24 22:46:56 +0900

    HW-4 : Create HistoryAdapter

---
+++ class15/showmiso/ArchitectureStudy/app/src/main/java/com/showmiso/architecturestudy/HistoryAdapter.kt
@@ -0,0 +1,52 @@
+package com.showmiso.architecturestudy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_history.view.*
+
+class HistoryAdapter(
+    private val listener: OnHistoryClickListener
+) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
+    private val historyList = mutableListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_history, parent, false)
+        return HistoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        val text = historyList[position]
+        holder.bind(text)
+    }
+
+    override fun getItemCount(): Int = historyList.size
+
+    fun setHistoryList(list: List<String>?) {
+        list?.let {
+            historyList.clear()
+            historyList.addAll(it)
+            notifyDataSetChanged()
+        }
+    }
+
+    inner class HistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(text: String) {
+            itemView.tv_text.text = text
+            itemView.setOnClickListener {
+                listener.onItemClick(text)
+            }
+            itemView.iv_delete.setOnClickListener {
+                listener.onDeleteItem(text)
+                historyList.remove(text)
+                notifyItemRemoved(adapterPosition)
+            }
+        }
+    }
+
+    interface OnHistoryClickListener {
+        fun onItemClick(text: String)
+        fun onDeleteItem(text: String)
+    }
+}
