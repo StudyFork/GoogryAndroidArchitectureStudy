950622cb1fd636f4157fb1b502c15c8a7ccf3ea1
hhi <hhanhi5258@gmail.com>
2020-11-25 21:08:28 +0900

    HW-4 SharedPreferenceUtil get/set 만 하도록 수정

--- class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/data/local/NaverLocalDataSourceImpl.kt
+++ class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/data/local/NaverLocalDataSourceImpl.kt
@@ -1,11 +1,33 @@
 package com.hhi.myapplication.data.local
 
 import com.hhi.myapplication.App
+import org.json.JSONArray
+
+private const val PREF_QUERY_LIST: String = "pref_query_list"
 
 class NaverLocalDataSourceImpl : NaverLocalDataSource {
     override fun saveQuery(query: String) {
-        App.prefs.saveQuery(query)
+        val queryList = getQueryList().toMutableList()
+        queryList.add(query)
+
+        if (queryList.size > 5) {
+            queryList.removeAt(0)
+        }
+
+        App.prefs.setString(JSONArray(queryList).toString(), PREF_QUERY_LIST)
     }
 
-    override fun getQueryList(): List<String> = App.prefs.getQueryList()
+    override fun getQueryList(): List<String> {
+        val queryListJSONString = App.prefs.getString(PREF_QUERY_LIST)
+        val queryList = mutableListOf<String>()
+
+        queryListJSONString?.let {
+            val jsonArray = JSONArray(it)
+            for (i in 0 until jsonArray.length()) {
+                queryList.add(jsonArray[i].toString())
+            }
+        }
+
+        return queryList
+    }
 }
--- class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/data/local/SharedPreferenceUtil.kt
+++ class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/data/local/SharedPreferenceUtil.kt
@@ -2,37 +2,17 @@ package com.hhi.myapplication.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
-import org.json.JSONArray
 
 private const val FILENAME: String = "prefs"
-private const val PREF_QUERY_LIST: String = "pref_query_list"
 
 class SharedPreferenceUtil(context: Context) {
     private val preference: SharedPreferences = context.getSharedPreferences(FILENAME, 0)
 
-    fun saveQuery(query: String) {
-        val queryList = getQueryList().toMutableList()
-        queryList.add(query)
-
-        if (queryList.size > 5) {
-            queryList.removeAt(0)
-        }
-
-        preference.edit().putString(PREF_QUERY_LIST, JSONArray(queryList).toString()).apply()
-
+    fun setString(value: String, key: String) {
+        preference.edit().putString(key, value).apply()
     }
 
-    fun getQueryList(): List<String> {
-        val json = preference.getString(PREF_QUERY_LIST, null)
-        val queryList = mutableListOf<String>()
-
-        json?.let {
-            val jsonArray = JSONArray(it)
-            for (i in 0 until jsonArray.length()) {
-                queryList.add(jsonArray[i].toString())
-            }
-        }
-
-        return queryList
+    fun getString(key: String): String? {
+        return preference.getString(key, null)
     }
 }
