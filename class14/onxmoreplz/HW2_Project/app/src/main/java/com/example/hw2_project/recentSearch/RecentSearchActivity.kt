4bfcd6b5f43dc96fbca587b34aba713340f07272
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-25 21:57:38 +0900

    최근 검색어 목록 Activity 및 Layout파일 생성

---
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/recentSearch/RecentSearchActivity.kt
@@ -0,0 +1,40 @@
+package com.example.hw2_project.recentSearch
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.hw2_project.R
+import com.example.hw2_project.data.repository.MovieRepositoryImpl
+import com.example.hw2_project.databinding.ActivityRecentSearchBinding
+import com.example.hw2_project.main.MainActivity
+
+class RecentSearchActivity : AppCompatActivity(), RecentSearchContract.View {
+    private lateinit var binding : ActivityRecentSearchBinding
+
+    private val recentSearchAdapter = RecentSearchRecyclerViewAdapter()
+
+    private val recentSearchPresenter =  RecentSearchPresenter(
+        this,
+        MovieRepositoryImpl()
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recent_search)
+
+        binding.rvRecentList.setHasFixedSize(true)
+        binding.rvRecentList.adapter = recentSearchAdapter
+
+        recentSearchPresenter.searchRecentQuery()
+    }
+
+    override fun showRecentSearchMovieList(list: List<String>) {
+        recentSearchAdapter.updateMovieList(list)
+    }
+
+    fun clickBackBtn() {
+        this.finish()
+    }
+}
