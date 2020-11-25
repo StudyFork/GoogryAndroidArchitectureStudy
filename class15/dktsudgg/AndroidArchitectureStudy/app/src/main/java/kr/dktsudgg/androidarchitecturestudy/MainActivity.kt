1df608ab054247ed017f1406b176016de7e276e6
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-11-25 18:56:54 +0900

    HW-4 메인 액티비티에 영화검색이력 조회 버튼 추가 및 클릭 시, 영화검색이력 액티비티로 이동하도록 구현

--- class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/MainActivity.kt
+++ class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.dktsudgg.androidarchitecturestudy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
@@ -21,6 +22,7 @@ class MainActivity : BaseActivity<MoviePresenter>(), MovieContract.View, View.On
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this@MainActivity
 
         moviePresenter = MoviePresenter(NaverMovieRepositoryImpl(), this);
 
@@ -35,20 +37,21 @@ class MainActivity : BaseActivity<MoviePresenter>(), MovieContract.View, View.On
             )
         )
 
-        /**
-         * 검색 버튼 OnClicklistener 등록
-         */
-        movieSearchBtn.setOnClickListener(this)
     }
 
     /**
      * 클릭 이벤트 처리 메소드
      */
-    override fun onClick(clickedView: View?) {
-        when (clickedView?.id) {
+    override fun onClick(clickedView: View) {
+        when (clickedView.id) {
             R.id.movieSearchBtn -> {    // 검색 버튼 클릭 시, 검색어 입력한 내용을 가지고 검색 수행
                 val searchText = movieSearchEditText.text.toString()
                 moviePresenter.searchMovies(searchText)
+            }
+            R.id.showMovieSearchHistoryBtn -> { // 최근검색이력 버튼 클릭 시, 영화검색이력을 보여주는 액티비티로 이동
+                startActivity(
+                    Intent(this, MovieSearchHistoryActivity::class.java)
+                )
             }
             else -> {
             }
