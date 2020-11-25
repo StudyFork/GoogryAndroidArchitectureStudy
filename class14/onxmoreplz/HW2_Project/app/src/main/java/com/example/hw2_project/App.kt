958c6073d37b807a6c343dbd4f120f7050fe22c2
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-25 22:03:12 +0900

    검색어 저장 구현을 위한 SharedPreference 사용

---
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/App.kt
@@ -0,0 +1,14 @@
+package com.example.hw2_project
+
+import android.app.Application
+
+class App  : Application() {
+    companion object {
+        lateinit var prefs : SharedPreferenceUtil
+    }
+
+    override fun onCreate() {
+        prefs = SharedPreferenceUtil(applicationContext)
+        super.onCreate()
+    }
+}
---
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/SharedPreferenceUtil.kt
@@ -0,0 +1,34 @@
+package com.example.hw2_project
+
+import android.content.Context
+import android.content.SharedPreferences
+import org.json.JSONArray
+
+class SharedPreferenceUtil( context : Context) {
+    private val preference : SharedPreferences = context.getSharedPreferences("prefs", 0)
+
+    fun saveQuery(query : String) {
+        val queryList = getSavedQueryList().toMutableList()
+        queryList.add(query)
+
+        if(queryList.size > 5) {
+            queryList.removeAt(0)
+        }
+
+        preference.edit().putString("pref_query_list", JSONArray(queryList).toString()).apply()
+
+    }
+
+    fun getSavedQueryList() : List<String> {
+        val json = preference.getString("pref_query_list", null)
+        val queryList = mutableListOf<String>()
+
+        json?.let {
+            val jsonArray = JSONArray(it)
+            for(i in 0 until jsonArray.length()) {
+                queryList.add(jsonArray[i].toString())
+            }
+        }
+        return queryList
+    }
+}
