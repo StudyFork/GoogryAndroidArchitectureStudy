44194e21bd8ae34a3cd3b1f179540d85788ca9bd
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-25 22:07:34 +0900

    RecentSearchMovie 액티비티 생성에 따른 Contract와 Presenter 추가

---
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/recentSearch/RecentSearchContract.kt
@@ -0,0 +1,10 @@
+package com.example.hw2_project.recentSearch
+
+interface RecentSearchContract {
+    interface View {
+        fun showRecentSearchMovieList(list : List<String>)
+    }
+    interface Presenter {
+        fun searchRecentQuery()
+    }
+}
