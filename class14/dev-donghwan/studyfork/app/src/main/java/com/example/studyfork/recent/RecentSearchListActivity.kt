e0ea4e7d0b3f445d5d1b87c237b795ff24edfa6d
donghwan <ycdoit@naver.com>
2020-11-26 22:18:48 +0900

    Add RecentSearchListActivity implements View

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchListActivity.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchListActivity.kt
@@ -1,17 +1,21 @@
 package com.example.studyfork.recent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.studyfork.R
 import com.example.studyfork.base.BaseActivity
 import com.example.studyfork.databinding.ActivityRecentSearchListBinding
 
 class RecentSearchListActivity :
-    BaseActivity<ActivityRecentSearchListBinding>(R.layout.activity_recent_search_list) {
+    BaseActivity<ActivityRecentSearchListBinding>(R.layout.activity_recent_search_list),
+    RecentSearchListContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.activity = this
 
+    }
+
+    override fun showRecentSearchList(list: List<String>) {
+        TODO("Not yet implemented")
     }
 }
