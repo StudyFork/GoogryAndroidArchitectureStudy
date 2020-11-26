0d354964004666db81b0a0501c93f040f0a51334
MJ Choi <1621139@gmail.com>
2020-11-27 04:20:47 +0900

    HW-4 UPDATE class to interface in Contract

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/searchhistory/SearchHistoryContract.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/searchhistory/SearchHistoryContract.kt
@@ -3,7 +3,7 @@ package com.architecture.androidarchitecturestudy.ui.searchhistory
 import com.architecture.androidarchitecturestudy.data.model.SearchHistoryEntity
 import com.architecture.androidarchitecturestudy.ui.base.BaseContract
 
-class SearchHistoryContract {
+interface SearchHistoryContract {
     interface View : BaseContract.View {
         fun setSearchHistoryList(list: List<SearchHistoryEntity>)
         fun onSearchHistoryMovie(keyword: String)
