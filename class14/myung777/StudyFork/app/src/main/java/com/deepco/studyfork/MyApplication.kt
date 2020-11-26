15821e2ec2bb0e521ee31446f12a06925e773b60
myung777 <ae3994@naver.com>
2020-11-27 00:47:31 +0900

    Update MyApplication

--- class14/myung777/StudyFork/app/src/main/java/com/deepco/studyfork/MyApplication.kt
+++ class14/myung777/StudyFork/app/src/main/java/com/deepco/studyfork/MyApplication.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import com.deepco.studyfork.data.local.SharedPreferenceUtil
 
 class MyApplication : Application() {
-    companion object {
-        lateinit var prefs: SharedPreferenceUtil
+    override fun onCreate() {
+        super.onCreate()
+        prefs = SharedPreferenceUtil(applicationContext)
     }
 
-    override fun onCreate() {
-        prefs = SharedPreferenceUtil(applicationContext)
-        super.onCreate()
+    companion object {
+        lateinit var prefs: SharedPreferenceUtil
     }
 }
