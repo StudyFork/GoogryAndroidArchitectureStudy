4741830ddef063bb2e78b4b27d2fe87fb67cf62f
donghwan <ycdoit@naver.com>
2020-11-26 22:17:49 +0900

    Add RecentResearchListPresenter

---
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchListPresenter.kt
@@ -0,0 +1,9 @@
+package com.example.studyfork.recent
+
+class RecentSearchListPresenter(private val view: RecentSearchListContract.View) :
+    RecentSearchListContract.Presenter {
+
+    override fun getRecentSearchList() {
+        TODO("Not yet implemented")
+    }
+}
