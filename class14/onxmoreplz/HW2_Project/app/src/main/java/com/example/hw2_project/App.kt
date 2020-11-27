23c07c9170eb6e0afc83c41509c546517e8a2dba
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-27 23:44:01 +0900

    companion object 코드 위치 수정

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/App.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/App.kt
@@ -3,12 +3,13 @@ package com.example.hw2_project
 import android.app.Application
 
 class App : Application() {
-    companion object {
-        lateinit var prefs: SharedPreferenceUtil
-    }
-
     override fun onCreate() {
         prefs = SharedPreferenceUtil(applicationContext)
         super.onCreate()
     }
+    
+    companion object {
+        lateinit var prefs: SharedPreferenceUtil
+    }
+
 }
