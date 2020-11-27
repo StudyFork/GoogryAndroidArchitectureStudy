cb66eda56bd7bdeca386021dd2af7fcf23335b4f
hhi <hhanhi5258@gmail.com>
2020-11-27 16:27:12 +0900

    HW-4 ViewDataBinding BaseActivity 에서 처리

--- class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/recentSearch/RecentSearchActivity.kt
+++ class15/hhi-5258/HHiApplication/app/src/main/java/com/hhi/myapplication/recentSearch/RecentSearchActivity.kt
@@ -1,16 +1,15 @@
 package com.hhi.myapplication.recentSearch
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.hhi.myapplication.R
 import com.hhi.myapplication.base.BaseActivity
 import com.hhi.myapplication.data.repository.NaverRepositoryDataSourceImpl
 import com.hhi.myapplication.databinding.ActivityRecentSearchBinding
 import kotlinx.android.synthetic.main.activity_recent_search.*
 
-class RecentSearchActivity : BaseActivity<RecentSearchContract.Presenter>(),
+class RecentSearchActivity :
+    BaseActivity<RecentSearchContract.Presenter, ActivityRecentSearchBinding>(R.layout.activity_recent_search),
     RecentSearchContract.View {
-    private lateinit var binding: ActivityRecentSearchBinding
     private val recyclerAdapter = RecentSearchRecyclerAdapter()
     private val recentSearchPresenter = RecentSearchPresenter(
         this,
@@ -19,14 +18,13 @@ class RecentSearchActivity : BaseActivity<RecentSearchContract.Presenter>(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_recent_search)
 
-        setUpUI()
+        setUpUi()
 
         recentSearchPresenter.searchRecentQuery()
     }
 
-    private fun setUpUI() {
+    private fun setUpUi() {
         recent_search_recyclerview.setHasFixedSize(false)
         recent_search_recyclerview.adapter = recyclerAdapter
     }
