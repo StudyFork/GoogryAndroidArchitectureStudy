8d66952d8fdcae6ad1315515dc4568b567f75892
Miso Park <showmiso@gmail.com>
2020-11-27 19:51:14 +0900

    HW-4 : Change sharedPreference to nullable

--- class15/showmiso/ArchitectureStudy/app/src/main/java/com/showmiso/architecturestudy/MainActivity.kt
+++ class15/showmiso/ArchitectureStudy/app/src/main/java/com/showmiso/architecturestudy/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity(), MovieContract.View {
             naverRepository = run {
                 NaverRepositoryImpl(
                     RemoteDataSourceImpl(),
-                    LocalDataSourceImpl(this@MainActivity)
+                    LocalDataSourceImpl()
                 )
             }
         )
--- class15/showmiso/ArchitectureStudy/app/src/main/java/com/showmiso/architecturestudy/data/local/LocalDataSourceImpl.kt
+++ class15/showmiso/ArchitectureStudy/app/src/main/java/com/showmiso/architecturestudy/data/local/LocalDataSourceImpl.kt
@@ -4,12 +4,14 @@ import android.content.SharedPreferences
 import com.showmiso.architecturestudy.Constants
 
 class LocalDataSourceImpl(
-    private val prefs: SharedPreferences
+    private val prefs: SharedPreferences? = null
 ) : LocalDataSource {
 
     private var searchHistoryText: String?
-        get() = prefs.getString(Constants.PREF_HISTORY_DATA, null)
-        set(value) = prefs.edit().putString(Constants.PREF_HISTORY_DATA, value).apply()
+        get() = prefs?.getString(Constants.PREF_HISTORY_DATA, null)
+        set(value) {
+            prefs?.edit()?.putString(Constants.PREF_HISTORY_DATA, value)?.apply()
+        }
 
     private val regex = ","
 
