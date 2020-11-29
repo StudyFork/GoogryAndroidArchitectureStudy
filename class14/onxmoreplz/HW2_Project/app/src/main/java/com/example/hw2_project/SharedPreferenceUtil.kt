e248add35afc088556bba026cb408af8e7a521bf
onxmoreplz <hamdoyoung95@gmail.com>
2020-11-29 18:07:49 +0900

    SharedPreference 관련 로직 처리 코드 Repository로 이동

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/SharedPreferenceUtil.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/SharedPreferenceUtil.kt
@@ -1,35 +1,19 @@
 package com.example.hw2_project
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import org.json.JSONArray
 
 class SharedPreferenceUtil(context: Context) {
-    private val preference: SharedPreferences =
-        context.getSharedPreferences("recentSearchedMovie", 0) //"prefs"
+    private val settings: SharedPreferences =
+        context.getSharedPreferences("recentMovieFile", MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = settings.edit()
 
-    fun saveQuery(query: String) {
-        val queryList = getSavedQueryList().toMutableList()
-        queryList.add(query)
-
-        if (queryList.size > 5) {
-            queryList.removeAt(0)
-        }
-
-        preference.edit().putString("pref_query_list", JSONArray(queryList).toString()).apply()
-
+    fun saveQueryList(queryList: MutableList<String>) {
+        editor.putString("recent_movie_data", JSONArray(queryList).toString()).apply()
     }
 
-    fun getSavedQueryList(): List<String> {
-        val json = preference.getString("pref_query_list", null)
-        val queryList = mutableListOf<String>()
-
-        json?.let {
-            val jsonArray = JSONArray(it)
-            for (i in 0 until jsonArray.length()) {
-                queryList.add(jsonArray[i].toString())
-            }
-        }
-        return queryList
-    }
+    fun getSavedQueryListTest(): String? =
+        settings.getString("recent_movie_data", null)
 }
--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/local/MovieLocalDataSourceImpl.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/local/MovieLocalDataSourceImpl.kt
@@ -1,13 +1,30 @@
 package com.example.hw2_project.data.local
 
 import com.example.hw2_project.App
+import com.google.gson.Gson
+import org.json.JSONArray
 
 class MovieLocalDataSourceImpl : MovieLocalDataSource {
     override fun saveQuery(query: String) {
-        App.prefs.saveQuery(query)
+        val queryList = getSavedQuery().toMutableList()
+        queryList.add(query)
+
+        if (queryList.size > 5) {
+            queryList.removeAt(0)
+        }
+        App.prefs.saveQueryList(queryList)
     }
 
     override fun getSavedQuery(): List<String> {
-        return App.prefs.getSavedQueryList()
+        val json = App.prefs.getSavedQueryListTest()
+        val queryList = mutableListOf<String>()
+
+        json?.let {
+            val jsonArray = JSONArray(it)
+            for (i in 0 until jsonArray.length()) {
+                queryList.add(jsonArray[i].toString())
+            }
+        }
+        return queryList
     }
 }
