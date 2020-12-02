9e16ee540dc7b016deea6d5ae43120ecbdd1f61c
MJ Choi <1621139@gmail.com>
2020-12-03 03:45:51 +0900

    HW-5 ADD bindingAdapter about recyclerview

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/adapter/DataBindingAdapter.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/adapter/DataBindingAdapter.kt
@@ -4,6 +4,10 @@ package com.architecture.androidarchitecturestudy.adapter
 import android.widget.ImageView
 import android.widget.RatingBar
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.architecture.androidarchitecturestudy.data.model.Movie
+import com.architecture.androidarchitecturestudy.data.model.SearchHistoryEntity
+import com.architecture.androidarchitecturestudy.ui.searchhistory.SearchHistoryAdapter
 import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
@@ -16,4 +20,35 @@ fun imageUrl(view: ImageView, url: String?) {
 @BindingAdapter("bindRatingStar")
 fun bindRatingStar(view: RatingBar, userRating: Double) {
     view.rating = (userRating / 2).toFloat()
+}
+
+@BindingAdapter("setMovieList")
+fun RecyclerView.setMovieList(list: List<Movie>?) {
+
+    if (list == null) return
+
+    val movieAdapter: MovieAdapter
+
+    if (adapter == null) {
+        movieAdapter = MovieAdapter()
+        adapter = movieAdapter
+    } else {
+        movieAdapter = adapter as MovieAdapter
+    }
+    movieAdapter.setMovieList(list)
+}
+
+@BindingAdapter("setSearchHistories")
+fun RecyclerView.setSearchHistories(list: List<SearchHistoryEntity>?) {
+    if (list == null) return
+
+    val historyAdapter: SearchHistoryAdapter
+
+    if (adapter == null) {
+        historyAdapter = SearchHistoryAdapter()
+        adapter = historyAdapter
+    } else {
+        historyAdapter = adapter as SearchHistoryAdapter
+    }
+    historyAdapter.setSearchHistories(list)
 }
