d1aab00383cde44f2d89eb1ffccfed52fd6e04a5
simuelunbo <simuelunbo@gmail.com>
2020-12-02 21:44:27 +0900

    HW-5: BaseActivity 수정

--- class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/base/BaseActivity.kt
+++ class14/simuelunbo/AndroidArchitectureStudy/app/src/main/java/com/example/androidarchitecturestudy/ui/base/BaseActivity.kt
@@ -1,20 +1,15 @@
 package com.example.androidarchitecturestudy.ui.base
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<presenter : BaseContract.Presenter, binding : ViewDataBinding>(@LayoutRes val layout: Int) :
-    AppCompatActivity(layout), BaseContract.View {
+abstract class BaseActivity<binding : ViewDataBinding>(@LayoutRes val layout: Int) :
+    AppCompatActivity() {
 
     lateinit var binding: binding
-
-    override fun showResultEmpty(error: String) {
-        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
