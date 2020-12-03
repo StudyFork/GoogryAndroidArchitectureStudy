d6303122a8a6ad0f14797a7b937676ecb58320ed
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-12-04 01:01:39 +0900

    HW-5 영화검색이력 액티비티 올라올 시, 뷰모델에 영화 검색이력 데이터 초기세팅하는 부분을 리팩토링

--- class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/MovieSearchHistoryActivity.kt
+++ class15/dktsudgg/AndroidArchitectureStudy/app/src/main/java/kr/dktsudgg/androidarchitecturestudy/MovieSearchHistoryActivity.kt
@@ -7,17 +7,16 @@ import kr.dktsudgg.androidarchitecturestudy.view.ui.mvvm.MovieSearchHistoryViewM
 class MovieSearchHistoryActivity :
     BaseActivity<ActivityMovieSearchHistoryBinding>(R.layout.activity_movie_search_history) {
 
-    private val movieSearchHistoryViewModel by lazy { MovieSearchHistoryViewModel() }
+    private val movieSearchHistoryViewModel by lazy {
+        val vm = MovieSearchHistoryViewModel()
+        vm.refreshMovieSearchHistoryList()
+        vm
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.vm = movieSearchHistoryViewModel
-
-        /**
-         * 로컬 저장소로부터 영화 검색 이력 조회
-         */
-        movieSearchHistoryViewModel.refreshMovieSearchHistoryList()
     }
 
 }
