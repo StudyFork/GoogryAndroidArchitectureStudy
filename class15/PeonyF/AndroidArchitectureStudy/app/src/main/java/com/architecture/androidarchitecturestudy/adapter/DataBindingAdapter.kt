7e7af17f26f45cef95bafb600cbd3b4d58375e4c
MJ Choi <1621139@gmail.com>
2020-12-05 02:00:22 +0900

    HW-5 UPDATE kotlin style

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/adapter/DataBindingAdapter.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/adapter/DataBindingAdapter.kt
@@ -23,29 +23,16 @@ fun bindRatingStar(view: RatingBar, userRating: Double) {
 
 @BindingAdapter("setMovieList")
 fun RecyclerView.setMovieList(list: List<Movie>?) {
-    val movieAdapter: MovieAdapter
-    if (list == null) return
-
-    if (adapter == null) {
-        movieAdapter = MovieAdapter()
-        adapter = movieAdapter
-    } else {
-        movieAdapter = adapter as MovieAdapter
-    }
+    list ?: return
+    val movieAdapter =
+        adapter as? MovieAdapter ?: MovieAdapter().apply { adapter = this }
     movieAdapter.setMovieList(list)
 }
 
 @BindingAdapter("setSearchHistories")
 fun RecyclerView.setSearchHistories(list: List<SearchHistoryEntity>?) {
-    val historyAdapter: SearchHistoryAdapter
-
-    if (list == null) return
-
-    if (adapter == null) {
-        historyAdapter = SearchHistoryAdapter()
-        adapter = historyAdapter
-    } else {
-        historyAdapter = adapter as SearchHistoryAdapter
-    }
+    list ?: return
+    val historyAdapter =
+        adapter as? SearchHistoryAdapter ?: SearchHistoryAdapter().apply { adapter = this }
     historyAdapter.setSearchHistories(list)
 }
