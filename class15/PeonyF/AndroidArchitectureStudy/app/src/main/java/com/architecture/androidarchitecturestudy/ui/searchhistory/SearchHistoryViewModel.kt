79d7942ab23147c13bad131265c13f797eb716c4
MJ Choi <1621139@gmail.com>
2020-12-10 03:32:41 +0900

    HW-6 ADD lifecycle function ObservableField to MutableLiveData in SearchHistoryViewModel

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/searchhistory/SearchHistoryViewModel.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/searchhistory/SearchHistoryViewModel.kt
@@ -1,13 +1,14 @@
 package com.architecture.androidarchitecturestudy.ui.searchhistory
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.architecture.androidarchitecturestudy.data.model.SearchHistoryEntity
 import com.architecture.androidarchitecturestudy.data.repository.MovieRepository
 
 class SearchHistoryViewModel(
     private val movieRepository: MovieRepository
-) {
-    val keywordList = ObservableField<List<SearchHistoryEntity>>()
+) : ViewModel() {
+    val keywordList = MutableLiveData<List<SearchHistoryEntity>>()
     fun getRecentKeywordList() {
         keywordList.set(movieRepository.getSearchHistoryList())
     }
