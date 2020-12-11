b3e107e87e7da8a9a4b300c26ce7b7cd63857214
donghwan <ycdoit@naver.com>
2020-12-11 20:41:52 +0900

    HW6 : Fix RecentSearchViewModel

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchViewModel.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchViewModel.kt
@@ -1,21 +1,21 @@
 package com.example.studyfork.recent
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import com.example.studyfork.base.BaseViewModel
 import com.example.studyfork.data.repository.Repository
+import io.reactivex.subjects.PublishSubject
 
 class RecentSearchViewModel(private val repository: Repository) : BaseViewModel() {
 
-    val recentSearchList: ObservableField<List<String>> = ObservableField()
-    val showListIsEmpty: ObservableField<Unit> = ObservableField()
+    val recentSearchList: MutableLiveData<List<String>> = MutableLiveData()
+    val showListIsEmpty: PublishSubject<Unit> = PublishSubject.create()
 
     fun getRecentSearchList() {
         val result = repository.getRecentSearchList().reversed()
         if (result.isEmpty()) {
-            showListIsEmpty.notifyChange()
+            showListIsEmpty.onNext(Unit)
         } else {
-            recentSearchList.set(result)
-            recentSearchList.notifyChange()
+            recentSearchList.value = result
         }
     }
 }
