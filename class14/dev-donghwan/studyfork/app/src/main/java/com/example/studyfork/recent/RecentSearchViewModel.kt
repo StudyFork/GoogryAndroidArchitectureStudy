934b627171155db2f488cb400cac59da3102e237
donghwan <ycdoit@naver.com>
2020-12-11 20:43:43 +0900

    HW6 : Add RecentSearchViewModelFactory

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchViewModel.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.studyfork.recent
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.studyfork.base.BaseViewModel
 import com.example.studyfork.data.repository.Repository
 import io.reactivex.subjects.PublishSubject
@@ -16,6 +18,16 @@ class RecentSearchViewModel(private val repository: Repository) : BaseViewModel(
             showListIsEmpty.onNext(Unit)
         } else {
             recentSearchList.value = result
+        }
+    }
+}
+
+class RecentSearchViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(RecentSearchViewModel::class.java)) {
+            RecentSearchViewModel(repository) as T
+        } else {
+            throw IllegalArgumentException()
         }
     }
 }
