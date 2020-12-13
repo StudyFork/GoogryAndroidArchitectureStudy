49c840e813aac7ec79014e5deeb775d3f103ce34
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-13 12:50:26 +0900

    HW-7 Bind repository, remote datasource, local datasource

--- class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieLocalDataSourceImpl.kt
+++ class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieLocalDataSourceImpl.kt
@@ -4,8 +4,14 @@ import com.jay.aas.model.Movie
 import com.jay.aas.model.SearchHistory
 import com.jay.aas.room.MovieDao
 import com.jay.aas.room.SearchHistoryDao
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class MovieLocalDataSourceImpl(
+class MovieLocalDataSourceImpl @Inject constructor(
     private val movieDao: MovieDao,
     private val searchHistoryDao: SearchHistoryDao,
 ) : MovieLocalDataSource {
@@ -23,5 +29,15 @@ class MovieLocalDataSourceImpl(
 
     override fun getSearchHistories(): List<SearchHistory> =
         searchHistoryDao.getSearchHistories()
+
+}
+
+@InstallIn(ApplicationComponent::class)
+@Module
+abstract class MovieLocalDataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMovieLocalDataSource(movieLocalDataSourceImpl: MovieLocalDataSourceImpl): MovieLocalDataSource
 
 }
