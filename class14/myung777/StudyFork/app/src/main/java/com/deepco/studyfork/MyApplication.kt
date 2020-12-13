40d4f5091b5d89c0c28fa955d75b8eee1519d712
myung777 <ae3994@naver.com>
2020-12-13 17:09:44 +0900

    HW-7 Application Hilt 적용

--- class14/myung777/StudyFork/app/src/main/java/com/deepco/studyfork/MyApplication.kt
+++ class14/myung777/StudyFork/app/src/main/java/com/deepco/studyfork/MyApplication.kt
@@ -2,7 +2,9 @@ package com.deepco.studyfork
 
 import android.app.Application
 import com.deepco.studyfork.data.local.SharedPreferenceUtil
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
