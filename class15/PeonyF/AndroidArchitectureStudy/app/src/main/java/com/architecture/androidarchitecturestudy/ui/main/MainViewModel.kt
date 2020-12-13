dfe5eacbe94631b75479728a7ac9bbd8da461b71
MJ Choi <1621139@gmail.com>
2020-12-14 00:03:48 +0900

    HW-6 refactor connect SingleLiveData class and delete pending variable

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/main/MainViewModel.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/ui/main/MainViewModel.kt
@@ -4,19 +4,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.architecture.androidarchitecturestudy.data.model.Movie
 import com.architecture.androidarchitecturestudy.data.repository.MovieRepository
-import java.util.concurrent.atomic.AtomicBoolean
+import com.architecture.androidarchitecturestudy.util.SingleLiveEvent
 
 class MainViewModel(private val movieRepository: MovieRepository) : ViewModel() {
     val keyword = MutableLiveData<String>()
     val movieList = MutableLiveData<List<Movie>>()
-    val msg = MutableLiveData<String>()
+    val msg = SingleLiveEvent<String>()
     val callSearchHistory = MutableLiveData<Unit>()
-    val pending: AtomicBoolean = AtomicBoolean(false)
-
 
     fun findMovie() {
         val keywordValue = keyword.value ?: return
-        pending.set(true)
         if (keywordValue.isNullOrBlank()) {
             msg.value = "emptyKeyword"
             return
