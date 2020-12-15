486b4251f5945bf018e6c7d97909cf9f0e5c23f9
MJ Choi <1621139@gmail.com>
2020-12-15 23:35:21 +0900

    HW-7 ADD Hilt annotation in application class

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/util/App.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/util/App.kt
@@ -2,7 +2,9 @@ package com.architecture.androidarchitecturestudy.util
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
