83df9dcf34452ae98d02ed5a493a00e7d2091803
onxmoreplz <hamdoyoung95@gmail.com>
2020-12-15 21:17:46 +0900

    Hilt 사용을 위한 Application 클래스에 annotation 추가

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/App.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/App.kt
@@ -1,7 +1,9 @@
 package com.example.hw2_project
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
