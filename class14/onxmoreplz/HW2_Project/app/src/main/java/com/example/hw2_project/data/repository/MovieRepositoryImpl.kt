a696ecb8b76b1bf737a751c30fe1ce03c1ceddca
onxmoreplz <hamdoyoung95@gmail.com>
2020-12-15 22:01:43 +0900

    Repository 의존성 주입

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/repository/MovieRepositoryImpl.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/repository/MovieRepositoryImpl.kt
@@ -3,11 +3,17 @@ package com.example.hw2_project.data.repository
 import com.example.hw2_project.data.api.NaverMovieData
 import com.example.hw2_project.data.local.MovieLocalDataSourceImpl
 import com.example.hw2_project.data.remote.MovieRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class MovieRepositoryImpl : MovieRepository {
-
-    private val remoteMovieDataSourceImp = MovieRemoteDataSourceImpl()
-    private val localMovieLocalDataSourceImpl = MovieLocalDataSourceImpl()
+class MovieRepositoryImpl @Inject constructor(
+    private val remoteMovieDataSourceImp: MovieRemoteDataSourceImpl,
+    private val localMovieLocalDataSourceImpl: MovieLocalDataSourceImpl
+) : MovieRepository {
 
     override fun getMovieList(
         query: String,
@@ -29,4 +35,15 @@ class MovieRepositoryImpl : MovieRepository {
     override fun getSavedQuery(): List<String> {
         return localMovieLocalDataSourceImpl.getSavedQuery()
     }
+}
+
+@Module
+@InstallIn(ApplicationComponent::class)
+abstract class MovieRepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMovieRepository(
+        movieRepositoryImpl: MovieRepositoryImpl
+    ): MovieRepository
 }
