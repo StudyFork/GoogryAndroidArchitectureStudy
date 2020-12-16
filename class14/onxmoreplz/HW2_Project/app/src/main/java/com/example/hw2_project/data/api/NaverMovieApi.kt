5f9b883768a426f02fd7ded01fee8eed95905734
onxmoreplz <hamdoyoung95@gmail.com>
2020-12-16 20:11:21 +0900

    NaverMovieApi 인터페이스 내 create함수 코드 이동(-> NaverMovieApiModule)

--- class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/api/NaverMovieApi.kt
+++ class14/onxmoreplz/HW2_Project/app/src/main/java/com/example/hw2_project/data/api/NaverMovieApi.kt
@@ -1,11 +1,6 @@
 package com.example.hw2_project.data.api
 
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -16,36 +11,4 @@ interface NaverMovieApi {
         @Query("query") query: String
     ): Call<NaverMovieData.NaverMovieResponse>
 
-    companion object {
-        private const val NAVER_MOVIE_BASE_URL = "https://openapi.naver.com"
-
-        private const val NAVER_CLIENT_ID = "fQFY7M9rMOVD2KDT8Aaq"
-        private const val NAVER_CLIENT_SECRET = "v8aD8p_Ri0"
-
-        fun create(): NaverMovieApi {
-
-            val httpLoggingInterceptor = HttpLoggingInterceptor()
-            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-            val headerInterceptor = Interceptor {
-                val request = it.request()
-                    .newBuilder()
-                    .addHeader("X-Naver-Client-Id", NAVER_CLIENT_ID)
-                    .addHeader("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
-                    .build()
-                return@Interceptor it.proceed(request)
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(headerInterceptor)
-                .addInterceptor(httpLoggingInterceptor)
-                .build()
-
-            return Retrofit.Builder()
-                .baseUrl(NAVER_MOVIE_BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(NaverMovieApi::class.java)
-        }
-
-    }
 }
