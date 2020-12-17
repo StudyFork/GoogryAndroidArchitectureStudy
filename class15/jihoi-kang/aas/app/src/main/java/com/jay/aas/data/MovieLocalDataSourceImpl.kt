65910515b2f10e296586378a0c26ab53d1dcd265
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-17 19:12:45 +0900

    Add Room module

--- class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieLocalDataSourceImpl.kt
+++ class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieLocalDataSourceImpl.kt
@@ -2,7 +2,8 @@ package com.jay.aas.data
 
 import com.jay.aas.model.Movie
 import com.jay.aas.model.SearchHistory
-import com.jay.aas.room.RoomHelper
+import com.jay.aas.room.MovieDao
+import com.jay.aas.room.SearchHistoryDao
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,22 +12,23 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 class MovieLocalDataSourceImpl @Inject constructor(
-    private val roomHelper: RoomHelper,
+    private val movieDao: MovieDao,
+    private val searchHistoryDao: SearchHistoryDao,
 ) : MovieLocalDataSource {
 
     override fun insertMovies(query: String, movies: List<Movie>) {
-        roomHelper.movieDao.insertMovies(movies)
-        roomHelper.searchHistoryDao.insertSearchHistory(SearchHistory(queryText = query))
+        movieDao.insertMovies(movies)
+        searchHistoryDao.insertSearchHistory(SearchHistory(queryText = query))
     }
 
-    override fun getMovies(): List<Movie> = roomHelper.movieDao.getMovies()
+    override fun getMovies(): List<Movie> = movieDao.getMovies()
 
     override fun clearMovies() {
-        roomHelper.movieDao.clearMovies()
+        movieDao.clearMovies()
     }
 
     override fun getSearchHistories(): List<SearchHistory> =
-        roomHelper.searchHistoryDao.getSearchHistories()
+        searchHistoryDao.getSearchHistories()
 
 }
 
