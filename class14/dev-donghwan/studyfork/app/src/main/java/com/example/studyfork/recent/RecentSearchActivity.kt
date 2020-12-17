ee26204550d1e1c0e2e241d1f59e35395a2df959
donghwan <ycdoit@naver.com>
2020-12-17 21:12:45 +0900

    HW& : Fix RecentActivity

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchActivity.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/recent/RecentSearchActivity.kt
@@ -2,30 +2,19 @@ package com.example.studyfork.recent
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.viewModels
 import com.example.studyfork.R
 import com.example.studyfork.RecentSearchAdapter
 import com.example.studyfork.base.BaseActivity
-import com.example.studyfork.data.local.LocalDataSourceImpl
-import com.example.studyfork.data.remote.RemoteDataSourceImpl
-import com.example.studyfork.data.repository.RepositoryImpl
 import com.example.studyfork.databinding.ActivityRecentSearchListBinding
 import com.example.studyfork.main.MainActivity.Companion.SEARCH_ITEM
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecentSearchActivity :
     BaseActivity<ActivityRecentSearchListBinding>(R.layout.activity_recent_search_list) {
 
-    private val viewModel by lazy {
-        ViewModelProvider(
-            this,
-            RecentSearchViewModelFactory(
-                RepositoryImpl(
-                    RemoteDataSourceImpl(),
-                    LocalDataSourceImpl(getSharedPreferences("local", MODE_PRIVATE))
-                )
-            )
-        ).get(RecentSearchViewModel::class.java)
-    }
+    private val viewModel: RecentSearchViewModel by viewModels()
 
     private lateinit var adapter: RecentSearchAdapter
 
