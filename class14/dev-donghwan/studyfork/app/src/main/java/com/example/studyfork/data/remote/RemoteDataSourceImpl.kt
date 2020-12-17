dba6b6ff72bbd8b6add2edfd9a285a801ee0586a
donghwan <ycdoit@naver.com>
2020-12-17 21:06:24 +0900

    HW7 : Fix Repository

--- class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/data/remote/RemoteDataSourceImpl.kt
+++ class14/dev-donghwan/studyfork/app/src/main/java/com/example/studyfork/data/remote/RemoteDataSourceImpl.kt
@@ -2,10 +2,22 @@ package com.example.studyfork.data.remote
 
 import com.example.studyfork.data.Network
 import com.example.studyfork.data.model.MovieSearchResponse
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import io.reactivex.Single
+import javax.inject.Inject
 
-class RemoteDataSourceImpl : RemoteDataSource {
+class RemoteDataSourceImpl @Inject constructor() : RemoteDataSource {
     override fun searchMovie(query: String): Single<MovieSearchResponse> {
         return Network.naverApi.searchMovie(query)
     }
+}
+
+@InstallIn(ApplicationComponent::class)
+@Module
+abstract class RemoteDataSourceModule {
+    @Binds
+    abstract fun bindRemoteDataSource(remoteDataSourceImpl: RemoteDataSourceImpl): RemoteDataSource
 }
