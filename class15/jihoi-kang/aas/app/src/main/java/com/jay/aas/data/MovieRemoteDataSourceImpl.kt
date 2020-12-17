7ad45c07376a2727d8ed0b5c2f775503a061faa7
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-17 19:08:01 +0900

    HW-7 Add network module

--- class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieRemoteDataSourceImpl.kt
+++ class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/data/MovieRemoteDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.jay.aas.data
 
-import com.jay.aas.api.RetrofitHelper
+import com.jay.aas.api.MovieService
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -9,11 +9,11 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 class MovieRemoteDataSourceImpl @Inject constructor(
-    private val retrofitHelper: RetrofitHelper,
+    private val movieService: MovieService,
 ) : MovieRemoteDataSource {
 
     override suspend fun getSearchMovies(query: String) =
-        retrofitHelper.movieService.getMovies(query).items
+        movieService.getMovies(query).items
 
 }
 
---
+++ class15/jihoi-kang/aas/app/src/main/java/com/jay/aas/di/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.jay.aas.di
+
+import com.jay.aas.api.MovieService
+import com.jay.aas.api.RequestInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class NetworkModule {
+
+    @Provides
+    @Reusable
+    fun provideRetrofit(): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(RequestInterceptor())
+            .build()
+
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl("https://openapi.naver.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Reusable
+    fun provideMovieService(retrofit: Retrofit): MovieService =
+        retrofit.create(MovieService::class.java)
+
+}
