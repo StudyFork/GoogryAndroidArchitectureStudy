f3f627e3a4dfa751e733dfe04c684e0c41c407a2
MJ Choi <1621139@gmail.com>
2020-12-19 02:47:20 +0900

    HW-7 UPDATE SharedPreferenceUtil apply DI

--- class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/data/local/SharedPreferenceUtil.kt
+++ class15/PeonyF/AndroidArchitectureStudy/app/src/main/java/com/architecture/androidarchitecturestudy/data/local/SharedPreferenceUtil.kt
@@ -3,14 +3,19 @@ package com.architecture.androidarchitecturestudy.data.local
 import android.content.Context
 import android.content.SharedPreferences
 import com.architecture.androidarchitecturestudy.data.model.SearchHistoryEntity
+import com.architecture.androidarchitecturestudy.util.App
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import javax.inject.Inject
+import javax.inject.Singleton
 
 private const val MOVIE_KEYWORD: String = "movieKeyword"
 private const val PREF_SEARCH_HISTORY: String = "preferenceSearchHistory"
 
-class SharedPreferenceUtil(context: Context) {
-    private val preferences: SharedPreferences = context.getSharedPreferences(MOVIE_KEYWORD, 0)
+
+@Singleton
+class SharedPreferenceUtil @Inject constructor() {
+    private val preferences: SharedPreferences = App.context.getSharedPreferences(MOVIE_KEYWORD, 0)
 
     fun saveSearchHistory(queryList: List<SearchHistoryEntity>) {
         val listJson = Gson().toJson(queryList)
